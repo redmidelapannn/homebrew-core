d9c4f2057940a01dff1ebcdd629d48f8010fb16c
Andreas Stenius <git@astekk.se>
2016-04-05 23:07:37 +0200

    bats-support 0.2.0 (new formula)
    bats-support is a supporting library providing common functions to test helper libraries written for Bats.
    https://github.com/sstephenson/bats
    https://github.com/ztombol/bats-support

---
+++ Formula/bats-support.rb
@@ -0,0 +1,18 @@
+class BatsSupport < Formula
+  desc "Supporting library for Bats test helpers"
+  homepage "https://github.com/ztombol/bats-support"
+  url "https://github.com/ztombol/bats-support/archive/v0.2.0.tar.gz"
+  sha256 "9bfa93d2db046e375e31e4c6cbbe834b015c695862c2dca1b46b71401de1038d"
+  depends_on "bats"
+
+  def install
+    system "mkdir", "bats-support"
+    system "mv", "load.bash", "src", "test", "bats-support/"
+    lib.install "bats-support"
+    ohai "Use `load '/usr/local/lib/bats-support/load'` in your bats test to load this library."
+  end
+
+  test do
+    system "bats", "#{lib}/bats-support/test"
+  end
+end
