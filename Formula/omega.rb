f1d281b32afa77d5d1dcb4239019b6ebbf666cb6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-18 01:41:34 +0100

    omega: update 1.2.18 bottle.

--- Formula/omega.rb
+++ Formula/omega.rb
@@ -5,9 +5,10 @@ class Omega < Formula
   sha256 "528feb8021a52ab06c7833cb9ebacefdb782f036e99e7ed5342046c3a82380c2"
 
   bottle do
-    revision 1
-    sha256 "68372f77905f65f4b00b8268bea8e2d17e3cfa042b0900de2f8ae58a1b0e55c2" => :el_capitan
-    sha256 "24f80ac193fc2f73440e60ff2be3aa8cdcfae9b4134223417b117d7cfb741c00" => :mavericks
+    revision 2
+    sha256 "4022655407c91189e73533232ed351360e63f7a2fcaa8aeaf03f860f6dd23a1d" => :el_capitan
+    sha256 "0dbffd02c5ea47af14496451d9845d7244f7115c6ac976bdb088814fb3d10d17" => :yosemite
+    sha256 "32d640be124c9a007fa30765b85576e227e5b2e0cc631bcf3bc095f53fe50bbb" => :mavericks
   end
 
   depends_on "pcre"
