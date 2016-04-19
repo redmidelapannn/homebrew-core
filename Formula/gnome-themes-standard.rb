8f4a39cbd03668538b9f907332ed286612a21a8f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-19 18:21:58 +0100

    gnome-themes-standard: update 3.20 bottle.

--- Formula/gnome-themes-standard.rb
+++ Formula/gnome-themes-standard.rb
@@ -6,9 +6,7 @@ class GnomeThemesStandard < Formula
 
   bottle do
     cellar :any
-    sha256 "fdd2ceb4074c51b300a9777d54d5eb6b2d37e1dc0b7f4bc70c8215bf042bfa0b" => :el_capitan
-    sha256 "594a1b8339120e31d0575234b0e9071daec6457a28c7d0d87ec8548311a0d64d" => :yosemite
-    sha256 "adcb641a47869ea2f41c6d5fd277c39496c8203256efa92fc1d8810802a693ed" => :mavericks
+    sha256 "cab185a61b4188e6e42cf90d5aae279a75c382bd715b14b5cbb0080dc04b247d" => :mavericks
   end
 
   depends_on "pkg-config" => :build
