aba987284d612a8237fb55fdd6877824dce53740
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-19 18:32:49 +0100

    docker-machine-driver-scaleway: add 1.0.1 bottle.

--- Formula/docker-machine-driver-scaleway.rb
+++ Formula/docker-machine-driver-scaleway.rb
@@ -8,6 +8,13 @@ class DockerMachineDriverScaleway < Formula
 
   head "https://github.com/scaleway/docker-machine-driver-scaleway.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "2677be89afa81d07c852b0ef3bddd19173501ae4ae3dcc1629936c77bfaa3ed2" => :el_capitan
+    sha256 "35e6dfea50b39aa8f2edb979b295aa83f9f82350c05439d4cf6c78ba6a4bc489" => :yosemite
+    sha256 "e12f0c1f36c2c54b5fd4a1d07806367cf57d133086d82274c7dd9457dc1bdbd2" => :mavericks
+  end
+
   depends_on "go" => :build
   depends_on "docker-machine" => :recommended
 
