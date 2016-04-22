9384fcff1e8340c5a6c79e82f087b7594092db95
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-23 00:18:15 +0100

    civl: add 7.3157 bottle.

--- Formula/civl.rb
+++ Formula/civl.rb
@@ -4,6 +4,13 @@ class Civl < Formula
   homepage "http://vsl.cis.udel.edu/civl/"
   url "http://vsl.cis.udel.edu/lib/sw/civl/1.7/r3157/release/CIVL-1.7_3157.tgz"
   sha256 "33fa5b4877fbf494b0a4b426c15d6a165fa3db48a9f97cbf391818d965d2dfb9"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "01b33fc3cddfc95c792a983255500335fca95c997e1e9b2e83d477682723c626" => :el_capitan
+    sha256 "aaf3d1d2b16017b0ebd40cc58ddbe856a748e042ab912320ed7861614841e384" => :yosemite
+    sha256 "f73d7bc804ca727b4653e34025b7956f658b36eec5979a991f20780c66f716e4" => :mavericks
+  end
+
   depends_on :java => "1.8+"
   depends_on "z3"
   depends_on "cvc4"
