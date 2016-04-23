d4e05069f83cf16510d7c0a9e2dedfd51814b941
Kasumi Hanazuki <kasumi@rollingapple.net>
2016-04-23 19:40:14 +0900

    png++: add test block

--- Formula/png++.rb
+++ Formula/png++.rb
@@ -14,7 +14,61 @@ class Pngxx < Formula
 
   depends_on "libpng"
 
+  # Fix strerror_r usage for OS X clang
+  # patch from <https://savannah.nongnu.org/bugs/?46312>
+  patch :DATA
+
   def install
     system "make", "PREFIX=#{prefix}", "install"
   end
+
+  test do
+    (testpath/"test.cc").write <<-EOF.undent
+      #include <png++/png.hpp>
+      #include <iostream>
+
+      int main(int argc, char **argv) {
+        try {
+          png::image<png::rgb_pixel> image(argv[1]);
+          png::rgb_pixel const pixel = image.get_pixel(0, 0);
+          if (pixel.red != 0x00 || pixel.green != 0x00 || pixel.blue != 0xFF) {
+            return 1;
+          }
+        } catch(png::error &e) {
+          std::cerr << e.what() << std::endl;
+          throw;
+        }
+        return 0;
+      }
+    EOF
+
+    system ENV.cxx, "test.cc", "-I#{include}", "-I#{Formula["libpng"].include}", "-L#{Formula["libpng"].lib}", "-lpng", "-o", "test"
+    system "./test", test_fixtures("test.png")
+  end
 end
+
+__END__
+--- a/error.hpp
++++ b/error.hpp
+@@ -93,20 +93,15 @@ namespace png
+     protected:
+         static std::string thread_safe_strerror(int errnum)
+         {
+-#define ERRBUF_SIZE 512
++#define ERRBUF_SIZE 8192
+             char buf[ERRBUF_SIZE] = { 0 };
+ 
+ #ifdef HAVE_STRERROR_S
+             strerror_s(buf, ERRBUF_SIZE, errnum);
+             return std::string(buf);
+ #else
+-#if (_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && !_GNU_SOURCE
+             strerror_r(errnum, buf, ERRBUF_SIZE);
+             return std::string(buf);
+-#else
+-            /* GNU variant can return a pointer to static buffer instead of buf */
+-            return std::string(strerror_r(errnum, buf, ERRBUF_SIZE));
+-#endif
+ #endif
+ 
+ #undef ERRBUF_SIZE
