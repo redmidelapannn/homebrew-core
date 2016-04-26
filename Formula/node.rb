a0572040854a6afb7f76c0e7773c0a06cddd2b7e
Aayush Kapoor <xeoneux@icloud.com>
2016-04-27 02:34:16 +0530

    node 6.0.0

--- Formula/node.rb
+++ Formula/node.rb
@@ -1,8 +1,8 @@
 class Node < Formula
   desc "Platform built on the V8 JavaScript runtime to build network applications"
   homepage "https://nodejs.org/"
-  url "https://nodejs.org/dist/v5.11.0/node-v5.11.0.tar.xz"
-  sha256 "083766aff72fc5c78f6999dd0d00bc0340a052ab270fd60d6928b719ca2b3fd1"
+  url "https://nodejs.org/dist/v6.0.0/node-v6.0.0.tar.xz"
+  sha256 "f0e5bdc3cf4af85b8a24bdbebed81e1a9f7fda91cab8a9475737940aa90da617"
   head "https://github.com/nodejs/node.git"
 
   bottle do
@@ -11,11 +11,11 @@ class Node < Formula
     sha256 "8254c09259aa2d869279f580e489d82181aec17b7555c2f434c290e2625906d1" => :mavericks
   end
 
-  devel do
-    url "https://nodejs.org/download/rc/v6.0.0-rc.3/node-v6.0.0-rc.3.tar.xz"
-    sha256 "472ae46a205fb65cd784b22825b7354cddb4da5f46c1974272be43e55745ce6e"
-    version "6.0.0-rc.3"
-  end
+  # devel do
+  #   url "https://nodejs.org/download/rc/v6.0.0-rc.3/node-v6.0.0-rc.3.tar.xz"
+  #   sha256 "472ae46a205fb65cd784b22825b7354cddb4da5f46c1974272be43e55745ce6e"
+  #   version "6.0.0-rc.3"
+  # end
 
   option "with-debug", "Build with debugger hooks"
   option "without-npm", "npm will not be installed"
@@ -62,7 +62,8 @@ class Node < Formula
     else
       args << "--with-intl=small-icu"
     end
-    args << "--tag=rc.3" << "--release-urlbase=https://nodejs.org/download/rc/" if build.devel?
+    # for devel releases
+    # args << "--tag=rc.3" << "--release-urlbase=https://nodejs.org/download/rc/" if build.devel?
     args << "--tag=head" if build.head?
 
     resource("icu4c").stage buildpath/"deps/icu"
