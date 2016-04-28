48660e31c92e94b2722ffb18ea4706d95d79fe89
Juan Lomán <jloman@expansion.com.mx>
2016-04-28 18:17:04 -0400

    State docs dependency and clean up a little

--- Formula/percona-xtrabackup.rb
+++ Formula/percona-xtrabackup.rb
@@ -1,14 +1,14 @@
-
 class PerconaXtrabackup < Formula
   desc "Open source hot backup tool for InnoDB and XtraDB databases"
   homepage "https://www.percona.com/software/mysql-database/percona-xtrabackup"
   url "https://www.percona.com/downloads/XtraBackup/Percona-XtraBackup-2.4.2/source/tarball/percona-xtrabackup-2.4.2.tar.gz"
   sha256 "faeac6f1db4a1270e5263e48c8a94cc5c81c772fdea36879d1be18dcbcd1926e"
 
-  option "with-man-pages", "Build man pages. Needs python-sphinx to be installed"
+  option "with-docs", "Build man pages"
 
   depends_on "cmake" => :build
   depends_on "libev" => :build
+  depends_on "sphinx-doc" => :build if build.with? "docs"
   depends_on "libgcrypt"
   depends_on :mysql => :recommended
 
@@ -19,11 +19,8 @@ class PerconaXtrabackup < Formula
       -DWITH_BOOST=boost
     ]
 
-    if build.without? "man-pages"
+    if build.without? "docs"
       cmake_args << "-DWITH_MAN_PAGES=OFF"
-      ohai "Building without man pages"
-    else
-      ohai "Building with man pages"
     end
 
     cmake_args.concat std_cmake_args
@@ -42,9 +39,6 @@ class PerconaXtrabackup < Formula
 
       brew install perl cpanminus
       cpanm DBD::mysql
-
-    All binaries are installed to #{bin}
-    and are symlinked to /usr/local/bin
 
     EOS
   end
