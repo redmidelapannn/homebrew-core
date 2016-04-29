eb055615deab521ed2fb1567b7a6d1e7305a47c7
Tim Byrne <sultan@locehilios.com>
2016-04-28 20:09:02 -0500

    Add more substantial tests

--- Formula/yadm.rb
+++ Formula/yadm.rb
@@ -10,6 +10,47 @@ class Yadm < Formula
   end
 
   test do
-    system "yadm", "version"
+    # init a new repo
+    system "yadm", "init"
+    unless File.exist?("#{testpath}/.yadm/repo.git/config")
+      onoe "Failed to init repository"
+      return false
+    end
+
+    # confirm worktree set to $HOME
+    ohai "Verify repo worktree"
+    worktree = `yadm gitconfig core.worktree`
+    if worktree.chomp != testpath.to_s
+      onoe "Repo worktree is set incorrectly"
+      return false
+    end
+
+    # disable auto-alt
+    system "yadm", "config", "yadm.auto-alt", "false"
+    ohai "Verify auto-alt configuration"
+    setting = `yadm config yadm.auto-alt`
+    if setting.chomp != "false"
+      onoe "auto-alt was not set"
+      return false
+    end
+
+    # create a test file, and add it to the repo
+    File.write("#{testpath}/testfile", "test")
+    system "yadm", "add", "#{testpath}/testfile"
+
+    # configure git identity
+    system "yadm", "gitconfig", "user.email", "test@test.org"
+    system "yadm", "gitconfig", "user.name", "Test User"
+
+    # commit the test file to the repo
+    system "yadm", "commit", "-m", "test commit"
+
+    # verify the log
+    ohai "Verify repo log"
+    commit = `yadm log --pretty=oneline`
+    unless commit.chomp =~ /test commit$/
+      onoe "Repository log is not correct"
+      return false
+    end
   end
 end
