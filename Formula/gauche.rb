6a391642a9b91e8615c5fe00ac9bb0b198e5c2b9
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-30 08:52:56 +0100

    gauche: update 0.9.4 bottle.

--- Formula/gauche.rb
+++ Formula/gauche.rb
@@ -5,11 +5,10 @@ class Gauche < Formula
   sha256 "7b18bcd70beaced1e004594be46c8cff95795318f6f5830dd2a8a700410fc149"
 
   bottle do
-    revision 1
-    sha256 "abb95196aadd8f233b04d2a14b8bbb4f0dbf3be8fa0ab089834cbc279b074daf" => :el_capitan
-    sha256 "4018f9ae2b0a179ef5df0482e6734186f0069e773496f2f6092971f10a4c7b11" => :yosemite
-    sha256 "e1258b3e8f0c5a61c8df484e173837e6fb3c7ecad7cd1fee277d1f8da56e98f9" => :mavericks
-    sha256 "ffd47c0dcb63a508ae95adfc1e9c7825e75dd40fa10e46c68fcb8bd9b00637d5" => :mountain_lion
+    revision 2
+    sha256 "f4d13975cdb0872d43ada65a9175debc671521c5eec19d054bc6d9a7a6367058" => :el_capitan
+    sha256 "00cc62a452ebbe50572cf686b9b653ccebfbdc90021f03a95bb75b7c3219cea4" => :yosemite
+    sha256 "281460f741c2994387a93ef56830663d456b338ef69b1af4a79757d0bc39e038" => :mavericks
   end
 
   def install
