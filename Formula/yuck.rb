3438249a22a794f750c65c8ff8cc357f7714b1ff
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-30 18:23:32 +0100

    yuck: add 0.2.2 bottle.

--- Formula/yuck.rb
+++ Formula/yuck.rb
@@ -4,6 +4,12 @@ class Yuck < Formula
   url "https://github.com/hroptatyr/yuck/releases/download/v0.2.2/yuck-0.2.2.tar.xz"
   sha256 "9cd0c1af9a4afe6002f1672c8ece91d305cea1f61fbc07c7ce94a0bbe634deec"
 
+  bottle do
+    sha256 "1807841bba67f59266f588fc1fdc8f00f1d793cb2f44891c671c7decd79bdead" => :el_capitan
+    sha256 "d8885a5e8994f42b3e2e752fadd1916054a54ef31beb9657be0fac59d55ccc85" => :yosemite
+    sha256 "a88f11dca44f5924fc9997917c4666b230fa7db2f98dbd84449319f8e008aa14" => :mavericks
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
 
