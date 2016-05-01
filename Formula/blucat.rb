f6a63dc15b5489759c217aba96ff265a7a4add0e
ilovezfs <ilovezfs@icloud.com>
2016-05-01 07:50:56 -0700

    blucat: set max to Yosemite

--- Formula/blucat.rb
+++ Formula/blucat.rb
@@ -6,7 +6,11 @@ class Blucat < Formula
   sha256 "6dcd6bf538a06c2f29d21a9e94d859d91667a7014244462bffca9767bba5307d"
 
   depends_on "ant" => :build
-  depends_on :java => "1.6"
+  depends_on :java
+
+  # Uses symbols missing from El Capitan's IOBluetooth.framework
+  # Reported upstream 1st May 2016 : https://github.com/ieee8023/blucat/issues/2
+  depends_on MaximumMacOSRequirement => :yosemite
 
   def install
     system "ant"
