b8ca77561747fdacb7e092e517c978e2c52b1f65
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-01 15:56:37 +0100

    blucat: add 0.9 bottle.

--- Formula/blucat.rb
+++ Formula/blucat.rb
@@ -5,6 +5,12 @@ class Blucat < Formula
   version "0.9"
   sha256 "6dcd6bf538a06c2f29d21a9e94d859d91667a7014244462bffca9767bba5307d"
 
+  bottle do
+    cellar :any
+    sha256 "f1faf0f8ddefe8bf4199c912a670403d7079667593b2a9a5a962f505008082a8" => :yosemite
+    sha256 "e9430662b8d062f60c15f048b4738476bc23c0273c85d43b43d1bbd6f2bd1559" => :mavericks
+  end
+
   depends_on "ant" => :build
   depends_on :java
 
