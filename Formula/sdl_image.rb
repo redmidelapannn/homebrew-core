cceb488569a0fa6136a918072df77151323a34bc
s172262 <s172262@users.noreply.github.com>
2016-05-01 20:13:41 +0900

    SDL_image
    exactly link with libpng and other recommended libraries

--- Formula/sdl_image.rb
+++ Formula/sdl_image.rb
@@ -3,7 +3,7 @@ class SdlImage < Formula
   homepage "https://www.libsdl.org/projects/SDL_image"
   url "https://www.libsdl.org/projects/SDL_image/release/SDL_image-1.2.12.tar.gz"
   sha256 "0b90722984561004de84847744d566809dbb9daf732a9e503b91a1b5a84e5699"
-  revision 2
+  revision 3
 
   bottle do
     cellar :any
@@ -12,22 +12,27 @@ class SdlImage < Formula
     sha256 "84272135304e7c475e8000a90af113127858273a4449ce6566805f5b4c2cb476" => :mavericks
   end
 
+  option :universal
+
   depends_on "pkg-config" => :build
   depends_on "sdl"
-  depends_on "jpeg" => :recommended
-  depends_on "libpng" => :recommended
-  depends_on "libtiff" => :recommended
-  depends_on "webp" => :recommended
-
-  option :universal
+  depends_on "libpng"
+  depends_on "jpeg"
+  depends_on "libtiff"
+  depends_on "webp" => :optional
 
   def install
     ENV.universal_binary if build.universal?
     inreplace "SDL_image.pc.in", "@prefix@", HOMEBREW_PREFIX
-
-    system "./configure", "--prefix=#{prefix}",
-                          "--disable-dependency-tracking",
-                          "--disable-sdltest"
+    args = %W[--prefix=#{prefix} --disable-dependency-tracking --disable-sdltest]
+    # The alternative of ImagIO framework or libpng, jpeg, libttf and webp.
+    # http://forums.libsdl.org/viewtopic.php?p=42910&sid=76c5b793b1e8f19d350a94773b1ced3b#42907
+    args << "--disable-imageio"
+    # --enable-feature-shared args work as inverse.
+    # https://bugzilla.libsdl.org/show_bug.cgi?id=3308
+    args << "--enable-jpg-shared=no" << "--enable-png-shared=no" << "--enable-tif-shared=no"
+    args << "--enable-webp-shared" + (build.with?("webp") ? "=no": "")
+    system "./configure", *args
     system "make", "install"
   end
 end
