929c4978190b801c85969addbd9782915a03713d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-03 10:42:06 +0100

    ammonite-repl-scala-2.10: add 0.5.7 bottle.

--- Formula/ammonite-repl-scala-2.10.rb
+++ Formula/ammonite-repl-scala-2.10.rb
@@ -5,6 +5,13 @@ class AmmoniteReplScala210 < Formula
   version "0.5.7"
   sha256 "9f8e67466f58f6490c2d4b4d0f4ff9ad555ce29909843ba63f5c7533b993642b"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "71c33523d70b7be84336261573aef29f737b5956f8fa9bd47f09b9e59e342a41" => :el_capitan
+    sha256 "b3d4760cb62bdbe4251b9a1928cba244f74ffe8e258a4ce7013eb86a2d499410" => :yosemite
+    sha256 "d92fd9bcf8a7f7c3dc47aef357f9130b17a714feb2b40b5518acacdeb743ffc6" => :mavericks
+  end
+
   depends_on :java => "1.7+"
 
   def install
