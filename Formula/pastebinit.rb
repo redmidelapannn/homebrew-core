fc582c16081222dc2ba09e3b5ce547dfad06e72a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-05 05:55:24 +0100

    pastebinit: update 1.5 bottle.

--- Formula/pastebinit.rb
+++ Formula/pastebinit.rb
@@ -6,9 +6,10 @@ class Pastebinit < Formula
 
   bottle do
     cellar :any_skip_relocation
+    revision 1
     sha256 "c4ba9f88e6fad1b21fb2b433adf16855086063c143df0b10aea669dce3df189a" => :el_capitan
     sha256 "42f4b20a8f4361de3f03077bda5c2ecdac87759723ac383b562f0c21b8791d93" => :yosemite
-    sha256 "628ce64e3127dff93a92aa08019ad7c191f0b285dc8ed8cc2248c09d72abc5f5" => :mavericks
+    sha256 "5af388fee921e62c51b60bce712b5af70d62984201e24824b3cd755e253f8614" => :mavericks
   end
 
   depends_on "python3"
