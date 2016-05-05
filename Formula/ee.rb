ea25a50899d72540cb4ca37796fd3b77f178d29f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-05 23:37:53 +0100

    ee: update 1.4.6 bottle.

--- Formula/ee.rb
+++ Formula/ee.rb
@@ -6,9 +6,10 @@ class Ee < Formula
 
   bottle do
     cellar :any_skip_relocation
+    revision 1
     sha256 "3da2d595dec856251eb734ce0f12b5d04fd2c7354d48198c1014a923c63769ab" => :el_capitan
     sha256 "7cff46a098f916a37f45fa09799b79a204ba9866871413e596ab29295ece7b40" => :yosemite
-    sha256 "b30c4ef2a49b136eb0912cad5ed4d36a954746c42aca3fb7d8197d827d8b3ff6" => :mavericks
+    sha256 "db898f89f7b33030636b1422964701742dae35f861f599111c30fc186cf2704e" => :mavericks
   end
 
   def install
