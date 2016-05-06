ed7104482a4756e562dc8050833c506092651042
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-06 03:39:34 +0100

    unixodbc: update 2.3.4 bottle.

--- Formula/unixodbc.rb
+++ Formula/unixodbc.rb
@@ -6,9 +6,10 @@ class Unixodbc < Formula
   sha256 "2e1509a96bb18d248bf08ead0d74804957304ff7c6f8b2e5965309c632421e39"
 
   bottle do
+    revision 1
     sha256 "a902055dc3ca4fbda0f26f5463cf971c1f80d13028ba8a7b2631823c8c84782b" => :el_capitan
     sha256 "91c8778727b1d8d5d2f2b0b21d25f3c8ff2f5221ec38231d06c54bbc75684f32" => :yosemite
-    sha256 "f968a389218dde09820ce6d7278bcd521d7fb43e27f186541a8bfc538ff8ead6" => :mavericks
+    sha256 "331b722297c203d7304abfefdf422d4ac8b5cc388ade5ff24e823d4946113424" => :mavericks
   end
 
   keg_only "Shadows system iODBC header files" if MacOS.version < :mavericks
