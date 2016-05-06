16ff3f7e7ebaa731eddb7fef8fc2265735ce5dbd
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-06 22:05:05 +0100

    libsvg-cairo: update 0.1.6_1 bottle.

--- Formula/libsvg-cairo.rb
+++ Formula/libsvg-cairo.rb
@@ -7,10 +7,10 @@ class LibsvgCairo < Formula
 
   bottle do
     cellar :any
-    revision 1
-    sha256 "84c8809a89f2f46b0c596611a1b340de5eaca8152d36892cd8ec8226225953b2" => :el_capitan
-    sha256 "7375866aa26344d381892626dccc558052addb96bb2c7f76968a5c7a530ee010" => :yosemite
-    sha256 "45bcab7ac22c1ade9e3f3cbf81c77224e8e66be0dad99b23445ecf6805aa853a" => :mavericks
+    revision 2
+    sha256 "087f0a756d6136fb37eb57d61041b8ef4fea0ae889d62c78b55e644dd06de0ae" => :el_capitan
+    sha256 "b0004115dbc9d113777e4ef62ef29244b78c6e1954b6dc140dcea6cc092afb24" => :yosemite
+    sha256 "acdd325a18fac5b463bb420355f72b9b78620c467257927cc375bace0a8c014d" => :mavericks
   end
 
   depends_on "pkg-config" => :build
