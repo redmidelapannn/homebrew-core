bc9381fea2a0de81665ec2289ed4ba738558a526
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-07 23:23:49 +0100

    grt: add 0.0.1 bottle.

--- Formula/grt.rb
+++ Formula/grt.rb
@@ -4,6 +4,13 @@ class Grt < Formula
   url "https://github.com/nickgillian/grt/archive/v0.0.1.tar.gz"
   sha256 "56f90a9ffa8b2bf4e5831d39f9e1912879cf032efa667a5237b57f68800a2dda"
 
+  bottle do
+    cellar :any
+    sha256 "20999e35eae6739105d465115e87843411177f4e403c0d7a7e3431cd5a586678" => :el_capitan
+    sha256 "daa2b093dbb1e90391f30a424a7ed72ee60d7378ce32c5cb0cac0af2c4d7dc6d" => :yosemite
+    sha256 "d40b0bd3f5f5268ccb3f324f0d6adfb7be442cee22cd898587924a67f2caeff2" => :mavericks
+  end
+
   depends_on "cmake" => :build
 
   def install
