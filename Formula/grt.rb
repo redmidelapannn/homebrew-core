8e4e49947bf8d2a3d8d4015e3c36ba8a2c0baa58
Ben Zhang <benzh@cs.berkeley.edu>
2016-05-08 15:39:29 -0700

    Fixes based on the review.
    1. New line before `test do` block.
    2. No `-j` in `make`.
    3. Use `mkdir [] do` block.
    4. Use `std_cmake_args`.
    5. Remove comments.
    6. Updated the URL.

--- Formula/grt.rb
+++ Formula/grt.rb
@@ -1,6 +1,6 @@
 class Grt < Formula
   desc "The Gesture Recognition Toolkit (GRT) for Real-time machine learning."
-  homepage "http://www.nickgillian.com/wiki/"
+  homepage "http://www.nickgillian.com/wiki/pmwiki.php/GRT/GestureRecognitionToolkit"
   url "https://github.com/nickgillian/grt/archive/v0.0.1.tar.gz"
   sha256 "56f90a9ffa8b2bf4e5831d39f9e1912879cf032efa667a5237b57f68800a2dda"
 
@@ -8,14 +8,14 @@ class Grt < Formula
 
   def install
     cd "build"
-    mkdir "build"
-    cd "build"
-    system "cmake", "-DCMAKE_INSTALL_PREFIX:PATH=#{prefix}", ".."
-    system "make", "-j#{Hardware::CPU.cores}"
-    system "make", "install"
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make"
+      system "make", "install"
+    end
   end
+
   test do
-    # A simple test to see if we have GRT setup properly (only compilation).
     (testpath/"test.cpp").write <<-EOS.undent
       #include <GRT/GRT.h>
       int main() {
