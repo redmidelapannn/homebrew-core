9797a302ad3dc578a1ca4f820f107ab6b6d4afad
Alexander Regueiro <alexreg@me.com>
2016-05-10 00:05:49 +0100

    czmqpp: added test case

--- Formula/czmqpp.rb
+++ Formula/czmqpp.rb
@@ -23,4 +23,59 @@ class Czmqpp < Formula
                           "--prefix=#{prefix}"
     system "make", "install"
   end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <iostream>
+      #include <string>
+      #include <vector>
+
+      #include <czmq++/czmqpp.hpp>
+
+      using namespace std;
+
+      int main()
+      {
+        const string addr = "inproc://hello-world";
+        const string msg = "Hello, World!";
+
+        czmqpp::context context;
+
+        czmqpp::socket pull_sock(context, ZMQ_PULL);
+        pull_sock.bind(addr);
+
+        czmqpp::socket push_sock(context, ZMQ_PUSH);
+        push_sock.connect(addr);
+
+        {
+          czmqpp::message send_msg;
+          const czmqpp::data_chunk msg_data(msg.begin(), msg.end());
+          send_msg.append(msg_data);
+          if (!send_msg.send(push_sock))
+            return 1;
+        }
+
+        {
+          czmqpp::message recv_msg;
+          if (!recv_msg.receive(pull_sock))
+            return 1;
+          const czmqpp::data_chunk msg_data = recv_msg.parts()[0];
+          string received_msg(msg_data.begin(), msg_data.end());
+          cout << received_msg << flush;
+        }
+
+        return 0;
+      }
+    EOS
+
+    flags = ENV.cxxflags.to_s.split + %W[
+      -std=c++11
+      -I#{include}
+      -L#{lib}
+      -lczmq++
+      -lczmq
+    ]
+    system ENV.cxx, "-o", "test", "test.cpp", *flags
+    assert_equal "Hello, World!", shell_output("./test")
+  end
 end
