c2a6fdf686e6c5e863a0bae7b9c15586cf31a4fb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-09 02:19:34 +0100

    czmq: update 3.0.2_3 bottle.

--- Formula/czmq.rb
+++ Formula/czmq.rb
@@ -7,9 +7,10 @@ class Czmq < Formula
 
   bottle do
     cellar :any
-    sha256 "9bbf6566cd74644ae22f5dd9338c1123bf3ecdf7a920dcaabf166aeb3902e3f7" => :el_capitan
-    sha256 "4a569da4e60f3b8252b4ef9a998e50153ac119108135ce832f2494b0edf7e87a" => :yosemite
-    sha256 "ae42e5b89ed47c00a3a45d9c3a4759a2f0a772c787f62b34cb024f489790efff" => :mavericks
+    revision 1
+    sha256 "c07f5882cd3c4699fd231dacb41f38f9b717340ff21da11f4de6953ce520a6b0" => :el_capitan
+    sha256 "c824b4af7f5bf022a03f96aa699f672b010ff57e92ec52bbf1312debc670db5c" => :yosemite
+    sha256 "47d9bf3d99ba6ab7565b6401a14206865e88e35f5697092a9ca29a0c8bba63a4" => :mavericks
   end
 
   conflicts_with "mono", :because => "both install `makecert` binaries"
