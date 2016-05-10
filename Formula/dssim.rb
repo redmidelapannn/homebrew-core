545a185dcf17f8095cd518154818df78db857456
Sebastian Werner <s.werner@sebastian-software.de>
2016-05-10 07:49:38 +0200

    Fixed outstanding issues

--- Formula/dssim.rb
+++ Formula/dssim.rb
@@ -1,15 +1,15 @@
 class Dssim < Formula
   desc "RGBA Structural Similarity C implementation (with a Rust API)"
   homepage "https://github.com/pornel/dssim"
-  url "https://github.com/pornel/dssim/archive/1.3.2.zip"
-  sha256 "393a5431f08c747ac19b00176f2b710010d87215aa6f5105e65e9fa7afed63be"
+  url "https://github.com/pornel/dssim/archive/1.3.2.tar.gz"
+  sha256 "2e79f8cf7cb7681f7f3244364662ce4723739745201217d85243bbc709124e63"
 
   depends_on "pkg-config" => :build
   depends_on "libpng"
 
   def install
     system "make"
-    bin.install Dir["bin/dssim"]
+    bin.install "bin/dssim"
   end
 
   test do
