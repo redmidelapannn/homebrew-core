4d42ef1819eb89df8d9c23e2cdd61b37988b3e54
Michael Ford <fanquake@users.noreply.github.com>
2016-05-12 20:36:52 +0800

    expat - remove revision

--- Formula/expat.rb
+++ Formula/expat.rb
@@ -4,7 +4,6 @@ class Expat < Formula
   url "https://downloads.sourceforge.net/project/expat/expat/2.1.1/expat-2.1.1.tar.bz2"
   mirror "https://fossies.org/linux/www/expat-2.1.1.tar.bz2"
   sha256 "aff584e5a2f759dcfc6d48671e9529f6afe1e30b0cd6a4cec200cbe3f793de67"
-  revision 1
 
   head ":pserver:anonymous:@expat.cvs.sourceforge.net:/cvsroot/expat", :using => :cvs
 
