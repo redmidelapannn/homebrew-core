4fb7d0a6c5d42420a81d309d0c96341723b83a2f
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-13 03:05:56 +0100

    frege-repl: add 1.4-SNAPSHOT bottle.

--- Formula/frege-repl.rb
+++ Formula/frege-repl.rb
@@ -5,6 +5,13 @@ class FregeRepl < Formula
   version "1.4-SNAPSHOT"
   sha256 "2cf1c2a8f7b64c9d70b21fbfd25b2af3f5e3bebe3662f724afd435d01bddafec"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9829555e726cc323a49348d3978012b5fbe3d2ff94f893b218fa85e2339efe3d" => :el_capitan
+    sha256 "07189a54129e001f0a2b289ef9ac340ed193d511e475aa4164175db47ac6074b" => :yosemite
+    sha256 "6df29fe763e09a1f57f7f101a7007351c0991cf68d5d19acc10b7a929a5018a4" => :mavericks
+  end
+
   depends_on :java => "1.8+"
 
   def install
