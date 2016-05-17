ac670afd5eb45df8d36dbe98de4032ed826342dd
matteocng <matteocng@users.noreply.github.com>
2016-05-17 07:04:04 +0200

    bitlbee 3.4.2

--- Formula/bitlbee.rb
+++ Formula/bitlbee.rb
@@ -3,7 +3,6 @@ class Bitlbee < Formula
   homepage "https://www.bitlbee.org/"
   url "https://get.bitlbee.org/src/bitlbee-3.4.2.tar.gz"
   sha256 "69c85554def74f314e3b6e390389a30b0e748f23ef37883e9d7545ee2c45ea57"
-  revision 1
 
   head "https://github.com/bitlbee/bitlbee.git"
 
