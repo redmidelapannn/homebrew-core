1f92285b9d3fa55ee9fca852185069f74de86baf
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-19 19:33:47 +0100

    wmc: add 0.1.2-alpha bottle.

--- Formula/wmc.rb
+++ Formula/wmc.rb
@@ -5,6 +5,13 @@ class Wmc < Formula
   version "0.1.2-alpha"
   sha256 "b9d3fdc1a6a9cb4fe66081cef6d52e56d4a90a126b0dcb7e26addc6376228d8b"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "95130a8f6355cceba0385433e57f12337a7a6738da8c6f2a13be47378fabb1ac" => :el_capitan
+    sha256 "6c51dcde13d01aed46909b918bf928f3beef1e0b629dba8719203a1de9b470a8" => :yosemite
+    sha256 "76103fa148dffd3ab5ab6bfe60b38dd9497245ff6e0d215f51ca88e2410b35ed" => :mavericks
+  end
+
   depends_on "go"
 
   def install
