229e5a0797a2fb050e1eb2d08a431535382af643
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-20 08:27:24 +0100

    xcclean: add 1.0 bottle.

--- Formula/xcclean.rb
+++ Formula/xcclean.rb
@@ -5,6 +5,11 @@ class Xcclean < Formula
   version "1.0"
   sha256 "23af560d848b455d6119453edffa3f86a683f8635e19e35414829b3dbd0d9f8d"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "96583d08f166a247d9d7d4fca30400aa6b577594d61128ce22d1a81bd4fdb9d1" => :el_capitan
+  end
+
   depends_on :macos => :el_capitan
 
   def install
