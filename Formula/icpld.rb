0215c331df640db19aa9ba5f23e20bda7fa6a7ae
BrewTestBot <brew-test-bot@googlegroups.com>
2016-05-20 01:33:42 +0100

    icpld: add 1.1.5 bottle.

--- Formula/icpld.rb
+++ Formula/icpld.rb
@@ -4,6 +4,13 @@ class Icpld < Formula
   url "http://www.ibiblio.org/icpld/download/icpld-1.1.5.tar.gz"
   sha256 "9e54c33ce3b2ff2b72f5772aca02ef9f34807b5456054c328eed5b5d61b3a7c1"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "60e6705db3f8d34eb8d7d8a32cb23247f0e76d1f8d30b2628c53705bf3ebb38b" => :el_capitan
+    sha256 "f11e2528e838c6be048107f37fa2d33aa58bf883765b8997b9e2489a211ae68c" => :yosemite
+    sha256 "e20acceb620d9e90eec6a52e99ed69ffe20ae118e69e70d99232779a1bb9b060" => :mavericks
+  end
+
   option "with-ipv6", "If you plan on using icpld with IPv6 network."
   patch :DATA
 
