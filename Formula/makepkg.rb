1ea56e631c3fd82f2aefb2d6ddd2548d16189655
Dominyk Tiller <dominyktiller@gmail.com>
2016-05-22 17:24:22 +0100

    makepkg 5.0.1
    * Version update.
    * Update deps to recognise libarchive isn't build-only.

--- Formula/makepkg.rb
+++ Formula/makepkg.rb
@@ -2,9 +2,8 @@ class Makepkg < Formula
   desc "Compile and build packages suitable for installation with pacman"
   homepage "https://wiki.archlinux.org/index.php/makepkg"
   url "https://projects.archlinux.org/git/pacman.git",
-      :tag => "v4.2.1",
-      :revision => "068f8cec42057751f528b19cece37db13ae92541"
-  revision 1
+      :tag => "v5.0.1",
+      :revision => "f38de43eb68f1d9c577b4378310640c1eaa93338"
 
   head "https://projects.archlinux.org/git/pacman.git"
 
@@ -20,10 +19,10 @@ class Makepkg < Formula
   depends_on "asciidoc" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
-  depends_on "libarchive" => :build
   depends_on "bash"
   depends_on "fakeroot"
   depends_on "gettext"
+  depends_on "libarchive"
   depends_on "openssl"
   depends_on "gpgme" => :optional
 
@@ -38,7 +37,11 @@ class Makepkg < Formula
   end
 
   test do
-    (testpath/"PKGBUILD").write("source=(https://androidnetworktester.googlecode.com/files/10kb.txt)")
+    (testpath/"PKGBUILD").write <<-EOS.undent
+      source=(https://androidnetworktester.googlecode.com/files/10kb.txt)
+      pkgver=2.1
+      pkgrel=1
+    EOS
     system "#{bin}/makepkg", "-dg"
   end
 end
