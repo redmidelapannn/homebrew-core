0e75d4d166ec3ec9a2e015c30a17b86eeb444f74
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-03 14:51:08 +0100

    crystal-icr: add 0.2.8 bottle.

--- Formula/crystal-icr.rb
+++ Formula/crystal-icr.rb
@@ -4,6 +4,12 @@ class CrystalIcr < Formula
   url "https://github.com/greyblake/crystal-icr/archive/v0.2.8.tar.gz"
   sha256 "5a235ee2f3065c6cf9479a7c32a11ba3d11f5e185d6c1458a97bd60eb11f88a3"
 
+  bottle do
+    sha256 "feb374eb0265136649b3f1b30b120d41f2de4265020ef4f84c04f77b319283e7" => :el_capitan
+    sha256 "81a6ec331b5be141948ae3a46bb4954808939d3078ce75345092bc6a3c908b2c" => :yosemite
+    sha256 "1bc39d5542413467ecc53212ba53c863fc8b16543b41530b254d841a02979604" => :mavericks
+  end
+
   depends_on "crystal-lang"
   depends_on "readline"
 
