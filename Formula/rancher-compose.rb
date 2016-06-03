84cb0fc3ca021dd4d565e05ef6f10281ddef1eae
Sebastian Mandrean <sebastian.mandrean@gmail.com>
2016-06-03 14:12:46 +0200

    rancher-compose 0.8.2 (new formula)

--- Formula/rancher-compose.rb
+++ Formula/rancher-compose.rb
@@ -1,26 +1,14 @@
 class RancherCompose < Formula
-  desc "Docker Compose compatible client to deploy to Rancher"
-  homepage "https://github.com/rancher/rancher-compose"
-  url "https://github.com/rancher/rancher-compose/archive/v0.7.4.tar.gz"
-  sha256 "a0d285dd0608323c28a78782e85fbe3f52ddaad9d101030fad39efe3b20dadc9"
-
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "3af6309c9af3e30deb10bb7744c352a8b25f9b5136c0a38d81deb28ec360e17d" => :el_capitan
-    sha256 "21559a34c86d1e4183336ba238651cc21f8c2ece3f78f4fcee006692b4f5751d" => :yosemite
-    sha256 "e83229b6f65f27e197a7e8f3397b45d05ca57ae2a2129f8527c8ac66bf4dbf65" => :mavericks
-  end
-
-  depends_on "go" => :build
+  desc "Docker compose compatible client to deploy to Rancher"
+  homepage "http://docs.rancher.com/rancher/latest/en/rancher-compose/"
+  url "https://github.com/rancher/rancher-compose/releases/download/v0.8.2/rancher-compose-darwin-386-v0.8.2.tar.gz"
+  sha256 "116af6f0228da287959e3db76f6de56c819add159e8f1948b66c4b27b76ad889"
 
   def install
-    ENV["GOPATH"] = "#{buildpath}:#{buildpath}/Godeps/_workspace"
-    mkdir_p "#{buildpath}/src/github.com/rancher"
-    ln_s buildpath, "#{buildpath}/src/github.com/rancher/rancher-compose"
-    system "go", "build", "-ldflags", "-w -X github.com/rancher/rancher-compose/version.VERSION #{version}", "-o", "#{bin}/rancher-compose"
+    bin.install "rancher-compose"
   end
 
   test do
-    system "#{bin}/rancher-compose", "help"
+    assert_match /#{version}/, shell_output(bin/"rancher-compose --version")
   end
 end
