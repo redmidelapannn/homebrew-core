a3832341d02a0e98b5c8296d554d8e2e74edc122
Giovanni Bajo <rasky@develer.com>
2016-06-03 19:37:38 +0200

    Compile judy as static library

--- Formula/traildb.rb
+++ Formula/traildb.rb
@@ -7,32 +7,25 @@ class Traildb < Formula
   depends_on "libarchive"
   depends_on "pkg-config" => :build
 
-  # only required because we run autoreconf for judy
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
-
   resource "judy" do
     url "https://downloads.sourceforge.net/project/judy/judy/Judy-1.0.5/Judy-1.0.5.tar.gz"
     sha256 "d2704089f85fdb6f2cd7e77be21170ced4b4375c03ef1ad4cf1075bd414a63eb"
   end
 
   def install
-    ENV["PREFIX"] = prefix
+    # We build judy as static library, so we don't need to install it
+    # into the real prefix
+    judyprefix = "#{buildpath}/resources/judy"
 
     resource("judy").stage do
-      # Only build library, we don't care about tools, docs.
-      # This also requires running autoreconf
-      inreplace "Makefile.am", "src tool doc test", "src"
-      system "autoreconf", "-fiv"
-
       system "./configure", "--disable-debug", "--disable-dependency-tracking",
-          "--prefix=#{prefix}"
+          "--disable-shared", "--prefix=#{judyprefix}"
       system "make", "-j1", "install"
     end
 
-    ENV["CFLAGS"] = "-I#{prefix}/include"
-    ENV["LDFLAGS"] = "-L#{prefix}/lib"
+    ENV["PREFIX"] = prefix
+    ENV.append "CFLAGS", "-I#{judyprefix}/include"
+    ENV.append "LDFLAGS", "-L#{judyprefix}/lib"
     system "./waf", "configure", "install"
   end
 
