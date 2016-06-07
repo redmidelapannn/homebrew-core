454eb341d08c7b1b8b2514ea6843bdc0b8117261
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-07 17:01:13 +0100

    doctl: update 1.2.0 bottle.

--- Formula/doctl.rb
+++ Formula/doctl.rb
@@ -9,9 +9,9 @@ class Doctl < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d488bd227e29d9483c8cffa1181233fa3d66c7e065fa5cd66abc6e2eebfd3f08" => :el_capitan
-    sha256 "d5917a2962f24087c0a106ee8896e8691bf5e7410ef30ecc071259f2f077455a" => :yosemite
-    sha256 "c198bf12a5b7adad667a9e05cc165fa20091d0a7bfb62eb46d57b8e3c6c110fd" => :mavericks
+    sha256 "8bfa98fa42e09ed58f730bb2c09373eb8ef0ff35bf8cee143e4e5376fe57889b" => :el_capitan
+    sha256 "91f3a5b6958b22344eab9ca2bbb4c376a418b4e68221425bd8e69aea6c6800fb" => :yosemite
+    sha256 "b640a5d66e4c88113efa96924c27dfed10fb83f46eaeaca76bd10dde7131ef04" => :mavericks
   end
 
   depends_on "go" => :build
