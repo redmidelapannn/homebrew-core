e2ee3bc17550f4c8d73f4997100f2040898dc55c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-09 13:16:38 +0100

    aoeui: update 1.7 bottle.

--- Formula/aoeui.rb
+++ Formula/aoeui.rb
@@ -7,9 +7,9 @@ class Aoeui < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "62a04ac0fd27e76f4f77da95e7d5aaf75488765f98b02574ae7dff0508cd9f13" => :el_capitan
-    sha256 "19f622466c20ad4ddff2fc97ac186e189d5a8bb02dd405ed2a93c5d13a88b1f4" => :yosemite
-    sha256 "a3ac3238356624a12b20df52c98d2bc52e0d785745e393928fc4c629212d1406" => :mavericks
+    sha256 "7ef462c737fb92cb3fabfb7727c28739940533d1ff9097ceb1144c4bfaaf1740" => :el_capitan
+    sha256 "ae3dd8757275de9d351d80b8487a6553e44d907daa19cb508bcd4f6974b2edac" => :yosemite
+    sha256 "aa5e0a4f8ceb9fde5a816af4288127b98807442927498344d8eb9252e7c23245" => :mavericks
   end
 
   def install
