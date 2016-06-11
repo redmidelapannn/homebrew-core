7df30a5cef846ef82b34bd775c60f8581f29cb24
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-11 17:45:31 +0100

    surfraw: update 2.2.9 bottle.

--- Formula/surfraw.rb
+++ Formula/surfraw.rb
@@ -6,11 +6,10 @@ class Surfraw < Formula
 
   bottle do
     cellar :any_skip_relocation
-    revision 1
-    sha256 "df27f0dc82f9f2f4efa5fc33244d66063592d0c7afd3d2dd8db0466ec10492d0" => :el_capitan
-    sha256 "cf0dd3b14c4e8034f0d0766fb09978eac0f19fa41730d72ed1422a67c0b0d0b3" => :yosemite
-    sha256 "1291ba20882c2dc1fbb092782e943ab0b99ad3642fb73db207dadff1fc00c5fc" => :mavericks
-    sha256 "30b885dc5908318868da2739f36834ce071bc7bff1a761fdc395afe82f75efa8" => :mountain_lion
+    revision 2
+    sha256 "1347e3ba2810c9292b5479a082892fcd27b0891707d45aad46b873f7814e1b86" => :el_capitan
+    sha256 "3cd3bc8e2c03f03547c380052600f22380d4c1e05fdd3e13951052e052961e00" => :yosemite
+    sha256 "5c0b00055cb42ce09c67a5eee6070427beca86409ffe40bb90134e6ff3ae6e1a" => :mavericks
   end
 
   head do
