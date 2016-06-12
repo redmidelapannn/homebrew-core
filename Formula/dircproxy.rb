404aac7f796a2802ac8baef35c5037caa3d8f7f9
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-12 13:48:29 +0100

    dircproxy: update 1.2.0-RC1 bottle.

--- Formula/dircproxy.rb
+++ Formula/dircproxy.rb
@@ -5,9 +5,10 @@ class Dircproxy < Formula
   sha256 "40ad50ffd13681114f995519dc3f65f48cb5eac41e780ad14ce8ffd49463757f"
 
   bottle do
+    revision 1
     sha256 "2e42623d53202a0ed7c76e7e9490bb95055bfceec1daee854b1746f73d96d2bd" => :el_capitan
     sha256 "5f9bec3190e4dbf8c58c719de4f28c1fa86be764be4d07ce3570524da0c49bec" => :yosemite
-    sha256 "ecb8e06f9493061e2b4c6004b160a995775ed98e7abc8daaafbe75e9fe5a9f29" => :mavericks
+    sha256 "eaffc5bbb87969b843917eed0ee60d803cd19ad09c4d8db77452ec8784e1bde5" => :mavericks
   end
 
   def install
