5a1bc609e87fc59a3c5270969ff7981d058da04a
ruki <waruqi@gmail.com>
2016-06-18 12:05:39 +0800

    xmake: restore bottle and fix test

--- Formula/xmake.rb
+++ Formula/xmake.rb
@@ -5,6 +5,13 @@ class Xmake < Formula
   sha256 "88b90a416abb0ccb5b3a910d8361eb9acd07b9b843de3db910948b02f59f2557"
   head "https://github.com/waruqi/xmake.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "43acc8707721039771c55da22ba3ca952a5455f5f2e429a5f77ed483a51eaf9d" => :el_capitan
+    sha256 "b410d812545bed22448185426ddc8efd4d469d0921268279b48481de797288a0" => :yosemite
+    sha256 "3226fee22117f1763b1167293311351c707740a9be7236c7f03fd867deab5b74" => :mavericks
+  end
+
   def install
     system "./install", "output"
     pkgshare.install Dir["xmake/*"]
@@ -13,7 +20,7 @@ class Xmake < Formula
   end
 
   test do
-    touch testpath/"xmake.lua"
-    system "#{bin}/xmake"
+    system bin/"xmake", "create", "-P", testpath
+    assert_match "build ok!", pipe_output(bin/"xmake")
   end
 end
