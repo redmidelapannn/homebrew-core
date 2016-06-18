17dfd8c8f806c343aa37f2b85a3561a2b48158e1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-18 11:38:48 +0100

    ndiff: update 2.00 bottle.

--- Formula/ndiff.rb
+++ Formula/ndiff.rb
@@ -6,9 +6,10 @@ class Ndiff < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "6faf20ce4c88110019c76cc4253cd65e5743fab7cff109fc8a7d41c8f411012e" => :el_capitan
-    sha256 "80adff8ec563059b7f49005c7e567b950ca58b392a4a5db18ae4957fe18b296d" => :yosemite
-    sha256 "7451587f9747af6e7ffd0e5dbacd337a72cd9b7f3c45a1240c2033e0731d5d46" => :mavericks
+    revision 1
+    sha256 "dbe43031efe6e591cc3af5e315b3c053cf11c325bf52521634363996e2ecb14f" => :el_capitan
+    sha256 "8d1b6aab8e6fd4b3c3122410d8195d4b19b7b9ab930471b83ea239154b9131f9" => :yosemite
+    sha256 "01f4c7e3606031a57086e141d2a8d95e3bd3efaf3a1f151c16e97fd7e61bc9bc" => :mavericks
   end
 
   conflicts_with "nmap", :because => "both install `ndiff` binaries"
