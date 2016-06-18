8f4f071f36157aa54e8ed756622877f32db66ec2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-18 20:55:01 +0100

    transmission: update 2.92 bottle.

--- Formula/transmission.rb
+++ Formula/transmission.rb
@@ -5,9 +5,10 @@ class Transmission < Formula
   sha256 "3a8d045c306ad9acb7bf81126939b9594553a388482efa0ec1bfb67b22acd35f"
 
   bottle do
-    sha256 "68af47554f408d92d04cbf86239a3d973cf7b4e9f0d254e127f5ead36074987a" => :el_capitan
-    sha256 "2071f4bb87d9d5e7cf6885caa8cf8605d81b108a097259113ba295a3c1f90bb7" => :yosemite
-    sha256 "9fb74d440426bbdf82d06dcd01e53db625cabfce0c4c85b15aec298007df6fd3" => :mavericks
+    revision 1
+    sha256 "7aae6f907fa0dcd303132630a06712a129653f83047891b5dc7a5a6d57ddc5f3" => :el_capitan
+    sha256 "ba02d517080ff34af6fdd0fdf8030ec3ee91a20415e10df3ac9645c8bd7b5891" => :yosemite
+    sha256 "2e47123f52a9e01d682b29e1244f538d327ab883fbf9d66a1ab980d84c1e3bfc" => :mavericks
   end
 
   option "with-nls", "Build with native language support"
