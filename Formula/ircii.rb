cb003ebff3c36599b348494d38cf0afb39d49f20
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-18 11:38:48 +0100

    ircii: update 20151120 bottle.

--- Formula/ircii.rb
+++ Formula/ircii.rb
@@ -5,9 +5,10 @@ class Ircii < Formula
   sha256 "5dfd3fd364a96960e1f57ade4d755474556858653e4ce64265599520378c5f65"
 
   bottle do
-    sha256 "ee1819c80cb867fe912a0e5cd904835c4d5cff2f1258fa677930dcc6c1bd92f2" => :el_capitan
-    sha256 "aeee9bd67c7ead873b8b0fda0d839a68ff67e923097dc20c543f52428d50c7a6" => :yosemite
-    sha256 "df68185a1cba612196b394b624f49688ecf7a6e71efe7a5f56dc5a03ba8aea5a" => :mavericks
+    revision 1
+    sha256 "4dadf9986ed6f3839018786f065889db7e7b8b9c5f4e43e0619e07df0acb5265" => :el_capitan
+    sha256 "02a6b477947133025eed4239eb95f4b2ec68c19d566dec6b5dc667e6e76ba894" => :yosemite
+    sha256 "1412ee5ed9f7b528f7d617a6e9824d0020472f15a7b801bdd1afa9c74ae12be5" => :mavericks
   end
 
   depends_on "openssl"
