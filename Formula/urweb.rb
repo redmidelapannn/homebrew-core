f5291d979fe1508796109f70d4ed1b81dd100f0c
Eran Meir <eranmeir@gmail.com>
2016-06-20 02:55:43 +0300

    Ur/web 20160515
    urweb.rb was moved to the boneyard because it used old binary-only mlton
    formula (see Homebrew/homebrew#21780).
    
    A new mlton formula was recently merged (Homebrew/homebrew/pull/48694)
    and the urweb formula is now updated as well.

---
+++ Formula/urweb.rb
@@ -0,0 +1,28 @@
+class Urweb < Formula
+  desc "Ur/Web programming language"
+  homepage "http://www.impredicative.com/ur/"
+  url "http://www.impredicative.com/ur/urweb-20160515.tgz"
+  sha256 "58c5cc0e96f0c311de468b4475e4bf595e6de5c04095136eed4148545e70442f"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "openssl"
+  depends_on "mlton" => :build
+  depends_on :postgresql => :optional
+  depends_on :mysql => :optional
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--with-openssl=/usr/local/opt/openssl",
+                          "--prefix=#{prefix}"
+
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/urweb"
+  end
+end
