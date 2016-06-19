b2afa2539c22abb820fa1f627e45249a25cd37fc
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-19 13:10:37 +0100

    tomsfastmath: add 0.13 bottle.

--- Formula/tomsfastmath.rb
+++ Formula/tomsfastmath.rb
@@ -4,6 +4,13 @@ class Tomsfastmath < Formula
   url "https://github.com/libtom/tomsfastmath/releases/download/v0.13.0/tfm-0.13.tar.bz2"
   sha256 "8defe6f20759a455ade46b2f3c4de46d2358a6222236f78012736a6aeed63407"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "64ba365d624e07bad2bbecd70367a443308271983209288cb1933624c1c99b26" => :el_capitan
+    sha256 "29988397e3f5654bc8576f047778cc58dfcb48415db9084a2ea3de886e73837d" => :yosemite
+    sha256 "3be98a16cdfca354265a507ba09d164e114a1ceb3d1b75e01e23acca5ac5af6c" => :mavericks
+  end
+
   def install
     system "make"
 
