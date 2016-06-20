8ca573c3517e0e123116cc4e8279e847b2870fe5
romangurevitch <romang@jfrog.com>
2016-06-20 12:25:39 +0300

    jfrog-cli-go 1.3.2

--- Formula/jfrog-cli-go.rb
+++ Formula/jfrog-cli-go.rb
@@ -3,7 +3,7 @@ require "language/go"
 class JfrogCliGo < Formula
   desc "command-line interface for Jfrog Artifactory and Bintray"
   homepage "https://github.com/JFrogDev/jfrog-cli-go"
-  url "https://github.com/JFrogDev/jfrog-cli-go/archive/1.3.2.tar.gz"
+  url "https://github.com/JFrogDev/jfrog-cli-go/archive/#{version}.tar.gz"
   sha256 "aac1e592d694996f1ff86e68245cd6e470a82ffafb3bd472f76523860023ac80"
 
   bottle do
@@ -30,6 +30,6 @@ class JfrogCliGo < Formula
   end
 
   test do
-    assert_equal "jfrog version 1.3.2", shell_output("#{bin}/jfrog -v").chomp
+    assert_equal "jfrog version #{version}", shell_output("#{bin}/jfrog -v").chomp
   end
 end
