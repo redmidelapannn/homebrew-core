10876526e5bdee2136cd7c5119efaffe61bee53d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-21 09:33:44 +0100

    libofx: update 0.9.9 bottle.

--- Formula/libofx.rb
+++ Formula/libofx.rb
@@ -5,11 +5,10 @@ class Libofx < Formula
   sha256 "94ef88c5cdc3e307e473fa2a55d4a05da802ee2feb65c85c63b9019c83747b23"
 
   bottle do
-    revision 1
-    sha256 "cb45299c0a279d0cf67a3f0de1875b78f6944cf899ae9844ba6cfc8f39689599" => :el_capitan
-    sha256 "5d29ae89fa771229fd80ce514db0303507a51f2bd3a32ea85f4db4727e11a59b" => :yosemite
-    sha256 "deddd527249a7d2a77772c42d656e4b44ea54a7b122da6b485594947927ec49b" => :mavericks
-    sha256 "88f857936a621b9ee2b09b7e0d2412673d49271e3528507c2071619490dc3534" => :mountain_lion
+    revision 2
+    sha256 "3ba692f5791af9bb94bbd3b92f2720d56fe16c51180b5434ace901bb02830fd0" => :el_capitan
+    sha256 "ba7a6327fee833da81eb2d92562f5db7ff5a4a66b5207dc1d84c58fa15dbf27c" => :yosemite
+    sha256 "da09129b74cd3a918531f468614ec380be7b7e0495b2ac995e9c994ffe763c6f" => :mavericks
   end
 
   depends_on "open-sp"
