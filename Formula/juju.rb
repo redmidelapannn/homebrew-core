2df4003d738225650a1acdc17fd8e48d6cd9ec4b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-21 18:56:47 +0100

    juju: update 1.25.5 bottle.

--- Formula/juju.rb
+++ Formula/juju.rb
@@ -6,9 +6,10 @@ class Juju < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ad7e0824a8180098a689e9087219acb14e37ff44fe7515d4cfb15e0c99182133" => :el_capitan
-    sha256 "3be1849359e87e9dd1bc4a44d3efeb033b7a6308215bfc0c13b889f3bfaf90fb" => :yosemite
-    sha256 "4961bebf88d7c5167e6df8da19d652fea3c3b87a7936d37d130bfcb80962e717" => :mavericks
+    revision 1
+    sha256 "2c2ea228f996826a375b4295933a75c2e79341f76552ff5fc39e14e8488aa273" => :el_capitan
+    sha256 "06e9f8b3d944730badeabfa3a85e38a4c6903ef056951b3dc24b8b73d1bf5f5d" => :yosemite
+    sha256 "1af1787e7da2756fc055303917219eb64c5b72b6d8382deb48a56492b8f27407" => :mavericks
   end
 
   devel do
