4b5f853a6dc7d348b777b022c0e91a8156e2c5ef
Eran Meir <eranmeir@gmail.com>
2016-06-22 03:41:53 +0300

    Update to version 20160621

--- Formula/urweb.rb
+++ Formula/urweb.rb
@@ -1,8 +1,8 @@
 class Urweb < Formula
   desc "Ur/Web programming language"
   homepage "http://www.impredicative.com/ur/"
-  url "http://www.impredicative.com/ur/urweb-20160515.tgz"
-  sha256 "58c5cc0e96f0c311de468b4475e4bf595e6de5c04095136eed4148545e70442f"
+  url "http://www.impredicative.com/ur/urweb-20160621.tgz"
+  sha256 "c5e487b11d44ab9945c04c305e644215282a60fcb2776d4939d79748a1497522"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
