0877e63facb3d544a779de7d302fbeff826304f0
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-23 16:18:39 +0100

    autogen: update 5.18.7 bottle.

--- Formula/autogen.rb
+++ Formula/autogen.rb
@@ -6,9 +6,10 @@ class Autogen < Formula
   sha256 "a7a580a5e18931cb341b255cec2fee2dfd81bea5ddbf0d8ad722703e19aaa405"
 
   bottle do
-    sha256 "319d4d6c54c3025e590d15553c86ffbe5c2db1c2d7946d5cd8fb4a02786adc27" => :el_capitan
-    sha256 "5805b867cab218e2a1a933f646344cec285d883c5c6c9f04dfb90795e21a7dcc" => :yosemite
-    sha256 "7a00b94115673c045497246de4f1e5b90cb636ae10fa3aea1409f7e4be52c2dc" => :mavericks
+    revision 1
+    sha256 "0606bddf5139289d4c8e049fa2cad60e2ed953b98e76460dfadb4038b582ace1" => :el_capitan
+    sha256 "cb70592e06a6807a8a2eb5796d89670c67ecbece03c3179167adc4240eab7fda" => :yosemite
+    sha256 "c0a72bd7ff4d553a796f498be2dac59c2f98925fac5346fbe4b912b19a28f93d" => :mavericks
   end
 
   depends_on "pkg-config" => :build
