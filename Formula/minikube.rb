a51ff6e15d64712979589a42232e909a7eddc1ab
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-27 06:44:11 +0100

    minikube: add 0.3.0 bottle.

--- Formula/minikube.rb
+++ Formula/minikube.rb
@@ -6,6 +6,13 @@ class Minikube < Formula
   sha256 "cada05315d6643a3d8813b75c36b703773df739f32a576b25b8572a51ca90ee6"
   head "https://github.com/kubernetes/minikube.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "dfa11236b9bf1f116102e75d485f3267289d8c609ec8a04fbd6dbb739ef87a71" => :el_capitan
+    sha256 "c7755173e1a9cb1f4ec36e8bf1c8595542dc9ef49985d6806c1cfa8800624431" => :yosemite
+    sha256 "ebb870834ed8b051acaf2a63f865788dd09867e6ca424a15ffa06e9aa14674f8" => :mavericks
+  end
+
   def install
     mv "minikube-darwin-amd64", "minikube"
     bin.install "minikube"
