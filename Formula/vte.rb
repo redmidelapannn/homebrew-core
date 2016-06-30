0038e06774dbe716faddca8f1e13a4cb0150ce4c
Rakesh <rakkesh@users.noreply.github.com>
2016-06-30 17:25:19 +0530

    vte: revision bump for python update

--- Formula/vte.rb
+++ Formula/vte.rb
@@ -3,6 +3,7 @@ class Vte < Formula
   homepage "https://developer.gnome.org/vte/"
   url "https://download.gnome.org/sources/vte/0.28/vte-0.28.2.tar.xz"
   sha256 "86cf0b81aa023fa93ed415653d51c96767f20b2d7334c893caba71e42654b0ae"
+  revision 1
 
   bottle do
     sha256 "2d4c27bd624e106946dc839ecab9eea7b0ffd51cf9df82b2bb50673e25f3cb35" => :el_capitan
@@ -24,7 +25,7 @@ class Vte < Formula
       "--disable-dependency-tracking",
       "--prefix=#{prefix}",
       "--disable-Bsymbolic",
-      "--enable-python"
+      "--enable-python",
     ]
 
     # pygtk-codegen-2.0 has been deprecated and replaced by
