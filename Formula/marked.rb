7bcde2dba0dc27eb600cfa48e62b1ff3add4e688
BrewTestBot <brew-test-bot@googlegroups.com>
2016-06-30 02:31:03 +0100

    marked: add 0.3.5 bottle.

--- Formula/marked.rb
+++ Formula/marked.rb
@@ -7,6 +7,13 @@ class Marked < Formula
   sha256 "4a9cb612dd9fa4a10f9c43e252626b568a5beead2e3fa02f7b0e4f71f8453adc"
   head "https://github.com/chjj/marked.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "34d6a64cdbbef4d14ca8729590b799b44529cc333b0886bac772308258804cfa" => :el_capitan
+    sha256 "8074b2bdb80dade78c08c23b54fbeb88339c7ff6b1816f8ff6e1fe225d48fb2f" => :yosemite
+    sha256 "1a0879416cca100b468b011b73bca2865158e2d4bbe9a237f0b6ce7848c04965" => :mavericks
+  end
+
   depends_on "node"
 
   def install
