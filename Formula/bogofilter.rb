a36a5d7285c9c90e469e6ae86e0f3bf9f6b25cb9
Zhiming Wang <zmwangx@gmail.com>
2016-07-01 22:47:19 +0800

    bogofilter: revision for berkeley-db update

--- Formula/bogofilter.rb
+++ Formula/bogofilter.rb
@@ -3,6 +3,7 @@ class Bogofilter < Formula
   homepage "http://bogofilter.sourceforge.net"
   url "https://downloads.sourceforge.net/project/bogofilter/bogofilter-1.2.4/bogofilter-1.2.4.tar.bz2"
   sha256 "e10287a58d135feaea26880ce7d4b9fa2841fb114a2154bf7da8da98aab0a6b4"
+  revision 1
 
   bottle do
     sha256 "22cdf81e4f0f6bc9b45f5e46ddcca5262141f726f425724ef3b208138fd6d528" => :el_capitan
