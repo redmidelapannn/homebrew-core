3e7bfe2ee7d33dfa144df37ff8c79a413c3e88fb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-02 19:55:00 +0100

    pngpaste: update 1.0.1 bottle.

--- Formula/pngpaste.rb
+++ Formula/pngpaste.rb
@@ -6,10 +6,9 @@ class Pngpaste < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "bfb5fd80a48aa24949ef9551f12144afeef8cbc54a04db47476754681dbcac2d" => :el_capitan
-    sha256 "e134f7bca93ef8e51d1729c145b060f688999f6d3eeda051f836831c6774c2ef" => :yosemite
-    sha256 "eeea5b97cd20b83f7d01fa73d9b8bc98f776dc122769cc682312981dde54b07e" => :mavericks
-    sha256 "c8577460546d1c8f7f4cf9e833a420989b1fb94969dc1fcffd1c8f6d07a78e93" => :mountain_lion
+    sha256 "de5f58e15c5d4477c3f085406db0361ba26d68500074921339f3f07bdc2c882c" => :el_capitan
+    sha256 "4a3842c6064d0d9f26158f7334737395c98f23812f8be7058dae1fb84849f861" => :yosemite
+    sha256 "1850d9468e4a1153098e7ef51892fc0ed09e43dea432c6e245ded503da672d75" => :mavericks
   end
 
   def install
