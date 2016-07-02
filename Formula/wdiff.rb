27e900fdfcd82611d002a15eddaefef9c611fcfd
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-02 03:41:39 +0100

    wdiff: update 1.2.2 bottle.

--- Formula/wdiff.rb
+++ Formula/wdiff.rb
@@ -7,10 +7,10 @@ class Wdiff < Formula
 
   bottle do
     cellar :any_skip_relocation
+    revision 1
     sha256 "2e3e40ebdb98e11d783fd5e8e9f5c7c553ae06c739b47a4cf3aa3c4c9483cdf2" => :el_capitan
-    sha256 "1e34ac95a5aa21146f93c5bd0d7d1b22c48941101dc684d019d6d9700da90e8f" => :yosemite
-    sha256 "6cf8260aaa5f0da951bf405f3ed05e1660f8ca7d585c11324319b0c1e6371d56" => :mavericks
-    sha256 "06da8b4a640ef51d0dd884b436d3909c4bd2c5c00ea5da9e81158554a00f0dbe" => :mountain_lion
+    sha256 "90e57be5c2d854f3467e0f6b7acf2221f6eb974da6e2f0e907b5169e138b89c5" => :yosemite
+    sha256 "16d6ca4fbb4644722cc74097e7fa5cd5648a0d6f112b091f881e66ccb7abfdf9" => :mavericks
   end
 
   depends_on "gettext" => :optional
