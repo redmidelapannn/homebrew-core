0be743675d27830590a3fdee33dec0e43e8331a9
Camden Narzt <c.narzt@me.com>
2016-07-02 18:52:10 -0600

    style nits

--- Formula/racer.rb
+++ Formula/racer.rb
@@ -1,7 +1,6 @@
 class Racer < Formula
   desc "Rust Code Completion utility"
   homepage "https://github.com/phildawes/racer"
-
   url "https://github.com/phildawes/racer/archive/v1.1.0.tar.gz"
   sha256 "f969e66d5119f544347e9f9424e83d739eef0c75811fa1a5c77e58df621e066d"
   head "https://github.com/phildawes/racer.git"
@@ -15,19 +14,20 @@ class Racer < Formula
 
   def install
     system "cargo", "build", "--release"
-    (libexec/"bin").install "./target/release/racer"
-    (bin/"racer").write_env_script(libexec/"bin/racer", :RUST_SRC_PATH => "#{share}/rust_src/current")
+    (libexec/"bin").install "target/release/racer"
+    (bin/"racer").write_env_script(libexec/"bin/racer", :RUST_SRC_PATH => pkgshare/"rust_src/current")
+
     resource("rust_source").stage do
-      rm_rf ["src/llvm", "src/test", "src/librustdoc", "src/etc/snapshot.pyc"]
-      (share/"rust_src/#{Formula["rust"].stable.version}").install Dir["src/*"]
+      rm_rf Dir["src/{llvm,test,librustdoc,etc/snapshot.pyc}"]
+      (pkgshare/"rust_src/#{Formula["rust"].stable.version}").install Dir["src/*"]
     end
   end
 
   def post_install
-    (share/"rust_src").install_symlink Formula["rust"].stable.version.to_s => "current"
+    (pkgshare/"rust_src").install_symlink Formula["rust"].stable.version.to_s => "current"
   end
 
   test do
-    system "#{bin}/racer", "complete", "std::io::B"
+    system bin/"racer", "complete", "std::io::B"
   end
 end
