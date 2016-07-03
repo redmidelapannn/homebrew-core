8dd9c8d676f65ca05c9bfd5b0e5f897b1e87fb00
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-03 04:08:46 +0100

    qt5: update 5.7.0 bottle.

--- Formula/qt5.rb
+++ Formula/qt5.rb
@@ -21,9 +21,8 @@ class Qt5 < Formula
   head "https://code.qt.io/qt/qt5.git", :branch => "5.7", :shallow => false
 
   bottle do
-    sha256 "2aaa410f2ab2fbbddbc8c3438e43bc9f4271774c794bcae8f935fb6b1b5a82ed" => :el_capitan
-    sha256 "eefa531c6ebc757982b31f17935fa2220aad52caf3112e389a878dce04f40490" => :yosemite
-    sha256 "73d33dd2563c39542844c276a7bd43463f2974fde141e7afeb3057168adbe606" => :mavericks
+    sha256 "51cb6b849fee1c69cdb1c04ecd4503464accee4652489c40b7034d91a1433ad3" => :el_capitan
+    sha256 "f5ada7a0d5971fab5fc0c568f7a65d2bc3f419a19ad9bc9f3e443c4d2c9799a7" => :yosemite
   end
 
   # Restore `.pc` files for framework-based build of Qt 5 on OS X. This
