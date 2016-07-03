c7449e8352b9f0e9559a7e1ee3d331778ed45215
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-03 05:29:45 +0100

    racer: add 1.1.0 bottle.

--- Formula/racer.rb
+++ Formula/racer.rb
@@ -5,6 +5,13 @@ class Racer < Formula
   sha256 "f969e66d5119f544347e9f9424e83d739eef0c75811fa1a5c77e58df621e066d"
   head "https://github.com/phildawes/racer.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ef441bc96eaa282d6a3327a0cad3a21d9ab570d7b7649db5b5b868e87e817349" => :el_capitan
+    sha256 "c9e9018fc6b415b1d25b421eff3760a32e981b0dace90a6249e7d8b35becfe7e" => :yosemite
+    sha256 "8a0290f9d20c5f90922acbf5eabaff5d6cd1fb5e445f68b21d4d54ac8820e123" => :mavericks
+  end
+
   depends_on "rust" => :build
 
   resource "rust_source" do
