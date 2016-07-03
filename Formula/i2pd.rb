5d22d06e591bacfe5e5bdfc1ade9288d7a301640
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-03 10:33:16 +0100

    i2pd: add 2.8.0 bottle.

--- Formula/i2pd.rb
+++ Formula/i2pd.rb
@@ -7,6 +7,13 @@ class I2pd < Formula
     url "https://github.com/PurpleI2P/i2pd/archive/2.8.0.tar.gz"
     sha256 "3f3f538b3c5b3095021311b434456c42d6f3b5e43dc7972c263b10ef179ba063"
   end
+  bottle do
+    cellar :any
+    sha256 "694ace3ff33409cf5a297a961bc2a256df48419d5943fc23ad24d4e67e673f4e" => :el_capitan
+    sha256 "8bfbc79714d8a710cf6003ef4e9356ea24874a49828194916ca39c2f89942003" => :yosemite
+    sha256 "378d6133729e4a8d2cdf697ffcef5d42240d5b6e88efe7c5a00cf076a7a90460" => :mavericks
+  end
+
 
   depends_on "libressl"
   depends_on "boost"
