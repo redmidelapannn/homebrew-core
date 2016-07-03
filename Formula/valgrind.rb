718534f71f2c289cd86dd6ec12e8e952828db57b
Tim D. Smith <git@tim-smith.us>
2016-07-02 23:51:10 -0700

    valgrind: fix assertion failure on 10.11

--- Formula/valgrind.rb
+++ Formula/valgrind.rb
@@ -5,6 +5,11 @@ class Valgrind < Formula
   stable do
     url "http://valgrind.org/downloads/valgrind-3.11.0.tar.bz2"
     sha256 "6c396271a8c1ddd5a6fb9abe714ea1e8a86fce85b30ab26b4266aeb4c2413b42"
+
+    # Fix tst->os_state.pthread - magic_delta assertion failure on OSX 10.11
+    # https://bugs.kde.org/show_bug.cgi?id=354883
+    # https://github.com/liquid-mirror/valgrind/commit/8f0b10fdc795f6011c17a7d80a0d65c36fcb8619.diff
+    patch :DATA
   end
 
   bottle do
@@ -54,3 +59,36 @@ class Valgrind < Formula
     system "#{bin}/valgrind", "ls", "-l"
   end
 end
+__END__
+diff --git a/coregrind/m_syswrap/syswrap-amd64-darwin.c b/coregrind/m_syswrap/syswrap-amd64-darwin.c
+index 8f13e71..7fb8b2c 100644
+--- a/coregrind/m_syswrap/syswrap-amd64-darwin.c
++++ b/coregrind/m_syswrap/syswrap-amd64-darwin.c
+@@ -479,10 +479,8 @@ void wqthread_hijack(Addr self, Addr kport, Addr stackaddr, Addr workitem,
+        UWord magic_delta = 0;
+ #      elif DARWIN_VERS == DARWIN_10_7 || DARWIN_VERS == DARWIN_10_8
+        UWord magic_delta = 0x60;
+-#      elif DARWIN_VERS == DARWIN_10_9 || DARWIN_VERS == DARWIN_10_10
++#      elif DARWIN_VERS == DARWIN_10_9 || DARWIN_VERS == DARWIN_10_10 || DARWIN_VERS == DARWIN_10_11
+        UWord magic_delta = 0xE0;
+-#      elif DARWIN_VERS == DARWIN_10_11
+-       UWord magic_delta = 0x100;
+ #      else
+ #        error "magic_delta: to be computed on new OS version"
+          // magic_delta = tst->os_state.pthread - self
+diff --git a/coregrind/m_syswrap/syswrap-x86-darwin.c b/coregrind/m_syswrap/syswrap-x86-darwin.c
+index a9282ee..37bbbc3 100644
+--- a/coregrind/m_syswrap/syswrap-x86-darwin.c
++++ b/coregrind/m_syswrap/syswrap-x86-darwin.c
+@@ -427,10 +427,8 @@ void wqthread_hijack(Addr self, Addr kport, Addr stackaddr, Addr workitem,
+       UWord magic_delta = 0;
+ #     elif DARWIN_VERS == DARWIN_10_7 || DARWIN_VERS == DARWIN_10_8
+       UWord magic_delta = 0x48;
+-#     elif DARWIN_VERS == DARWIN_10_9 || DARWIN_VERS == DARWIN_10_10
++#     elif DARWIN_VERS == DARWIN_10_9 || DARWIN_VERS == DARWIN_10_10 || DARWIN_VERS == DARWIN_10_11
+       UWord magic_delta = 0xB0;
+-#     elif DARWIN_VERS == DARWIN_10_11
+-      UWord magic_delta = 0x100;
+ #     else
+ #       error "magic_delta: to be computed on new OS version"
+         // magic_delta = tst->os_state.pthread - self
