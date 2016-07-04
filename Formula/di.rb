f0664b8025cc78375735346a0f122beac7b007f1
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-04 13:02:33 +0100

    di: update 4.36 bottle.

--- Formula/di.rb
+++ Formula/di.rb
@@ -6,10 +6,10 @@ class Di < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "088b843fb5b82aa978f8cfe3ae0d90dbac7811b326a6dd9be027e3bbc740b706" => :el_capitan
-    sha256 "aec4d32a7d6930610423b7a9d72033e996a2f50bfe54d1cb778ca55dca5d243d" => :yosemite
-    sha256 "00958ab5af10860c7d7283d7e240f03a56d27f85d36a89872740e7391609e203" => :mavericks
-    sha256 "ad54d80a30189a79074d05e4e79ac7ef5fa4436c217866dea2382b351ec44127" => :mountain_lion
+    revision 1
+    sha256 "a82a8b21b9cfadc1473f5212177b17431bad96b2bf79a106e87325e63d7240e0" => :el_capitan
+    sha256 "507b756c422e94ccbda955e0b740cb071833cf57d426d97e9e78f25684b5cd64" => :yosemite
+    sha256 "383da0d489569e028c6ab9372ed11e8387bed9c6316c6974c675516f1c0cf224" => :mavericks
   end
 
   def install
