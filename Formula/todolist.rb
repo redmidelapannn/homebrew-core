56a533c130799529b8ee553e85c718de31b7d5b5
Grant Ammons <gammons@gmail.com>
2016-07-05 15:06:32 -0400

    Remove head tag and fix the go_resource urls

--- Formula/todolist.rb
+++ Formula/todolist.rb
@@ -6,8 +6,6 @@ class Todolist < Formula
   url "https://github.com/gammons/todolist/archive/0.2.0.tar.gz"
   sha256 "13882bbbbcb86e05d6729f5bab2507de3aeabc1b4b4b34008480ee7549212c86"
 
-  head "https://github.com/gammons/todolist.git"
-
   depends_on "go" => :build
 
   go_resource "github.com/mattn/go-isatty" do
@@ -15,21 +13,19 @@ class Todolist < Formula
   end
 
   go_resource "github.com/mattn/go-colorable" do
-    url "https://github.com/mattn/go-colorable", :revision => "9056b7a9f2d1f2d96498d6d146acd1f9d5ed3d59"
+    url "https://github.com/mattn/go-colorable.git", :revision => "9056b7a9f2d1f2d96498d6d146acd1f9d5ed3d59"
   end
 
   go_resource "github.com/fatih/color" do
-    url "https://github.com/fatih/color", :revision => "87d4004f2ab62d0d255e0a38f1680aa534549fe3"
+    url "https://github.com/fatih/color.git", :revision => "87d4004f2ab62d0d255e0a38f1680aa534549fe3"
   end
 
   go_resource "github.com/jinzhu/now" do
-    url "https://github.com/jinzhu/now",
-      :revision => "d9861969c7a7e84746d341c09020c2ef8a617f8f", :using => :git
+    url "https://github.com/jinzhu/now.git", :revision => "d9861969c7a7e84746d341c09020c2ef8a617f8f"
   end
 
   go_resource "github.com/julienschmidt/httprouter" do
-    url "https://github.com/julienschmidt/httprouter",
-      :revision => "77366a47451a56bb3ba682481eed85b64fea14e8", :using => :git
+    url "https://github.com/julienschmidt/httprouter.git", :revision => "77366a47451a56bb3ba682481eed85b64fea14e8"
   end
 
   def install
