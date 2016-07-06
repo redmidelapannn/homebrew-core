d2bf9cf70fbb507d23e36b65486d408dd02f5a7f
ilovezfs <ilovezfs@icloud.com>
2016-07-05 22:56:00 -0700

    assimp 3.3

--- Formula/assimp.rb
+++ Formula/assimp.rb
@@ -1,8 +1,8 @@
 class Assimp < Formula
   desc "Portable library for importing many well-known 3D model formats"
   homepage "http://www.assimp.org"
-  url "https://github.com/assimp/assimp/archive/v3.2.tar.gz"
-  sha256 "187f825c563e84b1b17527a4da0351aa3d575dfd696a9d204ae4bb19ee7df94a"
+  url "https://github.com/assimp/assimp/archive/v3.3.tar.gz"
+  sha256 "a57359ae31e2a5ef4167eadb7c11a3339bd9cb4a5f6241c0fb22db0aebaf4436"
 
   head "https://github.com/assimp/assimp.git"
 
