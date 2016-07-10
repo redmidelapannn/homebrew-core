a1ae2e8eab69c1538c7f360a6bd396dfeda8feda
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-10 15:43:31 +0100

    hg-git: add 0.8.5 bottle.

--- Formula/hg-git.rb
+++ Formula/hg-git.rb
@@ -5,6 +5,13 @@ class HgGit < Formula
   sha256 "60bf47386fc0c9eef809344ff2be98eaa5add822f70dd25670470d06673fe2c4"
   head "https://bitbucket.org/durin42/hg-git", :using => :hg
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "cbc59f10245783fca8e90a45ef2b648770cdcf7ac92d09896b8ec313827a4a29" => :el_capitan
+    sha256 "c306e248f8718b930975fcbd6c6c317324f105cf39bf7f5723b667d3494ade0a" => :yosemite
+    sha256 "f5d7b940a9b50da5ec4f766213995a76a27be42b06905762c74abaa4f479448e" => :mavericks
+  end
+
   depends_on :python if MacOS.version <= :snow_leopard
   depends_on :hg
 
