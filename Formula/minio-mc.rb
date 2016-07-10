bc74862127d70300d8bb89354cec9bf5ad0ecd1e
Remco <remco@dutchcoders.io>
2016-07-10 22:47:05 +0200

    several fixes
    * removed extra desc
    * removed language/go
    * using bin/"mc" directly
    * changed test to use version

--- Formula/minio-mc.rb
+++ Formula/minio-mc.rb
@@ -1,12 +1,9 @@
-require "language/go"
-
 class MinioMc < Formula
   desc "ls, cp, mkdir, diff and rsync for filesystems and object storage"
   homepage "https://github.com/minio/mc"
   url "https://github.com/minio/mc/archive/RELEASE.2016-06-03T18-48-37Z.tar.gz"
   version "20160603184837"
   sha256 "ac27ce934d5d71a1150eed69641a7ba801f3ae90c064ee944c154e311a5e6dbc"
-  desc "object storage server compatible with Amazon S3"
   head "https://github.com/minio/mc.git"
 
   depends_on "go" => :build
@@ -18,13 +15,11 @@ class MinioMc < Formula
     clipath.install Dir["*"]
 
     cd buildpath/"src/github.com/minio/mc/" do
-      system "go", "build", "-o", buildpath/"mc"
+      system "go", "build", "-o", bin/"mc"
     end
-
-    bin.install buildpath/"mc"
   end
 
   test do
-    system "#{bin}/mc"
+    system bin/"mc","version"
   end
 end
