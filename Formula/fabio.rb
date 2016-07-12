838691261a6cc466f885ab86ac74e0f14bfb84e8
Frank Schroeder <frschroeder@ebay.com>
2016-07-12 18:44:35 +0200

    Drop tags netgo and vendor experiment

--- Formula/fabio.rb
+++ Formula/fabio.rb
@@ -23,9 +23,8 @@ class Fabio < Formula
     ln_s buildpath, buildpath/"src/github.com/eBay/fabio"
 
     ENV["GOPATH"] = buildpath.to_s
-    ENV["GO15VENDOREXPERIMENT"] = "1"
 
-    system "go", "install", "-tags", "netgo", "github.com/eBay/fabio"
+    system "go", "install", "github.com/eBay/fabio"
     bin.install "#{buildpath}/bin/fabio"
   end
 
