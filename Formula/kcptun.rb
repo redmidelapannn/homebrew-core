efacb83e31c7141a41de2b6c648438d0582ffaa9
keepwn <keepwn@gmail.com>
2016-07-13 18:14:55 +0800

    Update kcptun

--- Formula/kcptun.rb
+++ Formula/kcptun.rb
@@ -1,7 +1,7 @@
 require "language/go"
 
 class Kcptun < Formula
-  desc "extremely simple & fast udp tunnel based on kcp protocol"
+  desc "Extremely simple & fast UDP tunnel based on KCP protocol"
   homepage "https://github.com/xtaci/kcptun"
   url "https://github.com/xtaci/kcptun/archive/v20160701.tar.gz"
   sha256 "865ad3dccd5bdd96075e7653da3b8ff4aaff576413c3b4e330d993e6f5209f68"
@@ -10,39 +10,48 @@ class Kcptun < Formula
   depends_on "go" => :build
 
   go_resource "golang.org/x/crypto" do
-    url "https://github.com/golang/crypto.git"
+    url "https://github.com/golang/crypto.git",
+      :revision => "2c99acdd1e9b90d779ca23f632aad86af9909c62"
   end
 
   go_resource "github.com/golang/snappy" do
-    url "https://github.com/golang/snappy.git"
+    url "https://github.com/golang/snappy.git",
+      :revision => "d9eb7a3d35ec988b8585d4a0068e462c27d28380"
   end
 
   go_resource "github.com/hashicorp/yamux" do
-    url "https://github.com/hashicorp/yamux.git"
+    url "https://github.com/hashicorp/yamux.git",
+      :revision => "badf81fca035b8ebac61b5ab83330b72541056f4"
   end
 
   go_resource "github.com/urfave/cli" do
-    url "https://github.com/urfave/cli.git"
+    url "https://github.com/urfave/cli.git",
+      :revision => "3a5216227e14699bf7810b2573db60bf4b3f71b5"
   end
 
   go_resource "github.com/xtaci/kcp-go" do
-    url "https://github.com/xtaci/kcp-go.git"
+    url "https://github.com/xtaci/kcp-go.git",
+      :revision => "9b06339142a3461f727cda16ee11d8d402f8bbbe"
   end
 
   go_resource "github.com/klauspost/crc32" do
-    url "https://github.com/klauspost/crc32.git"
+    url "https://github.com/klauspost/crc32.git",
+      :revision => "19b0b332c9e4516a6370a0456e6182c3b5036720"
   end
 
   go_resource "github.com/klauspost/reedsolomon" do
-    url "https://github.com/klauspost/reedsolomon.git"
+    url "https://github.com/klauspost/reedsolomon.git",
+      :revision => "467733eb9c209042fb37d2074a5b6be33a529be0"
   end
 
   go_resource "github.com/klauspost/cpuid" do
-    url "https://github.com/klauspost/cpuid.git"
+    url "https://github.com/klauspost/cpuid.git",
+      :revision => "09cded8978dc9e80714c4d85b0322337b0a1e5e0"
   end
 
   go_resource "golang.org/x/net" do
-    url "https://github.com/golang/net.git"
+    url "https://github.com/golang/net.git",
+      :revision => "f841c39de738b1d0df95b5a7187744f0e03d8112"
   end
 
   def install
