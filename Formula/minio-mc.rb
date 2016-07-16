a869837f1820f62fb25a20382b96bbd708033b6a
Remco <remco@dutchcoders.io>
2016-07-16 22:02:44 +0200

    fixed issues

--- Formula/minio-mc.rb
+++ Formula/minio-mc.rb
@@ -1,10 +1,11 @@
 class MinioMc < Formula
   desc "ls, cp, mkdir, diff and rsync for filesystems and object storage"
   homepage "https://github.com/minio/mc"
-  url "https://github.com/minio/mc/archive/RELEASE.2016-06-03T18-48-37Z.tar.gz"
-  version "20160603184837"
-  sha256 "ac27ce934d5d71a1150eed69641a7ba801f3ae90c064ee944c154e311a5e6dbc"
+  url "https://github.com/minio/mc/archive/RELEASE.2016-07-13T21-46-05Z.tar.gz"
+  version "20160713214605"
+  sha256 "611444a66ea3d2cc8fbb821147bcd3995c1956ef2aa9d62cd35f6d07f3f972cf"
   head "https://github.com/minio/mc.git"
+  conflicts_with "midnight-commander"
 
   depends_on "go" => :build
 
@@ -14,12 +15,13 @@ class MinioMc < Formula
     clipath = buildpath/"src/github.com/minio/mc"
     clipath.install Dir["*"]
 
-    cd buildpath/"src/github.com/minio/mc/" do
+    cd clipath do
       system "go", "build", "-o", bin/"mc"
     end
   end
 
   test do
-    system bin/"mc", "version"
+    system bin/"mc", "mb", (testpath"/test")
+    assert File.exist?(testpath/"test")
   end
 end
