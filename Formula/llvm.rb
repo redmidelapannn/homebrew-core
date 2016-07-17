013f6d44b24cd7770770e94ee24a95203cbac864
ELI JOSEPH BRADLEY <EtherTyper@users.noreply.github.com>
2016-07-17 15:55:20 -0500

    Whoops, apply patch to normal installs.

--- Formula/llvm.rb
+++ Formula/llvm.rb
@@ -220,6 +220,12 @@ class Llvm < Formula
       ENV.permit_arch_flags
     end
 
+    if MacOS.version >= :el_capitan
+      inreplace "#{libcxx_buildpath}/include/string",
+        "basic_string<_CharT, _Traits, _Allocator>::basic_string(const allocator_type& __a)",
+        "basic_string<_CharT, _Traits, _Allocator>::basic_string(const allocator_type& __a) noexcept(is_nothrow_copy_constructible<allocator_type>::value)"
+    end
+
     args = %w[
       -DLLVM_OPTIMIZED_TABLEGEN=ON
       -DLLVM_ENABLE_EH=ON
@@ -394,12 +400,6 @@ class Llvm < Formula
     # link against installed libc++
     # related to https://github.com/Homebrew/legacy-homebrew/issues/47149
     if build_libcxx?
-      if MacOS.version >= :el_capitan
-        inreplace "#{libcxx_buildpath}/include/string",
-          "basic_string<_CharT, _Traits, _Allocator>::basic_string(const allocator_type& __a)",
-          "basic_string<_CharT, _Traits, _Allocator>::basic_string(const allocator_type& __a) noexcept(is_nothrow_copy_constructible<allocator_type>::value)"
-      end
-
       system "#{bin}/clang++", "-v", "-nostdinc",
               "-std=c++11", "-stdlib=libc++",
               "-I#{MacOS::Xcode.toolchain_path}/usr/include/c++/v1",
