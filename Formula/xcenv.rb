4f42398a6f0f92d5f2c8d9a90780697ad5939d58
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-18 20:50:21 +0100

    xcenv: add 1.0.0 bottle.

--- Formula/xcenv.rb
+++ Formula/xcenv.rb
@@ -5,6 +5,13 @@ class Xcenv < Formula
   sha256 "257fbb6c13aa434142a5b74686114eb432b6f0f005a6127aa53b1c2628870d39"
   head "https://github.com/xcenv/xcenv.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "44ef844200a744d69e48f8f5574e54e24560799d9ecf6cb04d9d7f20ab5ac7e7" => :el_capitan
+    sha256 "03a642b7af921d65c673de503131290cc932f3c153258bc47d398b938d7ea002" => :yosemite
+    sha256 "0224383ad76f82d9a6b8878c045740c5ac0663ea159ff794c6760d948e46a1ea" => :mavericks
+  end
+
   def install
     prefix.install ["bin", "libexec"]
   end
