65ed200f18742a945f9174ed3d2aaf363361ceec
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-19 03:21:53 +0100

    json-glib: update 1.2.0 bottle.

--- Formula/json-glib.rb
+++ Formula/json-glib.rb
@@ -5,9 +5,10 @@ class JsonGlib < Formula
   sha256 "99d6dfbe49c08fd7529f1fe8dcb1893b810a1bb222f1e7b65f41507658b8a7d3"
 
   bottle do
+    revision 1
     sha256 "552b84c3d28960bbd36105d0325c6a5ac9b7c1abb722e7cb3b8a4785f1acffc5" => :el_capitan
     sha256 "d9e25aeb683db66120566da0d01079bc05866e7e42465db2abd468e3a36d3597" => :yosemite
-    sha256 "3f0b31f25463eed33c0cc15d9593a121412b23216d2ac2956a64627d87526548" => :mavericks
+    sha256 "eed2c8ef6f3e07ff801cedba72cdad44f5022d07a98be50cfb4c02f847201301" => :mavericks
   end
 
   depends_on "pkg-config" => :build
