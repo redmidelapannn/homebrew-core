87d97fdb0357cac25f82276b0bbdac8d3bbf9cea
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-20 23:51:25 +0100

    vaulted: add 1.0.0 bottle.

--- Formula/vaulted.rb
+++ Formula/vaulted.rb
@@ -6,6 +6,13 @@ class Vaulted < Formula
 
   head "https://github.com/miquella/vaulted"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "35bd5cd56104062aec1fbeaa07b6177674763bf061ed54e92a65ccb980f6b2bd" => :el_capitan
+    sha256 "0ccb7e421d6e1443e658511ad49cad57546a84ceab3fe8871a5069b5a8492e66" => :yosemite
+    sha256 "845716dfca47c3e3d765b015351e54c3fa2ea530887e6723e794e6118dd1bd39" => :mavericks
+  end
+
   depends_on "go" => :build
 
   def install
