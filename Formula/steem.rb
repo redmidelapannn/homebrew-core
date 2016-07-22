cde57bcaa15b6a6540c786195a7569ebb18ddc87
Aleksandr Zykov <tiger@plexrayinc.com>
2016-07-22 09:21:27 +0300

    steem v0.12.0

--- Formula/steem.rb
+++ Formula/steem.rb
@@ -2,8 +2,8 @@ class Steem < Formula
   desc "Proof of Work blockchain with an unproven consensus algorithm."
   homepage "https://steemit.com/"
   url "https://github.com/steemit/steem.git",
-      :tag => "v0.11.0",
-      :revision => "f002ba9ff2054478347c1e8f5e0b6e07f2feeadd"
+      :tag => "v0.12.0",
+      :revision => "5c7378e3d16775683d7af70e16d52ed4f1de0977"
 
   depends_on :macos => [:yosemite, :el_capitan, :sierra]
 
