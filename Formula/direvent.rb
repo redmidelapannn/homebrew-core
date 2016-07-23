4b4a4957f4a58f282c1f2482606fe10bfe212fc1
Fredrik Fornwall <fredrik@fornwall.net>
2016-07-23 16:14:32 +0200

    Add mirror and assert version on test

--- Formula/direvent.rb
+++ Formula/direvent.rb
@@ -2,6 +2,7 @@ class Direvent < Formula
   desc "Monitors events in the file system directories"
   homepage "http://www.gnu.org.ua/software/direvent/direvent.html"
   url "https://ftpmirror.gnu.org/direvent/direvent-5.1.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/direvent/direvent-5.1.tar.gz"
   sha256 "c461600d24183563a4ea47c2fd806037a43354ea68014646b424ac797a959bdb"
 
   def install
@@ -14,6 +15,6 @@ class Direvent < Formula
   end
 
   test do
-    system "#{bin}/direvent", "--version"
+    assert_match version.to_s, shell_output("#{bin}/direvent --version")
   end
 end
