3b761969e348cb5a20abd6ce17211e4def8ff5ce
David Holm <dholmster@gmail.com>
2016-07-23 18:33:20 +0200

    uwsgi 2.0.13.1

--- Formula/uwsgi.rb
+++ Formula/uwsgi.rb
@@ -1,9 +1,8 @@
 class Uwsgi < Formula
   desc "Full stack for building hosting services"
   homepage "https://uwsgi-docs.readthedocs.org/en/latest/"
-  url "https://projects.unbit.it/downloads/uwsgi-2.0.11.2.tar.gz"
-  sha256 "0b889b0b4d2dd3f6625df28cb0b86ec44a68d074ede2d0dfad0b91e88914885c"
-  revision 2
+  url "https://projects.unbit.it/downloads/uwsgi-2.0.13.1.tar.gz"
+  sha256 "2eca0c2f12ab76f032154cd147f4d5957d3195a022678d59cb507f4995a48d7f"
 
   head "https://github.com/unbit/uwsgi.git"
 
@@ -13,13 +12,18 @@ class Uwsgi < Formula
     sha256 "e30fad4f2f875219109c554bd203038b3fc4769ddad9dc6914414fd40fa8f321" => :mavericks
   end
 
+  option "with-coro", "Compile with Perl Coro support"
+  option "with-greenlet", "Compile with Python greenlet support"
   option "with-java", "Compile with Java support"
   option "with-php", "Compile with PHP support (PHP must be built for embedding)"
   option "with-ruby", "Compile with Ruby support"
+  option "with-stackless", "Compile with Python stackless support"
+  option "with-tornado", "Compile with Python tornado support"
 
-  depends_on "pkg-config" => :build
-  depends_on "pcre"
   depends_on "openssl"
+  depends_on "pcre"
+  depends_on "pkg-config" => :build
+  depends_on "sqlite" => :linked
   depends_on :python if MacOS.version <= :snow_leopard
 
   depends_on "geoip" => :optional
@@ -27,6 +31,7 @@ class Uwsgi < Formula
   depends_on "go" => [:build, :optional]
   depends_on "jansson" => :optional
   depends_on "libffi" => :optional
+  depends_on "libmatheval" => :optional
   depends_on "libxslt" => :optional
   depends_on "libyaml" => :optional
   depends_on "lua51" => :optional
@@ -41,7 +46,9 @@ class Uwsgi < Formula
   depends_on "rrdtool" => :optional
   depends_on "rsyslog" => :optional
   depends_on "tcc" => :optional
+  depends_on :tuntap => :optional
   depends_on "v8" => :optional
+  depends_on "zabbix" => :optional
   depends_on "zeromq" => :optional
   depends_on "yajl" if build.without? "jansson"
 
@@ -58,11 +65,6 @@ class Uwsgi < Formula
     json = build.with?("jansson") ? "jansson" : "yajl"
     yaml = build.with?("libyaml") ? "libyaml" : "embedded"
 
-    # Fix build on case-sensitive filesystems
-    # https://github.com/Homebrew/homebrew/issues/45560
-    # https://github.com/unbit/uwsgi/pull/1128
-    inreplace "plugins/alarm_speech/uwsgiplugin.py", "'-framework appkit'", "'-framework AppKit'"
-
     (buildpath/"buildconf/brew.ini").write <<-EOS.undent
       [uwsgi]
       ssl = true
@@ -77,32 +79,39 @@ class Uwsgi < Formula
 
     plugins = ["airbrake", "alarm_curl", "alarm_speech", "asyncio", "cache",
                "carbon", "cgi", "cheaper_backlog2", "cheaper_busyness",
-               "corerouter", "curl_cron", "cplusplus", "dumbloop", "dummy",
-               "echo", "emperor_amqp", "fastrouter", "forkptyrouter", "gevent",
-               "http", "logcrypto", "logfile", "ldap", "logpipe", "logsocket",
-               "msgpack", "notfound", "pam", "ping", "psgi", "pty", "rawrouter",
-               "router_basicauth", "router_cache", "router_expires",
-               "router_hash", "router_http", "router_memcached",
-               "router_metrics", "router_radius", "router_redirect",
-               "router_redis", "router_rewrite", "router_static",
-               "router_uwsgi", "router_xmldir", "rpc", "signal", "spooler",
-               "sqlite3", "sslrouter", "stats_pusher_file",
-               "stats_pusher_socket", "symcall", "syslog",
-               "transformation_chunked", "transformation_gzip",
-               "transformation_offload", "transformation_tofile",
-               "transformation_toupper", "ugreen", "webdav", "zergpool"]
+               "corerouter", "cplusplus", "curl_cron", "dumbloop", "dummy",
+               "echo", "emperor_amqp", "exception_log", "fastrouter",
+               "forkptyrouter", "gevent", "graylog2", "http", "ldap",
+               "legion_cache_fetch", "logcrypto", "logfile", "logpipe",
+               "logsocket", "msgpack", "notfound", "pam", "ping", "psgi", "pty",
+               "rawrouter", "redislog", "router_basicauth", "router_cache",
+               "router_expires", "router_hash", "router_http",
+               "router_memcached", "router_metrics", "router_radius",
+               "router_redirect", "router_redis", "router_rewrite",
+               "router_static", "router_uwsgi", "router_xmldir", "rpc",
+               "signal", "spooler", "sqlite3", "sslrouter", "stats_pusher_file",
+               "stats_pusher_socket", "stats_pusher_statsd", "symcall",
+               "syslog", "transformation_chunked", "transformation_gzip",
+               "transformation_offload", "transformation_template",
+               "transformation_tofile", "transformation_toupper", "ugreen",
+               "webdav", "zergpool"]
 
     plugins << "alarm_xmpp" if build.with? "gloox"
+    plugins << "coroae" if build.with? "coro"
     plugins << "emperor_mongodb" if build.with? "mongodb"
     plugins << "emperor_pg" if build.with? "postgresql"
-    plugins << "ffi" if build.with? "libffi"
+    plugins << "emperor_zeromq" if build.with? "zeromq"
     plugins << "fiber" if build.with? "ruby"
     plugins << "gccgo" if build.with? "go"
     plugins << "geoip" if build.with? "geoip"
+    plugins << "greenlet" if build.with? "greenlet"
     plugins << "jvm" if build.with? "java"
     plugins << "jwsgi" if build.with? "java"
+    plugins << "libffi" if build.with? "libffi"
     plugins << "libtcc" if build.with? "tcc"
+    plugins << "logzmq" if build.with? "zeromq"
     plugins << "lua" if build.with? "lua"
+    plugins << "matheval" if build.with? "libmatheval"
     plugins << "mongodb" if build.with? "mongodb"
     plugins << "mongodblog" if build.with? "mongodb"
     plugins << "mongrel2" if build.with? "mongrel2"
@@ -116,9 +125,13 @@ class Uwsgi < Formula
     plugins << "rrdtool" if build.with? "rrdtool"
     plugins << "rsyslog" if build.with? "rsyslog"
     plugins << "servlet" if build.with? "java"
+    plugins << "stackless" if build.with? "stackless"
     plugins << "stats_pusher_mongodb" if build.with? "mongodb"
+    plugins << "tornado" if build.with? "tornado"
+    plugins << "tuntap" if build.with? "tuntap"
     plugins << "v8" if build.with? "v8"
     plugins << "xslt" if build.with? "libxslt"
+    plugins << "zabbix" if build.with? "zabbix"
 
     (libexec/"uwsgi").mkpath
     plugins.each do |plugin|
