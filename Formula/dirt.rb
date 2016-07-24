39f6a5830325f7183c9593ccd2f312abc20b98d9
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-24 08:09:03 +0100

    dirt: update 1.1_1 bottle.

--- Formula/dirt.rb
+++ Formula/dirt.rb
@@ -8,9 +8,9 @@ class Dirt < Formula
 
   bottle do
     cellar :any
-    sha256 "b1a17908e39a4ac31d147f52477a60b538c9c4b9f14caf4b3e6fa26a09e11d03" => :el_capitan
-    sha256 "2600455e336a98af56559766a30c38baa04539b5c3fc087285af12931db84e0b" => :yosemite
-    sha256 "5191aa383bb7edf3a85122140d8ebd6113ab56a851ac8d35e09e086632f8eb9c" => :mavericks
+    sha256 "f3082f9020f27661cbda367e48a67d83c279bb30526186e969971060d9b812e7" => :el_capitan
+    sha256 "92103bb2be80bac4689ebb1e6105ef9159f0cb1e9a24d842ddf017b6882265cf" => :yosemite
+    sha256 "8e773863966ac888da470be71bb9d250787fe08ecc078d64428842ad342dba76" => :mavericks
   end
 
   depends_on "jack"
