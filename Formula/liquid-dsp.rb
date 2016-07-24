b00f0584ce9a344a10ee605c9697e48d91cf82b4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-25 00:26:07 +0100

    liquid-dsp: add 1.2.0 bottle.

--- Formula/liquid-dsp.rb
+++ Formula/liquid-dsp.rb
@@ -5,6 +5,13 @@ class LiquidDsp < Formula
   sha256 "783854b63f5e9a9830dcd95b2c1d5c0bef7d10d22a0e27844a354b59283b6de3"
   head "https://github.com/jgaeddert/liquid-dsp.git"
 
+  bottle do
+    cellar :any
+    sha256 "eb35314488d6e8d748db1e7450cc78a089c6df2cfc3e51fb2897a91c146387c4" => :el_capitan
+    sha256 "3ac15d7cb2ef1a84472338d6eb35c1830a4fe5ef05a20da724ee9bd38db2ff33" => :yosemite
+    sha256 "a2367040f9757feeeb02a46d5bc257490ab3c6d16d16c35b8ebd24a94ff2c0dd" => :mavericks
+  end
+
   depends_on "automake" => :build
   depends_on "autoconf" => :build
   depends_on "fftw" => :recommended
