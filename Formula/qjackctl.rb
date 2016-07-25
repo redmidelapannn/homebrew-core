3ef717aa45d7b007c9fb879e46e0516ed16e0611
Martin Müller <mail@mueller-martin.net>
2016-07-25 03:59:19 +0200

    Change URL to patch from GitHub

--- Formula/qjackctl.rb
+++ Formula/qjackctl.rb
@@ -19,7 +19,7 @@ class Qjackctl < Formula
   # Fixes varaible length array error with LLVM/Clang
   # Reported upstream: https://github.com/rncbc/qjackctl/issues/17
   patch do
-    url "https://patch-diff.githubusercontent.com/raw/rncbc/qjackctl/pull/18.patch"
+    url "https://github.com/rncbc/qjackctl/pulls/18.patch"
     sha256 "cbb7cc72e0086b8e6df24c8c3a462dc0e9297f095573adb7a3cf98502a1902d4"
   end
 
