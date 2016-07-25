7b4af957e3bf18fca4774c239ccd4e3bb2e27182
rgcr <rogelio.cedillo.rivera@gmail.com>
2016-07-25 09:44:28 -0500

    Correcting for PR comments

--- Formula/m-cli.rb
+++ Formula/m-cli.rb
@@ -1,22 +1,21 @@
 class MCli < Formula
   desc "Swiss Army Knife for Mac OS X"
   homepage "https://github.com/rgcr/m-cli"
-  url "https://github.com/rgcr/m-cli/archive/v0.0.8.tar.gz"
-  sha256 "66956e3f21362f4baca25241dcf912c4e7aa27e329b5adb431338b52fd262a6f"
+  url "https://github.com/rgcr/m-cli/archive/v0.0.9.tar.gz"
+  sha256 "957e86c03a8196f60c852672aa0a7c86d9093347e37c16783e67e423fa56f341"
 
   def install
     prefix.install Dir["*"]
     inreplace "#{prefix}/m", /^\[ -L.*|^\s+\|\| pushd.*|^popd.*/, ""
     inreplace "#{prefix}/m", /MPATH=.*/, "MPATH=#{prefix}"
 
-    bin.mkpath
     bin.install_symlink "#{prefix}/m" => "m"
-    bash_completion.install prefix + "completion/bash/m" => "m"
-    zsh_completion.install prefix + "completion/zsh/_m" => "_m"
+	bash_completion.install prefix/"completion/bash/m"
+	zsh_completion.install prefix/"completion/zsh/_m"
   end
 
   test do
-    output = pipe_output("m help 2>&1")
+    output = pipe_output("#{bin}/m help 2>&1")
     assert_no_match /.*No such file or directory.*/, output
     assert_no_match /.*command not found.*/, output
     assert_match /.*Swiss Army Knife for Mac OS X.*/, output
