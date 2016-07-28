3239120ccf5c3f1b1e02d1d82f5f6ea6eee09a2c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-28 15:55:34 +0100

    open-zwave: add 1.4.164 bottle.

--- Formula/open-zwave.rb
+++ Formula/open-zwave.rb
@@ -4,6 +4,12 @@ class OpenZwave < Formula
   url "http://old.openzwave.com/downloads/openzwave-1.4.164.tar.gz"
   sha256 "4ecf39787aaf278c203764069b581dbc26094ce57cafeab4a0c1f012d2c0ac69"
 
+  bottle do
+    sha256 "ae0b9c6601f2316d67847eb7302330d421c1162f184004aa61e19d0a40c7c321" => :el_capitan
+    sha256 "568656c6d967e0bbb0e0d783058fa5db6e5987d1fd295c4af90ebcb26d3e7446" => :yosemite
+    sha256 "0aa245844089e9b144e8a2ae6dabb20fca411f7d07595abb8faa918672c4606c" => :mavericks
+  end
+
   depends_on "pkg-config" => :build
 
   def install
