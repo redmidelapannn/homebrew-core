9763caa239c12c06f1102f07139e26d2b3e08f9a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-29 19:07:38 +0100

    haskell-stack: update 1.1.2_3 bottle.

--- Formula/haskell-stack.rb
+++ Formula/haskell-stack.rb
@@ -13,9 +13,10 @@ class HaskellStack < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e8e2343689c99d85bb1c930d83cbafc8ca7be4fc64faaeb844771a1511a0ff3d" => :el_capitan
-    sha256 "fe0aa6086ec90d711ebf0cd0169f5a3dc76f6f1a8c81b357ba6c81d0a22a86af" => :yosemite
-    sha256 "06a004b35c817fc17b0496aa06a886d5d1a69e9436bb9e8aa5e039f3148212c2" => :mavericks
+    revision 1
+    sha256 "48e171fcdd70d8177b2b7003607ba882667d06965d23e6f1b8adc392045babf3" => :el_capitan
+    sha256 "1755fcc1254d4fe9dd64fbb98c43a8f9463787d0aa7617744d0c50942d203836" => :yosemite
+    sha256 "8fa551dc2b6965c5056920cd6abad822ced8d44aa0396f08e4e4ca6ef0d8e599" => :mavericks
   end
 
   option "without-bootstrap", "Don't bootstrap a stage 2 stack"
