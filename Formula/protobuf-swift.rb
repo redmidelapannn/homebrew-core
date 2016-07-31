7d327c6ebcfd5aa29ca60d80f74f79281e31c149
BrewTestBot <brew-test-bot@googlegroups.com>
2016-07-31 15:23:07 +0100

    protobuf-swift: add 3.0.0_1 bottle.

--- Formula/protobuf-swift.rb
+++ Formula/protobuf-swift.rb
@@ -5,6 +5,13 @@ class ProtobufSwift < Formula
   sha256 "3e93f410844049673a164698589f1cb2c8f8ee1e4169b6cee3c6c32f8f5c4edb"
   revision 1
 
+  bottle do
+    cellar :any
+    sha256 "9784503d21b0b9f9a867b6ff4f073bc892a5ebec63d992e4ddcb9bed453c32cd" => :el_capitan
+    sha256 "0f6fd6fb20abb7bf62c851d128062200fc5332dc9dcf72ae6091cb65d38e3202" => :yosemite
+    sha256 "9f6cdb31392cab7dc52d7665c3c5aceb1d27181b5e280f100f9fe80cb7f4e5dd" => :mavericks
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
