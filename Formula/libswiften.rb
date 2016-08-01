fa7a85f4c98822dd0eefda03430afb21b3e2f63f
ilovezfs <ilovezfs@icloud.com>
2016-08-01 15:17:21 -0700

    libswiften: trigger CI

--- Formula/libswiften.rb
+++ Formula/libswiften.rb
@@ -51,11 +51,14 @@ class Libswiften < Formula
     args << prefix
 
     scons *args
-    man1.install "Swift/Packaging/Debian/debian/swiften-config.1" unless build.stable?
+
+    unless build.stable?
+      man1.install "Swift/Packaging/Debian/debian/swiften-config.1"
+    end
   end
 
   test do
-    system "#{bin}/swiften-config"
+    system bin/"swiften-config"
   end
 end
 
