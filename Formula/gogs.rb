c63d717a277bd895660a1fc944ac1070bed2d470
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-03 02:06:06 +0100

    gogs: add 0.9.48 bottle.

--- Formula/gogs.rb
+++ Formula/gogs.rb
@@ -5,6 +5,13 @@ class Gogs < Formula
   version "0.9.48"
   sha256 "8afbd71fde279e77ed5b16c8a80ef409661ccb3953bf6cc867fbede532686475"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8394b373d68b7de36d9c0e496fb5cb98046dd2cff5770cb63a22d0b19ff1503d" => :el_capitan
+    sha256 "a5a62a8e5a3fb26a0adf9eaec9822fa4aea24f21f36258b902234bcd5054863b" => :yosemite
+    sha256 "35ee8127dd0134e605cb731d1089dc0b145e571cd83059ca1a886dadcb199714" => :mavericks
+  end
+
   def startup_script; <<-EOS.undent
     #!/bin/sh
     cd #{var/"gogs"} && ./gogs "$@"
