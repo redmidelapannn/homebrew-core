1c0c4813d8add7fb6ad1e12f149ee422e4885d07
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-05 16:46:18 +0100

    thrift: update 0.9.3 bottle.

--- Formula/thrift.rb
+++ Formula/thrift.rb
@@ -22,9 +22,9 @@ class Thrift < Formula
 
   bottle do
     cellar :any
-    sha256 "171011fa42efb2fcafb1bae1e2d173e585eda199f145a62a825359c7a622b24b" => :el_capitan
-    sha256 "655bb0a05eb51ff465f8f378a7d3ea2438095e2d4c2a70da45965731b5de9cfb" => :yosemite
-    sha256 "092ff2a100f41871d3527c450403cfc2cf1cc0527ce2fdb4089f93915365713d" => :mavericks
+    revision 1
+    sha256 "eb22b638fcdbfb6f17c95e473094564ce7b3f5e311e2a171469993ec15814c02" => :el_capitan
+    sha256 "ece2c1cf603f00b7f59357bba4a62f843c0aea95eea3f442752fa231b3da1512" => :mavericks
   end
 
   head do
