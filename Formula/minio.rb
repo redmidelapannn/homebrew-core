541a9144a1c1ec2ee69f19cccb18b4f625bcd28a
Remco <remco@dutchcoders.io>
2016-08-08 12:45:36 +0200

    using tag for versioning

--- Formula/minio.rb
+++ Formula/minio.rb
@@ -1,10 +1,8 @@
 class Minio < Formula
   desc "object storage server compatible with Amazon S3"
   homepage "https://github.com/minio/minio"
-  url "https://github.com/minio/minio/archive/RELEASE.2016-07-13T21-46-05Z.tar.gz"
+  url "https://github.com/minio/minio.git", :tag => "RELEASE.2016-07-13T21-46-05Z", :revision => "3f27734c22212f224037a223439a425e6d2b653a"
   version "20160713214605"
-  sha256 "40cd2faf5dd9503c20d2f5a5ad67277e960561fca5869a9d724d1efa763021df"
-  head "https://github.com/minio/minio.git"
 
   bottle do
     cellar :any_skip_relocation
@@ -21,14 +19,15 @@ class Minio < Formula
     clipath = buildpath/"src/github.com/minio/minio"
     clipath.install Dir["*"]
 
-    minio_version = DateTime.parse(version).strftime("%Y-%m-%dT%H:%M:%SZ")
-    minio_release = "RELEASE.#{minio_version}"
-    minio_commit = "3f27734c22212f224037a223439a425e6d2b653a"
-
     cd clipath do
       if build.head?
         system "go", "build", "-o", buildpath/"minio"
       else
+        minio_release = `git tag --points-at HEAD`
+        version = minio_release.gsub(/RELEASE\./, "")
+        minio_version = version
+        minio_commit = `git rev-parse HEAD`
+
         system "go", "build", "-ldflags", "-X main.minioVersion=#{minio_version} -X main.minioReleaseTag=#{minio_release} -X main.minioCommitID=#{minio_commit}", "-o", buildpath/"minio"
       end
     end
@@ -37,7 +36,7 @@ class Minio < Formula
   end
 
   test do
-    minio_version = DateTime.parse(version).strftime("%Y-%m-%dT%H:%M:%SZ")
+    minio_version = DateTime.parse(version).strftime("%Y-%m-%dT%H-%M-%SZ")
     assert_match minio_version, shell_output("#{bin}/minio version", "version doesn't match")
   end
 end
