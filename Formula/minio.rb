af4e14451bdb4f640b90434c8ce68a25f63f1847
Remco <remco@dutchcoders.io>
2016-08-08 11:48:47 +0200

    added bottle block

--- Formula/minio.rb
+++ Formula/minio.rb
@@ -6,6 +6,13 @@ class Minio < Formula
   sha256 "40cd2faf5dd9503c20d2f5a5ad67277e960561fca5869a9d724d1efa763021df"
   head "https://github.com/minio/minio.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "29d5d319618c235dc75b86b9849b1b8f9d3ccc8c5b313c28e73167119ae43c4e" => :el_capitan
+    sha256 "431a20fe6725e32f3a576937815b28e7af8fecfa1faab29e3c1d24d97edb0941" => :yosemite
+    sha256 "c79ee7ccfea2b00db2b8792a479bac3142ce9567c7c69eade8a435b99600a77b" => :mavericks
+  end
+
   depends_on "go" => :build
 
   def install
@@ -22,7 +29,6 @@ class Minio < Formula
       if build.head?
         system "go", "build", "-o", buildpath/"minio"
       else
-
         system "go", "build", "-ldflags", "-X main.minioVersion=#{minio_version} -X main.minioReleaseTag=#{minio_release} -X main.minioCommitID=#{minio_commit}", "-o", buildpath/"minio"
       end
     end
