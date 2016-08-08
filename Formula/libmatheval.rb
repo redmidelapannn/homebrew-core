da042930e6ba7a57d64274a607cc3b760d4c7fdf
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-08 14:03:07 +0100

    libmatheval: add 1.1.11 bottle.

--- Formula/libmatheval.rb
+++ Formula/libmatheval.rb
@@ -4,6 +4,13 @@ class Libmatheval < Formula
   url "https://ftpmirror.gnu.org/libmatheval/libmatheval-1.1.11.tar.gz"
   sha256 "474852d6715ddc3b6969e28de5e1a5fbaff9e8ece6aebb9dc1cc63e9e88e89ab"
 
+  bottle do
+    cellar :any
+    sha256 "3d407be94a7a9232883fbcb8400ab84b3b2f600f4ab5d23ca1d05c0e35a6b3ed" => :el_capitan
+    sha256 "0162d59fc86ed77bdd9f9b73dfc4f7d2b8326511564a1b2946f4cdc805d7f62f" => :yosemite
+    sha256 "6c4a81b46f853ada1386ea493f0b7fb99df9b81cba04fd49f5c3f72a6c222ef2" => :mavericks
+  end
+
   depends_on "bdw-gc" => :linked
   depends_on "bison" => :build
   depends_on "guile"
