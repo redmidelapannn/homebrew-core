8c789a50d9195c8bb498659e91a1ae138376a99e
Remco <remco@dutchcoders.io>
2016-08-08 11:01:36 +0200

    using old versioning

--- Formula/minio.rb
+++ Formula/minio.rb
@@ -2,9 +2,8 @@ class Minio < Formula
   desc "object storage server compatible with Amazon S3"
   homepage "https://github.com/minio/minio"
   url "https://github.com/minio/minio/archive/RELEASE.2016-07-13T21-46-05Z.tar.gz"
-  version "2016-07-13T21:46:05Z"
+  version "20160713214605"
   sha256 "40cd2faf5dd9503c20d2f5a5ad67277e960561fca5869a9d724d1efa763021df"
-
   head "https://github.com/minio/minio.git"
 
   depends_on "go" => :build
@@ -15,14 +14,16 @@ class Minio < Formula
     clipath = buildpath/"src/github.com/minio/minio"
     clipath.install Dir["*"]
 
+    minio_version = DateTime.parse(version).strftime("%Y-%m-%dT%H:%M:%SZ")
+    minio_release = "RELEASE.#{minio_version}"
+    minio_commit = "3f27734c22212f224037a223439a425e6d2b653a"
+
     cd clipath do
       if build.head?
         system "go", "build", "-o", buildpath/"minio"
       else
-        release = "RELEASE.2016-07-13T21-46-05Z"
-        commit = "3f27734c22212f224037a223439a425e6d2b653a"
 
-        system "go", "build", "-ldflags", "-X main.minioVersion=#{version} -X main.minioReleaseTag=#{release} -X main.minioCommitID=#{commit}", "-o", buildpath/"minio"
+        system "go", "build", "-ldflags", "-X main.minioVersion=#{minio_version} -X main.minioReleaseTag=#{minio_release} -X main.minioCommitID=#{minio_commit}", "-o", buildpath/"minio"
       end
     end
 
@@ -30,6 +31,7 @@ class Minio < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/minio version")
+    minio_version = DateTime.parse(version).strftime("%Y-%m-%dT%H:%M:%SZ")
+    assert_match minio_version, shell_output("#{bin}/minio version", "version doesn't match")
   end
 end
