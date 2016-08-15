c2992a0345c9328dde9c7d897497de437ea63ce4
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-15 20:55:34 +0100

    hg-fast-export: add 20140308 bottle.

--- Formula/hg-fast-export.rb
+++ Formula/hg-fast-export.rb
@@ -7,6 +7,13 @@ class HgFastExport < Formula
 
   head "git://repo.or.cz/fast-export.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "3fc2561935304c2097dbc97a31e60bbeef45ed34de3c55e47499f0017e1c7e32" => :el_capitan
+    sha256 "1a539b86ba5d596c81433c423aac866eeb4c4eadbdd1dc5e067c4c441076b23d" => :yosemite
+    sha256 "c56a75c38a878c6ab1cd18ce177a9b7f9b4fee42a866cca995e576e9a1fd347b" => :mavericks
+  end
+
   def install
     bin.install "hg-fast-export.py", "hg-fast-export.sh",
                 "hg-reset.py", "hg-reset.sh",
