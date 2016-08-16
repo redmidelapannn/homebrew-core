9b2f775eeddf649866d6c824e98891dfe0a8ccce
Thorsten Sommer <service@sommer-engineering.com>
2016-08-16 12:08:49 +0200

    Dscanner 0.3.0 (new formula)

--- Formula/dscanner.rb
+++ Formula/dscanner.rb
@@ -1,5 +1,5 @@
 class Dscanner < Formula
-  desc "A tool to analyse e.g. the style and syntax of D code."
+  desc "Analyses e.g. the style and syntax of D code."
   homepage "https://github.com/Hackerpilot/Dscanner"
   url "https://github.com/Hackerpilot/Dscanner/archive/v0.3.0.tar.gz"
   sha256 "2b5578ca98ad6805a1f1494dfbf90c8f491da20bfe5103ca0c8ac73d781558fe"
