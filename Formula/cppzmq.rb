ecd24f6c6883cce4ddf29dd04e9f224a5b9a2bfe
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-19 22:43:32 +0100

    cppzmq: add 757 bottle.

--- Formula/cppzmq.rb
+++ Formula/cppzmq.rb
@@ -3,6 +3,13 @@ class Cppzmq < Formula
   homepage "https://github.com/zeromq/cppzmq"
   url "https://github.com/zeromq/cppzmq/archive/92d2af6def80a01b76d5e73f073c439ad00ab757.tar.gz"
   sha256 "fa1c8d33367919edff6ea8d3de5a29e3227532092c77976806bfe3d89c3f83ec"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "614f0b7d1a470c4ec0485c4ac3948b45889ab8a45de46e0ff2cb471f51ed8e46" => :el_capitan
+    sha256 "b9bceed00911f4b83bc57d0bb6bdfe7e4f53763bd6496dbd07e7c7f8020650b2" => :yosemite
+    sha256 "b9bceed00911f4b83bc57d0bb6bdfe7e4f53763bd6496dbd07e7c7f8020650b2" => :mavericks
+  end
+
   depends_on "zeromq"
 
   def install
