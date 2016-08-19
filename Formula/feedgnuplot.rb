40cb48ec78905901b0e21136bf05293ae52d7a86
Alyssa Ross <hi@alyssa.is>
2016-08-19 20:49:46 +0100

    feedgnuplot: use full path in test

--- Formula/feedgnuplot.rb
+++ Formula/feedgnuplot.rb
@@ -13,6 +13,6 @@ class Feedgnuplot < Formula
   end
 
   test do
-    pipe_output "feedgnuplot --terminal 'dumb 80,20' --exit", "seq 5", 0
+    pipe_output "#{bin}/feedgnuplot --terminal 'dumb 80,20' --exit", "seq 5", 0
   end
 end
