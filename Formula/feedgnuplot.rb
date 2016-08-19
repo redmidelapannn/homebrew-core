59529ad0a8890b8cbf8751c2938132f41f1c538e
Alyssa Ross <hi@alyssa.is>
2016-08-19 12:47:50 +0100

    feedgnuplot: depends on gnuplot

--- Formula/feedgnuplot.rb
+++ Formula/feedgnuplot.rb
@@ -4,6 +4,8 @@ class Feedgnuplot < Formula
   url "https://github.com/dkogan/feedgnuplot/archive/v1.38.tar.gz"
   sha256 "8470129f4de9a663702035f15f098766268044bd0052f2a4de5d662990c72d03"
 
+  depends_on "gnuplot"
+
   def install
     system "perl", "Makefile.PL", "prefix=#{prefix}"
     system "make"
