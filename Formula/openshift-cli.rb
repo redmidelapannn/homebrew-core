f100857af10f74a88c2a54f8c3d0fc071c900a54
Donovan Muller <donovan@switchbit.io>
2016-08-21 16:55:34 +0200

    openshift-cli: add socat required dependency to devel block

--- Formula/openshift-cli.rb
+++ Formula/openshift-cli.rb
@@ -16,6 +16,8 @@ class OpenshiftCli < Formula
   end
 
   devel do
+    depends_on "socat"
+
     url "https://github.com/openshift/origin.git",
       :tag => "v1.3.0-alpha.3",
       :revision => "7998ae49782d89d17c78104d07a98d2aea704ae3"
