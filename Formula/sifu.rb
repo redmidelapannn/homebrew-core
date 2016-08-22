8d6fa702283467a82fb585bb22100dde3c5ad588
Dusko Vesin <vesindusko@gmail.com>
2016-08-22 10:09:34 +0200

    Sifu 0.1.1 formula.

---
+++ Formula/sifu.rb
@@ -0,0 +1,24 @@
+class Sifu < Formula
+  desc "An intuitive rapid software development tool made for creating cloud solutions."
+  homepage "https://codesifu.com"
+  url "https://codesifu.com/download/sifu-0.1.1.tar.gz"
+  sha256 "d0ef00a475766bf4717b4721044bdbd0f1c8c02be3a1f824738d306b08a06cd2"
+
+  depends_on :java
+
+  def install
+    rm_f Dir["*.bat"]
+    libexec.install Dir["*"]
+
+    (bin/"sifu").write <<-EOS.undent
+      #!/bin/sh
+      exec "#{libexec}/sifu" "$@"
+    EOS
+
+  end
+
+  test do
+    system "sifu --help"
+  end
+
+end
