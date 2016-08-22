469acce31fd16f04049f2b0dfb5ac3fe9ecebc06
Dusko Vesin <vesindusko@gmail.com>
2016-08-22 10:19:30 +0200

    updating formula

--- Formula/sifu.rb
+++ Formula/sifu.rb
@@ -1,5 +1,5 @@
 class Sifu < Formula
-  desc "An intuitive rapid software development tool made for creating cloud solutions."
+  desc "Rapid software development tool made for creating cloud solutions."
   homepage "https://codesifu.com"
   url "https://codesifu.com/download/sifu-0.1.1.tar.gz"
   sha256 "d0ef00a475766bf4717b4721044bdbd0f1c8c02be3a1f824738d306b08a06cd2"
@@ -14,11 +14,9 @@ class Sifu < Formula
       #!/bin/sh
       exec "#{libexec}/sifu" "$@"
     EOS
-
   end
 
   test do
-    system "sifu --help"
+    system "sifu", "--help"
   end
-
 end
