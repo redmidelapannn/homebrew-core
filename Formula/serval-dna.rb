54fb930b0f2c947e76a261f6229189fbce042d68
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-24 19:06:12 +0100

    serval-dna: add 0.93 bottle.

--- Formula/serval-dna.rb
+++ Formula/serval-dna.rb
@@ -6,6 +6,13 @@ class ServalDna < Formula
   sha256 "314b50bf0f45c6a4ff118a3f8b0d305e15c234bdb773bf505152689e9428e0b3"
   head "https://github.com/servalproject/serval-dna.git", :branch => "development"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "312602c4bdea9b55efb83715164ac36be66ee1cee25b94c1e5a94e4f279cc0e1" => :el_capitan
+    sha256 "b8370043eb60ff8844e035394bcd5f546873bdbab1050a3a4e75c368575478ea" => :yosemite
+    sha256 "125f1674c226a78af47018da53846cf31e40779e87c89c7f291402f0049e95e6" => :mavericks
+  end
+
   depends_on "automake" => :build
   depends_on "autoconf" => :build
 
