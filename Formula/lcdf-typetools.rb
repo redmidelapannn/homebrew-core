821221048ec5fa59c4adcf0d478bfbcbd3363df2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-08-30 19:31:32 +0100

    lcdf-typetools: update 2.104 bottle.

--- Formula/lcdf-typetools.rb
+++ Formula/lcdf-typetools.rb
@@ -5,9 +5,9 @@ class LcdfTypetools < Formula
   sha256 "d7985458ead0850cb9549ff1d619ffc18da5d7be892be5e1fce6048d510f0fff"
 
   bottle do
-    sha256 "5195d35913f9c1bf338ce1176e7a01c3e0dec967c68f69691ae4bcf201889d6b" => :el_capitan
-    sha256 "ebfc8ad02b619eea5860a58a96b2255437c6b769f22ffb22354da0d7aa782967" => :yosemite
-    sha256 "07c5729e83fcfd149cacac02fd432f500e87cee4a645e15dd53677a3798eeeef" => :mavericks
+    rebuild 1
+    sha256 "54303680a4c373b138cbab2ea8bcc9532b7273612b5b80123b51bb5afbdb34e2" => :el_capitan
+    sha256 "d44d736145de0d1730070a9401e4138f0be3ad50a4a70241fde9410f7683aa5e" => :mavericks
   end
 
   def install
