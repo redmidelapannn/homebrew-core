ba0f483e55902415064998622a56e92348784196
ilovezfs <ilovezfs@icloud.com>
2016-09-06 02:00:31 -0700

    tasksh 1.1.0
    also fix a regression that caused the test block to hang

--- Formula/tasksh.rb
+++ Formula/tasksh.rb
@@ -1,8 +1,8 @@
 class Tasksh < Formula
   desc "Shell wrapper for Taskwarrior commands"
   homepage "https://tasktools.org/projects/tasksh.html"
-  url "https://taskwarrior.org/download/tasksh-1.0.0.tar.gz"
-  sha256 "9accc81f5ae3a985e33be728d56aba0401889d21f856cd94734d73c221bf8652"
+  url "https://taskwarrior.org/download/tasksh-1.1.0.tar.gz"
+  sha256 "eef7c6677d6291b1c0e13595e8c9606d7f8dc1060d197a0d088cc1fddcb70024"
   head "https://git.tasktools.org/scm/ex/tasksh.git"
 
   bottle do
@@ -15,6 +15,14 @@ class Tasksh < Formula
 
   depends_on "cmake" => :build
   depends_on "task" => :recommended
+
+  # Hangs trying to read task from stdin
+  # Reported 6 Sep 2016 https://bug.tasktools.org/browse/TI-44?filter=-4
+  # Also reported to support at taskwarrior.org
+  patch do
+    url "https://raw.githubusercontent.com/ilovezfs/formula-patches/de81b74/tasksh/tasksh-1.1.0-fix-hang.patch"
+    sha256 "02011f7cf6d2e74ba716a8e87d53a5e26986d75f4a39c69eb406c3c85b551e8b"
+  end
 
   def install
     system "cmake", ".", *std_cmake_args
