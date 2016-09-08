25f0cce046c76eb987afc98f5ad7dcba04253df4
reorx <novoreorx@gmail.com>
2016-09-08 20:31:23 +0800

    fix indent and version redundant

--- Formula/httpstat.rb
+++ Formula/httpstat.rb
@@ -2,14 +2,13 @@ class Httpstat < Formula
   desc "curl statistics made simple"
   homepage "https://github.com/reorx/httpstat"
   url "https://github.com/reorx/httpstat/archive/v1.1.2.tar.gz"
-  version "1.1.2"
   sha256 "0468abebfbae9ff7ea35d577b60ff9af9db3ff67ffd81b11ac005c4e9602919b"
 
   bottle :unneeded
 
   def install
-      bin.install "httpstat.py"
-      mv bin/"httpstat.py", bin/"httpstat"
+    bin.install "httpstat.py"
+    mv bin/"httpstat.py", bin/"httpstat"
   end
 
   test do
