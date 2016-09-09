256987fe3418b6f300ef33428846e57e36edd5d6
reorx <novoreorx@gmail.com>
2016-09-09 15:42:00 +0800

    update formula by suggestions

--- Formula/httpstat.rb
+++ Formula/httpstat.rb
@@ -1,17 +1,16 @@
 class Httpstat < Formula
-  desc "curl statistics made simple"
+  desc "Curl statistics made simple"
   homepage "https://github.com/reorx/httpstat"
   url "https://github.com/reorx/httpstat/archive/v1.1.2.tar.gz"
   sha256 "0468abebfbae9ff7ea35d577b60ff9af9db3ff67ffd81b11ac005c4e9602919b"
 
-  bottle :unneeded
+  depends_on :python if MacOS.version <= :snow_leopard
 
   def install
-    bin.install "httpstat.py"
-    mv bin/"httpstat.py", bin/"httpstat"
+    bin.install "httpstat.py" => "httpstat"
   end
 
   test do
-    system "#{bin}/httpstat", "https://github.com"
+    assert_match "HTTP/1.1", shell_output("#{bin}/httpstat https://github.com")
   end
 end
