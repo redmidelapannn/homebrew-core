4058ee53640da5d5f84922b820936afae6f0fa12
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-11 20:32:46 +0100

    qemu: update 2.7.0 bottle.

--- Formula/qemu.rb
+++ Formula/qemu.rb
@@ -7,9 +7,10 @@ class Qemu < Formula
   head "git://git.qemu-project.org/qemu.git"
 
   bottle do
-    sha256 "ee10bda4f0f8fd9e3fda7747e239b3a736bb409b683392b61936a6fd7484874d" => :el_capitan
-    sha256 "2cba8f0c87c9e18fe4dcf1f7cce3ab6b386e94fbe4c966081f5353689b667ccb" => :yosemite
-    sha256 "f58b50c5106fa15e8792410b14e0c7f675358cde30c2340fc2da86b64bb3e921" => :mavericks
+    rebuild 1
+    sha256 "553d11b4b513cf57102356d19d13bcfbcc31103dba3c9427b2907824bf910fdc" => :el_capitan
+    sha256 "1eba91088db148d5366e003b32a0b0a1118aa17c9f33e348131df9595b1b1d94" => :yosemite
+    sha256 "08ed07cf252b8ddd0b38be9c39278baa39f331adbc40c8277f7549eabbfc1179" => :mavericks
   end
 
   depends_on "pkg-config" => :build
