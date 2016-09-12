21b25ebd2f86b9f9e376fd50e351f9b2e1c80ae8
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-12 14:29:03 +0100

    x264: update r2668 bottle.

--- Formula/x264.rb
+++ Formula/x264.rb
@@ -9,9 +9,10 @@ class X264 < Formula
 
   bottle do
     cellar :any
-    sha256 "8dd89dfa62eb8837f1a49450e7f6069502f49426ec4ea79357e980f534ea3e7a" => :el_capitan
-    sha256 "edc6732b61f996968cc07c591cb17a5b4358a0db400c69e881c0559f6cf41ef8" => :yosemite
-    sha256 "a64a08088aef050f6fc27f97a5bb411c9d75d94c0b9c1f0c8dfd99cdb7f1f4c9" => :mavericks
+    rebuild 1
+    sha256 "3dd85ad15c35a29f8a68750d79243986d33652a59b8a0b8a633ef8553caf1f42" => :el_capitan
+    sha256 "d573aa3cd0aff21ded1c1d4b4ced2acda21daaa3154d89cc5d071ef87d71c1ff" => :yosemite
+    sha256 "97751878b2b24b8d1debfd521488093ee0320cf6231a4516c83d3d08040f34f7" => :mavericks
   end
 
   devel do
