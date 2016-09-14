e05d78516cfd0b4a061f48c7ddcb98e2d9097bc9
Sean T Allen <sean@monkeysnatchbanana.com>
2016-09-14 17:27:16 -0400

    Update ponyc to version 0.3.1

--- Formula/ponyc.rb
+++ Formula/ponyc.rb
@@ -1,8 +1,8 @@
 class Ponyc < Formula
   desc "Object-oriented, actor-model, capabilities-secure programming language"
   homepage "http://www.ponylang.org"
-  url "https://github.com/ponylang/ponyc/archive/0.3.0.tar.gz"
-  sha256 "4b0fa26dd194663f648f8782af79e420bc962281f280233cadecbd9b6cad195f"
+  url "https://github.com/ponylang/ponyc/archive/0.3.1.tar.gz"
+  sha256 "1af638d69571776366cfcd4d0af4aa9913a4d83084b8949769290fa144a06a8c"
 
   bottle do
     cellar :any
