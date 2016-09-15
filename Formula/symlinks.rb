c6f0bf164f6a01c4dffb307d1cb857f18eea99e3
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-15 19:42:19 +0100

    symlinks: add 1.4.3 bottle.

--- Formula/symlinks.rb
+++ Formula/symlinks.rb
@@ -4,6 +4,12 @@ class Symlinks < Formula
   url "https://github.com/brandt/symlinks/archive/v1.4.3.tar.gz"
   sha256 "27105b2898f28fd53d52cb6fa77da1c1f3b38e6a0fc2a66bf8a25cd546cb30b2"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4081daa80041952e0683e0699daddf47bb677a95b9263f6aca567e8710f5c432" => :yosemite
+    sha256 "54cb05b5384e89fe135c5f78a68f4453dc17db1ea636ac96f367165c99c409c7" => :mavericks
+  end
+
   def install
     system "make", "install"
     bin.install "symlinks"
