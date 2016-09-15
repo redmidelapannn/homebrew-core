a6119e61dfc6838dac09b9b866e130cd48b7598e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-15 14:03:34 +0100

    gdb: update 7.11.1 bottle.

--- Formula/gdb.rb
+++ Formula/gdb.rb
@@ -19,10 +19,10 @@ class Gdb < Formula
   sha256 "e9216da4e3755e9f414c1aa0026b626251dfc57ffe572a266e98da4f6988fc70"
 
   bottle do
-    sha256 "ce9602dbc92b1e1d986a1a5536fcef9baf3f703d08a86c4d18e4b411b20b2a4c" => :sierra
-    sha256 "90b608379fefd418b72e6b73ae1bde9014d94b9f366259cbc3fea99dc63985b1" => :el_capitan
-    sha256 "588dcb9acd832060e189004a4c7fef14b7a3bdeda3a7780b1f1bb8106c810327" => :yosemite
-    sha256 "07db094029ff33ec19e0b90633f9a2b8fcceaec14d2bf30f7824b618ce993a3e" => :mavericks
+    rebuild 1
+    sha256 "c56f5730c017f04801199bf788bf5c1a76b18ed2950ff0b83dd9a4c9a227461c" => :el_capitan
+    sha256 "b2413ef5eb893a3a7b8660ae5344566bbc40e0fac50db0ff07d92e3248236cb7" => :yosemite
+    sha256 "019b103689134eda61230e12646efa1044b697ce5e829e551c05837136af32b0" => :mavericks
   end
 
   deprecated_option "with-brewed-python" => "with-python"
