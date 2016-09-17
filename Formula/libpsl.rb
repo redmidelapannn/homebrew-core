0c1ba2b576c237f76225ed0ae6001b82a101718b
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-17 07:17:27 +0100

    libpsl: add 0.14.0 bottle.

--- Formula/libpsl.rb
+++ Formula/libpsl.rb
@@ -4,6 +4,13 @@ class Libpsl < Formula
   url "https://github.com/rockdaboot/libpsl/releases/download/libpsl-0.14.0/libpsl-0.14.0.tar.gz"
   sha256 "e8c794bcedf45c87dee4810570bc62ba7130217887e34e164e3eadc2259f2efb"
 
+  bottle do
+    cellar :any
+    sha256 "3ff7f7a79d8d8dcaf6dfb054195d3c89ecc1b5b5e843b82210b1d7aae652ab3f" => :el_capitan
+    sha256 "852945b9ca4ae4662d7489e093c171c8159c90b6972f30bbcceb66367a9c64ff" => :yosemite
+    sha256 "6b911c8b1a62f5daf353ffc6f9d85024ae006f44061fe8010669ddfcf4ae1968" => :mavericks
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "md5sha1sum" => :build
