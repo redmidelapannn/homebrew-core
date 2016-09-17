8171c69b68f65a6a91e1e0538fbab2c7766b30b9
Krishnaswamy Subramanian <jskswamy@gmail.com>
2016-09-17 22:40:44 +0100

    Fix styling issues and more test to assert the functionality

--- Formula/kube-aws.rb
+++ Formula/kube-aws.rb
@@ -1,3 +1,5 @@
+require "yaml"
+
 class KubeAws < Formula
   desc "Kubernetes on AWS"
   homepage "https://github.com/coreos/coreos-kubernetes/tree/master/multi-node/aws"
@@ -8,16 +10,15 @@ class KubeAws < Formula
   depends_on "go" => :build
 
   def install
-    executable="bin/kube-aws"
+    executable = "bin/kube-aws"
     ENV["GOPATH_VENDOR"]="#{Dir.pwd}/_gopath-vendor"
     ENV["GOPATH_KUBE_AWS"]="#{Dir.pwd}/_gopath-kube-aws"
     ENV["KUBE_AWS_DIR"]="#{ENV["GOPATH_KUBE_AWS"]}/src/github.com/coreos/coreos-kubernetes/multi-node/aws"
-    ENV["OUTPUT_PATH"]=executable.to_s
     ENV["GOPATH"]="#{ENV["GOPATH_VENDOR"]}:#{ENV["GOPATH_KUBE_AWS"]}"
 
     Dir.chdir "#{Dir.pwd}/multi-node/aws/"
 
-    rm_rf %w(ENV['GOPATH_VENDOR'] ENV['GOPATH_KUBE_AWS'])
+    rm_rf [ENV["GOPATH_VENDOR"], ENV["GOPATH_KUBE_AWS"]]
 
     mkdir_p ENV["GOPATH_VENDOR"]
     mkdir_p File.dirname(ENV["KUBE_AWS_DIR"])
@@ -26,13 +27,18 @@ class KubeAws < Formula
     ln Dir.pwd.to_s, (ENV["KUBE_AWS_DIR"]).to_s
 
     system "go", "generate", "./pkg/config"
-    system "go build -ldflags '-X github.com/coreos/coreos-kubernetes/multi-node/aws/pkg/cluster.VERSION=#{version}' -a -tags netgo -installsuffix netgo -o '#{ENV["OUTPUT_PATH"]}' ./cmd/kube-aws"
+    system "go", "build", "-ldflags", "-X github.com/coreos/coreos-kubernetes/multi-node/aws/pkg/cluster.VERSION=#{version}", "-a", "-tags", "netgo", "-installsuffix", "netgo", "-o", executable.to_s, "./cmd/kube-aws"
 
     bin.install "#{Dir.pwd}/#{executable}" => "kube-aws"
   end
 
   test do
-    output = shell_output("#{bin}/kube-aws version 2>&1")
-    assert_match "kube-aws version #{version}", output
+    cluster = { "clusterName" => "test-cluster", "externalDNSName" => "dns", "keyName" => "key", "region" => "west", "availabilityZone" => "zone", "kmsKeyArn" => "arn" }
+    system "kube-aws", "init", "--cluster-name", "test-cluster", "--external-dns-name", "dns", "--region", "west", "--availability-zone", "zone", "--key-name", "key", "--kms-key-arn", "arn"
+    installed_version = shell_output("#{bin}/kube-aws version 2>&1")
+    cluster_info = YAML.load(File.read(File.join(Dir.pwd, "cluster.yaml")))
+
+    assert_equal cluster, cluster_info
+    assert_match "kube-aws version #{version}", installed_version
   end
 end
