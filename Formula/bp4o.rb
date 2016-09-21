c3f52f8ebfe03b3e04ed7966d096952257fe83a3
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-22 00:35:43 +0100

    bp4o: add 0.2 bottle.

--- Formula/bp4o.rb
+++ Formula/bp4o.rb
@@ -4,6 +4,13 @@ class Bp4o < Formula
   url "https://github.com/zachwhaley/bp4o/archive/v0.2.tar.gz"
   sha256 "610176c134432da43158a0c0ce2ae5831610624af32ef3106d27d87418cc0775"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1fb8827e9d3a203391a2b99c771b1d4ea01a6bdd2e4ecf0f31bd23a372c1940d" => :sierra
+    sha256 "1fb8827e9d3a203391a2b99c771b1d4ea01a6bdd2e4ecf0f31bd23a372c1940d" => :el_capitan
+    sha256 "1fb8827e9d3a203391a2b99c771b1d4ea01a6bdd2e4ecf0f31bd23a372c1940d" => :yosemite
+  end
+
   def install
     bin.install Dir["bin/*"]
     pkgshare.install Dir["./init.*"]
