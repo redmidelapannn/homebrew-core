ca2a2a4985ada34ffb569e2e3360434b3ba40c29
Marc Savy <marc@rhymewithgravy.com>
2016-09-24 21:53:29 +0100

    Utilise new functionality from Homebrew/brew#1043
    Bump version number

--- Formula/gofabric8.rb
+++ Formula/gofabric8.rb
@@ -1,19 +1,18 @@
 class Gofabric8 < Formula
   desc "CLI for fabric8 running on Kubernetes or OpenShift"
   homepage "https://github.com/fabric8io/gofabric8/"
-  url "https://github.com/fabric8io/gofabric8/archive/v0.4.69.tar.gz"
-  sha256 "21a34018367d8aeb9446be23747ff918770d0c8e827e7d3e0b7a57542eb61274"
+  url "https://github.com/fabric8io/gofabric8/archive/v0.4.71.tar.gz"
+  sha256 "4eacee14e73056933dab263918cc64e45c57a2c7c7575fd23dada2a6ff3c5fe1"
 
   depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = buildpath
     files = Dir["*"]
-    mkdir_p buildpath/"src/github.com/fabric8io/gofabric8"
-    mv files, buildpath/"src/github.com/fabric8io/gofabric8"
-    cd "src/github.com/fabric8io/gofabric8"
-    system "make", "install", "REV=homebrew"
-    cd buildpath
+    mkdir buildpath/"src/github.com/fabric8io/gofabric8" do
+      files.each { |f| mv buildpath/f, "." }
+      system "make", "install", "REV=homebrew"
+    end
     bin.install "bin/gofabric8"
   end
 
