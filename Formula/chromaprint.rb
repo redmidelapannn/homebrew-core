c078950ab47af1465d81d751b1f185aebea325f2
Alex Dunn <dunn.alex@gmail.com>
2016-09-24 10:07:13 -0700

    chromaprint 1.3.2

--- Formula/chromaprint.rb
+++ Formula/chromaprint.rb
@@ -1,9 +1,9 @@
 class Chromaprint < Formula
   desc "Core component of the AcoustID project (Audio fingerprinting)"
   homepage "https://acoustid.org/chromaprint"
-  url "https://bitbucket.org/acoustid/chromaprint/downloads/chromaprint-1.3.tar.gz"
-  sha256 "3dc3ff97abdce63abc1f52d5f5f8e72c22f9a690dd6625271aa96d3a585b695a"
-  revision 1
+  url "https://bitbucket.org/acoustid/chromaprint/downloads/chromaprint-1.3.2.tar.gz"
+  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/c/chromaprint/chromaprint_1.3.2.orig.tar.gz"
+  sha256 "c3af900d8e7a42afd74315b51b79ebd2e43bc66630b4ba585a54bf3160439652"
 
   bottle do
     cellar :any
