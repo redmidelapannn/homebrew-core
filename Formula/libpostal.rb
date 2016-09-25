f16e1617877582dd725e9ad150a244f8d397e682
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-25 08:09:51 +0100

    libpostal: add 0.3.1 bottle.

--- Formula/libpostal.rb
+++ Formula/libpostal.rb
@@ -4,6 +4,12 @@ class Libpostal < Formula
   url "https://github.com/openvenues/libpostal/archive/v0.3.1.tar.gz"
   head "https://github.com/openvenues/libpostal.git"
 
+  bottle do
+    sha256 "983710ec67ae58b5487eb4bce16a50bc2d4b8bd9391b03606397203b6b90ead0" => :sierra
+    sha256 "547e6396777c51a2332216ac34651475d023213e1891d0031b140cce555237fe" => :el_capitan
+    sha256 "095268531ff45556356c7b8e7317ba66275bbff06b23dbc1f3ba6edc145b1663" => :yosemite
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
