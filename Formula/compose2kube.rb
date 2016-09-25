c3cb85a95870cc28b884fe3fdae0043e8f3c2602
blackstar257 <blackstar257@gmail.com>
2016-09-25 01:18:01 -0700

    update

--- Formula/compose2kube.rb
+++ Formula/compose2kube.rb
@@ -2,7 +2,6 @@ class Compose2kube < Formula
   desc "Convert docker-compose service files to Kubernetes objects."
   homepage "https://github.com/kelseyhightower/compose2kube"
   url "https://github.com/kelseyhightower/compose2kube/archive/0.0.2.tar.gz"
-  version "0.0.2"
   sha256 "d09b86994949f883c5aa4d041a12f6c5a8989f3755a2fb49a2abac2ad5380c30"
   head "https://github.com/kelseyhightower/compose2kube.git"
 
@@ -12,7 +11,7 @@ class Compose2kube < Formula
     ENV["GOPATH"] = buildpath
     (buildpath/"src/github.com/kelseyhightower/compose2kube").install buildpath.children
     cd "src/github.com/kelseyhightower/compose2kube" do
-      system "go", "build", "-o", bin/"compose2kube"
+      system "go", "build", "-o", bin/  "compose2kube"
       prefix.install_metafiles
     end
   end
