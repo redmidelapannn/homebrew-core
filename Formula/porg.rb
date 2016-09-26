dc35dc4f41b6300a0e323e99d4ca040a7c6fccd3
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2016-09-26 15:41:30 +0900

    porg 0.10 (new formula)

---
+++ Formula/porg.rb
@@ -0,0 +1,26 @@
+class Porg < Formula
+  desc "Source Code Package Organizer"
+  homepage "http://porg.sourceforge.net/"
+  url "https://downloads.sourceforge.net/project/porg/porg-0.10.tar.gz"
+  sha256 "48f8433193f92097824ed7a72c4babafb29dc2ffe60e7df3635664f59f09cedd"
+
+  option "without-grop", "Don't install Grop, which is the graphic interface of porg."
+
+  depends_on "pkg-config" => :build
+  depends_on "gtkmm3" if build.with? "grop"
+
+  def install
+    args = ["--disable-silent-rules", "--prefix=#{prefix}"]
+
+    if build.without? "grop"
+      args << "--disable-grop"
+    end
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    system "porg", "--version"
+  end
+end
