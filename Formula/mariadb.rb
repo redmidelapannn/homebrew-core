fbf2fe10e1734bca74dfcfe77ce6736735c69628
Dan <dan@wadoku.de>
2016-09-27 17:28:24 +0200

    mariadb 10.2.2 (devel)

--- Formula/mariadb.rb
+++ Formula/mariadb.rb
@@ -12,12 +12,8 @@ class Mariadb < Formula
   end
 
   devel do
-    url "http://ftp.osuosl.org/pub/mariadb/mariadb-10.2.1/source/mariadb-10.2.1.tar.gz"
-    sha256 "90b7a17f3372c92c12dff084b37fcca8c4cf8106f4dcabd35fadc8efbaa348a2"
-
-    # upstream fix for compilation error
-    # https://jira.mariadb.org/browse/MDEV-10322
-    patch :DATA
+    url "http://ftp.osuosl.org/pub/mariadb/mariadb-10.2.2/source/mariadb-10.2.2.tar.gz"
+    sha256 "55cf9e357ae4511fc1330e7cb8a15746d99b3a7875b6da9bcf1acfb1aa6f893a"
   end
 
   option :universal
@@ -207,17 +203,3 @@ class Mariadb < Formula
     end
   end
 end
-__END__
-diff --git a/storage/connect/jdbconn.cpp b/storage/connect/jdbconn.cpp
-index 9b47927..7c0582d 100644
---- a/storage/connect/jdbconn.cpp
-+++ b/storage/connect/jdbconn.cpp
-@@ -270,7 +270,7 @@ PQRYRES JDBCColumns(PGLOBAL g, char *db, char *table, char *colpat,
- 		return NULL;
-
- 	// Colpat cannot be null or empty for some drivers
--	cap->Pat = (colpat && *colpat) ? colpat : "%";
-+	cap->Pat = (colpat && *colpat) ? colpat : PlugDup(g, "%");
-
- 	/************************************************************************/
- 	/*  Now get the results into blocks.                                    */
