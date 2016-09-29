27f8793a1bac4bfed2d6eb219a92ace0d199f4cf
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 21:28:13 +0100

    armor: add 0.1.0 bottle.

--- Formula/armor.rb
+++ Formula/armor.rb
@@ -5,6 +5,12 @@ class Armor < Formula
   sha256 "fc06d7fa1b6346dc3ab150d3eea5d85f35108cbd6b97c97ccff3e1732eb56209"
   head "https://github.com/labstack/armor.git"
 
+  bottle do
+    sha256 "1360b51fd2da40c923278f3a0fdfb041c6e81dae6664618a8ef2f28621d0fee4" => :sierra
+    sha256 "6255da9a8a196e89e5665e65db1885f2fc896eb8fcb2845d2e88cf3e6dcde875" => :el_capitan
+    sha256 "de3205a02eb8cba865df953af4abc1469b317077743ac843132daa67421d38d1" => :yosemite
+  end
+
   depends_on "glide"
 
   def install
