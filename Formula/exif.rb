992f4b1648893b81e6ff715f833ad450b091c7eb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-09-29 21:22:29 +0100

    exif: update 0.6.21 bottle.

--- Formula/exif.rb
+++ Formula/exif.rb
@@ -6,11 +6,10 @@ class Exif < Formula
 
   bottle do
     cellar :any
+    rebuild 1
     sha256 "ae8645e99466b553e7198556a2d331759c9f173956dc0c10f584903030c78876" => :sierra
-    sha256 "607827dc887cae86a2bedc7892e4d061990cb04814fb0ac2fd5d0cd1fdefcb30" => :el_capitan
-    sha256 "ebf412bb43ad3695f01f8ed1e5851e7c02218589fc7cbb175c9a3f1914894e37" => :yosemite
-    sha256 "e24a0bdfee07a46a49620c29726748e73a4eca0937c59c1f5efeeb51ea9f6971" => :mavericks
-    sha256 "0d5997d1d013822aee275d0e65403a1d99b579f53c2ee131b5b121d85377665d" => :mountain_lion
+    sha256 "7b3e7722476afc4a0e62fa5107c15da2652a48089d6deeeb0297d7000bef17e1" => :el_capitan
+    sha256 "9a71fc4b9c85e72ef1b5936fd946ff92251bf97006753cdad00a0c81177952e8" => :yosemite
   end
 
   option "with-gettext", "Build with Native Language Support"
