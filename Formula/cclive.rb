2deaada5f8d1deb55968a3c4c89b2e350999ae67
ilovezfs <ilovezfs@icloud.com>
2016-09-30 04:28:45 -0700

    cclive: revision for boost@1.61

--- Formula/cclive.rb
+++ Formula/cclive.rb
@@ -3,6 +3,7 @@ class Cclive < Formula
   homepage "http://cclive.sourceforge.net/"
   url "https://downloads.sourceforge.net/project/cclive/0.7/cclive-0.7.16.tar.xz"
   sha256 "586a120faddcfa16f5bb058b5c901f1659336c6fc85a0d3f1538882a44ee10e1"
+  revision 1
 
   bottle do
     cellar :any
@@ -16,7 +17,7 @@ class Cclive < Formula
 
   depends_on "pkg-config" => :build
   depends_on "quvi"
-  depends_on "boost"
+  depends_on "boost@1.61"
   depends_on "pcre"
 
   def install
