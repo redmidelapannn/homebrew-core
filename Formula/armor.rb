ef50b06ed163fcdc337c7cc4824774464180832d
Vishal Rana <vr@labstack.com>
2016-09-29 20:11:52 -0700

    armor 0.1.1 (new formula)
    Signed-off-by: Vishal Rana <vr@labstack.com>

---
+++ Formula/armor.rb
@@ -0,0 +1,23 @@
+class Armor < Formula
+  desc "Simple HTTP server, supports HTTP/2 and auto TLS"
+  homepage "https://github.com/labstack/armor"
+  url "https://github.com/labstack/armor/archive/v0.1.1.tar.gz"
+  sha256 "1772c99dd16cb4cc05c27f34382c33c5ff60aa0f4703ad25d7d6127f2d0b745d"
+  head "https://github.com/labstack/armor.git"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    armorpath = buildpath/"src/github.com/labstack/armor"
+    armorpath.install buildpath.children
+
+    cd armorpath do
+      system "go", "build", "-o", bin/"armor", "cmd/armor/main.go"
+    end
+  end
+
+  test do
+    system "armor", "-v"
+  end
+end
