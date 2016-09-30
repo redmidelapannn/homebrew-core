bed1e924959530c969aa16d7258112cf883e0d4d
ilovezfs <ilovezfs@icloud.com>
2016-09-30 04:40:33 -0700

    metaproxy: revision for boost@1.61

--- Formula/metaproxy.rb
+++ Formula/metaproxy.rb
@@ -3,7 +3,7 @@ class Metaproxy < Formula
   homepage "https://www.indexdata.com/metaproxy"
   url "http://ftp.indexdata.dk/pub/metaproxy/metaproxy-1.10.0.tar.gz"
   sha256 "83a282a9aefa71fd073adc2ef1c474e8b594c921da0c2c4b977821bfc3cf5a5e"
-  revision 2
+  revision 3
 
   bottle do
     cellar :any
@@ -15,7 +15,7 @@ class Metaproxy < Formula
 
   depends_on "pkg-config" => :build
   depends_on "yazpp"
-  depends_on "boost"
+  depends_on "boost@1.61"
 
   def install
     system "./configure", "--disable-dependency-tracking",
