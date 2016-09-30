01ea2297b57b41c032f4bfee90f74827e8493abd
ilovezfs <ilovezfs@icloud.com>
2016-09-30 04:46:48 -0700

    pktanon: revision for boost@1.61

--- Formula/pktanon.rb
+++ Formula/pktanon.rb
@@ -3,6 +3,7 @@ class Pktanon < Formula
   homepage "https://www.tm.uka.de/software/pktanon/index.html"
   url "https://www.tm.uka.de/software/pktanon/download/pktanon-1.4.0-dev.tar.gz"
   sha256 "db3f437bcb8ddb40323ddef7a9de25a465c5f6b4cce078202060f661d4b97ba3"
+  revision 1
 
   bottle do
     cellar :any
@@ -13,7 +14,7 @@ class Pktanon < Formula
   end
 
   depends_on "xerces-c"
-  depends_on "boost"
+  depends_on "boost@1.61"
 
   def install
     # fix compile failure caused by undefined function 'sleep'.
