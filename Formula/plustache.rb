2632047184ced8083bdffb3588d53ab1717a3efb
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 01:48:58 +0100

    plustache: update 0.3.0_1 bottle.

--- Formula/plustache.rb
+++ Formula/plustache.rb
@@ -7,10 +7,9 @@ class Plustache < Formula
 
   bottle do
     cellar :any
-    sha256 "54055df2b3b0431ba1903eed2f2abf7de05a368b7b8e7660158ef9a7ba2c9bcd" => :sierra
-    sha256 "45a5e0e42d835e175ff923fc3e2c9fe75a25d5e9c6cc24dd1449622bc0a82d01" => :el_capitan
-    sha256 "7535a016ea3e2e1ab59f452d11feeed4b287591c566a08d0c0c3680f8b2e1239" => :yosemite
-    sha256 "6d6fd22aa6da55eef0eb939aebf4a5ec17c5e3ad101bc5c3132213b573d265d7" => :mavericks
+    sha256 "48ffbd231777c2ff026935d1f304256857669ef7c1a1512e0121d0ecc8b6e31f" => :sierra
+    sha256 "341e0488971c04b23b56c35c7a81e653a1da01be7b095069f01c0f152f97b879" => :el_capitan
+    sha256 "eba6810c73472cd056d9f41bce7c23829e42dbc6046dc5553f3615b38fb0b120" => :yosemite
   end
 
   depends_on "autoconf" => :build
