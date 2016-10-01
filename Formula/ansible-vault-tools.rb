a3a104c0c79f37804ccf1c53d2e826db5ba81b10
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-01 13:16:25 +0100

    ansible-vault-tools: add 2.0.1 bottle.

--- Formula/ansible-vault-tools.rb
+++ Formula/ansible-vault-tools.rb
@@ -4,6 +4,13 @@ class AnsibleVaultTools < Formula
   url "https://github.com/building5/ansible-vault-tools/archive/v2.0.1.tar.gz"
   sha256 "ccbb255c22e2ed8af6ce9c8c3f855a0c0a983ca01784bedce7fa9629e66d2d5e"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b31e61034935825e7111715ca6fb9310905ff5060aa231c8eb4707f5e708fde0" => :sierra
+    sha256 "b31e61034935825e7111715ca6fb9310905ff5060aa231c8eb4707f5e708fde0" => :el_capitan
+    sha256 "b31e61034935825e7111715ca6fb9310905ff5060aa231c8eb4707f5e708fde0" => :yosemite
+  end
+
   depends_on "ansible"
   depends_on "gnupg"
 
