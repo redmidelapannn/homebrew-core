3292c0b0897b8bef10e512bbbab28ecc5e1c32f3
David Gasperoni <mcdado@gmail.com>
2016-10-01 11:49:12 +0200

    Correct the link, use the whole archive

--- Formula/borg.rb
+++ Formula/borg.rb
@@ -1,9 +1,9 @@
 class Borg < Formula
   desc "Terminal based search engine for bash commands"
   homepage "http://ok-b.org"
-  url "https://raw.githubusercontent.com/crufter/borg/v0.0.1/main.go"
+  url "https://github.com/crufter/borg/archive/v0.0.1.zip"
   version "0.0.1"
-  sha256 "6133cd8948b51ab90d0faa4b7f1e548fddacfcb4747be168e8ec5ef3e753fdde"
+  sha256 "78242fc87fac5b03b821972a7271ab2fbaa0e7cb7758c1a00dbb85a2329692d6"
 
   depends_on "go" => :build
 
