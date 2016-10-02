9ad93ee110771e85f5d227533ad79e76b4d665b8
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2016-10-02 23:10:58 +0900

    Change `without-grop` to `without-gtkmm3`

--- Formula/porg.rb
+++ Formula/porg.rb
@@ -4,15 +4,15 @@ class Porg < Formula
   url "https://downloads.sourceforge.net/project/porg/porg-0.10.tar.gz"
   sha256 "48f8433193f92097824ed7a72c4babafb29dc2ffe60e7df3635664f59f09cedd"
 
-  option "without-grop", "Don't install Grop, which is the graphic interface of porg."
+  option "without-gtkmm3", "Don't install Grop, which is the graphic interface of porg."
 
   depends_on "pkg-config" => :build
-  depends_on "gtkmm3" if build.with? "grop"
+  depends_on "gtkmm3" => :recommended
 
   def install
     args = %W[--disable-silent-rules --prefix=#{prefix}]
 
-    args << "--disable-grop" if build.without? "grop"
+    args << "--disable-grop" if build.without? "gtkmm3"
 
     system "./configure", *args
     system "make", "install"
