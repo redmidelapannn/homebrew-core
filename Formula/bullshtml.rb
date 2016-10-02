4a1a9d1c7bb2422d65b4ab30b2ab61afe2969295
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-02 14:47:24 +0100

    bullshtml: add 1.0.6 bottle.

--- Formula/bullshtml.rb
+++ Formula/bullshtml.rb
@@ -5,6 +5,13 @@ class Bullshtml < Formula
       :tag => "1.0.6",
       :revision => "43efec2ff5ea7db9622c679616908aeacf46e7b8"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ac2c8ec76425f27b131aded240fd509d9d81704a2753a38497979fdf04c5b08b" => :sierra
+    sha256 "1bb77aac08a34b4dd356337aa0d7793a2eed4c6b59e9121a03769369043c99d8" => :el_capitan
+    sha256 "67c7939f487037e259a8bee6841a39bf2c9c16cd593ff3194979d13caa37817e" => :yosemite
+  end
+
   depends_on "ant" => :build
   depends_on :java => "1.6+"
 
