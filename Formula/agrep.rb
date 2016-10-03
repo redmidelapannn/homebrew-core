84323eeb2a10fe4e5c63fe7ed4748759fd5ffb7c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-03 17:47:54 +0100

    agrep: add 3.41.5 bottle.

--- Formula/agrep.rb
+++ Formula/agrep.rb
@@ -5,6 +5,13 @@ class Agrep < Formula
   version "3.41.5"
   sha256 "b15ffb7319d8e7ec302b45d6693479a41f97c4a4f66500d30f65388620c4e24c"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5de1af2ee4ea53235444bc5a3a5d67412609250379ff4eed3b1e2eff2b46872e" => :sierra
+    sha256 "bae595e8b0d70dc6273485fa507b4394f136673428ab08e5bfd8c3124a6fea28" => :el_capitan
+    sha256 "ae2f8a9d536eaf6a51ca1cf3634bbf2ea2642588150179fe57a5ce1fe201083d" => :yosemite
+  end
+
   conflicts_with "tre", :because => "tre also ships an agrep binary"
 
   patch do
