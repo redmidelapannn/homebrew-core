d30603e210f752246bfc33974055e49b723be369
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-03 09:13:56 +0100

    gitless: add 0.8.3 bottle.

--- Formula/gitless.rb
+++ Formula/gitless.rb
@@ -5,6 +5,13 @@ class Gitless < Formula
   version "0.8.3"
   sha256 "4b5cdc00da5fe93a12904c9992a332484201b979687ce9cce5160576c7cb2048"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "432867161de1375aef2d72c14aace02267c374b145fad487943bdfcdf78744d5" => :sierra
+    sha256 "432867161de1375aef2d72c14aace02267c374b145fad487943bdfcdf78744d5" => :el_capitan
+    sha256 "432867161de1375aef2d72c14aace02267c374b145fad487943bdfcdf78744d5" => :yosemite
+  end
+
   def install
     # The tarball is already compiled.
     # No need to do anything but move it.
