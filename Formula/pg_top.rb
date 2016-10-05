16ccdd7aa450d9c0a79d46e9114c23aa85b4dd26
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-05 14:37:14 +0100

    pg_top: update 3.7.0_1 bottle.

--- Formula/pg_top.rb
+++ Formula/pg_top.rb
@@ -7,10 +7,9 @@ class PgTop < Formula
 
   bottle do
     cellar :any
-    sha256 "91b171e0653b1e77a416a4d5ab59f28daaaf5b24cdeaf9b9e88cea81b5f24c81" => :sierra
-    sha256 "f6942875416fa688254e82feac6f0991cab3181ca0a545bce53ca4af9dc856f3" => :el_capitan
-    sha256 "2e1a209d75f91eea0f22dc6e9c391c95a65289436d5060df7cc803bec8c3df35" => :yosemite
-    sha256 "6d9c0960d9594b6643126ef60ecdae71b21b8ab9ebdcd6d0a7efc9a1847dab87" => :mavericks
+    sha256 "f95c91f77d8bfe25a3e330c12594f6fbeb15319df54fc1f0e7dc5abe66a26418" => :sierra
+    sha256 "e0f208639cfc0c3fcfd257298bce229d2d2325eb4e3d2dc8200d8bdf3250eb3c" => :el_capitan
+    sha256 "84604c4231d85ead3981a145994990360f4af6e56fbad4c0e3c2d50ad8ad6000" => :yosemite
   end
 
   depends_on :postgresql
