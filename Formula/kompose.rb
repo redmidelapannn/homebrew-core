829955dd078af711237198aef5b20c19cfc0ccbc
blackstar257 <blackstar257@gmail.com>
2016-10-05 09:02:00 -0700

    update kompose

--- Formula/kompose.rb
+++ Formula/kompose.rb
@@ -1,23 +1,17 @@
-class Compose2kube < Formula
-  desc "Convert docker-compose service files to Kubernetes objects."
-  homepage "https://github.com/kelseyhightower/compose2kube"
-  url "https://github.com/kelseyhightower/compose2kube/archive/0.0.2.tar.gz"
-  sha256 "d09b86994949f883c5aa4d041a12f6c5a8989f3755a2fb49a2abac2ad5380c30"
-  head "https://github.com/kelseyhightower/compose2kube.git"
+class Kompose < Formula
+  desc "Tool to move from `docker-compose` to Kubernetes"
+  homepage "https://github.com/skippbox/kompose"
+  url "https://github.com/skippbox/kompose/releases/download/v0.1.0/kompose_darwin-amd64.tar.gz"
+  sha256 "2cd1836fc0d5cd62bb72fb923cba98665c586087967b205b53dbcf2cdea9a9aa"
 
-  depends_on "go" => :build
+  bottle :unneeded
 
   def install
-    ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/kelseyhightower/compose2kube").install buildpath.children
-    cd "src/github.com/kelseyhightower/compose2kube" do
-      system "go", "get"
-      system "go", "build", "-o", bin/"compose2kube"
-      prefix.install_metafiles
-    end
+    bin.install "kompose"
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/compose2kube -h 2>&1", 2)
+    system "#{bin}/kompose", "--version"
   end
 end
+
