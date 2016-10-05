f20076acb0addc8d9529d659cf538917111a09ce
Thierry Moisan <thierry.moisan@jda.com>
2016-10-05 10:42:33 -0400

    Use :apr dependency

--- Formula/ctail.rb
+++ Formula/ctail.rb
@@ -16,8 +16,7 @@ class Ctail < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
-  depends_on "apr"
-  depends_on "apr-util"
+  depends_on :apr
 
   conflicts_with "byobu", :because => "both install `ctail` binaries"
 
