251dfaed3e2ccc8d7649bc73047f53afc2a04942
Jeremy Tan <jtanx@outlook.com>
2016-10-05 18:11:36 +0800

    fontforge 20161004

--- Formula/fontforge.rb
+++ Formula/fontforge.rb
@@ -1,9 +1,8 @@
 class Fontforge < Formula
   desc "Command-line outline and bitmap font editor/converter"
   homepage "https://fontforge.github.io"
-  url "https://github.com/fontforge/fontforge/archive/20161001.tar.gz"
-  sha256 "103af2a6c8799390f20790e44064ea4f9ec6795255b7a065b3f9a352c2723c40"
-  head "https://github.com/fontforge/fontforge.git"
+  url "https://github.com/fontforge/fontforge/releases/download/20161005/fontforge-dist-20161004.tar.gz"
+  sha256 "ccba2d84cf009e2a51656af4e0191c6a95aa1a63e5dfdeb7423969c889a24a64"
 
   bottle do
     sha256 "aeeb0031067149f9795f9aac5bf3b623ab53557345509086de1124cd941b0ad8" => :sierra
@@ -11,33 +10,43 @@ class Fontforge < Formula
     sha256 "e5e8e6a5522841bccf8dc4fbfd4953b35e5375320785474ddd0dc7aa066b1fcb" => :yosemite
   end
 
+  head do
+    url "https://github.com/fontforge/fontforge.git"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+
+    resource "gnulib" do
+      url "git://git.savannah.gnu.org/gnulib.git",
+        :revision => "29ea6d6fe2a699a32edbe29f44fe72e0c253fcee"
+    end
+  end
+
   option "with-giflib", "Build with GIF support"
   option "with-extra-tools", "Build with additional font tools"
 
   deprecated_option "with-gif" => "with-giflib"
 
-  # Autotools are required to build from source in all releases.
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
+  depends_on :x11 => :optional
+  if build.with? "x11"
+    depends_on "cairo" => "with-x11"
+    depends_on "pango" => "with-x11"
+  else
+    depends_on "cairo"
+    depends_on "pango"
+  end
   depends_on "pkg-config" => :build
   depends_on "libtool" => :run
   depends_on "gettext"
-  depends_on "pango"
   depends_on "zeromq"
   depends_on "czmq"
-  depends_on "cairo"
   depends_on "fontconfig"
   depends_on "libpng" => :recommended
   depends_on "jpeg" => :recommended
   depends_on "libtiff" => :recommended
+  depends_on "libspiro" => :recommended
+  depends_on "libuninameslist" => :recommended
   depends_on "giflib" => :optional
-  depends_on "libspiro" => :optional
   depends_on :python if MacOS.version <= :snow_leopard
-
-  resource "gnulib" do
-    url "git://git.savannah.gnu.org/gnulib.git",
-        :revision => "29ea6d6fe2a699a32edbe29f44fe72e0c253fcee"
-  end
 
   fails_with :llvm do
     build 2336
@@ -45,30 +54,21 @@ class Fontforge < Formula
   end
 
   def install
-    # Don't link libraries to libpython, but do link binaries that expect
-    # to embed a python interpreter
-    # https://github.com/fontforge/fontforge/issues/2353#issuecomment-121009759
     ENV["PYTHON_CFLAGS"] = `python-config --cflags`.chomp
-    ENV["PYTHON_LIBS"] = "-undefined dynamic_lookup"
-    python_libs = `python2.7-config --ldflags`.chomp
-    inreplace "fontforgeexe/Makefile.am" do |s|
-      oldflags = s.get_make_var "libfontforgeexe_la_LDFLAGS"
-      s.change_make_var! "libfontforgeexe_la_LDFLAGS", "#{python_libs} #{oldflags}"
-    end
+    ENV["PYTHON_LIBS"] = `python-config --ldflags`.chomp
 
     args = %W[
       --prefix=#{prefix}
-      --disable-silent-rules
       --disable-dependency-tracking
-      --with-pythonbinary=#{which "python2.7"}
-      --without-x
     ]
 
+    args << "--without-x" if build.without? "x11"
     args << "--without-libpng" if build.without? "libpng"
     args << "--without-libjpeg" if build.without? "jpeg"
     args << "--without-libtiff" if build.without? "libtiff"
     args << "--without-giflib" if build.without? "giflib"
     args << "--without-libspiro" if build.without? "libspiro"
+    args << "--without-libuninameslist" if build.without? "libuninameslist"
 
     # Fix linker error; see: https://trac.macports.org/ticket/25012
     ENV.append "LDFLAGS", "-lintl"
@@ -76,11 +76,12 @@ class Fontforge < Formula
     # Reset ARCHFLAGS to match how we build
     ENV["ARCHFLAGS"] = "-arch #{MacOS.preferred_arch}"
 
-    # Bootstrap in every build: https://github.com/fontforge/fontforge/issues/1806
-    resource("gnulib").fetch
-    system "./bootstrap",
-           "--gnulib-srcdir=#{resource("gnulib").cached_download}",
-           "--skip-git"
+    if build.head?
+      resource("gnulib").fetch
+      system "./bootstrap",
+             "--gnulib-srcdir=#{resource("gnulib").cached_download}",
+             "--skip-git"
+    end
     system "./configure", *args
     system "make"
     system "make", "install"
@@ -105,6 +106,9 @@ class Fontforge < Formula
 
     Alternatively, install with Homebrew-Cask:
       brew cask install fontforge
+
+    As a last resort, you may try the *unsupported* option:
+      brew install fontforge --with-x11
     EOS
   end
 
