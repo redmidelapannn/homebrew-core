1f6092fd7e8e2ff7fe1884ad2f5dad8a432d1c3d
Tom MacWright <tom@macwright.org>
2016-10-07 12:21:09 -0400

    osrm-backend 5.4.0 (new formula)

---
+++ Formula/osrm-backend.rb
@@ -0,0 +1,30 @@
+class OsrmBackend < Formula
+  desc "High performance routing engine"
+  homepage "http://project-osrm.org/"
+  url "https://github.com/Project-OSRM/osrm-backend/archive/v5.4.0.tar.gz"
+  sha256 "6393745fe8250ad0387b1fe3f4a2218cf692528acb55a4e0198ba5fbe1c81231"
+
+  depends_on "cmake" => :build
+  depends_on "boost"
+  depends_on "cmake"
+  depends_on "libzip"
+  depends_on "libstxxl"
+  depends_on "libxml2"
+  depends_on "lua51"
+  depends_on "luajit"
+  depends_on "luabind"
+  depends_on "tbb"
+  depends_on "ccache"
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", "-DCMAKE_BUILD_TYPE=Release", *std_cmake_args
+      system "cmake", "--build", "."
+      system "cmake", "--build", ".", "--target", "install"
+    end
+  end
+
+  test do
+    system "false"
+  end
+end
