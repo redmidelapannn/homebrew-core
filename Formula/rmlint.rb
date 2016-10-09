b7ef5a27ed643c2922676efbe29102cc36e8c5c9
Tom Hoover <tom@hisword.net>
2016-10-09 15:53:19 -0500

    incorporate review comments
    add 'option' comments
    remove '-j 4' compile switch
    update test

--- Formula/rmlint.rb
+++ Formula/rmlint.rb
@@ -4,24 +4,27 @@ class Rmlint < Formula
   url "https://github.com/sahib/rmlint/archive/v2.4.4.tar.gz"
   sha256 "294708e7c98783a7782df1ed7f6fc79e9036571b7f69f76c5b3455545ce568bc"
 
+  option "with-json-glib", "Add support for reading json caches"
+  option "with-libelf", "Add support for finding non-stripped binaries"
+
   depends_on "glib" => :run
-  depends_on "scons" => :build        # build system
-  depends_on "gettext" => :build      # support for localization
-  depends_on "pkg-config" => :build   # manage compile and link flags for libraries
-  depends_on "sphinx-doc" => :build   # manpage/documentation generation - Build manpage from docs/rmlint.1.rst
-  depends_on "libelf" => :optional    # find non-stripped binaries
-  depends_on "json-glib" => :optional # support for reading json caches
+  depends_on "gettext" => :build
+  depends_on "pkg-config" => :build
+  depends_on "scons" => :build
+  depends_on "sphinx-doc" => :build
+  depends_on "json-glib" => :optional
+  depends_on "libelf" => :optional
 
   def install
     scons "config"
-    scons "-j 4"
+    scons
     bin.install "rmlint"
     man1.install "docs/rmlint.1.gz"
   end
 
   test do
     (testpath/"1.txt").write("1")
-    (testpath/"2.txt").write("2")
-    system bin/"rmlint"
+    (testpath/"2.txt").write("1")
+    system "(#{bin}/rmlint | grep '^# Duplicate(s):$') || false"
   end
 end
