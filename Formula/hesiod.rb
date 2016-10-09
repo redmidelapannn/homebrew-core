3a53aec510f0e9aa9ed7c48052840168d207b560
Eugene Lee <leee@mit.edu>
2016-10-08 23:57:50 -0400

    hesiod 3.2.1
    Signed-off-by: Eugene Lee <leee@mit.edu>

--- Formula/hesiod.rb
+++ Formula/hesiod.rb
@@ -1,8 +1,7 @@
 class Hesiod < Formula
   desc "Library for the simple string lookup service built on top of DNS"
   homepage "https://github.com/achernya/hesiod"
-  version "3.2.1"
-  url "https://github.com/achernya/hesiod/archive/hesiod-#{version}.tar.gz"
+  url "https://github.com/achernya/hesiod/archive/hesiod-3.2.1.tar.gz"
   sha256 "813ccb091ad15d516a323bb8c7693597eec2ef616f36b73a8db78ff0b856ad63"
 
   depends_on "automake"
