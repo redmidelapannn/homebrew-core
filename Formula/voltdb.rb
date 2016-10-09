b5a39199b96db40a1a43d7f409904c077b32e828
Tomi Pakarinen <tomi.pakarinen@resys.io>
2016-10-09 13:03:11 +0300

    voltdb 6.6

--- Formula/voltdb.rb
+++ Formula/voltdb.rb
@@ -1,8 +1,8 @@
 class Voltdb < Formula
   desc "Horizontally-scalable, in-memory SQL RDBMS"
   homepage "https://github.com/VoltDB/voltdb"
-  url "https://github.com/VoltDB/voltdb/archive/voltdb-5.6.tar.gz"
-  sha256 "9ea24d8cacdf2e19ba60487f3e9dfefa83c18cb3987571abc44b858ce0db7c3e"
+  url "https://github.com/VoltDB/voltdb/archive/voltdb-6.6.tar.gz"
+  sha256 "300d06c8f5a1cc4a3537f216ea105d5ccc8987b10bad2c676a40543c8903d72e"
   head "https://github.com/VoltDB/voltdb.git"
 
   bottle do
@@ -14,11 +14,14 @@ class Voltdb < Formula
   end
 
   depends_on :ant => :build
+  depends_on "cmake" => :build
+
+  patch :DATA
 
   def install
     system "ant"
 
-    inreplace Dir["bin/*"] - ["bin/voltadmin", "bin/voltdb", "bin/rabbitmqloader"],
+    inreplace Dir["bin/*"] - ["bin/voltadmin", "bin/voltdb", "bin/rabbitmqloader", "bin/voltdeploy", "bin/voltenv"],
       %r{VOLTDB_LIB=\$VOLTDB_HOME\/lib}, "VOLTDB_LIB=$VOLTDB_HOME/lib/voltdb"
 
     (lib/"voltdb").install Dir["lib/*"]
@@ -30,3 +33,25 @@ class Voltdb < Formula
     assert_match version.to_s, shell_output("#{bin}/voltdb --version")
   end
 end
+
+__END__
+--- voltdb-voltdb-6.6/bin/voltenv.orig	2016-10-09 11:08:34.000000000 +0300
++++ voltdb-voltdb-6.6/bin/voltenv	2016-10-09 11:09:20.000000000 +0300
+@@ -28,7 +28,7 @@
+ if [ -d "$VOLTDB_BIN/../lib/voltdb" ]; then
+     VOLTDB_BASE=$(dirname "$VOLTDB_BIN")
+     VOLTDB_LIB="$VOLTDB_BASE/lib/voltdb"
+-    VOLTDB_VOLTDB="$VOLTDB_LIB"
++    VOLTDB_VOLTDB="$VOLTDB_BASE/voltdb"
+ # distribution layout has libraries in separate lib and voltdb directories
+ else
+     VOLTDB_BASE=$(dirname "$VOLTDB_BIN")
+@@ -115,7 +115,7 @@
+ # if this script is run directly:
+ # Run the target passed as the first arg on the command line
+ # If no first arg, do nothing
+-if [ "${0}" = "$SOURCE" ]; then
++if [ "${0}" = "${BASH_SOURCE[0]}" ]; then
+     if [ $# -gt 1 ]; then
+         envhelp
+         exit 0
