5e1e86ce448fbd8a84c14e3b633ba1d3713509db
MikeLund <MikeLund@users.noreply.github.com>
2016-10-10 18:31:46 +0200

    apt-dater 1.0.3

--- Formula/apt-dater.rb
+++ Formula/apt-dater.rb
@@ -1,8 +1,11 @@
 class AptDater < Formula
   desc "Manage package updates on remote hosts using SSH"
   homepage "https://www.ibh.de/apt-dater/"
-  url "https://downloads.sourceforge.net/project/apt-dater/apt-dater/0.9.0/apt-dater-0.9.0.tar.gz"
-  sha256 "10b8335c156dabae249aa071cf8689900fae325c2e9540e36a840a2a77d3eeb4"
+  url "https://github.com/DE-IBH/apt-dater.git",
+      :tag => "v1.0.3",
+      :revision => "fdad3b43e50bc3582e391d6feabae150e133b3b3"
+
+  head "https://github.com/DE-IBH/apt-dater.git"
 
   bottle do
     sha256 "e8c3997c31dd62b04f5da31bd2055fea9ebb12e3959d09840d9eac0801e1befc" => :sierra
