4c9ccd710bf02988fbc774675ec1a94a2bfb035e
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-10 03:35:22 +0100

    open-scene-graph: update 3.4.0_1 bottle.

--- Formula/open-scene-graph.rb
+++ Formula/open-scene-graph.rb
@@ -8,11 +8,9 @@ class OpenSceneGraph < Formula
   head "http://www.openscenegraph.org/svn/osg/OpenSceneGraph/trunk/"
 
   bottle do
-    rebuild 1
-    sha256 "e0210bf841def60a869d605ff2ffa0f9e0d449d773053bd360f2b906632dcb13" => :sierra
-    sha256 "d34411461ff10b207645ac58981b71476b0e7da7d4461ea636fe2d530be5872a" => :el_capitan
-    sha256 "d85daf3e7ffa72f7fbfa2c3c9d0423689cce7f2864545775ded0899616f07c86" => :yosemite
-    sha256 "64d9990922aeff846ae9263e4f4457c6173d4b3be998150206c113490506e4ff" => :mavericks
+    rebuild 2
+    sha256 "ce31975464f869b839e540c853d87b0b583edd4f28b443bf2f6e75ca342d7a53" => :el_capitan
+    sha256 "22c2940d2f9a1aad69b05658a4eec0f9e52fa805aec0417180ac199861d63056" => :yosemite
   end
 
   option :cxx11
