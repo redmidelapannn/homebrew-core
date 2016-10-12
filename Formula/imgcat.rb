c1313261d15c0bf6aa490712c15f83610b71fe56
Dorian <dorian@doma.io>
2016-10-13 00:44:52 +0200

    Update imgcat.rb

--- Formula/imgcat.rb
+++ Formula/imgcat.rb
@@ -5,14 +5,12 @@ class Imgcat < Formula
   version "0.0.1"
   sha256 "5d471f24d512143796b81de873fb7b6660b0a57bc1c99bb26fd1c9ef8dff64de"
 
-  depends_on "bash" => :run
-
   def install
     bin.install "imgcat"
   end
 
   test do
-    touch "test.png"
-    system "imgcat", "test.png"
+    test_fixtures("test.png")
+    system "#{bin}/imgcat", "test.png"
   end
 end
