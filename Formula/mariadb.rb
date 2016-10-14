12ad6d3745db9cedfe1e3e10f5b9ef77b4c3f81f
Dan <dan@wadoku.de>
2016-10-14 08:54:29 +0200

    mariadb: use std_cmake_args

--- Formula/mariadb.rb
+++ Formula/mariadb.rb
@@ -61,10 +61,6 @@ class Mariadb < Formula
 
     # -DINSTALL_* are relative to prefix
     args = %W[
-      .
-      -DCMAKE_INSTALL_PREFIX=#{prefix}
-      -DCMAKE_FIND_FRAMEWORK=LAST
-      -DCMAKE_VERBOSE_MAKEFILE=ON
       -DMYSQL_DATADIR=#{var}/mysql
       -DINSTALL_INCLUDEDIR=include/mysql
       -DINSTALL_MANDIR=share/man
@@ -104,7 +100,7 @@ class Mariadb < Formula
     # Build with local infile loading support
     args << "-DENABLED_LOCAL_INFILE=1" if build.with? "local-infile"
 
-    system "cmake", *args
+    system "cmake", ".", *std_cmake_args, *args
     system "make"
     system "make", "install"
 
