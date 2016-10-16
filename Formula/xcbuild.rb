9e478be9badac100d1246ba50b8b3c158bf5f403
Romain Pouclet <romain.pouclet@gmail.com>
2016-10-16 19:39:58 -0400

    Use tag only

--- Formula/xcbuild.rb
+++ Formula/xcbuild.rb
@@ -1,10 +1,7 @@
 class Xcbuild < Formula
   desc "Xcode-compatible build tool"
   homepage "https://github.com/facebook/xcbuild"
-  url "https://github.com/facebook/xcbuild.git",
-      :tag => "0.1.0",
-      :revision => "08575f65f1c907e280ed4663b5461b7cfaf5cfaa",
-      :shallow => false
+  url "https://github.com/facebook/xcbuild.git", :tag => "0.1.0"
   head "https://github.com/facebook/xcbuild.git", :shallow => false
 
   depends_on "ninja" => :recommended
