68389aace6f8f6d89cddc4743a8b002552ef7f6e
Ri Xu <xuri.me@gmail.com>
2016-10-17 16:26:28 +0800

    Add new formula: aurora 1.2 (brew audit --strict aurora fixed)

--- Formula/aurora.rb
+++ Formula/aurora.rb
@@ -2,7 +2,6 @@ class Aurora < Formula
   desc "Beanstalk queue server console"
   homepage "https://xuri.me/aurora"
   url "https://github.com/Luxurioust/aurora/releases/download/1.2/aurora_darwin_amd64_v1.2.tar.gz"
-  version "1.2"
   sha256 "c480e46592d6fb6f025572795bac80357f81b662cefdb3d149c77c723fb9a81f"
 
   def install
