a4709ab11d7ef0136cba007e17679251f13cff61
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-17 07:15:40 +0100

    xchelper: add 1.0.10 bottle.

--- Formula/xchelper.rb
+++ Formula/xchelper.rb
@@ -5,6 +5,12 @@ class Xchelper < Formula
   version "1.0.10"
   sha256 "5b69efd533d89e3a6d202e9021df27a958533d691e64cfb2192713d7acbc1597"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7a416c28734996f163aa7373adfb802ad5fe1cc4e20f2b49ca95ce2911325de7" => :sierra
+    sha256 "7a416c28734996f163aa7373adfb802ad5fe1cc4e20f2b49ca95ce2911325de7" => :el_capitan
+  end
+
   def install
     system "swift", "build", "--configuration", "release"
     bin.install "./.build/release/xchelper"
