2df614444ce3e8f02c837da32225078426bf8d27
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-18 02:32:09 +0100

    librdkafka: update 0.9.1 bottle.

--- Formula/librdkafka.rb
+++ Formula/librdkafka.rb
@@ -8,10 +8,10 @@ class Librdkafka < Formula
 
   bottle do
     cellar :any
-    sha256 "40bc4c4295471e4c9209c28d5b1b3485717e5e310c5fb297e86558853a5178aa" => :sierra
-    sha256 "dd1b9001bfd24f6cdc942fbe6c1b52e992e1e65ff1849962b2528fc8cd5773d5" => :el_capitan
-    sha256 "e516c7b35fb951370b102a73ef89f07fc40884c8f92e4c449c59a6933c32d148" => :yosemite
-    sha256 "a002a4f38bc21030d9c5a69dec3de560ec67ce5e976f80cbcb7cd52fbdfb22f3" => :mavericks
+    rebuild 1
+    sha256 "03ffd080053dc66cde63d18ff63068953d5bb0e5a50571acefc77e091a3cccff" => :sierra
+    sha256 "6b89f5307c79adfc62710ed7aed09eb15854936d37024296102d756db1c25a9c" => :el_capitan
+    sha256 "e09aacba4551ee64487532557d0b37ccd5cf9d4a89b93b3fa9152d7c8e67faf2" => :yosemite
   end
 
   depends_on "pkg-config" => :build
