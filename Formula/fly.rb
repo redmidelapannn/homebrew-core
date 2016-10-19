954879736104078a2677e6d654023c3ba9172b3a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-19 21:45:21 +0100

    fly: add 64 bottle.

--- Formula/fly.rb
+++ Formula/fly.rb
@@ -4,6 +4,12 @@ class Fly < Formula
   url "https://github.com/concourse/concourse/releases/download/v2.3.1/fly_darwin_amd64"
   sha256 "4940c41cddc94887a2a838e3c4259e5d6ff4448550d2a680621518c14a648039"
 
+  bottle do
+    sha256 "037a77c95a0027e040149737b3e0d71c09403cf23151bef3f56c38067ab4612e" => :sierra
+    sha256 "037a77c95a0027e040149737b3e0d71c09403cf23151bef3f56c38067ab4612e" => :el_capitan
+    sha256 "037a77c95a0027e040149737b3e0d71c09403cf23151bef3f56c38067ab4612e" => :yosemite
+  end
+
   def install
     bin.install "fly_darwin_amd64" => "fly"
   end
