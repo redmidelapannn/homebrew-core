32fa8169eeb6548af5aeba93e909400847c598d2
Thierry Moisan <thierry.moisan@jda.com>
2016-10-20 11:00:27 -0400

    Update the deparallelize comment with upstream answer

--- Formula/gsoap.rb
+++ Formula/gsoap.rb
@@ -13,8 +13,9 @@ class Gsoap < Formula
   depends_on "openssl"
 
   def install
-    # Contacted upstream by email on October 12th 2016 about this, it is due to the
-    # compilation of symbol2.c and soapcpp2_yacc.h not being ordered correctly in parallel.
+    # Contacted upstream by email and been told this should be fixed by 2.8.37,
+    # it is due to the compilation of symbol2.c and soapcpp2_yacc.h not being
+    # ordered correctly in parallel.
     ENV.deparallelize
     system "./configure", "--prefix=#{prefix}"
     system "make"
