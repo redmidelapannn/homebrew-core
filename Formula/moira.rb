74b8ee202e20314501f14e1782e967633b2c261a
Eugene Lee <leee@mit.edu>
2016-10-20 08:03:53 -0400

    moira 4.0.0.3
    Signed-off-by: Eugene Lee <leee@mit.edu>

--- Formula/moira.rb
+++ Formula/moira.rb
@@ -4,32 +4,43 @@ class Moira < Formula
   url "https://github.com/mit-athena/moira/archive/4.0.0.3+51+g65d55c5.tar.gz"
   sha256 "d418681ae4ec61a124c55fb67a6bba0d89b59dc760c4d1c9a1e8e1c9c7b69b19"
 
+  option "with-krb5", "Build with homebrew/dupes/krb5"
+
   depends_on "e2fsprogs" # for com_err, as recommended by Debian packaging
   depends_on "hesiod"
-  depends_on "homebrew/dupes/krb5"
+  depends_on "homebrew/dupes/krb5" if build.with? "krb5"
 
   def install
+    args = %W[
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --prefix=#{prefix}
+      --with-com_err=#{Formula["e2fsprogs"].opt_prefix}
+      --without-krb4
+      --with-hesiod=#{Formula["hesiod"].opt_prefix}
+      --without-zephyr
+      --without-oracle
+      --without-afs
+      --disable-rpath
+      --mandir=#{man}
+    ]
+
+    if build.with? "krb5"
+      args << "--with-krb5=#{Formula["krb5"].opt_prefix}"
+    else
+      args << "--with-krb5"
+    end
+
     cd "moira" do
-      system "./configure", "--disable-dependency-tracking",
-                            "--disable-silent-rules",
-                            "--prefix=#{prefix}",
-                            "--with-com_err=#{Formula["e2fsprogs"].opt_prefix}",
-                            "--with-krb5=#{Formula["krb5"].opt_prefix}",
-                            "--without-krb4",
-                            "--with-hesiod=#{Formula["hesiod"].opt_prefix}",
-                            "--without-zephyr",
-                            "--without-oracle",
-                            "--without-afs",
-                            "--disable-rpath",
-                            "--mandir=#{man}"
+      system "./configure", *args
       system "make"
       system "make", "install"
     end
-    cd '#{bin}' do
+    cd bin do
       mv "chfn", "chfn.moira"
       mv "chsh", "chsh.moira"
     end
-    cd '#{man1}' do
+    cd man1 do
       mv "chsh.1", "chsh.moira.1"
       mv "chfn.1", "chfn.moira.1"
     end
