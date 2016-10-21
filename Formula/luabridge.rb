53abf05aeacc844e76cf20c2f7f0beddf829db27
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-21 11:43:35 +0100

    luabridge: add 1.0.2 bottle.

--- Formula/luabridge.rb
+++ Formula/luabridge.rb
@@ -5,6 +5,13 @@ class Luabridge < Formula
   version "1.0.2"
   sha256 "9cc5943b6fd9581744d37c19c65f3f825b5d6b4f88f402197cb27daa532099db"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6f6a504f89d520bf12a0915ff018d710489dcb08b8c736f25af9b941a8462335" => :sierra
+    sha256 "6f6a504f89d520bf12a0915ff018d710489dcb08b8c736f25af9b941a8462335" => :el_capitan
+    sha256 "6f6a504f89d520bf12a0915ff018d710489dcb08b8c736f25af9b941a8462335" => :yosemite
+  end
+
   def install
     include.install Dir["Source/*"]
   end
