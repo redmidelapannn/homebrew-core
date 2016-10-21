a7c3422c0062cd46a441a598bcf6f4cf9d334c84
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-21 16:56:59 +0100

    dscanner: update 0.3.0 bottle.

--- Formula/dscanner.rb
+++ Formula/dscanner.rb
@@ -8,10 +8,10 @@ class Dscanner < Formula
   head "https://github.com/Hackerpilot/Dscanner.git"
 
   bottle do
-    sha256 "21462544cf3d662d79ad62fac0d27a6d6346404a4c7a58f1d0e3df86962a17b4" => :sierra
-    sha256 "e4109d118bfc3ea842bf3d2aa9588dec4e5be8350557f644727b752ec261cd32" => :el_capitan
-    sha256 "5f8b2e4fb9df04ba43eaba0c1f7fe53c3cee1bf71c4f23f0fcd7871c7269ba4a" => :yosemite
-    sha256 "93916657176868ebab187de76075da38ef1f62e7fcf126670c297d1413c172af" => :mavericks
+    rebuild 1
+    sha256 "b5f6cbdffeddf2e17a8ad833ff3d01554157a5beb479d3e6c9b4c8a65538784a" => :sierra
+    sha256 "62fb0f5c203ff270aac288fa910c99ba83495431f8d20c6605277c51f6cd8d4c" => :el_capitan
+    sha256 "7ed01cd8a18347e03e2798e6c74f367f60e453f79dd90971e2d19b7bd34a76ed" => :yosemite
   end
 
   devel do
