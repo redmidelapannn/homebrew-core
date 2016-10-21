17106815d713c5d8168dfeaba7e6ae8c0316481c
André Vennberg <andre.vennberg@gmail.com>
2016-10-21 12:23:08 +0200

    LuaBridge 1.0.2 (new formula)
    A lightweight, dependency-free library for binding Lua to C++

---
+++ Formula/luabridge.rb
@@ -0,0 +1,15 @@
+class Luabridge < Formula
+  desc "Lightweight, dependency-free library for binding Lua to C++"
+  homepage "https://github.com/vinniefalco/LuaBridge"
+  url "https://github.com/vinniefalco/LuaBridge/archive/master.tar.gz"
+  version "1.0.2"
+  sha256 "9cc5943b6fd9581744d37c19c65f3f825b5d6b4f88f402197cb27daa532099db"
+
+  def install
+    include.install Dir["Source/*"]
+  end
+
+  test do
+    system "false"
+  end
+end
