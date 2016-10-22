74260685631172aaaaf66254426f918344f15ee9
designerror <designerror@yandex.ru>
2016-10-22 13:23:57 +0400

    Update wdc.rb

--- Formula/wdc.rb
+++ Formula/wdc.rb
@@ -1,8 +1,7 @@
 class Wdc < Formula
   desc "WebDAV Client provides easy and convenient to work with WebDAV-servers."
   homepage "https://designerror.github.io/webdav-client-cpp"
-  url "https://github.com/designerror/webdav-client-cpp/archive/v#{version}.tar.gz"
-  version "1.0.0"
+  url "https://github.com/designerror/webdav-client-cpp/archive/v1.0.0.tar.gz"
   sha256 "938aa52ae96595d0370f4d6d13beb7b70bbea56434aecd6259e7ead2dd6f9050"
 
   depends_on "cmake" => :build
