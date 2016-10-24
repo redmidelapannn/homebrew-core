78619afc332b625dbd9e08d05d3d49a04053be61
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-24 21:56:45 +0100

    mmark: add 1.3.4 bottle.

--- Formula/mmark.rb
+++ Formula/mmark.rb
@@ -6,6 +6,13 @@ class Mmark < Formula
   url "https://github.com/miekg/mmark/archive/v1.3.4.tar.gz"
   sha256 "e03744da8d16cc742423685e2ad7cb1af61bf6dc5364c6875057b7c28ab26bb8"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "419a58ae448440698f885a0f6a5e0c7cf96c1ddcb2faeb186d67f592ddc69f4d" => :sierra
+    sha256 "12732ca195c7f119e5125468264c93857c0c3d338961d2f6ebce7b8ef73f9dc0" => :el_capitan
+    sha256 "4fce71b6fedbfb9633ed70e879f70a59acb0d33ab5db9dbbf4de171ab8be4c56" => :yosemite
+  end
+
   depends_on "go" => :build
 
   go_resource "github.com/BurntSushi/toml" do
