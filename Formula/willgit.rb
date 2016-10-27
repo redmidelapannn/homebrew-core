ef1d2e3882c96d5b2276090ff0c4be2a62bad4f8
BrewTestBot <brew-test-bot@googlegroups.com>
2016-10-27 23:01:13 +0100

    willgit: add 1.0.0 bottle.

--- Formula/willgit.rb
+++ Formula/willgit.rb
@@ -5,6 +5,13 @@ class Willgit < Formula
   sha256 "3bb99d6ec2614a90f40962311daf51f393b3d0abfdb0f9e0a14ba7340b33a2c8"
   head "https://github.com/DanielVartanov/willgit.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "26bc53b5d48158229ebfcedc000ad66c8320707af0493429e6620b5cc73cbd17" => :sierra
+    sha256 "26bc53b5d48158229ebfcedc000ad66c8320707af0493429e6620b5cc73cbd17" => :el_capitan
+    sha256 "26bc53b5d48158229ebfcedc000ad66c8320707af0493429e6620b5cc73cbd17" => :yosemite
+  end
+
   def install
     prefix.install "bin"
   end
