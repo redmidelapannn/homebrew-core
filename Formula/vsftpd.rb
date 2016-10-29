a2dc22b2e6935dc91e383f36c4d08b9c39a0d3f5
jabenninghoff <jbenninghoff@mac.com>
2016-10-28 20:54:10 -0500

    Add support for brew services

--- Formula/vsftpd.rb
+++ Formula/vsftpd.rb
@@ -49,6 +49,31 @@ class Vsftpd < Formula
 
     s
   end
+
+  plist_options :startup => true, :manual => "sudo #{HOMEBREW_PREFIX}/sbin/vsftpd #{HOMEBREW_PREFIX}/etc/vsftpd.conf"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{sbin}/vsftpd</string>
+        <string>#{etc}/vsftpd.conf</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+    </dict>
+    </plist>
+    EOS
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{sbin}/vsftpd -v 0>&1")
+  end
 end
 
 __END__
