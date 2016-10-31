7a2d2c233c340ca94970a12acae320d2b6af3590
Romain Pouclet <romain.pouclet@gmail.com>
2016-10-30 21:25:53 -0400

    Require ninja

--- Formula/xcbuild.rb
+++ Formula/xcbuild.rb
@@ -4,7 +4,7 @@ class Xcbuild < Formula
   url "https://github.com/facebook/xcbuild.git", :tag => "0.1.0", :revision => "08575f65f1c907e280ed4663b5461b7cfaf5cfaa"
   head "https://github.com/facebook/xcbuild.git", :shallow => false
 
-  depends_on "ninja" => :recommended
+  depends_on "ninja"
   depends_on "cmake"
   depends_on "libpng"
   depends_on "pkg-config"
