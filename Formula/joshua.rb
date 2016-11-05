079e11dd5599d734aa7ac108dc67129f0122478a
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-05 06:55:06 +0000

    joshua: update 6.0.5 bottle.

--- Formula/joshua.rb
+++ Formula/joshua.rb
@@ -7,11 +7,10 @@ class Joshua < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "7b04fb7031b9f002a418eb7d674d2ceb05be0926c0a7d8abfea644be6d381df4" => :sierra
-    sha256 "b649095ea4a944799fbc1ccd8425464b7d2711b0a149049b4d2d5e92d604c5ae" => :el_capitan
-    sha256 "6ac9fb24f8b1bb70a32c72c8436b8ad43717cf83d65499cb011214061b6ce6ba" => :yosemite
-    sha256 "176fa47a6a2722fb5b6bf1e2efba8da32bab6355f3d844424a817882ed7b3a8e" => :mavericks
+    rebuild 2
+    sha256 "c1d7803e89f8591d545ef3557fc7911e8a7ca99d9243cc463195860f3131650d" => :sierra
+    sha256 "c1d7803e89f8591d545ef3557fc7911e8a7ca99d9243cc463195860f3131650d" => :el_capitan
+    sha256 "c1d7803e89f8591d545ef3557fc7911e8a7ca99d9243cc463195860f3131650d" => :yosemite
   end
 
   option "with-en-ru-phrase-pack", "Build with English --> Russian hiero-based model [4.6G]."
