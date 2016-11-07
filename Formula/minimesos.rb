370827261719128677c5561caaf753b13f715f33
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-07 17:27:22 +0000

    minimesos: add 0.10.2 bottle.

--- Formula/minimesos.rb
+++ Formula/minimesos.rb
@@ -4,6 +4,13 @@ class Minimesos < Formula
   url "https://github.com/ContainerSolutions/minimesos/archive/0.10.2.tar.gz"
   sha256 "16b198078c9384426f8849b08e92e173816c4d6626331bc6569aa5de89dffbdd"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c60a5566df04fc822f621e6db6c090a2eeeca4cf0f36fb2dd9250e9ab6f647df" => :sierra
+    sha256 "c60a5566df04fc822f621e6db6c090a2eeeca4cf0f36fb2dd9250e9ab6f647df" => :el_capitan
+    sha256 "c60a5566df04fc822f621e6db6c090a2eeeca4cf0f36fb2dd9250e9ab6f647df" => :yosemite
+  end
+
   depends_on "docker"
 
   def install
