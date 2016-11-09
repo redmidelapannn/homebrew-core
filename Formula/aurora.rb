0bf59719dd9f07c834ca55633eedad77f45921a2
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-09 06:20:17 +0000

    aurora: add 1.4 bottle.

--- Formula/aurora.rb
+++ Formula/aurora.rb
@@ -6,6 +6,13 @@ class Aurora < Formula
   url "https://github.com/Luxurioust/aurora/archive/1.4.tar.gz"
   sha256 "ab37905703d5265efbc9b588cbad9099e5863696d5a4f88f05f4107d5a2530e7"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8be9579b274dcb842bd5727383d1804fc6326479f7fa42cf165def027021003f" => :sierra
+    sha256 "19ff7e4024118944c4f591e0b3a206c01e352968017604832ba5ee3d4aab1774" => :el_capitan
+    sha256 "215538725ef5de47b8ccd6def623c2597bd0143e521aa768e775f3bd2bb190a3" => :yosemite
+  end
+
   depends_on "go" => :build
 
   go_resource "github.com/BurntSushi/toml" do
