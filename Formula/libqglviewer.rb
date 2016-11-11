b08fe7b7db9d68d674c392cec88bc9a84bad6f45
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-11 09:56:53 +0000

    libqglviewer: update 2.6.3 bottle.

--- Formula/libqglviewer.rb
+++ Formula/libqglviewer.rb
@@ -7,9 +7,10 @@ class Libqglviewer < Formula
 
   bottle do
     cellar :any
-    sha256 "1f00c73ef441a6ffcddd7a4b086701b4a56796c4b9fd6ca979f0cbc7e485c97e" => :el_capitan
-    sha256 "ee30fec03b3a34c4377211779212bae6f0bf1211b20abed235ef2503b5b72d9c" => :yosemite
-    sha256 "8bb883a52487feb86cd2bbe1c83ac4c70a7d45282ef76a4a22e7dfa89774d9f8" => :mavericks
+    rebuild 1
+    sha256 "323d6b2bb95797bf01c8c4fafa61e3bdc89de7320f14c9bded7349d24bf70afa" => :sierra
+    sha256 "9558d99ff53c42ff841dc04bdd4914ff4e15e909fc5f12fef77519daa68b5451" => :el_capitan
+    sha256 "ead95a70ae439e64e19ce69f55909e8de0ce174d37e6edb3f438eeca0064138e" => :yosemite
   end
 
   option :universal
