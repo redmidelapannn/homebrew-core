d380465b94d3c3da0876714c2512f6316f966ae6
Edward Loveall <edward@edwardloveall.com>
2016-11-14 10:57:55 -0500

    Create a better test for eject formula

--- Formula/eject.rb
+++ Formula/eject.rb
@@ -14,6 +14,36 @@ class Eject < Formula
   end
 
   test do
-    system "#{bin}/eject"
+    xib = File.open "view.xib", "a+" do |file|
+      file.write <<-XML
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<document type="com.apple.InterfaceBuilder3.CocoaTouch.XIB" version="3.0" toolsVersion="11134" systemVersion="15F34" targetRuntime="iOS.CocoaTouch" propertyAccessControl="none" useAutolayout="YES" useTraitCollections="YES" colorMatched="YES">
+    <dependencies>
+        <plugIn identifier="com.apple.InterfaceBuilder.IBCocoaTouchPlugin" version="11106"/>
+        <capability name="documents saved in the Xcode 8 format" minToolsVersion="8.0"/>
+    </dependencies>
+    <objects>
+        <placeholder placeholderIdentifier="IBFilesOwner" id="-1" userLabel="File's Owner"/>
+        <placeholder placeholderIdentifier="IBFirstResponder" id="-2" customClass="UIResponder"/>
+        <view contentMode="scaleToFill" id="iN0-l3-epB">
+            <rect key="frame" x="0.0" y="0.0" width="375" height="667"/>
+            <autoresizingMask key="autoresizingMask" widthSizable="YES" heightSizable="YES"/>
+            <color key="backgroundColor" red="1" green="1" blue="1" alpha="1" colorSpace="custom" customColorSpace="sRGB"/>
+        </view>
+    </objects>
+</document>
+      XML
+    end
+
+    swift = <<-SWIFT
+// Create Views
+let view = UIView()
+view.frame = CGRect(x: 0, y: 0, width: 375, height: 667)
+view.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+view.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+    SWIFT
+
+    system "eject --file view.xib > view.swift"
+    File.read("view.swift") == swift
   end
 end
