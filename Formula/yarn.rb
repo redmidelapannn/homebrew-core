b607351b1dc6d7474710020dde627ea1d06657b5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-15 15:20:21 +0000

    yarn: update 0.17.0_1 bottle.

--- Formula/yarn.rb
+++ Formula/yarn.rb
@@ -10,9 +10,9 @@ class Yarn < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "7f6f2621524c87e309fbea9ad7d8f49086dd481358f76f3f57f682ea0f57c2ab" => :sierra
-    sha256 "aafe13b85ed27f46f884f38675927f29c4b5ed4fd78e92a3d2c803dcdb743c54" => :el_capitan
-    sha256 "bd3f2ece7e38bd73d6090cb478893658515107ca8cee334ba895b030cfaf88f8" => :yosemite
+    sha256 "ddb5c1575638d79fa538bd79f2884bd9e9443bd35eefd691b4a44fa75eefb027" => :sierra
+    sha256 "d50e30dd8267afa65c04c1193f12103bab89488e26769dcfa98b9f113c96b4b3" => :el_capitan
+    sha256 "49d857265db4c93fdf4c5c9dff8fd5bb769ba2597d3cbabfd41aaec29d0ea110" => :yosemite
   end
 
   depends_on "node"
