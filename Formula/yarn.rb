ae1dac9d124dd2aa8116f706a9bd70a92e1a0ffd
Rifat Nabi <to.rifat@gmail.com>
2016-11-15 20:35:32 +0600

    Remove npm install dependency
    **POC**
    
    `brew install yarn -s`

--- Formula/yarn.rb
+++ Formula/yarn.rb
@@ -1,5 +1,3 @@
-require "language/node"
-
 class Yarn < Formula
   desc "Javascript package manager"
   homepage "https://yarnpkg.com/"
@@ -17,7 +15,7 @@ class Yarn < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    cp_r '.', prefix
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
