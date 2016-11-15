0a52a6f90e0f67e2d042f3df426dfa6fe4f0f4bc
ilovezfs <ilovezfs@icloud.com>
2016-11-15 06:07:21 -0800

    yarn: fix for global add bug

--- Formula/yarn.rb
+++ Formula/yarn.rb
@@ -5,6 +5,7 @@ class Yarn < Formula
   homepage "https://yarnpkg.com/"
   url "https://yarnpkg.com/downloads/0.17.0/yarn-v0.17.0.tar.gz"
   sha256 "bb87332c23baec5680e13c9afa858d851276eca27e33e215a84338fb4acb0026"
+  revision 1
   head "https://github.com/yarnpkg/yarn.git"
 
   bottle do
@@ -15,6 +16,12 @@ class Yarn < Formula
   end
 
   depends_on "node"
+
+  # https://github.com/yarnpkg/yarn/pull/1840
+  patch do
+    url "https://gist.githubusercontent.com/ilovezfs/319a920d4de838f1e61f42f08f8b65b5/raw/db81515c9aecaf2a8c7c976797749d4993ba3b07/gistfile1.txt"
+    sha256 "3de71734cc04d4c8766423e31f21abd5f206ba1b250f26d6a35ffc7c2ae0022b"
+  end
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
