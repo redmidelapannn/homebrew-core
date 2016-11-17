13a89cde227eb64b4fb089a26896bdb9da901a21
Noé Falzon <noe@yousician.com>
2016-11-17 15:28:49 +0200

    libgc 7.6.0 (new formula)

--- Formula/libgc.rb
+++ Formula/libgc.rb
@@ -1,16 +1,22 @@
 class Libgc < Formula
   desc "The Boehm-Demers-Weiser conservative garbage collector"
   homepage "http://www.hboehm.info/gc/"
-  url "http://www.hboehm.info/gc/gc_source/gc-7.2g.tar.gz"
-  sha256 "584e29e2f1be4a389ca30f78dcd2c991031e7d1e1eb3d7ce2a0f975218337c2f"
+  url "http://www.hboehm.info/gc/gc_source/gc-7.6.0.tar.gz"
+  sha256 "a14a28b1129be90e55cd6f71127ffc5594e1091d5d54131528c24cd0c03b7d90"
+
+  resource "libatomic_ops" do
+    url "http://www.hboehm.info/gc/gc_source/libatomic_ops-7.4.4.tar.gz"
+    sha256 "bf210a600dd1becbf7936dd2914cf5f5d3356046904848dcfd27d0c8b12b6f8f"
+  end
 
   def install
+    resource("libatomic_ops").stage("#{buildpath}/libatomic_ops")
     system "./configure", "--disable-silent-rules", "--prefix=#{prefix}"
     system "make", "install"
   end
 
   test do
-    (testpath/"test.c").write <<-'EOS'.undent
+    (testpath/"test.c").write <<-EOS.undent
       #include <assert.h>
       #include <stdio.h>
       #include "gc.h"
@@ -26,8 +32,6 @@ class Libgc < Formula
           int *q = (int *) GC_MALLOC_ATOMIC(sizeof(int));
           assert(*p == 0);
           *p = (int *) GC_REALLOC(q, 2 * sizeof(int));
-          if (i % 100000 == 0)
-            printf("Heap size = %zu\n", GC_get_heap_size());
         }
         return 0;
       }
