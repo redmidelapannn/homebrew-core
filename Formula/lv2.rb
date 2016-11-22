c037866b2c5981e60fff0993303d8355c172f538
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-22 09:29:17 +0000

    lv2: update 1.14.0 bottle.

--- Formula/lv2.rb
+++ Formula/lv2.rb
@@ -4,12 +4,10 @@ class Lv2 < Formula
   url "http://lv2plug.in/spec/lv2-1.14.0.tar.bz2"
   sha256 "b8052683894c04efd748c81b95dd065d274d4e856c8b9e58b7c3da3db4e71d32"
   bottle do
-    cellar :any_skip_relocation
-    rebuild 1
-    sha256 "6e05929ad39ab40031bb8b141eeb9c3ad172cc1f1d0719f79b03c7c339a6d880" => :sierra
-    sha256 "8ad6c88d4ea316fc2e1e15504b3cc88e634e072508932e4278da6824471ac539" => :el_capitan
-    sha256 "a4a75ab0aca13e96c7fe10568ddd905bd511909e3a1cb84d1f640cce6e90acc6" => :yosemite
-    sha256 "ce29adad60e4b904f15e15cfde2cd07f5a2490e7053279a605db0b87a0e7bdd1" => :mavericks
+    cellar :any
+    sha256 "9410ab55c95bbe9577e828fb88d2d7b1c004fa1a53f65bfa9ba5ba5c7aef5222" => :sierra
+    sha256 "64b76a65718d9c1be383864398f7aaec72ae723adea16ceec82ffd80b47c2dff" => :el_capitan
+    sha256 "cc97683d6a0b04c84d2d8e63c3caf064b188b9ae559a0853d41ad57e3db22378" => :yosemite
   end
 
   def install
