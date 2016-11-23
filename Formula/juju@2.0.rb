daf055fa7fcb0a326841448992076053c9a18c50
Curtis Hovey <sinzui.is@gmail.com>
2016-11-23 12:36:03 -0500

    Update juju to 2.0.1 and 1.25.8 with sierra support.

--- Formula/juju@2.0.rb
+++ Formula/juju@2.0.rb
@@ -1,18 +1,11 @@
 class JujuAT20 < Formula
   desc "DevOps management tool"
   homepage "https://jujucharms.com/"
-  url "https://launchpad.net/juju/2.0/2.0.0/+download/juju-core_2.0.0.tar.gz"
-  sha256 "dac0b59ac670ba922cfbe7dc29652a07f4166c927c0fdba2f0b5760035b1fe18"
+  url "https://launchpad.net/juju/2.0/2.0.1/+download/juju-core_2.0.1.tar.gz"
+  sha256 "af5d59f4b4508c3f81b15fe052fe377876f5de845885d6d41d054f4ac605b9e9"
 
   depends_on "go" => :build
   conflicts_with "juju@1.25", :because => "juju 1 and 2 cannot be installed simultaneously."
-
-  # The juju client wants to know that MacOS Sierra is supported.
-  # https://bugs.launchpad.net/juju/+bug/1633495
-  patch do
-    url "https://raw.githubusercontent.com/sinzui/patches/master/homebrew-juju-sierra.patch"
-    sha256 "363bbf23437e07805e0fafd54a758e37ff135c5a529344e53841ae453ad1c39a"
-  end
 
   def install
     ENV["GOPATH"] = buildpath
