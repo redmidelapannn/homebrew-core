099bc2380c2656cd6f1e479cdbd30bba0ca70ad5
BrewTestBot <brew-test-bot@googlegroups.com>
2016-11-23 17:38:53 +0000

    red: add 061 bottle.

--- Formula/red.rb
+++ Formula/red.rb
@@ -5,6 +5,13 @@ class Red < Formula
   sha256 "afefaa392e5dbc1ec6d8805376ecffe86a1f6d1ce46d426800556f3c4f698693"
   head "https://github.com/red/red.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "2c0d3deb8a5f3be80c792954115c86b74455de394d09df8350a65f15e7082350" => :sierra
+    sha256 "2c0d3deb8a5f3be80c792954115c86b74455de394d09df8350a65f15e7082350" => :el_capitan
+    sha256 "2c0d3deb8a5f3be80c792954115c86b74455de394d09df8350a65f15e7082350" => :yosemite
+  end
+
   def install
     mv "red-061", "red"
     bin.install "red"
