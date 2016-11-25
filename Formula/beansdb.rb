b08a666c317d976e5caa83181f2f50ea5077feb2
Thierry Moisan <thierry.moisan@jda.com>
2016-11-25 09:22:48 -0500

    Remove revision line

--- Formula/beansdb.rb
+++ Formula/beansdb.rb
@@ -3,7 +3,6 @@ class Beansdb < Formula
   homepage "https://github.com/douban/beansdb"
   url "https://github.com/douban/beansdb/archive/v0.6.tar.gz"
   sha256 "b24512862f948d5191f5c43316a41f632bc386f43dcbb69b03ffffe95122a33e"
-  revision 1
 
   bottle do
     cellar :any_skip_relocation
