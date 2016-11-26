9ede45437ad359fc03ec407ca48083bdc5d1e250
A. Jesse Jiryu Davis <jesse@mongodb.com>
2016-11-26 18:21:50 -0500

    response to critique

--- Formula/yelp-tools.rb
+++ Formula/yelp-tools.rb
@@ -13,15 +13,17 @@ class YelpTools < Formula
   end
 
   depends_on "gettext" => :build
-  depends_on "gtk+3" => :run
+  depends_on "gtk+3"
   depends_on "intltool" => :build
   depends_on "itstool" => :build
   depends_on "libxml2" => :build
   depends_on "libxslt" => :build
   depends_on "pkg-config" => :build
 
+  yelp_tools_version = version
+
   resource "yelp-xsl" do
-    url "https://download.gnome.org/sources/yelp-xsl/3.18/yelp-xsl-3.18.0.tar.xz"
+    url "https://download.gnome.org/sources/yelp-xsl/3.18/yelp-xsl-#{yelp_tools_version}.tar.xz"
     sha256 "893620857b72b3b43ee3b462281240b7ca4d80292f469552827f0597bf60d2b2"
   end
 
@@ -32,7 +34,7 @@ class YelpTools < Formula
                             "--disable-silent-rules",
                             "--prefix=#{prefix}"
       system "make", "install"
-      ENV.append_path "PKG_CONFIG_PATH", "#{prefix}/share/pkgconfig"
+      ENV.append_path "PKG_CONFIG_PATH", "#{share}/pkgconfig"
     end
 
     system "./configure", "--prefix=#{prefix}"
@@ -40,17 +42,21 @@ class YelpTools < Formula
   end
 
   def post_install
-    system "#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/hicolor"
+    system "#{Formula["gtk+3"].opt_bin}/gtk3-update-icon-cache",
+           "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/hicolor"
   end
 
   test do
     system "#{bin}/yelp-new", "task", "ducksinarow"
     system "#{bin}/yelp-build", "html", "ducksinarow.page"
     system "#{bin}/yelp-check", "validate", "ducksinarow.page"
-
-    assert (pkgshare/"xslt/mallard/html/mal2html-links.xsl").exist?
-    assert (pkgshare/"js/jquery.syntax.brush.smalltalk.js").exist?
-    assert (pkgshare/"icons/hicolor/24x24/status/yelp-note-warning.png").exist?
-    assert (share/"pkgconfig/yelp-xsl.pc").exist?
+    [
+      prefix/"share/yelp-xsl/icons/hicolor/24x24/status/yelp-note-warning.png",
+      prefix/"share/yelp-xsl/js/jquery.syntax.brush.smalltalk.js",
+      prefix/"share/yelp-xsl/xslt/mallard/html/mal2html-links.xsl",
+      share/"pkgconfig/yelp-xsl.pc",
+    ].each do |filename|
+      assert filename.exist?, "#{filename} doesn't exist"
+    end
   end
 end
