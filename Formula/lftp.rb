49965fe505aa5ec11199c207ab0ce177e813abc7
Hidenori MATSUKI <MATSUKI.Hidenori@gmail.com>
2016-11-29 12:09:36 +0900

    lftp 4.7.4
    Add modified version for OSX.
    ref. https://github.com/lavv17/lftp/commit/f38a17745ef01e1bca9a3420995e193bf9768c7c

---
+++ Formula/lftp.rb
@@ -0,0 +1,21 @@
+class Lftp < Formula
+  desc "Sophisticated file transfer program"
+  homepage "https://lftp.yar.ru/"
+  url "https://lftp.yar.ru/ftp/lftp-4.7.4.tar.xz"
+  sha256 "bf67c4d128b6f769a4082947376a9679c5ee3463a24ab761a0757f75d70bd92c"
+
+  depends_on "pkg-config" => :build
+  depends_on "readline"
+  depends_on "openssl"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--with-openssl=#{Formula["openssl"].opt_prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system bin/"lftp", "-c", "open ftp://mirrors.kernel.org; ls"
+  end
+end
