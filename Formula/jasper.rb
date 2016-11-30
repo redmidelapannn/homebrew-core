a4526f8267169da8687682074d341899ec473211
ilovezfs <ilovezfs@icloud.com>
2016-11-30 00:41:58 -0800

    jasper 2.0.1

--- Formula/jasper.rb
+++ Formula/jasper.rb
@@ -1,8 +1,8 @@
 class Jasper < Formula
   desc "Library for manipulating JPEG-2000 images"
   homepage "https://www.ece.uvic.ca/~frodo/jasper/"
-  url "https://github.com/mdadams/jasper/archive/version-2.0.0.tar.gz"
-  sha256 "37fb86fbdc880e8ee566cf2ac226f0bfe259394914fad4d9e26bbe0764f8c378"
+  url "https://github.com/mdadams/jasper/archive/version-2.0.1.tar.gz"
+  sha256 "7cba519b98e59725bee2c6234cf75a44a28b000780d019466a13425c5c126061"
 
   bottle do
     sha256 "362a325e069e0929def99a97ffad3d7dad2ede225e5c21120c451904df14c12d" => :sierra
@@ -14,6 +14,16 @@ class Jasper < Formula
 
   depends_on "cmake" => :build
   depends_on "jpeg"
+
+  # Remove for > 2.0.1
+  # Prevent libicns build failure "jas_config.h:6:10: fatal error:
+  # 'jasper/jas_dll.h' file not found"
+  # Upstream commit from 30 Nov 2016 "Added jas_dll.h to the list of header
+  # files in src/libjasper/CMakeLists.txt"
+  patch do
+    url "https://github.com/mdadams/jasper/commit/8bdcbe0.patch"
+    sha256 "cfb7b2e363014adddb49f496313682c9f54356effa51bf74a17ef37c3e58511c"
+  end
 
   def install
     ENV.universal_binary if build.universal?
