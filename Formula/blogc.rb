8cb008a9952f30b010551d584fb2d4e46a588ebd
CJ Niemira <siege@siege.org>
2016-12-02 15:58:24 -0500

    blogc 0.12.0 (new formula)

---
+++ Formula/blogc.rb
@@ -0,0 +1,18 @@
+class Blogc < Formula
+  desc "Static blog compiler"
+  homepage "https://blogc.rgm.io"
+  url "https://github.com/blogc/blogc/releases/download/v0.12.0/blogc-#{version}.tar.bz2"
+  version "0.12.0"
+  sha256 "421474fa089024015f7dca77386c3ca360531ae4510ae50b4dc3446653a4edbe"
+
+  def install
+    system "./configure", "--enable-runserver",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "git", "clone", "https://github.com/blogc/blogc-example.git"
+    system "#{bin}/blogc", "-t", "blogc-example/templates/main.tmpl", "blogc-example/content/post/post1.txt"
+  end
+end
