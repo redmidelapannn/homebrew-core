1b5798aab82a11bcc1a6b036abb449d73dde8795
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-02 21:27:08 +0000

    speculid: add 1.0.0 bottle.

--- Formula/speculid.rb
+++ Formula/speculid.rb
@@ -5,6 +5,12 @@ class Speculid < Formula
   sha256 "929f313fd09576d4d17ea48afe8a0247fb2e788b000b5ae3bdaade71f2b2df3f"
   head "https://github.com/brightdigit/speculid.git", :branch => "develop"
 
+  bottle do
+    cellar :any
+    sha256 "846b429646e068dac516ba0ae0b97a3475108430ac70130974eeaa62cd3adada" => :sierra
+    sha256 "98b066c9e760ec471602445aeac4d4b91dee5455dbcbe0a19d10b5025bf61f37" => :el_capitan
+  end
+
   option "with-debug", "Compile Speculid with debug options enabled"
 
   depends_on "homebrew/gui/inkscape"
