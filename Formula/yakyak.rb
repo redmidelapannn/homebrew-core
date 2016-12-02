3677fadf3f925a1c18d73d403ceba49fb43cf8ca
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-02 16:19:47 +0000

    yakyak: add 1.4.0 bottle.

--- Formula/yakyak.rb
+++ Formula/yakyak.rb
@@ -7,6 +7,13 @@ class Yakyak < Formula
   sha256 "2e7c71ce4d6c1abf78e81f3d0e9470ede17d187bdd2fcf51a2b30c6e757a390e"
   head "https://github.com/yakyak/yakyak.git"
 
+  bottle do
+    cellar :any
+    sha256 "9853980ee3bab1c019794819a11e875a9dbcdebf140d61095dca114ec46b1cb7" => :sierra
+    sha256 "9853980ee3bab1c019794819a11e875a9dbcdebf140d61095dca114ec46b1cb7" => :el_capitan
+    sha256 "9853980ee3bab1c019794819a11e875a9dbcdebf140d61095dca114ec46b1cb7" => :yosemite
+  end
+
   depends_on "node"
 
   def install
