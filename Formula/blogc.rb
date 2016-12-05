972b9f62ff136728ca320fe0dc90ac1663c2d31a
Chris Niemira <chris.niemira@teamaol.com>
2016-12-05 10:16:24 -0500

    removed version param

--- Formula/blogc.rb
+++ Formula/blogc.rb
@@ -1,8 +1,7 @@
 class Blogc < Formula
   desc "Static blog compiler"
   homepage "https://blogc.rgm.io"
-  url "https://github.com/blogc/blogc/releases/download/v0.12.0/blogc-#{version}.tar.bz2"
-  version "0.12.0"
+  url "https://github.com/blogc/blogc/releases/download/v0.12.0/blogc-0.12.0.tar.bz2"
   sha256 "421474fa089024015f7dca77386c3ca360531ae4510ae50b4dc3446653a4edbe"
 
   def install
