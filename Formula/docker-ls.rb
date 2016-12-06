e3edccd5989f52d0efda0ac5ce0161c8fad02912
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-06 18:13:54 +0000

    docker-ls: add 0.2.0 bottle.

--- Formula/docker-ls.rb
+++ Formula/docker-ls.rb
@@ -7,6 +7,13 @@ class DockerLs < Formula
 
   head "https://github.com/mayflower/docker-ls.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "14f616762f9f93eefdb77a5ea2e5aa164d708ed5bfda7a794bfe0e449609651c" => :sierra
+    sha256 "804cc7cfc85d70f6d3fba3fb591d58ab741b6038a9dab704d3d72441a9181d1d" => :el_capitan
+    sha256 "c6842a1d13e640fda7faecf60c87c2e2ac4a35b19917e578bce5eabc1bfbc816" => :yosemite
+  end
+
   depends_on "go" => :build
 
   def install
