32b1fe1ecb832fa88151f5779867334a00010111
Rakesh <rakkesh@users.noreply.github.com>
2016-12-08 12:44:17 +0000

    webdis: add plist file

--- Formula/webdis.rb
+++ Formula/webdis.rb
@@ -26,6 +26,34 @@ class Webdis < Formula
     (var/"log").mkpath
   end
 
+  plist_options :manual => "webdis #{HOMEBREW_PREFIX}/etc/webdis.json"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+            <string>#{opt_bin}/webdis</string>
+            <string>#{etc}/webdis.prod.json</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <dict>
+            <key>SuccessfulExit</key>
+            <false/>
+        </dict>
+        <key>WorkingDirectory</key>
+        <string>#{var}</string>
+      </dict>
+    </plist>
+    EOS
+  end
+
   test do
     begin
       server = fork do
