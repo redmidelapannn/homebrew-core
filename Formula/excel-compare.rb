b368d887f2b45963f32e8d25f3ab1505640f075f
Josh Hagins <hagins.josh@gmail.com>
2016-12-11 10:28:44 -0500

    excel-compare: use nounzip for test resource

--- Formula/excel-compare.rb
+++ Formula/excel-compare.rb
@@ -5,7 +5,7 @@ class ExcelCompare < Formula
   sha256 "63bda982644ec8633b60eed5bc199892c428b54addb2eb63dda0b894d98a56c4"
 
   resource "sample_workbook" do
-    url "https://github.com/na-ka-na/ExcelCompare/raw/0.6.0/test/resources/ss1.xlsx"
+    url "https://github.com/na-ka-na/ExcelCompare/raw/0.6.0/test/resources/ss1.xlsx", :using => :nounzip
     sha256 "f362153aea24092e45a3d306a16a49e4faa19939f83cdcb703a215fe48cc196a"
   end
 
@@ -19,14 +19,8 @@ class ExcelCompare < Formula
   end
 
   test do
-    ss1path = testpath/"ss1"
-    resource("sample_workbook").stage ss1path
-    ss1path.cd do
-      # xlsx files are just zip archives, so Homebrew unpacks them automatically.
-      # We need to reconstruct the xlsx file before running excel_cmp
-      system "/usr/bin/zip", "-r", "ss1.xlsx", "."
-      assert_match /Excel files ss1.xlsx and ss1.xlsx match/,
-        shell_output("#{bin}/excel_cmp ss1.xlsx ss1.xlsx")
-    end
+    resource("sample_workbook").stage testpath
+    assert_match %r{Excel files #{testpath}/ss1.xlsx and #{testpath}/ss1.xlsx match},
+      shell_output("#{bin}/excel_cmp #{testpath}/ss1.xlsx #{testpath}/ss1.xlsx")
   end
 end
