cb4f4b1c10566b5d4bc80f1dc3354bedd07de5bc
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-12 16:28:26 +0000

    isl@0.11: add 0.11.1 bottle.

--- Formula/isl@0.11.rb
+++ Formula/isl@0.11.rb
@@ -1,6 +1,13 @@
 class IslAT011 < Formula
   desc "Integer Set Library for the polyhedral model"
   homepage "http://freecode.com/projects/isl"
+  bottle do
+    cellar :any
+    sha256 "b52e01c560ac6abb87dd6bad6953295374b5b947e6b08757cf704743bec283b7" => :sierra
+    sha256 "7cd0df5b49c0652fb1c0d97b4f96c7d24294c7c55c5cb1744f74344af9875b55" => :el_capitan
+    sha256 "2f169ec06b1bf91a7ce2ca96438acea4498aa746c5c8b9e2a2deeca02f7187cc" => :yosemite
+  end
+
   # Track gcc infrastructure releases.
   url "http://isl.gforge.inria.fr/isl-0.11.1.tar.bz2"
   mirror "ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-0.11.1.tar.bz2"
