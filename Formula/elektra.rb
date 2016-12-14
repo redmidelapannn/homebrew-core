1c7690456bfed0b627007c9ded92b27236a61c3d
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-14 11:04:57 +0000

    elektra: add 0.8.19 bottle.

--- Formula/elektra.rb
+++ Formula/elektra.rb
@@ -5,6 +5,12 @@ class Elektra < Formula
   sha256 "cc14f09539aa95623e884f28e8be7bd67c37550d25e08288108a54fd294fd2a8"
   head "https://github.com/ElektraInitiative/libelektra.git"
 
+  bottle do
+    sha256 "b0aa7ff5ea594bcb40b4b5699394a7a693c183e08cf93a8996e5879467f2e769" => :sierra
+    sha256 "549fc25811695ac31985b233d60c7c970c5f9aadac67532c5c9078a01f1f67d1" => :el_capitan
+    sha256 "ab9185468bbd38110c8a6ac6c9f5debde090bf368d814d6f5d497917f382de08" => :yosemite
+  end
+
   depends_on "cmake" => :build
 
   def install
