933a2ec7dcfd751976dcd4b3f892c6fe22628b49
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-14 03:20:41 +0000

    pdfbooklet: add 1.0 bottle.

--- Formula/pdfbooklet.rb
+++ Formula/pdfbooklet.rb
@@ -4,6 +4,12 @@ class Pdfbooklet < Formula
   url "https://github.com/sptim/pdfbooklet/archive/v1.0.tar.gz"
   sha256 "7cb3dfb4c380d23a2e2e8ea866af91a0bdc6dc83dd9b03f118a06225e13a485d"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a2c8bad8e7f1c9814c8aa6ab0e503c932a6ccb8c2c8fd852d5aa17be4211a478" => :sierra
+    sha256 "cdcc2e7eedd4f4ed1f5864d1e20d59f00bb74c9453463258dc970c9a47139d65" => :el_capitan
+  end
+
   depends_on :xcode => :build
 
   def install
