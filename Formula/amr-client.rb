481000d8505d3a685f9ba076e4688faeeab7cb21
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2016-12-16 18:08:57 -0600

    amr-client 159 (new formula)
    I created Formula for Ask Mr. Robot's Simulation client.  This was mostly a learning experience and I am not affiliated with AMR.

---
+++ Formula/amr-client.rb
@@ -0,0 +1,19 @@
+class AmrClient < Formula
+  desc "Ask Mr. Robot Simulator Client"
+  homepage "http://www.askmrrobot.com/"
+  url "https://static2.askmrrobot.com/amrbeta/client/AskMrRobotClient-159-osx.10.11-x64.zip"
+  version "159"
+  sha256 "53346959796c7f25038d17eccd5f21a56bbadd9d80f0c90387bcce01db8addd5"
+
+  def install
+    chmod 0755, "updater/amrupdate"
+    chmod 0755, "client/amr"
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"updater/amrupdate"
+    bin.install_symlink libexec/"client/amr"
+  end
+
+  test do
+    system "true"
+  end
+end
