2496702555c58110d27d4922a98ce52002454311
JMoVS <git@justinscholz.de>
2016-12-24 19:49:36 +0100

    preparing for release

--- Formula/handbrake.rb
+++ Formula/handbrake.rb
@@ -1,8 +1,7 @@
 class Handbrake < Formula
   desc "Open-source video transcoder available for Linux, Mac, and Windows."
   homepage "https://handbrake.fr/"
-  url "https://github.com/HandBrake/HandBrake.git", :revision => "d6e2e09ee7c00d9ed371d0a9b16ca87b6ee417e1"
-  version "1.0.0-alpha1"
+  url "https://github.com/HandBrake/HandBrake.git", :revision => "3f4a076243d2821ae78bc97aaed0ca47301d0d65", :tag => "1.0.0"
   head "https://github.com/HandBrake/HandBrake.git"
 
   depends_on "autoconf" => :build
