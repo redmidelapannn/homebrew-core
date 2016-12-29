818819a01523ef9819eec85ec12fa5e7cb53fde6
BrewTestBot <brew-test-bot@googlegroups.com>
2016-12-29 19:16:57 +0000

    kawa: add 2.2 bottle.

--- Formula/kawa.rb
+++ Formula/kawa.rb
@@ -5,6 +5,14 @@ class Kawa < Formula
   mirror "https://ftp.gnu.org/gnu/kawa/kawa-2.2.zip"
   sha256 "eb2b8301786d5e983769c2e29ba7caf69afcb14977eac16d544471b9a3c79f79"
 
+  bottle do
+    cellar :any_skip_relocation
+    rebuild 1
+    sha256 "11fc6035e8d804b8a685e2d2a75e9841d729201f8e4c703cf97147312a5ff62a" => :sierra
+    sha256 "0adc35e64ba9641ee686b7cbf2e1adddc8eb9b5a19b08b6b330e236f2749ddba" => :el_capitan
+    sha256 "0adc35e64ba9641ee686b7cbf2e1adddc8eb9b5a19b08b6b330e236f2749ddba" => :yosemite
+  end
+
   depends_on :java
 
   def install
