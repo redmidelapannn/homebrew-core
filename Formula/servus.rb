c8abefcc3dee75022912ecda973f63e4829ec00a
Elliot Saba <staticfloat@gmail.com>
2016-12-31 15:08:47 -0800

    servus: Embed "serializeable" test source

--- Formula/servus.rb
+++ Formula/servus.rb
@@ -10,16 +10,101 @@ class Servus < Formula
   def install
     system "cmake", ".", *std_cmake_args
     system "make", "install"
-
-    # Install tests to share/Servus/tests so that we can test later
-    mkdir share/"Servus/tests"
-    (share/"Servus/tests").install Dir["tests/{serializable,servus,uint128_t,uri}"]
   end
 
   test do
-    system share/"Servus/tests/serializable"
-    system share/"Servus/tests/servus"
-    system share/"Servus/tests/uint128_t"
-    system share/"Servus/tests/uri"
+    # Embed "serializeable" test from the servus 1.5.0 source
+    (testpath/"test.cpp").write <<-EOS.undent
+#define BOOST_TEST_MODULE servus_serializable
+#include <boost/test/unit_test.hpp>
+
+#include <servus/uint128_t.h>
+#include <servus/serializable.h>
+
+void dummyFunction(){}
+
+class SerializableObject : public servus::Serializable
+{
+public:
+    std::string getTypeName() const final { return "test::serializable"; }
+
+    servus::uint128_t getTypeIdentifier() const final
+    {
+        return servus::make_uint128( getTypeName( ));
+    }
+
+private:
+    bool _fromBinary( const void*, const size_t ) { return true; }
+    bool _fromJSON( const std::string& ) { return true; }
+};
+
+
+BOOST_AUTO_TEST_CASE(serializable_types)
+{
+    SerializableObject obj;
+    BOOST_CHECK_EQUAL( obj.getTypeName(), "test::serializable" );
+    BOOST_CHECK_EQUAL( servus::make_uint128( obj.getTypeName( )),
+                       obj.getTypeIdentifier( ));
+    BOOST_CHECK_EQUAL( obj.getSchema(), std::string( ));
+}
+
+BOOST_AUTO_TEST_CASE(serializable_registerSerialize)
+{
+    SerializableObject obj;
+    servus::Serializable::SerializeCallback callback( dummyFunction );
+
+    obj.registerSerializeCallback( callback );
+    BOOST_CHECK_THROW( obj.registerSerializeCallback( callback ),
+                       std::runtime_error ); // callback already registered
+
+    BOOST_CHECK_NO_THROW( obj.registerSerializeCallback(
+        servus::Serializable::SerializeCallback( )));
+    BOOST_CHECK_NO_THROW( obj.registerSerializeCallback( callback ));
+
+    BOOST_CHECK_THROW( obj.registerSerializeCallback( callback ),
+                       std::runtime_error ); // callback already registered
+}
+
+BOOST_AUTO_TEST_CASE(serializable_registerDeserialized)
+{
+    SerializableObject obj;
+    servus::Serializable::DeserializedCallback callback( dummyFunction );
+
+    obj.registerDeserializedCallback( callback );
+    BOOST_CHECK_THROW( obj.registerDeserializedCallback( callback ),
+                       std::runtime_error ); // callback already registered
+
+    BOOST_CHECK_NO_THROW( obj.registerDeserializedCallback(
+        servus::Serializable::DeserializedCallback( )));
+    BOOST_CHECK_NO_THROW( obj.registerDeserializedCallback( callback ));
+
+    BOOST_CHECK_THROW( obj.registerDeserializedCallback( callback ),
+                       std::runtime_error ); // callback already registered
+}
+
+BOOST_AUTO_TEST_CASE(serializable_binary)
+{
+    SerializableObject obj;
+
+    // fromBinary implemented
+    BOOST_CHECK_NO_THROW( obj.fromBinary( new float[3], 3 ));
+
+    // default toBinary (unimplemented)
+    BOOST_CHECK_THROW( obj.toBinary(), std::runtime_error );
+}
+
+BOOST_AUTO_TEST_CASE(serializable_json)
+{
+    SerializableObject obj;
+
+    // fromJson implemented
+    BOOST_CHECK_NO_THROW( obj.fromJSON( std::string( "testing..." )));
+
+    // default toJson (unimplemented)
+    BOOST_CHECK_THROW( obj.toJSON(), std::runtime_error );
+}
+    EOS
+    system ENV.cxx, "-lServus", "-DBOOST_TEST_DYN_LINK", "-lboost_unit_test_framework-mt", "-std=gnu++11", "test.cpp", "-o", "test"
+    system "./test"
   end
 end
