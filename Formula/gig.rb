022fb536af97fcbbd6f643d997b4a4c7377c0989
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-03 04:36:31 +0000

    gig: add 0.0.3 bottle.

--- Formula/gig.rb
+++ Formula/gig.rb
@@ -4,6 +4,13 @@ class Gig < Formula
   url "https://github.com/Horta/gig/archive/v0.0.3.tar.gz"
   sha256 "0289839b01536bdaeba8cef5cd61a5ff07ffd7b0776dccd60d9a4c5395fc2387"
 
+  bottle do
+    cellar :any
+    sha256 "8cf1c6069a22672c0b16000a7ec0f57533a708fe90486760037b670872810936" => :sierra
+    sha256 "59dcd872091bea5a1792f2d09e30be64c30415382cd0c39a5d68e0283e825347" => :el_capitan
+    sha256 "98a43fb33773b76166ab1c2bb1e00bfd264ebf1d406606841c844e670ea2a003" => :yosemite
+  end
+
   depends_on "cmake" => :build
 
   def install
