5f1cdb5970949611538bfb1f294e62d3f80a1254
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-04 04:39:23 +0000

    stathat: add 0.2.0 bottle.

--- Formula/stathat.rb
+++ Formula/stathat.rb
@@ -6,6 +6,12 @@ class Stathat < Formula
 
   head "https://github.com/stathat/cmd.git"
 
+  bottle do
+    sha256 "d24235651a0584eb84c1ef7a14db1e5de77341bfb0dc28c6e7e5181956c5a5f3" => :sierra
+    sha256 "d8165a8fc0c69b3d80759a0148b97fd6efd1505cb15b7cfeff6c3978c31ff598" => :el_capitan
+    sha256 "fe132d054526faa5d6c40311c5961bf265e7c5ad5d4edf54d735e04c2004fca3" => :yosemite
+  end
+
   depends_on "go" => :build
 
   def install
