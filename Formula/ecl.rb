aad74cb1ea7b4b3b9282e3d0c30f0392cad312f6
MichaelJFox <mfox@cavium.com>
2017-01-04 18:21:11 -0800

    ecl 1.16.3
    Seems to work. One problem is `make check` doesn't work but doesn't
    fail. Still better than the previous formula which results in a compiler
    that doesn't compile.

--- Formula/ecl.rb
+++ Formula/ecl.rb
@@ -1,26 +1,49 @@
 class Ecl < Formula
   desc "Embeddable Common Lisp"
   homepage "https://common-lisp.net/project/ecl/"
-  url "https://common-lisp.net/project/ecl/static/files/release/ecl-16.1.2.tgz"
-  sha256 "2d482b1a0a4fbd5d881434517032279d808cb6405e22dd91ef6d733534464b99"
+  url "https://common-lisp.net/project/ecl/static/files/release/ecl-16.1.3.tgz"
+  sha256 "76a585c616e8fa83a6b7209325a309da5bc0ca68e0658f396f49955638111254"
 
   head "https://gitlab.com/embeddable-common-lisp/ecl.git"
 
   bottle do
-    sha256 "3ff5a197a14d03d9e8c5083289506439f473685db7fe15b27f2659652c8165b9" => :sierra
+    sha256 "777aa2584459020d3f4d42488b60a06a0857158a1ee39e14754695fd4eaec139" => :sierra
     sha256 "8915d3c5862aa5b89beb28119778715308f6639abde6fcefe052f716c3db9560" => :el_capitan
     sha256 "46647c3577257ff30197afe689161d36a8bd8e99a2b24eaa44f97f2f38e644b1" => :yosemite
     sha256 "5fa6a6a6f0ac717897ed635484a4b1675a48b8455e6178990bbce5109353131d" => :mavericks
   end
 
   depends_on "gmp"
+  depends_on "bdw-gc"
 
   def install
-    system "./configure", "--prefix=#{prefix}",
+    # These environment variables are necessary or gmp / boehmgc won't be found
+    # No idea why
+    gmp = Formula["gmp"]
+    boehmgc = Formula["boehmgc"]
+    ENV.append "CFLAGS", "-I#{gmp.opt_include} -I#{boehmgc.opt_include}"
+    ENV.append "CXXFLAGS", "-I#{gmp.opt_include} -I#{boehmgc.opt_include}"
+    ENV.append "LDFLAGS", "-L#{gmp.opt_lib} -L#{boehmgc.opt_lib}"
+
+    system "./configure",
+                          "--prefix=#{prefix}",
                           "--enable-threads=yes",
-                          "--with-system-gmp=yes"
-    system "make"
+                          "--with-dffi=included",
+                          "--with-cxx",
+                          "--enable-gmp=system"
+    system "make", "-j", "1"
     system "make", "install"
+    # make check
+    # Doesn't work but doesn't fail the build
+    # FIXME
+    #
+    # ==> make check
+    # cd build && /Applications/Xcode.app/Contents/Developer/usr/bin/make check
+    # TESTS=""
+    # dyld: Library not loaded: @libdir@/libecl.16.1.dylib
+    #   Referenced from: /usr/local/Cellar/ecl/16.1.3//bin/ecl
+    #     Reason: image not found
+    system "make", "check"
   end
 
   test do
