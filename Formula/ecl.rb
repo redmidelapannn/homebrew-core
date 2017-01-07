7e6db702be807385d3aee25ca227afc9b1afadb5
MichaelJFox <mfox@cavium.com>
2017-01-07 06:36:37 -0800

    ecl 16.1.3
    * ENV.deparallelize
    * Revert bottle hash
    * Link issues

--- Formula/ecl.rb
+++ Formula/ecl.rb
@@ -7,7 +7,7 @@ class Ecl < Formula
   head "https://gitlab.com/embeddable-common-lisp/ecl.git"
 
   bottle do
-    sha256 "777aa2584459020d3f4d42488b60a06a0857158a1ee39e14754695fd4eaec139" => :sierra
+    sha256 "3ff5a197a14d03d9e8c5083289506439f473685db7fe15b27f2659652c8165b9" => :sierra
     sha256 "8915d3c5862aa5b89beb28119778715308f6639abde6fcefe052f716c3db9560" => :el_capitan
     sha256 "46647c3577257ff30197afe689161d36a8bd8e99a2b24eaa44f97f2f38e644b1" => :yosemite
     sha256 "5fa6a6a6f0ac717897ed635484a4b1675a48b8455e6178990bbce5109353131d" => :mavericks
@@ -25,16 +25,40 @@ class Ecl < Formula
     ENV.append "CXXFLAGS", "-I#{gmp.opt_include} -I#{boehmgc.opt_include}"
     ENV.append "LDFLAGS", "-L#{gmp.opt_lib} -L#{boehmgc.opt_lib}"
 
+    # FIXME: Remove deparallelize after:
+    # https://gitlab.com/embeddable-common-lisp/ecl/issues/339
+    #
+    # @dkochmanski notes:
+    #
+    # This branch is not tested and may break some unusual builds. Parallels
+    # are the only (known and confirmed) issue with the build scripts in the
+    # release, while the recent changes are not widely tested and may break
+    # some unusual builds. If you are interested in using it, here is the
+    # mentioned branch:
+    #
+    # https://gitlab.com/embeddable-common-lisp/ecl/tree/revert-make-split
+    ENV.deparallelize
+
     system "./configure",
+      # @MikeMcQuaid requested that I do it like this rather than ENV.append,
+      # above but it breaks the build.
+      # Really should be able to find depends_on libraries without further ado
+      # but here we are.
+                          # "CFLAGS=-I'#{gmp.opt_include} -I#{boehmgc.opt_include}'",
+                          # "CXXFLAGS=-I'#{gmp.opt_include} -I#{boehmgc.opt_include}'",
+                          # "LDFLAGS=-L'#{gmp.opt_lib} -L#{boehmgc.opt_lib}'",
                           "--prefix=#{prefix}",
                           "--enable-threads=yes",
                           "--with-dffi=included",
                           "--with-cxx",
                           "--enable-gmp=system"
-    system "make", "-j", "1"
+    system "make"
     system "make", "install"
     # make check
     # Doesn't work but doesn't fail the build
+    # But the real issue is 'make check' should be able to run before 'make
+    # install'
+    # https://gitlab.com/embeddable-common-lisp/ecl/issues/342
     # FIXME
     #
     # ==> make check
@@ -46,6 +70,8 @@ class Ecl < Formula
     system "make", "check"
   end
 
+  # FIXME
+  # Should be 'make check', but see above
   test do
     (testpath/"simple.cl").write <<-EOS.undent
       (write-line (write-to-string (+ 2 2)))
