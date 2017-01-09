a14503784fca793e5de83dc90673f947dae22a8e
ArchiFleKs <lefevre.kevin@gmail.com>
2017-01-09 15:45:43 +0100

    add devel

--- Formula/kube-aws.rb
+++ Formula/kube-aws.rb
@@ -35,7 +35,7 @@ class KubeAws < Formula
     ENV["GOPATH"] = "#{gopath_vendor}:#{gopath_kube_aws}"
 
     cd kube_aws_dir do
-      system "go", "generate", ".config"
+      system "go", "generate", "./config"
       system "go", "build", "-ldflags",
              "-X github.com/coreos/coreos-kubernetes/kube-aws/cluster/cluster.VERSION=#{version}",
              "-a", "-tags", "netgo", "-installsuffix", "netgo",
