51d53da74b699d39f77a7422769398a8542d6caa
Gautam Korlam <gkorlam@uber.com>
2017-01-11 17:16:05 -0800

    update to 1.1.0

--- Formula/redex.rb
+++ Formula/redex.rb
@@ -1,21 +1,18 @@
 class Redex < Formula
   desc "Bytecode optimizer for Android apps"
   homepage "http://fbredex.com"
-  url "https://github.com/facebook/redex/archive/v1.0.0.tar.gz"
-  sha256 "58a41e7fb224c74438431d579f3ffa0ea7758d07730361d8a06d2c9e75d803a2"
+  url "https://github.com/facebook/redex/archive/v1.1.0.tar.gz"
+  sha256 "af2c81db4e0346e1aeef570e105c60ebfea730d62fd928d996f884abda955990"
 
   head "https://github.com/facebook/redex.git"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
-  depends_on "libtool" => :build
-  depends_on "python3" => :build
-  depends_on "boost" => :build
-  depends_on "double-conversion" => :build
-  depends_on "gflags" => :build
-  depends_on "glog" => :build
   depends_on "libevent" => :build
-  depends_on "jsoncpp" => :build
+  depends_on "libtool" => :build
+  depends_on "boost" => :run
+  depends_on "python3" => :run
+  depends_on "jsoncpp" => :run
 
   def install
     system "autoreconf", "-ivf"
