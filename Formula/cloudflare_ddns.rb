596f2e32458279128f330bc0b2212d1e5c3f5fd6
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-12 22:26:11 +0000

    cloudflare_ddns: add 1.0.1 bottle.

--- Formula/cloudflare_ddns.rb
+++ Formula/cloudflare_ddns.rb
@@ -5,6 +5,12 @@ class CloudflareDdns < Formula
   sha256 "e13ab6c203eb646ebb23384c0136f469b8e72b05d31f43f288ff9671e601809c"
   head "https://github.com/wyattjoh/cloudflare-ddns.git"
 
+  bottle do
+    sha256 "087155e33698e48e0b6e128fef162bacb632a16f761cf765a1b45645c1183e97" => :sierra
+    sha256 "7ffe173b78524355cb630ff780c864ae864dd34db5667d724524d0ac17eb7035" => :el_capitan
+    sha256 "4fc2aa0480ba51d853b9404163337303450939d55c2414c1ff23205bae7975b0" => :yosemite
+  end
+
   depends_on "go" => :build
 
   def install
