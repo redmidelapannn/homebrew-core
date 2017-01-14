ab889f4753bea17242856dfef6b9f30199a65f37
Geert Bevin <gbevin@uwyn.com>
2017-01-14 23:28:39 +0100

    sendmidi 1.0.2 (new formula)

---
+++ Formula/sendmidi.rb
@@ -0,0 +1,17 @@
+class Sendmidi < Formula
+  desc "Multi-platform command-line tool to send out MIDI messages"
+  homepage "https://github.com/gbevin/SendMIDI"
+  url "https://github.com/gbevin/SendMIDI/archive/1.0.2.tar.gz"
+  sha256 "46ab36516b6d4ba022c4b57aa35c384ccd491c99993464e56bc317bc4748330b"
+
+  depends_on :xcode => :build
+
+  def install
+    xcodebuild "-project", "./Builds/MacOSX/sendmidi.xcodeproj", "-configuration", "Release", "SYMROOT=build"
+    bin.install "./Builds/MacOSX/build/Release/sendmidi"
+  end
+
+  test do
+    assert_match /Usage: sendmidi/, shell_output("#{bin}/sendmidi")
+  end
+end
