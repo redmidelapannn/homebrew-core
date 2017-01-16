65a7f32e1f80215b2f1a9e1db2709701c9b15f5e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-16 06:37:34 +0000

    jpeg: update 9b bottle.

--- Formula/jpeg.rb
+++ Formula/jpeg.rb
@@ -6,13 +6,9 @@ class Jpeg < Formula
 
   bottle do
     cellar :any
-    rebuild 2
-    sha256 "85d1f6055de99c1b764c697498bb86d003f5d24b324133f036f0393b97b3e869" => :sierra
-    sha256 "bbc74f8b5980065d7bf95927150c2d56806d30abea459c2b1edcbdeed2d7c458" => :el_capitan
-    sha256 "75419be793eefb3decd69de8ee0444ef697aef3d0f942361437c5d967fd8ecec" => :yosemite
-    sha256 "c94f5859603b34d9b3c1b021ee21cf3a41a5d92b178a41ed5c05a1e5b7d19ff5" => :mavericks
-    sha256 "816f0f826e633cb70f8efc173b6fe745fb9b90334d6cb78813de25f48aec6191" => :mountain_lion
-    sha256 "f94b7976c0e867dfc36ff6cead1d88db8b1198a91a01589e008d2006fcf37c61" => :lion
+    sha256 "db4a192710ffa041956744ac446006842464cccb1bda2f3c1f99e6d5985c91e8" => :sierra
+    sha256 "99fbe9664c4471ef6298e6997f22cfc827673a5a7a3ccea4918ba0131f158754" => :el_capitan
+    sha256 "6c6896daee4640b1e14bdb40e9ee216366cf0ea73e61f7a4e57573f7a52c0cca" => :yosemite
   end
 
   option :universal
