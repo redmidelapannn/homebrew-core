df4cdec00d87096e1c57ad85fe1958abeb40ae82
Philippe Hässig <phil@nine.ch>
2017-01-16 14:43:30 +0100

    Building sources of source-to-image

--- Formula/source-to-image.rb
+++ Formula/source-to-image.rb
@@ -1,11 +1,16 @@
 class SourceToImage < Formula
   desc "Tool for building source and injecting into docker images"
   homepage "https://github.com/openshift/source-to-image"
-  url "https://github.com/openshift/source-to-image/releases/download/v1.1.3/source-to-image-v1.1.3-ddb10f1-darwin-amd64.tar.gz"
-  sha256 "b5d179e1da3e5e3d4b206a767c69bc6d5860d82227a475ecc17c4279bfb0f862"
+  url "https://github.com/openshift/source-to-image.git",
+    :tag => "v1.1.3",
+    :revision => "ddb10f16412977688136b77c76c30c0632e510d6"
+  head "https://github.com/openshift/source-to-image.git"
+
+  depends_on "go" => :build
 
   def install
-    bin.install "s2i"
+    system "hack/build-go.sh"
+    bin.install "_output/local/bin/darwin/amd64/s2i"
   end
 
   test do
