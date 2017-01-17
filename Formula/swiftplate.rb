1bc8076be959e74577f884199d4fc4aa20b67084
codeOfRobin <me@rmalhotra.com>
2017-01-18 01:52:39 +0530

    Removed :tag, split test into multiple lines, rewrote tests in ruby

--- Formula/swiftplate.rb
+++ Formula/swiftplate.rb
@@ -1,8 +1,9 @@
+require "set"
+
 class Swiftplate < Formula
   desc "Cross-platform Swift framework templates from the command-line"
   homepage "https://github.com/JohnSundell/SwiftPlate"
-  url "https://github.com/JohnSundell/SwiftPlate/archive/1.2.1.tar.gz",
-    :tag => "1.2.1"
+  url "https://github.com/JohnSundell/SwiftPlate/archive/1.2.1.tar.gz"
   sha256 "c4a47411340c88dbacc87d63daf69eed12c80cbc2e1fc8c02cc8d6b2966c04b9"
   head "https://github.com/JohnSundell/SwiftPlate.git"
   depends_on :xcode => "8.2"
@@ -18,42 +19,36 @@ class Swiftplate < Formula
   end
 
   test do
-    system "#{bin}/swiftplate", "--destination", ".", "--project", "testProject", "--name", "testUser", "--email", "test@example.com", "--url", "https://github.com/johnsundell/swiftplate", "--organization", "exampleOrg", "--force"
+    project_name = "test"
 
-    (testpath/"test.py").write <<-EOS.undent
-        # -*- coding: utf-8 -*-
-        import sys
-        import os
-        from os.path import join, getsize
+    system "#{bin}/swiftplate", "--destination", ".",
+      "--project", project_name, "--name", "testUser",
+      "--email", "test@example.com", "--url", "https://github.com/johnsundell/swiftplate",
+      "--organization", "exampleOrg", "--force"
 
-        rootFiles = set(['LICENSE', 'Package.swift', 'README.md', sys.argv[1] + '.podspec'])
-        configFiles = set([sys.argv[1] + '.plist', sys.argv[1] + 'Tests.plist'])
-        sourceFiles = set([sys.argv[1]+ '.swift'])
-        testDirs = set([sys.argv[1] + 'Tests'])
+    root_files = Set.new ["LICENSE", "Package.swift", "README.md", project_name + ".podspec"]
+    config_files = Set.new [project_name + ".plist", project_name + "Tests.plist"]
+    source_files = Set.new [project_name + ".swift"]
+    test_dirs = Set.new [project_name + "Tests"]
 
-        for root, dirs, files in os.walk('.'):
-            if root == '.':
-                if sys.argv[1] + '.xcodeproj' not in dirs:
-                    print("xcode project not found")
-                    exit(1)
-                if not rootFiles.issubset(files):
-                    print("root level files not found")
-                    exit(1)
-            if root == './Configs':
-                if not configFiles.issubset(files):
-                    print("Config files not found")
-                    exit(1)
-            if root == './Sources':
-                if not sourceFiles.issubset(files):
-                    print("Source files not found")
-                    exit(1)
-            if root == './Tests':
-                if not testDirs.issubset(dirs):
-                    print("Test Directories not found")
-                    exit(1)
-        print("All tests run successfully ⭐⭐⭐⭐⭐")
-        exit(0)
-    EOS
-    system "python", "test.py", "testProject"
+    def die(directory)
+      abort("directory structure of " + directory + " doesn't match expected results")
+    end
+
+    unless root_files.subset? Dir.glob("*").to_set
+      die("root")
+    end
+
+    unless config_files.subset? Dir["Configs/*"].map { |x| File.basename(x) }.to_set
+      die("Configs")
+    end
+
+    unless source_files.subset? Dir["Sources/*"].map { |x| File.basename(x) }.to_set
+      die("Sources")
+    end
+
+    unless test_dirs.subset? Dir["Tests/*"].map { |x| File.basename(x) }.to_set
+      die("Tests")
+    end
   end
 end
