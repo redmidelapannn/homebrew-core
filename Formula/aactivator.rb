13c72bfe2969810dc085e8b50c889c8b3cf83864
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-17 04:53:38 +0000

    aactivator: add 1.0.0 bottle.

--- Formula/aactivator.rb
+++ Formula/aactivator.rb
@@ -6,6 +6,13 @@ class Aactivator < Formula
   url "https://github.com/Yelp/aactivator/archive/v1.0.0.tar.gz"
   sha256 "395430f9dbabd2644a030d534761899165e2d5c8cb5aa71bf620808543cdfb02"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8ea513df5538b0cef53b212392ae6dd182f88f61d4a42c53773304ee24a66f1d" => :sierra
+    sha256 "34705b90d1862b621c4bcbbcf52b5e563a3bc656b5b21c80a275a53a92134f93" => :el_capitan
+    sha256 "80797b7cf75f3bbf72dbe6962c26d9a5e9d05ab0e70f56ec1a1c48035a98f2c0" => :yosemite
+  end
+
   depends_on :python if MacOS.version <= :snow_leopard
 
   def install
