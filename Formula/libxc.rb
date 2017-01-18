89c74089d496e39a7c71e2fd0895ca4a344475a0
FX Coudert <fxcoudert@gmail.com>
2017-01-18 15:57:51 +0100

    libxc 3.0.0

--- Formula/libxc.rb
+++ Formula/libxc.rb
@@ -1,9 +1,8 @@
 class Libxc < Formula
   desc "Library of exchange and correlation functionals for codes"
   homepage "http://www.tddft.org/programs/octopus/wiki/index.php/Libxc"
-  url "http://www.tddft.org/programs/octopus/down.php?file=libxc/libxc-2.2.2.tar.gz"
-  sha256 "6ca1d0bb5fdc341d59960707bc67f23ad54de8a6018e19e02eee2b16ea7cc642"
-  revision 2
+  url "http://www.tddft.org/programs/octopus/down.php?file=libxc/libxc-3.0.0.tar.gz"
+  sha256 "5542b99042c09b2925f2e3700d769cda4fb411b476d446c833ea28c6bfa8792a"
 
   bottle do
     cellar :any
