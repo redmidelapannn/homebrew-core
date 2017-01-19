d5389e5d84b9b64ced992df154f229d823974fc4
codeOfRobin <me@rmalhotra.com>
2017-01-19 17:56:00 +0530

    Fixes in code style

--- Formula/swiftplate.rb
+++ Formula/swiftplate.rb
@@ -6,7 +6,7 @@ class Swiftplate < Formula
   url "https://github.com/JohnSundell/SwiftPlate/archive/1.2.1.tar.gz"
   sha256 "c4a47411340c88dbacc87d63daf69eed12c80cbc2e1fc8c02cc8d6b2966c04b9"
   head "https://github.com/JohnSundell/SwiftPlate.git"
-  
+
   depends_on :xcode => "8.2"
 
   def install
@@ -21,7 +21,7 @@ class Swiftplate < Formula
 
   test do
     project_name = "test"
-    
+
     system "#{bin}/swiftplate", "--destination", ".",
       "--project", project_name, "--name", "testUser",
       "--email", "test@example.com", "--url", "https://github.com/johnsundell/swiftplate",
@@ -31,14 +31,14 @@ class Swiftplate < Formula
       "." => ["LICENSE", "Package.swift", "README.md", "#{project_name}.podspec"],
       "Configs" => ["#{project_name}.plist", "#{project_name}Tests.plist"],
       "Sources" => ["#{project_name}.swift"],
-      "Tests" => ["#{project_name}Tests"]
+      "Tests" => ["#{project_name}Tests"],
     }
 
-    required_files.each { |dir_name, expected|
-      actual = Dir.entries(dir_name) 
+    required_files.each do |dir_name, expected|
+      actual = Dir.entries(dir_name)
       unless (expected - actual).empty?
         abort("directory structure of #{dir_name} doesn't match expected results - #{files}, #{actual}")
       end
-    }
+    end
   end
 end
