a68cbf1b8d61c62951a5d8cb3f5489e2f2c91730
HazCod <HazCod@users.noreply.github.com>
2017-01-20 15:33:05 +0100

    foo 2.3.4 (new formula)
    Obligatory commit msg

--- Formula/maclaunch.rb
+++ Formula/maclaunch.rb
@@ -5,11 +5,13 @@ class Maclaunch < Formula
   sha256 "1a2c7f62cb5331df9804c481fecc41e29b4dff29"
 
   def install
+    # for clarity, remove .sh
     mv "maclaunch.sh", "maclaunch"
     bin.install "maclaunch"
   end
 
   test do
+    # just run the command to test the install
     system "#{bin}/maclaunch"
   end
 end
