942621a10909b5e2f67b4a631f601b484752e601
HazCod <HazCod@users.noreply.github.com>
2017-01-20 15:31:17 +0100

    Update maclaunch.rb
    Update to 0.2 release

--- Formula/maclaunch.rb
+++ Formula/maclaunch.rb
@@ -1,8 +1,8 @@
 class Maclaunch < Formula
   desc "Command-line utility for managing your launch agents and daemons."
   homepage "https://github.com/HazCod/maclaunch"
-  url "https://github.com/HazCod/maclaunch/archive/0.1.tar.gz"
-  sha256 "f7aee097e3871f72cb38e1f04bf0a586f8371f984a71e212cf210b56df2cebb2"
+  url "https://github.com/HazCod/maclaunch/archive/0.2.tar.gz"
+  sha256 "1a2c7f62cb5331df9804c481fecc41e29b4dff29"
 
   def install
     mv "maclaunch.sh", "maclaunch"
