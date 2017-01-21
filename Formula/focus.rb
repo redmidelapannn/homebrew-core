79e03fd9840aa7257c2a223345c22ab19dc93071
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-21 18:47:13 +0000

    focus: add 1.0 bottle.

--- Formula/focus.rb
+++ Formula/focus.rb
@@ -4,6 +4,12 @@ class Focus < Formula
   url "https://github.com/lfaoro/focus/archive/v1.0.tar.gz"
   sha256 "ecfa00683bc47eebd6f8603a442c3d1017170ec9b5721de19e682612b27bb52a"
 
+  bottle do
+    sha256 "535e161d9d42e8a4ad1a30c2fede3af9c2f6de694e26067f645d2df655ad0cb3" => :sierra
+    sha256 "3db3465ad1a2059eb956e99f9711a9526173dda097613e34517270fb530c4bba" => :el_capitan
+    sha256 "bf4e32687ca1b893a25c924c6825534d16ebe90067994de388e1f87de3de2b01" => :yosemite
+  end
+
   depends_on "go" => :build
 
   def install
