711386899e58eb33bb396852563434f9b47cdda4
Leonardo Faoro <lfaoro@me.com>
2017-01-21 19:09:52 +0100

    removed redundant version from focus.rb formula

--- Formula/focus.rb
+++ Formula/focus.rb
@@ -2,7 +2,6 @@ class Focus < Formula
   desc "CLI implementation of the Pomodoro technique."
   homepage "https://github.com/lfaoro/focus"
   url "https://github.com/lfaoro/focus/archive/v1.0.tar.gz"
-  version "1.0"
   sha256 "ecfa00683bc47eebd6f8603a442c3d1017170ec9b5721de19e682612b27bb52a"
 
   depends_on "go" => :build
