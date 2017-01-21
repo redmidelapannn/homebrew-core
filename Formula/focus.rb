7fbbfb101fd8c90bd2879c214a71f28c9e9c0b81
Leonardo Faoro <lfaoro@me.com>
2017-01-21 18:41:12 +0100

    added new focus formula
    CLI implementation of the Pomodoro technique.

---
+++ Formula/focus.rb
@@ -0,0 +1,22 @@
+class Focus < Formula
+  desc "CLI implementation of the Pomodoro technique."
+  homepage "https://github.com/lfaoro/focus"
+  url "https://github.com/lfaoro/focus/archive/v1.0.tar.gz"
+  version "1.0"
+  sha256 "ecfa00683bc47eebd6f8603a442c3d1017170ec9b5721de19e682612b27bb52a"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    focus_path = buildpath/"src/github.com/lfaoro/focus/"
+    focus_path.install Dir["{*,.git}"]
+    cd "src/github.com/lfaoro/focus/" do
+      system "go", "build", "-o", bin/"focus", "-v"
+    end
+  end
+
+  test do
+    system "#{bin}/focus", "-v"
+  end
+end
