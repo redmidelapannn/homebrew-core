7451cc9078544f9b69147c3ad2cc52787570e94b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-01-23 18:12:38 +0000

    ydk: add 0.5.2 bottle.

--- Formula/ydk.rb
+++ Formula/ydk.rb
@@ -4,6 +4,13 @@ class Ydk < Formula
   url "https://github.com/CiscoDevNet/ydk-cpp/archive/0.5.2.tar.gz"
   sha256 "39ca26b57e0d784243ebd0c07eb0e35fc0ad8600886fde2be4440eae898b844d"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a2b68f0f824c1dfe8af5325ecb0f718c5af33f807d6e2f1a168f96017aca4329" => :sierra
+    sha256 "3ee533df6b2898aa7fe0737668d34f8af3acc077ab6da05fa95732bbe0a5b1e4" => :el_capitan
+    sha256 "73dd2953791b049238b36661ef3f79fd34c5f82b3efa28b96e35105a45622019" => :yosemite
+  end
+
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "boost-python"
