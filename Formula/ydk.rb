8a414837dcceeb750a26bd104e756880c71e21a4
lily <ylil93@gmail.com>
2017-01-23 10:01:22 -0800

    homepage change, separate test into multiple lines

--- Formula/ydk.rb
+++ Formula/ydk.rb
@@ -1,6 +1,6 @@
 class Ydk < Formula
   desc "generate API bindings to YANG data models"
-  homepage "https://github.com/abhikeshav/ydk-cpp/blob/master/README.md"
+  homepage "https://github.com/CiscoDevNet/ydk-cpp"
   url "https://github.com/CiscoDevNet/ydk-cpp/archive/0.5.2.tar.gz"
   sha256 "39ca26b57e0d784243ebd0c07eb0e35fc0ad8600886fde2be4440eae898b844d"
 
@@ -32,7 +32,12 @@ class Ydk < Formula
         return 0;
       }
     EOS
-    system ENV.cxx, "-std=c++11", "-Wall", "-Wextra", "-g", "-O0", "test.cpp", "-otest", "-lboost_log_setup-mt", "-lboost_log-mt", "-lboost_thread-mt", "-lboost_date_time-mt", "-lboost_system-mt", "-lboost_filesystem-mt", "-lboost_log_setup-mt", "-lboost_log-mt", "-lboost_thread-mt", "-lboost_date_time-mt", "-lboost_system-mt", "-lboost_filesystem-mt", "-lboost_log_setup-mt", "-lboost_log-mt", "-lboost_filesystem-mt", "-lboost_system-mt", "-lxml2", "-lcurl", "-lssh_threads", "-lpcre", "-lxslt", "-lssh", "-lpthread", "-ldl", "-lydk"
+    system ENV.cxx, "-std=c++11", "-Wall", "-Wextra", "-g", "-O0", "test.cpp", "-otest",
+    "-lboost_log_setup-mt", "-lboost_log-mt", "-lboost_thread-mt", "-lboost_date_time-mt",
+    "-lboost_system-mt", "-lboost_filesystem-mt", "-lboost_log_setup-mt", "-lboost_log-mt",
+    "-lboost_thread-mt", "-lboost_date_time-mt", "-lboost_system-mt", "-lboost_filesystem-mt",
+    "-lboost_log_setup-mt", "-lboost_log-mt", "-lboost_filesystem-mt", "-lboost_system-mt",
+    "-lxml2", "-lcurl", "-lssh_threads", "-lpcre", "-lxslt", "-lssh", "-lpthread", "-ldl", "-lydk"
     system "./test"
   end
 end
