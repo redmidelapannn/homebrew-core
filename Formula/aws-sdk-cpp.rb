92c25ba7493d329d98c6c6fdaebe1e95d2f3fd67
ilovezfs <ilovezfs@icloud.com>
2017-01-25 09:02:47 -0800

    aws-sdk-cpp 1.0.57

--- Formula/aws-sdk-cpp.rb
+++ Formula/aws-sdk-cpp.rb
@@ -1,8 +1,8 @@
 class AwsSdkCpp < Formula
   desc "AWS SDK for C++"
   homepage "https://github.com/aws/aws-sdk-cpp"
-  url "https://github.com/aws/aws-sdk-cpp/archive/1.0.56.tar.gz"
-  sha256 "e78d2c00e042b1f010d6cbbfc2b5f1a51ddbc32ae894e7f6ecc863ee67b730f3"
+  url "https://github.com/aws/aws-sdk-cpp/archive/1.0.57.tar.gz"
+  sha256 "2fab7011680c94b9f6fccada108f918d718d3c491a60b428ed88962b5b00cc9c"
   head "https://github.com/aws/aws-sdk-cpp.git"
 
   bottle do
