79d625e43921bbe3c7ef4fc657ec055d1ed44c93
ArchiFleKs <lefevre.kevin@gmail.com>
2017-01-26 08:02:15 +0100

    fix version offense

--- Formula/kube-aws.rb
+++ Formula/kube-aws.rb
@@ -17,7 +17,6 @@ class KubeAws < Formula
   devel do
     url "https://github.com/coreos/kube-aws/archive/v0.9.3.tar.gz"
     sha256 "cd33c88ee607fdcbca34af4a01b3aa386014f0394bd47c651d309b431bc51c30"
-    version "0.9.3"
   end
 
   depends_on "go" => :build
