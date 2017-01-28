302c9ec76d16d1a21520b4f71256cc25155903ea
Kevin Wojniak <kainjow@users.noreply.github.com>
2017-01-28 08:35:11 -0800

    Update to version 2.0

--- Formula/tundra.rb
+++ Formula/tundra.rb
@@ -1,8 +1,8 @@
 class Tundra < Formula
   desc "Code build system that tries to be accurate and fast for incremental builds"
   homepage "https://github.com/deplinenoise/tundra"
-  url "https://github.com/deplinenoise/tundra/archive/469c9bfc3f1ff4b855f172cd874d0740734f292a.tar.gz"
-  version "0.20161210"
+  url "https://github.com/deplinenoise/tundra/archive/v2.0.tar.gz"
+  version "2.0"
   sha256 "bee27c6df2b2960c2075e911fe04daa444b0185083a0c8615ee12434e7e23ed6"
 
   def install
