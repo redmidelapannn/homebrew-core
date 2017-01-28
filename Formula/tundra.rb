8bff6be38048c6dce7d73414a733c5c131cdf40f
Kevin Wojniak <kainjow@users.noreply.github.com>
2017-01-28 09:22:29 -0800

    Remove version, fix sha, add complete test

--- Formula/tundra.rb
+++ Formula/tundra.rb
@@ -2,8 +2,7 @@ class Tundra < Formula
   desc "Code build system that tries to be accurate and fast for incremental builds"
   homepage "https://github.com/deplinenoise/tundra"
   url "https://github.com/deplinenoise/tundra/archive/v2.0.tar.gz"
-  version "2.0"
-  sha256 "bee27c6df2b2960c2075e911fe04daa444b0185083a0c8615ee12434e7e23ed6"
+  sha256 "0d9f2b756959db76619aab563b412fa9e8a8bbf6fe0fc44836a725febb2c7662"
 
   def install
     system "make"
@@ -11,6 +10,32 @@ class Tundra < Formula
   end
 
   test do
-    system "#{bin}/tundra2", "-h"
+    (testpath/"test.c").write <<-'EOS_SRC'.undent
+      #include <stdio.h>
+      int main() {
+        printf("Hello World\n");
+        return 0;
+      }
+    EOS_SRC
+    (testpath/"tundra.lua").write <<-'EOS_CONFIG'.undent
+      Build {
+        Units = function()
+          local test = Program {
+            Name = "test",
+            Sources = { "test.c" },
+          }
+          Default(test)
+        end,
+        Configs = {
+          {
+            Name = "macosx-clang",
+            DefaultOnHost = "macosx",
+            Tools = { "clang-osx" },
+          },
+        },
+      }
+    EOS_CONFIG
+    system bin/"tundra2"
+    system "./t2-output/macosx-clang-debug-default/test"
   end
 end
