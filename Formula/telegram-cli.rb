5486b1b87177f0cff01e39e6bac166909b62e55d
Alexander Regueiro <alexreg@me.com>
2017-01-29 00:01:12 +0000

    Now installs Telegram server key and patches program (telegram-cli)
    Program is patched to read config file path from `/usr/local` prefix. Submitted and pending upstream: <https://github.com/vysheng/tg/issues/1296>.

--- Formula/telegram-cli.rb
+++ Formula/telegram-cli.rb
@@ -5,6 +5,7 @@ class TelegramCli < Formula
   url "https://github.com/vysheng/tg.git",
       :tag => "1.3.1",
       :revision => "5935c97ed05b90015418b5208b7beeca15a6043c"
+  head "https://github.com/vysheng/tg.git"
   revision 1
 
   depends_on "pkg-config" => :build
@@ -15,6 +16,8 @@ class TelegramCli < Formula
   depends_on "jansson" => :recommended
   depends_on "lua" => :recommended
   depends_on "python" => :recommended
+
+  patch :DATA
 
   def install
     args = %W[
@@ -33,5 +36,22 @@ class TelegramCli < Formula
     system "make"
 
     bin.install "bin/telegram-cli"
+    (etc/"telegram-cli").install "server.pub"
   end
 end
+
+
+__END__
+diff --git a/main.c b/main.c
+index 4776f07..cfa7411 100644
+--- a/main.c
++++ b/main.c
+@@ -990,7 +990,7 @@ int main (int argc, char **argv) {
+   running_for_first_time ();
+   parse_config ();
+
+-  #ifdef __FreeBSD__
++  #if defined(__FreeBSD__) || (defined(__APPLE__) && defined(__MACH__))
+   tgl_set_rsa_key (TLS, "/usr/local/etc/" PROG_NAME "/server.pub");
+   #else
+   tgl_set_rsa_key (TLS, "/etc/" PROG_NAME "/server.pub");
