29c0af5dcc50407a63e8c5c9c54093298a4e8d75
Amit <amit@cocycles.com>
2017-02-01 11:23:55 +0200

    updated bit version

--- Formula/bit.rb
+++ Formula/bit.rb
@@ -2,8 +2,8 @@ require "language/node"
 class Bit < Formula
   desc "Distributed Code Component Manager"
   homepage "https://www.bitsrc.io"
-  url "http://104.154.76.155:8081/artifactory/bit-brew/stable/bit/0.1.7/bit-0.1.7-brew.tar.gz"
-  sha256 "4957423b77965bade8a28c0215d33f51d9b8362121b22e1d1e1663c98546fe88"
+  url "https://bitsrc.jfrog.io/bitsrc/bit-brew/stable/bit/0.1.12/bit-0.1.12-brew.tar.gz"
+  sha256 "122ec737cca207bba1fa9ae6e3ea22acf53cecd56b542a2a5bacd380a88bdef9"
 
   depends_on "node"
 
@@ -14,6 +14,6 @@ class Bit < Formula
 
   test do
     assert_equal "successfully initialized an empty bit scope.\n",
-      shell_output("#{bin}/bit init")
+                 shell_output("#{bin}/bit init")
   end
 end
