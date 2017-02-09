8777471c442d231c77abf64543cbed9bffb63f81
Alexander Hamann <neutric@users.noreply.github.com>
2017-02-09 22:21:28 +0100

    pg_top: Add mirror
    The canonical URL for pg_top is not accessible at the moment, as
    pgFoundry, the host, is undergoing a rebuild from scratch. Move
    primary URL to mirror and make working alternate URL the primary.

--- Formula/pg_top.rb
+++ Formula/pg_top.rb
@@ -1,7 +1,8 @@
 class PgTop < Formula
   desc "Monitor PostgreSQL processes"
   homepage "http://ptop.projects.postgresql.org/"
-  url "http://pgfoundry.org/frs/download.php/3504/pg_top-3.7.0.tar.bz2"
+  url "http://pkgs.fedoraproject.org/repo/pkgs/pg_top/pg_top-3.7.0.tar.bz2/4d0c61a1aaa32f815d2e2452513fbfec/pg_top-3.7.0.tar.bz2"
+  mirror "http://pgfoundry.org/frs/download.php/3504/pg_top-3.7.0.tar.bz2"
   sha256 "c48d726e8cd778712e712373a428086d95e2b29932e545ff2a948d043de5a6a2"
   revision 2
 
