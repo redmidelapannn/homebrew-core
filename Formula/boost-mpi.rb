a382b683408ea7e7c9fdf8bbd7ea8ab7a3a12d91
Michael Kuron <mkuron@users.noreply.github.com>
2017-02-11 11:06:23 +0100

    boost-mpi: Remove bottles

--- Formula/boost-mpi.rb
+++ Formula/boost-mpi.rb
@@ -5,10 +5,6 @@ class BoostMpi < Formula
   sha256 "beae2529f759f6b3bf3f4969a19c2e9d6f0c503edcb2de4a61d1428519fcb3b0"
   head "https://github.com/boostorg/boost.git"
 
-  bottle do
-    cellar :any
-  end
-
   option :cxx11
 
   if build.cxx11?
