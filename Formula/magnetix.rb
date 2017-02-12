43129c30a80c63d0b421fe0b10854b1d4cd67c0b
Misty De Meo <mistydemeo@gmail.com>
2017-02-12 19:43:08 +1100

    magnetix: add AVFoundation patch

--- Formula/magnetix.rb
+++ Formula/magnetix.rb
@@ -7,6 +7,14 @@ class Magnetix < Formula
 
   depends_on :xcode => :build
 
+  # Port audio code from QTKit to AVFoundation
+  # Required since 10.12 SDK no longer includes QTKit.
+  # Submitted by email to the developer.
+  patch do
+    url "https://raw.githubusercontent.com/Homebrew/formula-patches/4fe0b7b6c43f75738782e047606c07446db07c4f/magnetix/avfoundation.patch"
+    sha256 "16caedaebcc05f03893bf0564b9c3212d1c919aebfdf1ee21126a39f8db5f441"
+  end
+
   def install
     cd "magnetiX_src" do
       xcodebuild
