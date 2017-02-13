c95e95d31fd3f6e14dcebc7440de1a52af6c47b0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-13 20:41:17 +0000

    spidermonkey: update 1.8.5_3 bottle.

--- Formula/spidermonkey.rb
+++ Formula/spidermonkey.rb
@@ -10,9 +10,9 @@ class Spidermonkey < Formula
 
   bottle do
     cellar :any
-    sha256 "62193341691f6f35a1d844409c587b431aa7540b70c02d90451e2cb3623788de" => :sierra
-    sha256 "5e7789a8ba4e3259364bd3ae827037ba83bf3a076633799bf8f5869b885db399" => :el_capitan
-    sha256 "38d1b7f54b5dbdd4a0e28e3a1077aed2ada42a9266cfaddeda6a08d761a2d8b2" => :yosemite
+    sha256 "bce361dcf388dbe0c3bcca7455833c3946fbc54e261257951a5dc8c8a941a6e2" => :sierra
+    sha256 "a32ab0df85da78fd4eb05d185a0c5a703131fc5fe95c86a2776f5bd1bdb8b844" => :el_capitan
+    sha256 "09d21390ceb18b95d2e8ebf766acc5bf73ec441e1cb3b9778a1e0c97c84ab835" => :yosemite
   end
 
   depends_on "readline"
