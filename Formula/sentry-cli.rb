cc43ccc8788f95cccaf842c5add4708fd42641cc
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-13 22:10:11 +0000

    sentry-cli: add 0.24.0 bottle.

--- Formula/sentry-cli.rb
+++ Formula/sentry-cli.rb
@@ -5,6 +5,13 @@ class SentryCli < Formula
   version "0.24.0"
   sha256 "eac9c2d127111b6c6036ab1596a8fe4d186d6feb52d95a433d9018f9c288e5ca"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "25eed701fa769c708926bc53a533f6a5da4aad1fd89308e3b71ec3e51ce46e57" => :sierra
+    sha256 "25eed701fa769c708926bc53a533f6a5da4aad1fd89308e3b71ec3e51ce46e57" => :el_capitan
+    sha256 "25eed701fa769c708926bc53a533f6a5da4aad1fd89308e3b71ec3e51ce46e57" => :yosemite
+  end
+
   def install
     mv "sentry-cli-Darwin-x86_64", "sentry-cli"
     bin.install "sentry-cli"
