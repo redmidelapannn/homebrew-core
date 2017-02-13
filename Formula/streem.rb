a1a801b0304eb9bc2701a764fd3fdef78f81cc9e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-13 14:30:49 +0000

    streem: add 201611 bottle.

--- Formula/streem.rb
+++ Formula/streem.rb
@@ -5,6 +5,13 @@ class Streem < Formula
   sha256 "807a9ccf3162efb061a15a333943050b329f0232cceb55492e5cd582c09a7fb4"
   head "https://github.com/matz/streem.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c1db4b66df6e88669e9b3301242a9c8ec30f2a3e5fc95912a892d22de1e42adb" => :sierra
+    sha256 "4968d4ff815b02813cbcab35f38bbcde1cce17e380064a409d7c0dc046e72b14" => :el_capitan
+    sha256 "0f352f03bfda5eff5467d9e35d184067f055b6c60d3657612d22cc7513318d1e" => :yosemite
+  end
+
   def install
     system "make"
     prefix.install "bin"
