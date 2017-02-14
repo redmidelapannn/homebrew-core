fad46bbf3f1bbe8521932f4f1f913c6ba5d7500e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-14 08:56:11 +0000

    netcat: update 0.7.1 bottle.

--- Formula/netcat.rb
+++ Formula/netcat.rb
@@ -6,11 +6,10 @@ class Netcat < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "5a8c59723d1f2a1f0ecf4772064469dd01dacf9ff40db0be7eaa1c4ef25dc910" => :sierra
-    sha256 "a600542d98c534b9790961b1f25c3e23276d1f7483c51efd06523129070a3f0e" => :el_capitan
-    sha256 "7ae495a909a2a25f6d8314857009a475034994234186682cc8dace4ea3c40b0c" => :yosemite
-    sha256 "fcdf724d792f286ff086a53f214b26eb2ebf61d6430e0f9305bf274b5faf85a5" => :mavericks
-    sha256 "d30aa6a393e3b6b28542e1d2a79252cda875e9a0dc817ad6b4920385669a2aaa" => :mountain_lion
+    rebuild 1
+    sha256 "d1a0cc9c535b3aa2a6334811c0911e7b4a1d208f620e14cea2c95766733a3df4" => :sierra
+    sha256 "d804466e883d15c2727a7f271a06d2f928846e976dbcbd6167b9c57508ab9724" => :el_capitan
+    sha256 "2dec0bedefb5befc9aa01bbf3e5fc0ca984a9524bac43d9a9650c374aa924b2d" => :yosemite
   end
 
   def install
