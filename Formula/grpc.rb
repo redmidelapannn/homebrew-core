29d5138298039873fbdf3f6cfd246d8ad367a2ef
Stanley Cheung <stanleycheung@google.com>
2017-02-15 11:09:25 -0800

    grpc: code review updates

--- Formula/grpc.rb
+++ Formula/grpc.rb
@@ -5,40 +5,26 @@ class Grpc < Formula
   sha256 "373ef66b0919bedef70021ba6798eaefe1cb560a2c94ccd2631d38fc9268f524"
   head "https://github.com/grpc/grpc.git"
 
-  bottle do
-    root_url "http://storage.googleapis.com/grpc-public/bottles"
-  end
-
-  option "with-plugins", "Also install gRPC protoc plugins."
-
   depends_on "openssl"
-  depends_on "pkg-config" => :run
+  depends_on "pkg-config" => :build
   depends_on "protobuf"
 
   def install
-    # Clone nanopb into third_party/nanopb IF THE DIRECTORY IS EMPTY.
-    # gRPC v1.0 has nanopb as a submodule of grpc, which requires a clone
-    # of nanopb into the directory. gRPC Master removed the submodule and
-    # always have nanopb files in place, in which case a clone would give
-    # error.
-    # TODO(mxyan): find a better solution
-    (bin+"grpc").write <<-EOS.undent
-      #!/bin/sh
-      [ "$(ls -A third_party/nanopb)" ] || git clone --branch=nanopb-0.3.5 https://github.com/nanopb/nanopb.git third_party/nanopb
-    EOS
-
     system "make", "install", "prefix=#{prefix}"
 
-    if build.with? "plugins"
-      system "make", "install-plugins", "prefix=#{prefix}"
-
-      # Link the Objective-C plugin to the name protoc expects.
-      # TODO: Do this renaming on make install, for all languages.
-      bin.install_symlink bin/"grpc_objective_c_plugin" => "protoc-gen-objcgrpc"
-    end
+    system "make", "install-plugins", "prefix=#{prefix}"
   end
 
   test do
-    system Formula["pkg-config"].bin/"pkg-config", "--libs", "grpc"
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <grpc/grpc.h>
+      int main() {
+        grpc_init();
+        grpc_shutdown();
+        return GRPC_STATUS_OK;
+      }
+    EOS
+    system ENV.cc, "test.cpp", "-L#{lib}", "-lgrpc", "-o", "test"
+    system "./test"
   end
 end
