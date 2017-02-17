8758505e07bc757785264e2bca3f9702a13ea062
Alyssa Ross <hi@alyssa.is>
2017-02-17 21:42:04 +0000

    zsh-syntax-highlighting: remove non-Homebrew-specific caveats

--- Formula/zsh-syntax-highlighting.rb
+++ Formula/zsh-syntax-highlighting.rb
@@ -20,17 +20,7 @@ class ZshSyntaxHighlighting < Formula
   def caveats
     <<-EOS.undent
     To activate the syntax highlighting, add the following at the end of your .zshrc:
-
       source #{HOMEBREW_PREFIX}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
-    You will also need to force reload of your .zshrc:
-
-      source ~/.zshrc
-
-    Additionally, if you receive "highlighters directory not found" error message,
-    you may need to add the following to your .zshenv:
-
-      export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=#{HOMEBREW_PREFIX}/share/zsh-syntax-highlighting/highlighters
     EOS
   end
 
