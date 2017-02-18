002315bf7cd0d57f815f7f6930a371a4b3683274
Jason Costomiris <jcostom@jasons.org>
2017-02-17 20:35:41 -0800

    maclock 1.0
    Adding formula to build & install maclock, a simple Obj-C program
    that is the shell/CLI equivalent of the Keychain Access.app's
    menu option for "Lock Screen".

---
+++ Formula/maclock.rb
@@ -0,0 +1,11 @@
+class Maclock < Formula
+  desc "Triggers Immediate screen lock on macOS"
+  homepage "https://github.com/jcostom/maclock"
+  url "https://github.com/jcostom/maclock/archive/1.0.tar.gz"
+  sha256 "a0b8090e7bcf3b3cabc20d6e9fe2536f0d06f74e7a2367c2fea9cccbc5b16a24"
+
+  def install
+    system ENV.cc, "-framework", "Foundation", "maclock.m", "-o", "maclock"
+    bin.install "maclock"
+  end
+end
