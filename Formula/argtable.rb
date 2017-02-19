df942b2e7e9786cd534c2e7a6bea055db7420fcc
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-19 00:28:24 +0000

    argtable: update 2.13 bottle.

--- Formula/argtable.rb
+++ Formula/argtable.rb
@@ -7,10 +7,10 @@ class Argtable < Formula
 
   bottle do
     cellar :any
-    sha256 "9485d1e045ed40c0145eb867f9d24425ccedd53b4f0cb0ec949139b0c99507c7" => :sierra
-    sha256 "0a720e738557215bf1b58fa642ec2fc51971da38e98b987862fcd05cc54756f7" => :el_capitan
-    sha256 "9e9d1451712580f090f0078ec7774a0daeb1057be3b1762e3d8465264d969432" => :yosemite
-    sha256 "7081198c76023e34380d35682b7a4274a9faf98d3e3e3fa2a9fa801e0a320a8c" => :mavericks
+    rebuild 1
+    sha256 "687d98b6fb7ec666f73e025e80a3d575b454e1ff6d7774ea74522a4c7a583118" => :sierra
+    sha256 "3ec4920e82ffba8c6b1d0f2e41abf637acc2d770406ba33808842d5864746d85" => :el_capitan
+    sha256 "3e77ba851e398aafee700a91c2144d180aa86cbe35b9cc66d19e3629ad674933" => :yosemite
   end
 
   def install
