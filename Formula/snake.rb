ffb3f64f8df35ee5f837ec4e1be5b2f7b396f023
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-02-21 15:48:58 -0500

    snake 1.0.0 (new formula)

--- Formula/snake.rb
+++ Formula/snake.rb
@@ -3,12 +3,13 @@ class Snake < Formula
   homepage "http://silo.cs.indiana.edu:32903/snake/snakehome.html"
   url "http://silo.cs.indiana.edu:32903/snake/snake.tar.gz"
   version "1.0.0"
-  sha256 "9a078047dd9b553f458703afad98ebdcf27907382d05abf99900662de05178dd"
+  sha256 "0ce0cf8cd1cc77c67be06ea66f11909f43d5557e7c74595a37b6af85c9788abe"
   depends_on "ant" => :build
   depends_on :java
   def install
     system "ant", "compile", "jar"
-    bin.install "snake", "snake.jar"
+    system "mv " "snake.jar " "#{prefix}/"
+    bin.install "snake"
   end
   test do
     system "java", "Snake", "--version"
