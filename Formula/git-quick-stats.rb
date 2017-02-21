b4e2385a50e6be83f5aa4f4b212ca202749db19c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-21 18:09:25 +0000

    git-quick-stats: add 1.0.1 bottle.

--- Formula/git-quick-stats.rb
+++ Formula/git-quick-stats.rb
@@ -4,6 +4,13 @@ class GitQuickStats < Formula
   url "https://github.com/arzzen/git-quick-stats/archive/1.0.1.tar.gz"
   sha256 "397fabda3e894bf058767e32114ce8496ecf23f088e6de6203cae4486e1f755b"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "23e4d977de1fa408dbbebf1420cfb132fded75ce9fd7acdf83b18e9190ff7d89" => :sierra
+    sha256 "23e4d977de1fa408dbbebf1420cfb132fded75ce9fd7acdf83b18e9190ff7d89" => :el_capitan
+    sha256 "23e4d977de1fa408dbbebf1420cfb132fded75ce9fd7acdf83b18e9190ff7d89" => :yosemite
+  end
+
   def install
     bin.install "git-quick-stats"
   end
