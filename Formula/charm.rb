f1f77b17931dc856009b60c85ece3a38c2540fb4
ilovezfs <ilovezfs@icloud.com>
2017-02-22 21:22:01 -0800

    charm 2.2.1

--- Formula/charm.rb
+++ Formula/charm.rb
@@ -3,8 +3,8 @@ require "language/go"
 class Charm < Formula
   desc "Tool for managing Juju Charms"
   homepage "https://github.com/juju/charmstore-client"
-  url "https://github.com/juju/charmstore-client/archive/2.2.0.tar.gz"
-  sha256 "ce4c9dc8b03fbb6047d95217626c4218bba798083da16399691a32bba98647c2"
+  url "https://github.com/juju/charmstore-client/archive/2.2.1.tar.gz"
+  sha256 "6ec369db9dfe2b8991ef08d885477772a78d5165a49a55d542ca368898349dbe"
 
   bottle do
     cellar :any_skip_relocation
