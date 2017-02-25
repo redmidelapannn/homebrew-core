dff6001bac8d91f0433e3622fa14ee151cdff943
Matthew Raspberry <nixalot@users.noreply.github.com>
2017-02-24 20:25:18 -0600

    python3: Added dtrace support to python 3

--- Formula/python3.rb
+++ Formula/python3.rb
@@ -85,6 +85,7 @@ class Python3 < Formula
       --datadir=#{share}
       --enable-framework=#{frameworks}
       --without-ensurepip
+      --with-dtrace
     ]
 
     args << "--without-gcc" if ENV.compiler == :clang
