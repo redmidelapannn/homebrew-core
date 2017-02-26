104823e65173444971b7cff97f1f5e0a8fa34c84
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-26 13:45:56 +0000

    tinyxml: update 2.6.2 bottle.

--- Formula/tinyxml.rb
+++ Formula/tinyxml.rb
@@ -6,11 +6,10 @@ class Tinyxml < Formula
 
   bottle do
     cellar :any
-    sha256 "ef8c7bbbae6148e161b6f3369ede8bd3533a32847dc716000b46d26e6fb1c26c" => :sierra
-    sha256 "16e6052892b43e68c45f5122b6802e9bc32001dc9478dfcd89511a24544660e5" => :el_capitan
-    sha256 "4b1df9cb229b04f9968621a52737d96e86fcd6c2ad8904ae8a5c324347845f50" => :yosemite
-    sha256 "75f79bb5d502e7be74de20e1cd3e3dcdd4702b37ef7de53d9d9a546a51776b50" => :mavericks
-    sha256 "2db8d34dafd503a2982dd9ca5ffb4a2a2740e0eb78195933cb121dd7b7836728" => :mountain_lion
+    rebuild 1
+    sha256 "c7c30e090a9d3af57bf85b1e4246704ac44041243c508d9b7d10a3be263fb8c0" => :sierra
+    sha256 "7882b556f4785af33efe7852e4ea07200891e0617b6159d0e5df29116942c4e6" => :el_capitan
+    sha256 "19e1262491f160b9c1cb2b417e24e5176195ffb7468e1a337f568456bb0ff3fa" => :yosemite
   end
 
   depends_on "cmake" => :build
