1c1c91043c681195784f82080a92e3c8a945bd2a
Tomasz Pajor <tomek@polishgeeks.com>
2017-02-26 21:11:10 +0100

    tesseract: remove comment

--- Formula/tesseract.rb
+++ Formula/tesseract.rb
@@ -86,11 +86,6 @@ class Tesseract < Formula
 
     ENV.cxx11
 
-    # Fix broken pkg-config file
-    # Can be removed with next version bump
-    # https://github.com/tesseract-ocr/tesseract/issues/241
-    # inreplace "tesseract.pc.in", "@OPENCL_LIB@", "@OPENCL_LDFLAGS@" if build.stable?
-
     system "./autogen.sh"
 
     args = %W[
