25166cf93ff15e063970fc42148a11691c9be4a4
Mikhail Filippov <Mikhail.Filippov@jetbrains.com>
2017-02-28 17:05:33 +0300

    fsharp 4.1.0.1

--- Formula/mono.rb
+++ Formula/mono.rb
@@ -33,8 +33,8 @@ class Mono < Formula
 
   resource "fsharp" do
     url "https://github.com/fsharp/fsharp.git",
-        :tag => "4.1.0.0",
-        :revision => "e4cbfa48ece45b2fd0ee44bb271954afd5b1f083"
+        :tag => "4.1.0.1",
+        :revision => "262deb017cfcd0f0d4138779ff42ede7dbf44c46"
   end
 
   def install
