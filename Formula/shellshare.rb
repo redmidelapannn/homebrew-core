7fd798b85d3b38510a0ecea0ea45b9260de0575d
Atharva Deshmukh <arthar360@users.noreply.github.com>
2017-02-28 21:11:09 +0530

    Update shellshare.rb

--- Formula/shellshare.rb
+++ Formula/shellshare.rb
@@ -1,16 +1,12 @@
 class Shellshare < Formula
-  desc "Shellshare broadcasting service"
+  desc "LIVE TERMINAL BROADCAST"
   homepage "http://shellshare.net"
   url "https://github.com/arthar360/shellshare/archive/1.0.0.tar.gz"
   version "1.0.0"
   sha256 "e14080e5f58f513975f893dce07412097155c38f1798a96e65b78bcf9992d34c"
-
   def install
-
-        bin.install "shellshare"
-
+    bin.install "shellshare"
   end
-
   test do
   end
 end
