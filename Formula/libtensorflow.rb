c2fef3742f763fb05e35773947f8caf9612754e7
Mathieu Poumeyrol <kali@zoy.org>
2017-02-28 09:47:40 +0100

    rename formula and library

--- Formula/libtensorflow.rb
+++ Formula/libtensorflow.rb
@@ -1,4 +1,4 @@
-class TensorflowC < Formula
+class Libtensorflow < Formula
   desc "C interface for Google's OS library for Machine Intelligence"
   homepage "https://www.tensorflow.org/"
   url "https://github.com/tensorflow/tensorflow.git", :tag => "v1.0.0",
@@ -19,14 +19,14 @@ class TensorflowC < Formula
     ENV["TF_NEED_CUDA"]="0"
     system "./configure"
 
-    system "bazel", "build", "--compilation_mode=opt", "--copt=-march=native", "tensorflow:libtensorflow_c.so"
-    lib.install "bazel-bin/tensorflow/libtensorflow_c.so"
+    system "bazel", "build", "--compilation_mode=opt", "--copt=-march=native", "tensorflow:libtensorflow.so"
+    lib.install "bazel-bin/tensorflow/libtensorflow.so"
     (include/"tensorflow/c").install "tensorflow/c/c_api.h"
-    (lib/"pkgconfig/tensorflow_c.pc").write <<-EOS.undent
-      Name: tensorflow_c
-      Description: Tensorflow c lib
+    (lib/"pkgconfig/tensorflow.pc").write <<-EOS.undent
+      Name: tensorflow
+      Description: Tensorflow library
       Version: #{version}
-      Libs: -L#{lib} -ltensorflow_c
+      Libs: -L#{lib} -ltensorflow
       Cflags: -I#{include}
     EOS
   end
@@ -40,7 +40,7 @@ class TensorflowC < Formula
         printf("%s", TF_Version());
       }
     EOS
-    system ENV.cc, "-L#{lib}", "-ltensorflow_c", "-o", "test_tf", "test.c"
+    system ENV.cc, "-L#{lib}", "-ltensorflow", "-o", "test_tf", "test.c"
     assert_equal version, shell_output("./test_tf")
   end
 end
