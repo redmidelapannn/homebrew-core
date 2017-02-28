90bd6669bffdbd268b9fdc712846856bfee4c826
BrewTestBot <brew-test-bot@googlegroups.com>
2017-02-28 08:54:02 +0000

    popt: update 1.16 bottle.

--- Formula/popt.rb
+++ Formula/popt.rb
@@ -5,12 +5,10 @@ class Popt < Formula
   sha256 "e728ed296fe9f069a0e005003c3d6b2dde3d9cad453422a10d6558616d304cc8"
 
   bottle do
-    rebuild 1
-    sha256 "ceae94fc8e588309670a7a045186eee8ff3a9966a68650f044a14d101267b7b2" => :sierra
-    sha256 "60a7f19e8fecafd92a5beb7d6438efac915e8f3afe3d83575fb64bb4a6190aab" => :el_capitan
-    sha256 "56d1104516e23bb314a248904b8ec85afe2fdbf71555417eb8f91edc1286e6da" => :yosemite
-    sha256 "ba122e7f34b9b03ab5a32ab01124b61eb608c29e0c0d023462953ed03782dd2a" => :mavericks
-    sha256 "6d95c3530a7bd4d7099d91f448669b53bb51a071c5e9a8b9915cdc750bd72aec" => :mountain_lion
+    rebuild 2
+    sha256 "bf535a9b212cb5bd0bd9990332a99c4a85bfd8fe6c7309a365560156b28e7b40" => :sierra
+    sha256 "0c7caf8bb3286d5fe41b1f4ed9546372edcb4a2c32066581c8b958fc882576a8" => :el_capitan
+    sha256 "214e5b93ebd612c667f57d3945932a3609eae3d81a47574ff313687b8c495915" => :yosemite
   end
 
   def install
