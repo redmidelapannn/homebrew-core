6026f0791740c014c5b0273039f929757f34c474
Tomasz Pajor <tomek@polishgeeks.com>
2017-03-01 15:54:43 +0100

    notmuch: bump zlib

--- Formula/notmuch.rb
+++ Formula/notmuch.rb
@@ -21,10 +21,10 @@ class Notmuch < Formula
   depends_on :python3 => :optional
   depends_on :ruby => ["1.9", :optional]
 
-  # Requires zlib >= 1.2.10
+  # Requires zlib >= 1.2.11
   resource "zlib" do
-    url "http://zlib.net/zlib-1.2.10.tar.gz"
-    sha256 "8d7e9f698ce48787b6e1c67e6bff79e487303e66077e25cb9784ac8835978017"
+    url "http://zlib.net/zlib-1.2.11.tar.gz"
+    sha256 "c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1"
   end
 
   # Fix SIP issue with python bindings
