9a7f2239c4ff142e2f6521633832f8d0956e93f6
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-01 14:44:44 +0000

    functions: add 0.2.25 bottle.

--- Formula/functions.rb
+++ Formula/functions.rb
@@ -4,6 +4,13 @@ class Functions < Formula
   url "https://github.com/iron-io/functions/archive/0.2.25.tar.gz"
   sha256 "9c26678e7a7c9606a5c8fc32288624dccb2efbb493ed8747550b8f4faa7d449a"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "27247e725a40416bb1ccc9c57695ecabfcc4d9dd981636962d8e02318085e0dc" => :sierra
+    sha256 "5f5140090856f68d25d5c4be169511ae61595e1a6983a2aafa21818c4aad0280" => :el_capitan
+    sha256 "fd23da31c17c4e626f515f0639dc012d9ec64a5e517c2438e1e2aa20612c183c" => :yosemite
+  end
+
   depends_on "go" => :build
   depends_on "glide" => :build
 
