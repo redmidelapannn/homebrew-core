049bfd60585c139e9147a2953927caadde7c4ebd
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-02 19:28:33 +0000

    shellshare: add 1.0.0 bottle.

--- Formula/shellshare.rb
+++ Formula/shellshare.rb
@@ -4,6 +4,13 @@ class Shellshare < Formula
   url "https://github.com/vitorbaptista/shellshare/archive/v1.0.0.tar.gz"
   sha256 "72bd49928982611ae44aac4a308dc6054d9c8ebad87644bd5eb68ff0bb704ce3"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7e8b682b866614ee795f93fa44c3e6ec1887c248b0a9d35a1263c1cd71ee6993" => :sierra
+    sha256 "7e8b682b866614ee795f93fa44c3e6ec1887c248b0a9d35a1263c1cd71ee6993" => :el_capitan
+    sha256 "7e8b682b866614ee795f93fa44c3e6ec1887c248b0a9d35a1263c1cd71ee6993" => :yosemite
+  end
+
   depends_on :python
 
   def install
