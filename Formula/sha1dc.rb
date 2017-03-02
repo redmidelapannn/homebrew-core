acfb8bc9bf698b514383fd6a9a4d22a1e989def0
John Hawkinson <jhawk@mit.edu>
2017-03-01 19:42:35 -0500

    sha1dc: Use "make install"
    Now that upstream now supports it. Still no tag though.

--- Formula/sha1dc.rb
+++ Formula/sha1dc.rb
@@ -2,22 +2,22 @@ class Sha1dc < Formula
   desc "Tool to detect SHA-1 collisions in files, including SHAttered"
   homepage "https://github.com/cr-marcstevens/sha1collisiondetection"
   url "https://github.com/cr-marcstevens/sha1collisiondetection.git",
-      :revision => "cb10f50d20a1c6c9efbf526c5da510c492d595b9"
-  version "HEAD-cb10f0"
-  head "https://github.com/cr-marcstevens/sha1collisiondetection.git"
+      :revision => "6b1c12f3606d0959f8658d44a324fcf5f4497f88"
+  version "HEAD-6b1c12"
 
   depends_on "libtool" => :build
 
   def install
     inreplace "Makefile", "libtool", "glibtool"
 
-    # By default tries to build with HAVEAVX=1, fails.
-    inreplace "Makefile", "HAVEAVX=1", "HAVEAVX=0"
-    system "make"
+    ENV.deparallelize
 
-    bin.install "bin/sha1dcsum"
-    bin.install "bin/sha1dcsum_partialcoll"
-    lib.install "bin/.libs/libdetectcoll.a"
+    # By default tries to build with HAVEAVX=1, fails.
+    system "make", "HAVEAVX=0"
+
+    system "make", "PREFIX=#{prefix}", "install"
+    (pkgshare/"test").install Dir["test/*"]
+
   end
 
   test do
