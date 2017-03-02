bd3223c54fbc5e7e8e064c3e2efdbe079846d2d1
Atharva Deshmukh <arthar360@users.noreply.github.com>
2017-03-03 00:48:02 +0530

    Update shellshare.rb

--- Formula/shellshare.rb
+++ Formula/shellshare.rb
@@ -3,10 +3,13 @@ class Shellshare < Formula
   homepage "https://shellshare.net"
   url "https://github.com/vitorbaptista/shellshare/archive/v1.0.0.tar.gz"
   sha256 "72bd49928982611ae44aac4a308dc6054d9c8ebad87644bd5eb68ff0bb704ce3"
+
   depends_on :python
+
   def install
     bin.install "public/bin/shellshare"
   end
+  
   test do
     system "#{bin}/shellshare", "-v"
   end
