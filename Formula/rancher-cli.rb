f5fa915e79340d3accc70f9b0650664bf41ecc87
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-07 17:50:38 +0000

    rancher-cli: update 0.5.0 bottle.

--- Formula/rancher-cli.rb
+++ Formula/rancher-cli.rb
@@ -5,9 +5,10 @@ class RancherCli < Formula
   sha256 "c126fd29834e3f99de7262eb455d3c318cb8d1f602c13f702023d38a9059fbb1"
 
   bottle do
-    sha256 "c126fd29834e3f99de7262eb455d3c318cb8d1f602c13f702023d38a9059fbb1" => :sierra
-    sha256 "c126fd29834e3f99de7262eb455d3c318cb8d1f602c13f702023d38a9059fbb1" => :el_capitan
-    sha256 "c126fd29834e3f99de7262eb455d3c318cb8d1f602c13f702023d38a9059fbb1" => :yosemite
+    cellar :any_skip_relocation
+    sha256 "64f0532f85959c5d3ac9b89dec50042d1f2f76f041401d681850582af9d13866" => :sierra
+    sha256 "fc87596f48fc933d6e8e90fe881ec0c5d2633ec13e029c37fbf2baca61002185" => :el_capitan
+    sha256 "e771dc34da31ff9908e3b6184fa7927a8ed73d773f00e065c28e1b8173bf7fdf" => :yosemite
   end
 
   depends_on "go" => :build
