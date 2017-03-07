8514a6ee45f23a008e99f9b9d099ec44aaeb1f44
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2017-03-07 12:26:00 -0500

    Update formula

--- Formula/gitfame.rb
+++ Formula/gitfame.rb
@@ -1,12 +1,13 @@
 class Gitfame < Formula
-  desc "A Swift CLI that logs your GitHub Stars and Forks."
+  desc "Swift CLI that logs your GitHub Stars and Forks."
   homepage "http://www.sabintsev.com"
   url "https://github.com/ArtSabintsev/GitFame.git", :tag => "1.0.0"
   head "https://github.com/ArtSabintsev/GitFame.git"
 
   def install
     xcodebuild "-project", "GitFame.xcodeproj",
-    "CONFIGURATION_BUILD_DIR=build"
+    "CONFIGURATION_BUILD_DIR=build",
+    "SYMROOT=."
     bin.install "build/gitfame"
   end
 
