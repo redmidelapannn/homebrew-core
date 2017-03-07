39f0c97a9e072d095846967338bb100f6a38ef66
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-07 17:30:23 +0000

    gitfame: add 1.0.0 bottle.

--- Formula/gitfame.rb
+++ Formula/gitfame.rb
@@ -4,6 +4,12 @@ class Gitfame < Formula
   url "https://github.com/ArtSabintsev/GitFame.git", :tag => "1.0.0"
   head "https://github.com/ArtSabintsev/GitFame.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "cbc37c39b38a5ce770f26e53ceea36c880cf6f2ef76910e71e0265142addff4e" => :sierra
+    sha256 "90b0f871530bf5afadfbd8a99af7c6db56ef04170645ce9fc4ad740c9b13204e" => :el_capitan
+  end
+
   def install
     xcodebuild "-project", "GitFame.xcodeproj",
     "CONFIGURATION_BUILD_DIR=build",
