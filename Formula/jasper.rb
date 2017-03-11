62eb2bf364d827379b7439e41884b9279b720f16
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-11 18:38:05 +0000

    jasper: update 2.0.12 bottle.

--- Formula/jasper.rb
+++ Formula/jasper.rb
@@ -5,9 +5,10 @@ class Jasper < Formula
   sha256 "f0bcc1c4de5fab199f2e792acf269eb34d54689777c305d80e2498788f9f204b"
 
   bottle do
-    sha256 "386f26d020cbe06b80066c34cf6a4f679562b93211ef74d244ca119604af8d13" => :sierra
-    sha256 "be03c9d8ef6f4a3dd056ce7358c0ebbdd366542e9c00639c223ea0d5bb941ada" => :el_capitan
-    sha256 "9390f990b9bd0e4605c2c012c80e8ec40ac2f539cd79814fcf276a12bbe32b4a" => :yosemite
+    rebuild 1
+    sha256 "9dbd3e8946fa6168bc9e8361f2be2c992d2860e2ee9f1b501e6ad9e875c64c48" => :sierra
+    sha256 "7cd162027f014a69c96afe37b426a0a6ea4aaa78ad08e147dc6dc03ffca54414" => :el_capitan
+    sha256 "4dd89722227b0c9456a118b2f4b68bd3998d633ecfa3971cd3c3b80c99099af9" => :yosemite
   end
 
   depends_on "cmake" => :build
