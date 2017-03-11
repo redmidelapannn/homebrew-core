17ab5e968e2cc7ceec24febc14fa01892bf7d4f0
dudongcheng <dongcheng.du@gmail.com>
2017-03-11 10:12:27 +0800

    httpflow 0.0.3 fix

--- Formula/httpflow.rb
+++ Formula/httpflow.rb
@@ -1,7 +1,3 @@
-# Documentation: http://docs.brew.sh/Formula-Cookbook.html
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Httpflow < Formula
   desc "Packet capture and analysis utility similar to tcpdump for HTTP Edit"
   homepage "https://github.com/six-ddc/httpflow/releases"
@@ -10,28 +6,16 @@ class Httpflow < Formula
   head "https://github.com/six-ddc/httpflow.git", :branch => "master"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     args = %W[
       PREFIX=#{prefix}
       CXX=#{ENV.cxx}
     ]
 
-
-    # Remove unrecognized options if warned by configure
     system "make"
     system "make", "install", *args
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test httpflow`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "#{bin}/httpflow", "-h"
   end
 end
