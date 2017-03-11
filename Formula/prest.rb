2223388961d1f635d683dc9093686ea8ad51597d
Avelino <thiago@avelino.xxx>
2017-03-11 10:22:06 -0300

    Update with the recommendations of the @bfontaine

--- Formula/prest.rb
+++ Formula/prest.rb
@@ -1,21 +1,19 @@
 class Prest < Formula
   desc "Serve a RESTful API from any PostgreSQL database"
   homepage "https://github.com/nuveo/prest"
-  url "https://github.com/nuveo/prest/archive/v0.1.3.tar.gz"
-  sha256 "ddb7485d8e6395438d645c93a557fb981b1581ad93f97545ccd411dbf2ae3cc1"
+  url "https://github.com/nuveo/prest/archive/v0.1.4.tar.gz"
+  sha256 "8d1a38e1ea45ebf7bffb34258ba2db5c404a58139597b6a8439bcaaeec12084e"
 
-  depends_on 'go'
-  bottle :unneeded
+  depends_on 'go' => :build
 
   def install
     ENV["GOPATH"] = buildpath
     (buildpath/"src/github.com/nuveo").mkpath
     ln_s buildpath, buildpath/"src/github.com/nuveo/prest"
-    system "go", "build", "-o", "prest"
-    bin.install "prest"
+    system "go", "build", "-o", bin/"prest"
   end
 
   test do
-    system "#{bin}/prest", "--help"
+    system "#{bin}/prest", "version"
   end
 end
