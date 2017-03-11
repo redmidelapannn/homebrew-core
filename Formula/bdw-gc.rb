405e2fb6335464bc9e63d48db76db7d0168b0bb9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-11 19:42:25 +0000

    bdw-gc: update 7.6.0 bottle.

--- Formula/bdw-gc.rb
+++ Formula/bdw-gc.rb
@@ -5,10 +5,10 @@ class BdwGc < Formula
   sha256 "a14a28b1129be90e55cd6f71127ffc5594e1091d5d54131528c24cd0c03b7d90"
 
   bottle do
-    sha256 "fcbf63ca7801f54e098335ba3f7968bbcdf600e30ac878c68d61061d5923b9f1" => :sierra
-    sha256 "939e43625f304cc5d315f28147db4323da910e7ade2efddea01fade0c56faf48" => :el_capitan
-    sha256 "a49e8aaa7f869a5c7dec0d4e38bb02f6be7e82a45341953f3e761716e3836ef2" => :yosemite
-    sha256 "392bb21f15af6c0ea2a9edfe0641362672e9d7ce8d9fee121e6a64e6081f79b0" => :mavericks
+    rebuild 1
+    sha256 "a82caf4e655d2b8f9c66be8915903419c84f5cc3bf133117256d21ccb9ad2e5d" => :sierra
+    sha256 "a65f13f2064fb98016bfaa9ff17c7a2a45e63e7c25dd924eeb930b9f20dbdf89" => :el_capitan
+    sha256 "04ebad9df50f83b62a889ca906edabd8251d6922bb4084af96a6f4c332e6ef29" => :yosemite
   end
 
   head do
