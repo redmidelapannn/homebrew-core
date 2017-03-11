925bcf8054e6fd0af2984e7afafa1a08b2e8822c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-11 12:45:54 +0000

    puzzles: update 20161228.7cae89f bottle.

--- Formula/puzzles.rb
+++ Formula/puzzles.rb
@@ -10,9 +10,10 @@ class Puzzles < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "57b04776dc4696ac93b9230c4df569f3aecd36b238a05e73dcde3493659dc847" => :sierra
-    sha256 "57b04776dc4696ac93b9230c4df569f3aecd36b238a05e73dcde3493659dc847" => :el_capitan
-    sha256 "e01a6fc502cf54a2fc1d1c05a8d1e04b3762defb50aadd9d67e4049435649676" => :yosemite
+    rebuild 1
+    sha256 "563a799fe9fdcffd1e32797db4fd708366610e6652cba51d64f8067bdd1aa545" => :sierra
+    sha256 "563a799fe9fdcffd1e32797db4fd708366610e6652cba51d64f8067bdd1aa545" => :el_capitan
+    sha256 "48bcfdebbefb2115b304945e562f88a88d7315f35f1e5109a1a8900675fb14cd" => :yosemite
   end
 
   depends_on "halibut"
