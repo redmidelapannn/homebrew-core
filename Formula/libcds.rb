3acb90836af53e9d65ca66e34bafbf3f362d9786
Anders Åslund <anders.aslund1@saabgroup.com>
2017-03-12 10:36:41 +0100

    Adding formula for libcds - a C++ library of Concurrent Data Structures

---
+++ Formula/libcds.rb
@@ -0,0 +1,29 @@
+class Libcds < Formula
+  desc "C++ library of Concurrent Data Structures"
+  homepage "https://libcds.sourceforge.io/doc/cds-api/index.html"
+  url "https://github.com/khizmax/libcds/archive/v2.2.0.tar.gz"
+  sha256 "e582fbd6492c91ff24b11468f094b111d5f62eca69e6445c88bf16cad50e40ed"
+
+  depends_on "cmake" => :build
+  depends_on "boost"
+
+  def install
+    system "cmake", ".", "-DUSE_PROGRESSBAR=ON", "-DWITH_BOOST_ATOMIC=ON", *std_cmake_args
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<-EOS.undent
+      #include <cds/init.h>
+      int main() {
+        cds::Initialize();
+        cds::threading::Manager::attachThread();
+        cds::Terminate();
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "-o", "test", "test.cpp", "-L#{lib}64", "-lcds", "-std=c++11"
+    system "./test"
+  end
+end
