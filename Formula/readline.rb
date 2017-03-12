8af517fc0431df6af3f98e85a78c93ba56aaeaa9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-12 05:58:29 +0000

    readline: update 7.0.3_1 bottle.

--- Formula/readline.rb
+++ Formula/readline.rb
@@ -9,9 +9,10 @@ class Readline < Formula
 
   bottle do
     cellar :any
-    sha256 "af7886c963fe3e9f58c45d679a64b278f4df7b172bbd978cf42658a7fd7b4a2a" => :sierra
-    sha256 "86766a343a07e08c52e7e87e64a12d3aa34bf71ba248fc779a2c5b0664797ba9" => :el_capitan
-    sha256 "11589e87c4860e414fe5a4b3481d20e47258f41a91a7490a5c88e1a57d5e1d18" => :yosemite
+    rebuild 1
+    sha256 "29763f266ebef8b01a633fc4988f6f17e588a9f6db3ab5a1f0d081b977688a4a" => :sierra
+    sha256 "5b915847aa4b6812cadfc659772a4681be377a1e0b26775ae15e20b0285535e1" => :el_capitan
+    sha256 "c796a5f83989d660d337d1980bbc458c8a14de752822a243e36836d70494ea5a" => :yosemite
   end
 
   %w[
