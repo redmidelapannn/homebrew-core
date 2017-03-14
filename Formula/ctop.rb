66e6f9e6976f16ec491411178467eb0d77bd6c07
Avelino <thiago@avelino.xxx>
2017-03-13 23:32:55 -0300

    fixed version installed ``` $ brew install --build-from-source Formula/ctop.rb ==> Downloading https://github.com/bcicen/ctop/archive/v0.4.1-deps.tar.gz Already downloaded: /Users/avelino/Library/Caches/Homebrew/ctop-0.4.1.tar.gz ==> glide install ==> go build -o /usr/local/Cellar/ctop/0.4.1/bin/ctop 🍺 /usr/local/Cellar/ctop/0.4.1: 3 files, 7.4MB, built in 9 seconds ```

--- Formula/ctop.rb
+++ Formula/ctop.rb
@@ -1,8 +1,8 @@
 class Ctop < Formula
   desc "Top-like interface for container metrics"
   homepage "https://bcicen.github.io/ctop/"
-  version "0.4.1"
   url "https://github.com/bcicen/ctop/archive/v0.4.1-deps.tar.gz"
+  version "0.4.1"
   sha256 "39c4247a7c6715bc45db078769419ff9bdab1219378ac39cb97febbc0130dbf5"
 
   depends_on "go" => :build
