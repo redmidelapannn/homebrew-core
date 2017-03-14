c5f76c0211b22267980b1afcb8ff91d5a44c3683
Michel Bierlaire <michel.bierlaire@epfl.ch>
2017-03-14 18:05:48 +0100

    biogeme 2.5 (new formula) Requested changes implemtented

--- Formula/biogeme.rb
+++ Formula/biogeme.rb
@@ -23,17 +23,15 @@ class Biogeme < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test biogeme`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    (testpath/"minimal.py").write("from biogeme import *\nrowIterator('obsIter')\nBIOGEME_OBJECT.SIMULATE = Enumerate({'Test':1},'obsIter')")
-    (testpath/"minimal.dat").write("TEST\n1\n")
+    (testpath/"minimal.py").write <<-EOS.undent
+    from biogeme import *
+    rowIterator('obsIter')
+    BIOGEME_OBJECT.SIMULATE = Enumerate({'Test':1},'obsIter')
+    EOS
+    (testpath/"minimal.dat").write <<-EOS.undent
+    TEST
+    1
+    EOS
     system bin/"pythonbiogeme", "minimal", "minimal.dat"
   end
 end
