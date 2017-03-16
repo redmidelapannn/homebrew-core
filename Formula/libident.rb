b21ad5e2e36c9ad6d21dca2d4cb3d78a8733a428
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-16 00:57:56 +0000

    libident: update 0.32 bottle.

--- Formula/libident.rb
+++ Formula/libident.rb
@@ -6,12 +6,10 @@ class Libident < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "4fb8a991f9f83d499b32a814b2d68465327b7b77c0108764e58e4296a968100f" => :sierra
-    sha256 "6236d3b4ee424795859cc64da30997ff67f7ac5bd42702e8eabe10f99339ca48" => :el_capitan
-    sha256 "53db8e889d8efa34b4a3b6a145bcec2bcb53595e7db0cfdd55c8d857dff3a442" => :yosemite
-    sha256 "d8e20cc9d2015b83785d4eb05110afc082a6389267df82546cc89e693dd2db6a" => :mavericks
-    sha256 "0bb13ba0c69e3bcf77da2edaac0b500c074724a7a191674bc2914525dbf7dcd0" => :mountain_lion
+    rebuild 2
+    sha256 "4bea5aacca509d92412687b82375ca286f327c6e0a59cc5590929e8d4f227910" => :sierra
+    sha256 "8e35b300ef94249f6b5ab5c4b627533e025d259c96d8c27870c79836bc811cfe" => :el_capitan
+    sha256 "fbf02f2fc744338eaaae12f96112e9242a48c5a72ca8fd794f4172ebdf69147e" => :yosemite
   end
 
   def install
