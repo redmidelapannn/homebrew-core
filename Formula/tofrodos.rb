f7b7596af69c9454d5f17a3b27041004b16417f8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-16 23:01:34 +0000

    tofrodos: update 1.7.13 bottle.

--- Formula/tofrodos.rb
+++ Formula/tofrodos.rb
@@ -6,10 +6,10 @@ class Tofrodos < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "3d5363cda2170ce2fbcb7e03c84f715b62ead1e5646000dd06395f5677fd2269" => :sierra
-    sha256 "4a2b22ff08d0fb65c80be7359be2f04d12b70f4e6d490b96cb819ea69b3e3d88" => :el_capitan
-    sha256 "4a5427c6870c3d4822ef4da3ddd8d79c18b91e5b7f14edb4aa449a53da70114e" => :yosemite
-    sha256 "c9759ec570e6a284b250563b8d66076401641f40c1836e293f4eab82cc9fe4ff" => :mavericks
+    rebuild 1
+    sha256 "50a2687cd3e4530b04dfd72907791d0da18912ffaafc738b01bd38fa6de3d0c9" => :sierra
+    sha256 "9013a22cbf305c5ef27d4c1579fd195f4dd6aa9992b3b1e531844be3bae5c84a" => :el_capitan
+    sha256 "103d6969f8180a7b9da4783cb0d109d15fe8ef62591b37a46aa9237452fb26d1" => :yosemite
   end
 
   def install
