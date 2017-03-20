34f7ec1b628d124ed1d0faa4b4359d51f86280dd
Viktor Szakats <vszakats@users.noreply.github.com>
2017-03-20 21:40:59 +0100

    cowsay: readd historical homepage as comment

--- Formula/cowsay.rb
+++ Formula/cowsay.rb
@@ -1,5 +1,6 @@
 class Cowsay < Formula
   desc "Configurable talking characters in ASCII art"
+  # Historical homepage: https://web.archive.org/web/20120225123719/www.nog.net/~tony/warez/cowsay.shtml
   homepage "https://github.com/tnalpgge/rank-amateur-cowsay"
   url "https://github.com/tnalpgge/rank-amateur-cowsay/archive/cowsay-3.04.tar.gz"
   sha256 "d8b871332cfc1f0b6c16832ecca413ca0ac14d58626491a6733829e3d655878b"
