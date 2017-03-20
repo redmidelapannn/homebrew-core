f265ecab4d895fb7e6ff09754874fdaa19e2d686
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-20 13:42:47 +0000

    man2html: update 3.0.1 bottle.

--- Formula/man2html.rb
+++ Formula/man2html.rb
@@ -7,11 +7,10 @@ class Man2html < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ced5194219735226831e611db4247de1affdec0c2e53e813d5c1a7c5d3bce928" => :sierra
-    sha256 "37bfcf3cab42938fff23a66429872e935b59cf769caf238928bd4acc6544d8d4" => :el_capitan
-    sha256 "2e3cc12c0e7bc0ae5b194f397874015df1fe6b8a8ab52c6972e17ad992732463" => :yosemite
-    sha256 "7ebaf5a969df65809220222b69414a51ed06b90601a28fc2ad140955e17febe0" => :mavericks
-    sha256 "82efff57b082ea9f817287e96d185bf15f351cfb0d4a7c3837f89bb5e14ce30c" => :mountain_lion
+    rebuild 1
+    sha256 "bfd9a93a20d42e7b33ebb4bd6176c6daf5a898f195daa7f5e9dcaca4d281501b" => :sierra
+    sha256 "bfd9a93a20d42e7b33ebb4bd6176c6daf5a898f195daa7f5e9dcaca4d281501b" => :el_capitan
+    sha256 "bfd9a93a20d42e7b33ebb4bd6176c6daf5a898f195daa7f5e9dcaca4d281501b" => :yosemite
   end
 
   def install
