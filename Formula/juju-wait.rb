7516057c88d73e118300edb3e892222b91854fa8
Adam Stokes <battlemidget@users.noreply.github.com>
2017-03-22 13:26:06 -0400

    dont include formula name in desc, remove unnecessary cellar data
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- Formula/juju-wait.rb
+++ Formula/juju-wait.rb
@@ -1,16 +1,13 @@
 class JujuWait < Formula
   include Language::Python::Virtualenv
 
-  desc "juju-wait, Juju plugin for waiting for deployments to settle."
+  desc "Juju plugin for waiting for deployments to settle."
   homepage "https://launchpad.net/juju-wait"
   url "https://pypi.python.org/packages/96/82/6b1b566b75f668605469d9af220bed0104bd4dc12c66160771b32f3aab58/juju-wait-2.5.0.tar.gz"
   sha256 "05354b87e65b19a67176e470b4edf2588ae3ec301576b4a5214bc698c420671e"
 
   bottle do
     cellar :any
-    sha256 "2f286917e60870d00a44d30e7bcda422555494f3357424a9c2f8297b4daf8a35" => :sierra
-    sha256 "c4e02d209d4ecd993f270a1460f406f296a6624f0354e4baffd8d865d41c5d6e" => :el_capitan
-    sha256 "58f505e114f5d0d766845b90381e93c8f7c5d7600575bb8333e07d8187371c9e" => :yosemite
   end
 
   depends_on :python3
