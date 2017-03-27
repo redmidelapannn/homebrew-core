83baf180a8bf369d5ee25471a91e07efbb9231b4
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-27 20:49:23 +0100

    jvm-mon: add 0.3 bottle.

--- Formula/jvm-mon.rb
+++ Formula/jvm-mon.rb
@@ -4,6 +4,13 @@ class JvmMon < Formula
   url "https://github.com/ajermakovics/jvm-mon/releases/download/0.3/jvm-mon-0.3.tar.gz"
   sha256 "9b5dd3d280cb52b6e2a9a491451da2ee41c65c770002adadb61b02aa6690c940"
 
+  bottle do
+    cellar :any
+    sha256 "b2729f3fb6719aaf370ee30df6f7e95fa250c3c9528e3c85a80bc71fb3085fe8" => :sierra
+    sha256 "b2729f3fb6719aaf370ee30df6f7e95fa250c3c9528e3c85a80bc71fb3085fe8" => :el_capitan
+    sha256 "b2729f3fb6719aaf370ee30df6f7e95fa250c3c9528e3c85a80bc71fb3085fe8" => :yosemite
+  end
+
   depends_on :java => "1.8+"
 
   def install
