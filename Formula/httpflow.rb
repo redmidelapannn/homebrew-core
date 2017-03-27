e5a9872eafe5e5a6bb08096de5b2f3b8e29e401b
dudongcheng <dongcheng.du@gmail.com>
2017-03-27 11:40:27 +0800

    update homepage

--- Formula/httpflow.rb
+++ Formula/httpflow.rb
@@ -1,6 +1,6 @@
 class Httpflow < Formula
   desc "Packet capture and analysis utility similar to tcpdump for HTTP"
-  homepage "https://github.com/six-ddc/httpflow/releases"
+  homepage "https://github.com/six-ddc/httpflow"
   url "https://github.com/six-ddc/httpflow/archive/0.0.3.tar.gz"
   sha256 "26e8d1f8d6c0742b552bc333edd340dc3b3e3ad4590fe2e0e125d830142f0b37"
   head "https://github.com/six-ddc/httpflow.git", :branch => "master"
