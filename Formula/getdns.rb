312bcd0b84328d86a70cfb0d5b7e2b2b300c5f79
Joel Purra <code+github@joelpurra.com>
2017-03-27 21:15:29 +0200

    Add getdns --devel

--- Formula/getdns.rb
+++ Formula/getdns.rb
@@ -3,7 +3,6 @@ class Getdns < Formula
   homepage "https://getdnsapi.net"
   url "https://getdnsapi.net/releases/getdns-1-0-0/getdns-1.0.0.tar.gz"
   sha256 "a0460269c6536501a7c0af9bc97f9339e05a012f8191d5c10f79042aa62f9e96"
-
   head "https://github.com/getdnsapi/getdns.git", :branch => "develop"
 
   bottle do
@@ -12,6 +11,11 @@ class Getdns < Formula
     sha256 "1ae532218ee2efd6c557a876d062a220ec4d604e24eca19160b394bea813a718" => :el_capitan
     sha256 "4e2eff05d371aedbd66bb428d8f01350134900ed4f4b647897d9c25b8492a45a" => :yosemite
     sha256 "18dcbddc502946fc6a146a52f255a4de75df80235b9b2dfcbaeee054fac355b2" => :mavericks
+  end
+
+  devel do
+    url "https://getdnsapi.net/releases/getdns-1-1-0-rc1/getdns-1.1.0-rc1.tar.gz"
+    sha256 "d91ec104b33880ac901f36b8cc01b22f9086fcf7d4ab94c0cbc56336d1f6bec0"
   end
 
   depends_on "openssl"
