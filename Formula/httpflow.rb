6c9f48694a7686861d15ca7116fd05a8629bc3e5
dudongcheng <dongcheng.du@gmail.com>
2017-03-27 18:16:14 +0800

    modify formula

--- Formula/httpflow.rb
+++ Formula/httpflow.rb
@@ -3,16 +3,11 @@ class Httpflow < Formula
   homepage "https://github.com/six-ddc/httpflow"
   url "https://github.com/six-ddc/httpflow/archive/0.0.3.tar.gz"
   sha256 "26e8d1f8d6c0742b552bc333edd340dc3b3e3ad4590fe2e0e125d830142f0b37"
-  head "https://github.com/six-ddc/httpflow.git", :branch => "master"
+  head "https://github.com/six-ddc/httpflow.git"
 
   def install
-    args = %W[
-      PREFIX=#{prefix}
-      CXX=#{ENV.cxx}
-    ]
-
     system "make"
-    system "make", "install", *args
+    system "make", "install", "PREFIX=#{prefix}", "CXX=#{ENV.cxx}"
   end
 
   test do
