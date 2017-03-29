c8a45bd24e16972efebcbc76f332d36b04c2e0be
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-29 11:52:44 +0100

    wordplay: update 7.22 bottle.

--- Formula/wordplay.rb
+++ Formula/wordplay.rb
@@ -6,10 +6,10 @@ class Wordplay < Formula
   sha256 "9436a8c801144ab32e38b1e168130ef43e7494f4b4939fcd510c7c5bf7f4eb6d"
 
   bottle do
-    sha256 "217fcfc1df6ae5c4dfb339fa4752a76486a4820006aac76be16a64a2d12d103d" => :sierra
-    sha256 "0f549555c287b010bd2936d355942e0b652e520a36b6a0c2b887516d08c9db36" => :el_capitan
-    sha256 "693f5920e44376dfcb1535c7dfc61c1d01a5c707756b2f05ce015486e8bd6c39" => :yosemite
-    sha256 "1255545f067e86a01a7958d212af815c155026cfc430a421555fceb1587b0931" => :mavericks
+    rebuild 1
+    sha256 "07824a81c9da37796bfea374bdf54349bc4481628438eaf9d138c774a24ab74b" => :sierra
+    sha256 "9e14d6411cde57578b760f819f1d2aff40e5984b8174d002c05ab5e70089b8fc" => :el_capitan
+    sha256 "4794a4ed8440ec2037972a239aea833d3566a2b49a3f9e3525bef5d5b7887a89" => :yosemite
   end
 
   # Fixes compiler warnings on Darwin, via MacPorts.
