3d2445f005c6d500f71bdc8f1657e4b651127e93
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-30 15:22:36 +0100

    dri3proto: add 1.0 bottle.

--- Formula/dri3proto.rb
+++ Formula/dri3proto.rb
@@ -4,6 +4,13 @@ class Dri3proto < Formula
   url "https://www.x.org/releases/individual/proto/dri3proto-1.0.tar.bz2"
   sha256 "01be49d70200518b9a6b297131f6cc71f4ea2de17436896af153226a774fc074"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d9723180ec6f0d88669518ff6e74b660900b68926166582eb5055a90f2e8e300" => :sierra
+    sha256 "d9723180ec6f0d88669518ff6e74b660900b68926166582eb5055a90f2e8e300" => :el_capitan
+    sha256 "d9723180ec6f0d88669518ff6e74b660900b68926166582eb5055a90f2e8e300" => :yosemite
+  end
+
   depends_on :x11
   depends_on "pkg-config" => :optional
 
