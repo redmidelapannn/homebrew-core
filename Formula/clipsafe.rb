e8d375fcba61256fc8b688529e3f95cf1e1721a0
FX Coudert <fxcoudert@gmail.com>
2017-03-31 16:18:53 +0200

    Update clipsafe.rb

--- Formula/clipsafe.rb
+++ Formula/clipsafe.rb
@@ -28,9 +28,21 @@ class Clipsafe < Formula
   end
 
   resource "DateTime" do
-    url "https://cpan.metacpan.org/authors/id/D/DR/DROLSKY/DateTime-1.42.tar.gz"
-    mirror "http://search.cpan.org/CPAN/authors/id/D/DR/DROLSKY/DateTime-1.42.tar.gz"
-    sha256 "efa4badf07365d1b03ee5527fc79baaf7d8b449bf7baad13599f04177232416e"
+    url "https://cpan.metacpan.org/authors/id/D/DR/DROLSKY/DateTime-1.35.tar.gz"
+    mirror "http://search.cpan.org/CPAN/authors/id/D/DR/DROLSKY/DateTime-1.35.tar.gz"
+    sha256 "42da13b8b15373c1a1790ef4217742d34f7f80f7e5c51c377cc9eed3c7641525"
+  end
+
+  resource "DateTime::Locale" do
+    url "https://cpan.metacpan.org/authors/id/D/DR/DROLSKY/DateTime-Locale-1.05.tar.gz"
+    mirror "http://search.cpan.org/CPAN/authors/id/D/DR/DROLSKY/DateTime-Locale-1.05.tar.gz"
+    sha256 "692d674558589e2a6ad2d68cf58a6e4e4b1bfe2f7f76caf20cb91b975a51d155"
+  end
+
+  resource "DateTime::TimeZone" do
+    url "https://cpan.metacpan.org/authors/id/D/DR/DROLSKY/DateTime-TimeZone-2.01.tar.gz"
+    mirror "http://search.cpan.org/CPAN/authors/id/D/DR/DROLSKY/DateTime-TimeZone-2.01.tar.gz"
+    sha256 "1f673c8156fa42b2030aae46fd7e4c117f229da99dfbc7c57ef9eadd5eaa69e1"
   end
 
   def install
@@ -49,6 +61,18 @@ class Clipsafe < Formula
     end
 
     resource("DateTime").stage do
+      system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+      system "make"
+      system "make", "install"
+    end
+
+    resource("DateTime::Locale").stage do
+      system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+      system "make"
+      system "make", "install"
+    end
+
+    resource("DateTime::TimeZone").stage do
       system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
       system "make"
       system "make", "install"
