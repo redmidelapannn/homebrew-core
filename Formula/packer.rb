53f0ecfad18dbcacbaac3c359244965c385fda9f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-03-31 02:41:15 +0100

    packer: update 0.12.3 bottle.

--- Formula/packer.rb
+++ Formula/packer.rb
@@ -10,9 +10,10 @@ class Packer < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "cc2fce1dc2b1fad1ffb48be64a5a592f92091cdaf9eb23d25591e28108744e73" => :sierra
-    sha256 "78acfb57ec3136f9d6a8e51f35d8ba737b68432496f0347887307799954e7261" => :el_capitan
-    sha256 "a7ad3c8b1e29e5dbc0a3e2186a854734857dba886dbce220164e6515d53e1c76" => :yosemite
+    rebuild 1
+    sha256 "1740da150ac0a7f01ab8b69c491270a65fb4aab9a263da7680b2fdbfb58abce6" => :sierra
+    sha256 "70c96f71c79a10c4264f6cd6a4017315d9e85e9b89124e2d6cfba77372a84155" => :el_capitan
+    sha256 "b814f8da65c12a2d7d24b09ae7d34efb763c33b382730901933b951a406cac41" => :yosemite
   end
 
   devel do
