95a06df2876932ccf462d3383bd6d765792d37f8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-01 19:42:15 +0100

    fast: add 0.0.1 bottle.

--- Formula/fast.rb
+++ Formula/fast.rb
@@ -4,6 +4,13 @@ class Fast < Formula
   url "https://github.com/yijunyu/fast/archive/v0.0.1.tar.gz"
   sha256 "844467051325cee43ba98b52e9512133ec53388153b32339190509b82570a4e9"
 
+  bottle do
+    cellar :any
+    sha256 "6f47986dee31e0981b2ad5fcbcd5d5c52c561ddc444f67c1941225bb80482ab1" => :sierra
+    sha256 "bac17a426c9c8fa4bd9662cc76d7fb69a3530fb80b8b3c4c180e935ba31dfed3" => :el_capitan
+    sha256 "08b101af324df0cd54f02a6111507386ad6bdb2ee19bb9c021ed94a66d448fc2" => :yosemite
+  end
+
   depends_on "cmake" => :build
   depends_on "flatbuffers" => :build
   depends_on "pkg-config" => :build
