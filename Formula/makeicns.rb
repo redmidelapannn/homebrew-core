54bd3127b7b2a1de1e7f8d0764d446ffbf16eb40
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-05 21:46:00 +0100

    makeicns: update 1.4.10a bottle.

--- Formula/makeicns.rb
+++ Formula/makeicns.rb
@@ -7,11 +7,10 @@ class Makeicns < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "3a673790c42724f75b905713e269f8bfa3e54bb64fde48130164c68b5656c871" => :sierra
-    sha256 "96f91bccf728f040931c2816156a7c5de739ae91e63191795cd108d0a46370ac" => :el_capitan
-    sha256 "40c3d4befe2d4625d7013ea40f307b4f5b26e122a6dad51706a25bb22734f075" => :yosemite
-    sha256 "8c54ce9e5f819dda4eb274f8bf8a22d49e1d0086e33300f236840acf1a46837f" => :mavericks
-    sha256 "dfffe46a25b846de31dc220c279a628b719a3b353aa220ed31189bc8ce5da4b2" => :mountain_lion
+    rebuild 1
+    sha256 "cbb7e92aaaeddb739f2a6b4d449bb5ccdf52f1b09eca200b314acfd624a03a1f" => :sierra
+    sha256 "e47842ebe844f59ec6aca1f33aaf0ca0da0fd895916c9426a19c9182f776300e" => :el_capitan
+    sha256 "6a62438d69032a4cac611d1c8e9ba8a2b865743b0ecb01df8d5c93f5f828d626" => :yosemite
   end
 
   patch :p0 do
