325b71bf296f074c7b668d66999721ec02849eee
Michael Goodness <mgoodness@gmail.com>
2017-04-07 11:20:39 -0500

    kubernetes-helm@2.2 (new formula)

---
+++ Formula/kubernetes-helm@2.2.rb
@@ -0,0 +1,43 @@
+class KubernetesHelmAT22 < Formula
+  desc "The Kubernetes package manager"
+  homepage "https://helm.sh/"
+  url "https://github.com/kubernetes/helm.git",
+      :tag => "v2.2.3",
+      :revision => "1402a4d6ec9fb349e17b912e32fe259ca21181e3"
+
+  keg_only :versioned_formula
+
+  depends_on :hg => :build
+  depends_on "go" => :build
+  depends_on "glide" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["GLIDE_HOME"] = HOMEBREW_CACHE/"glide_home/#{name}"
+    ENV.prepend_create_path "PATH", buildpath/"bin"
+    arch = MacOS.prefer_64_bit? ? "amd64" : "x86"
+    ENV["TARGETS"] = "darwin/#{arch}"
+    dir = buildpath/"src/k8s.io/helm"
+    dir.install buildpath.children - [buildpath/".brew_home"]
+
+    cd dir do
+      # Bootstap build
+      system "make", "bootstrap"
+
+      # Make binary
+      system "make", "build"
+      bin.install "bin/helm"
+
+      # Install bash completion
+      bash_completion.install "scripts/completions.bash" => "helm"
+    end
+  end
+
+  test do
+    system "#{bin}/helm", "create", "foo"
+    assert File.directory? "#{testpath}/foo/charts"
+
+    version_output = shell_output("#{bin}/helm version --client 2>&1")
+    assert_match "GitTreeState:\"clean\"", version_output
+  end
+end
