49c4f11707caeff0db53537f94d86533e3b13fb1
ilovezfs <ilovezfs@icloud.com>
2017-04-07 23:12:25 -0700

    archi-steam-farm 2.3.0.9

--- Formula/archi-steam-farm.rb
+++ Formula/archi-steam-farm.rb
@@ -1,9 +1,9 @@
 class ArchiSteamFarm < Formula
   desc "ASF is a C# application that allows you to farm steam cards"
   homepage "https://github.com/JustArchi/ArchiSteamFarm"
-  url "https://github.com/JustArchi/ArchiSteamFarm/releases/download/2.3.0.6/ASF.zip"
-  version "2.3.0.6"
-  sha256 "2f28fd5cd815b14ee278745be1a2e15a0cea6a2818b9be7a981edd1cd3e85ccb"
+  url "https://github.com/JustArchi/ArchiSteamFarm/releases/download/2.3.0.9/ASF.zip"
+  version "2.3.0.9"
+  sha256 "b117312f67d91c7a8d6d59d357d995458754ed456ba81251e35c47a82cd3cd89"
 
   bottle do
     cellar :any_skip_relocation
