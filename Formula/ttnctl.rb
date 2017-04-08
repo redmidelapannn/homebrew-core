66a7edf365c24d4d919df109608e4b09d17fed03
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-08 04:16:48 +0100

    ttnctl: add 2.5.3 bottle.

--- Formula/ttnctl.rb
+++ Formula/ttnctl.rb
@@ -5,6 +5,13 @@ class Ttnctl < Formula
   version "2.5.3"
   sha256 "c913ff2db1e49d30539057a707d31443bd95d484d3433545cac5b09b7a915f75"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c4868898ab9b780b2acdc13a88d1e73c93fd17c473c66c17cfeec56f959ad838" => :sierra
+    sha256 "d9c25d4a12c6701c325ab75433b770403aad4264e00173b9ded87bfa271e8b77" => :el_capitan
+    sha256 "d9c25d4a12c6701c325ab75433b770403aad4264e00173b9ded87bfa271e8b77" => :yosemite
+  end
+
   def install
     bin.install "ttnctl-darwin-amd64"
     mv "#{bin}/ttnctl-darwin-amd64", "#{bin}/ttnctl"
