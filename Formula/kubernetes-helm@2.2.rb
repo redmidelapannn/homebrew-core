053ab5624fc5bf59ee4c212ff10ec871c4c316e7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-08 07:31:15 +0100

    kubernetes-helm@2.2: add 2.2.3 bottle.

--- Formula/kubernetes-helm@2.2.rb
+++ Formula/kubernetes-helm@2.2.rb
@@ -5,6 +5,13 @@ class KubernetesHelmAT22 < Formula
       :tag => "v2.2.3",
       :revision => "1402a4d6ec9fb349e17b912e32fe259ca21181e3"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4d2670e0090b548c2fc8c7fa509a8b04c5f6e6e168c36b2e272961b83ae2cd9a" => :sierra
+    sha256 "b1881bff80f72907fb7cac15ef2cd7ebd9df0f7521fbf7e9f2d7508182d55141" => :el_capitan
+    sha256 "6ca551c393193841fd347b5190c3fe7378c4a4d5fd7c1f4e5e8508c20108f959" => :yosemite
+  end
+
   keg_only :versioned_formula
 
   depends_on :hg => :build
