49a836d3cce08eee5ab44be27a6fedc5179949d8
Jacob Zimmerman <zimmerman.jake@gmail.com>
2017-04-09 16:11:37 -0400

    sc-im 0.5.0 (new formula)
    - [x] Have you followed the [guidelines for
      contributing][guidelines]?
    - [x] Have you checked that there aren't other open [pull
      requests][pulls] for the same formula update/change?
    - [x] Have you built your formula locally with `brew install
      --build-from-source <formula>`, where `<formula>` is the name of the
      formula you're submitting?
    - [x] Does your build pass `brew audit --strict <formula>` (after doing
      `brew install <formula>`)?
    
    [guidelines]: https://github.com/Homebrew/homebrew-core/blob/master/CONTRIBUTING.md
    [pulls]: https://github.com/Homebrew/homebrew-core/pulls
    -----
    
    One thing to note is that `sc-im` doesn't pass this check:
    
    ```
    ❯ brew audit --new-formula sc-im
    sc-im:
      * Dependency 'ncurses' may be unnecessary as it is provided by macOS; try to build this formula without it.
      * Dependency 'ncurses' may be unnecessary as it is provided by macOS; try to build this formula without it.
    Error: 2 problems in 1 formula
    ```
    
    When I built it against the macOS default ncurses, there were some
    visual glitches where the screen wouldn't always reflow correctly. I'm
    not sure if I'm supposed to silence this error by modifying the formula
    somehow, or if it's fine as it is.

---
+++ Formula/sc-im.rb
@@ -0,0 +1,20 @@
+class ScIm < Formula
+  desc "Spreadsheet program for the terminal, using ncurses"
+  homepage "https://github.com/andmarti1424/sc-im"
+  url "https://github.com/andmarti1424/sc-im/archive/v0.5.0.tar.gz"
+  sha256 "d7a31c5225e02239e925b50b414d7e69d12bc3554f218621823782872ccc5e4d"
+  head "https://github.com/andmarti1424/sc-im.git"
+
+  depends_on "ncurses"
+
+  def install
+    cd "src" do
+      system "make", "prefix=#{prefix}", "LDFLAGS=-L#{Formula["ncurses"].lib}"
+      system "make", "prefix=#{prefix}", "install"
+    end
+  end
+
+  test do
+    system "#{bin}/scim", "--quit_afterload"
+  end
+end
