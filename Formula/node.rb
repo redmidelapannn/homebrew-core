c1c9eecbd793aef4e5a41bf34b7b7c7c6ad8a3c3
Frederick Stark <coagmano@gmail.com>
2017-04-12 10:04:29 +1000

    node 7.9.0

--- Formula/node.rb
+++ Formula/node.rb
@@ -1,8 +1,8 @@
 class Node < Formula
   desc "Platform built on V8 to build network applications"
   homepage "https://nodejs.org/"
-  url "https://nodejs.org/dist/v7.8.0/node-v7.8.0.tar.xz"
-  sha256 "6821aaee58bbc8bc8d08fec6989a42278b725a21382500dc20fd9d9f71398f02"
+  url "https://nodejs.org/dist/v7.9.0/node-v7.9.0.tar.gz"
+  sha256 "5cc131bb16f7ca688c29634a18b3c6a7816609a431a509f31baedf7caf25d626"
   head "https://github.com/nodejs/node.git"
 
   bottle do
