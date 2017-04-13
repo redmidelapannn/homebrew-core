333749c704a23ac4739bec71adff44316ee8d431
rollbrettler <rollbrettler@users.noreply.github.com>
2017-04-13 14:36:43 +0200

    Add formula to install immortal

---
+++ Formula/immortal.rb
@@ -0,0 +1,32 @@
+class Immortal < Formula
+  desc "OS agnostic (*nix) cross-platform supervisor"
+  homepage "https://immortal.run/"
+  url "https://github.com/immortal/immortal.git",
+      :tag => "0.13.1",
+      :revision => "8ec87531e9f5d2ce1ec64e76c8bbae5dc58feecf"
+
+  head "https://github.com/immortal/immortal.git"
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "179e07bb22e2755e3d082f07607cf2e98e266defaace3ceb764a4c32f03ccf8d" => :sierra
+    sha256 "5be8ef01a8568b7de75b19d525a4830b0c934c43b9e8f9fb150cbdcb18091a26" => :el_capitan
+    sha256 "6dcf9d7de2489f5e3d6fd56488c3abe2bff1ea6f6bf0d9de89421b0e9a7e2e38" => :yosemite
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/immortal/immortal").install buildpath.children
+    cd "src/github.com/immortal/immortal" do
+      system "make", "install", "DESTDIR=#{prefix}"
+    end
+  end
+
+  test do
+    system bin/"immortal", "-v"
+    system bin/"immortalctl", "-v"
+    system bin/"immortaldir", "-v"
+  end
+end
