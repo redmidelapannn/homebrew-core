3034b11641ad6032437c8fafbb932db2e90cc293
Jake McKnight <jake.mcknight@gmail.com>
2017-04-14 20:41:40 -0700

    Change command line to command-line.

--- Formula/historian.rb
+++ Formula/historian.rb
@@ -1,5 +1,5 @@
 class Historian < Formula
-  desc "Command line utility for managing shell history in a SQLite database."
+  desc "Command-line utility for managing shell history in a SQLite database."
   homepage "https://github.com/jcsalterego/historian"
   url "https://github.com/jcsalterego/historian/archive/0.0.2.tar.gz"
   sha256 "80981cf45b6c51dd2969a6d5cd2d69a488e9a58a411fc51d5bee68aaf267d7a5"
