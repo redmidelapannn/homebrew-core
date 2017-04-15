729dea95306650a9008b37655c6e2a6b6db7c6bd
Jake McKnight <jake.mcknight@gmail.com>
2017-04-14 21:06:06 -0700

    Fix: remove unnecessary sqlite dependency.

--- Formula/historian.rb
+++ Formula/historian.rb
@@ -4,8 +4,6 @@ class Historian < Formula
   url "https://github.com/jcsalterego/historian/archive/0.0.2.tar.gz"
   sha256 "691b131290ddf06142a747755412115fec996cb9cc2ad8e8f728118788b3fe05"
 
-  depends_on "sqlite"
-
   def install
     bin.install "hist"
   end
