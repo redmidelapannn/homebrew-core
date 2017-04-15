f3823ac34f52ee441a513029f529bc5138511ff0
Jake McKnight <jake.mcknight@gmail.com>
2017-04-14 20:35:36 -0700

    historian 0.0.2 - Changed test from version check to importing a dummy history file. - Updated to 0.0.2 release of historian.

--- Formula/historian.rb
+++ Formula/historian.rb
@@ -1,7 +1,7 @@
 class Historian < Formula
-  desc "Command-line utility for managing shell history in a SQLite database."
+  desc "Command line utility for managing shell history in a SQLite database."
   homepage "https://github.com/jcsalterego/historian"
-  url "https://files.jakemcknight.com/historian-0.1.tar.gz"
+  url "https://github.com/jcsalterego/historian/archive/0.0.2.tar.gz"
   sha256 "80981cf45b6c51dd2969a6d5cd2d69a488e9a58a411fc51d5bee68aaf267d7a5"
 
   depends_on "sqlite"
@@ -11,6 +11,7 @@ class Historian < Formula
   end
 
   test do
-    system "/usr/local/bin/hist", "version"
+    ENV["HISTORIAN_SRC"] = "./.bash_history"
+    system bin/"hist", "import"
   end
 end
