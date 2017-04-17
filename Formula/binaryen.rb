340ec98658aff4596aad4d525b1ae80c255b3b8c
Alexander Regueiro <alexreg@me.com>
2017-04-17 23:45:04 +0100

    binaryen: added patch that fixes some install locations

--- Formula/binaryen.rb
+++ Formula/binaryen.rb
@@ -1,14 +1,19 @@
 class Binaryen < Formula
   desc "Compiler infrastructure and toolchain library for WebAssembly"
   homepage "http://webassembly.org/"
-  url "https://github.com/WebAssembly/binaryen/archive/version_30.tar.gz"
-  sha256 "66635b931f1daa5a4f912887388ec972d3d7dde3f2852f9f5cfa51a3e6ddf9a5"
+  url "https://github.com/WebAssembly/binaryen/archive/version_31.tar.gz"
+  sha256 "31007e95b3b207497879d171bb2c557aa5625a14087aaa57de05d5641f7fb7c9"
 
   head "https://github.com/WebAssembly/binaryen.git"
 
   depends_on "cmake" => :build
 
   needs :cxx11
+
+  patch do
+    url "https://github.com/WebAssembly/binaryen/commit/0f3102d13617e70b598e7d82f12684d8fa01969b.patch"
+    sha256 "0182bc6310219b7993553c5155661db780c75b3c1372552bd58f364c594324ad"
+  end
 
   def install
     ENV.cxx11
