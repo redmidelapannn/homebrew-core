95312029d1f9f798463e35bc8e25fa2b6c66633b
Paul J. Lucas <plucas@splunk.com>
2017-04-25 15:13:20 -0700

    Put "bottle" block back.

--- Formula/cdecl.rb
+++ Formula/cdecl.rb
@@ -4,6 +4,14 @@ class Cdecl < Formula
   url "https://github.com/paul-j-lucas/cdecl/releases/download/cdecl-3.0.1/cdecl-3.0.1.tar.gz"
   sha256 "68437d334f1392f2e0555ae65cc9de5e13d8f9d13f9363e893c90d9654e11e37"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1d424613881cf9109d824664fc77fc947f2968b9850d448db4b02c6f0a562b5c" => :sierra
+    sha256 "4f0e990d88823aa9f3d1dcea71ffa442c13640ce82cc9da41f90a1be5ef457dc" => :el_capitan
+    sha256 "e8f53a0e5b3649f0c691c60380b9c77af573387240f3479a41550583fcc4e22c" => :yosemite
+    sha256 "b1e1618d0f1bcbb801c669c314c36c72e47e8829950a8bf0899d0517f3036ccc" => :mavericks
+  end
+
   def install
     ENV.deparallelize # must ensure parser.h builds first
 
