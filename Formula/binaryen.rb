15a2df3e786a8b2d6cdeb99f8fb67c5629b970df
Alexander Regueiro <alexreg@me.com>
2017-04-26 03:40:06 +0100

    binaryen 32

--- Formula/binaryen.rb
+++ Formula/binaryen.rb
@@ -1,8 +1,8 @@
 class Binaryen < Formula
   desc "Compiler infrastructure and toolchain library for WebAssembly"
   homepage "http://webassembly.org/"
-  url "https://github.com/WebAssembly/binaryen/archive/version_31.tar.gz"
-  sha256 "31007e95b3b207497879d171bb2c557aa5625a14087aaa57de05d5641f7fb7c9"
+  url "https://github.com/WebAssembly/binaryen/archive/version_32.tar.gz"
+  sha256 "724dfdeb0ea1130ba86dde207635a4eaf32e1acabe1e24d02e99f7d00ef59f56"
 
   head "https://github.com/WebAssembly/binaryen.git"
 
@@ -10,22 +10,17 @@ class Binaryen < Formula
 
   needs :cxx11
 
-  patch do
-    url "https://github.com/WebAssembly/binaryen/commit/0f3102d13617e70b598e7d82f12684d8fa01969b.patch"
-    sha256 "0182bc6310219b7993553c5155661db780c75b3c1372552bd58f364c594324ad"
-  end
-
   def install
     ENV.cxx11
 
     system "cmake", ".", *std_cmake_args
     system "make", "install"
 
-    prefix.install "test/"
+    pkgshare.install "test/"
   end
 
   test do
-    system "#{bin}/wasm-opt", "#{prefix}/test/passes/O.wast"
-    system "#{bin}/asm2wasm", "#{prefix}/test/hello_world.asm.js"
+    system "#{bin}/wasm-opt", "#{pkgshare}/test/passes/O.wast"
+    system "#{bin}/asm2wasm", "#{pkgshare}/test/hello_world.asm.js"
   end
 end
