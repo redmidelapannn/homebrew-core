7caa7ef5fa74f0ce490d8df63338901b9af530f4
Alexander Hamann <neutric@users.noreply.github.com>
2017-04-26 10:46:40 +0200

    pure-ftpd: add another mirror

--- Formula/pure-ftpd.rb
+++ Formula/pure-ftpd.rb
@@ -2,6 +2,7 @@ class PureFtpd < Formula
   desc "Secure and efficient FTP server"
   homepage "https://www.pureftpd.org/"
   url "https://download.pureftpd.org/pub/pure-ftpd/releases/pure-ftpd-1.0.46.tar.gz"
+  mirror "https://fossies.org/linux/misc/pure-ftpd-1.0.46.tar.gz"
   mirror "ftp://ftp.pureftpd.org/pub/pure-ftpd/releases/pure-ftpd-1.0.46.tar.gz"
   sha256 "0609807335aade4d7145abdbb5cb05c9856a3e626babe90658cb0df315cb0a5c"
 
