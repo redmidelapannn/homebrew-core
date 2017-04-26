9517dcd790f2f6016943628c4f45b62fb9e25f10
Dominyk Tiller <dominyktiller@gmail.com>
2017-04-26 18:44:37 +0100

    libidn 2.0.2

--- Formula/libidn.rb
+++ Formula/libidn.rb
@@ -1,9 +1,9 @@
 class Libidn < Formula
-  desc "International domain name library"
-  homepage "https://www.gnu.org/software/libidn/"
-  url "https://ftp.gnu.org/gnu/libidn/libidn-1.33.tar.gz"
-  mirror "https://ftpmirror.gnu.org/libidn/libidn-1.33.tar.gz"
-  sha256 "44a7aab635bb721ceef6beecc4d49dfd19478325e1b47f3196f7d2acc4930e19"
+  desc "International domain name library (IDNA2008, Punycode and TR46)"
+  homepage "https://www.gnu.org/software/libidn/#libidn2"
+  url "https://ftp.gnu.org/gnu/libidn/libidn2-2.0.2.tar.gz"
+  mirror "https://ftpmirror.gnu.org/libidn/libidn2-2.0.2.tar.gz"
+  sha256 "8cd62828b2ab0171e0f35a302f3ad60c3a3fffb45733318b3a8205f9d187eeab"
 
   bottle do
     cellar :any
@@ -13,18 +13,39 @@ class Libidn < Formula
     sha256 "07e19d25263d77030cccc3899967c4505dcf0c771da90a658b4f27de136a326b" => :mavericks
   end
 
+  head do
+    url "https://gitlab.com/libidn/libidn2.git"
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+    depends_on "libtool" => :build
+    depends_on "gettext" => :build
+    depends_on "gengetopt" => :build
+  end
+
   depends_on "pkg-config" => :build
+  depends_on "libunistring"
 
   def install
+    system "./bootstrap" if build.head?
     system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
                           "--prefix=#{prefix}",
-                          "--disable-csharp",
-                          "--with-lispdir=#{elisp}"
+                          "--with-packager=Homebrew"
     system "make", "install"
   end
 
   test do
     ENV["CHARSET"] = "UTF-8"
-    system bin/"idn", "räksmörgås.se", "blåbærgrød.no"
+    system bin/"idn2", "räksmörgås.se", "blåbærgrød.no"
+
+    # Compatibility test for libidn 1.x functions.
+    (testpath/"test.c").write <<-EOS.undent
+      #include <idn2.h>
+      int main() {
+        return idna_to_ascii_lz(NULL, NULL, 0);
+      }
+    EOS
+    system ENV.cc, "test.c", "-lidn2", "-o", "test"
+    system "./test"
   end
 end
