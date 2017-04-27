f29f30ac1e362c528b834449336a2deb7f98c322
Chris Long <christopherglong@mac.com>
2017-04-26 20:42:02 -0700

    firebase-cli: fix description per audit

--- Formula/firebase-cli.rb
+++ Formula/firebase-cli.rb
@@ -1,7 +1,7 @@
 require "language/node"
 
 class FirebaseCli < Formula
-  desc "Firebase Command Line Tools"
+  desc "Firebase command-line tools"
   homepage "https://firebase.google.com/docs/cli/"
   url "https://registry.npmjs.org/firebase-tools/-/firebase-tools-3.6.1.tgz"
   sha256 "0235f8752d8ab7db647c07a255cfbe0a3db62c75e770595170fb9126ce8456b8"
