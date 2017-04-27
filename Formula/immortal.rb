6ecf3e9e68fbbb5dfa848ed8180fb88c04661957
FX Coudert <fxcoudert@gmail.com>
2017-04-27 10:00:32 +0200

    Update immortal.rb
    We prefer to use source archives (for caching). And the `bottle` block is added automatically.

--- Formula/immortal.rb
+++ Formula/immortal.rb
@@ -1,18 +1,9 @@
 class Immortal < Formula
   desc "OS agnostic (*nix) cross-platform supervisor"
   homepage "https://immortal.run/"
-  url "https://github.com/immortal/immortal.git",
-      :tag => "0.13.2",
-      :revision => "7dbcf52fa0d68c11051d5f2b621202aded3242fe"
-
+  url "https://github.com/immortal/immortal/archive/0.13.2.tar.gz"
+  sha256 "f34674720828c984ef34df33b75b614fabe81e4fd50eb152746a33a273daa4f9"
   head "https://github.com/immortal/immortal.git"
-
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "179e07bb22e2755e3d082f07607cf2e98e266defaace3ceb764a4c32f03ccf8d" => :sierra
-    sha256 "5be8ef01a8568b7de75b19d525a4830b0c934c43b9e8f9fb150cbdcb18091a26" => :el_capitan
-    sha256 "6dcf9d7de2489f5e3d6fd56488c3abe2bff1ea6f6bf0d9de89421b0e9a7e2e38" => :yosemite
-  end
 
   depends_on "go" => :build
 
