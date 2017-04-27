5b5fc7f3c57c77aeb97e998e7284e37c58eb6b57
Ondřej Surý <ondrej@sury.org>
2017-04-27 17:19:19 +0200

    lmdb: add simple lmdb.pc pkg-config file

--- Formula/lmdb.rb
+++ Formula/lmdb.rb
@@ -8,10 +8,8 @@ class Lmdb < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "31c30590d1d685e7418d45b50b5e636cea5ea5147b472172ff4f809686297892" => :sierra
-    sha256 "655ec022ac655fde5a3c88ca4b71c0c6942a0c758e12262d495d15747fc65ecd" => :el_capitan
-    sha256 "36f056edff5219f3efca010d290f8882d5dd053c10cf8f1d673a7a6477f7b20e" => :yosemite
+    rebuild 2
+    sha256 "2479fecf209528e4a8ea9af3a692c28b32fe775f3ee4b9bf391887ae942cb9e1" => :sierra
   end
 
   def install
@@ -20,6 +18,23 @@ class Lmdb < Formula
       system "make", "test", "SOEXT=.dylib"
       system "make", "install", "SOEXT=.dylib", "prefix=#{prefix}"
     end
+    (buildpath/"lmdb.pc").write(lmdb_pc)
+    (lib/"pkgconfig").install "lmdb.pc"
+  end
+
+  def lmdb_pc; <<-EOS.undent
+    prefix=#{prefix}
+    exec_prefix=#{prefix}
+    libdir=${exec_prefix}/lib
+    includedir=${prefix}/include
+
+    Name: liblmdb
+    Description: Lightning Memory-Mapped Database
+    URL: https://symas.com/products/lightning-memory-mapped-database/
+    Version: #{version}
+    Libs: -L${libdir} -llmdb
+    Cflags: -I${includedir}
+    EOS
   end
 
   test do
