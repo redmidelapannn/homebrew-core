2e027b36795d2c73a16ea98cac64d4a4898c6afd
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-27 15:44:06 +0000

    terminally-tetris: add 1.0.0 bottle.

--- Formula/terminally-tetris.rb
+++ Formula/terminally-tetris.rb
@@ -4,6 +4,13 @@ class TerminallyTetris < Formula
   url "https://github.com/thecardkid/terminally-tetris/archive/v1.0.0.tar.gz"
   sha256 "213f8a7581e69d3284291187eb887a1c0822fe699d53a7768f0fea777bc1427b"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ba8f60970e88bd8ccaaad77d1349b31ec109f82390d22d938c2fe1125c51021c" => :sierra
+    sha256 "31d85ecd3882e8b9abef2aa7638025128a5d6070ba99b2a54640932a64c9f713" => :el_capitan
+    sha256 "ff57ceb2afdffc4ac4f77cd33daa76cf5691d9cbb052462490b05efd064dcc00" => :yosemite
+  end
+
   depends_on "cmake" => :build
 
   def install
