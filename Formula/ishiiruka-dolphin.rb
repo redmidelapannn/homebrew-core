7ad79b1d3c99dd541a594a8c2f074a445e6f5cf6
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-28 07:59:17 +0000

    ishiiruka-dolphin: add 5.0 bottle.

--- Formula/ishiiruka-dolphin.rb
+++ Formula/ishiiruka-dolphin.rb
@@ -6,6 +6,13 @@ class IshiirukaDolphin < Formula
   sha256 "8a7b46ff645cd0303a5c7aaaf60bf2f8d39ac1c7a0df161d0385d064812f8e02"
   head "https://github.com/Tinob/Ishiiruka.git"
 
+  bottle do
+    cellar :any
+    sha256 "bdd1e7089eb494a7887cf0dbb2d9230d7bbddca9bcc0f4f03cedca86872a2938" => :sierra
+    sha256 "007884ed4fa29b783a331db06f73de37029d5b8e7a9bcd67b5ddec835b2d98b3" => :el_capitan
+    sha256 "24daccdb5ffa83d14adc11fcf65f2a16970a344111d6932a7b946508c34efafe" => :yosemite
+  end
+
   option "with-avi-frame-dumps", "AVI Frame dumping support"
 
   depends_on "cmake" => :build
