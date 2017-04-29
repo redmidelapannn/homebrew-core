ae7eb7edd42ee283a9a080a1f391587c69a2340f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-29 09:42:15 +0000

    snapcast: add 0.0.10_1 bottle.

--- Formula/snapcast.rb
+++ Formula/snapcast.rb
@@ -5,6 +5,13 @@ class Snapcast < Formula
   version "0.0.10"
   sha256 "e4a9e12aca8cfeed5e51866a0143e7e8d0a6e1ce0c076e8f78ce23c5c735c8d3"
   revision 1
+  bottle do
+    cellar :any
+    sha256 "d9b1e94e0b0215aa8a1934d1c0d975c2fe6ec98b94f9fdf6b6fd6a660a31df95" => :sierra
+    sha256 "60e2331822777358121adf697e268a8b21e0beaedf4e59df611f86198a62beb5" => :el_capitan
+    sha256 "f6cd2f6588289742b4569ce9cf7c6adcb99aa763e5dc9494a5a067c08c2e9c03" => :yosemite
+  end
+
   head do
     url "https://github.com/badaix/snapcast.git"
   end
