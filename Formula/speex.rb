74787415180baef689590c86eaa6246e188edf34
s172262 <s172262@users.noreply.github.com>
2017-04-30 23:48:41 +0900

    Speex : add "fftw" option

--- Formula/speex.rb
+++ Formula/speex.rb
@@ -3,6 +3,7 @@ class Speex < Formula
   homepage "https://speex.org"
   url "http://downloads.us.xiph.org/releases/speex/speex-1.2.0.tar.gz"
   sha256 "eaae8af0ac742dc7d542c9439ac72f1f385ce838392dc849cae4536af9210094"
+  revision 1
 
   bottle do
     cellar :any
@@ -13,11 +14,18 @@ class Speex < Formula
 
   depends_on "pkg-config" => :build
   depends_on "libogg" => :recommended
+  depends_on "fftw" => :optional
 
   def install
     ENV.deparallelize
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    args = %W[
+      --prefix=#{prefix}
+      --disable-debug
+      --disable-dependency-tracking
+      --enable-sse
+    ]
+    args << "--with-fft=gpl-fftw3" if build.with? "fftw"
+    system "./configure", *args
     system "make", "install"
   end
 end
