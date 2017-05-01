35cda11aaa22cdec4a709eef37f1cf301e1c1c91
Ralic Lo <raliclo@gmail.com>
2017-05-01 05:13:24 -0700

    Upgrade gradle to 4.0

--- Formula/gradle.rb
+++ Formula/gradle.rb
@@ -1,8 +1,8 @@
 class Gradle < Formula
   desc "Build system based on the Groovy language"
   homepage "https://www.gradle.org/"
-  url "https://services.gradle.org/distributions/gradle-3.5-all.zip"
-  sha256 "d84bf6b6113da081d0082bcb63bd8547824c6967fe68704d1e3a6fde822b7212"
+  url "https://services.gradle.org/distributions-snapshots/gradle-4.0-20170426102539+0000-bin.zip"
+  sha256 "4e502f1c152c0df787e7cd8fcb043c3d03a8c65cf1d8ae46c9475ddbc80ceebb"
 
   bottle :unneeded
 
