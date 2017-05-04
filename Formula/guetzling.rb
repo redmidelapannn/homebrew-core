c20ce8e0d47ef8f2b90b0852025a44a8654044b9
Jacob Roy <lejacobroy@gmail.com>
2017-05-04 14:42:09 -0400

    Add the Guetzling formula.

--- Formula/guetzling.rb
+++ Formula/guetzling.rb
@@ -1,18 +1,17 @@
 class Guetzling < Formula
-  desc "Guetzling is a simple script for macOS and Linux written in Bash, to automate (recursively finding files) the compression of jpegs using the Guetzli algorithm."
+  desc "A tool to automate image compression using the Guetzli algorithm."
   homepage "https://github.com/lejacobroy/Guetzling/tree/1.0.0"
   url "https://github.com/lejacobroy/Guetzling/archive/1.0.0.tar.gz"
   sha256 "a645587ad55916a1e977fb4b457390d6bef354e6b871b3a21514a9501e3a870f"
 
-   depends_on "guetzli"
+  depends_on "guetzli"
 
   def install
-
-    bin.install 'guetzling'
+    bin.install "guetzling"
   end
 
   test do
-    system "curl http://i.imgur.com/BljEIfj.jpg"
+    system "curl" "http://i.imgur.com/BljEIfj.jpg"
     system "#{bin}/guetzling"
     system "true"
   end
