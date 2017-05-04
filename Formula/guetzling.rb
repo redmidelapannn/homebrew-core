9c83d7718627e7ad5f72578ea6e83d9e213df449
Jacob Roy <lejacobroy@gmail.com>
2017-05-04 14:44:19 -0400

    Guetzling 1.0.1 (new formula)\n\nThis commit adds the Guetzling formula to Homebrew. Guetzling is a small script that automate the compression of images using google's Guetzli Library.

--- Formula/guetzling.rb
+++ Formula/guetzling.rb
@@ -1,5 +1,5 @@
 class Guetzling < Formula
-  desc "A tool to automate image compression using the Guetzli algorithm."
+  desc "Tool to automate image compression using the Guetzli algorithm."
   homepage "https://github.com/lejacobroy/Guetzling/tree/1.0.0"
   url "https://github.com/lejacobroy/Guetzling/archive/1.0.0.tar.gz"
   sha256 "a645587ad55916a1e977fb4b457390d6bef354e6b871b3a21514a9501e3a870f"
@@ -11,7 +11,7 @@ class Guetzling < Formula
   end
 
   test do
-    system "curl" "http://i.imgur.com/BljEIfj.jpg"
+    system "curl", "http://i.imgur.com/BljEIfj.jpg"
     system "#{bin}/guetzling"
     system "true"
   end
