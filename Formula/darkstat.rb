b6157b88caec2bb3425b0cdfb73cd5d715ca71e0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-07 10:11:48 +0000

    darkstat: update 3.0.719 bottle.

--- Formula/darkstat.rb
+++ Formula/darkstat.rb
@@ -6,10 +6,10 @@ class Darkstat < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "8cfff973e95ff4c31248690df0ae28e05ddbef97f926ed4f075b919274c59116" => :el_capitan
-    sha256 "290629ecfb0a650104bd6560bb352af9b54e2d0c1e1e0de0d7113dab13167133" => :yosemite
-    sha256 "c613e70eb9f84aa7acaef6f1791495762537ab0fe12368ddec009a66fb91d3f8" => :mavericks
-    sha256 "fba985f30c240602c9b5ebccda87fcea7c52caba69c4c8cc5375e090a773ce19" => :mountain_lion
+    rebuild 1
+    sha256 "2e5c5dc0412b544835c8e84bccc8bce9b07b40267da255dff0ec9aeeaba57131" => :sierra
+    sha256 "39bd8a48b448cdfb0998bc222ab40e91afdeea0da5097cb6d02ece2d67aedf57" => :el_capitan
+    sha256 "9b1cd336bed92c630aa1f3fdb6ebff617b8e0293b3dfba8064b65d2ba2073cfa" => :yosemite
   end
 
   head do
