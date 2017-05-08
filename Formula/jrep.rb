fa263ed98f211d4bf9cb44ffed7753055d05fdab
Aleksey Cheusov <vle@gmx.net>
2017-05-08 16:59:13 +0300

    new formulae "jrep" (grep-like utility written in Java) was added

---
+++ Formula/jrep.rb
@@ -0,0 +1,23 @@
+class Jrep < Formula
+  desc "grep-like utility written in Java"
+  homepage "https://github.com/cheusov/jrep"
+  url "https://github.com/cheusov/jrep/archive/1.1.1.tar.gz"
+  sha256 "0d56a9892a7abf0f53a52aa448fac90cd07b78fa08fa88e1567a95157c7d2fac"
+
+  depends_on "maven" => :build
+  depends_on "mk-configure" => :build
+  depends_on :java => "1.7+"
+
+  def install
+    ENV["JAVA_HOME"] = `/usr/libexec/java_home`.chomp
+    ENV["MAVEN_OPTS"] = "-Dmaven.repo.local=" + `pwd`.chomp + "/m2repo/"
+    ENV["PREFIX"] = "#{prefix}"
+    
+    system "mkcmake"
+    system "mkcmake", "install"
+  end
+
+  test do
+    system "jrep", "a", "/etc/hosts"
+  end
+end
