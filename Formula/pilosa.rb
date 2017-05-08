9040ac1250b955a6c0de50a51b45a89e7d51fb71
Brian Gyss <brian.gyss@gmail.com>
2017-05-08 07:45:20 -0700

    updates to test and plist

--- Formula/pilosa.rb
+++ Formula/pilosa.rb
@@ -20,7 +20,45 @@ class Pilosa < Formula
            "github.com/pilosa/pilosa/cmd/pilosa"
   end
 
+  plist_options :manual => "pilosa server"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+            <string>#{bin}/pilosa</string>
+            <string>server</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <dict>
+            <key>SuccessfulExit</key>
+            <false/>
+        </dict>
+        <key>WorkingDirectory</key>
+        <string>#{var}</string>
+      </dict>
+    </plist>
+    EOS
+  end
+
   test do
-    system "#{bin}/pilosa", "--help"
+    begin
+      server = fork do
+        exec "#{bin}/pilosa", "server", "--bind", "10101"
+      end
+      sleep 0.5
+      # Test that the response is from webdis
+      assert_match(/^Welcome. Pilosa is running.*/, shell_output("curl localhost:10101"))
+    ensure
+      Process.kill "TERM", server
+      Process.wait server
+    end
   end
 end
