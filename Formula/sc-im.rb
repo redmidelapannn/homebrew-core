06612323fc0deb5074d4bf0a2174261356100622
Claudia <claui@users.noreply.github.com>
2017-05-10 14:10:51 +0200

    sc-im 0.6.0

--- Formula/sc-im.rb
+++ Formula/sc-im.rb
@@ -1,15 +1,13 @@
 class ScIm < Formula
   desc "Spreadsheet program for the terminal, using ncurses"
   homepage "https://github.com/andmarti1424/sc-im"
-  url "https://github.com/andmarti1424/sc-im/archive/v0.5.0.tar.gz"
-  sha256 "d7a31c5225e02239e925b50b414d7e69d12bc3554f218621823782872ccc5e4d"
+  url "https://github.com/andmarti1424/sc-im/archive/v0.6.0.tar.gz"
+  sha256 "5da644d380ab3752de283b83cce18c3ba12b068d0762c44193c34367a0dcbc38"
   head "https://github.com/andmarti1424/sc-im.git", :branch => "freeze"
 
   depends_on "ncurses"
 
   def install
-    ENV.prepend "LDFLAGS", "-L#{Formula["ncurses"].opt_lib}"
-
     cd "src" do
       system "make", "prefix=#{prefix}"
       system "make", "prefix=#{prefix}", "install"
@@ -22,7 +20,7 @@ class ScIm < Formula
       getnum A1
     EOS
     output = pipe_output(
-      "#{bin}/scim --nocurses --quit_afterload", input, 0
+      "#{bin}/scim --nocurses --quit_afterload 2>/dev/null", input
     )
     assert_equal "nowide 2", output.lines.last.chomp
   end
