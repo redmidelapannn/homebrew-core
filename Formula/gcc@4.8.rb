f686652db9c44cfea2f4d84913252e06a39e11bd
Viktor Szakats <vszakats@users.noreply.github.com>
2017-05-10 12:32:03 +0200

    gcc@4.8: secure/git head url(s)

--- Formula/gcc@4.8.rb
+++ Formula/gcc@4.8.rb
@@ -26,7 +26,7 @@ class GccAT48 < Formula
   mirror "https://ftpmirror.gnu.org/gcc/gcc-4.8.5/gcc-4.8.5.tar.bz2"
   sha256 "22fb1e7e0f68a63cee631d85b20461d1ea6bda162f03096350e38c8d427ecf23"
 
-  head "svn://gcc.gnu.org/svn/gcc/branches/gcc-4_8-branch"
+  head "https://gcc.gnu.org/git/gcc.git", :branch => "gcc-4_8-branch"
 
   bottle do
     rebuild 2
