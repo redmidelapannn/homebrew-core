dc541e10d630f05f3453a38435f7340264e6106a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-10 18:21:11 +0000

    unifdef: update 2.11 bottle.

--- Formula/unifdef.rb
+++ Formula/unifdef.rb
@@ -7,10 +7,10 @@ class Unifdef < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "be948ffc760273a09943faa7c156e4014534da82c78bc2c3c4f2a4a4416e962e" => :sierra
-    sha256 "a89e5cc9b179fa5135077ad0c27e34cebe13a33dc02adccab4969855ba173357" => :el_capitan
-    sha256 "3277bf0977c385e3cf5ccf3355e11f30b057c58d8539a27d5a23531ce7de9542" => :yosemite
-    sha256 "8533d1ce7b70e51256a3c24d557345b7b7ac0b7e6562de5a0f942c058ae518db" => :mavericks
+    rebuild 1
+    sha256 "924a80e6dd9f094aa62abc2184dee7a2d4cef21f66210524c6284acd443c7e75" => :sierra
+    sha256 "40e78cdb3c886982eab5a501a5be6f96755a010421f7d74f1298b27f5fb22fb2" => :el_capitan
+    sha256 "04c9e79a66b3781777be8eafd4e34fd7c31faeb4b849041d699e28a7210def0c" => :yosemite
   end
 
   keg_only :provided_by_osx, "the unifdef provided by Xcode cannot compile gevent"
