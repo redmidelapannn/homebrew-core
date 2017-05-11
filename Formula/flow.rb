87abb43ff7f3243c75c9f230f91a21eea0bfa4fa
ilovezfs <ilovezfs@icloud.com>
2017-05-10 20:54:42 -0700

    Update flow.rb

--- Formula/flow.rb
+++ Formula/flow.rb
@@ -1,12 +1,9 @@
 class Flow < Formula
   desc "Static type checker for JavaScript"
   homepage "https://flowtype.org/"
+  url "https://github.com/facebook/flow/archive/v0.46.0.tar.gz"
+  sha256 "f6991604d95285c0944cab4b1b075facae53a4dd59bd836ee24cacd7f85b42a7"
   head "https://github.com/facebook/flow.git"
-
-  stable do
-    url "https://github.com/facebook/flow/archive/v0.46.0.tar.gz"
-    sha256 "f6991604d95285c0944cab4b1b075facae53a4dd59bd836ee24cacd7f85b42a7"
-  end
 
   bottle do
     cellar :any_skip_relocation
