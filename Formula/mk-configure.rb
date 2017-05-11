9735102b462c47d5c3c9a0790dced4bf6a7df7b5
Aleksey Cheusov <vle@gmx.net>
2017-05-12 02:09:41 +0300

    mk-configure 0.29.1 (new formula)

---
+++ Formula/mk-configure.rb
@@ -0,0 +1,23 @@
+class MkConfigure < Formula
+  desc "Lightweight replacement for GNU autotools"
+  homepage "https://github.com/cheusov/mk-configure"
+  url "https://downloads.sourceforge.net/project/mk-configure/mk-configure/mk-configure-0.29.1/mk-configure-0.29.1.tar.gz"
+  sha256 "a675c532f6a857c79dedef2cce4776dda8becfbe7d2126e5f67175aee56c3957"
+
+  depends_on "bmake" => :build
+  depends_on "makedepend" => :build
+  depends_on "makedepend"
+
+  def install
+    ENV["PREFIX"] = prefix.to_s
+    ENV["MANDIR"] = man.to_s
+
+    system "bmake", "all"
+    system "bmake", "install"
+    cp "presentation/presentation.pdf", "#{share}/doc/mk-configure/"
+  end
+
+  test do
+    system "#{bin}/mkcmake", "-V", "MAKE_VERSION", "-f", "/dev/null"
+  end
+end
