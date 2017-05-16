33b14c7d77eaf795ca3a1c7ef71cb85450dea25e
FX Coudert <fxcoudert@gmail.com>
2017-05-16 23:44:37 +0200

    Update audacious.rb

--- Formula/audacious.rb
+++ Formula/audacious.rb
@@ -1,47 +1,57 @@
 class Audacious < Formula
   desc "Free and advanced audio player based on GTK+"
   homepage "http://audacious-media-player.org"
-  url "http://distfiles.audacious-media-player.org/audacious-3.8.2.tar.bz2"
-  sha256 "bdf1471cce9becf9599c742c03bdf67a2b26d9101f7d865f900a74d57addbe93"
-  head "https://github.com/audacious-media-player/audacious.git"
+  
+  stable do
+    url "http://distfiles.audacious-media-player.org/audacious-3.8.2.tar.bz2"
+    sha256 "bdf1471cce9becf9599c742c03bdf67a2b26d9101f7d865f900a74d57addbe93"
 
-  depends_on "make" => :build
-  depends_on "automake" => :build
-  depends_on "autoconf" => :build
+    resource "plugins" do
+      url "http://distfiles.audacious-media-player.org/audacious-plugins-3.8.2.tar.bz2"
+      sha256 "d7cefca7a0e32bf4e58bb6e84df157268b5e9a6771a0e8c2da98b03f92a5fdd4"
+    end
+  end
+  
+  head do
+    url "https://github.com/audacious-media-player/audacious.git"
+
+    resource "plugins" do
+      url "https://github.com/audacious-media-player/audacious-plugins.git"
+    end
+
+    depends_on "automake" => :build
+    depends_on "autoconf" => :build
+    depends_on "libtool" => :build
+  end
+
   depends_on "gettext" => :build
-  depends_on "libtool" => :build
+  depends_on "make" => :build
   depends_on "pkg-config" => :build
+
   depends_on "neon"
-  depends_on "sdl2" => :recommended
-  depends_on "wavpack" => :recommended
   depends_on "glib"
-  depends_on "gtk+" => :optional
-  depends_on "ffmpeg" => :recommended
-  depends_on "libnotify" => :recommended
-  depends_on "libvorbis" => :recommended
-  depends_on "libmms" => :optional
-  depends_on "libcue" => :recommended
-  depends_on "libbs2b" => :recommended
-  depends_on "libsamplerate" => :recommended
-  depends_on "libsoxr" => :recommended
-  depends_on "libmodplug" => :optional
-  depends_on "jack" => :optional
-  depends_on "lame" => :recommended
-  depends_on "fluid-synth" => :recommended
-  depends_on "faad2" => :recommended
-  depends_on "flac" => :recommended
-  depends_on "mpg123" => :recommended
-  depends_on "qt" => :recommended
   depends_on :python if MacOS.version <= :snow_leopard
 
-  resource "audacious-plugins" do
-    url "http://distfiles.audacious-media-player.org/audacious-plugins-3.8.2.tar.bz2"
-    sha256 "d7cefca7a0e32bf4e58bb6e84df157268b5e9a6771a0e8c2da98b03f92a5fdd4"
-  end
+  depends_on "faad2" => :recommended
+  depends_on "ffmpeg" => :recommended
+  depends_on "flac" => :recommended
+  depends_on "fluid-synth" => :recommended
+  depends_on "lame" => :recommended
+  depends_on "libbs2b" => :recommended
+  depends_on "libcue" => :recommended
+  depends_on "libnotify" => :recommended
+  depends_on "libsamplerate" => :recommended
+  depends_on "libsoxr" => :recommended
+  depends_on "libvorbis" => :recommended
+  depends_on "mpg123" => :recommended
+  depends_on "qt" => :recommended
+  depends_on "sdl2" => :recommended
+  depends_on "wavpack" => :recommended
 
-  resource "audacious-plugins-head" do
-    url "https://github.com/audacious-media-player/audacious-plugins.git"
-  end
+  depends_on "gtk+" => :optional
+  depends_on "jack" => :optional
+  depends_on "libmms" => :optional
+  depends_on "libmodplug" => :optional
 
   def install
     args = %W[
@@ -59,10 +69,7 @@ class Audacious < Formula
     system "make"
     system "make", "install"
 
-    resourcec = "audacious-plugins"
-    resourcec = "audacious-plugins-head" if build.head?
-
-    resource(resourcec).stage do
+    resource("plugins").stage do
       ENV.prepend_path "PKG_CONFIG_PATH", "#{lib}/pkgconfig"
 
       system "./autogen.sh" if build.head?
