93e5c2cc676fd9d4c53e94711524086cdeeabb82
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-16 18:23:49 +0000

    linuxmain-generator: add 0.2.0 bottle.

--- Formula/linuxmain-generator.rb
+++ Formula/linuxmain-generator.rb
@@ -4,6 +4,12 @@ class LinuxmainGenerator < Formula
   url "https://github.com/valeriomazzeo/linuxmain-generator/archive/0.2.0.tar.gz"
   sha256 "84a7a29ef8680426d52591631610b683c853910c49532ad17d7101bd35ed207b"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "cda671286602cda1dd4ff2cef1576f3dedd1b7d302a51cb8f1922d7fd4447faf" => :sierra
+    sha256 "d6935d8169e1cc10490fe7baf9b59954bf088f712e49a3bed3adf7f8e214aab6" => :el_capitan
+  end
+
   def install
     ENV["CC"] = ""
     system "swift", "build", "-c", "release"
