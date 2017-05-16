408d1d330b9b66ab85fb2431ddf930f4cbcca101
Valerio Mazzeo <valeriomazzeo@users.noreply.github.com>
2017-05-16 19:19:49 +0100

    Update linuxmain-generator.rb

--- Formula/linuxmain-generator.rb
+++ Formula/linuxmain-generator.rb
@@ -1,5 +1,5 @@
 class LinuxmainGenerator < Formula
-  desc "A shell command to keep SPM project's tests in sync on OSX and Linux."
+  desc "Shell command to keep SPM tests in sync on OSX and Linux."
   homepage "https://github.com/valeriomazzeo/linuxmain-generator"
   url "https://github.com/valeriomazzeo/linuxmain-generator/archive/0.2.0.tar.gz"
   sha256 "84a7a29ef8680426d52591631610b683c853910c49532ad17d7101bd35ed207b"
@@ -9,7 +9,7 @@ class LinuxmainGenerator < Formula
     system "swift", "build", "-c", "release"
     bin.install ".build/release/linuxmain-generator"
   end
-  
+
   test do
     system "#{bin}/linuxmain-generator", "--help"
   end
