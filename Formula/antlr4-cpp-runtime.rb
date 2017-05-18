92638161c7c7233df4ce352f10f1cd57977d3688
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-18 06:34:39 +0000

    antlr4-cpp-runtime: add 4.7 bottle.

--- Formula/antlr4-cpp-runtime.rb
+++ Formula/antlr4-cpp-runtime.rb
@@ -4,6 +4,13 @@ class Antlr4CppRuntime < Formula
   url "http://www.antlr.org/download/antlr4-cpp-runtime-4.7-source.zip"
   sha256 "3aa4bac23c60df14a687839b5e6aa7e94054112d3d3c5c8b1cffe270a4aeeaf7"
 
+  bottle do
+    cellar :any
+    sha256 "c8e224adb8feea25510a0df4b5dd814b72f995b8134fd461ef00198bbfc78575" => :sierra
+    sha256 "7c16c11a205a518bc776fc88bd1be54fe8d72a6298c25aabaf9530929d9568f0" => :el_capitan
+    sha256 "e5bf7b9aa2b3c62bfaf56bae402ebea7d7d9d66108b3c17a249ef1325b360845" => :yosemite
+  end
+
   depends_on "cmake" => :build
   depends_on "antlr" => :recommended
 
