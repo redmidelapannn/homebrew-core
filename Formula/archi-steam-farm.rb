2339f3e9770c0c83fb29ef5c390b1fca9467ae87
Dominyk Tiller <dominyktiller@gmail.com>
2017-05-19 19:04:35 +0100

    archi-steam-farm: doesn't need bottles

--- Formula/archi-steam-farm.rb
+++ Formula/archi-steam-farm.rb
@@ -5,12 +5,7 @@ class ArchiSteamFarm < Formula
   version "2.3.1.4"
   sha256 "73941584b97a8b4820a5c4587e033092873f520ff88dbe9b8da085e72b7525bf"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "7a2e792ce756de3ca77ae63d00073da56b7ba0b599f2583b8e286ba60be63c97" => :sierra
-    sha256 "0054489a26c12f9379ad9f79ac959c07e9b4a6d0a387e08400ad8cb978a602d8" => :el_capitan
-    sha256 "0054489a26c12f9379ad9f79ac959c07e9b4a6d0a387e08400ad8cb978a602d8" => :yosemite
-  end
+  bottle :unneeded
 
   depends_on "mono" => :recommended
 
