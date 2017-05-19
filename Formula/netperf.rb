7b10cc0ae82d1f44e90ead7df3d9bea85899ff18
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-19 19:32:00 +0000

    netperf: update 2.7.0 bottle.

--- Formula/netperf.rb
+++ Formula/netperf.rb
@@ -7,11 +7,10 @@ class Netperf < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "1445f286d21e7de5f75cc905a85fb58020af00be7c630aaebbb77e1f7deb255b" => :sierra
-    sha256 "9b3e6bfce1057cc7a3454288ae45684ef1c9805b23f2c4f52afa0fa6376e8057" => :el_capitan
-    sha256 "12102c72722ff5a7a337414a41b6f3bc7fa67fd4dc32aadd84693c22907c99e8" => :yosemite
-    sha256 "026fe8418ea6be50576850a9b3cde6a5a71a7d93924a1eb6b77cba88675b2a89" => :mavericks
-    sha256 "4e0aa192670d1de341e77e0bd3af4b2cccc6a5b12a734e922e92c7b7d50f281d" => :mountain_lion
+    rebuild 1
+    sha256 "4b3c96d8473fd4de2576935ea36df4577dde92ad21cf06970a5d1530133f97f2" => :sierra
+    sha256 "08122b9cd65cd1f92fb82571f26645f714764f834d0912f07452ad10ab11f7f5" => :el_capitan
+    sha256 "6b47dad57f0fc0a718c7186b001de71732c8c57bb98f3131f35828925156fa35" => :yosemite
   end
 
   def install
