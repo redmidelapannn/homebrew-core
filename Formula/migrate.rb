f2c4653bda2262a3b123d45e1e9a68e53ecb5281
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-22 10:13:40 +0000

    migrate: add 3.0.1 bottle.

--- Formula/migrate.rb
+++ Formula/migrate.rb
@@ -5,6 +5,13 @@ class Migrate < Formula
   version "3.0.1"
   sha256 "17e19678e930d4a48114937fb07300c107088d86a80c798630fabc08d32ee4ee"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "21c50f285eaab8f4def36348515689d9d8a681e559cf28b4d8f238bebd41abaa" => :sierra
+    sha256 "efdb8d4ac880b18e10bc9ded2c0901f4953d015a84c508e2d7855ac94013c41d" => :el_capitan
+    sha256 "efdb8d4ac880b18e10bc9ded2c0901f4953d015a84c508e2d7855ac94013c41d" => :yosemite
+  end
+
   def install
     mv "migrate.darwin-amd64", "migrate"
     bin.install "migrate"
