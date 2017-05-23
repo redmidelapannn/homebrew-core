7ce48cc620cca31b7c0ac8343524004b066b0ce8
Dj Padzensky <djpadz@padz.net>
2017-05-23 09:52:02 -0700

    Changes, per comments:
    * Added freedink-data resource inside of freedink.
    * Minor rework of test phase.

--- Formula/freedink.rb
+++ Formula/freedink.rb
@@ -14,7 +14,11 @@ class Freedink < Formula
   depends_on "libzip"
   depends_on "fontconfig"
   depends_on "pkg-config" => :build
-  depends_on "freedink-data"
+
+  resource "freedink-data" do
+    url "https://ftpmirror.gnu.org/freedink/freedink-data-1.08.20170401.tar.xz"
+    sha256 "e8d3f3ff55b1a86661949d08d901740d65a6092027886fd791f6c315b9c7a7ff"
+  end
 
   def install
     system "./configure", "--disable-debug",
@@ -23,14 +27,17 @@ class Freedink < Formula
                           "--prefix=#{prefix}"
 
     system "make", "install"
-  end
-
-  def post_install
-    ln_s "#{HOMEBREW_PREFIX}/freedink-data/share/dink", "#{share}/dink"
+    resource("freedink-data").stage do
+      system "sed", "-i.bak", "-e", "s,^VERSION,#VERSION,", "-e", "s,^SOURCE_DATE_EPOCH,#SOURCE_DATE_EPOCH,", "-e", "s,xargs -0r,xargs -0,", "Makefile"
+      version=Dir.pwd.split("/")[-1].split("-")[-1]
+      source_date = Dir.pwd.split("/")[-1].split(".")[-1].match(/(\d{4})(\d{2})(\d{2})/).to_a[1..-1].join("-")
+      source_date_epoch = Time.parse("#{source_date} 0:00 UTC").to_i
+      system "make", "install", "VERSION=#{version}", "SOURCE_DATE_EPOCH=#{source_date_epoch}", "PREFIX=#{prefix}"
+    end
   end
 
   test do
     FileTest.executable? "src/freedink" &&
-    %x[src/freedink -vwis].split("\n").first == 'GNU FreeDink 108.4'
+    shell_output("#{bin}/freedink", "-vwis").split("\n").first == "GNU FreeDink 108.4"
   end
 end
