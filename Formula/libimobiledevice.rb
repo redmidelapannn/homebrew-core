942f99ad0116d669d8da6a8d8656a0e854b37de7
Vyacheslav Frolov <frolov78@gmail.com>
2017-05-24 15:36:55 +0100

    libimobiledevice: fix url for HEAD git repo

--- Formula/libimobiledevice.rb
+++ Formula/libimobiledevice.rb
@@ -14,7 +14,7 @@ class Libimobiledevice < Formula
   end
 
   head do
-    url "https://git.libimobiledevice.org/libimobiledevice.git"
+    url "https://github.com/libimobiledevice/libimobiledevice.git"
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
