e80df8382d03737c2c8d20c8f599dd669d98a6d6
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-25 20:37:10 +0000

    k2cli: add 64 bottle.

--- Formula/k2cli.rb
+++ Formula/k2cli.rb
@@ -4,6 +4,13 @@ class K2cli < Formula
   url "https://github.com/samsung-cnct/k2cli/releases/download/1.0.5/k2cli_1.0.5_darwin_amd64.tar.gz"
   sha256 "7e5c0fcd1a696cdbdf0c9bab0035e1c6b336b3b8d81ad7d568fbe54743d67a55"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "11c35a8714a577b60f8570b73d2466ddbf7b3d5c7659b5e49f5611f102c3f960" => :sierra
+    sha256 "572ec3d377eb74110d58d35bd304d614d5c3499e458a01ff294ab82f0c991efd" => :el_capitan
+    sha256 "572ec3d377eb74110d58d35bd304d614d5c3499e458a01ff294ab82f0c991efd" => :yosemite
+  end
+
   depends_on "docker" => :run
 
   def install
