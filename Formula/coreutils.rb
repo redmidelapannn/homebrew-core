a7d1bb4b4f0b5b79652407d247a01c0ac02d77e9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-29 13:31:09 +0000

    coreutils: update 8.27 bottle.

--- Formula/coreutils.rb
+++ Formula/coreutils.rb
@@ -39,9 +39,10 @@ class Coreutils < Formula
   end
 
   bottle do
-    sha256 "a951d21ffbf3407ca84356d369ed6009d248b263587b79f644d9a95300465fa6" => :sierra
-    sha256 "dafd72ff298ed109503928a3d7cf1623327b4bc65318e99b48f3415b7c469ac8" => :el_capitan
-    sha256 "5d636c1ad28b1ef25c140b1486fdb368486bcca563901ad543d62ce1bd5f8b70" => :yosemite
+    rebuild 1
+    sha256 "49951563cc5de362ba98cc3e1147379159d45aea0d8f3fdd6c547b24971cc75a" => :sierra
+    sha256 "a12678836aac5797d3a7deea9c1375efdbc05e75a298b1e5ed556382d1f7533d" => :el_capitan
+    sha256 "76847af9f08a27174c0c824074a629e1e43cf2497a6bfed76788f7aa4ca0e27b" => :yosemite
   end
 
   head do
