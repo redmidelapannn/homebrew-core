930aedb6f3dc892cecbee9b2dfbaad75e0ea5031
ilovezfs <ilovezfs@icloud.com>
2017-05-29 04:55:40 -0700

    cimg 2.0.0

--- Formula/cimg.rb
+++ Formula/cimg.rb
@@ -1,8 +1,8 @@
 class Cimg < Formula
   desc "C++ toolkit for image processing"
   homepage "http://cimg.eu/"
-  url "http://cimg.eu/files/CImg_1.7.9.zip"
-  sha256 "fa7800d420807e367c0f77e6d5076decba55dc51c9319f9e9e5cf822ac0ce81d"
+  url "http://cimg.eu/files/CImg_2.0.0.zip"
+  sha256 "f6ee5c6b16fac167c120a060998ab6864856ff0a2bbb78134472db7ca0f26889"
 
   bottle do
     cellar :any_skip_relocation
