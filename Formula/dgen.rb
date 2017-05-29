072bfcb2856dc38e22dd744d5576b7f16afea7ea
BrewTestBot <brew-test-bot@googlegroups.com>
2017-05-29 13:31:09 +0000

    dgen: update 1.33 bottle.

--- Formula/dgen.rb
+++ Formula/dgen.rb
@@ -5,9 +5,10 @@ class Dgen < Formula
   sha256 "99e2c06017c22873c77f88186ebcc09867244eb6e042c763bb094b02b8def61e"
   bottle do
     cellar :any
-    sha256 "50383807ec76387aa156cf6157ea537465bf20ad35e4e9eddda7d34685ded635" => :sierra
-    sha256 "ebcab68ba8d0aa9c6aacae94d43a67ce016dcdd219c5770c3b7d6d9c3590ef9f" => :el_capitan
-    sha256 "53f1fc72dbaab000eae45e143ca46a054a6ff655f91190d6aa30e71e8e505494" => :yosemite
+    rebuild 1
+    sha256 "c9721d39dc35f9dd44c7a96700f9c7e3a0fb9ca8b1574f475e6fae1fb143cd5f" => :sierra
+    sha256 "d0c8ec1555d7e32b6e9bba46d35460654e48ee0f0ceeb054fb7d8a1d8bb668fa" => :el_capitan
+    sha256 "7791d516c807df92b5d6dc95461acdd1f4b5d3a019b11bd3980ce4208060cd4e" => :yosemite
   end
 
   head do
