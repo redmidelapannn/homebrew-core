e44da6e23e53fc241cee5a9472507005a8aa1b81
Julien Dubois <julien.dubois@gmail.com>
2017-05-30 10:56:20 +0200

    A bottle will be needed, as asked by @ilovezfs

--- Formula/jhipster.rb
+++ Formula/jhipster.rb
@@ -6,8 +6,6 @@ class Jhipster < Formula
   url "https://registry.npmjs.org/generator-jhipster/-/generator-jhipster-4.5.2.tgz"
   sha256 "5df0edbbdb685df5ae598b53fb2def43b6c23bb75d44763a63cda69567395954"
 
-  bottle :unneeded
-
   depends_on "node"
 
   def install
