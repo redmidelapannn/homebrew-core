870fd5b78f6cf6c7cc2860c8a007bc687722da49
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-01 14:27:46 +0000

    grakn: add 0.12.1 bottle.

--- Formula/grakn.rb
+++ Formula/grakn.rb
@@ -4,6 +4,13 @@ class Grakn < Formula
   url "https://github.com/graknlabs/grakn/releases/download/v0.12.1/grakn-dist-0.12.1.tar.gz"
   sha256 "3658138ccd74e51a0072a42f5e4fff43763506b02e581b572b05f50b1c63eb72"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "55ea780f977fcc7fb14dd6790b838fc32b1c04c103e42992c90eb26711f59411" => :sierra
+    sha256 "3c13ec24cd28a25af95b3b9fbe2066f9138183468c626ad8f3c30fe67c658672" => :el_capitan
+    sha256 "3c13ec24cd28a25af95b3b9fbe2066f9138183468c626ad8f3c30fe67c658672" => :yosemite
+  end
+
   depends_on :java => "1.8+"
 
   def install
