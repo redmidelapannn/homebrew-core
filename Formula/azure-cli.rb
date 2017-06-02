5106e6ee1295c1681de98288f28006ccd94353a8
Antoine Cotten <tonio.cotten@gmail.com>
2017-06-02 12:31:04 -0400

    azure-cli 2.0.2.10

--- Formula/azure-cli.rb
+++ Formula/azure-cli.rb
@@ -1,44 +1,46 @@
-require "language/node"
-
 class AzureCli < Formula
-  desc "Official Azure CLI"
-  homepage "https://github.com/azure/azure-xplat-cli"
-  url "https://github.com/Azure/azure-xplat-cli/archive/v0.10.12-May2017.tar.gz"
-  version "0.10.13"
-  sha256 "60194e770b8dca0485db9d4c99f9cd432f7b43096cc5ad0353f52fd5b1b29181"
-  head "https://github.com/azure/azure-xplat-cli.git", :branch => "dev"
+  include Language::Python::Virtualenv
 
-  bottle do
-    cellar :any_skip_relocation
-    rebuild 1
-    sha256 "3d9cce38a38527dda6131d937e22c5ead62e071b7dfafc49f2f6eb3123bba0aa" => :sierra
-    sha256 "6f892658bd3cb909822027352e8826eefd7c9258d9b3488513526da0995d8af7" => :el_capitan
-    sha256 "a9301281b69223213f638fc92b99a4f8032fd354464adfe30d5031664ad4af75" => :yosemite
-  end
+  desc "Microsoft Azure CLI 2.0"
+  homepage "https://docs.microsoft.com/en-us/cli/azure/overview"
+  url "https://azurecliprod.blob.core.windows.net/releases/azure-cli_packaged_0.2.10.tar.gz"
+  version "2.0.2.10"
+  sha256 "be72ddb0983b3466e868602e68e4d3bf67379fbe080bdaa6aa321c03f9bcce48"
+  head "https://github.com/Azure/azure-cli.git"
 
-  depends_on "node"
-  depends_on :python => :build
+  depends_on :python if MacOS.version <= :snow_leopard
 
   def install
-    rm_rf "bin/windows"
+    virtualenv_create(libexec)
+    bin_dir = libexec/"bin"
 
-    # Workaround for incorrect file system permissios inside the npm published
-    # easy_table@0.0.1 package, which would break build with npm@5.
-    # See: https://github.com/Azure/azure-xplat-cli/issues/3605
-    inreplace "package.json", '"easy-table": "0.0.1",',
-              '"easy-table": "https://github.com/eldargab/easy-table/archive/v0.0.1.tar.gz",'
+    components = [
+      buildpath/"src/azure-cli",
+      buildpath/"src/azure-cli-core",
+      buildpath/"src/azure-cli-nspkg",
+      buildpath/"src/azure-cli-command_modules-nspkg",
+    ] + Pathname.glob("src/command_modules/azure-cli-*/")
 
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink Dir["#{libexec}/bin/*"]
-    (bash_completion/"azure").write Utils.popen_read("#{bin}/azure --completion")
+    # Create wheel distribution of included components
+    components.each do |c|
+      c.cd { system bin_dir/"python", "setup.py", "bdist_wheel", "-d", buildpath/"dist" }
+    end
+
+    # Install CLI from wheel distributions
+    system bin_dir/"pip", "install", "azure-cli", "-f", buildpath/"dist"
+
+    # Generate executable
+    (bin/"az").write <<-EOS.undent
+      #!/usr/bin/env bash
+      #{bin_dir}/python -m azure.cli "$@"
+    EOS
+
+    # Install bash completion
+    bash_completion.install "az.completion" => "az"
   end
 
   test do
-    shell_output("#{bin}/azure telemetry --disable")
-    json_text = shell_output("#{bin}/azure account env show AzureCloud --json")
-    azure_cloud = JSON.parse(json_text)
-    assert_equal azure_cloud["name"], "AzureCloud"
-    assert_equal azure_cloud["managementEndpointUrl"], "https://management.core.windows.net"
-    assert_equal azure_cloud["resourceManagerEndpointUrl"], "https://management.azure.com/"
+    version_output = shell_output("#{bin}/az --version")
+    assert_match "azure-cli", version_output
   end
 end
