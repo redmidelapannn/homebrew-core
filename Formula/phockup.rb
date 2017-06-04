9278b24cbea5ec9d32a7090df9ee16d6c2b9ed7a
Ivan Dokov <ivangdokov@gmail.com>
2017-06-04 15:55:12 +0300

    Fixed audit error with desc length

--- Formula/phockup.rb
+++ Formula/phockup.rb
@@ -1,5 +1,5 @@
 class Phockup < Formula
-  desc "Media sorting and backup tool to organize photos and videos from your camera in folders by year, month and day."
+  desc "Organize photos and videos in folders by year, month and day."
   homepage "https://github.com/ivandokov/phockup"
   url "https://github.com/ivandokov/phockup/archive/v1.2.0.tar.gz"
   sha256 "8bced90fd663ab9acf8e48e630904da7d493e2fe78add63b0c5d60c18c6de2ad"
