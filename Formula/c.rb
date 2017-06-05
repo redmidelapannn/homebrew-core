4360fc2e47f30537ee9166e0c093dbc9f62e4e3e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-05 04:58:48 +0000

    c: add 0.0.2 bottle.

--- Formula/c.rb
+++ Formula/c.rb
@@ -4,6 +4,13 @@ class C < Formula
   url "https://github.com/AndrewRayCode/c-releases/raw/master/c-0.0.2.tar.gz"
   sha256 "a20071425f7b91d3f37c9f2a9af3a0c643588dce4f2466a4ea197e42b707ccfb"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "65a42d47c8887775e34523d7091a3e1f37082584bb39d046daf8e8faf9a45ef5" => :sierra
+    sha256 "d3036cd6bef3d9c44d836870122950c507f9adb15dd909a00069d77b9cf4329e" => :el_capitan
+    sha256 "d3036cd6bef3d9c44d836870122950c507f9adb15dd909a00069d77b9cf4329e" => :yosemite
+  end
+
   def install
     bin.install %w[c c_recent_branches_completer]
   end
