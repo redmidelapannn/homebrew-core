091a44e57701f6f8402bc1ecc645dbb6cc2ee8be
Ben <BenjaminHCCarr@users.noreply.github.com>
2017-06-09 01:22:56 -0400

    Change homepage to https
    Used brew create, missed that it chose `http` by default.

--- Formula/phoronix-test-suite.rb
+++ Formula/phoronix-test-suite.rb
@@ -1,6 +1,6 @@
 class PhoronixTestSuite < Formula
   desc "Open-source automated testing/benchmarking software."
-  homepage "http://www.phoronix-test-suite.com/"
+  homepage "https://www.phoronix-test-suite.com/"
   url "https://github.com/phoronix-test-suite/phoronix-test-suite/archive/v7.2.0.tar.gz"
   sha256 "20bdab1e979636476ebbfc003cbbbd36a04cc35acef825ff0d11a0b3a33a2474"
 
