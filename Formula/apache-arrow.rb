d8f846e0c8e9ce867579a18d2a9fb5768fdb4256
Hideo Hattori <hattori-h@klab.com>
2017-06-12 14:42:06 +0900

    improve description

--- Formula/apache-arrow.rb
+++ Formula/apache-arrow.rb
@@ -1,5 +1,5 @@
 class ApacheArrow < Formula
-  desc "Apache Arrow"
+  desc "Columnar in-memory analytics layer designed to accelerate big data"
   homepage "https://arrow.apache.org/"
   url "https://github.com/apache/arrow/archive/apache-arrow-0.4.0.tar.gz"
   sha256 "0ce9f47e42735d8d48dd83f9ff6a1612c5f2a8e846f3ae23595697e3519dd9b8"
