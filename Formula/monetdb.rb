1415cafadd50be19beecd6740f38d211ea4495f2
Sjoerd Mullender <sjoerd@acm.org>
2017-06-12 14:52:34 +0200

    monetdb 11.25.23

--- Formula/monetdb.rb
+++ Formula/monetdb.rb
@@ -16,8 +16,8 @@ end
 class Monetdb < Formula
   desc "Column-store database"
   homepage "https://www.monetdb.org/"
-  url "https://www.monetdb.org/downloads/sources/Dec2016-SP4/MonetDB-11.25.21.tar.xz"
-  sha256 "02cd4b2638bf301c0ba828461a51b063418adf4f3f343708922e3cd66d5ab245"
+  url "https://www.monetdb.org/downloads/sources/Dec2016-SP5/MonetDB-11.25.23.tar.xz"
+  sha256 "8f3a39cfcd11dc81746e062795a8e46eb9b1ca9fdf03a3dbd5290826f62d1c07"
 
   bottle do
     sha256 "29e53987c25b36626fe8d191c98c1fdb0387c8bfc78b2636df2231ce46b9a7e9" => :sierra
