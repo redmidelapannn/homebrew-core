e908c0b4fd8b10143c6e46ebdaf49d7930774db2
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-13 03:03:57 +0000

    bayon: add 0.1.1 bottle.

--- Formula/bayon.rb
+++ Formula/bayon.rb
@@ -4,6 +4,13 @@ class Bayon < Formula
   url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/bayon/bayon-0.1.1.tar.gz"
   sha256 "1749f8d2202459e33dd739e62fd8c128facaf16a06cb3f38a9e4f10cce542e0c"
 
+  bottle do
+    cellar :any
+    sha256 "c1e2883be3e409c82ef64c13cd99b233c4da680bc80946cfad9ac56ba4bb1275" => :sierra
+    sha256 "ade9a0ab1ff8a37609615979a54b8316378fef24c952de1a89381b1f43b8b489" => :el_capitan
+    sha256 "6f38bc11e10f0d4d77eb50f9b26114030c95783b4082d040d0d672fee81164c0" => :yosemite
+  end
+
   depends_on "google-sparsehash" => :recommended
 
   def install
