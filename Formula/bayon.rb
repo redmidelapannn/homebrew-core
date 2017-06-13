a5c569ac12623cc3dce8c813f1034fe8c758d6c4
x00.x7f@gmail.com <x00.x7f@gmail.com>
2017-06-13 11:47:32 +0900

    bayon: fix format

--- Formula/bayon.rb
+++ Formula/bayon.rb
@@ -1,5 +1,5 @@
 class Bayon < Formula
-  desc "Bayon is a simple and fast hard-clustering tool."
+  desc "Simple and fast hard-clustering tool"
   homepage "https://githhub.com/fujimizu/bayon"
   url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/bayon/bayon-0.1.1.tar.gz"
   sha256 "1749f8d2202459e33dd739e62fd8c128facaf16a06cb3f38a9e4f10cce542e0c"
@@ -13,6 +13,6 @@ class Bayon < Formula
   end
 
   test do
-    system "bayon", "-v"
+    system "#{bin}/bayon", "-v"
   end
 end
