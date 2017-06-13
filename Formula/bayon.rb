1a0a9e32653afbd6caccdb381680346a78a7b8a7
x00.x7f@gmail.com <x00.x7f@gmail.com>
2017-06-13 12:00:25 +0900

    bayon: fix homepage

--- Formula/bayon.rb
+++ Formula/bayon.rb
@@ -1,6 +1,6 @@
 class Bayon < Formula
   desc "Simple and fast hard-clustering tool"
-  homepage "https://githhub.com/fujimizu/bayon"
+  homepage "https://github.com/fujimizu/bayon"
   url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/bayon/bayon-0.1.1.tar.gz"
   sha256 "1749f8d2202459e33dd739e62fd8c128facaf16a06cb3f38a9e4f10cce542e0c"
 
