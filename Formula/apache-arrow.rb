89c9f4ef3d1b74d3f9960a35cd3e3ba4b42d9193
Hideo Hattori <hattori-h@klab.com>
2017-06-21 18:52:18 +0900

    indent

--- Formula/apache-arrow.rb
+++ Formula/apache-arrow.rb
@@ -25,12 +25,12 @@ class ApacheArrow < Formula
 
   test do
     (testpath/"test.cpp").write <<-EOS.undent
-    #include "arrow/api.h"
-    int main(void)
-    {
-      arrow::Int64Builder builder(arrow::default_memory_pool(), arrow::int64());
-      return 0;
-    }
+      #include "arrow/api.h"
+      int main(void)
+      {
+        arrow::Int64Builder builder(arrow::default_memory_pool(), arrow::int64());
+        return 0;
+      }
     EOS
     system ENV.cxx, "test.cpp", "-std=c++11", "-I#{include}", "-L#{lib}", "-larrow", "-o", "test"
     system "./test"
