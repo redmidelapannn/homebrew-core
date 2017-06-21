2e928c93078311a5694db009778aa503882d6c9e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-21 16:56:20 +0000

    minishift: add 1.1.0 bottle.

--- Formula/minishift.rb
+++ Formula/minishift.rb
@@ -5,6 +5,13 @@ class Minishift < Formula
   version "1.1.0"
   sha256 "6c516e11c8b69995df76e39100cda98906259ea5f5ceb860cac51a451bc39d0a"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "2eba283745853ff6d321d83c53303a807b40f122e459cc73d819c7eb2776cb1b" => :sierra
+    sha256 "ca21748d8e1d9aa43fe3aa9a198e745fc3481b4a0480b268b1c6d26bedcff18b" => :el_capitan
+    sha256 "ca21748d8e1d9aa43fe3aa9a198e745fc3481b4a0480b268b1c6d26bedcff18b" => :yosemite
+  end
+
   def install
     bin.install "minishift"
   end
