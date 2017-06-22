3c3a41480887145f6093daf3a30734e38a370585
Jeremy Jung <jeremyjung@users.noreply.github.com>
2017-06-21 19:17:33 -0700

    Remove patch and modify build configuration in formula

--- Formula/mruby-cli.rb
+++ Formula/mruby-cli.rb
@@ -4,19 +4,29 @@ class MrubyCli < Formula
   url "https://github.com/hone/mruby-cli/archive/v0.0.4.tar.gz"
   sha256 "97d889b5980193c562e82b42089b937e675b73950fa0d0c4e46fbe71d16d719f"
 
-  # The mruby-cli build_config.rb assumes that it will be compiled on Linux.
-  # It also automatically cross compiles to MacOS, Linux, and Windows
-  # This patch compiles a single MacOS binary using clang
-  patch do
-    url "https://gist.githubusercontent.com/jeremyjung/ca4f5baa0e6567b96332b6a9281ab37b/raw/84d9dc64fbe02162cab5625243d7e477adb32ab3/mruby-cli_patch.diff"
-    sha256 "4d148001ac180c1942f157829d5a18e553987fc370b5d98d9f1123ce3b21436f"
-  end
-
   def install
     ENV["MRUBY_CLI_LOCAL"] = "true"
+    rewrite_build_config
     system "rake", "compile"
     cp "mruby/build/host/bin/mruby-cli", "mruby-cli"
     bin.install "mruby-cli"
+  end
+
+  # The upstream build_config.rb assumes it will be compiled on Linux.
+  # It also automatically cross compiles to MacOS, Linux, and Windows
+  # This configuration compiles a single MacOS binary using clang
+  def rewrite_build_config
+    build_config = <<-eos
+      MRuby::Build.new do |conf|
+        toolchain :clang
+
+        conf.gem File.expand_path(File.dirname(__FILE__))
+      end
+    eos
+
+    File.open("build_config.rb", "w") do |f|
+      f.write(build_config)
+    end
   end
 
   test do
