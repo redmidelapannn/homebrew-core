277ed9d67f75b027655c8350ba4c01810174dfe9
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-22 03:21:16 +0000

    now-cli: add 7.0.2 bottle.

--- Formula/now-cli.rb
+++ Formula/now-cli.rb
@@ -5,6 +5,13 @@ class NowCli < Formula
   version "7.0.2"
   sha256 "5dce15bc8af083bee4647fb1b5f7861f6813bd2e86128a26858627a5c882d1df"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1488ebafca5aa0b155fd210c0f047e56258a2730cf349a7085da185a1cc810bf" => :sierra
+    sha256 "a708d53ef399efec1aff2632f0af39d818fc79b6ce151e2483119759379671cb" => :el_capitan
+    sha256 "a708d53ef399efec1aff2632f0af39d818fc79b6ce151e2483119759379671cb" => :yosemite
+  end
+
   def install
     bin.install "now-macos"
     mv bin/"now-macos", bin/"now"
