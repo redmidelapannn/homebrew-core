f76bf2b17dc92f7adf63fff425faf7f3c38e0dd7
s172262 <s172262@users.noreply.github.com>
2017-06-23 06:14:39 +0900

    frei0r 1.6.1

--- Formula/frei0r.rb
+++ Formula/frei0r.rb
@@ -1,8 +1,8 @@
 class Frei0r < Formula
   desc "Minimalistic plugin API for video effects"
   homepage "https://frei0r.dyne.org/"
-  url "https://files.dyne.org/frei0r/releases/frei0r-plugins-1.4.tar.gz"
-  sha256 "8470fcabde9f341b729be3be16385ffc8383d6f3328213907a43851b6e83be57"
+  url "https://files.dyne.org/frei0r/frei0r-plugins-1.6.1.tar.gz"
+  sha256 "e0c24630961195d9bd65aa8d43732469e8248e8918faa942cfb881769d11515e"
 
   bottle do
     cellar :any_skip_relocation
@@ -13,15 +13,12 @@ class Frei0r < Formula
     sha256 "a20918ebf08da3636deb4bb4c3bacaf395186f2bf88685ad8380d665e7402e24" => :mountain_lion
   end
 
-  depends_on "autoconf" => :build
-  depends_on "pkg-config" => :build
+  depends_on "cmake" => :build
   depends_on "cairo" => :optional
   depends_on "homebrew/science/opencv" => :optional
 
   def install
-    ENV["CAIRO_CFLAGS"] = "-I#{Formula["cairo"].opt_include}/cairo" if build.with? "cairo"
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    system "cmake", ".", *std_cmake_args
     system "make", "install"
   end
 end
