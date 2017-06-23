de81ae41592ea92183022c8e7b680ee7d4c4cb57
Alexander Payne <myrrlyn@outlook.com>
2017-06-23 15:53:36 -0600

    Update tokei.rb
    Forgot the install function

--- Formula/tokei.rb
+++ Formula/tokei.rb
@@ -4,6 +4,10 @@ class Tokei < Formula
   url "https://github.com/Aaronepower/tokei/releases/download/v6.0.1/tokei-v6.0.1-x86_64-apple-darwin.tar.gz"
   sha256 "ac62187453747125b473b1a540e788097ae309fccff3ba71007c7a882db46aea"
 
+  def install
+    bin.install "tokei"
+  end
+
   test do
     (testpath/"lib.rs").write <<-EOS
   #[cfg(test)]
