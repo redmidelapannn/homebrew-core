aab753dd32e141514b5a585698d0d770bd4a141b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-06-25 17:34:19 +0000

    gopass: add 1.0.2 bottle.

--- Formula/gopass.rb
+++ Formula/gopass.rb
@@ -5,6 +5,13 @@ class Gopass < Formula
   sha256 "a2c64329355f72527a1e269b3ad97cd2d37012ebf283210ef947c10657d8f650"
   head "https://github.com/justwatchcom/gopass.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8c847a6bbb941fd2e7c1716f7b168c8250d2d01ed5b018c7ec9b2d9ba6e3ef53" => :sierra
+    sha256 "3c298beb6335d31834a0a6deaba31b53e601dde35ba9731746268081d56a83c5" => :el_capitan
+    sha256 "57331ec26464f8b796f27c5516345abcffe3d56cef9c3cfb8d7e9c56de603105" => :yosemite
+  end
+
   depends_on "go" => :build
   depends_on :gpg => :run
 
