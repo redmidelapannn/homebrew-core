f7c256241c66fd128b4eb95d6d8d2a48f69abb9a
ilovezfs <ilovezfs@icloud.com>
2017-06-26 00:08:55 -0700

    Update frei0r.rb

--- Formula/frei0r.rb
+++ Formula/frei0r.rb
@@ -1,7 +1,7 @@
 class Frei0r < Formula
   desc "Minimalistic plugin API for video effects"
   homepage "https://frei0r.dyne.org/"
-  url "https://files.dyne.org/frei0r/frei0r-plugins-1.6.1.tar.gz"
+  url "https://files.dyne.org/frei0r/releases/frei0r-plugins-1.6.1.tar.gz"
   sha256 "e0c24630961195d9bd65aa8d43732469e8248e8918faa942cfb881769d11515e"
 
   bottle do
