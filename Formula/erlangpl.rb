cf917812f3ef1c132b78b8869f3e609df281324b
Tomasz Cichocinski <tomaszcichocinski@gmail.com>
2017-06-29 17:11:42 +0200

    Shorten erlangpl description

--- Formula/erlangpl.rb
+++ Formula/erlangpl.rb
@@ -1,5 +1,5 @@
 class Erlangpl < Formula
-  desc "Tool for developers working with systems running on the Erlang VM (BEAM). It helps with performance analysis."
+  desc "Tool for developers working with systems on the Erlang VM (BEAM)."
   homepage "http://www.erlang.pl/"
   url "https://github.com/erlanglab/erlangpl/releases/download/0.7.0/erlangpl.tar.gz"
   sha256 "f67c85024300a958e4ee582b24b87bee25a3f4a2325d7190585611212f4f8191"
