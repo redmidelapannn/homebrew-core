4f6c246d272f3307428259112cc918cb3d52ced5
Ole-Martin Bratteng <ole-martin@bratteng.me>
2017-06-30 18:49:56 +0200

    bash-snippets 1.5.0 (new formula)

--- Formula/bash-snippets.rb
+++ Formula/bash-snippets.rb
@@ -1,22 +1,26 @@
 class BashSnippets < Formula
   desc "Collection of small bash scripts for heavy terminal users"
   homepage "https://github.com/alexanderepstein/Bash-Snippets"
-  url "https://github.com/alexanderepstein/Bash-Snippets/archive/v1.1.1.tar.gz"
-  sha256 "d82c9dc428fd609fe1135d74daa2fe03684855e9bc062eab1d4fcd6b66a42b87"
+  url "https://github.com/alexanderepstein/Bash-Snippets/archive/v1.5.0.tar.gz"
+  sha256 "4b96af15bdb34e6b48ec38f95bd0a68b0315776bf7e14c56011d5f27876ec520"
 
   option "without-crypt", "Don't install crypt"
   option "without-currency", "Don't install currency"
   option "without-movies", "Don't install movies"
+  option "without-short", "Don't install short"
   option "without-stocks", "Don't install stocks"
+  option "without-taste", "Don't install taste"
   option "without-weather", "Don't install weather"
 
   def install
-    snippets = %w[crypt currency movies stocks weather]
+    snippets = %w[crypt currency movies short stocks taste weather]
 
     snippets.delete("crypt") if build.without?("crypt")
     snippets.delete("currency") if build.without?("currency")
     snippets.delete("movies") if build.without?("movies")
+    snippets.delete("short") if build.without?("short")
     snippets.delete("stocks") if build.without?("stocks")
+    snippets.delete("taste") if build.without?("taste")
     snippets.delete("weather") if build.without?("weather")
 
     snippets.each do |snippet|
@@ -25,6 +29,6 @@ class BashSnippets < Formula
   end
 
   test do
-    system "false"
+    system "#{bin}/crypt", "-h"
   end
 end
