415afbf83824279fea8a1ae243a8fd381a15266a
ilovezfs <ilovezfs@icloud.com>
2017-06-30 02:29:10 -0700

    Update aurora-cli.rb

--- Formula/aurora-cli.rb
+++ Formula/aurora-cli.rb
@@ -4,6 +4,13 @@ class AuroraCli < Formula
   url "https://www.apache.org/dyn/closer.cgi?path=/aurora/0.18.0/apache-aurora-0.18.0.tar.gz"
   sha256 "8918e041369ae415e28df07fad544b0078132a4831b4c437432a1f5f28dcf648"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a06288f99cada7e310455d243a99780151d7bb484ee230d769577279f2db06fb" => :sierra
+    sha256 "e11368a651d7823e8c5a399ed98b970fcbb788d7e9cbb1f5359ed96c39c42508" => :el_capitan
+    sha256 "fe5c388a7d9cfe8a22a84301ee6fb2819e99d9bf364d768f7373d2d1030324a0" => :yosemite
+  end
+
   depends_on :python if MacOS.version <= :snow_leopard
 
   def install
