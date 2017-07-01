7698dddf4a13bc29fab9346bb22e1224e1a5adee
ilovezfs <ilovezfs@icloud.com>
2017-07-01 09:39:13 -0700

    treefrog 1.18.0

--- Formula/treefrog.rb
+++ Formula/treefrog.rb
@@ -1,8 +1,8 @@
 class Treefrog < Formula
   desc "High-speed C++ MVC Framework for Web Application"
   homepage "http://www.treefrogframework.org/"
-  url "https://github.com/treefrogframework/treefrog-framework/archive/v1.17.0.tar.gz"
-  sha256 "8e9ca157610848ba5290a37377d1787a16fada8e32babb7400464485b6873e19"
+  url "https://github.com/treefrogframework/treefrog-framework/archive/v1.18.0.tar.gz"
+  sha256 "9d9c60c9b36a4a76ac89adcdf1bf3c9a7135f89774332087b34f27e8fc89e0ca"
   head "https://github.com/treefrogframework/treefrog-framework.git", :branch => "master"
 
   bottle do
