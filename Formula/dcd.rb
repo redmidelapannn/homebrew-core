f92e4fc2f05ba0e7508e40f796a819e7e0721b8a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-03 13:23:04 +0000

    dcd: update 0.9.0 bottle.

--- Formula/dcd.rb
+++ Formula/dcd.rb
@@ -8,9 +8,10 @@ class Dcd < Formula
   head "https://github.com/dlang-community/dcd.git", :shallow => false
 
   bottle do
-    sha256 "495bb87a467b94666178e4f4454ef96895bf2bf48a604efd7f1d6ecd43005971" => :sierra
-    sha256 "8877d9cfc361eeb82ea73d328b61c73b66c3f7781e911790d143e5ef52bfd12c" => :el_capitan
-    sha256 "78bc5c22508ba34d32c035f581cdb9bc09989fd6939c13a4f30a095aaa8fae20" => :yosemite
+    rebuild 1
+    sha256 "7c00dea9e81f0892df40aacc047b997b4c1f665bf2c25ac6df348b5f601e2306" => :sierra
+    sha256 "eb86ae0f7b6fbf80f61b48479fe71a3eb5590dab8778961d8e89b059f6d94df9" => :el_capitan
+    sha256 "2e603e6605e42bc1508af976f9b7b241ded3ac8a796c804ed91faf34eab60c26" => :yosemite
   end
 
   depends_on "dmd" => :build
