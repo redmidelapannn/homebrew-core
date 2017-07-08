a1cb6446e896f701a31d523faab369ce1a59bd84
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-08 11:51:46 +0000

    libswiften: update 3.0_1 bottle.

--- Formula/libswiften.rb
+++ Formula/libswiften.rb
@@ -10,10 +10,10 @@ class Libswiften < Formula
   patch :DATA
 
   bottle do
-    sha256 "d7a96ec5a0f396486acf810c88efec48beff0778e770084a980d09773029ffd7" => :sierra
-    sha256 "e9bf41171f626c71350d0db7f13857b56c57f63248a229fe0ac4ed09c42dcfcf" => :el_capitan
-    sha256 "162f1c07d37888abd2c2f616f3bc512209ed5575444f5f17b555b974e0461939" => :yosemite
-    sha256 "0cd2296d234b0c59bcd9dc5e0ebf78f7439ac7c91e415efad693976d01666338" => :mavericks
+    rebuild 1
+    sha256 "c32cc077d5357bf0e3b9df3a9540dda72953fee20b05015feaac914279adea21" => :sierra
+    sha256 "5ddf00fb3efa0944feb9bde0324ee2729313c87c819d1fd03647c7debd8d18d1" => :el_capitan
+    sha256 "319d0362bdca805a9ed1f0205584ead08335e27a3ed60ec49788d9486b9d407d" => :yosemite
   end
 
   depends_on "scons" => :build
