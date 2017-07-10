9922cabddbe698a2b28389f97cf48a57dc215d08
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2017-07-10 22:26:23 +0200

    Added functionality test
    Instead of simple `pkg-config` check there's simple program
    written in C that uses one of the headers provided by library and
    one function.

--- Formula/libnice.rb
+++ Formula/libnice.rb
@@ -17,7 +17,43 @@ class Libnice < Formula
   end
 
   test do
-    output = shell_output("pkg-config --modversion nice")
-    assert_match version.to_s, output
+    # Based on https://github.com/libnice/libnice/blob/master/examples/simple-example.c
+    (testpath/"test.c").write <<-EOS.undent
+      #include <agent.h>
+      int main(int argc, char *argv[]) {
+        NiceAgent *agent;
+        GMainLoop *gloop;
+        gloop = g_main_loop_new(NULL, FALSE);
+        // Create the nice agent
+        agent = nice_agent_new(g_main_loop_get_context (gloop),
+          NICE_COMPATIBILITY_RFC5245);
+        if (agent == NULL)
+          g_error("Failed to create agent");
+
+        g_main_loop_unref(gloop);
+        g_object_unref(agent);
+        return 0;
+      }
+    EOS
+
+    gettext = Formula["gettext"]
+    glib = Formula["glib"]
+    flags = %W[
+      -I#{gettext.opt_include}
+      -I#{glib.opt_include}/glib-2.0
+      -I#{glib.opt_lib}/glib-2.0/include
+      -I#{include}/nice
+      -D_REENTRANT
+      -L#{gettext.opt_lib}
+      -L#{glib.opt_lib}
+      -L#{lib}
+      -lgio-2.0
+      -lglib-2.0
+      -lgobject-2.0
+      -lintl
+      -lnice
+    ]
+    system ENV.cc, *flags, "test.c", "-o", "test"
+    system "./test"
   end
 end
