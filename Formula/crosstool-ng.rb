18c3666822dc277807cd57d1fab8df1acd9c98fd
Matteo Pacini <ispeakprogramming@gmail.com>
2017-07-16 17:33:19 +0200

    crosstool-ng (updated for v1.23.0)
    This commit updates the formula for crosstool-ng 1.23.0

--- Formula/crosstool-ng.rb
+++ Formula/crosstool-ng.rb
@@ -1,62 +1,56 @@
 class CrosstoolNg < Formula
+
   desc "Tool for building toolchains"
   homepage "http://crosstool-ng.org"
-  url "http://crosstool-ng.org/download/crosstool-ng/crosstool-ng-1.22.0.tar.xz"
-  sha256 "a8b50ddb6e651c3eec990de54bd191f7b8eb88cd4f88be9338f7ae01639b3fba"
+  url "http://crosstool-ng.org/download/crosstool-ng/crosstool-ng-1.23.0.tar.xz"
+  sha256 "68a43ea98ccf9cb345cb6eec494a497b224fee24c882e8c14c6713afbbe79196"
   revision 1
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "3ada31fa193f947f2145794eb2b24f12b0d27106c707685f0e92678ac180d9e0" => :sierra
-    sha256 "1563e4b907a4e290d0b3b4e9c0bbfb840f42eb90b2c1a10c7a6632560d59dd9e" => :el_capitan
-    sha256 "a1a7a286d87ff625b6e5910f962e0248ddd610dcf91ddd3b8923bf4f7476a23d" => :yosemite
+    sha256 "2342870cc18491835bf49be8cb41f0af89c5e16a3835a4ca555b8f6e66461908" => :sierra
   end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "help2man" => :build
+  depends_on "xz" => :build
   depends_on "coreutils"
   depends_on "wget"
   depends_on "gnu-sed"
   depends_on "gawk"
   depends_on "binutils"
   depends_on "libelf"
-  depends_on "grep" => :optional
-  depends_on "make" => :optional
+  depends_on "grep"
+  depends_on "make"
 
   def install
+
+    ENV.append "MAKE", "gmake"
+    ENV.append "OBJDUMP", "gobjdump"
+    ENV.append "READELF", "greadelf"
+    ENV.append "LIBTOOL", "glibtool"
+    ENV.append "LIBTOOLIZE", "glibtoolize"
+    ENV.append "OBJCOPY", "gobjcopy"
+    ENV.append "GREP", "ggrep"
+    ENV.append "INSTALL", "ginstall"
+
     args = %W[
       --prefix=#{prefix}
       --exec-prefix=#{prefix}
-      --with-objcopy=gobjcopy
-      --with-objdump=gobjdump
-      --with-readelf=greadelf
-      --with-libtool=glibtool
-      --with-libtoolize=glibtoolize
-      --with-install=ginstall
-      --with-sed=gsed
-      --with-awk=gawk
     ]
-
-    args << "--with-grep=ggrep" if build.with? "grep"
-    args << "--with-make=#{Formula["make"].opt_bin}/gmake" if build.with? "make"
-    args << "CFLAGS=-std=gnu89"
 
     system "./configure", *args
 
     # Must be done in two steps
     system "make"
     system "make", "install"
-  end
 
-  def caveats; <<-EOS.undent
-    You will need to install a modern gcc compiler in order to use this tool.
-    EOS
   end
 
   test do
     assert_match "This is crosstool-NG", shell_output("make -rf #{bin}/ct-ng version")
   end
+  
 end
