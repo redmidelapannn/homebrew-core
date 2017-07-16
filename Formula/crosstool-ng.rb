fce786ec961abd2c37e9286eb9a98ae49db469d3
Matteo Pacini <ispeakprogramming@gmail.com>
2017-07-16 18:16:48 +0200

    Fix newlines II and removed revision

--- Formula/crosstool-ng.rb
+++ Formula/crosstool-ng.rb
@@ -3,12 +3,6 @@ class CrosstoolNg < Formula
   homepage "http://crosstool-ng.org"
   url "http://crosstool-ng.org/download/crosstool-ng/crosstool-ng-1.23.0.tar.xz"
   sha256 "68a43ea98ccf9cb345cb6eec494a497b224fee24c882e8c14c6713afbbe79196"
-  revision 1
-
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "2342870cc18491835bf49be8cb41f0af89c5e16a3835a4ca555b8f6e66461908" => :sierra
-  end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -49,5 +43,4 @@ class CrosstoolNg < Formula
   test do
     assert_match "This is crosstool-NG", shell_output("make -rf #{bin}/ct-ng version")
   end
-
 end
