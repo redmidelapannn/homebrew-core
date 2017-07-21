287f1d5483e51f0693aa734c4f9c424f6c5dc5bb
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-21 16:41:55 +0000

    v2ray-core: add 1.4.3 bottle.

--- Formula/v2ray-core.rb
+++ Formula/v2ray-core.rb
@@ -5,6 +5,13 @@ class V2rayCore < Formula
   version "1.4.3"
   sha256 "65506efa3810945b7506d00f237a65d399fe682558e869a41a4ee3f93011e287"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9c28b7ce5c8381ab8b8e783534903ec58f27dbb4cefb39677c3f3ce7398defd4" => :sierra
+    sha256 "9c28b7ce5c8381ab8b8e783534903ec58f27dbb4cefb39677c3f3ce7398defd4" => :el_capitan
+    sha256 "9c28b7ce5c8381ab8b8e783534903ec58f27dbb4cefb39677c3f3ce7398defd4" => :yosemite
+  end
+
   def install
       bin.install "v2ray"
       bin.install_symlink "v2ray" => "v2ray"
