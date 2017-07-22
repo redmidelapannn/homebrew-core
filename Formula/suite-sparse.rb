e2fc6c55c610c9b8c89df534cc8f9e55000225ad
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-22 16:37:02 +0000

    suite-sparse: update 4.5.5 bottle.

--- Formula/suite-sparse.rb
+++ Formula/suite-sparse.rb
@@ -6,9 +6,10 @@ class SuiteSparse < Formula
 
   bottle do
     cellar :any
-    sha256 "9344c95830eebd2e6a418beb5a3671cb3955a8b798486994d2c2163c7a4a86c2" => :sierra
-    sha256 "925358aebf66ab22eb8f577cb3adcfc1f16045645266c4bab226a520261f78c0" => :el_capitan
-    sha256 "3a527e97525683038594795e95794cb763aabad83c6df44fcecc0c7e57a8ef45" => :yosemite
+    rebuild 1
+    sha256 "159adb29b8e4878c08468ec0fe3d2f4ee98c9977ee23449d2ffd31b158cf12ca" => :sierra
+    sha256 "41a4805719104a4a970dd6143fe3f15dcfba2281f2e7adda0c0dde6273e05cce" => :el_capitan
+    sha256 "b2c9b8f46d8e6c5c07c9867016109c7b45773216185837c1b07641fe80ea95b6" => :yosemite
   end
 
   depends_on "metis"
