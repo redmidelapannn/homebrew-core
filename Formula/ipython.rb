e4e9fbab2f66b0895326fb1ce1bea271bda8a8c0
Chris Long <christopherglong@mac.com>
2017-07-27 02:24:57 -0700

    ipython: install kernel and outside virtualenv

--- Formula/ipython.rb
+++ Formula/ipython.rb
@@ -113,15 +113,41 @@ class Ipython < Formula
   end
 
   def install
-    venv = virtualenv_create(libexec, "python3")
+    xy = Language::Python.major_minor_version "python3"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python#{xy}/site-packages"
+
+    # install other resources
     ipykernel = resource("ipykernel")
+    (resources - [ipykernel]).each do |r|
+      r.stage do
+        system "python3", *Language::Python.setup_install_args(libexec/"vendor")
+      end
+    end
 
-    venv.pip_install (resources - [ipykernel])
-    venv.pip_install_and_link buildpath
-    venv.pip_install ipykernel
+    # install and link IPython
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
+    system "python3", *Language::Python.setup_install_args(libexec)
+    bin.install libexec/"bin/ipython"
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
 
+    # install IPython man page
     man1.mkpath
     man1.install libexec/"share/man/man1/ipython.1"
+
+    # install IPyKernel
+    ipykernel.stage do
+      system "python3", *Language::Python.setup_install_args(libexec/"vendor")
+    end
+
+    # install kernel
+    system libexec/"bin/ipython", "kernel", "install", "--prefix", buildpath
+    inreplace buildpath/"share/jupyter/kernels/python3/kernel.json", "]", <<-EOS.undent
+      ],
+      "env": {
+        "PYTHONPATH": "#{ENV["PYTHONPATH"]}"
+      }
+    EOS
+    (etc/"jupyter/kernels/python3").install Dir[buildpath/"share/jupyter/kernels/python3/*"]
   end
 
   test do
