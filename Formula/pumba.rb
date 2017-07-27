8abd79e02fe7a744d4611a87c174f8afa0548c99
BrewTestBot <brew-test-bot@googlegroups.com>
2017-07-27 07:29:47 +0000

    pumba: add 0.4.4 bottle.

--- Formula/pumba.rb
+++ Formula/pumba.rb
@@ -5,6 +5,13 @@ class Pumba < Formula
   sha256 "fdf11426752c69e79c2db10e2f57ef41c8b5d3d6815602ed11a95402b5db2d35"
   head "https://github.com/gaia-adm/pumba.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "80995e4dc7ba0553b8a6ad611c45b26bf094c7b24f12af777866c5e7b20625c3" => :sierra
+    sha256 "1374f57ce94bdce9a0512b9dbea753bf81d5ca4ceb5eddac3b6d06cc1c21470a" => :el_capitan
+    sha256 "e74021e46d70d4d00f6c9358d1f827c90222833faa0e76e8626a14cae36be8c5" => :yosemite
+  end
+
   depends_on "go" => :build
   depends_on "glide" => :build
 
