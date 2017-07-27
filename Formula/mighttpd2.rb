d2c70323835e12adc0e56d024f3435d9abb88cad
ilovezfs <ilovezfs@icloud.com>
2017-07-27 10:51:19 -0700

    mighttpd2: depend on ghc
    instead of ghc@8.0

--- Formula/mighttpd2.rb
+++ Formula/mighttpd2.rb
@@ -7,6 +7,7 @@ class Mighttpd2 < Formula
   homepage "https://www.mew.org/~kazu/proj/mighttpd/en/"
   url "https://hackage.haskell.org/package/mighttpd2-3.4.1/mighttpd2-3.4.1.tar.gz"
   sha256 "0f24c72662be4a0e3d75956fff53899216e29ac7eb29dae771c7e4eb77bdd8d5"
+  revision 1
 
   bottle do
     sha256 "7f26272fd123a12d54b5cdbf52ede0131885da7e1f9778eb36b6481c6b9e4186" => :sierra
@@ -14,7 +15,7 @@ class Mighttpd2 < Formula
     sha256 "b21f19f4262c05883e11b37388ff46126c1130bf84c568105e9e47b6266290d6" => :yosemite
   end
 
-  depends_on "ghc@8.0" => :build
+  depends_on "ghc" => :build
   depends_on "cabal-install" => :build
 
   def install
