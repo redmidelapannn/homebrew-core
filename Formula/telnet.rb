5cb83e3791af0f521c5b01ea17f08fc5fc0436d4
Kirill byss Bystrov <kirrbyss@gmail.com>
2017-07-30 05:35:35 +0300

    Merged libtelnet & telnet formulas

--- Formula/telnet.rb
+++ Formula/telnet.rb
@@ -4,10 +4,24 @@ class Telnet < Formula
   url "https://opensource.apple.com/tarballs/remote_cmds/remote_cmds-54.50.1.tar.gz"
   sha256 "156ddec946c81af1cbbad5cc6e601135245f7300d134a239cda45ff5efd75930"
 
+  depends_on :xcode => :build
   depends_on :macos => :high_sierra
 
+  resource "libtelnet" do
+    url "https://opensource.apple.com/tarballs/libtelnet/libtelnet-13.tar.gz"
+    sha256 "e7d203083c2d9fa363da4cc4b7377d4a18f8a6f569b9bcf58f97255941a2ebd1"
+  end
+
   def install
-    system "make", "-C", "telnet.tproj", "SDKROOT=", "OBJROOT=build/Intermediates", "SYMROOT=build/Products", "DSTROOT=build/Archive", "install"
+    resource("libtelnet").stage do
+      xcodebuild "SYMROOT=build"
+
+      libtelnet_dst = buildpath / "telnet.tproj" / "build" / "Products"
+      libtelnet_dst.install("build/Release/libtelnet.a")
+      libtelnet_dst.install("build/Release/usr/local/include/libtelnet/")
+    end
+
+    system "make", "-C", "telnet.tproj", "OBJROOT=build/Intermediates", "SYMROOT=build/Products", "DSTROOT=build/Archive", "CFLAGS=$(CC_Flags) -isystembuild/Products/", "LDFLAGS=$(LD_Flags) -Lbuild/Products/", "install"
 
     bin.install "telnet.tproj/build/Archive/usr/bin/telnet"
     man.install "telnet.tproj/build/Archive/usr/share/man/man1/"
@@ -16,5 +30,8 @@ class Telnet < Formula
   test do
     output = shell_output("(sleep 2; echo 'quit') | telnet towel.blinkenlights.nl", 1)
     assert_match "So long And Thanks for all the fish", output
+
+    output = shell_output("MANPAGER='head -n 2' man telnet")
+    assert_match "TELNET(1)", output
   end
 end
