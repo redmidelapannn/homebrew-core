c5d057cabcc8179a7eece97a019049c00e73c5c8
tpoole <t.b.poole@gmail.com>
2017-07-31 16:29:43 +0100

    gnuplot: fix for compiling cairo based terminals

--- Formula/gnuplot.rb
+++ Formula/gnuplot.rb
@@ -67,7 +67,8 @@ class Gnuplot < Formula
       system "./configure", "--prefix=#{buildpath}/libcerf", "--enable-static", "--disable-shared"
       system "make", "install"
     end
-    ENV.prepend "PKG_CONFIG_PATH", buildpath/"libcerf/lib/pkgconfig"
+
+    ENV.prepend_path "PKG_CONFIG_PATH", "#{buildpath}/libcerf/lib/pkgconfig"
 
     args = %W[
       --disable-dependency-tracking
