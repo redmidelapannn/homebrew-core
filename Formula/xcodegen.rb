a852dc3a1b79e7f9e2025691a9593c3cb9a6f1a4
Pedro Piñera <pepibumur@gmail.com>
2017-08-02 13:36:19 +0200

    Remove unnecessary require, update description to remove the period at the end, and remove the version and let Homebrew to figure it out from the url

--- Formula/xcodegen.rb
+++ Formula/xcodegen.rb
@@ -1,10 +1,7 @@
-require "English"
-
 class Xcodegen < Formula
-  desc "Tool that generates your Xcode project from a project spec."
+  desc "Tool that generates your Xcode project from a project spec"
   homepage "https://github.com/yonaskolb/XcodeGen"
-  url "https://github.com/yonaskolb/XcodeGen/archive/#{version}.tar.gz"
-  version "0.1"
+  url "https://github.com/yonaskolb/XcodeGen/archive/0.1.tar.gz"
   sha256 "29338d4fb17160408fc781e01143b3eff3dc4dc8db8305ef04864dea9e11bb62"
   head "https://github.com/yonaskolb/XcodeGen.git"
 
@@ -16,7 +13,6 @@ class Xcodegen < Formula
     ohai "Building XcodeGen"
     system("swift build -c release -Xlinker -rpath -Xlinker @executable_path -Xswiftc -static-stdlib")
     system("install_name_tool -change #{yaml_lib_path} #{frameworks}/libCYaml.dylib #{xcodegen_path}")
-    odie "Error building XcodeGen" if $CHILD_STATUS.exitstatus != 0
     frameworks.install yaml_lib_path
     bin.install xcodegen_path
   end
