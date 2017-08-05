13c76127f8318bea974c10e9a11d9d8548266fa2
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-05 20:24:30 +0000

    allegro: update 5.2.2 bottle.

--- Formula/allegro.rb
+++ Formula/allegro.rb
@@ -8,9 +8,10 @@ class Allegro < Formula
 
   bottle do
     cellar :any
-    sha256 "f1bcce974ffeeedb441e364fdc4a3fdc7eb34a0f051f612534a408baf206cf5c" => :sierra
-    sha256 "50894c87f68741f5f65e148a31662cf5a7feead3072884352b77a4e0be8860fa" => :el_capitan
-    sha256 "f87c93bceed439a7cd581ee6d305b30268d113cd1f76205c5f32651acde370a6" => :yosemite
+    rebuild 1
+    sha256 "8aa9a8bbfa6d4989ba412ad64ad8a708a22019e85289003e6fed4c5fc628477c" => :sierra
+    sha256 "7d1ca0056f72b30567b75639f04238c8380ddd1a94d846bfb5d6905598e74e59" => :el_capitan
+    sha256 "41a14b5c1041e104ab33e2f4e8fa83361a9a744f4b688a2caaca5f785e824173" => :yosemite
   end
 
   depends_on "cmake" => :build
