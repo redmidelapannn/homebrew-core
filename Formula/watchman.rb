fbb593e234350ccebd8391201189ef16a95c8a07
Wez Furlong <wez@wezfurlong.org>
2017-08-08 16:01:46 -0700

    add openssl dep
    We're depend on this for SHA1 computation

--- Formula/watchman.rb
+++ Formula/watchman.rb
@@ -15,6 +15,7 @@ class Watchman < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
+  depends_on "openssl"
   depends_on "pkg-config" => :build
   depends_on "pcre"
 
