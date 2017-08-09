500802723d1ba5c1fbdb1f4314fb3d8606fd00ef
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-09 12:44:38 +0000

    xmrig: add 2.2.1 bottle.

--- Formula/xmrig.rb
+++ Formula/xmrig.rb
@@ -3,6 +3,13 @@ class Xmrig < Formula
   homepage "https://github.com/xmrig/xmrig"
   url "https://github.com/xmrig/xmrig.git", :tag => "v2.2.1", :revision => "92d787c817dfa92ca38e36f634195f31ff7d40c3"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "0ed568eb7bba26596e5477e39c7f457773acd31e914021e07057d22a03a8d4fd" => :sierra
+    sha256 "5d8a7d51e12e0221c74a809b84e02fd12a763a6363cfb0395d71631a7cda1b44" => :el_capitan
+    sha256 "593eaef3d1112fbf8592d3c0d69b92e31d6615b3bbc024af000ffd28bc60f913" => :yosemite
+  end
+
   depends_on "cmake" => :build
   depends_on "libuv"
 
