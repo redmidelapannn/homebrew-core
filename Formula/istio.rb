c0e15cc9971da8f5f528bcec54a2515fcfc165e8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-09 20:57:06 +0000

    istio: add 0.1.6 bottle.

--- Formula/istio.rb
+++ Formula/istio.rb
@@ -4,6 +4,13 @@ class Istio < Formula
   url "https://github.com/istio/istio/releases/download/0.1.6/istio-0.1.6-osx.tar.gz"
   sha256 "347904068bb6100aa15b2a030be872687143b86d8529c790212517405ddb0582"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c19c9673d57bdad31349c7558aff07901a183093fd73754dc8448e44d457030a" => :sierra
+    sha256 "c19c9673d57bdad31349c7558aff07901a183093fd73754dc8448e44d457030a" => :el_capitan
+    sha256 "c19c9673d57bdad31349c7558aff07901a183093fd73754dc8448e44d457030a" => :yosemite
+  end
+
   def install
     bin.install "bin/istioctl"
   end
