98bb74afb90cec1aca45561a68822c66098994db
Dominik Lohmann <info@dominik-lohmann.de>
2017-08-10 05:28:41 +0200

    Apply further changes as requesteds by DomT4

--- Formula/chunkwm.rb
+++ Formula/chunkwm.rb
@@ -65,8 +65,6 @@ class Chunkwm < Formula
     EOS
   end
 
-  plist_options :startup => false
-
   def plist; <<-EOS.undent
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
