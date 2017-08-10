93c1178db91b043639ae2fa307a4c2203ce025b0
Dominik Lohmann <info@dominik-lohmann.de>
2017-08-10 09:26:15 +0200

    Fix #{var} not being available in options

--- Formula/chunkwm.rb
+++ Formula/chunkwm.rb
@@ -12,7 +12,7 @@ class Chunkwm < Formula
   option "without-ffm", "Do not build focus-follow-mouse plugin."
   option "without-border", "Do not build border plugin."
   option "with-transparency", "Build transparency plugin."
-  option "with-logging", "Redirect stdout and stderr to #{var}/log/chunkwm.log"
+  option "with-logging", "Redirect stdout and stderr to /usr/local/var/log/chunkwm.log"
 
   def install
     # install chunkwm
