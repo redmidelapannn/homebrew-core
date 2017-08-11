b3614f21e8ac490bddf5ef3d27cda3fa4324bdd3
Simon Wells <simonrwells@gmail.com>
2017-08-12 01:42:56 +1200

    readded :sierra if/else due to https/http

--- Formula/spim.rb
+++ Formula/spim.rb
@@ -2,7 +2,11 @@ class Spim < Formula
   desc "MIPS32 simulator"
   homepage "https://spimsimulator.sourceforge.io/"
   # No source code tarball exists
-  url "https://svn.code.sf.net/p/spimsimulator/code", :revision => 707
+  if MacOS.version >= :sierra
+    url "https://svn.code.sf.net/p/spimsimulator/code", :revision => 707
+  else
+    url "http://svn.code.sf.net/p/spimsimulator/code", :revision => 707
+  end
   version "9.1.19"
   head "https://svn.code.sf.net/p/spimsimulator/code/"
 
