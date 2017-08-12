37f29206f16ed1f15f05ccf0abf423cb7b2cf818
ilovezfs <ilovezfs@icloud.com>
2017-08-12 06:55:48 -0700

    Update makensis.rb

--- Formula/makensis.rb
+++ Formula/makensis.rb
@@ -43,7 +43,8 @@ class Makensis < Formula
     end
 
     # Don't strip, see https://github.com/Homebrew/homebrew/issues/28718
-    args = ["STRIP=0", "ZLIB_W32=#{@zlib_path}", "SKIPUTILS=NSIS Menu", "VERSION=#{version}"]
+    args = ["STRIP=0", "ZLIB_W32=#{@zlib_path}", "SKIPUTILS=NSIS Menu",
+            "VERSION=#{version}"]
     args << "NSIS_MAX_STRLEN=8192" if build.with? "large-strings"
     scons "makensis", *args
     bin.install "build/urelease/makensis/makensis"
