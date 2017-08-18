13a7a7e07515bd7d8d28a6325ff38b8246447f2f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-18 13:11:19 +0000

    s3deploy: add 1.0.8 bottle.

--- Formula/s3deploy.rb
+++ Formula/s3deploy.rb
@@ -5,6 +5,13 @@ class S3deploy < Formula
   version "1.0.8"
   sha256 "c6d47f0635bbd821553e72414c49dd767df980622112e23e81a9edd54c3b7499"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "73c159a04490f3c289aae658135ced495ae2d7bf48368ac76fcb4301e3f1b27d" => :sierra
+    sha256 "73c159a04490f3c289aae658135ced495ae2d7bf48368ac76fcb4301e3f1b27d" => :el_capitan
+    sha256 "73c159a04490f3c289aae658135ced495ae2d7bf48368ac76fcb4301e3f1b27d" => :yosemite
+  end
+
   def install
     bin.install "s3deploy"
   end
