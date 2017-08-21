5254c9f515c38c91cb534a21f9ef3b4000429acc
Peter Romfeld <peter@exiconglobal.com>
2017-08-21 15:33:50 +0800

    geth 1.6.7 (new formula)
    Add formula for geth. Official golang implementation of the Ethereum
    protocol (https://github.com/ethereum/go-ethereum). This formula does
    not include other development tools provided by go-ethereum.

---
+++ Formula/geth.rb
@@ -0,0 +1,19 @@
+class Geth < Formula
+  desc "Official Go implementation of the Ethereum protocol"
+  homepage "https://ethereum.github.io/go-ethereum/"
+  url "https://github.com/ethereum/go-ethereum/archive/v1.6.7.tar.gz"
+  sha256 "3e2a75b55ee8f04f238682164a7a255cae7a1f939893c5c97c2adcf48d7d4d49"
+  head "https://github.com/ethereum/go-ethereum.git"
+
+  depends_on "go" => :build
+  depends_on "cmake" => :build
+
+  def install
+    system "make", "geth"
+    bin.install "build/bin/geth"
+  end
+
+  test do
+    system "#{bin}/geth", "-h"
+  end
+end
