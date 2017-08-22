af27c569bd7f1a62b944b7d00d12d0e5f9302bcf
ilovezfs <ilovezfs@icloud.com>
2017-08-22 09:42:52 -0700

    Update geth.rb

--- Formula/geth.rb
+++ Formula/geth.rb
@@ -14,20 +14,20 @@ class Geth < Formula
 
   test do
     (testpath/"genesis.json").write <<-EOS.undent
-    {
-      "config": {
-        "homesteadBlock": 10
-      },
-      "nonce": "0",
-      "difficulty": "0x20000",
-      "mixhash": "0x00000000000000000000000000000000000000647572616c65787365646c6578",
-      "coinbase": "0x0000000000000000000000000000000000000000",
-      "timestamp": "0x00",
-      "parentHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
-      "extraData": "0x",
-      "gasLimit": "0x2FEFD8",
-      "alloc": {}
-    }
+      {
+        "config": {
+          "homesteadBlock": 10
+        },
+        "nonce": "0",
+        "difficulty": "0x20000",
+        "mixhash": "0x00000000000000000000000000000000000000647572616c65787365646c6578",
+        "coinbase": "0x0000000000000000000000000000000000000000",
+        "timestamp": "0x00",
+        "parentHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+        "extraData": "0x",
+        "gasLimit": "0x2FEFD8",
+        "alloc": {}
+      }
     EOS
     system "#{bin}/geth", "--datadir", "testchain", "init", "genesis.json"
     assert_predicate testpath/"testchain/geth/chaindata/000001.log", :exist?,
