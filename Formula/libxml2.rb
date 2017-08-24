ca2a3d4980e0d5ced154cbce8f7c135e471357ec
ilovezfs <ilovezfs@icloud.com>
2017-08-24 02:17:35 -0700

    Update libxml2.rb

--- Formula/libxml2.rb
+++ Formula/libxml2.rb
@@ -46,6 +46,7 @@ class Libxml2 < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
 
   def install
     inreplace "autogen.sh", "libtoolize", "glibtoolize"
