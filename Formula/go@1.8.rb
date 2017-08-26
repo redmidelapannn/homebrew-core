af8391e8f95bc2ebd96cfa593cf6da6cf3f62de0
ilovezfs <ilovezfs@icloud.com>
2017-08-26 04:48:49 -0700

    Update go@1.8.rb

--- Formula/go@1.8.rb
+++ Formula/go@1.8.rb
@@ -10,7 +10,6 @@ class GoAT18 < Formula
   keg_only :versioned_formula
 
   option "without-cgo", "Build without cgo (also disables race detector)"
-  option "without-godoc", "godoc will not be installed for you"
   option "without-race", "Build without race detector"
 
   depends_on :macos => :mountain_lion
@@ -20,24 +19,17 @@ class GoAT18 < Formula
         :branch => "release-branch.go1.8"
   end
 
+  # Don't update this unless this version cannot bootstrap the new version.
   resource "gobootstrap" do
-    url "https://storage.googleapis.com/golang/go1.4-bootstrap-20161024.tar.gz"
-    sha256 "398c70d9d10541ba9352974cc585c43220b6d8dbcd804ba2c9bd2fbf35fab286"
+    url "https://storage.googleapis.com/golang/go1.7.darwin-amd64.tar.gz"
+    version "1.7"
+    sha256 "51d905e0b43b3d0ed41aaf23e19001ab4bc3f96c3ca134b48f7892485fc52961"
   end
 
   def install
-    # GOROOT_FINAL must be overidden later on real Go install
-    ENV["GOROOT_FINAL"] = buildpath/"gobootstrap"
-
-    # build the gobootstrap toolchain Go >=1.4
     (buildpath/"gobootstrap").install resource("gobootstrap")
-    cd "#{buildpath}/gobootstrap/src" do
-      system "./make.bash", "--no-clean"
-    end
+    ENV["GOROOT_BOOTSTRAP"] = buildpath/"gobootstrap"
 
-    # This should happen after we build the test Go, just in case
-    # the bootstrap toolchain is aware of this variable too.
-    ENV["GOROOT_BOOTSTRAP"] = ENV["GOROOT_FINAL"]
     cd "src" do
       ENV["GOROOT_FINAL"] = libexec
       ENV["GOOS"]         = "darwin"
@@ -48,31 +40,28 @@ class GoAT18 < Formula
     (buildpath/"pkg/obj").rmtree
     rm_rf "gobootstrap" # Bootstrap not required beyond compile.
     libexec.install Dir["*"]
-    bin.install_symlink Dir["#{libexec}/bin/go*"]
+    bin.install_symlink Dir[libexec/"bin/go*"]
 
     # Race detector only supported on amd64 platforms.
     # https://golang.org/doc/articles/race_detector.html
     if build.with?("cgo") && build.with?("race") && MacOS.prefer_64_bit?
-      system "#{bin}/go", "install", "-race", "std"
+      system bin/"go", "install", "-race", "std"
     end
 
-    if build.with?("godoc")
-      ENV.prepend_path "PATH", bin
-      ENV["GOPATH"] = buildpath
-      (buildpath/"src/golang.org/x/tools").install resource("gotools")
-
-      if build.with? "godoc"
-        cd "src/golang.org/x/tools/cmd/godoc/" do
-          system "go", "build"
-          (libexec/"bin").install "godoc"
-        end
-        bin.install_symlink libexec/"bin/godoc"
-      end
+    # Build and install godoc
+    ENV.prepend_path "PATH", bin
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/golang.org/x/tools").install resource("gotools")
+    cd "src/golang.org/x/tools/cmd/godoc/" do
+      system "go", "build"
+      (libexec/"bin").install "godoc"
     end
+    bin.install_symlink libexec/"bin/godoc"
   end
 
   def caveats; <<-EOS.undent
-    As of go 1.2, a valid GOPATH is required to use the `go get` command:
+    A valid GOPATH is required to use the `go get` command.
+    If $GOPATH is not specified, $HOME/go will be used by default:
       https://golang.org/doc/code.html#GOPATH
 
     You may wish to add the GOROOT-based install location to your PATH:
@@ -83,21 +72,21 @@ class GoAT18 < Formula
   test do
     (testpath/"hello.go").write <<-EOS.undent
     package main
+
     import "fmt"
+
     func main() {
         fmt.Println("Hello World")
     }
     EOS
-
     # Run go fmt check for no errors then run the program.
     # This is a a bare minimum of go working as it uses fmt, build, and run.
-    system "#{bin}/go", "fmt", "hello.go"
+    system bin/"go", "fmt", "hello.go"
     assert_equal "Hello World\n", shell_output("#{bin}/go run hello.go")
 
-    if build.with? "godoc"
-      assert File.exist?(libexec/"bin/godoc")
-      assert File.executable?(libexec/"bin/godoc")
-    end
+    # godoc was installed
+    assert File.exist?(libexec/"bin/godoc")
+    assert File.executable?(libexec/"bin/godoc")
 
     if build.with? "cgo"
       ENV["GOOS"] = "freebsd"
