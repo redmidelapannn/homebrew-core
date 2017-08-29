cf3eb99733414dbc46f9b690507ff381841693f6
Mikhail Filippov <Mikhail.Filippov@jetbrains.com>
2017-08-29 22:36:58 +0300

    mono 5.2.0.215, fsharp 4.1.25, msbuild 15.3

--- Formula/mono.rb
+++ Formula/mono.rb
@@ -1,8 +1,8 @@
 class Mono < Formula
   desc "Cross platform, open source .NET development framework"
   homepage "http://www.mono-project.com/"
-  url "https://download.mono-project.com/sources/mono/mono-5.0.1.1.tar.bz2"
-  sha256 "48d6ae71d593cd01bf0f499de569359d45856cda325575e1bacb5fabaa7e9718"
+  url "https://download.mono-project.com/sources/mono/mono-5.2.0.215.tar.bz2"
+  sha256 "8f0cebd3f7b03f68b9bd015706da9c713ed968004612f1ef8350993d8fe850ea"
 
   bottle do
     sha256 "ae35573df33c718c3d9999572392480d2426cc995cfc4b123f0a66f885ea8053" => :high_sierra
@@ -19,22 +19,30 @@ class Mono < Formula
   link_overwrite "bin/fsharpiAnyCpu"
   link_overwrite "bin/fsharpc"
   link_overwrite "bin/fssrgen"
+  link_overwrite "bin/msbuild"
   link_overwrite "lib/mono"
   link_overwrite "lib/cli"
 
   option "without-fsharp", "Build without support for the F# language."
+  option "without-msbuild", "Build without support for the MSBuild."
 
   depends_on "automake" => :build
   depends_on "autoconf" => :build
   depends_on "pkg-config" => :build
   depends_on "cmake" => :build
+  depends_on "openssl" => :build
 
   conflicts_with "xsd", :because => "both install `xsd` binaries"
 
   resource "fsharp" do
     url "https://github.com/fsharp/fsharp.git",
-        :tag => "4.1.18",
-        :revision => "3245fd24efcc7a54d4314a2897257f68cd194244"
+        :tag => "4.1.25",
+        :revision => "9687f27c3e6be7b9e1646bb9ee1ae0b02080daae"
+  end
+
+  resource "msbuild" do
+    url "https://@github.com/mono/msbuild.git",
+        :revision => "17f02c2719a2c503f2955f8b15d00cc7db90ab2a"
   end
 
   def install
@@ -46,13 +54,40 @@ class Mono < Formula
     ]
 
     args << "--build=" + (MacOS.prefer_64_bit? ? "x86_64": "i686") + "-apple-darwin"
-
     system "./configure", *args
     system "make"
     system "make", "install"
     # mono-gdb.py and mono-sgen-gdb.py are meant to be loaded by gdb, not to be
     # run directly, so we move them out of bin
     libexec.install bin/"mono-gdb.py", bin/"mono-sgen-gdb.py"
+
+    # Now build and install msbuild as well
+    if build.with? "msbuild"
+      resource("msbuild").stage do
+        ENV.prepend_path "PATH", bin
+        inreplace "init-tools.sh", "$__DOTNET_CMD restore \"",
+                    "  DYLD_LIBRARY_PATH=#{Formula["openssl"].opt_prefix}/lib $__DOTNET_CMD restore \""
+        inreplace "init-tools.sh", "    $__BUILD_TOOLS_PATH/init-tools.sh",
+                    "    sed -i '.origin' 's|^$__DOTNET_CMD |DYLD_LIBRARY_PATH=#{Formula["openssl"].opt_prefix}/lib $__DOTNET_CMD |g' $__BUILD_TOOLS_PATH/init-tools.sh\n    $__BUILD_TOOLS_PATH/init-tools.sh"
+        inreplace "init-tools.sh", "    $__BUILD_TOOLS_PATH/init-tools.sh",
+                    "    sed -i '.origin' $'s|# Temporary Hacks to fix couple of issues in the msbuild and roslyn nuget packages" \
+                    "|mv $__DOTNET_CMD \"${__DOTNET_CMD}.exe\"<NEWLINE>necho \"#!/usr/bin/env bash\" >> $__DOTNET_CMD<NEWLINE>n" \
+                    "echo \"DYLD_LIBRARY_PATH=/usr/local/opt/openssl/lib ${__DOTNET_CMD}.exe \\\\\\\\\\$@\" >> \"$__DOTNET_CMD\"<NEWLINE>n" \
+                    "chmod +x \"$__DOTNET_CMD\"<NEWLINE>n# Temporary Hacks to fix couple of issues in the msbuild and roslyn nuget packages|g'" \
+                    " $__BUILD_TOOLS_PATH/init-tools.sh\n    $__BUILD_TOOLS_PATH/init-tools.sh"
+        inreplace "init-tools.sh", "<NEWLINE>", "\\\\\\\\\\\\"
+        system "./cibuild.sh", "--scope", "Compile", "--host", "Mono", "--target", "Mono", "--config", "Release"
+        mkpath "#{lib}/mono/msbuild/15.0/bin"
+        cp_r "bin/Release-MONO/AnyCPU/OSX/OSX_Deployment/.", "#{lib}/mono/msbuild/15.0/bin"
+        rm_r Dir["#{lib}/mono/msbuild/15.0/bin/*UnitTests*"]
+        rm_r Dir["#{lib}/mono/msbuild/15.0/bin/*xunit*"]
+        open("#{bin}/msbuild", "w+") do |f|
+          f << "#!/bin/sh\n"
+          f << "MONO_GC_PARAMS=\"nursery-size=64m,$MONO_GC_PARAMS\" exec #{bin}/mono $MONO_OPTIONS #{lib}/mono/msbuild/15.0/bin/MSBuild.dll \"$@\"\n"
+        end
+        chmod "a=rwx", "#{bin}/msbuild"
+      end
+    end
 
     # Now build and install fsharp as well
     if build.with? "fsharp"
@@ -69,7 +104,7 @@ class Mono < Formula
   def caveats; <<-EOS.undent
     To use the assemblies from other formulae you need to set:
       export MONO_GAC_PREFIX="#{HOMEBREW_PREFIX}"
-    Note that the 'mono' formula now includes F#. If you have
+    Note that the 'mono' formula now includes F# and msbuild. If you have
     the 'fsharp' formula installed, remove it with 'brew uninstall fsharp'.
     EOS
   end
@@ -105,6 +140,7 @@ class Mono < Formula
       </Project>
     EOS
     system bin/"xbuild", "test.csproj"
+    system bin/"msbuild", "/version"
 
     if build.with? "fsharp"
       # Test that fsharpi is working
