4ce536b9fbab2a1ab09216ca43527c9db81e97d7
rien333 <rien334@gmail.com>
2017-08-29 15:12:51 +0200

    Prefer a shared library over a static library; static library is now optional.

--- Formula/brainfuck.rb
+++ Formula/brainfuck.rb
@@ -1,23 +1,31 @@
 class Brainfuck < Formula
   desc "Interpreter for brainfuck written in C"
   homepage "https://github.com/fabianishere/brainfuck"
-  url "https://github.com/fabianishere/brainfuck/archive/2.7.0.tar.gz"
-  sha256 "445e00ef848f8b9a3ac101872ac08046b32b3477520ff06804d0ede5c3654c63"
+  url "https://github.com/fabianishere/brainfuck/archive/2.7.1.tar.gz"
+  sha256 "528d57d823f3ee4bdcdc3315f99fed69d2e23aec34682fb6670d02f5dec0b40f"
+
   head "https://github.com/fabianishere/brainfuck.git"
 
   option "with-debug", "Compile interpreter with debug support"
+  option "with-static-lib", "Build a static library"
 
   depends_on "cmake" => :build
 
   def install
     args = std_cmake_args
+    args << "-DBUILD_SHARED_LIB=ON"
     args << "-DENABLE_DEBUG=ON" if build.with? "debug"
 
     system "cmake", ".", *args
     system "make"
     bin.install "brainfuck"
     man1.install "man/brainfuck.1"
-    lib.install "libbrainfuck.a"
+    lib.install "libbrainfuck.dylib"
+    if build.with? "static-lib"
+      lib.install "libbrainfuck.a"
+    else # upstream uses this static library in the build process
+      rm "libbrainfuck.a"
+    end
     pkgshare.install "examples"
   end
 
