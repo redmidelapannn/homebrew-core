f58e8c3d9afb03096c33049063013f3f7964e143
rien333 <rien334@gmail.com>
2017-08-30 13:27:18 +0200

    Addition of a make install command, which adds a flexible examples option

--- Formula/brainfuck.rb
+++ Formula/brainfuck.rb
@@ -2,11 +2,12 @@ class Brainfuck < Formula
   desc "Interpreter for brainfuck written in C"
   homepage "https://github.com/fabianishere/brainfuck"
   url "https://github.com/fabianishere/brainfuck/archive/2.7.1.tar.gz"
-  sha256 "528d57d823f3ee4bdcdc3315f99fed69d2e23aec34682fb6670d02f5dec0b40f"
+  sha256 "06534de715dbc614f08407000c2ec6d497770069a2d7c84defd421b137313d71"
 
   head "https://github.com/fabianishere/brainfuck.git"
 
   option "with-debug", "Compile interpreter with debug support"
+  option "with-examples", "Install brainfuck example programs"
   option "with-static-lib", "Build a static library"
 
   depends_on "cmake" => :build
@@ -15,21 +16,15 @@ class Brainfuck < Formula
     args = std_cmake_args
     args << "-DBUILD_SHARED_LIB=ON"
     args << "-DENABLE_DEBUG=ON" if build.with? "debug"
+    args << "-DINSTALL_EXAMPLES=ON" if build.with? "examples"
+    args << "-DBUILD_STATIC_LIB=OFF" if build.without? "static-lib"
 
     system "cmake", ".", *args
     system "make"
-    bin.install "brainfuck"
-    man1.install "man/brainfuck.1"
-    lib.install "libbrainfuck.dylib"
-    if build.with? "static-lib"
-      lib.install "libbrainfuck.a"
-    else # upstream uses this static library in the build process
-      rm "libbrainfuck.a"
-    end
-    pkgshare.install "examples"
+    system "make", "install"
   end
 
   test do
-    system "#{bin}/brainfuck", "#{doc}/examples/hello.bf"
+    system "#{bin}/brainfuck", "-e", "++++++++[>++++++++<-]>+.+.+."
   end
 end
