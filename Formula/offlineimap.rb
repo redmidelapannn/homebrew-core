2dfad2034f458b7f5d3ee815b4b6f8cd22dabb7a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-08-31 09:45:50 +0000

    offlineimap: update 7.1.2_1 bottle.

--- Formula/offlineimap.rb
+++ Formula/offlineimap.rb
@@ -8,9 +8,10 @@ class Offlineimap < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "383a8102a3cd84f85424f4fd280ff4ff3fad8c3032802a6013a08846e8800d91" => :sierra
-    sha256 "c89e281547c1cf23754a80e0fddd48f2aecc7e94de8b606daac6d400bedca3b4" => :el_capitan
-    sha256 "c89e281547c1cf23754a80e0fddd48f2aecc7e94de8b606daac6d400bedca3b4" => :yosemite
+    rebuild 1
+    sha256 "22008aa235eb8a93e0a56e8a261ac1450240c21251e1636a5257fda79847464e" => :sierra
+    sha256 "22008aa235eb8a93e0a56e8a261ac1450240c21251e1636a5257fda79847464e" => :el_capitan
+    sha256 "22008aa235eb8a93e0a56e8a261ac1450240c21251e1636a5257fda79847464e" => :yosemite
   end
 
   depends_on "asciidoc" => :build
