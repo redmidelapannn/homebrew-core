09c522119226a035d49b13c976749c5537ff35fb
FX Coudert <fxcoudert@gmail.com>
2017-09-02 09:27:10 +0200

    xpdf 4.00

--- Formula/xpdf.rb
+++ Formula/xpdf.rb
@@ -1,9 +1,8 @@
 class Xpdf < Formula
-  desc "PDF viewer"
-  homepage "http://www.foolabs.com/xpdf/"
-  url "ftp://ftp.foolabs.com/pub/xpdf/xpdf-3.04.tar.gz"
-  sha256 "11390c74733abcb262aaca4db68710f13ffffd42bfe2a0861a5dfc912b2977e5"
-  revision 1
+  desc "PDF tools and viewer"
+  homepage "https://www.xpdfreader.com/"
+  url "https://www.xpdfreader.com/dl/xpdf-4.00.tar.gz"
+  sha256 "ff3d92c42166e35b1ba6aec9b5f0adffb5fc05a3eb95dc49505b6e344e4216d6"
 
   bottle do
     sha256 "a1abda067ab10b0e3a79ab9a93695ca2ad5fc674fff46a686ff11df47a076119" => :sierra
@@ -11,31 +10,19 @@ class Xpdf < Formula
     sha256 "3bd281f7bbc232ec0e353e3a54955383e13897fe563dfcadc4057e625803a6fb" => :yosemite
   end
 
-  depends_on "openmotif"
+  depends_on "cmake" => :build
   depends_on "freetype"
-  depends_on :x11
+  depends_on "libpng"
+  depends_on "qt" => :optional
 
   conflicts_with "pdf2image", "poppler",
     :because => "xpdf, pdf2image, and poppler install conflicting executables"
 
   def install
-    freetype = Formula["freetype"]
-    openmotif = Formula["openmotif"]
-    system "./configure", "--prefix=#{prefix}",
-                          "--with-freetype2-library=#{freetype.opt_lib}",
-                          "--with-freetype2-includes=#{freetype.opt_include}/freetype2",
-                          "--with-Xm-library=#{openmotif.opt_lib}",
-                          "--with-Xm-includes=#{openmotif.opt_include}",
-                          "--with-Xpm-library=#{MacOS::X11.lib}",
-                          "--with-Xpm-includes=#{MacOS::X11.include}",
-                          "--with-Xext-library=#{MacOS::X11.lib}",
-                          "--with-Xext-includes=#{MacOS::X11.include}",
-                          "--with-Xp-library=#{MacOS::X11.lib}",
-                          "--with-Xp-includes=#{MacOS::X11.include}",
-                          "--with-Xt-library=#{MacOS::X11.lib}",
-                          "--with-Xt-includes=#{MacOS::X11.include}"
-    system "make"
-    system "make", "install"
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
   end
 
   test do
