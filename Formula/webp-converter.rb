8f633ff0ccb038a57704949f5abb27c6519e4a00
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-06 07:09:23 +0000

    webp-converter: add 4.0.1 bottle.

--- Formula/webp-converter.rb
+++ Formula/webp-converter.rb
@@ -4,6 +4,13 @@ class WebpConverter < Formula
   url "https://github.com/Jacksgong/webp-converter/archive/v4.0.1.tar.gz"
   sha256 "4be6d5f6cb2e70779c3c6ad114d1d2e30c96d9682d85f3892e444c6cc197b39c"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "cadff697f1c416b8737537d7a9278a44a39f4c334cab56e221e221a9681ed3ce" => :sierra
+    sha256 "8b98bc2d65a41e7f8bd41b60bc0c6cce95ad380df99c7436d9b2df49999fb705" => :el_capitan
+    sha256 "8b98bc2d65a41e7f8bd41b60bc0c6cce95ad380df99c7436d9b2df49999fb705" => :yosemite
+  end
+
   depends_on :python if MacOS.version <= :snow_leopard
   depends_on "webp" => :run
 
