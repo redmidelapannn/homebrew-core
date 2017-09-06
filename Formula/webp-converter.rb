8d4ce22be0fbfacd95d099dd70710040f6388fb0
Jacksgong <igzhenjie@gmail.com>
2017-09-06 10:06:50 +0800

    feat: add webpc command to batch converter images to webp and analytics differ the code has been open source, and doc is also available on: https://github.com/Jacksgong/webp-converter

---
+++ Formula/webp-converter.rb
@@ -0,0 +1,26 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class WebpConverter < Formula
+  desc "Converter PNG/JPG to Webp and analytics all changes"
+  homepage "https://github.com/Jacksgong/webp-converter"
+  url "https://github.com/Jacksgong/webp-converter/archive/v4.0.0.tar.gz"
+  sha256 "481fc8d8a76b810e783dd8febbc4f88e4648c56e81af1cc6bda80d89c7e8f776"
+
+  depends_on :python if MacOS.version <= :snow_leopard
+  depends_on "webp"           => :run
+  # depends_on "cmake" => :build
+
+  def install
+    ENV["PYTHONPATH"] = libexec/"lib/python2.7/site-packages"
+
+    system "python", *Language::Python.setup_install_args(libexec)
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+  end
+
+  test do
+    system "#{bin}/webpc", "--help"
+  end
+end
