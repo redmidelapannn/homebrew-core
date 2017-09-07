dd6cdf162ae742f36abae147e652e839393b3fdd
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-07 23:12:42 +0000

    sdfcli: add 17.2.0 bottle.

--- Formula/sdfcli.rb
+++ Formula/sdfcli.rb
@@ -4,6 +4,13 @@ class Sdfcli < Formula
   url "https://github.com/limebox/sdf/raw/master/Files/brew/sdfcli-17.2.0.tar.gz"
   sha256 "984155c6ed680c581aa85f0e32e43bc7894db5182b3a372267780b5402f441f3"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6ba6bbeb1bd44936abd42b0d59d16d37a286471dc40ca6c722f4fa47a8b56a93" => :sierra
+    sha256 "6ba6bbeb1bd44936abd42b0d59d16d37a286471dc40ca6c722f4fa47a8b56a93" => :el_capitan
+    sha256 "6ba6bbeb1bd44936abd42b0d59d16d37a286471dc40ca6c722f4fa47a8b56a93" => :yosemite
+  end
+
   depends_on "maven" => :build
 
   def install
