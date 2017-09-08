a30d03b60bf926e51c4b764dbac484792b8524be
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-08 13:24:19 +0000

    apibuilder-cli: add 0.1.7 bottle.

--- Formula/apibuilder-cli.rb
+++ Formula/apibuilder-cli.rb
@@ -4,6 +4,13 @@ class ApibuilderCli < Formula
   url "https://github.com/apicollective/apibuilder-cli/archive/0.1.7.tar.gz"
   sha256 "69a828ebb11270fbc507042d140c54a46ba8050b723fc75f55104f78abc13be9"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5d76d7c779ca31f4d1fa5e76fcb1c9719fca10b32bcad088d2488e8b6d22ba39" => :sierra
+    sha256 "5d76d7c779ca31f4d1fa5e76fcb1c9719fca10b32bcad088d2488e8b6d22ba39" => :el_capitan
+    sha256 "5d76d7c779ca31f4d1fa5e76fcb1c9719fca10b32bcad088d2488e8b6d22ba39" => :yosemite
+  end
+
   def install
     system "./install.sh", prefix
   end
