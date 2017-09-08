797828beb2d56b2a2bbe63624cb4cfe077ed7531
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-08 11:24:42 +0000

    ydict: add 0.6 bottle.

--- Formula/ydict.rb
+++ Formula/ydict.rb
@@ -4,6 +4,12 @@ class Ydict < Formula
   url "https://github.com/TimothyYe/ydict/releases/download/V0.6/ydict-mac64-0.6.tar.gz"
   sha256 "8596ca84cee14975a83645f8aa3529ff47e26d939ba9521b95170dc13471c2e3"
 
+  bottle do
+    sha256 "817d05181d0c2621dddae0c2a019ec40e4fec93bb5c350dd159a9fc9db5122d7" => :sierra
+    sha256 "817d05181d0c2621dddae0c2a019ec40e4fec93bb5c350dd159a9fc9db5122d7" => :el_capitan
+    sha256 "817d05181d0c2621dddae0c2a019ec40e4fec93bb5c350dd159a9fc9db5122d7" => :yosemite
+  end
+
   def install
     bin.install "ydict"
   end
