d0ed006614d1d720d7de73df97ea04725b1498ce
FX Coudert <fxcoudert@gmail.com>
2017-09-10 12:41:13 +0200

    Update mpw.rb

--- Formula/mpw.rb
+++ Formula/mpw.rb
@@ -1,12 +1,10 @@
 class Mpw < Formula
   desc "Stateless/deterministic password and identity manager"
   homepage "https://ssl.masterpasswordapp.com/"
-
-  stable do
-    url "https://ssl.masterpasswordapp.com/mpw-2.6-cli-2-0-g30fdb54e.tar.gz"
-    sha256 "c206e512d193a154814a7c5d1faa8dc89e3fa0817b059bd6d957a61dc8ee1e68"
-    version "2.6-cli-2"
-  end
+  url "https://ssl.masterpasswordapp.com/mpw-2.6-cli-2-0-g30fdb54e.tar.gz"
+  version "2.6-cli-2"
+  sha256 "c206e512d193a154814a7c5d1faa8dc89e3fa0817b059bd6d957a61dc8ee1e68"
+  head "https://github.com/Lyndir/MasterPassword.git"
 
   bottle do
     cellar :any
@@ -17,12 +15,8 @@ class Mpw < Formula
     sha256 "290586cc77c94562e08977227209e16b9b821cb84e068bcf748b2e0ce07bdb0f" => :mavericks
   end
 
-  head do
-    url "https://github.com/Lyndir/MasterPassword.git"
-  end
-
-  option "without-json-c", "Disable JSON configuration support."
-  option "without-ncurses", "Disable colorized identicon support."
+  option "without-json-c", "Disable JSON configuration support"
+  option "without-ncurses", "Disable colorized identicon support"
 
   depends_on "libsodium"
   depends_on "json-c" => :recommended
@@ -31,28 +25,12 @@ class Mpw < Formula
   def install
     cd "platform-independent/cli-c" if build.head?
 
-    # Features
-    if build.with? "json-c"
-      ENV["mpw_json"] = "1"
-    else
-      ENV["mpw_json"] = "0"
-    end
-    if build.with? "ncurses"
-      ENV["mpw_color"] = "1"
-    else
-      ENV["mpw_color"] = "0"
-    end
-
-    # Targets
     ENV["targets"] = "mpw"
+    ENV["mpw_json"] = build.with?("json-c") ? "1" : "0"
+    ENV["mpw_color"] = build.with?("ncurses") ? "1" : "0"
 
-    # Build
     system "./build"
-
-    # Test
     system "./mpw-cli-tests"
-
-    # Install
     bin.install "mpw"
   end
 
