8db1e1491d23612df13b62f59fc5958a19673fff
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-11 17:53:29 +0000

    hello: update 2.10 bottle.

--- Formula/hello.rb
+++ Formula/hello.rb
@@ -6,11 +6,9 @@ class Hello < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "9691f91e7c6897d525b76f5910144e0ed0adcb8bd8f7bb155f29fd431d8ff431" => :sierra
-    sha256 "54ac46b692fcca2abe77aeeca41976172a2dc24a6762f84a47fabcaba336df89" => :el_capitan
-    sha256 "f81a305402e8f8b6cf11a17dac81f604b6f48d940909886a6733cf4f6a64c05f" => :yosemite
-    sha256 "c80495cb6d1ad8f2c3a64c22c9dcee9d0117ca25fa6426f20a6acca275cd6c56" => :mavericks
-    sha256 "c3468e676f2c9cb511e537774424299342ffd52740e252e515bddae6d9e79df3" => :mountain_lion
+    rebuild 1
+    sha256 "5978b53d1e833ef55d8d3a1ca5ff2fc7a386559ea2cb4383cc7ac094515c898f" => :sierra
+    sha256 "9170809a8d4be0c8811d91a5c25cbc8415f4766d98fb53405254c7ee8e3ba339" => :el_capitan
   end
 
   conflicts_with "camlistore", :because => "both install `hello` binaries"
