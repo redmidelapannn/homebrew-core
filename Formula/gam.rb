e90f2ecda00a55a5352b470d125c46c298d657c0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-11 01:18:16 +0000

    gam: add 4.30 bottle.

--- Formula/gam.rb
+++ Formula/gam.rb
@@ -5,6 +5,12 @@ class Gam < Formula
   sha256 "846b2902fe36019a9f4ebd4a911e780e103fb2d210971b8a4e407ddb6d8db4f6"
   head "https://github.com/jay0lee/GAM.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c5a2ada2a04737a700bd2d6a267ff5efd0c79896aaf27c071a5482efba22694b" => :sierra
+    sha256 "c5a2ada2a04737a700bd2d6a267ff5efd0c79896aaf27c071a5482efba22694b" => :el_capitan
+  end
+
   def install
     libexec.install Dir["*"]
     bin.install_symlink libexec/"src/gam.py" => "gam"
