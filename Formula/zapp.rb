22b03368b8e519e2c740d77a13a2b4bbda64019b
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-11 06:40:38 +0000

    zapp: add 0.1.0 bottle.

--- Formula/zapp.rb
+++ Formula/zapp.rb
@@ -4,6 +4,12 @@ class Zapp < Formula
   url "https://cdn.zappjs.com/apps/zapp-cli/v0.1.0/zapp-cli_mac_v0.1.0.tar.bz2"
   sha256 "66b99a8dfd3a6b728f2d41b1e548abbc63235656112225293333ec03df97ce28"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ca01391e8480b0beb773ea78c1992299bc6d27b9b147a57dbbdde8e7f8014cf4" => :sierra
+    sha256 "ca01391e8480b0beb773ea78c1992299bc6d27b9b147a57dbbdde8e7f8014cf4" => :el_capitan
+  end
+
   def install
     bin.install "zapp"
   end
