127f2bcf01ca617acb23c36f94866d32449bd15d
ilovezfs <ilovezfs@icloud.com>
2017-09-11 22:59:52 -0700

    Update pony-stable.rb

--- Formula/pony-stable.rb
+++ Formula/pony-stable.rb
@@ -1,6 +1,6 @@
 class PonyStable < Formula
-  desc "Dependency manager for the Pony language."
-  homepage "https://github.com/ponylang/pony-stable/blob/master/README.md"
+  desc "Dependency manager for the Pony language"
+  homepage "https://github.com/ponylang/pony-stable"
   url "https://github.com/ponylang/pony-stable/archive/0.0.1.tar.gz"
   sha256 "b175d5a235676eaf48b62cf28d5c44f6c13b9986d5dc97f75b477285fe9ec8fb"
 
@@ -17,6 +17,6 @@ class PonyStable < Formula
         env.out.print("Hello World!")
     EOS
     system "#{bin}/stable", "env", "ponyc", "test"
-    assert_equal "Hello World!", shell_output("./test1").strip
+    assert_equal "Hello World!", shell_output("./test1").chomp
   end
 end
