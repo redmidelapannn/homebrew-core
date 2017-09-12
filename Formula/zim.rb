37614c0b976179076171bb4e2722ed8e6dac9bce
David Crosby <davcrosb@cisco.com>
2017-09-12 17:31:45 -0600

    Add pyxdg dependency

--- Formula/zim.rb
+++ Formula/zim.rb
@@ -13,11 +13,21 @@ class Zim < Formula
   depends_on "pygtksourceview" => :optional
   depends_on "graphviz" => :optional
 
+  resource "pyxdg" do
+    url "https://files.pythonhosted.org/packages/source/p/pyxdg/pyxdg-0.25.tar.gz"
+    sha256 "81e883e0b9517d624e8b0499eb267b82a815c0b7146d5269f364988ae031279d"
+  end
+
   def install
+    ENV.prepend_create_path "PYTHONPATH", libexec / "lib/python2.7/site-packages"
+    resource("pyxdg").stage do
+      system "python", *Language::Python.setup_install_args(libexec/"vendor")
+    end
+    ENV["XDG_DATA_DIRS"] = libexec/"share"
     ENV.prepend_create_path "PYTHONPATH", libexec / "lib/python2.7/site-packages"
     system "python", "./setup.py", "install", "--prefix=#{libexec}", "--skip-xdg-cmd"
     bin.install Dir[libexec / "bin/*"]
-    bin.env_script_all_files libexec / "bin", :PYTHONPATH => ENV["PYTHONPATH"]
+    bin.env_script_all_files libexec / "bin", :PYTHONPATH => ENV["PYTHONPATH"], :XDG_DATA_DIRS => libexec/"share"
     pkgshare.install "zim"
   end
 
