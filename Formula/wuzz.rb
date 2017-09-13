a7a7b173dc584f9522127842c86a8c197803e3ea
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-13 20:21:29 +0000

    wuzz: add 0.4.0 bottle.

--- Formula/wuzz.rb
+++ Formula/wuzz.rb
@@ -5,6 +5,12 @@ class Wuzz < Formula
   version "0.4.0"
   sha256 "c8a4bc7eef2511093fd55a1e38b90aeabb4b0016b01875ec143ae19358342470"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "2caeb0c39355020ef5d3bb08762015c535c88c2d443f08c19c1db65b62058974" => :sierra
+    sha256 "2caeb0c39355020ef5d3bb08762015c535c88c2d443f08c19c1db65b62058974" => :el_capitan
+  end
+
   def install
     mv "wuzz_darwin_amd64", "wuzz"
     bin.install "wuzz"
