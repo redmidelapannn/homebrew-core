55b6510264ce34928b9d73cd6be3c577245a4863
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-18 15:25:19 +0000

    certbot: update 0.18.1_1 bottle.

--- Formula/certbot.rb
+++ Formula/certbot.rb
@@ -10,10 +10,8 @@ class Certbot < Formula
 
   bottle do
     cellar :any
-    sha256 "bbebc161dc7e34a225bc2615696bb14d9d21d3d348c3a163949179a183f88e40" => :high_sierra
-    sha256 "50b7e67278dd181915458b87ca76bc78799a1cb21460b030b49898ae14ae9461" => :sierra
-    sha256 "cfcbb202425bd6d80b6f0cb6a7ff0220d11193455a193544221b942cd293929e" => :el_capitan
-    sha256 "8eb8e8840aab66bec9eab2594db31700732fd975a793123ad59299b6bda3cf6c" => :yosemite
+    sha256 "f1ed4730759c200cf5af554c02bf9b35f3135ac118093e50460ba8db2ad7b73c" => :sierra
+    sha256 "3d78f5c371d6de4a177c338f4f70439bc4d2604acd69a006aca96575b9288466" => :el_capitan
   end
 
   depends_on "augeas"
