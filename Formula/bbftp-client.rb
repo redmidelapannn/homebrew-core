0bb10b822ef1f299bdad7a4ea2a54ac3682d12a3
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 10:32:19 +0000

    bbftp-client: update 3.2.1_1 bottle.

--- Formula/bbftp-client.rb
+++ Formula/bbftp-client.rb
@@ -8,12 +8,9 @@ class BbftpClient < Formula
   revision 1
 
   bottle do
-    sha256 "e95d1e5e6ea17e93d635f900d0ee4517587b9ea076fb2f6c8eaa96bae8e002e1" => :high_sierra
-    sha256 "027138bf779c95260fe90d543c9c5767c32c8f7c1afeb4c6ad872ecfdffc0a9b" => :sierra
-    sha256 "d813b37a04edcd071198dacd750fbac54fa3cd692fb7dda774aae88c5b8a2d9f" => :el_capitan
-    sha256 "d1b3299d2308aac2881b5049e55e912e871e98fe44a4d3586ad6afc4a565d2e6" => :yosemite
-    sha256 "8619a2f08f735d7e2387ba67ca53bf6f503f37835db08b127033d5c66019688d" => :mavericks
-    sha256 "613133ffd2d9eb3d064a7ecfd12939655362d9d6f7c951f93260c0a47ddd835c" => :mountain_lion
+    rebuild 1
+    sha256 "faf95d7204de2cb8e62248134ac78f550ce9294fd4a8073456810036b964f749" => :sierra
+    sha256 "b76dca745e11a5a4e58ee96d2fd52396f738f2b5e33650d2188a2bf5befd6d02" => :el_capitan
   end
 
   depends_on "openssl"
