ab23dc029df62a3392da2b2476dd3b06c806d0e7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-19 02:32:10 +0000

    lumo: update 1.7.0 bottle.

--- Formula/lumo.rb
+++ Formula/lumo.rb
@@ -7,9 +7,9 @@ class Lumo < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f99d3b8cbd2fe4bd10f691477b353c5fc4acd430f441c086b46d1559969668c9" => :sierra
-    sha256 "27e79277bf35a0cfc401f2ba925f6ae914157522e6315a2cf4bae1af6244c4b2" => :el_capitan
-    sha256 "72dc7ea30baecd347ee8ebaa3cb822a4337088e02efd2dd5b4e6e62b38bf0dca" => :yosemite
+    rebuild 1
+    sha256 "71db68183bb8d3bc236427f04e39085f3af8428aec24a1576ef3b7e8f43e4149" => :sierra
+    sha256 "22490158b8e7ff9136881a0c98f4fa9d86949c35ebe378717bafd945bdc49cab" => :el_capitan
   end
 
   devel do
