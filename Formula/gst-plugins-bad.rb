c08fe9643fa168e91f78a4c1f1545bfa66ef07f4
ilovezfs <ilovezfs@icloud.com>
2017-09-19 02:11:59 -0700

    gst-plugins-bad 1.12.3

--- Formula/gst-plugins-bad.rb
+++ Formula/gst-plugins-bad.rb
@@ -1,9 +1,8 @@
 class GstPluginsBad < Formula
   desc "GStreamer plugins less supported, not fully tested"
   homepage "https://gstreamer.freedesktop.org/"
-  url "https://gstreamer.freedesktop.org/src/gst-plugins-bad/gst-plugins-bad-1.12.2.tar.xz"
-  sha256 "9c2c7edde4f59d74eb414e0701c55131f562e5c605a3ce9b091754f106c09e37"
-  revision 1
+  url "https://gstreamer.freedesktop.org/src/gst-plugins-bad/gst-plugins-bad-1.12.3.tar.xz"
+  sha256 "36d059761852bed0f1a7fcd3ef64a8aeecab95d2bca53cd6aa0f08054b1cbfec"
 
   bottle do
     rebuild 1
