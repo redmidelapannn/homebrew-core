11b8adeb2748360d74dff34eff000fe54d1636d1
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-20 18:47:38 +0000

    osxutils: update 1.9.0 bottle.

--- Formula/osxutils.rb
+++ Formula/osxutils.rb
@@ -7,10 +7,8 @@ class Osxutils < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "576127145840dc6ffa7af17adeef36f61de6a9bafcb17b80286109103729cf41" => :sierra
-    sha256 "8a3039916a2cc607a98f5d4576b534e341f7823b7b16bdcfa66fca487379f366" => :el_capitan
-    sha256 "86cee8409262fdda5d6634a86a29fe2d0fcee537baa8c9696de3a8abd27c2aa8" => :yosemite
-    sha256 "91808d79c75537c563ee9a36b45e21703fcc4377d6c6ea7e7215f5ad9b0aa605" => :mavericks
+    sha256 "617de97c66d5728e5f5de577a9580d046b71bd2938a9cbe005762aee22c77be7" => :sierra
+    sha256 "75c830f9a34e194d51f04fdc58870fca889a668700b5a3aec0f020413e8783f4" => :el_capitan
   end
 
   conflicts_with "trash", :because => "both install a `trash` binary"
