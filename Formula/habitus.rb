34b6fc9ed50aeeb73efbc90c027ec98e100b55c8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-09-27 05:32:51 +0000

    habitus: add 1.0.1 bottle.

--- Formula/habitus.rb
+++ Formula/habitus.rb
@@ -5,6 +5,13 @@ class Habitus < Formula
   version "1.0.1"
   sha256 "b12f3b2d6def75287d9b9e699bab0b4f231f8fe272e21bd359ab5b1bb5628458"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d8b92aabe4492b47ebfb584ed7b87e1bedc8b8f8019219b3fec1405465e9df96" => :high_sierra
+    sha256 "d8b92aabe4492b47ebfb584ed7b87e1bedc8b8f8019219b3fec1405465e9df96" => :sierra
+    sha256 "d8b92aabe4492b47ebfb584ed7b87e1bedc8b8f8019219b3fec1405465e9df96" => :el_capitan
+  end
+
   def install
     mkdir_p bin
     cp "./habitus_darwin_amd64", "#{bin}/habitus"
