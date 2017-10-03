72cb990e90e9124aec2697567de4cd837d44337f
Samuel Bishop <lucractius@me.com>
2017-10-04 00:27:03 +0800

    Fix whitespace and capitalisation.

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -57,7 +57,7 @@ class Ffmpeg < Formula
   option "without-securetransport", "Disable use of SecureTransport"
   option "without-x264", "Disable H.264 encoder"
   option "without-xvid", "Disable Xvid MPEG-4 video encoder"
-  option "without-gpl", "Disable building GPL Licensed parts of FFMPEG"
+  option "without-gpl", "Disable building GPL licensed parts of FFmpeg"
 
   deprecated_option "with-ffplay" => "with-sdl2"
   deprecated_option "with-sdl" => "with-sdl2"
@@ -119,7 +119,7 @@ class Ffmpeg < Formula
       --host-cflags=#{ENV.cflags}
       --host-ldflags=#{ENV.ldflags}
     ]
-    
+
     args << "--enable-gpl" if build.with? "gpl"
     args << "--disable-indev=qtkit" if build.without? "qtkit"
     args << "--disable-securetransport" if build.without? "securetransport"
