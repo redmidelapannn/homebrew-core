0bb79eead01481576475b10dcf717ea6654f3a27
ilovezfs <ilovezfs@icloud.com>
2017-10-04 05:17:19 -0700

    gofabric8 0.4.162

--- Formula/gofabric8.rb
+++ Formula/gofabric8.rb
@@ -1,8 +1,8 @@
 class Gofabric8 < Formula
   desc "CLI for fabric8 running on Kubernetes or OpenShift"
   homepage "https://github.com/fabric8io/gofabric8/"
-  url "https://github.com/fabric8io/gofabric8/archive/v0.4.157.tar.gz"
-  sha256 "0fe0ff370fe4074cffa676b1b92ea1ed36610b9bfe4c24334b9173ba9aa6fecf"
+  url "https://github.com/fabric8io/gofabric8/archive/v0.4.162.tar.gz"
+  sha256 "e1eee26bebf4fa4e0d499d228cd80ec22f17ff2eb7a24e10d2d040e3878b205e"
 
   bottle do
     cellar :any_skip_relocation
