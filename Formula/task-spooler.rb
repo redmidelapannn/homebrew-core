505ddd4a4ecbb574b0f3502c28a612c109e2371c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-05 18:44:17 +0000

    task-spooler: update 1.0 bottle.

--- Formula/task-spooler.rb
+++ Formula/task-spooler.rb
@@ -6,12 +6,9 @@ class TaskSpooler < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c0731e16f98483ad9cc2dddba0895fcaa1ace8cb78102fb108b84dc39ea33298" => :high_sierra
-    sha256 "4e40910e93dbad52616762c4fd85eaa2ffe7f8c75c769f3d5c81ff2115b22869" => :sierra
-    sha256 "2d54706f5ac7cd1f0ab18af122223fb63960cb04da9effb3100fb4e1c53fc891" => :el_capitan
-    sha256 "bda2858d1071b3091c48b9774786c3b88b0ded7cb7a569054aa1774b141e5555" => :yosemite
-    sha256 "48fec6fbf078cffbc77ad1a5f7eaa01ca28561efbd5d847957bdf574bbb6c8fe" => :mavericks
-    sha256 "00fdb36feaadc5c6f552318cf7e3aed402b1c49ffde203652a0eb0ccf4ffec8b" => :mountain_lion
+    sha256 "a44646a9142dc1bf4d8b036f6853002cdb925912c938e38e271a82ee732c6477" => :high_sierra
+    sha256 "0a56e3d68bf1b7023f953d38f3a9f9be4b9dc6e0ffb5032a6eafd57ada27c048" => :sierra
+    sha256 "8db4855e85381d623d2d4e82d51683cb4b6087b8f319cf1fd56f3a703105a653" => :el_capitan
   end
 
   conflicts_with "moreutils",
