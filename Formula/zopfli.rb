2f44cb9eab2be34cfa3ed6ea1c39a4149b4b1afc
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-06 23:15:06 +0000

    zopfli: update 1.0.1 bottle.

--- Formula/zopfli.rb
+++ Formula/zopfli.rb
@@ -7,11 +7,10 @@ class Zopfli < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "52a345f7dc7df34ea5f13c0e91bfd4252964fe9f774b17a37c9ec33b94c1bcd1" => :high_sierra
-    sha256 "5f99599ab4ec444d4eab8fda1dc408dbf66b7b2755500e2f77367690d25566db" => :sierra
-    sha256 "c79a06778c3f97ea3480bba4f16ce15748d258674be2170423205163a56efed3" => :el_capitan
-    sha256 "a1eee001d7ade7d9a6920b05e8a9f1ae834c32ef8b9ccd5b1e02e7b9e09fd5e5" => :yosemite
-    sha256 "ae4f89b431c3f641385a66f61cab2e460b7217d0120b50da187b07dafb3a559c" => :mavericks
+    rebuild 1
+    sha256 "462560656ee895a9591cc2c15f934b7543e6f7968441946e9f7e5873a8b6252f" => :high_sierra
+    sha256 "aecaccf1c8b67b6c6c57b9c5d2665cc0af1c5f94dc363ebe77e658f2dbc57bff" => :sierra
+    sha256 "e41bfcdae0807dd480647ca47ccd58d05277fc1aa745668987defe97bb6d6a31" => :el_capitan
   end
 
   def install
