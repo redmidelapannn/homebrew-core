f9f126e45c29634e800f2d61364c5936b8386b4f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-06 11:24:45 +0000

    statik: update 0.18.0 bottle.

--- Formula/statik.rb
+++ Formula/statik.rb
@@ -9,10 +9,10 @@ class Statik < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "1b489bd7c98323abc21d2f391f3d3e8d9164c055bb56192099d96820f1a31085" => :high_sierra
-    sha256 "e0cd9ad785ac92d8caf198e258960cc933351e80db92d88ee3ddf795db00cfb5" => :sierra
-    sha256 "65a8324b2e2505d4925f2036a8377e37553e85fed00bac47aea9cc75441f7aee" => :el_capitan
-    sha256 "41060cbcd4e13ca6e49b52cf7890652a93d85dbd38f09ad116c47f6f8660cda4" => :yosemite
+    rebuild 1
+    sha256 "d46ac6d1a257101c38a664ccb2321dab42e51774a010f3712e56578a70f9a1a5" => :high_sierra
+    sha256 "407238bf942c2b7f300dacfc8bdaaa1f332881c0ca6c59b20865299ee6115903" => :sierra
+    sha256 "97118752a493526d093dfa291e4581a0dbc8d6f74b3113308734405ca56f26a3" => :el_capitan
   end
 
   depends_on :python if MacOS.version <= :snow_leopard
