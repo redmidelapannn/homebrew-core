b6b1ece6d412518b22f3b0b82adbf036a40dfeb8
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-07 08:18:39 +0000

    fastbit: update 2.0.3 bottle.

--- Formula/fastbit.rb
+++ Formula/fastbit.rb
@@ -8,11 +8,7 @@ class Fastbit < Formula
 
   bottle do
     cellar :any
-    sha256 "8ddbbfacf64944c202e98960f7d7a2bed3115d6184dd642842712bb2d12bba3a" => :sierra
-    sha256 "2805a5e4739ab396077fad99782174b25def6d0c8ce3d3728ed39fed2160f286" => :el_capitan
-    sha256 "33aa7e0a593b07b6d1615200b06ccd6eecd742beac345bd94b262f1b386d3591" => :yosemite
-    sha256 "f2da8bed14e66f334f870cec65c678961f925d519bddefc74ef806b493346833" => :mavericks
-    sha256 "d62226b902928b479e2835848a8eafdcd52557cb4249c59bd15cc1bd23d1e67e" => :mountain_lion
+    sha256 "552fb77c518b46836532889d7b39920200248b59c2403eb48f5391b7979664f0" => :el_capitan
   end
 
   depends_on "flex" => :build
