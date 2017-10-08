5314ea69f126f20a4a859dac5858f59af9d563cd
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-08 10:49:08 +0000

    htpdate: update 1.2.0 bottle.

--- Formula/htpdate.rb
+++ Formula/htpdate.rb
@@ -6,11 +6,8 @@ class Htpdate < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "2814f0254b6e9398fe30003348f9f0b455b25c20c42ced875d1dad5d22feadaf" => :high_sierra
-    sha256 "c5490cb23845604c332f2e5536639d7cb87ecc75fc3b6f8e0f8e799fc959a320" => :sierra
-    sha256 "fbdf082a4cbde49e5a9e5ea28f8f9c76e634d1f4fc79397e70e55306947c37b8" => :el_capitan
-    sha256 "56073fc56009dac7f807d436f09732bf4e659d2374bb6a61646dc7f94740daa0" => :yosemite
-    sha256 "973fb72128a9fe5e0c4c1aaaf9671b4ea706bc98ba5f96ecd10a2dae46049e57" => :mavericks
+    sha256 "536951cf2270af7a03936528dabae3a4bfb2e8dbd334ced928580192cb1462cd" => :high_sierra
+    sha256 "f8c6badd478b959fdfc814daf77972efeb804cd314d4b14be5729c64631bfd7f" => :sierra
   end
 
   def install
