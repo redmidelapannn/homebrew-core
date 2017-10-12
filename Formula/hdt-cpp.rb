f024fe722ee4cdcc3edfdb878a56aad9954c6d85
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-12 16:14:26 +0000

    hdt-cpp: add 1.2.0 bottle.

--- Formula/hdt-cpp.rb
+++ Formula/hdt-cpp.rb
@@ -4,6 +4,13 @@ class HdtCpp < Formula
   url "https://github.com/rdfhdt/hdt-cpp/archive/v1.2.0.tar.gz"
   sha256 "5eff3ba15839bfba7b3c15c35ea62c4352e0ad58cca1a1cd93d9f82c1eeed4dc"
 
+  bottle do
+    cellar :any
+    sha256 "433083acf34dba68ac3e0399bbdefdd6a9350b6ffe46776eea86fde5a0033638" => :high_sierra
+    sha256 "07bd34b562fdf28bb9a0c57d6ac6fe58cf4064716457ced52c36e99d12b4560a" => :sierra
+    sha256 "26e140ec97c89eccb20fc2f2cb2b2ff0f7eb2baa8cd49fe063ce5f2883a856e3" => :el_capitan
+  end
+
   depends_on "kyoto-cabinet"
   depends_on "serd"
   depends_on "qt"
