f9137ffe54ad82acd881997aeb154ce5604b828a
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-13 01:49:00 +0000

    rat: add 0.0.2 bottle.

--- Formula/rat.rb
+++ Formula/rat.rb
@@ -5,6 +5,13 @@ class Rat < Formula
   version "0.0.2"
   sha256 "9da8d67a1e16056e3f05cdf536766e5571ede54a6c241c941d6c74eee5d64d31"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "96f5f399718dd3bcf062ab825f9cdccf33d3d2f06f3ec88ba144d04b6a780452" => :high_sierra
+    sha256 "496da445e4e5ca01b7049f86554920dd96b0c3b57a2846fb37c8fb9384b79b1b" => :sierra
+    sha256 "c282724b0d1ad2bde75f61d6cdf0288d205446fc37440fadac502bbb34e39656" => :el_capitan
+  end
+
   depends_on "go" => :build
 
   def install
