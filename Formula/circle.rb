91f9a7bffb18f88da6a53f94dbaa52f52416b57f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-13 23:04:09 +0000

    circle: add 0.25 bottle.

--- Formula/circle.rb
+++ Formula/circle.rb
@@ -6,6 +6,13 @@ class Circle < Formula
   version "0.25"
   sha256 "c481df9024a8f143e4e05913eacd39b23cdc2b3ff6c344b8b074c4af61534b25"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ab0ed1a930385b68564b7be34f8f23742d3d9198b7432c848417ec7be8839a09" => :high_sierra
+    sha256 "ab0ed1a930385b68564b7be34f8f23742d3d9198b7432c848417ec7be8839a09" => :sierra
+    sha256 "ab0ed1a930385b68564b7be34f8f23742d3d9198b7432c848417ec7be8839a09" => :el_capitan
+  end
+
   def install
     bin.install "circle-darwin-amd64" => "circle"
   end
