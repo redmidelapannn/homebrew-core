1013d3d58e0c77b6ee435de7d837bd15d10d2360
Martin Schimandl <martin.schimandl@gmail.com>
2017-10-14 12:51:44 +0200

    Remove build dependency with option from ccache

--- Formula/ccache.rb
+++ Formula/ccache.rb
@@ -18,7 +18,6 @@ class Ccache < Formula
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
-    depends_on "asciidoc" => ["with-docbook-xsl", :build]
   end
 
   def install
