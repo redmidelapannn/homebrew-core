e203e942d0aaabfbaaf6ad2f533376e349f6ed56
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-15 23:02:15 +0000

    mypubip: add 1.0.1 bottle.

--- Formula/mypubip.rb
+++ Formula/mypubip.rb
@@ -6,6 +6,13 @@ class Mypubip < Formula
   sha256 "0160a676555988b7823debb7116efb579c8999f00c6d5e8387b23b75aa101551"
 
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "28265204ab250332ca9a2df122dbcd6300cfe5ecc1c8b33dcfe1d1713e067213" => :high_sierra
+    sha256 "28265204ab250332ca9a2df122dbcd6300cfe5ecc1c8b33dcfe1d1713e067213" => :sierra
+    sha256 "28265204ab250332ca9a2df122dbcd6300cfe5ecc1c8b33dcfe1d1713e067213" => :el_capitan
+  end
+
   def install
       bin.install "myPubip"
   end
