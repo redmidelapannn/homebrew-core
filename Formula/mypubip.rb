d1515c80bb48bfed76acd494e64d93b9348e9fd4
garymalaysia <gtsai70@gmail.com>
2017-10-15 18:56:16 -0400

    Added mypubip formula

---
+++ Formula/mypubip.rb
@@ -0,0 +1,12 @@
+
+class Mypubip < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/garymalaysia/myPubip/archive/v1.0.1.tar.gz"
+  sha256 "0160a676555988b7823debb7116efb579c8999f00c6d5e8387b23b75aa101551"
+
+
+  def install
+      bin.install "myPubip"
+  end
+end
