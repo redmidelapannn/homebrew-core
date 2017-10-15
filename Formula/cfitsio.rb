03bec86ba611c44e1cc04b48130d4642ac961cad
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-15 07:34:07 +0000

    cfitsio: update 3.420 bottle.

--- Formula/cfitsio.rb
+++ Formula/cfitsio.rb
@@ -8,9 +8,10 @@ class Cfitsio < Formula
 
   bottle do
     cellar :any
-    sha256 "8b21e0a610d1caa8d8900546d2b2d9f83f74a9f1bd0d3e729f1ab1ad28b81813" => :high_sierra
-    sha256 "b2f55b8504d79e27d23b5fa93f77ff9bc2488eef4f5ed94c7a16757505df4462" => :sierra
-    sha256 "7681f60baee8d9a73639be3f54d7f4a1c71c9fc9f4ac7fd0e3ba20cb2a2c3c7b" => :el_capitan
+    rebuild 1
+    sha256 "479098537f2d866133e7dd85bd4b9eb3d2ef4f8472f404d05cfe3abe2ef6d702" => :high_sierra
+    sha256 "288a78110f365d9905ed2e91b75e82dd0341e149c40c24b63e5dbbac792edb8c" => :sierra
+    sha256 "54303befcf5650094d721892accaaff31ad9ea1164ba81829f0903f8bab68359" => :el_capitan
   end
 
   option "with-reentrant", "Build with support for concurrency"
