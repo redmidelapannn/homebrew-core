ae3b5c33bfcb13691b6bd6742b3b64f2e2b04b4f
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-18 18:49:02 +0000

    smenu: add 0.9.9 bottle.

--- Formula/smenu.rb
+++ Formula/smenu.rb
@@ -5,6 +5,13 @@ class Smenu < Formula
   sha256 "04c9ea56130a8d2398c5fba712f6e099d7a7adb0d9072d1610624fe902b5c54d"
   head "https://github.com/p-gen/smenu.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "310c5e9004e0ca027257eee757d08c5737c0c43ca389d1c2e0f20422d54b9878" => :high_sierra
+    sha256 "88f5aa3b749570d8d87612fb1214ae87528aac33ce28f28a12a9b5c14a3251a5" => :sierra
+    sha256 "a34b219e4787402fd42b82ce6ab67877e511774a69b35fe5180d1e20b3cdbfcf" => :el_capitan
+  end
+
   def install
     system "./build.sh", "--disable-dependency-tracking",
                          "--disable-silent-rules",
