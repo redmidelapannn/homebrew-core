0967d00ec0535d3bc0e5a821a9d61cf292b73997
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-19 21:48:32 +0000

    goocanvasmm: add 1.90.11 bottle.

--- Formula/goocanvasmm.rb
+++ Formula/goocanvasmm.rb
@@ -4,6 +4,13 @@ class Goocanvasmm < Formula
   url "https://download.gnome.org/sources/goocanvasmm/1.90/goocanvasmm-1.90.11.tar.xz"
   sha256 "80ff11873ec0e73d9e38b0eb2ffb1586621f0b804093b990e49fdb546476ed6e"
 
+  bottle do
+    cellar :any
+    sha256 "0b9233014a96b0ecc12a1d94d120e1f404e57e5529e17c4bc5c816aa7213b595" => :high_sierra
+    sha256 "4a818e0d2b4257b4f711d3176b968c9339bd92f918b4c43d1386defde2c5730e" => :sierra
+    sha256 "5322a7a475df566ba02b8e7a3b8bff121013bbae656bdeb005decaf78b69bbc9" => :el_capitan
+  end
+
   depends_on "pkg-config" => :build
   depends_on "glibmm"
   depends_on "gtkmm3"
