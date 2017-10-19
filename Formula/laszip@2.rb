49b937f2df40824d4f9f750d7305ca837f21c588
ilovezfs <ilovezfs@icloud.com>
2017-10-19 04:06:54 -0700

    Update laszip@2.rb

--- Formula/laszip@2.rb
+++ Formula/laszip@2.rb
@@ -3,7 +3,6 @@ class LaszipAT2 < Formula
   homepage "https://www.laszip.org/"
   url "https://github.com/LASzip/LASzip/archive/v2.2.0.tar.gz"
   sha256 "b8e8cc295f764b9d402bc587f3aac67c83ed8b39f1cb686b07c168579c61fbb2"
-  head "https://github.com/LASzip/LASzip.git"
 
   depends_on "cmake" => :build
 
