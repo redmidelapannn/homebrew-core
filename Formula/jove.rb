15870d6137fa5386a7bcce2fd70481aa48dc9157
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-19 04:34:38 +0000

    jove: update 4.16.0.73 bottle.

--- Formula/jove.rb
+++ Formula/jove.rb
@@ -6,11 +6,10 @@ class Jove < Formula
   sha256 "9c9e202607f5972c382098d10b63c815ac01e578f432626c982e6aa65000c630"
 
   bottle do
-    sha256 "852bb69e61f542d5741b4a442c72c8f72304468d0c1101c2843b1c272111a6d1" => :high_sierra
-    sha256 "a4559511caca50cf08e01049dda2a35a9423b5c976bf0d930ba819afebc1387b" => :sierra
-    sha256 "c083761f33516e9d18718b6f78f6468b9aa72c0c80bb625987c60c05cc4f1895" => :el_capitan
-    sha256 "4e741042364faa5ef07f7957d9e811c204561ecad03ebbcd98f82761211ec78c" => :yosemite
-    sha256 "260e22d1b9cabcc9b8cacd415d8e5cf840c6970e302a2840cb4076f3583a591d" => :mavericks
+    rebuild 1
+    sha256 "2fdde02dd0b5a7c16b965306922e654c1b159016654849dc4e8ed048fdba2e38" => :high_sierra
+    sha256 "683360060d9a9f19b232bb77bbc4daad9b38a0f332b80751881df2ef172937f3" => :sierra
+    sha256 "115fd032e7a2557ffd55b7b1457b96c5c8438285dc5df96f82c5f435130c405e" => :el_capitan
   end
 
   # Per MacPorts, avoid clash with libc getline
