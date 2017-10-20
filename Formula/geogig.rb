cf0a2c2aed8f81e87181e82639b1ddb0a25090f7
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-20 20:16:42 +0000

    geogig: add 1.2.0 bottle.

--- Formula/geogig.rb
+++ Formula/geogig.rb
@@ -4,6 +4,12 @@ class Geogig < Formula
   url "http://download.locationtech.org/geogig/geogig-1.2.0.zip"
   sha256 "c438d97d8978a9d4985bd4f2ba3fff481b7956bfc4bd986ee3c79c6c07cd4f34"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e04d8dc154b892c9c4ad5711840475a9fc038f2a472c5c2b502d953fc594c533" => :high_sierra
+    sha256 "c2f671a71f3d3eb631769beca1555e26672afb03dcdeb0a81471b99aadd24094" => :el_capitan
+  end
+
   depends_on :java => ["1.8+", :recommended]
 
   def install
