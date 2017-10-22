d2029bac725f0915ba5d2df36cccb2ff9d79434e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-22 20:27:42 +0000

    pyyaml: add 3.12 bottle.

--- Formula/pyyaml.rb
+++ Formula/pyyaml.rb
@@ -4,6 +4,13 @@ class Pyyaml < Formula
   url "https://pyyaml.org/download/pyyaml/PyYAML-3.12.tar.gz"
   sha256 "592766c6303207a20efc445587778322d7f73b161bd994f227adaa341ba212ab"
 
+  bottle do
+    cellar :any
+    sha256 "6604821c49ff17b3be30b3532fd16a9dd51e6d6dfc3cf73bf964ecd2bd2b9239" => :high_sierra
+    sha256 "06f646741db71d76c0282a6574c13bb842f4771e3424d2a933171dccefd01e25" => :sierra
+    sha256 "e98b1c4cc42b0ec3316866d6bc673d1053f842c62dce7576eb6df281730b6586" => :el_capitan
+  end
+
   depends_on "libyaml"
   depends_on :python if MacOS.version <= :snow_leopard
 
