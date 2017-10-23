8a056cdd7c84c4a41e8db5260119d196bcbb5b42
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-23 22:23:36 +0000

    bane: add 1.0.0 bottle.

--- Formula/bane.rb
+++ Formula/bane.rb
@@ -5,6 +5,13 @@ class Bane < Formula
   version "1.0.0"
   sha256 "3622fad27f0cbb3cc45f100463ccb296ed3cd6448f913313fd5e113cbe9ff5d2"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c5573f4892c3db97431bdd8b62ec01702bcb9b321e1047295562a0c4da42e3b0" => :high_sierra
+    sha256 "38b7a39ab706a4939e46694b3809617683a900f47708bc684706719990745adc" => :sierra
+    sha256 "1f6480caac3e4b4555a2f7e2e1b9d3ed943dd44457e1dda15792b561977ab419" => :el_capitan
+  end
+
   def install
     system "make"
     bin.install "build/bane"
