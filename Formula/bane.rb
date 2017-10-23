45687e13ecc49cb0c7f84d4d785285a5f155632f
Charlton Trezevant <ct@ctis.me>
2017-10-23 17:17:40 -0400

    bane 1.0.0 (new formula)

---
+++ Formula/bane.rb
@@ -0,0 +1,16 @@
+class Bane < Formula
+  desc "The magic of Old Memes, now in your command-line."
+  homepage "https://github.com/ctrezevant/bane"
+  url "https://github.com/ctrezevant/bane/archive/v1.0.tar.gz"
+  sha256 "3622fad27f0cbb3cc45f100463ccb296ed3cd6448f913313fd5e113cbe9ff5d2"
+  version "1.0.0"
+
+  def install
+    system "make"
+    bin.install "build/bane"
+  end
+
+  test do
+    system "#{bin}/bane"
+  end
+end
