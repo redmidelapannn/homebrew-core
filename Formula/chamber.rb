ec3be2adb3d7106a4d493e087df3f347361e4e98
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-25 21:12:53 +0000

    chamber: add 1.9 bottle.

--- Formula/chamber.rb
+++ Formula/chamber.rb
@@ -4,6 +4,13 @@ class Chamber < Formula
   url "https://github.com/segmentio/chamber/releases/download/v1.9.0/chamber-v1.9.0-darwin-amd64"
   sha256 "723d3ec07ca968f5423b8c7f67a2a5d40e79991f002653c2b3d6cf2784e81154"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e26b3ed1233e071c28ee8fc9f9262891250d7bdba76a402dac4ea6d13ae3fe51" => :high_sierra
+    sha256 "e26b3ed1233e071c28ee8fc9f9262891250d7bdba76a402dac4ea6d13ae3fe51" => :sierra
+    sha256 "e26b3ed1233e071c28ee8fc9f9262891250d7bdba76a402dac4ea6d13ae3fe51" => :el_capitan
+  end
+
   def install
     bin.install Dir["chamber-*"].first => "chamber"
   end
