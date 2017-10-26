a1cb032668935128380bbd9440154c8f52e3c226
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-26 22:20:19 +0000

    vapor: add 3.0.3 bottle.

--- Formula/vapor.rb
+++ Formula/vapor.rb
@@ -5,6 +5,13 @@ class Vapor < Formula
   version "3.0.3"
   sha256 "0461af9672896918e66bf3f698f0e78df0b8eb51c2a4a115919a0a30e055452c"
 
+  bottle do
+    cellar :any
+    sha256 "905718809257c7cf80c3df39c12968b7f248cb76a85bca44d656a53e1d95d84e" => :high_sierra
+    sha256 "905718809257c7cf80c3df39c12968b7f248cb76a85bca44d656a53e1d95d84e" => :sierra
+    sha256 "905718809257c7cf80c3df39c12968b7f248cb76a85bca44d656a53e1d95d84e" => :el_capitan
+  end
+
   depends_on "vapor/tap/ctls" => :run
 
   def install
