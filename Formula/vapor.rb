544d23ae6bfceccd74d0873d08edbf12cabcf171
Tanner Nelson <me@tanner.xyz>
2017-10-26 18:14:29 -0400

    vapor version non-dynamic

--- Formula/vapor.rb
+++ Formula/vapor.rb
@@ -1,7 +1,7 @@
 class Vapor < Formula
   desc "Simplifies common command-line tasks when using Vapor."
   homepage "https://vapor.codes"
-  url "https://github.com/vapor/toolbox/releases/download/#{version}/macOS-sierra.tar.gz"
+  url "https://github.com/vapor/toolbox/releases/download/3.0.3/macOS-sierra.tar.gz"
   version "3.0.3"
   sha256 "0461af9672896918e66bf3f698f0e78df0b8eb51c2a4a115919a0a30e055452c"
 
