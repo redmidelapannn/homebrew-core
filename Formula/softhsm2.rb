e944497431ff4035779ede6d6c2cc5ad43aba2f5
Gijs Molenaar <gijs@pythonic.nl>
2017-10-27 15:53:22 +0200

    softhsm2 2.3.0 (new formula)

---
+++ Formula/softhsm2.rb
@@ -0,0 +1,38 @@
+class Softhsm2 < Formula
+  desc "Cryptographic store accessible through a PKCS #11"
+  homepage "https://www.opendnssec.org/softhsm/"
+  url "https://dist.opendnssec.org/source/softhsm-2.3.0.tar.gz"
+  sha256 "5ed604c89a3a6ef9d7d1ee92c28a2c4b3cd1f86f302c808e2d12c8f39aa2c127"
+
+  depends_on "cppunit" => :build
+  depends_on "openssl"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--with-objectstore-backend-db",
+                          "--with-migrate",
+                          "--with-openssl=" + Formula["openssl"].opt_prefix,
+                          "--prefix=#{prefix}",
+                          "--sysconfdir=#{etc}",
+                          "--localstatedir=#{var}"
+
+    system "make", "install"
+
+    include.install "src/lib/pkcs11/cryptoki.h",
+                    "src/lib/pkcs11/pkcs11.h",
+                    "src/lib/pkcs11/pkcs11f.h",
+                    "src/lib/pkcs11/pkcs11t.h"
+  end
+
+  def post_install
+    (var/"lib/softhsm/softhsm").mkpath
+  end
+
+  test do
+    system "#{bin}/softhsm2-util", "--init-token", "--free",
+                                   "--label", "test",
+                                   "--so-pin", "1234",
+                                   "--pin", "1234"
+  end
+end
