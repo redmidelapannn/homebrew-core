a19f70e53b5cd035bb39afe91b787a00cece2f96
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-27 22:48:13 +0000

    libefi: add 1.0 bottle.

--- Formula/libefi.rb
+++ Formula/libefi.rb
@@ -4,6 +4,13 @@ class Libefi < Formula
   url "https://github.com/rickmark/gnu-efi/archive/v1.0.tar.gz"
   sha256 "7eadece64bd593885d2d76bd5e69910ad1b246e98387df915fa15d029bf0ab45"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7e292e5c6cac6993dcf05f7e9241b622b673f529dff63db7e852694406c64c3b" => :high_sierra
+    sha256 "7e228498a328ec59c786f7b7498e0b6e4074abbe63d5401f2153205597df4923" => :sierra
+    sha256 "7b972b4b0ca753bf39d4d5eb5dc9ec5ccdf4d3409eaa37408ff99c0a8e807a3c" => :el_capitan
+  end
+
   depends_on "gcc" => :build
 
   def install
