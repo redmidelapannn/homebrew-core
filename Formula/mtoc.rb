10f5b60141b198cdbcf943f23db63f713fbe6715
Rick Mark <rickmark@outlook.com>
2017-10-27 02:25:51 -0700

    Comply with formula style guide

--- Formula/mtoc.rb
+++ Formula/mtoc.rb
@@ -1,46 +1,35 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook.html
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
-require 'fileutils'
-
 class Mtoc < Formula
-  desc "Mach-O to COFF converter to make EFI binaries out of static Apple executables"
+  desc "Mach-O to COFF/EFI binarie converter from static binaries"
   homepage "https://opensource.apple.com/source/cctools/cctools-895/"
   url "https://opensource.apple.com/tarballs/cctools/cctools-895.tar.gz"
-  version "895"
   sha256 "ce66034fa35117f9ae76bbb7dd72d8068c405778fa42e877e8a13237a10c5cb7"
-  depends_on 'llvm' => :build
-
-  # depends_on "cmake" => :build
+  depends_on "llvm" => :build
 
   def install
-    disassembler_path = 'include/llvm-c/Disassembler.h'
+    disassembler_path = "include/llvm-c/Disassembler.h"
 
     disassembler_header = File.read(File.join(buildpath, disassembler_path))
 
-    llvm_include = File.join(buildpath, 'include')
-    llvm_c_include = File.join(buildpath, 'include')
+    llvm_include = File.join(buildpath, "include")
+    llvm_c_include = File.join(buildpath, "include")
 
-    FileUtils.mkdir_p llvm_include
-    FileUtils.mkdir_p llvm_c_include
+    mkdir_p llvm_include
+    mkdir_p llvm_c_include
 
-    FileUtils.cp_r "#{HOMEBREW_PREFIX}/opt/llvm/include/llvm", llvm_include
-    FileUtils.cp_r "#{HOMEBREW_PREFIX}/opt/llvm/include/llvm-c", llvm_c_include
+    cp_r "#{HOMEBREW_PREFIX}/opt/llvm/include/llvm", llvm_include
+    cp_r "#{HOMEBREW_PREFIX}/opt/llvm/include/llvm-c", llvm_c_include
 
-    File.open(File.join(buildpath, disassembler_path), 'w') { |f| f.write(disassembler_header) }
+    File.open(File.join(buildpath, disassembler_path), "w") { |f| f.write(disassembler_header) }
 
-    # ENV.deparallelize  # if your formula fails when building in parallel
+    `make > /dev/null 2>&1`
 
-    `make`
+    Dir.chdir File.join(buildpath, "efitools")
 
-    Dir.chdir File.join(buildpath, 'efitools')
+    system "make"
 
-    system 'make'
+    mkdir_p bin
 
-    FileUtils.mkdir_p bin
-    
-    FileUtils.cp File.join(buildpath, 'efitools/mtoc.NEW'), File.join(bin, 'mtoc')
+    cp File.join(buildpath, "efitools/mtoc.NEW"), File.join(bin, "mtoc")
   end
 
   test do
@@ -53,6 +42,6 @@ class Mtoc < Formula
     #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "which mtoc"
+    system "which", "mtoc"
   end
 end
