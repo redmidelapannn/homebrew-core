359c42df9d9ee24fae42fb98e31c5ae220527c81
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-29 20:12:01 +0000

    py3c: add 0.8 bottle.

--- Formula/py3c.rb
+++ Formula/py3c.rb
@@ -4,6 +4,13 @@ class Py3c < Formula
   url "https://github.com/encukou/py3c/archive/v0.8.tar.gz"
   sha256 "c97e538ed25b6fe76f0ff97e9de9dbf3b08cc46b30a5a29d80a991f50c9dfc0d"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d8db59fc58bb8190b0e9ffd6dddf79fb2a97d27f0152c888dbd729a4c3a7439f" => :high_sierra
+    sha256 "d8db59fc58bb8190b0e9ffd6dddf79fb2a97d27f0152c888dbd729a4c3a7439f" => :sierra
+    sha256 "d8db59fc58bb8190b0e9ffd6dddf79fb2a97d27f0152c888dbd729a4c3a7439f" => :el_capitan
+  end
+
   depends_on "pkg-config" => :run
 
   # Prevent strange behaviour of $(realpath ...) on OSX returning an empty path
