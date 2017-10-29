d4fe5b786f2aff9e49189f26175d65953c3c7316
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-29 16:03:54 +0000

    ydcv: add 0.4 bottle.

--- Formula/ydcv.rb
+++ Formula/ydcv.rb
@@ -4,6 +4,13 @@ class Ydcv < Formula
   url "https://github.com/felixonmars/ydcv/archive/0.4.tar.gz"
   sha256 "2d9f6309bbf2d35c0c34c5ee945cf40769cc8201e6f374fa2a4f2d4b827fbdbb"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "04e19fd5a9cacf826f54b5eae958de8bd2e3d4bcdabb299288b451bbb6bb1ebe" => :high_sierra
+    sha256 "04e19fd5a9cacf826f54b5eae958de8bd2e3d4bcdabb299288b451bbb6bb1ebe" => :sierra
+    sha256 "04e19fd5a9cacf826f54b5eae958de8bd2e3d4bcdabb299288b451bbb6bb1ebe" => :el_capitan
+  end
+
   depends_on :python if MacOS.version <= :snow_leopard
 
   def install
