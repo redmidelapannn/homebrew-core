ef6459c1c62a985314ed0ff78460aa6ec1d97673
BrewTestBot <brew-test-bot@googlegroups.com>
2017-10-30 10:10:17 +0000

    hqx: update 1.1_2 bottle.

--- Formula/hqx.rb
+++ Formula/hqx.rb
@@ -7,10 +7,10 @@ class Hqx < Formula
 
   bottle do
     cellar :any
-    sha256 "5a548a5e9b16d78b7f913c4b6fece78202b13e416962a9a5a965c3e2d27461a8" => :high_sierra
-    sha256 "7e58bc40ff9214f1b074595ac85c842ebafe676c6f3db42e3e0712c77c3377f4" => :sierra
-    sha256 "6bf8b3b1b203ae43cc833480c8b395776d1369a38bb78fe2b47034ff8a8a0645" => :el_capitan
-    sha256 "82f3574ae2e08ed7312d22b751b94be4783eccb2166fc1e45cc25ae90a7e5046" => :yosemite
+    rebuild 1
+    sha256 "8a1aec179044c581988cf258f8e1a3e9bfaaee78ec211448b361e70d16f3f7ac" => :high_sierra
+    sha256 "9703c67cda33f17def549d06c527e35f1fdff12367344b630584026af6ae5595" => :sierra
+    sha256 "77ea6ee4cbf73fc237ea0c79303c2e3f1fa43afc78b1b085daf7662da7d4c036" => :el_capitan
   end
 
   depends_on "devil"
