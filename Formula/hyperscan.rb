95c8c815da62ca429a53a4086cbc086b0500cd97
Patrick Toomey <ptoomey3@biasedcoin.com>
2017-10-31 12:38:14 -0600

    Follow coding guidlines

--- Formula/hyperscan.rb
+++ Formula/hyperscan.rb
@@ -22,9 +22,7 @@ class Hyperscan < Formula
   def install
     mkdir "build" do
       args = std_cmake_args
-      args += %w[
-        -DBUILD_STATIC_AND_SHARED=on
-      ]
+      args << "-DBUILD_STATIC_AND_SHARED=on"
 
       if build.with? "debug"
         args -= %w[
