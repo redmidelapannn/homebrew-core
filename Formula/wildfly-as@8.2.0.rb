82129ae3519cec4d14b1eb8734ba0c6c660fd229
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-01 15:51:49 +0000

    wildfly-as@8.2.0: add 8.2.0.Final bottle.

--- Formula/wildfly-as@8.2.0.rb
+++ Formula/wildfly-as@8.2.0.rb
@@ -5,6 +5,13 @@ class WildflyAsAT820 < Formula
   url "https://download.jboss.org/wildfly/8.2.0.Final/wildfly-8.2.0.Final.tar.gz"
   sha256 "bf16e2be38fd1476b0e8a0b038f7d41d7ab525fc96c2a1077338814b7442728b"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "77a3bd8c3e84c6c02e686d559b5b304de6ca8bdf650bd2bf51971a40819a19f2" => :high_sierra
+    sha256 "77a3bd8c3e84c6c02e686d559b5b304de6ca8bdf650bd2bf51971a40819a19f2" => :sierra
+    sha256 "77a3bd8c3e84c6c02e686d559b5b304de6ca8bdf650bd2bf51971a40819a19f2" => :el_capitan
+  end
+
   depends_on :java => "1.7+"
 
   def install
