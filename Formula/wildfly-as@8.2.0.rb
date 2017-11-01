e8c021d5b7fb6480443547fc2c080b19a0b83622
J.Kevin Tu <ktu@jkevintu.com>
2017-11-01 11:46:39 -0400

    Update https

--- Formula/wildfly-as@8.2.0.rb
+++ Formula/wildfly-as@8.2.0.rb
@@ -2,7 +2,7 @@
 class WildflyAsAT820 < Formula
   desc "Managed application runtime for building applications"
   homepage "http://wildfly.org/"
-  url "http://download.jboss.org/wildfly/8.2.0.Final/wildfly-8.2.0.Final.tar.gz"
+  url "https://download.jboss.org/wildfly/8.2.0.Final/wildfly-8.2.0.Final.tar.gz"
   sha256 "bf16e2be38fd1476b0e8a0b038f7d41d7ab525fc96c2a1077338814b7442728b"
 
   depends_on :java => "1.7+"
