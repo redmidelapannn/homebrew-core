e3de5ca687a2b12b55a2f176dca74095b2b9b693
Stephen <stephengroat@users.noreply.github.com>
2017-11-02 06:25:32 -0700

    Remove package name from description

--- Formula/keystone.rb
+++ Formula/keystone.rb
@@ -1,5 +1,5 @@
 class Keystone < Formula
-  desc "Keystone assembler framework: Core + bindings"
+  desc "Assembler framework: Core + bindings"
   homepage "http://www.keystone-engine.org"
   url "https://github.com/keystone-engine/keystone/archive/0.9.1.tar.gz"
   sha256 "e9d706cd0c19c49a6524b77db8158449b9c434b415fbf94a073968b68cf8a9f0"
