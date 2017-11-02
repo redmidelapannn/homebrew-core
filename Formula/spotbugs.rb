55d8a5e2d269290d16290fd348bd134fe0f74bac
Stephen <stephengroat@users.noreply.github.com>
2017-11-02 10:47:53 -0700

    adjust for rubocop

--- Formula/spotbugs.rb
+++ Formula/spotbugs.rb
@@ -1,5 +1,5 @@
 class Spotbugs < Formula
-  desc "A tool for Java static analysis (FindBugs' successor)"
+  desc "Tool for Java static analysis (FindBugs' successor)"
   homepage "https://spotbugs.github.io/"
   url "https://github.com/spotbugs/spotbugs/archive/3.1.0.tar.gz"
   sha256 "ac6169a551212756a05dd9c066ef9212653d8a99b754ef4dade6a7008f8ad781"
@@ -17,15 +17,15 @@ class Spotbugs < Formula
 
   test do
     (testpath/"HelloWorld.java").write <<~EOS
-    public class HelloWorld {
-      private double[] myList;
-      public static void main(String[] args) {
-        System.out.println("Hello World");
+      public class HelloWorld {
+        private double[] myList;
+        public static void main(String[] args) {
+          System.out.println("Hello World");
+        }
+        public double[] getList() {
+          return myList;
+        }
       }
-      public double[] getList() {
-        return myList;
-      }
-    }
     EOS
     system "javac", "HelloWorld.java"
     system "jar", "cvfe", "HelloWorld.jar", "HelloWorld", "*.class"
