46aa9062e14d992230be76ce24aae4c9eafbe1fb
s172262 <s172262@users.noreply.github.com>
2017-11-06 12:09:14 +0900

    ffmpeg: revision +1
    for bumping x264 https://github.com/Homebrew/homebrew-core/pull/20315

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -3,6 +3,7 @@ class Ffmpeg < Formula
   homepage "https://ffmpeg.org/"
   url "https://ffmpeg.org/releases/ffmpeg-3.4.tar.bz2"
   sha256 "5d8911fe6017d00c98a359d7c8e7818e48f2c0cc2c9086a986ea8cb4d478c85e"
+  revision 1
   head "https://github.com/FFmpeg/FFmpeg.git"
 
   bottle do
