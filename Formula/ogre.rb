4a6a655e9e6bef082cae256a5e0c34a46b23630b
s172262 <s172262@users.noreply.github.com>
2017-11-07 04:21:21 +0900

    ogre: add python and java component support

--- Formula/ogre.rb
+++ Formula/ogre.rb
@@ -4,6 +4,9 @@ class Ogre < Formula
   url "https://github.com/OGRECave/ogre/archive/v1.10.9.tar.gz"
   sha256 "85ba2cc6a35c67ff93a9a498af9f8f3113fd3a16e7cc43c18b9769d8bf1e9101"
 
+  option "with-java-component", "Build with java component"
+  option "with-python-component", "Build with python component"
+
   needs :cxx11
 
   depends_on "cmake" => :build
@@ -17,6 +20,16 @@ class Ogre < Formula
   depends_on "openexr"
   depends_on "sdl"
   depends_on "sdl2"
+
+  if (build.with?("java-component") || build.with?("python-component"))
+    depends_on "swig"
+    if build.with?("java-component")
+      depends_on :java
+    end
+    if build.with?("python-component")
+      depends_on :python
+    end
+  end
 
   def install
     if build.with? "boost"
@@ -45,7 +58,7 @@ class Ogre < Formula
     args = std_cmake_args
     args += %W[-DCMAKE_SKIP_BUILD_RPATH=FALSE
                -DOGRE_FULL_RPATH=TRUE
-               -DOGRE_LIB_DIRECTORY=#{lib}
+               -DOGRE_LIB_DIRECTORY=lib
                -DOGRE_BUILD_DEPENDENCIES=FALSE
                -DOGRE_INSTALL_DEPENDENCIES=FALSE
                -DOGRE_COPY_DEPENDENCIES=FALSE
@@ -53,15 +66,35 @@ class Ogre < Formula
                -DOGRE_BUILD_SAMPLES=OFF
                -DOGRE_BUILD_PLUGIN_CG=OFF
                -DOGRE_BUILD_LIBS_AS_FRAMEWORKS=OFF
-               -DOGRE_BUILD_COMPONENT_JAVA=OFF
-               -DOGRE_BUILD_COMPONENT_PYTHON=OFF]
+               ]
     # "std" meands using c++11.
     args << "-DOGRE_CONFIG_THREAD_PROVIDER=" + (build.with?("boost") ? "boost" : "std")
+
+	# See https://github.com/OGRECave/ogre/issues/563
+    if build.with?("python-component")
+      inreplace "Components/Python/CMakeLists.txt", "set(CMAKE_SWIG_FLAGS -w401,314 -builtin)", "set(CMAKE_SWIG_FLAGS -w401,314 -builtin -D_LIBCPP_VERSION -DOGRE_FULL_RPATH=TRUE)"
+    else
+      args << "-DOGRE_BUILD_COMPONENT_PYTHON=OFF"
+    end
+    if build.with?("java-component")
+      inreplace "Components/Java/CMakeLists.txt", "set(CMAKE_SWIG_FLAGS -w401,314 -package org.Ogre)", "set(CMAKE_SWIG_FLAGS -w401,314 -package org.Ogre -D_LIBCPP_VERSION -DOGRE_FULL_RPATH=TRUE)"
+    else
+      args << "-DOGRE_BUILD_COMPONENT_JAVA=OFF"
+    end
 
     system "cmake", ".", *args
     system "make", "install"
   end
 
+  def caveats
+    if build.with? "python-component" then <<~EOS
+      You might add "#{HOMEBREW_PREFIX}/lib/python2.7/dist-packages" to PYTHONPATH as following.
+
+        export PYTHONPATH="#{HOMEBREW_PREFIX}/lib/python2.7/dist-packages:$PYTHONPATH"
+
+      EOS
+    end
+  end
   test do
     system "#{bin}/OgreXMLConverter", "-v"
   end
