b2dfe75d5f96723b5ed95ccde1e5ffcb2669781d
Miaoqi Zhu <mzhu@oscars.org>
2017-11-07 12:12:41 -0800

    add a script to brew rawtoaces

---
+++ Formula/rawtoaces.rb
@@ -0,0 +1,26 @@
+class Rawtoaces < Formula
+  desc "RAW to ACES Utility"
+  homepage "https://github.com/ampas/rawtoaces"
+  url "https://github.com/ampas/rawtoaces/archive/v1.0.tar.gz"
+  sha256 "9d15e7e30c4fe97baedfdafb5fddf95534eee26392002b23e81649bbe6e501e9"
+
+  depends_on "cmake" => :build
+  depends_on "libraw"
+  depends_on "ilmbase"
+  depends_on "aces_container"
+  depends_on "boost"
+  depends_on "ceres-solver"
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make"
+      system "make", "test"
+      system "make", "install"
+    end
+  end
+
+  test do
+    assert_match /convert RAW digital camera files to ACES/, shell_output("#{bin}/rawtoaces --help", 255).strip
+  end
+end
