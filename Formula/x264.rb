b71f88b68b54b380f89880f4f95bf6be38bb4ada
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-07 19:09:55 +0000

    x264: update r2795_1 bottle.

--- Formula/x264.rb
+++ Formula/x264.rb
@@ -10,10 +10,9 @@ class X264 < Formula
 
   bottle do
     cellar :any
-    sha256 "1f75c3a57ca3e1b7b528c04725c7d5c32dc1f4b222a289702dd7c057db8e34a2" => :high_sierra
-    sha256 "815cd74498e36ce6df804d22561d99e1ef0d4b5706f28e2850b2ea3f6c6df406" => :sierra
-    sha256 "be983033e47329fe52c1a32b1fcdfac071c495b24f4662584ad6165a0d126f30" => :el_capitan
-    sha256 "1a7be693b06e4219ef7721586fce481065433759d0dc9ee3b6caab2e776317c4" => :yosemite
+    sha256 "12be31c64f090488a744f701d7eef5b7c2b8370706c3b255fdf9d0c126a670fb" => :high_sierra
+    sha256 "036497460a7fde91dc78bb59aade2648adf641ffa97e3e7ef4662aa46c8648ed" => :sierra
+    sha256 "7b9ad0d0094e9b041a69163f1265e48d40ca6cdd9255cdeba8c71b1a57c35628" => :el_capitan
   end
 
   option "with-10-bit", "Build a 10-bit x264 (default: 8-bit)"
