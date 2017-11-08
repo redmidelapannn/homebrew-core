b8c4bcc49fc91d27eb11fe543218d422d63a7875
Robin Joseph <robin@lob.com>
2017-11-07 16:41:09 -0800

    use tarball and install metafiles

--- Formula/chamber.rb
+++ Formula/chamber.rb
@@ -1,9 +1,7 @@
 class Chamber < Formula
   desc "CLI for managing secrets through AWS SSM Parameter Store"
   homepage "https://github.com/segmentio/chamber"
-  url "https://github.com/segmentio/chamber.git",
-    :tag => "v1.9.0",
-    :revision => "acc4072df92f2763217ccfa5373e14f342e178ba"
+  url "https://github.com/segmentio/chamber/archive/v1.9.0.tar.gz"
   sha256 "098568ddda04956b8dc270aa8e6b4eb83d8cfe92add8ea0172715eab7db050cb"
   head "https://github.com/segmentio/chamber.git"
 
@@ -22,6 +20,7 @@ class Chamber < Formula
     cd buildpath/"src/github.com/segmentio/chamber" do
       system "govendor", "sync"
       system "go", "build", "-o", bin/"chamber"
+      prefix.install_metafiles
     end
   end
 
