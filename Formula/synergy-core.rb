c00cbc0799b4bfcda8bce44de5366266d7f20850
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-10 05:01:01 +0000

    synergy-core: add 1.9.0-rc4 bottle.

--- Formula/synergy-core.rb
+++ Formula/synergy-core.rb
@@ -6,6 +6,12 @@ class SynergyCore < Formula
   version "1.9.0-rc4"
   head "https://github.com/symless/synergy-core.git"
 
+  bottle do
+    sha256 "d7955fd5708c72b8e3aaaadc090a8d2b0c0718a83db1de4e1708ff5ddf86543e" => :high_sierra
+    sha256 "c45d16583a464a08755373995afba99d01b8554af86ca77532e5558c27c7667b" => :sierra
+    sha256 "13a6e28ace0f8ddf111123795cdf8f63b26bb3031b411fdcbf6f5b53b21ac8a8" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on "qt"
   depends_on "openssh"
