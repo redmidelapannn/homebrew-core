2402a72260706a447f426ea86e476a38c4d3b4af
sl1pkn07 <sl1pkn07@gmail.com>
2017-11-13 18:37:01 +0100

    fix install qca qt plugins in right path (/foo/bar/lib/qt5/plugins)

--- Formula/qca.rb
+++ Formula/qca.rb
@@ -62,6 +62,7 @@ class Qca < Formula
     args = std_cmake_args
     args << "-DQT4_BUILD=OFF"
     args << "-DBUILD_TESTS=OFF"
+    args << "-DQCA_PLUGINS_INSTALL_DIR=#{lib}/qt5/plugins"
 
     # Plugins (qca-ossl, qca-cyrus-sasl, qca-logger, qca-softstore always built)
     args << "-DWITH_botan_PLUGIN=#{build.with?("botan") ? "YES" : "NO"}"
