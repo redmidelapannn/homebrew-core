280f2ee950dca2102f9933a8376785820c9e5723
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-13 19:33:36 +0000

    libparamtuner: add 1.2 bottle.

--- Formula/libparamtuner.rb
+++ Formula/libparamtuner.rb
@@ -4,6 +4,13 @@ class Libparamtuner < Formula
   url "https://github.com/casiez/libparamtuner/releases/download/v1.2/libparamtuner-mac-1.2.tar.gz"
   sha256 "baba6c6eb2317573d5ce5e76378599dc075240075671fd2e8990354e40da72ce"
 
+  bottle do
+    cellar :any
+    sha256 "b75c82efddc83c168e0e50ee553e1e8377426091826f20bdda053ac8330f9d17" => :high_sierra
+    sha256 "1a8802c70ef9fecc3b8c8cd31e8a4ee76225159cdb792ab53414b5a8e75bba2a" => :sierra
+    sha256 "47874643fda0d72eba828a1226b67a4b302e75014a535e1dd3a693c5674f90c2" => :el_capitan
+  end
+
   needs :cxx11
 
   def install
