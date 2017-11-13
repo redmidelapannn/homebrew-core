f8b47c60bd2657476b892ef306a62a6b38bb6ab2
Ryo Nitami <nitry.n2t1m1@rnita.me>
2017-11-13 15:26:18 +0900

    Fix dependencies & test on dryrun.rb

--- Formula/dryrun.rb
+++ Formula/dryrun.rb
@@ -1,19 +1,18 @@
 class Dryrun < Formula
-    desc ":cloud: Try the demo project of any Android Library"
-    homepage "https://github.com/cesarferreira/dryrun/blob/master/README.md"
-    url "https://github.com/cesarferreira/dryrun/archive/v1.0.0.tar.gz"
-    sha256 "220a07109bc5f4a7ef2561a3f55a01c67de1c4c63c59047d10c811d093e26414"
+  desc ":cloud: Try the demo project of any Android Library"
+  homepage "https://github.com/cesarferreira/dryrun/blob/master/README.md"
+  url "https://github.com/cesarferreira/dryrun/archive/v1.0.0.tar.gz"
+  sha256 "220a07109bc5f4a7ef2561a3f55a01c67de1c4c63c59047d10c811d093e26414"
 
-    def install
-        ENV["GEM_HOME"] = libexec
-        system "gem", "build", "dryrun.gemspec"
-        system "gem", "install", "dryrun"
+  depends_on :ruby => "1.8"
 
-        bin.install libexec/"bin/pod", libexec/"bin/xcodeproj"
-        bin.env_script_all_files(libexec/"bin", :GEM_HOME => ENV["GEM_HOME"])
-    end
+  def install
+    ENV["GEM_HOME"] = libexec
+    system "gem", "build", "dryrun.gemspec"
+    system "gem", "install", "dryrun"
+  end
 
-    test do
-        system "false"
-    end
+  test do
+    system "#{bin}/dryrun"
+  end
 end
