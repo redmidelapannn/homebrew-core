419b91a0a4eab7c497cf4d88085d6b0e8b1cc7db
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-14 15:36:05 +0000

    gnomon: add 1.5.0 bottle.

--- Formula/gnomon.rb
+++ Formula/gnomon.rb
@@ -7,6 +7,13 @@ class Gnomon < Formula
   sha256 "5c9d83c33a1b364f0b2ad710416f97efd701b4f7a4bce800065a6b154772a695"
   head "https://github.com/paypal/gnomon.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b0b66a5ca004805b70da817e9b10f95ab989db52bcd397559919a5138ad8a55f" => :high_sierra
+    sha256 "85b77a7837d85a334d9919d4c573481f89394a952c2d104a72e14668f2a03723" => :sierra
+    sha256 "28b7865e5c6dcdf4667b50777880b4b27b0a8873576e99f3ba370a6cfdff35c4" => :el_capitan
+  end
+
   depends_on "node"
 
   def install
