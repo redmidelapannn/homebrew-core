51ad630ac64aaa54d8a30422eebb829d83f98af3
Laurent Cozic <laurent22@users.noreply.github.com>
2017-11-15 15:33:18 +0000

    Updated description

--- Formula/joplin.rb
+++ Formula/joplin.rb
@@ -1,7 +1,7 @@
 require "language/node"
 
 class Joplin < Formula
-  desc "A note taking and to-do application with synchronisation capabilities."
+  desc "Note taking and to-do application with synchronisation capabilities"
   homepage "http://joplin.cozic.net/"
   url "https://registry.npmjs.org/joplin/-/joplin-0.10.65.tgz"
   sha256 "52dcd067e670ede25a74d06a10223a9d7e134e56606219621a21dc3915127f8c"
