d53cece8382311e9a8da94db1b215f4457ffa745
ilovezfs <ilovezfs@icloud.com>
2017-11-23 06:50:47 -0800

    Update bedtools.rb

--- Formula/bedtools.rb
+++ Formula/bedtools.rb
@@ -3,21 +3,25 @@ class Bedtools < Formula
   homepage "https://github.com/arq5x/bedtools2"
   url "https://github.com/arq5x/bedtools2/archive/v2.26.0.tar.gz"
   sha256 "15db784f60a11b104ccbc9f440282e5780e0522b8d55d359a8318a6b61897977"
-  head "https://github.com/arq5x/bedtools2.git"
 
-  patch do
-    url "https://github.com/arq5x/bedtools2/commit/c0b7d934cc61ad6c83eb3d99374263e7ec51722d.diff?full_index=1"
-    sha256 "b37113ff55b916787f29c12dece8b7f4289de30427c3a22a8cb332aa32d936dd"
+  # Remove for > 2.26.0
+  # 24 Sep 2017 "strandqueue: de-const for stricter Xcode 9 rules"
+  if DevelopmentTools.clang_build_version >= 900
+    patch do
+      url "https://github.com/arq5x/bedtools2/commit/c0b7d934cc61ad6c83eb3d99374263e7ec51722d.diff?full_index=1"
+      sha256 "b37113ff55b916787f29c12dece8b7f4289de30427c3a22a8cb332aa32d936dd"
+    end
   end
 
   def install
     system "make"
-    prefix.install "bin"
-    doc.install %w[README.md RELEASE_HISTORY]
+    system "make", "install", "prefix=#{prefix}"
+    prefix.install "RELEASE_HISTORY"
   end
 
   test do
-    (testpath/"t.bed").write "c	1	5\nc	4	9"
-    assert_equal "c	1	9\n", shell_output("#{bin}/bedtools merge -i t.bed")
+    (testpath/"t.bed").write "c 1       5\nc    4       9"
+    output = shell_output("#{bin}/bedtools merge -i t.bed")
+    assert_equal "c     1       9", output.chomp
   end
 end
