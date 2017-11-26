db1012e46ec615ffb79174064ba532853f3b9aa1
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-26 05:17:03 +0000

    libatomic_ops: update 7.4.8 bottle.

--- Formula/libatomic_ops.rb
+++ Formula/libatomic_ops.rb
@@ -6,9 +6,10 @@ class LibatomicOps < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "dac326b799a710b727d87dd76811395b5c272efdbd0a5fb45d1e0603e0a0d921" => :high_sierra
-    sha256 "9e2926d6e7e2ce0b177411e55e76256974d6175874936009244bdc5f8c06971f" => :sierra
-    sha256 "4094450946626f7cd537cb4d821d7da0f8ae85d4dce46f1631799225506b9189" => :el_capitan
+    rebuild 1
+    sha256 "ab36c4b220c7c6e5175c7bfca66d393022ebbf82c62fd0b88ac24a65f7754219" => :high_sierra
+    sha256 "3b6a4a87d9dc8dbb3ea38c3aa8bbebbd7a48e73d6f35d9a2bac4e83f014377ca" => :sierra
+    sha256 "1aea728c40cbd116f8843a12b6c61b1e15261e936efa34516e57f193b86fcbaf" => :el_capitan
   end
 
   devel do
