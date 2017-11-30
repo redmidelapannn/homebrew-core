803b4f5a4475dc88f3681e487739d53b86e3bdaf
BrewTestBot <brew-test-bot@googlegroups.com>
2017-11-30 16:38:16 +0000

    gsound: add 1.0.2 bottle.

--- Formula/gsound.rb
+++ Formula/gsound.rb
@@ -4,6 +4,12 @@ class Gsound < Formula
   url "https://download.gnome.org/sources/gsound/1.0/gsound-1.0.2.tar.xz"
   sha256 "bba8ff30eea815037e53bee727bbd5f0b6a2e74d452a7711b819a7c444e78e53"
 
+  bottle do
+    sha256 "f2d8c964103a8a04b53751bccd63e13f207ce4f8fbec972871451391a3af53f2" => :high_sierra
+    sha256 "34eb6e8460916fdecfead37eecd9c72571ca0ee30480b96bf2d7c465a6619790" => :sierra
+    sha256 "be53025b52cc7756349ad9dec8857dd351137b9ef7cd931f3f45d3bc6849b3c3" => :el_capitan
+  end
+
   depends_on "gobject-introspection" => :build
   depends_on "pkg-config" => :build
   depends_on "glib"
