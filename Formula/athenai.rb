ff2b1ec8a5f90750001b642e045b99262e6207a0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-01 06:31:27 +0000

    athenai: add 0.2.0 bottle.

--- Formula/athenai.rb
+++ Formula/athenai.rb
@@ -5,6 +5,12 @@ class Athenai < Formula
   version "0.2.0"
   sha256 "90a0806047f42a1fdae80f534779f42174999b73546ab03dceefcd7c3ee9e0c4"
 
+  bottle do
+    sha256 "8a0c109990dcca2b716464a131e7cc512ea96d8093471d21ba232ff9748c45bd" => :high_sierra
+    sha256 "8a0c109990dcca2b716464a131e7cc512ea96d8093471d21ba232ff9748c45bd" => :sierra
+    sha256 "8a0c109990dcca2b716464a131e7cc512ea96d8093471d21ba232ff9748c45bd" => :el_capitan
+  end
+
   def install
     bin.install "athenai"
   end
