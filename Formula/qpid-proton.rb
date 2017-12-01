e7737f16b662341ea3eb2963b56774f2ba572b5e
ilovezfs <ilovezfs@icloud.com>
2017-12-01 08:28:21 -0800

    Update qpid-proton.rb

--- Formula/qpid-proton.rb
+++ Formula/qpid-proton.rb
@@ -1,30 +1,20 @@
 class QpidProton < Formula
-  desc "High-performance, lightweight AMQP 1.0 messaging library."
+  desc "High-performance, lightweight AMQP 1.0 messaging library"
   homepage "https://qpid.apache.org/proton/"
   url "https://www.apache.org/dyn/closer.lua?path=qpid/proton/0.18.0/qpid-proton-0.18.0.tar.gz"
   sha256 "7f54c3555a8482d439759c087d656d881369252af23f209f1fd5a7e4a8c59bd7"
-
-  head do
-    url "git://git.apache.org/qpid-proton.git"
-  end
+  head "git://git.apache.org/qpid-proton.git"
 
   depends_on "cmake" => :build
   depends_on "libuv"
   depends_on "openssl"
 
   def install
-    # Do not build language bindings and enforce installation into lib/ instead of lib64/
-    args = std_cmake_args + ["-DBUILD_BINDINGS=", "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=#{lib}"]
-
-    if MacOS.version == "10.11" && MacOS::Xcode.installed? && MacOS::Xcode.version >= "8.0"
-      inreplace "proton-c/CMakeLists.txt",
-        "CHECK_SYMBOL_EXISTS(clock_gettime \"time.h\" CLOCK_GETTIME_IN_LIBC)",
-        "CHECK_SYMBOL_EXISTS(undefined_gibberish \"time.h\" CLOCK_GETTIME_IN_LIBC)"
-    end
-
     ENV["OPENSSL_ROOT_DIR"] = Formula["openssl"].opt_prefix
 
-    system "cmake", ".", *args
+    system "cmake", ".", "-DBUILD_BINDINGS=",
+                         "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=#{lib}",
+                         *std_cmake_args
     system "make", "install"
   end
 
@@ -42,7 +32,7 @@ class QpidProton < Formula
           return 0;
       }
     EOS
-    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lqpid-proton", "-o", "test"
+    system ENV.cc, "test.c", "-L#{lib}", "-lqpid-proton", "-o", "test"
     system "./test"
   end
 end
