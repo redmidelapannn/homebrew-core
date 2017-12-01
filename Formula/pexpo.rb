b697f2139b7def5445a56b2e52472056406a576e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-01 19:02:33 +0000

    pexpo: add 1.32 bottle.

--- Formula/pexpo.rb
+++ Formula/pexpo.rb
@@ -7,6 +7,13 @@ class Pexpo < Formula
   head "https://github.com/nnao45/pexpo.git"
   version "1.32"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "672618397bbb446b8ab1fa6207bb5866d2d7352ba193c9d41fc190442c121af3" => :high_sierra
+    sha256 "898524f3dafbb89d3e8435b1bb0412f586f01e9bc2ed6191997e1ddb3bb0aa23" => :sierra
+    sha256 "901dca9460ab486a25410702d4a21e9ff3b345050f0816b46e650e841db3d66f" => :el_capitan
+  end
+
   depends_on "go" => :build
   depends_on :hg => :build
   
