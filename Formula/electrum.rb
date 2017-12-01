4441a71aaaad0200572f9d6ddd80037126b0a7b0
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-01 01:35:54 +0000

    electrum: add 3.0.2 bottle.

--- Formula/electrum.rb
+++ Formula/electrum.rb
@@ -4,6 +4,13 @@ class Electrum < Formula
   url "https://download.electrum.org/3.0.2/Electrum-3.0.2.tar.gz"
   sha256 "4dff75bc5f496f03ad7acbe33f7cec301955ef592b0276f2c518e94e47284f53"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9032701ee860b11fbd3143e08e8807557e774016d731de925585b52604e179d6" => :high_sierra
+    sha256 "9032701ee860b11fbd3143e08e8807557e774016d731de925585b52604e179d6" => :sierra
+    sha256 "9032701ee860b11fbd3143e08e8807557e774016d731de925585b52604e179d6" => :el_capitan
+  end
+
   depends_on "pyqt"
   depends_on "qt"
   depends_on "gettext"
