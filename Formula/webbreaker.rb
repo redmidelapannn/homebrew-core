acd0c0227aee41baa0c09da955e1c704777015b1
Jim Nelson <jim.nelson2@target.com>
2017-12-02 13:22:22 -0600

    webbreaker 2.0.2 (new formula)
    Initial formula creation

--- Formula/webbreaker.rb
+++ Formula/webbreaker.rb
@@ -1,17 +1,19 @@
+require "fileutils"
+
 class Webbreaker < Formula
   desc "Webbreaker: Dynamic Application Security Test Orchestration"
   homepage "https://github.com/target/webbreaker"
   url "https://github.com/target/webbreaker/files/1512597/webbreaker-macos.zip"
+  version "2.0.2"
   sha256 "f66d545cb29e2186315815ba6a8766b3f6024ca14807e46bcf807a401aea9902"
-	version "2.0.2"
 
   def install
-		bin.install "webbreaker-macos"
-		system "mv #{prefix}/bin/webbreaker-macos #{prefix}/bin/webbreaker"
-		bin.install_symlink "#{prefix}/bin/webbreaker" "webbreaker"
+    bin.install "webbreaker-macos"
+    mv("#{bin}/webbreaker-macos", "#{bin}/webbreaker")
+    bin.install_symlink "#{bin}/webbreaker" "webbreaker"
   end
 
   test do
-    system "webbreaker"
+    system "#{bin}/webbreaker"
   end
 end
