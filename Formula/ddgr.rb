bfc491f6f0f2b1cc74b2b511eafc4af94b96429f
Zhiming Wang <zmwangx@gmail.com>
2017-12-05 17:28:41 -0500

    ddgr 1.1 (new formula)

---
+++ Formula/ddgr.rb
@@ -0,0 +1,50 @@
+class Ddgr < Formula
+  include Language::Python::Virtualenv
+
+  desc "DuckDuckGo from the terminal"
+  homepage "https://github.com/jarun/ddgr"
+  url "https://github.com/jarun/ddgr/archive/v1.1.tar.gz"
+  sha256 "6fbf7a638e3e4c0db57ef4a86f253b5810e5755b88727444b17d4528e3a4f5d8"
+
+  depends_on :python3
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/23/3f/8be01c50ed24a4bd6b8da799839066ce0288f66f5e11f0367323467f0cbc/certifi-2017.11.5.tar.gz"
+    sha256 "5ec74291ca1136b40f0379e1128ff80e866597e4e2c1e755739a913bbc3613c0"
+  end
+
+  resource "chardet" do
+    url "https://files.pythonhosted.org/packages/fc/bb/a5768c230f9ddb03acc9ef3f0d4a3cf93462473795d18e9535498c8f929d/chardet-3.0.4.tar.gz"
+    sha256 "84ab92ed1c4d4f16916e05906b6b75a6c0fb5db821cc65e70cbd64a3e2a5eaae"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/f4/bd/0467d62790828c23c47fc1dfa1b1f052b24efdf5290f071c7a91d0d82fd3/idna-2.6.tar.gz"
+    sha256 "2c6a5de3089009e3da7c5dde64a141dbc8551d5b7f6cf4ed7c2568d0cc520a8f"
+  end
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/b0/e1/eab4fc3752e3d240468a8c0b284607899d2fbfb236a56b7377a329aa8d09/requests-2.18.4.tar.gz"
+    sha256 "9c443e7324ba5b85070c4a818ade28bfabedf16ea10206da1132edaa6dda237e"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/ee/11/7c59620aceedcc1ef65e156cc5ce5a24ef87be4107c2b74458464e437a5d/urllib3-1.22.tar.gz"
+    sha256 "cc44da8e1145637334317feebd728bd869a35285b93cbb4cca2577da7e62db4f"
+  end
+
+  def install
+    venv = virtualenv_create(libexec, "python3")
+    venv.pip_install resources
+    inreplace "ddgr", "#!/usr/bin/env python3", "#!#{libexec}/bin/python"
+    system "make", "install", "PREFIX=#{prefix}"
+    bash_completion.install "auto-completion/bash/ddgr-completion.bash"
+    fish_completion.install "auto-completion/fish/ddgr.fish"
+    zsh_completion.install "auto-completion/zsh/_ddgr"
+  end
+
+  test do
+    ENV["PYTHONIOENCODING"] = "utf-8"
+    assert_match "Homebrew", shell_output("#{bin}/ddgr --noprompt Homebrew")
+  end
+end
