befa4a2485d245240d12d71c2b31172d9ac78c88
Ben Konosky <bkonosky@gmail.com>
2017-12-05 23:11:21 -0800

    inspectrum: Cleaned up formula.

--- Formula/inspectrum.rb
+++ Formula/inspectrum.rb
@@ -1,9 +1,9 @@
-
 class Inspectrum < Formula
   desc "Offline radio signal analyser"
   homepage "https://github.com/miek/inspectrum"
   url "https://github.com/miek/inspectrum/archive/v0.2.tar.gz"
   sha256 "50b7db9b86208f414c387700a358eb58364094f3e8a4985f586f4f815645898a"
+  head "http://github.com/miek/inspectrum.git"
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
