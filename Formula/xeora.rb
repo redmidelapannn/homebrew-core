e44dc7a5e1c04bc33a38890681a3e088c1c490cf
Tuna Celik <tuna@jakpark.com>
2017-12-07 02:44:15 +0100

    Xeora Web Development Framework CLI Formula is added

---
+++ Formula/xeora.rb
@@ -0,0 +1,18 @@
+class Xeora < Formula
+  desc "Web Development Framework Command-line Interface"
+  homepage "http://www.xeora.org"
+  url "http://www.xeora.org/Tools/CLI/xeora-cli_v1.0.17506_macos.tgz"
+  version "1.0.17506"
+  sha256 "9da57f2b699a66d15c93a269651fbcbb7e0a6f3c008dec815c87c8658c1649c1"
+
+  def install
+    libexec.install Dir["*.pdb", "*.json", "*.dll"]
+    bin.install "xeora"
+    File.rename bin/"xeora", bin/"xeora-cli"
+    (bin/"xeora").write_env_script(bin/"xeora-cli", :XEORAPATH => libexec)
+  end
+
+  test do
+    system "#{bin}/xeora", "-v"
+  end
+end
