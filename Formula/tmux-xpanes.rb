d37d9329f6d7c5b9337dc7d75a1f846d2ecb5fd6
greymd <greengregson@gmail.com>
2017-12-10 11:03:14 +0900

    tmux-xpanes: Use tarball

--- Formula/tmux-xpanes.rb
+++ Formula/tmux-xpanes.rb
@@ -1,8 +1,9 @@
 class TmuxXpanes < Formula
   desc "Ultimate terminal divider powered by tmux"
   homepage "https://github.com/greymd/tmux-xpanes"
-  url "https://github.com/greymd/tmux-xpanes.git", :tag => "v2.2.1"
-  head "https://github.com/greymd/tmux-xpanes.git", :branch => "master"
+  url "https://github.com/greymd/tmux-xpanes/archive/v2.2.1.tar.gz"
+  sha256 "b0c537f078e8ba2657da8f66c9ac5b0ed31d558c55bd0fff3b7898791dee1a1e"
+
   depends_on "tmux" => :recommended
 
   def install
