8ea18aa581a749b234311dff51323a1c6ed2d627
Shaun Jackman <sjackman@gmail.com>
2017-12-11 12:23:39 -0800

    prodigal 1.5.3 (new formula)
    Migrate from Homebrew/science.

---
+++ Formula/prodigal.rb
@@ -0,0 +1,20 @@
+class Prodigal < Formula
+  desc "Microbial gene prediction"
+  homepage "https://github.com/hyattpd/Prodigal"
+  url "https://github.com/hyattpd/Prodigal/archive/v2.6.3.tar.gz"
+  sha256 "89094ad4bff5a8a8732d899f31cec350f5a4c27bcbdd12663f87c9d1f0ec599f"
+  head "https://github.com/hyattpd/Prodigal.git"
+
+  def install
+    system "make", "install", "INSTALLDIR=#{bin}"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/prodigal -v 2>&1")
+    fasta = <<~EOS
+      >U00096.2:1-70
+      AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC
+    EOS
+    assert_match "CDS", pipe_output("#{bin}/prodigal -q -p meta", fasta)
+  end
+end
