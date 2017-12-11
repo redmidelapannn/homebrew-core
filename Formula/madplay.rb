9528db57780a61fd4ed885e9afe24d808525c035
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-11 19:42:34 +0000

    madplay: update 0.15.2b bottle.

--- Formula/madplay.rb
+++ Formula/madplay.rb
@@ -5,11 +5,10 @@ class Madplay < Formula
   sha256 "5a79c7516ff7560dffc6a14399a389432bc619c905b13d3b73da22fa65acede0"
 
   bottle do
-    sha256 "7ff11d9521cb9507f669753e8c862efa44f5673cc009578202c1ec7dcba379d1" => :high_sierra
-    sha256 "a4a1b057547c65f8d793e874632e98ee10bfdae234ff011d16d99593c3fa7853" => :sierra
-    sha256 "81dbc8781c5da50f7188a4031ed5d500b07c51a7589da6799c6bf3477bb90bf6" => :el_capitan
-    sha256 "4ab0b6303cafe408494e85c38b80a3c44964953995c024d2b65a019bc5608c05" => :yosemite
-    sha256 "2b1967955d83ca172724b119e837457aec0eeaa7ded354c810f3635dafbec057" => :mavericks
+    rebuild 1
+    sha256 "501b84cf0440fd53b9cfb5309fdc71c63a144e2ffcd4fe3ee5b8db90493a833c" => :high_sierra
+    sha256 "d37814111a53f3f69af2db4a86127c0b0d001a4b249bdfeefe84286aae557642" => :sierra
+    sha256 "3434afd72f6576caa7cc3830004d7628d9e2ec328c68c5c942befd753e019989" => :el_capitan
   end
 
   depends_on "mad"
