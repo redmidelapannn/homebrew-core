5021e94a73d3b26f95d7ac2dead0b530074f156c
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-11 19:42:34 +0000

    mad: update 0.15.1b bottle.

--- Formula/mad.rb
+++ Formula/mad.rb
@@ -6,13 +6,10 @@ class Mad < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "a053f1e30d1d04f8c1f472d9b0b243d9572c3e7ff2c3e46a665564021bf37432" => :high_sierra
-    sha256 "1b48168bf5df9fc66703afc68ad3574b9ceb723fbcbf0f1e84c3c1c18fe6ff92" => :sierra
-    sha256 "a8a162813aad00169410a8f14a39927028969c914929fafb0685f0eb80075546" => :el_capitan
-    sha256 "863c71f31ecda8f97effc4dd148564e03219f8ddd162c89e054a7e57623c18c6" => :yosemite
-    sha256 "7bd46d4da0f695b3a5bcc899b7139f14d11741f2e47d34f21a984f9bab953c81" => :mavericks
-    sha256 "11391349b4ee378f31ae681ff5c96f7c8d1b5f2a6ad5ba6b28d12bc102cc9d6b" => :mountain_lion
+    rebuild 2
+    sha256 "1d4e5dde92207dd577b6a5a05183778f187a8b5c22e46fae6aece6cb47ee5149" => :high_sierra
+    sha256 "2da5b805f025076b85d7e5c0bab55463eace09f86c2da8255bc5c4217c50def7" => :sierra
+    sha256 "1a35e3c580fbeb2202f2b4e2ce10fc07170242a07742b79d5e5e09512e86cac1" => :el_capitan
   end
 
   def install
