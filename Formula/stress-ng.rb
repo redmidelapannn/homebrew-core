7a0a56397959660f5479daca0c46475b74996e0c
Jacob Bednarz <jacob.bednarz@gmail.com>
2017-12-14 07:01:50 +1100

    Use offical release tarball

--- Formula/stress-ng.rb
+++ Formula/stress-ng.rb
@@ -1,8 +1,8 @@
 class StressNg < Formula
   desc "Stress test a computer system in various selectable ways"
   homepage "http://kernel.ubuntu.com/~cking/stress-ng/"
-  url "https://github.com/ColinIanKing/stress-ng/archive/V0.09.04.tar.gz"
-  sha256 "83bb1f11e66df753f16dd924c36fea9df20281ca85d49d07c15d970fe902baa4"
+  url "http://kernel.ubuntu.com/~cking/tarballs/stress-ng/stress-ng-0.09.06.tar.xz"
+  sha256 "59c09c7cccb34222dc124ce8622a0ac73fe9391ba16a4a561267ca55c02f2520"
 
   def install
     system "make"
