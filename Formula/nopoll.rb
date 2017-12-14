e953cb9645f97f44647ef6ca63fc7c9387e64706
ilovezfs <ilovezfs@icloud.com>
2017-12-14 05:13:36 -0800

    Update nopoll.rb

--- Formula/nopoll.rb
+++ Formula/nopoll.rb
@@ -2,6 +2,7 @@ class Nopoll < Formula
   desc "Open-source C WebSocket toolkit"
   homepage "https://www.aspl.es/nopoll/"
   url "https://www.aspl.es/nopoll/downloads/nopoll-0.4.5.b375.tar.gz"
+  version "0.4.5.b375"
   sha256 "4ecbd277714c9acd154277a46388689f9d651836ca5c69990e35d1eee8c7ceae"
 
   depends_on "openssl"
