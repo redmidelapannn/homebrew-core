c9eabe024a72c0067ab06fab403b06af75ee9f56
Ben Konosky <bkonosky@gmail.com>
2017-12-15 01:43:49 -0800

    Inspectrum: Fixed almost working test

--- Formula/inspectrum.rb
+++ Formula/inspectrum.rb
@@ -17,6 +17,6 @@ class Inspectrum < Formula
   end
 
   test do
-    assert_equal "Usage: inspectrum [options] file\nspectrum viewer\n\nOptions:\n  -h, --help       Displays this help.\n  -r, --rate <Hz>  Set sample rate.\n\nArguments:\n  file             File to view.", shell_output("inspectrum -h").strip
+    assert_equal "Usage: inspectrum [options] file\nspectrum viewer\n\nOptions:\n  -h, --help       Displays this help.\n  -r, --rate <Hz>  Set sample rate.\n\nArguments:\n  file             File to view.", shell_output("#{bin}/inspectrum -h").strip
   end
 end
