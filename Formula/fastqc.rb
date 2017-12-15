73407650ff4700235e140b0c312bbe8400f2fb9e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-15 21:27:59 +0000

    fastqc: add 0.11.5 bottle.

--- Formula/fastqc.rb
+++ Formula/fastqc.rb
@@ -4,6 +4,13 @@ class Fastqc < Formula
   url "https://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.11.5.zip"
   sha256 "dd7a5ad80ceed2588cf6d6ffe35e0f161c0d9977ed08355f5e4d9473282cbd66"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "74a97385cdbdaf5dc3ef9c053638d18a73bffeb48bea755cc464d686685f19db" => :high_sierra
+    sha256 "74a97385cdbdaf5dc3ef9c053638d18a73bffeb48bea755cc464d686685f19db" => :sierra
+    sha256 "74a97385cdbdaf5dc3ef9c053638d18a73bffeb48bea755cc464d686685f19db" => :el_capitan
+  end
+
   depends_on :java
 
   def install
