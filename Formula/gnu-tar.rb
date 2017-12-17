eaa154f10e277a98e5752fd554a4244abf9de6b1
ilovezfs <ilovezfs@icloud.com>
2017-12-17 11:10:09 -0800

    gnu-tar 1.30

--- Formula/gnu-tar.rb
+++ Formula/gnu-tar.rb
@@ -1,10 +1,9 @@
 class GnuTar < Formula
   desc "GNU version of the tar archiving utility"
   homepage "https://www.gnu.org/software/tar/"
-  url "https://ftp.gnu.org/gnu/tar/tar-1.29.tar.gz"
-  mirror "https://ftpmirror.gnu.org/tar/tar-1.29.tar.gz"
-  sha256 "cae466e6e58c7292355e7080248f244db3a4cf755f33f4fa25ca7f9a7ed09af0"
-  revision 1
+  url "https://ftp.gnu.org/gnu/tar/tar-1.30.tar.gz"
+  mirror "https://ftpmirror.gnu.org/tar/tar-1.30.tar.gz"
+  sha256 "4725cc2c2f5a274b12b39d1f78b3545ec9ebb06a6e48e8845e1995ac8513b088"
 
   bottle do
     sha256 "20f2d3cad911cb885ac3760daa283d5fd709e570f8a97486e91a6f41a49183c8" => :high_sierra
