5b2967d7d586116fd64ec3ed660e263d6aeebee8
José L. Patiño <jose.lpa@gmail.com>
2017-12-20 16:49:24 +0000

    libjwt 1.8.0 (new formula)
    C library for JSON Web Tokens, by Ben Collins.
    GitHub homepage: https://github.com/benmcollins/libjwt

---
+++ Formula/libjwt.rb
@@ -0,0 +1,41 @@
+class Libjwt < Formula
+  desc "JWT C Library"
+  homepage "https://benmcollins.github.io/libjwt/"
+  url "https://github.com/benmcollins/libjwt/archive/v1.8.0.tar.gz"
+  sha256 "e97c15c14dbd49d5ba435cb26e06e6cdd6e32cf9f0be6b6e9a2d2d330923ea17"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "check" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "jansson"
+  depends_on "openssl"
+
+  def install
+    system "autoreconf", "-v", "-i"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "all"
+    system "make", "check"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdlib.h>
+      #include <jwt.h>
+
+      int main() {
+        int token = 1;
+
+        jwt_t *jwt = NULL;
+
+        if (jwt_new(&jwt) != 0) abort();
+
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-I#{include}", "-ljwt", "-o", "test", "-v"
+    system "./test"
+  end
+end
