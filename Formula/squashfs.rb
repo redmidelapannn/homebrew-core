23d113f76f0b99d3371f432dc9ccb70215e2f247
Alexander Nestorov <alexandernst@gmail.com>
2017-12-21 14:47:05 +0100

    Update squashfs.rb

--- Formula/squashfs.rb
+++ Formula/squashfs.rb
@@ -3,6 +3,7 @@ class Squashfs < Formula
   homepage "https://squashfs.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/squashfs/squashfs/squashfs4.3/squashfs4.3.tar.gz"
   sha256 "0d605512437b1eb800b4736791559295ee5f60177e102e4d4ccd0ee241a5f3f6"
+  revision 1
 
   bottle do
     cellar :any
