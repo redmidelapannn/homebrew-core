0a515dd15b721daac895e72c051feb728adb5a91
tmozgach <tmozgach@sfu.ca>
2017-12-22 13:09:17 -0800

    Update diamond.rb

--- Formula/diamond.rb
+++ Formula/diamond.rb
@@ -1,7 +1,6 @@
 class Diamond < Formula
   desc "Accelerated BLAST compatible local sequence aligner"
   homepage "https://ab.inf.uni-tuebingen.de/software/diamond/"
-
   url "https://github.com/bbuchfink/diamond/archive/v0.9.13.tar.gz"
   sha256 "066d2744ef9e8f3d6f7eba5e6eb226434299b18574c8716bbdd8faca31b325de"
 
@@ -13,6 +12,26 @@ class Diamond < Formula
   end
 
   test do
-    assert_match "gapextend", shell_output("#{bin}/diamond help 2>&1")
+    (testpath/"nr.faa").write <<~EOS
+      >gnl|alu|HSU14568_Alu_Sb_consensus_rf1
+      grarwltpvipalweaeaggsrgqeietilantvkprlyXkyknXpgvvagacspsysgg
+      XgrrmaXtreaelavsrdratalqpgrqsetpsqkk
+      >gnl|alu|HSU14568_Alu_Sb_consensus_rf2
+      agrggsrlXsqhfgrprradhevrrsrpswltrXnpvstkntkisrawwrapvvpatrea
+      eagewrepgrrslqXaeiaplhsslgdrarlrlkk
+      >gnl|alu|HSU14568_Alu_Sb_consensus_rf3
+      pgavahacnpstlggrggritrsgdrdhpgXhgetpsllkiqklagrgggrlXsqllgrl
+      rqengvnpgggacseprsrhctpawaterdsvskk
+      >gnl|alu|HSU14568_Alu_Sb_consensus_rf-1
+      fflrrslalsprlecsgaisahcklrlpgsrhspasasrvagttgarhharlifvflvet
+      gfhrvsqdgldlltsXsarlglpkcwdyrrepprpa
+      >gnl|alu|HSU14568_Alu_Sb_consensus_rf-2
+      ffXdgvslcrpgwsavarsrltassasrvhaillpqppeXlglqapattpgXflyfXXrr
+      gftvlarmvsisXprdppasasqsagitgvshrar
+      >gnl|alu|HSU14568_Alu_Sb_consensus_rf-3
+      ffetesrsvaqagvqwrdlgslqapppgftpfsclslpsswdyrrppprpanfcifsrdg
+      vspcXpgwsrspdlvirpprppkvlglqaXatapg
+    EOS
+    assert_match "Processed 6 sequences, 572 letters.", shell_output("#{bin}/diamond makedb --in nr.faa -d nr")
   end
 end
