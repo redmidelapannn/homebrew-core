b48f8f989c100bb264be3ad97607b277f02124c8
Alex Gaynor <alex.gaynor@gmail.com>
2017-12-25 22:26:15 -0500

    pypy 5.10.0

--- Formula/pypy.rb
+++ Formula/pypy.rb
@@ -4,8 +4,8 @@ class Pypy < Formula
   head "https://bitbucket.org/pypy/pypy", :using => :hg
 
   stable do
-    url "https://bitbucket.org/pypy/pypy/downloads/pypy2-v5.9.0-src.tar.bz2"
-    sha256 "de4bf05df47f1349dbac97233d9277bbaf1ef3331663ea2557fd5da3dbcfd0a7"
+    url "https://bitbucket.org/pypy/pypy/downloads/pypy2-v5.10.0-src.tar.bz2"
+    sha256 "1209f2db718e6afda17528baa5138177a14a0938588a7d3e1b7c722c483079a8"
   end
 
   bottle do
