5b046dcf52a0f1ad9e42e23518bc8edf7f92523e
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-27 17:15:05 +0000

    fdclone: update 3.01d bottle.

--- Formula/fdclone.rb
+++ Formula/fdclone.rb
@@ -5,11 +5,9 @@ class Fdclone < Formula
   sha256 "aa33c09d2e51c486fb428e3a17c31a1db0acc3b04083b84c4f9e6259c7ffb6da"
 
   bottle do
-    sha256 "14fe7c148d8e95a516c1477ccf58f041f2029731638a401346f7dfb047313175" => :high_sierra
-    sha256 "ce0319a6a58dbf44dbfdd8d8716c86cf34fbb856bf660034eae9b18dc1362dc6" => :sierra
-    sha256 "f9992eacec6b447a8bef9946445e371f560483799c0912e5ecd6b095bbd0b542" => :el_capitan
-    sha256 "e5983768aec9c3d61a72a06ea25b121cf54178461b22f3848c446249a99b26a9" => :yosemite
-    sha256 "1add0151a1961ea9b9b167b220dc0248a0fec6263efa351c2558f7eded98c361" => :mavericks
+    sha256 "7c6dd78810c73e60766e15d41e47861a31978b55315afc1b4044ed1bc39588b0" => :high_sierra
+    sha256 "f52e2352bdf61282f7b7d4f93e66c257d926cfa62929fd9899c0396857e10047" => :sierra
+    sha256 "5a47bda98dfcdd47f0ff089172a2fc0e6f3c10fff20417b57c4c105971f226e8" => :el_capitan
   end
 
   depends_on "nkf" => :build
