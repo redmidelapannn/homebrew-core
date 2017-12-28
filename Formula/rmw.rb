547ae5336c208cfae8480e17697c3995a1291103
Martijn de Boer <github@sexybiggetje.nl>
2017-12-28 10:47:34 +0100

    Remove dependency on ncurses
    Test specifies that ncurses isn't needed.

--- Formula/rmw.rb
+++ Formula/rmw.rb
@@ -4,8 +4,6 @@ class Rmw < Formula
   url "https://github.com/andy5995/rmw/archive/v0.4.03.tar.gz"
   sha256 "5b08fce4392901d7900bfbd65bd8794a1bf9b6700073bc48ed8e88d6185c4177"
 
-  depends_on "ncurses"
-
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
