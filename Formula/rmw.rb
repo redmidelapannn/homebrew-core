672bfaa671fc1361372529648debf1f92c80c4d0
Martijn de Boer <github@sexybiggetje.nl>
2017-12-28 10:50:52 +0100

    Add head source line
    Add head repository for source/devel install.

--- Formula/rmw.rb
+++ Formula/rmw.rb
@@ -3,6 +3,7 @@ class Rmw < Formula
   homepage "https://github.com/andy5995/rmw/wiki"
   url "https://github.com/andy5995/rmw/archive/v0.4.03.tar.gz"
   sha256 "5b08fce4392901d7900bfbd65bd8794a1bf9b6700073bc48ed8e88d6185c4177"
+  head "https://github.com/andy5995/rmw.git"
 
   def install
     system "./configure", "--disable-debug",
