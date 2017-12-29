0d9fbc558c8097a23cb4aa2188d2281d009b40c5
BrewTestBot <brew-test-bot@googlegroups.com>
2017-12-29 04:38:15 +0000

    ipfs-update: add 1.5.2 bottle.

--- Formula/ipfs-update.rb
+++ Formula/ipfs-update.rb
@@ -5,6 +5,13 @@ class IpfsUpdate < Formula
   version "1.5.2"
   sha256 "9f7017fe7453fd42b35a52d631ba4765fdc0e6d0cd890f0eb1a12a64f086c922"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "2a5e1a50920c3fffbd1050a4bc75881befc6ac3ea04d1687f35cf631c36a134d" => :high_sierra
+    sha256 "2a5e1a50920c3fffbd1050a4bc75881befc6ac3ea04d1687f35cf631c36a134d" => :sierra
+    sha256 "2a5e1a50920c3fffbd1050a4bc75881befc6ac3ea04d1687f35cf631c36a134d" => :el_capitan
+  end
+
   def install
     bin.install "ipfs-update"
   end
