0ecffc85e49d48aebc49d2425f9d64cb6fc4a686
ilovezfs <ilovezfs@icloud.com>
2017-12-31 01:13:28 -0800

    Update bwa.rb

--- Formula/bwa.rb
+++ Formula/bwa.rb
@@ -3,17 +3,16 @@ class Bwa < Formula
   homepage "https://github.com/lh3/bwa"
   url "https://github.com/lh3/bwa/releases/download/v0.7.16/bwa-0.7.16a.tar.bz2"
   sha256 "8fecdb5f88871351bbe050c18d6078121456c36ad75c5c78f33a926560ffc170"
-  head "https://github.com/lh3/bwa.git"
 
   def install
-    system "make", "CC=#{ENV.cc}", "CFLAGS=#{ENV.cflags}"
+    system "make"
     bin.install "bwa"
     man1.install "bwa.1"
   end
 
   test do
     (testpath/"test.fasta").write ">0\nAGATGTGCTG\n"
-    system "#{bin}/bwa", "index", "test.fasta"
+    system bin/"bwa", "index", "test.fasta"
     assert_predicate testpath/"test.fasta.bwt", :exist?
     assert_match "AGATGTGCTG", shell_output("#{bin}/bwa mem test.fasta test.fasta")
   end
