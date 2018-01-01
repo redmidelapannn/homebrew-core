4776f242c5856751cfee55fa5c7712f6a3fe697f
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-01 22:11:25 +0000

    krb5: update 1.16 bottle.

--- Formula/krb5.rb
+++ Formula/krb5.rb
@@ -5,9 +5,10 @@ class Krb5 < Formula
   sha256 "faeb125f83b0fb4cdb2f99f088140631bb47d975982de0956d18c85842969e08"
 
   bottle do
-    sha256 "c31f35f264498fbf82bfb940e6c671e53ca1acf9db338ff6bfbc11c43e790a19" => :high_sierra
-    sha256 "dbf24c411aa982dcc5c1496ee026205f8fb5f6e51531490e1d7f4aafd14a3473" => :sierra
-    sha256 "542388bcad53bf98142ed2803ab1054be4e2bbb89920a883f504fdd254db5fae" => :el_capitan
+    rebuild 1
+    sha256 "7118c3cce933d59e0236633b00e752d811ace158c3608948c15d0f315ec7a12b" => :high_sierra
+    sha256 "4a2efa99152cd3274585f2a4e79e0144731808f8ee7286f0649520eae323a387" => :sierra
+    sha256 "67e72387bb98c5c96d31e114e9cda88c9ffd21af7cd8201c8e5479144f662edd" => :el_capitan
   end
 
   keg_only :provided_by_osx
