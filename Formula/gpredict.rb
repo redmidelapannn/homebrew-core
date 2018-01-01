28cf1453cc14a277c0ca351b4d0b218655f06de1
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-01 12:22:39 +0000

    gpredict: add 2.0 bottle.

--- Formula/gpredict.rb
+++ Formula/gpredict.rb
@@ -4,6 +4,12 @@ class Gpredict < Formula
   url "https://downloads.sourceforge.net/project/gpredict/Gpredict/2.0/gpredict-2.0.tar.gz"
   sha256 "508f882383eac326aecb0b058378fc71f13b431c581e0efc28ee3c4216c76e16"
 
+  bottle do
+    sha256 "62c56ff78bccab17e95cfb5b930ef38823683a14f10dfe5e7e971586cc0d3378" => :high_sierra
+    sha256 "b4063816617f5c10308089e5b4ca808d53230d60504e97f790615b454dfc35f8" => :sierra
+    sha256 "44f0d0a3db18b8dc60762266591654ac342f6e68f52359c8adc726ca8f1c258f" => :el_capitan
+  end
+
   depends_on :x11
   depends_on "pkg-config" => :build
   depends_on "intltool" => :build
