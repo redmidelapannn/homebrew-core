d21f596a5053a3c4dc200c8c1a02496a3139ac77
Steven A. Cholewiak <steven@cholewiak.com>
2018-01-02 10:14:15 -0800

    librealsense (bumping to 2.9.0)

--- Formula/librealsense.rb
+++ Formula/librealsense.rb
@@ -1,16 +1,9 @@
 class Librealsense < Formula
   desc "Intel RealSense D400 series and SR300 capture"
   homepage "https://github.com/IntelRealSense/librealsense"
-  url "https://github.com/IntelRealSense/librealsense/archive/v2.8.3.tar.gz"
-  sha256 "4b99e684d047089e6b641ed1975ee50f862e7ea56280466ab7d8cb822627cdc6"
+  url "https://github.com/IntelRealSense/librealsense/archive/v2.9.0.tar.gz"
+  sha256 "851bab01eb5c4e5d33fe0759197e6f89b7406da277b7a272016a0b47306ccff1"
   head "https://github.com/IntelRealSense/librealsense.git"
-
-  bottle do
-    cellar :any
-    sha256 "3509c5d9d563e6db458166816331ac1a86f790fc7cd0ccc1d58fd7ac57b49edb" => :high_sierra
-    sha256 "7c8d47e698f30527724f1f44c1b1fbfed0465afec43cef59c0ff1f51772721ce" => :sierra
-    sha256 "2ccabb3e828fa7dd9df88a8dcd590139d96686965468b85851924b27b880ea4c" => :el_capitan
-  end
 
   option "with-examples", "Install examples"
 
