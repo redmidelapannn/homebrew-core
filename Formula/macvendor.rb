dab4f97ae9e1a44404a045b20561809cacaf4e61
Ryan Draga <ryan.draga@scotiabank.com>
2018-01-03 10:58:13 -0500

    add macvendor formula

---
+++ Formula/macvendor.rb
@@ -0,0 +1,14 @@
+class Macvendor < Formula
+  desc "CLI tool which looks up a given MAC Address on macvendors.com"
+  homepage "https://github.com/tuxotaku/macvendor"
+  url "https://github.com/TuxOtaku/macvendor/releases/download/1.0/macvendor-darwin-amd64"
+  sha256 "cb67c08ece570cdac599d87ad295e46b1a195213ecf18ad986b584e671cbf2c2"
+
+  def install
+    bin.install "macvendor"
+  end
+
+  test do
+    shell_output("#{bin}/macvendor -h", 1)
+  end
+end
