108bcd1ba45bfe09d67e26fc0d7d82d0a75f0b78
Fabien Spindler <Fabien.Spindler@inria.fr>
2018-01-03 17:00:43 +0100

    Apply requested changes

--- Formula/visp.rb
+++ Formula/visp.rb
@@ -1,10 +1,10 @@
 class Visp < Formula
   desc "Visual Servoing Platform library"
-  homepage "https://visp.inria.fr"
+  homepage "https://visp.inria.fr/"
   url "https://gforge.inria.fr/frs/download.php/latestfile/475/visp-3.1.0.tar.gz"
   sha256 "2a1df8195b06f9a057bd4c7d987697be2fdcc9d169e8d550fcf68e5d7f129d96"
 
-  depends_on "cmake"     => :build
+  depends_on "cmake" => :build
   depends_on "eigen"
   depends_on "gsl"
   depends_on "jpeg"
@@ -12,39 +12,32 @@ class Visp < Formula
   depends_on "libpng"
   depends_on "opencv"
   depends_on "zbar"
-  depends_on :x11
 
   def install
-
-    args = std_cmake_args + %w[
-      -DBUILD_DEMOS=OFF
-      -DBUILD_EXAMPLES=OFF
-      -DBUILD_TESTS=OFF
-      -DBUILD_TUTORIALS=OFF
-      -DUSE_DC1394=ON
-      -DUSE_EIGEN3=ON
-      -DUSE_GSL=ON
-      -DUSE_JPEG=ON
-      -DUSE_LAPACK=OFF
-      -DUSE_LIBUSB_1=OFF
-      -DUSE_OPENCV=ON
-      -DUSE_PCL=OFF
-      -DUSE_PNG=ON
-      -DUSE_PTHREAD=ON
-      -DUSE_PYLON=OFF
-      -DUSE_REALSENSE=OFF
-      -DUSE_REALSENSE2=OFF
-      -DUSE_X11=ON
-      -DUSE_XML2=ON
-      -DUSE_ZBAR=ON
-      -DUSE_ZLIB=OFF
-      -DWITH_LAPACK=OFF
-    ]
-
-    mkdir "build" do
-      system "cmake", "..", *args
-      system "make", "install"
-    end
+    system "cmake", ".", "-DBUILD_DEMOS=OFF",
+                         "-DBUILD_EXAMPLES=OFF",
+                         "-DBUILD_TESTS=OFF",
+                         "-DBUILD_TUTORIALS=OFF",
+                         "-DUSE_DC1394=ON",
+                         "-DUSE_EIGEN3=ON",
+                         "-DUSE_GSL=ON",
+                         "-DUSE_JPEG=ON",
+                         "-DUSE_LAPACK=OFF",
+                         "-DUSE_LIBUSB_1=OFF",
+                         "-DUSE_OPENCV=ON",
+                         "-DUSE_PCL=OFF",
+                         "-DUSE_PNG=ON",
+                         "-DUSE_PTHREAD=ON",
+                         "-DUSE_PYLON=OFF",
+                         "-DUSE_REALSENSE=OFF",
+                         "-DUSE_REALSENSE2=OFF",
+                         "-DUSE_X11=OFF",
+                         "-DUSE_XML2=ON",
+                         "-DUSE_ZBAR=ON",
+                         "-DUSE_ZLIB=OFF",
+                         "-DWITH_LAPACK=OFF",
+                         *std_cmake_args
+    system "make", "install"
   end
 
   test do
@@ -59,7 +52,6 @@ class Visp < Formula
       }
     EOS
     system ENV.cxx, "test.cpp", "-I#{include}", "-L#{lib}", "-o", "test"
-    output = shell_output("./test")
-    assert_equal version.to_s, output.chomp
+    assert_equal version.to_s, shell_output("./test").chomp
   end
 end
