036f1caa57166a2f1cdf134d3ef9c662d80497a2
ilovezfs <ilovezfs@icloud.com>
2018-01-03 06:26:32 -0800

    Update librealsense.rb

--- Formula/librealsense.rb
+++ Formula/librealsense.rb
@@ -5,6 +5,13 @@ class Librealsense < Formula
   sha256 "851bab01eb5c4e5d33fe0759197e6f89b7406da277b7a272016a0b47306ccff1"
   head "https://github.com/IntelRealSense/librealsense.git"
 
+  bottle do
+    cellar :any
+    sha256 "3509c5d9d563e6db458166816331ac1a86f790fc7cd0ccc1d58fd7ac57b49edb" => :high_sierra
+    sha256 "7c8d47e698f30527724f1f44c1b1fbfed0465afec43cef59c0ff1f51772721ce" => :sierra
+    sha256 "2ccabb3e828fa7dd9df88a8dcd590139d96686965468b85851924b27b880ea4c" => :el_capitan
+  end
+
   option "with-examples", "Install examples"
 
   depends_on "cmake" => :build
