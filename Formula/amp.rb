ff396fcbcbcbca25a2af080eb6be66c0f6ddef88
jordan@wastedintelligence.com <jordan@wastedintelligence.com>
2018-01-04 14:00:43 -0500

    Add initial amp formula

---
+++ Formula/amp.rb
@@ -0,0 +1,46 @@
+class Amp < Formula
+  desc "Text editor for your terminal"
+  homepage "https://amp.rs"
+  url "https://github.com/jmacdonald/amp/archive/0.3.2.tar.gz"
+  sha256 "9080c894111149d58a0b805e0c08edaf1d9e7cd56c4b955ba9111feec6b55208"
+  head "https://github.com/jmacdonald/amp.git"
+
+  depends_on "rust" => :build
+  depends_on "cmake" => :build
+  depends_on "openssl"
+
+  def install
+    # Ensure we link against openssl@1.0, not openssl@1.1.
+    ENV["OPENSSL_INCLUDE_DIR"] = Formula["openssl"].opt_include
+    ENV["OPENSSL_LIB_DIR"] = Formula["openssl"].opt_lib
+
+    system "cargo", "build", "--release"
+    bin.install "target/release/amp"
+  end
+
+  test do
+    ENV["TERM"] = "xterm"
+
+    # Setup a path to which Amp will write data.
+    amp_file_path = testpath/"amp_file"
+
+    (testpath/"test.exp").write <<~EOS
+      spawn "#{bin}/amp" "#{amp_file_path}"
+      interact timeout 1 return
+
+      # switch to insert mode and add data
+      send "i"
+      send "test data"
+
+      # escape to normal mode, save the file, and quit
+      send "\x1b"
+      send "s"
+      send "Q"
+      expect eof
+    EOS
+
+    # Run Amp using expect and verify that it writes the correct data.
+    system "expect", "-f", "test.exp"
+    assert_match "test data\n", IO.read(amp_file_path)
+  end
+end
