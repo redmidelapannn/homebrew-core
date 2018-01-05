d4025f1a6b8e6c4627ba69127766bd6d2774ecb4
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-05 18:25:00 +0000

    duviz: add 1.1.0 bottle.

--- Formula/duviz.rb
+++ Formula/duviz.rb
@@ -7,6 +7,13 @@ class Duviz < Formula
   url "https://github.com/soxofaan/duviz/archive/1.1.0.tar.gz"
   sha256 "72ecd1ffc5bcc0900bd2b5c5708cf1eb6de2c1ba512b1dfb80a802e9754dea32"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "805588dcebb17d182604119878947f0727391f4465384eed0e930777b961fe6f" => :high_sierra
+    sha256 "ee20dbb134271cddbe77b68769993dfe900ed31c11f5b8372419d437680ac386" => :sierra
+    sha256 "6417ec67cbc9681ee37b4bfd9c05bee0b8314be8888d221278eb27e613ed5b1d" => :el_capitan
+  end
+
   depends_on "python" if MacOS.version <= :snow_leopard
 
   def install
