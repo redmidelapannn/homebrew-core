0d92449ed23dcc7552dbbefbf99e8c2341a2a66c
ilovezfs <ilovezfs@icloud.com>
2018-01-08 15:40:50 +0000

    Update bioawk.rb

--- Formula/bioawk.rb
+++ Formula/bioawk.rb
@@ -3,7 +3,6 @@ class Bioawk < Formula
   homepage "https://github.com/lh3/bioawk"
   url "https://github.com/lh3/bioawk/archive/v1.0.tar.gz"
   sha256 "5cbef3f39b085daba45510ff450afcf943cfdfdd483a546c8a509d3075ff51b5"
-  head "https://github.com/lh3/bioawk.git"
 
   def install
     # Fix make: *** No rule to make target `ytab.h', needed by `b.o'.
@@ -20,6 +19,7 @@ class Bioawk < Formula
       AGCTTTTCATTCTGACTGCAACGGGCAATATGTCT
       CTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC
     EOS
-    assert_equal "70\n", shell_output("#{bin}/bioawk -cfastx '{print length($seq)}' test.fasta")
+    cmd = "#{bin}/bioawk -cfastx '{print length($seq)}' test.fasta" 
+    assert_equal "70", shell_output(cmd).chomp
   end
 end
