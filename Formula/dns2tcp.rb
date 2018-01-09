7119645937ecf41cce05803df324a6c51eaf80cf
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-09 11:13:12 +0000

    dns2tcp: update 0.5.2 bottle.

--- Formula/dns2tcp.rb
+++ Formula/dns2tcp.rb
@@ -7,11 +7,10 @@ class Dns2tcp < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d6fb240175854e0a0b5069544a58c4fbcd161d3337288c2f289f48999c4dde10" => :high_sierra
-    sha256 "e948ddde1e95f055a9cd3e73cd2756c22f729d9feed9ebc2929cb3df6fe09584" => :sierra
-    sha256 "2cd5e77bec42f0f5e2715494c38eb8773ab30d53b140509d3f428d38890bf640" => :el_capitan
-    sha256 "3e805ac804eea824b81bd15191b71cdc42d4ac779ebfc1d74d5de51500be18a5" => :yosemite
-    sha256 "2f69efb2f705eb1514e8b46d7daa61379df3f4892cfe2d570c233a18ff109e7d" => :mavericks
+    rebuild 1
+    sha256 "b056139401a10cdd7e5faae4f92824bc40703c5a17c4451a85d5290f9469bcec" => :high_sierra
+    sha256 "bf17f3a1d03908e182c3be7ce152baf6f8535c1d838c9470ac81523006a05ffb" => :sierra
+    sha256 "574bcb2776854172317c9fb04f06ae90d337e9dbe65ddb15e53b605f181de4b5" => :el_capitan
   end
 
   def install
