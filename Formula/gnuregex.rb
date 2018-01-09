36df3d47c62e1fb110b32a3da3f9764ffe92b672
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-09 11:04:04 +0000

    gnuregex: add 0.12 bottle.

--- Formula/gnuregex.rb
+++ Formula/gnuregex.rb
@@ -4,6 +4,13 @@ class Gnuregex < Formula
   url "https://ftp.gnu.org/old-gnu/regex/regex-0.12.tar.gz"
   sha256 "f36e2d8d56bf15054a850128fcb2f51807706a92d3bb4b37ceadd731535ce230"
 
+  bottle do
+    cellar :any
+    sha256 "86e1efa58eddf7e08d0b1298c4d4052ff0a6cc1f84fe954d1fca1605085c84db" => :high_sierra
+    sha256 "7b722b7e08f5ef14d9018f1f6b49c882d6156bf10834193f1036ad25aef22e20" => :sierra
+    sha256 "181e99ade8dc9f328cdc6bbd27a5440f34f5b35a4521765d0b58e88864d5dfa2" => :el_capitan
+  end
+
   def install
     system "./configure"
     system "make", "subdirs=test", "all"
