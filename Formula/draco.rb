f4da51aa8530ddffb8ff587cb3f3af143afb0735
Howard Butler <howard@hobu.co>
2018-01-10 11:34:30 -0600

    fix-ups to let brew audit --strict pass

--- Formula/draco.rb
+++ Formula/draco.rb
@@ -1,5 +1,5 @@
 class Draco < Formula
-  desc "Draco is a library for compressing and decompressing 3D geometric meshes and point clouds. It is intended to improve the storage and transmission of 3D graphics."
+  desc "3D geometric mesh and point cloud compression library"
   homepage "https://google.github.io/draco/"
   url "https://github.com/google/draco/archive/1.2.4.tar.gz"
   sha256 "993c78880f4bbd2bfbc7b1e16809c2f62f8ecf9040f68db75ca65be3ac34be4f"
@@ -8,11 +8,14 @@ class Draco < Formula
 
   def install
     mkdir "build" do
-    args = std_cmake_args
-    system "cmake", "..", *args
-    system "make", "install"
+      args = std_cmake_args
+      args << "-DENABLE_TESTS=ON"
+      system "cmake", "..", *args
+      system "make", "install"
     end
-
   end
 
+  test do
+    shell_output("#{bin}/draco_encoder --help", 255)
+  end
 end
