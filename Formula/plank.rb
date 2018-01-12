80711bc928c35df9c3945eba09a8373a27c46d91
Rahul Malik <rmalik@pinterest.com>
2018-01-12 10:08:49 -0800

    Address comments - Update URL to be tarball rather than git tag - Add sha256 - Remove dependencies / extra install commands

--- Formula/plank.rb
+++ Formula/plank.rb
@@ -1,15 +1,14 @@
 class Plank < Formula
-  plank_version = "v1.2"
   desc "Framework for generating immutable model objects"
   homepage "https://pinterest.github.io/plank/"
-  url "https://github.com/pinterest/plank.git", :tag => plank_version
+  url "https://github.com/pinterest/plank/archive/v1.2.tar.gz"
+  sha256 "9ee8c248d7b4488f7f952b01d913b34cd83bd40111092fd49015d1c63b9e885d"
   head "https://github.com/pinterest/plank.git"
 
   depends_on :xcode => ["9.0", :build]
-  depends_on "kylef/formulae/swiftenv" => [:build]
 
   def install
-    system "unset CC; make archive"
+    system "make", "archive"
     bin.install ".build/release/plank"
   end
 
