4c1eb632ccabe2b16032472086d894c83edb5e2d
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-13 21:17:47 +0000

    notmuch-address-cache: add 0.1 bottle.

--- Formula/notmuch-address-cache.rb
+++ Formula/notmuch-address-cache.rb
@@ -5,6 +5,13 @@ class NotmuchAddressCache < Formula
   sha256 "db43c114184f402c9baba20215f0cf57cc83b4701f034b96f0511b3c81463088"
 
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8ab34015963ff8df58ca1fe5aca05c50186dffe07527178ccf8676742fb16080" => :high_sierra
+    sha256 "8ab34015963ff8df58ca1fe5aca05c50186dffe07527178ccf8676742fb16080" => :sierra
+    sha256 "8ab34015963ff8df58ca1fe5aca05c50186dffe07527178ccf8676742fb16080" => :el_capitan
+  end
+
   def install
     bin.install "bin/notmuch-address-cache"
     man.mkpath
