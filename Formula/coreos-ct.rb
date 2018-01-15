36b8e6a96faa9757a371b97e44bc9cd7d6d58267
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-15 05:44:28 +0000

    coreos-ct: add 0.5.0 bottle.

--- Formula/coreos-ct.rb
+++ Formula/coreos-ct.rb
@@ -4,6 +4,13 @@ class CoreosCt < Formula
   url "https://github.com/coreos/container-linux-config-transpiler/archive/v0.5.0.tar.gz"
   sha256 "172e44796d39ec117584e121e73194cbcb56701407badc1899cea4607a343e15"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "96fceaa1f50ed8494b28f1fb29df27e6a3c6a1e15ee56163cc5aa36795e8dc31" => :high_sierra
+    sha256 "91aef878b6307cb9689703a9ab98124a6e8328b7b5c7ac81d65bcbe88678dbc7" => :sierra
+    sha256 "f05308a898c131eee24e8fd82b2db3c91853af9873a93ef18cbeaef9bdbc1d3b" => :el_capitan
+  end
+
   depends_on "go" => :build
 
   def install
