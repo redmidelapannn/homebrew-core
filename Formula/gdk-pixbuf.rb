62c73847bc4de6f08ba631f6c9e5c2391673f7fd
Bryce Glover <RandomDSdevel@gmail.com>
2018-01-16 18:16:22 -0500

    gdk-pixbuf: Redirect `gdk-pixbuf-query-loaders`'s output to a file.
    Save that command's output to the filesystem so that dependents requiring it to
    exist there don't barf when they can't find it.  (I _hope_ this is the correct
    approach to doing that…)
    
    Per @yetisyny's first suggestion in
    https://github.com/Homebrew/homebrew-core/issues/22768#issuecomment-357399813.

--- Formula/gdk-pixbuf.rb
+++ Formula/gdk-pixbuf.rb
@@ -84,7 +84,12 @@ class GdkPixbuf < Formula
 
   def post_install
     ENV["GDK_PIXBUF_MODULEDIR"] = "#{module_dir}/loaders"
-    system "#{bin}/gdk-pixbuf-query-loaders", "--update-cache"
+
+    File.open "#{module_dir}/loaders.cache", "w" do |loader_cache|
+      Utils.popen_read "#{bin}/gdk-pixbuf-query-loaders", "--update-cache" do |pipe|
+        loader_cache.puts pipe
+      end
+    end
   end
 
   def caveats
