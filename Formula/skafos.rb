33fece40b97aa321f084b06bb5426c28ca1b0f88
Wess Cope <wcope@me.com>
2018-01-16 16:48:31 -0500

    Fixes sha

--- Formula/skafos.rb
+++ Formula/skafos.rb
@@ -2,7 +2,7 @@ class Skafos < Formula
   desc      "Command-line tool for working with the Metis Machine platform"
   homepage  "https://metismachine.com"
   url       "https://github.com/MetisMachine/skafos/archive/1.0.tar.gz"
-  sha256    "ff62f76e8cbc9e3ccf4109a7f8cadc07bc8307ebe6d718f77de8481cef6bae31"
+  sha256    "254500f6a5bd4bee3cbfc8a7d1c8ceec1595430d5b14a69d859f9de80b8ebd16"
   head      "https://github.com/metismachine/skafos.git"
 
   bottle :unneeded
