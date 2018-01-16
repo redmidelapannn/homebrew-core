82aa9478a9499ad564d2482f214b5ee458451601
Wess Cope <wcope@me.com>
2018-01-16 15:20:54 -0500

    updates system install command

--- Formula/skafos.rb
+++ Formula/skafos.rb
@@ -2,7 +2,7 @@ class Skafos < Formula
   desc      "Command-line tool for working with the Metis Machine platform"
   homepage  "https://metismachine.com"
   url       "https://github.com/MetisMachine/skafos/archive/1.0.tar.gz"
-  sha256    "bc98f57ec136685f4171e693f569f0f0593dd0d319d3f9e1e534bb2cc2cb492c"
+  sha256    "ff62f76e8cbc9e3ccf4109a7f8cadc07bc8307ebe6d718f77de8481cef6bae31"
   head      "https://github.com/metismachine/skafos.git"
 
   bottle :unneeded
@@ -14,7 +14,7 @@ class Skafos < Formula
   def install
     ENV.deparallelize
 
-    `system "make", "homebrew"`
+    system "make", "homebrew"
     prefix.install Dir["_build/*"]
   end
 
