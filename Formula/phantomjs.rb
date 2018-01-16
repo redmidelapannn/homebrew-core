689c3099b6c7c0daac1721eb21ea29ee3b8c6e2e
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-16 06:22:03 +0000

    phantomjs: update 2.1.1 bottle.

--- Formula/phantomjs.rb
+++ Formula/phantomjs.rb
@@ -23,12 +23,10 @@ class Phantomjs < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "0634bc067a6e257d15e57bc65d0c1660fce797c005ab83990300d4b177583ae6" => :high_sierra
-    sha256 "7833bb1de72bd88057d2260a9021ce4ec0d5f2925273d537c69549d8e30ebc6d" => :sierra
-    sha256 "370e6f729ac20091c408dc5a1be14361b861bef78f1a52efb201e27e7440cfa4" => :el_capitan
-    sha256 "ec65660b5c4097886d52fe0b4928aaefd6d09fb0e6ab707b1fa4d762acf873e1" => :yosemite
-    sha256 "d837e04d137ae8ddc8eb807b7ca5a08a0fccdfd513f4fdd4f1d610ce8abc0874" => :mavericks
+    rebuild 2
+    sha256 "84b3f230ac4f28f309db4abb8bafcab4ec4a39d994c29d100614212bec57cc22" => :high_sierra
+    sha256 "ba32d5b315045912f5af02cdf64bce4edabcfe5e424a524ee4d43b1f0d42554f" => :sierra
+    sha256 "3e45b40db175e24abfa78f0b3e2c8951279f3be7a493960a461c9741e2b40938" => :el_capitan
   end
 
   depends_on MinimumMacOSRequirement => :lion
