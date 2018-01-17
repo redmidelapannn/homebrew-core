d181531dfe6c1493381850d67f6640beeea4566c
Rahul Malik <rmalik@pinterest.com>
2018-01-16 23:51:22 -0800

    Remove :head from Formula

--- Formula/plank.rb
+++ Formula/plank.rb
@@ -3,7 +3,6 @@ class Plank < Formula
   homepage "https://pinterest.github.io/plank/"
   url "https://github.com/pinterest/plank/archive/v1.2.tar.gz"
   sha256 "9ee8c248d7b4488f7f952b01d913b34cd83bd40111092fd49015d1c63b9e885d"
-  head "https://github.com/pinterest/plank.git"
 
   depends_on :xcode => ["9.0", :build]
 
