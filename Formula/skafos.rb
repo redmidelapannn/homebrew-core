0962a9854698b3b401577b4711111ae818c802d0
Wess Cope <wcope@me.com>
2018-01-17 12:05:19 -0500

    Clean up and moving docopt from shared to in project

--- Formula/skafos.rb
+++ Formula/skafos.rb
@@ -2,7 +2,7 @@ class Skafos < Formula
   desc "Command-line tool for working with the Metis Machine platform"
   homepage "https://metismachine.com/"
   url "https://github.com/MetisMachine/skafos/archive/1.0.tar.gz"
-  sha256 "254500f6a5bd4bee3cbfc8a7d1c8ceec1595430d5b14a69d859f9de80b8ebd16"
+  sha256 "f955123a0193042e654909fb0ad2446df1cabccc18ec537b8e21a1969c0cb026"
 
   depends_on "cmake" => :build
   depends_on "libarchive"
