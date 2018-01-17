516f528d71b0a51ca10ff67987d2e3dfeeb073d9
Wess Cope <wcope@me.com>
2018-01-17 09:22:24 -0500

    Made requested changes
    removed ENV.deparallelize
    
    removed bottle :unneeded
    
    removed backticks from system calls
    
    Fixed spacing
    
    added trailing slash
    
    removed head
    
    removed unused libs
    
    sorted deps
    
    used standard cmake setup with setup calls

--- Formula/skafos.rb
+++ Formula/skafos.rb
@@ -1,26 +1,26 @@
 class Skafos < Formula
-  desc      "Command-line tool for working with the Metis Machine platform"
-  homepage  "https://metismachine.com"
-  url       "https://github.com/MetisMachine/skafos/archive/1.0.tar.gz"
-  sha256    "254500f6a5bd4bee3cbfc8a7d1c8ceec1595430d5b14a69d859f9de80b8ebd16"
-  head      "https://github.com/metismachine/skafos.git"
-
-  bottle :unneeded
+  desc "Command-line tool for working with the Metis Machine platform"
+  homepage "https://metismachine.com/"
+  url "https://github.com/MetisMachine/skafos/archive/1.0.tar.gz"
+  sha256 "254500f6a5bd4bee3cbfc8a7d1c8ceec1595430d5b14a69d859f9de80b8ebd16"
 
   depends_on "cmake" => :build
-  depends_on "libgit2"
-  depends_on "yaml-cpp"
   depends_on "libarchive"
+  depends_on "yaml-cpp"
 
   def install
-    ENV.deparallelize
+    system "make", "clean"
+    system "make", "_create_version_h"
+    system "make", "_env_for_prod"
 
-    system "make", "homebrew"
-    prefix.install Dir["_build/*"]
+    mkdir "_build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
   end
 
   test do
-    `system "#{bin}/skafos", "--version"`
-    `system "#{bin}/skafos", "--help"`
+    system "#{bin}/skafos", "--version"
+    system "#{bin}/skafos", "--help"
   end
 end
