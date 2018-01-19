e757fea5b0e9b93cb8d467984882bb7330cd61aa
ilovezfs <ilovezfs@icloud.com>
2018-01-19 13:50:18 +0000

    Update hmmer.rb

--- Formula/hmmer.rb
+++ Formula/hmmer.rb
@@ -1,6 +1,6 @@
 class Hmmer < Formula
   desc "Build profile HMMs and scan against sequence databases"
-  homepage "http://hmmer.janelia.org"
+  homepage "http://hmmer.org/"
   url "http://eddylab.org/software/hmmer3/3.1b2/hmmer-3.1b2.tar.gz"
   sha256 "dd16edf4385c1df072c9e2f58c16ee1872d855a018a2ee6894205277017b5536"
   revision 2
