1258311dee421679d23e7a626e52487016949abb
Sebastian Höffner <info@sebastian-hoeffner.de>
2018-01-20 01:40:08 +0100

    caffe 1.0 (new formula)

---
+++ Formula/caffe.rb
@@ -0,0 +1,56 @@
+class Caffe < Formula
+  desc "Fast open framework for deep learning"
+  homepage "http://caffe.berkeleyvision.org/"
+  url "https://github.com/BVLC/caffe/archive/1.0.tar.gz"
+  sha256 "71d3c9eb8a183150f965a465824d01fe82826c22505f7aa314f700ace03fa77f"
+
+  depends_on "cmake" => :build
+  depends_on "boost"
+  depends_on "gflags"
+  depends_on "glog"
+  depends_on "hdf5"
+  depends_on "leveldb"
+  depends_on "lmdb"
+  depends_on "opencv"
+  depends_on "protobuf"
+  depends_on "python"
+  depends_on "snappy"
+  depends_on "szip"
+
+  resource "test_model_weights" do
+    url "http://dl.caffe.berkeleyvision.org/bvlc_reference_caffenet.caffemodel"
+    sha256 "472d4a06035497b180636d8a82667129960371375bd10fcb6df5c6c7631f25e0"
+  end
+
+  def install
+    args = std_cmake_args + %w[
+      -DCPU_ONLY=ON
+      -DUSE_NCCL=OFF
+      -DBUILD_SHARED_LIBS=ON
+      -DBUILD_python=OFF
+      -DBUILD_matlab=OFF
+      -DBUILD_docs=OFF
+      -DBUILD_python_layer=OFF
+      -DUSE_OPENCV=ON
+      -DUSE_LEVELDB=ON
+      -DUSE_LMDB=ON
+      -DALLOW_LMDB_NOLOCK=OFF
+      -DUSE_OPENMP=OFF
+    ]
+
+    system "cmake", ".", *args
+    system "make", "install"
+    pkgshare.install "models"
+  end
+
+  test do
+    model = "bvlc_reference_caffenet"
+    m_path = "#{pkgshare}/models/#{model}"
+    resource("test_model_weights").stage do
+      system "#{bin}/caffe", "test",
+             "-model", "#{m_path}/deploy.prototxt",
+             "-solver", "#{m_path}/solver.prototxt",
+             "-weights", "#{model}.caffemodel"
+    end
+  end
+end
