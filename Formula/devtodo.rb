0b54b5e42bbe2921c07cf380f0c6fe02ec475642
Utkarsh Tripathi <f2016894@pilani.bits-pilani.ac.in>
2018-01-20 07:13:19 +0530

    Update devtodo.rb

--- Formula/devtodo.rb
+++ Formula/devtodo.rb
@@ -30,6 +30,20 @@ class Devtodo < Formula
     system "make", "install"
     doc.install "contrib"
   end
+
+  test do
+    (testpath/"test").write <<~EOS
+      #!/usr/bin/expect -f
+      spawn devtodo --add HomebrewWork
+      expect "priority*"
+      send -- "2\r"
+      expect eof
+      EOS
+    chmod 0755, testpath/"test"
+    system "./test"
+    output = shell_output("grep HomebrewWork #{testpath}/.todo")
+    assert_match "HomebrewWork", output
+  end
 end
 
 __END__
