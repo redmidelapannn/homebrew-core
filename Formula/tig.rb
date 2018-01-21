4831ed5bc3271eef32d7eb7abf92fae400b986b3
ilovezfs <ilovezfs@icloud.com>
2018-01-21 11:43:03 +0000

    Update tig.rb

--- Formula/tig.rb
+++ Formula/tig.rb
@@ -13,9 +13,10 @@ class Tig < Formula
 
   head do
     url "https://github.com/jonas/tig.git"
+
+    depends_on "asciidoc" => :build
     depends_on "autoconf" => :build
     depends_on "automake" => :build
-    depends_on "asciidoc" => :build
     depends_on "xmlto" => :build
   end
 
