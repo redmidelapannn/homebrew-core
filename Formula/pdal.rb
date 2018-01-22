ed03d704a6753280e2cfdaf698a3d14029f2d769
Howard Butler <howard@hobu.co>
2018-01-22 11:37:04 -0600

    More feedback from fxcoudert
    * WITH_LASZIP was lost
    * simple cmake invocation

--- Formula/pdal.rb
+++ Formula/pdal.rb
@@ -19,16 +19,16 @@ class Pdal < Formula
   depends_on "hdf5"
 
   def install
-    args = std_cmake_args
 
-    args << "-DBUILD_PLUGIN_PGPOINTCLOUD=ON" \
-         << "-DBUILD_PLUGIN_GREYHOUND=ON" \
-         << "-DBUILD_PLUGIN_PYTHON=ON" \
-         << "-DBUILD_PLUGIN_PCL=ON" \
-         << "-DBUILD_PLUGIN_ICEBRIDGE=ON" \
-         << "-DBUILD_PLUGIN_SQLITE=ON"
+    system "cmake", ".", *std_cmake_args,
+                         "-DWITH_LASZIP=TRUE"
+                         "-DBUILD_PLUGIN_PGPOINTCLOUD=ON"
+                         "-DBUILD_PLUGIN_GREYHOUND=ON"
+                         "-DBUILD_PLUGIN_PYTHON=ON"
+                         "-DBUILD_PLUGIN_PCL=ON"
+                         "-DBUILD_PLUGIN_ICEBRIDGE=ON"
+                         "-DBUILD_PLUGIN_SQLITE=ON"
 
-    system "cmake", ".", *args
     system "make", "install"
     doc.install "examples", "test"
   end
