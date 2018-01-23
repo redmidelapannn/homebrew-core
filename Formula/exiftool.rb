718495989b2b7564402553546e1dc7e24086e04b
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-23 05:46:32 +0000

    exiftool: update 10.55_1 bottle.

--- Formula/exiftool.rb
+++ Formula/exiftool.rb
@@ -10,9 +10,10 @@ class Exiftool < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "5039ab4f2bc31d2afc9b4cd1fab9ab5d6deec2bf2d1bba0a95f4d54e747dd117" => :high_sierra
-    sha256 "5039ab4f2bc31d2afc9b4cd1fab9ab5d6deec2bf2d1bba0a95f4d54e747dd117" => :sierra
-    sha256 "5039ab4f2bc31d2afc9b4cd1fab9ab5d6deec2bf2d1bba0a95f4d54e747dd117" => :el_capitan
+    rebuild 1
+    sha256 "822922947e54dbfa28592767f6e85b9389994a402cf8818d395eb0d402f6bf51" => :high_sierra
+    sha256 "822922947e54dbfa28592767f6e85b9389994a402cf8818d395eb0d402f6bf51" => :sierra
+    sha256 "822922947e54dbfa28592767f6e85b9389994a402cf8818d395eb0d402f6bf51" => :el_capitan
   end
 
   devel do
