d3eb600674f0883e1d61cb0277fa681727246408
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-24 09:45:14 +0000

    kirimori: add 0.0.3 bottle.

--- Formula/kirimori.rb
+++ Formula/kirimori.rb
@@ -5,6 +5,13 @@ class Kirimori < Formula
   sha256 "5f9a0f1b30f888137201b4ba4558ba7e633855a53d53b6058f386521838aee0f"
   head "https://github.com/syossan27/kirimori.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "3a0015e1e8b40fd3c5473750137cf00e396b73e0b27bf1d0b8aa909ffa50795c" => :high_sierra
+    sha256 "da1bb1ea339896500e57d746730446ebf7e09ccfd93431b0409a5466f0bc8e5c" => :sierra
+    sha256 "f83a04976d92e02cce52c86dcedbf0398e5365defc5ac0e40827976f17d138b4" => :el_capitan
+  end
+
   depends_on "dep" => :build
   depends_on "go" => :build
 
