25898b262b11c2e24dfd830c8353594db9240220
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-24 08:35:48 +0000

    openctm: add 1.0.3 bottle.

--- Formula/openctm.rb
+++ Formula/openctm.rb
@@ -4,6 +4,13 @@ class Openctm < Formula
   url "https://download.sourceforge.net/project/openctm/OpenCTM-1.0.3/OpenCTM-1.0.3-src.tar.bz2"
   sha256 "4a8d2608d97364f7eec56b7c637c56b9308ae98286b3e90dbb7413c90e943f1d"
 
+  bottle do
+    cellar :any
+    sha256 "1dd922f975a48413dc2a577c99dedb8027bf2f737c4395e3e0b66bbb876e03b2" => :high_sierra
+    sha256 "b5741828382a83e19de79976535fa4c0bfb3c6e2a68a53f7d8d0580f77830592" => :sierra
+    sha256 "962a7a9552d3f34b6fdf32603ecf600d5f5fea937af547fc878891b3db34ead5" => :el_capitan
+  end
+
   depends_on "gtk+"
 
   def install
