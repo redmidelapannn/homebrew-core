436e4907c7af51ff381e7a6f31c443d578680aa1
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-27 20:44:11 +0000

    splint: update 3.1.2 bottle.

--- Formula/splint.rb
+++ Formula/splint.rb
@@ -6,11 +6,10 @@ class Splint < Formula
   sha256 "c78db643df663313e3fa9d565118391825dd937617819c6efc7966cdf444fb0a"
 
   bottle do
-    sha256 "4d97939d58a64670256ea18422ae7ddd75ea1d12e89d12f40faa0b54e6d9d3a8" => :high_sierra
-    sha256 "e5847a77e137e1f2339b55ae1fff93a94de33c6ad1a3a34c8a45b3d06a6bf0f9" => :sierra
-    sha256 "9eac9f8e530c1d9fc238b57f9d4e143fbf5727450657ba92e6d721660777753b" => :el_capitan
-    sha256 "4b385e4fcf9b82fa2ebd8dabaef7e712039b3f7c83d2f6d5e3263ebf51e7b6d7" => :yosemite
-    sha256 "ad8551b508f303c69499a60456de49d2b77d1f0f2383383d3c01c1b657a230b6" => :mavericks
+    rebuild 1
+    sha256 "da8a641966635b210f3975ce2a31c9d4657314382f747489f943c2accda1fe7e" => :high_sierra
+    sha256 "36c7d8f1ffb2656f074ecc79d346128e9d1b4aae5fea810244dc2a4ce0cc6b11" => :sierra
+    sha256 "9f5e377de80f6595d8e842aa029f9f7ba7b15077259a87b651aad2ce4ff2c2f5" => :el_capitan
   end
 
   # fix compiling error of osd.c
