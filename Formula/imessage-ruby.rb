4aab30ff65495c2d833b6ba66903f87628f525d7
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-27 14:42:33 +0000

    imessage-ruby: update 0.3.1 bottle.

--- Formula/imessage-ruby.rb
+++ Formula/imessage-ruby.rb
@@ -7,11 +7,10 @@ class ImessageRuby < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e287b21ce1694d5ec9c5376fb142232b2df72fb907b12cb5b0ff22bd2fc04ab2" => :high_sierra
-    sha256 "446892e091382593a46ee69b8fb01354f1cc363a97b8a967332553a577bab8f6" => :sierra
-    sha256 "0e7fd4f055a6ba4e81273a5952504ceb74b835387c144a24e61f020e55e6018e" => :el_capitan
-    sha256 "1024e6cee26ed9fc8ae4ef1941edc64d0e6d16006bd2a530d5644e7c00f8a350" => :yosemite
-    sha256 "7b546ccf5cf13a7d474c635a57eebc8e74ff61ea6c7c3cdfefffe4c78737ab47" => :mavericks
+    rebuild 1
+    sha256 "302495acccd21891e9c25aaca5d419e956a05d27a3000ecca8652bc43f22bc98" => :high_sierra
+    sha256 "302495acccd21891e9c25aaca5d419e956a05d27a3000ecca8652bc43f22bc98" => :sierra
+    sha256 "302495acccd21891e9c25aaca5d419e956a05d27a3000ecca8652bc43f22bc98" => :el_capitan
   end
 
   depends_on :macos => :mavericks
