8cb8e1c1aa15ee346c3e70c235804cc59b87ac0d
ilovezfs <ilovezfs@icloud.com>
2018-01-27 07:36:21 +0000

    Update ice.rb

--- Formula/ice.rb
+++ Formula/ice.rb
@@ -3,7 +3,7 @@ class Ice < Formula
   homepage "https://zeroc.com"
   url "https://github.com/zeroc-ice/ice/archive/v3.7.0.tar.gz"
   sha256 "809fff14a88a7de1364c846cec771d0d12c72572914e6cc4fb0b2c1861c4a1ee"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any
