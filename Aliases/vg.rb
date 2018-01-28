6021d0b65e5433a674da90ea8d7e4e410e2daf59
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2018-01-27 17:53:56 -0800

    virtualgo 0.8.0 (new formula)

---
+++ Aliases/vg.rb
@@ -0,0 +1,1 @@
+../Formula/virtualgo.rb
---
+++ Formula/virtualgo.rb
@@ -0,0 +1,26 @@
+class Virtualgo < Formula
+  desc "Easy and powerful workspace based development for Go"
+  homepage "https://github.com/GetStream/vg"
+  url "https://github.com/GetStream/vg/archive/v0.8.0.tar.gz"
+  sha256 "e7267b1306c36b3a4a4d10cb9e7ece66a9436a430bbf15028b82313496c92ebe"
+
+  depends_on "go" => :build
+  depends_on "dep" => :build
+  depends_on "bindfs" => :recommended
+
+  def install
+    ENV["GOPATH"] = buildpath
+    vgpath = buildpath/"src/github.com/GetStream/vg"
+    vgpath.install buildpath.children
+    cd vgpath do
+      system "dep", "ensure"
+      system "go", "build", "-o", bin/"vg",
+        "-ldflags", "-w -s -X github.com/GetStream/vg/cmd.Version=#{version}"
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/vg version")
+  end
+end
