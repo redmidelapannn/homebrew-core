26df04cf45b636648c23ae568966e4d1578f0afd
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2018-01-27 16:09:35 -0800

    Fix output test

--- Formula/coreos-ct.rb
+++ Formula/coreos-ct.rb
@@ -12,8 +12,6 @@ class CoreosCt < Formula
   end
 
   test do
-    # assert_equal "ct ", shell_output("#{bin}/ct -version").chomp
-
     (testpath/"input").write <<~EOS
       passwd:
         users:
@@ -21,30 +19,7 @@ class CoreosCt < Formula
             ssh_authorized_keys:
               - ssh-rsa mykey
       EOS
-    assert_equal test_output.strip, shell_output("#{bin}/ct -pretty -in-file #{testpath}/input").strip
-  end
-
-  def test_output; <<~EOS
-    {
-      "ignition": {
-        "config": {},
-        "timeouts": {},
-        "version": "2.1.0"
-      },
-      "networkd": {},
-      "passwd": {
-        "users": [
-          {
-            "name": "core",
-            "sshAuthorizedKeys": [
-              "ssh-rsa mykey"
-            ]
-          }
-        ]
-      },
-      "storage": {},
-      "systemd": {}
-    }
-    EOS
+    output = shell_output("#{bin}/ct -pretty -in-file #{testpath}/input")
+    assert_match /.*"sshAuthorizedKeys":\s*["ssh-rsa mykey"\s*].*/m, output.strip
   end
 end
