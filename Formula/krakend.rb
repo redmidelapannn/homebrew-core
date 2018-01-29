1826f105687af5aa25f7d2fe2c1b668f27ba003b
ilovezfs <ilovezfs@icloud.com>
2018-01-29 13:21:17 +0000

    Update krakend.rb

--- Formula/krakend.rb
+++ Formula/krakend.rb
@@ -1,11 +1,11 @@
 class Krakend < Formula
   desc "Ultra-High performance API Gateway built in Go"
-  homepage "http://krakend.io"
+  homepage "http://krakend.io/"
   url "https://github.com/devopsfaith/krakend-ce/archive/0.4.0.tar.gz"
   sha256 "c0e48b0e8234b1d7975c3dfa953f3c7568e4f1583c5ffb3fef95ee236a902afd"
 
-  depends_on "go" => :build
   depends_on "dep" => :build
+  depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = buildpath
@@ -14,6 +14,7 @@ class Krakend < Formula
       system "make", "deps"
       system "make", "build"
       bin.install "krakend"
+      prefix.install_metafiles
     end
   end
 
