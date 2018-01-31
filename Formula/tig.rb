509e04394c8fdcd57d3b4a39fb206ad71ccb1915
ilovezfs <ilovezfs@icloud.com>
2018-01-31 05:57:51 -0800

    Update tig.rb

--- Formula/tig.rb
+++ Formula/tig.rb
@@ -1,8 +1,8 @@
 class Tig < Formula
   desc "Text interface for Git repositories"
   homepage "https://jonas.github.io/tig/"
-  url "https://github.com/jonas/tig/archive/tig-2.3.3.tar.gz"
-  sha256 "18a5a9c05735f3e457ac0c74ab6663b9e6087148d975c45f3f88bebf07a718ff"
+  url "https://github.com/jonas/tig/releases/download/tig-2.3.3/tig-2.3.3.tar.gz"
+  sha256 "d39d10c5a6db7b7663d51eff8ac2eaf075e319f5edabe09cb63a2b1b24846bea"
 
   bottle do
     sha256 "31bc0f57539393ca38dfe8e60c5a67bd2844e36f934c9c15aaa635cb413c23ee" => :high_sierra
