16c3b0171380e62cf93b0ba5588c04b12ac24209
BrewTestBot <brew-test-bot@googlegroups.com>
2018-01-31 15:46:34 +0000

    abnfgen: update 0.18_1 bottle.

--- Formula/abnfgen.rb
+++ Formula/abnfgen.rb
@@ -7,9 +7,9 @@ class Abnfgen < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "fb10a0bd2410bcd5ecb53ee7c39798b1a8d87a8ac69ee11de3444c1f0af458d2" => :high_sierra
-    sha256 "31c28287fdf41f1c5767a267343c387b0208e615da3616877c8c6a1c694974af" => :sierra
-    sha256 "ac1d3849426bec85a2b5eaba4bac04111aed9c118fc9c76278ffeba2b42cd016" => :el_capitan
+    sha256 "c1757fae2d0ab7ccc095882f26b691201a31421a44f37da6408fcf5b209db4fb" => :high_sierra
+    sha256 "aeeaddecbca233e5148ad90deab851c2047d4fee59a2ec14a8dbe5c8a810f84e" => :sierra
+    sha256 "3334426d2ce99d9ba09a013d1e82e78c042c28abf7f4497ce12a653ddd7b92a5" => :el_capitan
   end
 
   def install
