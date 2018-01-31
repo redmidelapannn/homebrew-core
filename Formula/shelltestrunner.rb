e9c61181da45117edd0b50c9883817fb5d12d54b
taavi.valjaots <taavi.valjaots@guardtime.com>
2018-01-31 10:25:21 +0200

    Review fix.

--- Formula/shelltestrunner.rb
+++ Formula/shelltestrunner.rb
@@ -5,21 +5,19 @@ class Shelltestrunner < Formula
 
   desc "Portable command-line tool for testing command-line programs"
   homepage "https://github.com/simonmichael/shelltestrunner"
-  url "https://github.com/simonmichael/shelltestrunner/archive/1.9.tar.gz"
+  url "https://hackage.haskell.org/package/shelltestrunner-1.9/shelltestrunner-1.9.tar.gz"
   sha256 "066b0f41ee180cf4acc548cdb26034b6cda0c6c81b22f608b0c3f0a93bfcd9c9"
 
-  bottle do
-    sha256 "58d77951c7c82d120f496a991d0bb5277a0c7c3974bb525f30c9a26ac5a677bb" => :sierra
-  end
-
-  depends_on "ghc" => :build
   depends_on "cabal-install" => :build
+  depends_on "ghc" => :build
 
   def install
     install_cabal_package :using => ["happy"]
   end
 
   test do
-    system "#{bin}/shelltest", "--version"
+    (testpath/"shelltestrunner.test").write("$$$ {exe} {in}\n>>> /{out}/\n>>>= 0")
+
+    system "#{bin}/shelltest", "-D{exe}=echo", "-D{in}=My message", "-D{out}=My message", "-D{doNotExist}=null", "shelltestrunner.test"
   end
 end
