c9140e1182e847789ef9906d0bff32d1160e3168
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-01 21:52:06 +0000

    rbspy: add 0.1.8 bottle.

--- Formula/rbspy.rb
+++ Formula/rbspy.rb
@@ -4,6 +4,12 @@ class Rbspy < Formula
   url "https://github.com/rbspy/rbspy/archive/v0.1.8.tar.gz"
   sha256 "87f759acd9d660178737b9f24cc07f0113a81a9fffc1604bae2c756d7f4d815b"
 
+  bottle do
+    sha256 "dc4e8536412380b155edf14b41f66379596c1c389b73610edd220b6c2697091d" => :high_sierra
+    sha256 "06979fcc8f24807dd22221a82a5e91113e334baeac2b0fa2f91294df795ead67" => :sierra
+    sha256 "4beee080f596226972d8d0333c89cffd9ea1e2cb5e1ea2d1abfb3972f20fe7a6" => :el_capitan
+  end
+
   depends_on "rust" => :build
 
   def install
