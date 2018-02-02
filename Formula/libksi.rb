46b9d449745cfe39ecdef5525585b7b1b9602dd4
taavi.valjaots <taavi.valjaots@guardtime.com>
2018-02-02 08:27:28 +0200

    Formula for libksi added.

---
+++ Formula/libksi.rb
@@ -0,0 +1,36 @@
+class Libksi < Formula
+  desc "C SDK for Keyless Signature Infrastructure (c) Guardtime"
+  homepage "https://github.com/guardtime/libksi"
+  url "https://github.com/guardtime/libksi/archive/v3.16.2482.tar.gz"
+  sha256 "a21c6ccdc432ec421df8977948c67bc4d62b50741b59f016c35a1aaf6767ee57"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "openssl"
+
+  def install
+    system "autoreconf", "-if"
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <ksi/ksi.h>
+      #include <assert.h>
+      int main()
+      {
+        KSI_CTX *ksi = NULL;
+        assert(KSI_CTX_new(&ksi) == KSI_OK);
+        KSI_CTX_free(ksi);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}",
+                   "-lksi", "-o", "test"
+    system "./test"
+  end
+end
