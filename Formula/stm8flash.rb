caeaf9f4672da0148ce083c5632ba1a1357977e9
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-02 03:55:22 +0000

    stm8flash: add 20170616-1.1 bottle.

--- Formula/stm8flash.rb
+++ Formula/stm8flash.rb
@@ -3,6 +3,13 @@ class Stm8flash < Formula
   homepage "https://github.com/vdudouyt/stm8flash"
   url "https://github.com/vdudouyt/stm8flash.git", :revision => "6007cb7062bb86d4c17c79174ee177e80939f602"
   version "20170616-1.1"
+  bottle do
+    cellar :any
+    sha256 "602346cf8080d6b6214e680870bf40b11926287808bb5eb3b6360394816d29ee" => :high_sierra
+    sha256 "47e737521fd980553f021b58f32fb933ab0fd63a3b2b1b2330338e15bd5e681a" => :sierra
+    sha256 "011b12071ba9ebc54485c487c1e3351341b78381fb72e3ed9b9a676df02042f1" => :el_capitan
+  end
+
   patch :DATA
   depends_on "libusb"
   depends_on "pkg-config" => :build
