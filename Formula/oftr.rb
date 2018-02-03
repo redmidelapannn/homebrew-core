55ca019f34a40fefa9f116aaab09eadafb619ea2
Bill Fisher <william.w.fisher@gmail.com>
2018-02-03 16:54:26 -0700

    oftr 0.45.0 (new formula)

---
+++ Formula/oftr.rb
@@ -0,0 +1,30 @@
+class Oftr < Formula
+  desc "OpenFlow to YAML Translator and Microservice"
+  homepage "https://github.com/byllyfish/oftr"
+  url "https://launchpad.net/~byllyfish/+archive/ubuntu/oftr/+files/oftr_0.45.0.orig.tar.gz"
+  sha256 "d54cdf84bf54478f51cea56dba2a42f88642031da84374a7662c1a215ab0c115"
+  head "https://github.com/byllyfish/oftr.git"
+
+  bottle do
+    root_url "https://dl.bintray.com/byllyfish/bottles"
+    cellar :any_skip_relocation
+    sha256 "13d992194d8b97827905c8d9e2c159e382b299deb77b503657a9e3c2620e78a3" => :high_sierra
+  end
+
+  depends_on "cmake" => :build
+  depends_on "go" => :build
+
+  def install
+    args = std_cmake_args
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"hello.yml").write("type: HELLO")
+    system "#{bin}/oftr", "encode", "--hex", "--ofversion=4", "hello.yml"
+  end
+end
