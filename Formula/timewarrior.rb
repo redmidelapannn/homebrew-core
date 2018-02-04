2dd48cd2b7805e2958bf86bda74a62561f1619a9
Federico Hernandez <f@ederi.co>
2018-02-04 10:25:57 +0100

    Software moved from own git server to github

--- Formula/timewarrior.rb
+++ Formula/timewarrior.rb
@@ -1,9 +1,9 @@
 class Timewarrior < Formula
   desc "Command-line time tracking application"
   homepage "https://taskwarrior.org/docs/timewarrior/"
-  url "https://taskwarrior.org/download/timew-1.0.0.tar.gz"
-  sha256 "ac027910e1e8365bdd218a8b42389b26d017d38d3c96516c408db6d5a44e0bb5"
-  head "https://git.tasktools.org/scm/tm/timew.git"
+  url "https://taskwarrior.org/download/timew-1.1.1.tar.gz"
+  sha256 "1f7d9a62e55fc5a3126433654ccb1fd7d2d135f06f05697f871897c9db77ccc9"
+  head "https://github.com/GothenburgBitFactory/timewarrior.git"
 
   bottle do
     cellar :any_skip_relocation
