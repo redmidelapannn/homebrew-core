4e8fc7c7b17babe263066b85d99c701e575ff312
David Creemer <david@zachary.com>
2018-02-04 18:22:55 -0800

    app agilebits 1passworld cli

---
+++ Formula/op.rb
@@ -0,0 +1,15 @@
+class Op < Formula
+  desc "CLI for 1password.com"
+  homepage "https://support.1password.com/command-line-getting-started/"
+  url "https://cache.agilebits.com/dist/1P/op/pkg/v0.2.1/op_darwin_amd64_v0.2.1.zip"
+  sha256 "fd15c2c0e429623e3d0bff17f7f94867cd5d8b55d10f69076c2ec277da755d77"
+
+  def install
+    bin.install "op"
+  end
+
+  test do
+    # 'op -version' exits with status 1, rather than 0
+    assert_match "#{version}", shell_output("#{bin}/op --version", 1)
+  end
+end
