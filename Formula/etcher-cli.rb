ff4034230f3fbc456aa0960e74cde2f88c45e3ed
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-07 21:12:58 +0000

    etcher-cli: add 1.3.1 bottle.

--- Formula/etcher-cli.rb
+++ Formula/etcher-cli.rb
@@ -5,6 +5,12 @@ class EtcherCli < Formula
   version "1.3.1"
   sha256 "6e862e2d2e64c264fb90ec409d82b43c1719453f5ed1cff18aa31b2d890ac6c7"
 
+  bottle do
+    sha256 "260a645183553f52d2162d88839da96628f49d90b590099628aaa338fb450598" => :high_sierra
+    sha256 "260a645183553f52d2162d88839da96628f49d90b590099628aaa338fb450598" => :sierra
+    sha256 "260a645183553f52d2162d88839da96628f49d90b590099628aaa338fb450598" => :el_capitan
+  end
+
   def install
     libexec.install Dir["*"]
     bin.install_symlink Dir["#{libexec}/etcher"]
