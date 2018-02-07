620ecb358204fd8fe578b8fa0cb7100d69b78806
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-07 21:40:58 +0000

    grv: add 0.1.2 bottle.

--- Formula/grv.rb
+++ Formula/grv.rb
@@ -5,6 +5,13 @@ class Grv < Formula
   sha256 "81f06796b94791b2b4e10e5c92dabd91bcebab185747446c8f2974539e4ef001"
   head "https://github.com/rgburke/grv.git"
 
+  bottle do
+    cellar :any
+    sha256 "d852055653b9bd2c6950bea13231d07b9db2cd57c7bb115f09a60a9806db2062" => :high_sierra
+    sha256 "5e78fd3be501b65e72d4076bd599127ab1fe5eaa71a76d7009f8d6e5e1fd95d8" => :sierra
+    sha256 "4803b6c7cbdb34ad16597de9b654c168a78dc985bc30110d4516f73a90310fc8" => :el_capitan
+  end
+
   depends_on "go" => :build
   depends_on "pkg-config" => :build
   depends_on "cmake" => :build
