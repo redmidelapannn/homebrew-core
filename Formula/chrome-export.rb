8fe308333a2332630c42be65a614f6ccf52657a2
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-07 15:57:16 +0000

    chrome-export: add 2.0 bottle.

--- Formula/chrome-export.rb
+++ Formula/chrome-export.rb
@@ -4,6 +4,13 @@ class ChromeExport < Formula
   url "https://github.com/bdesham/chrome-export/archive/v2.0.tar.gz"
   sha256 "ff972c3d88b030b65823388e08cd65ebd8795f0674b5581e9e8fdf3a0f4dc7ef"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "3a993254e0e1bf36f442d4e7010dcff164ae79ba2820cfc3a2e6a1290b62c99b" => :high_sierra
+    sha256 "3a993254e0e1bf36f442d4e7010dcff164ae79ba2820cfc3a2e6a1290b62c99b" => :sierra
+    sha256 "3a993254e0e1bf36f442d4e7010dcff164ae79ba2820cfc3a2e6a1290b62c99b" => :el_capitan
+  end
+
   depends_on "python" if MacOS.version <= :snow_leopard
 
   def install
