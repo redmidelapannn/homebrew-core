194811450dfa5fb3cd8f641fb96ff36e324e3a1b
Richard Burke <rich.g.burke@gmail.com>
2018-02-07 21:24:05 +0000

    grv 0.1.2 (new formula)

---
+++ Formula/grv.rb
@@ -0,0 +1,27 @@
+class Grv < Formula
+  desc "Terminal interface for viewing git repositories"
+  homepage "https://github.com/rgburke/grv"
+  url "https://github.com/rgburke/grv/releases/download/v0.1.2/grv-0.1.2-src.tar.gz"
+  sha256 "1f2f8adb28085fd892f4b23cf5cf17925fc502cf479be35193e1c6b93cad1e49"
+  head "https://github.com/rgburke/grv.git"
+
+  depends_on "go" => :build
+  depends_on "pkg-config" => :build
+  depends_on "cmake" => :build
+  depends_on "readline"
+
+  def install
+    ENV["GOPATH"] = buildpath
+    path = buildpath/"src/github.com/rgburke/grv"
+    path.install buildpath.children
+
+    cd path do
+      system "make", "build-only"
+      bin.install "grv"
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/grv -version")
+  end
+end
