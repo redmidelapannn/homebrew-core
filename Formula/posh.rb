97dd9526a7d7a4ab65d3f2f70054f3296f0fa881
Dan Kaplun <dbkaplun@gmail.com>
2018-02-08 19:25:20 -0500

    PR comments

--- Formula/posh.rb
+++ Formula/posh.rb
@@ -4,18 +4,12 @@ class Posh < Formula
   url "https://salsa.debian.org/clint/posh/repository/debian/0.13.1/archive.tar.gz"
   sha256 "c2c10db047294309a109f09e0c76a0cdc33af39563c847b84149b9ac5210f115"
 
-  head "https://salsa.debian.org/clint/posh.git"
-
   depends_on "autoconf" => :build
   depends_on "automake" => :build
 
   def install
-    system "aclocal"
-    system "autoconf"
-    system "autoheader"
-    system "automake", "--add-missing"
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
+    system "autoreconf", "-fiv"
+    system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
     system "make", "install"
