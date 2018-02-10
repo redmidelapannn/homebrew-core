eb6684bfa69359bcbc0179d7e2cfee36713df341
Jeongseok Lee <jslee02@gmail.com>
2018-02-10 14:51:29 -0800

    Address @fxcoudert's comments

--- Formula/urdfdom.rb
+++ Formula/urdfdom.rb
@@ -1,13 +1,15 @@
 class Urdfdom < Formula
-  desc "URDF parser"
+  desc "Unified Robot Description Format (URDF) parser"
   homepage "https://wiki.ros.org/urdf"
   url "https://github.com/ros/urdfdom/archive/1.0.0.tar.gz"
   sha256 "243ea925d434ebde0f9dee35ee5615ecc2c16151834713a01f85b97ac25991e1"
 
   depends_on "cmake" => :build
-  depends_on "tinyxml"
   depends_on "console_bridge"
+  depends_on "tinyxml"
   depends_on "urdfdom_headers"
+
+  needs :cxx11
 
   def install
     system "cmake", ".", *std_cmake_args
@@ -29,7 +31,8 @@ class Urdfdom < Formula
         return 0;
       }
     EOS
-    system ENV.cc, "test.cpp", "-L#{lib}", "-lurdfdom_world", "-lc++", "-std=c++11", "-o", "test"
+    system ENV.cxx11, "test.cpp", "-L#{lib}", "-lurdfdom_world", "-std=c++11",
+                      "-o", "test"
     system "./test"
   end
 end
