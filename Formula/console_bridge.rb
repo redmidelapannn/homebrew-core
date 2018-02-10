fe740d49be0260729657c13308ce1c16a7489a51
Jeongseok Lee <jslee02@gmail.com>
2018-02-10 14:55:46 -0800

    Update desc and homepage

--- Formula/console_bridge.rb
+++ Formula/console_bridge.rb
@@ -1,6 +1,6 @@
 class ConsoleBridge < Formula
-  desc "ROS-independent package for logging"
-  homepage "https://wiki.ros.org/console_bridge"
+  desc "ROS(Robot Operating System)-independent package for logging"
+  homepage "https://wiki.ros.org/console_bridge/"
   url "https://github.com/ros/console_bridge/archive/0.3.2.tar.gz"
   sha256 "fd12e48c672cb9c5d516d90429c4a7ad605859583fc23d98258c3fa7a12d89f4"
 
