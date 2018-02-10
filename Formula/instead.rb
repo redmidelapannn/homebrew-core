083d3179b960390ef6258506ecf9429ccb8b72e5
ilovezfs <ilovezfs@icloud.com>
2018-02-10 01:30:13 -0800

    instead 3.2.0
    depend on sdl2, sdl2_image, sdl2_mixer and sdl2_ttf instead of sdl,
    sdl_image, sdl_mixer and sdl_tff

--- Formula/instead.rb
+++ Formula/instead.rb
@@ -1,9 +1,8 @@
 class Instead < Formula
   desc "Interpreter of simple text adventures"
   homepage "https://instead.syscall.ru/"
-  url "https://github.com/instead-hub/instead/archive/3.1.2.tar.gz"
-  sha256 "622c04a58fd780d8efdf0706c03596ab68672b31e97865dad6a1fc1540619754"
-  revision 1
+  url "https://github.com/instead-hub/instead/archive/3.2.0.tar.gz"
+  sha256 "ed25f8efcebe1b33401704ff1c0b9151ca08e799a2f840b10ebfd2f0a0bf3f7a"
   head "https://github.com/instead-hub/instead.git"
 
   bottle do
@@ -14,12 +13,14 @@ class Instead < Formula
 
   depends_on "cmake" => :build
   depends_on "lua"
-  depends_on "sdl"
-  depends_on "sdl_image"
-  depends_on "sdl_mixer"
-  depends_on "sdl_ttf"
+  depends_on "sdl2"
+  depends_on "sdl2_image"
+  depends_on "sdl2_mixer"
+  depends_on "sdl2_ttf"
 
   def install
+    ENV["LDFLAGS"] = "-framework AppKit"
+
     mkdir "build" do
       system "cmake", "..", "-DWITH_GTK2=OFF", *std_cmake_args
       system "make", "install"
