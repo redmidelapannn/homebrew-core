4eedfded1758532c7f4d3beaa5d288519f17ccd3
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-11 08:53:51 +0000

    io: update 2017.09.06 bottle.

--- Formula/io.rb
+++ Formula/io.rb
@@ -6,10 +6,10 @@ class Io < Formula
   head "https://github.com/stevedekorte/io.git"
 
   bottle do
-    sha256 "686d5d23790b53c27765d49da0a74ec96ee949353b31646a0a92ee931270a23d" => :high_sierra
-    sha256 "2d0e05344917ad3a1d322f2860030013315ceb7e8ae962cf6070d1ee8cc395d4" => :sierra
-    sha256 "3a5a0e9a1ec0ce7f4bc6bcfc5fb8c782f0b1ba0451251aaab51a796452b59e67" => :el_capitan
-    sha256 "16d31a7062e2c7ebab815bcd48b03aab9597a6c40071cb407e2bc6dec91fef0b" => :yosemite
+    rebuild 1
+    sha256 "d7fe003b6f14045a76695c777172681e37575a9ba09750715899ad4f29f6a679" => :high_sierra
+    sha256 "8f8af5355fb393f945ab8203624bb3a3085da9a141dbfc3cbdfe48e8bc48fd38" => :sierra
+    sha256 "88ecbd8f780934304d99f2ec60322f76f1014268231474c68a24c21ad30b551a" => :el_capitan
   end
 
   option "without-addons", "Build without addons"
