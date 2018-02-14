4c054280be0e6f364d2e446f015fd9ca3e03f860
Howard Butler <howard@hobu.co>
2018-02-14 08:47:23 -0600

    Respond to PR comments
    * Update to 1.2.5
    * Remove dead test
    * Don't build tests

--- Formula/draco.rb
+++ Formula/draco.rb
@@ -1,21 +1,16 @@
 class Draco < Formula
   desc "3D geometric mesh and point cloud compression library"
   homepage "https://google.github.io/draco/"
-  url "https://github.com/google/draco/archive/1.2.4.tar.gz"
-  sha256 "993c78880f4bbd2bfbc7b1e16809c2f62f8ecf9040f68db75ca65be3ac34be4f"
+  url "https://github.com/google/draco/archive/1.2.5.tar.gz"
+  sha256 "76064614bcf5bc80c13291d38c2ea7b8e9c8827a98de93f7bc8bbe4b6e1ec1c4"
 
   depends_on "cmake" => :build
 
   def install
     mkdir "build" do
-      args = std_cmake_args
-      args << "-DENABLE_TESTS=ON"
-      system "cmake", "..", *args
+      system "cmake", "..", * std_cmake_args
       system "make", "install"
     end
   end
 
-  test do
-    shell_output("#{bin}/draco_encoder --help", 255)
-  end
 end
