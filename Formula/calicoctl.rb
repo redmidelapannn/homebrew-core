0b0a199c9e5f919440c715eb8d247aa3f5d103ce
ilovezfs <ilovezfs@icloud.com>
2018-02-14 07:46:42 -0800

    Update calicoctl.rb

--- Formula/calicoctl.rb
+++ Formula/calicoctl.rb
@@ -5,16 +5,15 @@ class Calicoctl < Formula
       :tag => "v1.6.1",
       :revision => "1724e011ac0e608190d7d5512ab8028bcd18ae7b"
 
-  depends_on "go" => :build
   depends_on "glide" => :build
+  depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = buildpath
     ENV["GLIDE_HOME"] = HOMEBREW_CACHE/"glide_home/#{name}"
-    pkg_name = "github.com/projectcalico/calicoctl"
-    build_dir = buildpath/"src/#{pkg_name}"
-    build_dir.install buildpath.children
-    cd build_dir do
+    dir = buildpath/"src/github.com/projectcalico/calicoctl"
+    dir.install buildpath.children
+    cd dir do
       system "glide", "install", "-strip-vendor"
       system "make", "binary"
       bin.install "dist/calicoctl-darwin-amd64" => "calicoctl"
@@ -23,6 +22,6 @@ class Calicoctl < Formula
   end
 
   test do
-    system "#{bin}/calicoctl", "--version"
+    system bin/"calicoctl", "--version"
   end
 end
