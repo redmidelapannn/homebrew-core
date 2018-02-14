6e1d975e34857810d9da700460baeaa4172c999f
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-14 14:48:00 +0000

    sake: add 0.4.0 bottle.

--- Formula/sake.rb
+++ Formula/sake.rb
@@ -5,6 +5,11 @@ class Sake < Formula
   sha256 "579a77feeaa4c21653bc74c664a57e3dea61ae5b7275eb55115ea5c7109293ea"
   head "https://github.com/xcodeswift/sake.git"
 
+  bottle do
+    sha256 "d4bf52d4773106ad9869ed76a207bff7682b874961278e2be13d0802e39376d4" => :high_sierra
+    sha256 "0d0cdb2643cfe32ff97534a6e19684a480b383d967d56c15956950d80d716176" => :sierra
+  end
+
   depends_on :xcode => "9.2"
 
   def install
