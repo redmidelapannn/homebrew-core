ece28ef5867dda195dab1a4c89780d3b74372a12
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-15 19:29:42 +0000

    samtools: update 1.7 bottle.

--- Formula/samtools.rb
+++ Formula/samtools.rb
@@ -6,9 +6,10 @@ class Samtools < Formula
 
   bottle do
     cellar :any
-    sha256 "368dbaf769be26a7c383ee6ab33b9368d70d15b144701489fa33a3475ca5301e" => :high_sierra
-    sha256 "9be1d5c1a480482885a880c3004906d51b7cb1a89dac25c239513a8b04acfbba" => :sierra
-    sha256 "931f8245341783d7d96789c8cc5b3eea6c8071d1ce82644486884cece294e54e" => :el_capitan
+    rebuild 1
+    sha256 "6bddb32b1713d76fa395b46796056aa213352a4b961e0803c229fbe62827d3ea" => :high_sierra
+    sha256 "32934aef949606e6e40de0238b5c980b82661bcbd6074aa4f42fa9faf2be0903" => :sierra
+    sha256 "933274d42b9cc46c18b948fb0619452059af7ff3dfa54e489bb5504575d50069" => :el_capitan
   end
 
   depends_on "htslib"
