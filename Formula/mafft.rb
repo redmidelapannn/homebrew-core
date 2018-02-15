3252de35a26fcf1c5c83664250971fdf9014c6bd
Shohei1029 <t14650sn@sfc.keio.ac.jp>
2018-02-16 01:28:34 +0900

    mafft 7.313

--- Formula/mafft.rb
+++ Formula/mafft.rb
@@ -4,19 +4,12 @@ class Mafft < Formula
   url "https://mafft.cbrc.jp/alignment/software/mafft-7.313-with-extensions-src.tgz"
   sha256 "c48e5e05b427cae0d862daaef6148675d5ef57e24425c17b4c3d8da5b060eabd"
 
-  fails_with :clang do
-    build 421
-    cause <<-EOS
-      Clang does not allow default arguments in out-of-line definitions of
-      class template members.
-      EOS
-  end
+  depends_on :macos => :lion
 
   def install
     make_args = %W[CC=#{ENV.cc} CXX=#{ENV.cxx} CFAGS=#{ENV.cflags}
-                   CXXFLAGS=#{ENV.cxxflags} PREFIX=#{prefix} MANDIR=#{man1}]
-    make_args << "ENABLE_MULTITHREAD=" if MacOS.version <= :snow_leopard
-    make_args << "install"
+                   CXXFLAGS=#{ENV.cxxflags} PREFIX=#{prefix} MANDIR=#{man1}
+                   install]
 
     cd "core" do
       system "make", *make_args
@@ -27,17 +20,9 @@ class Mafft < Formula
     end
   end
 
-  def caveats
-    if MacOS.version <= :snow_leopard
-      <<-EOS
-        This build of MAFFT is not multithreaded on Snow Leopard
-        because its compiler does not support thread-local storage.
-      EOS
-    end
-  end
-
   test do
     (testpath/"test.fa").write ">1\nA\n>2\nA"
-    system "#{bin}/mafft", "test.fa"
+    output = shell_output("#{bin}/mafft test.fa")
+    assert_match ">1\na\n>2\na", output
   end
 end
