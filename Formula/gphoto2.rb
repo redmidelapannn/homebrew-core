9596e07b29ae2dfa41bd74f8c50a76f1e618614c
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-16 00:01:58 +0000

    gphoto2: update 2.5.15 bottle.

--- Formula/gphoto2.rb
+++ Formula/gphoto2.rb
@@ -6,9 +6,10 @@ class Gphoto2 < Formula
 
   bottle do
     cellar :any
-    sha256 "28856ab94ba2f28245649734265ac41cb29911d78818bd5aea0dddbbfc005510" => :high_sierra
-    sha256 "1c1e980bde3f0cd72de42d9196254ffbec516b4ee667232090f0f68bb9284a73" => :sierra
-    sha256 "f663e168d3e139ea2544e560aa4666441622a95bc425fa755c7fbd6060c5aaa0" => :el_capitan
+    rebuild 1
+    sha256 "be3492e514d4b4197836a402e2d20ac6e4197d88ecf48f082838c72eea814207" => :high_sierra
+    sha256 "0ec7c2f0ad7a9c79454e5fbd77da63b95059809bff0c49dad784f36bfc91d76e" => :sierra
+    sha256 "c41258cf4ed5d30278d4cc8814d4def7a238a0121592650ae4be18e362307c40" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
