870066389bfcd0fa44a321d00375fc6da587ca28
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-16 20:20:34 +0000

    detekt: add 3 bottle.

--- Formula/detekt.rb
+++ Formula/detekt.rb
@@ -4,6 +4,13 @@ class Detekt < Formula
   url "https://github.com/arturbosch/detekt/archive/RC6-3.tar.gz"
   sha256 "aeff12326240ca76d9a1d72eec2d66d1e6cc3ac906937706ab37babd96ab9b9f"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f1b37646d882984bd032c15dcfdb060a5c59b49b5fb3f94ad183717c012e8fc0" => :high_sierra
+    sha256 "ac92976f81e4597c28fed7d1821b2238aec1536e6f230705e2dbd280459a1d57" => :sierra
+    sha256 "b13fc1e6ae1a5aa0145dd448d335d0350f8a806171d1858fb9a526ffe3da89fb" => :el_capitan
+  end
+
   depends_on :java => "1.8"
 
   def install
