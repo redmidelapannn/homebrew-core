14e056efb258f7e880473fcc48ea7ad627e04673
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-19 17:17:30 +0000

    tinycdb: add 0.78 bottle.

--- Formula/tinycdb.rb
+++ Formula/tinycdb.rb
@@ -4,6 +4,14 @@ class Tinycdb < Formula
   url "http://www.corpit.ru/mjt/tinycdb/tinycdb-0.78.tar.gz"
   sha256 "50678f432d8ada8d69f728ec11c3140e151813a7847cf30a62d86f3a720ed63c"
 
+  bottle do
+    cellar :any
+    rebuild 1
+    sha256 "570d27433c691cfeb84d10024eb53a4b1f3bf032a84addecd01d8f211629b55b" => :high_sierra
+    sha256 "8e891ff506b21c7cc879947444dd0c2587fbdc8855df260a775dbb4f55cc15f7" => :sierra
+    sha256 "16f012bb07efacd4e4d9db184fc891561fa6daa95632e76cf35398849b2b626c" => :el_capitan
+  end
+
   # This patch enables install-sharedlib to build a .dylib on macOS.
   patch do
     url "https://gist.githubusercontent.com/zeha/f11da05f59dcef00c7098ac8c988534b/raw/ebfa8035fa2abf04a57b56058c6cce6f402e593e/tinycdb-dylib.patch"
