b156fffa392e31d5b6400a8451b55fbbc1053012
kkdd <kkondo_z0@nifty.com>
2018-02-19 20:18:26 +0900

    mas.rb: xcode 9.0 for sierra or later

--- Formula/mas.rb
+++ Formula/mas.rb
@@ -1,17 +1,23 @@
 class Mas < Formula
   desc "Mac App Store command-line interface"
   homepage "https://github.com/mas-cli/mas"
-  url "https://github.com/mas-cli/mas/archive/v1.4.1.tar.gz"
-  sha256 "4fd91c13b46d403b52dbee3891adb3cd6571e07ad20cf58de0100c9f695e6c24"
   head "https://github.com/mas-cli/mas.git"
+
+  if MacOS.version >= :sierra
+    url "https://github.com/mas-cli/mas/archive/v1.4.1.tar.gz"
+    sha256 "4fd91c13b46d403b52dbee3891adb3cd6571e07ad20cf58de0100c9f695e6c24"
+    depends_on :xcode => ["9.0", :build]
+  else
+    url "https://github.com/mas-cli/mas/archive/v1.3.1.tar.gz"
+    sha256 "9326058c9e572dd38df644313307805757d7ea6dfea8626e0f41c373ecbd46b5"
+    depends_on :xcode => ["8.0", :build]
+  end
 
   bottle do
     cellar :any_skip_relocation
     sha256 "b7585ced3a93d60e95357e93d729913a6f628fda82359e77c6553c2e802c50dc" => :high_sierra
     sha256 "af5be6aa9902d9cfc2aa69dbf313441a7c201463d516face721f900ceae9556b" => :sierra
   end
-
-  depends_on :xcode => ["9.0", :build]
 
   def install
     xcodebuild "-project", "mas-cli.xcodeproj",
