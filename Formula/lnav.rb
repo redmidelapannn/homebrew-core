8b4392c9db26cba5297a2a4f1899762cdba12d9d
ilovezfs <ilovezfs@icloud.com>
2018-02-20 10:02:58 +0000

    Update lnav.rb

--- Formula/lnav.rb
+++ Formula/lnav.rb
@@ -23,25 +23,15 @@ class Lnav < Formula
   depends_on "readline"
   depends_on "pcre"
   depends_on "sqlite" if MacOS.version < :sierra
-  depends_on "curl" => ["with-libssh2", :optional]
 
   def install
     # Fix errors such as "use of undeclared identifier 'sqlite3_value_subtype'"
     ENV.delete("SDKROOT")
 
-    args = %W[
-      --disable-dependency-tracking
-      --prefix=#{prefix}
-      --with-readline=#{Formula["readline"].opt_prefix}
-    ]
-
-    # macOS ships with libcurl by default, albeit without sftp support. If we
-    # want lnav to use the keg-only curl formula that we specify as a
-    # dependency, we need to pass in the path.
-    args << "--with-libcurl=#{Formula["curl"].opt_lib}" if build.with? "curl"
-
     system "./autogen.sh" if build.head?
-    system "./configure", *args
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--with-readline=#{Formula["readline"].opt_prefix}"
     system "make", "install"
   end
 end
