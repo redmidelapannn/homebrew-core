9e8a06f9a214098df63213355188c64925304f52
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-21 01:16:53 +0000

    libbitcoin-protocol: add 3.5.0 bottle.

--- Formula/libbitcoin-protocol.rb
+++ Formula/libbitcoin-protocol.rb
@@ -4,6 +4,13 @@ class LibbitcoinProtocol < Formula
   url "https://github.com/libbitcoin/libbitcoin-protocol/archive/v3.5.0.tar.gz"
   sha256 "9deac6908489e2d59fb9f89c895c49b00e01902d5fdb661f67d4dbe45b22af76"
 
+  bottle do
+    cellar :any
+    sha256 "5d90833384377a333d777d877e22db39954ab87316bf2d518a395d69fa554f30" => :high_sierra
+    sha256 "e2f61cf47bf8fbab60d851dd5e8369e796065edc63537a49dac4e79953ca5494" => :sierra
+    sha256 "309b5900b72b225d0585c8197b60a5335cfa0121f41121f48d960a6f44fb50cc" => :el_capitan
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
