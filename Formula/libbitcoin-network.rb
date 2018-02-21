0d21efcbe8ceb6cc7f525b8270efd178e8a9242b
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-21 01:16:52 +0000

    libbitcoin-network: add 3.5.0 bottle.

--- Formula/libbitcoin-network.rb
+++ Formula/libbitcoin-network.rb
@@ -4,6 +4,12 @@ class LibbitcoinNetwork < Formula
   url "https://github.com/libbitcoin/libbitcoin-network/archive/v3.5.0.tar.gz"
   sha256 "e065bd95f64ad5d7b0f882e8759f6b0f81a5fb08f7e971d80f3592a1b5aa8db4"
 
+  bottle do
+    sha256 "4e49f2e6855da1682889d69964c0bf0219448120756a4acb4fa780a1b3dc6891" => :high_sierra
+    sha256 "e20d5da8e68b3287e349a6da869bbaec7ca058863bc0ef9717948b6ee11a58a7" => :sierra
+    sha256 "cfed069def42c20ef32e42f61f3c20e8a11618b3266b51c95ec594a387fdbbc1" => :el_capitan
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
