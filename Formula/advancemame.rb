b3bec15ca40b4aa17d0e87a609315821a1d0dbfe
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-23 17:20:29 +0000

    advancemame: update 3.7 bottle.

--- Formula/advancemame.rb
+++ Formula/advancemame.rb
@@ -5,9 +5,10 @@ class Advancemame < Formula
   sha256 "36c88305dc485e85ff86854b3d8bb75c4c81fa7356f6dbfcbfd6a5e192199b2c"
 
   bottle do
-    sha256 "cebd9f5bdee213f0994527f63a3c8ad6d78cb43e377bdd19978fa2d9e03acd15" => :high_sierra
-    sha256 "3bf6b73863b14621de91fe5908921d322e28e4e26855eae7f5cf3ba15b360c07" => :sierra
-    sha256 "e5f14218046ce7f164a270378743aefff119d0fc7c69987bfb78fc8d1105ea7b" => :el_capitan
+    rebuild 1
+    sha256 "13729fe0e76da7806841475dea51ffec88b54c3370dd7b39da03a094b30aa781" => :high_sierra
+    sha256 "d97cbc914092c301810a51465653ee89166116f5bdaf5d7acad8f2d04bf0722e" => :sierra
+    sha256 "2467942feb836757b04e288f7eec0785cde917e0f0af69d755c6cf485de8cc98" => :el_capitan
   end
 
   depends_on "sdl"
