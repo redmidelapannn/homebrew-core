f41b6973ccd58d8139eb184fbf0391d298e7bc00
ilovezfs <ilovezfs@icloud.com>
2018-02-23 19:05:29 +0000

    Update mongodb.rb

--- Formula/mongodb.rb
+++ Formula/mongodb.rb
@@ -32,8 +32,8 @@ class Mongodb < Formula
   end
 
   resource "typing" do
-    url "https://files.pythonhosted.org/packages/ca/38/16ba8d542e609997fdcd0214628421c971f8c395084085354b11ff4ac9c3/typing-3.6.2.tar.gz"
-    sha256 "d514bd84b284dd3e844f0305ac07511f097e325171f6cc4a20878d11ad771849"
+    url "https://files.pythonhosted.org/packages/ec/cc/28444132a25c113149cec54618abc909596f0b272a74c55bab9593f8876c/typing-3.6.4.tar.gz"
+    sha256 "d400a9344254803a2368533e4533a4200d21eb7b6b729c173bc38201a74db3f2"
   end
 
   needs :cxx11
