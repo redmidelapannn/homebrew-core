60c36be22b1566374679da91569469993932b4f2
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-26 10:20:25 +0000

    libtextcat: update 2.2 bottle.

--- Formula/libtextcat.rb
+++ Formula/libtextcat.rb
@@ -7,13 +7,10 @@ class Libtextcat < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "7997ea512b672f165e1e53e941147e9a520a9ab5d71b8b22e4a71622690e7cdb" => :high_sierra
-    sha256 "24fe8791549204d8ef6e596fc327fbd3a645c729b440ba31ef47cf545f6f5b30" => :sierra
-    sha256 "afa51f83d0a3c96ffc6f6c35011c864347f31d2c3aea987102c59f0257177072" => :el_capitan
-    sha256 "1a63f24b16949843f6a3f6c17d9467208a471cfa6bf1b193738fa94c2d320f02" => :yosemite
-    sha256 "e7880fa731747f117a943fd99bd41da25bae3e6440316d782c4798cf3f55e0b7" => :mavericks
-    sha256 "645dab2e1766e0ea0fc3f6bc897e372ca26826a8ca136463ac864da1ddf74202" => :mountain_lion
+    rebuild 2
+    sha256 "ade517342a050c00fc69a96ab9bfa2a1221ff8bf41240617cc1da569dd3260c4" => :high_sierra
+    sha256 "68b97689d5fa2717f711f80d52dc98b656394e2eba8ae7930db7596d5d3d210f" => :sierra
+    sha256 "9f5bb5df407f0075fd0321d010e09c2ae07333682c76832e9a114d90933976b8" => :el_capitan
   end
 
   def install
