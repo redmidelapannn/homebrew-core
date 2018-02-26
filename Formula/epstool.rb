380b6a82796793b229f3c11176b308e18143a30d
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-26 00:52:38 +0000

    epstool: update 3.08 bottle.

--- Formula/epstool.rb
+++ Formula/epstool.rb
@@ -7,12 +7,10 @@ class Epstool < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "4e41615a63d60d963029768b0618d43e29142e5495fb693a7f5844d2cb6f0d6b" => :high_sierra
-    sha256 "d53d1f3d353d0f68f52b87603a67ca97942fb714f766a54636c142550568cbd0" => :sierra
-    sha256 "92efa66cd268f0447dc52c14e9da04ae8af01b1691ec8eec3df61bbeb947b713" => :el_capitan
-    sha256 "bb2aefa17b699127f2f6ed65004c9acbf7e5e5122f6f4b920d1e03fb9bd87b2e" => :yosemite
-    sha256 "ae3c4b14dd19d3ac43947eff025a0fa3eabbe832333922359f9f74e0fe5e1d3d" => :mavericks
-    sha256 "408c2744b730a01eed68f19d4f643b060f026bbbc2616cf4b6afcb08ef608b90" => :mountain_lion
+    rebuild 1
+    sha256 "30ff766b51400410c72dbbceeaeb3b77de99107da4a14bb54dad8b56f0efb21d" => :high_sierra
+    sha256 "36fdda950988dad3b9e90d417515ac6b0f39066cdc21299fa115a3820321ba91" => :sierra
+    sha256 "e4f0fb478fb1c3fe5bac0371aa363c5a83c465a479cdf0330161ba76cef362c2" => :el_capitan
   end
 
   depends_on "ghostscript"
