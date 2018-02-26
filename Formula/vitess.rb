9b8bdf31a9347ce0803ccedc6c5644e9dcff0a56
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-26 15:17:22 +0000

    vitess: add 2.1.1.75b5eb41 bottle.

--- Formula/vitess.rb
+++ Formula/vitess.rb
@@ -8,6 +8,13 @@ class Vitess < Formula
   version "2.1.1.75b5eb41"
   head "https://github.com/arthurnn/vitess.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8bd2523346d6d921823026008d96d9dae92f1a2d9ab85b8cf6603db2411a594d" => :high_sierra
+    sha256 "fe7a9b11a0b3a63f21a88300a9344adafc656abd0eeab1b1b05dc312b9f36abc" => :sierra
+    sha256 "77950990144baab8a9f948e2302130fffd27fe2206c3b613c20d8791b36e4c14" => :el_capitan
+  end
+
   depends_on "go" => :build
   depends_on "pkg-config" => :build
 
