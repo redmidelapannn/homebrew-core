64a2d0a12fdd281e167292102e845b5da7f749d1
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-27 00:44:07 +0000

    t1lib: update 5.1.2 bottle.

--- Formula/t1lib.rb
+++ Formula/t1lib.rb
@@ -6,12 +6,10 @@ class T1lib < Formula
   sha256 "821328b5054f7890a0d0cd2f52825270705df3641dbd476d58d17e56ed957b59"
 
   bottle do
-    rebuild 2
-    sha256 "a36bc3913f6b51cb7772609a52049f90fc6241ffca3bf18c4295455ef5f4df4c" => :high_sierra
-    sha256 "94789287c849a04f05a40c79940aee6efe3e03318c95db9c2be69ee4e6806d82" => :sierra
-    sha256 "fa356a5405f5b0cf57c15ebd5b680c215e1e498189914e9b9663eb132655a8c1" => :el_capitan
-    sha256 "6d1bf242eb7a5201180b9d4b505a7f83663802383d358180cea696714ae57fc8" => :yosemite
-    sha256 "ec107b30d4b9a95bbc094a7a944cab862ed78a875c5ed0002aa7232ec514e9d7" => :mavericks
+    rebuild 3
+    sha256 "723b2861c99395b94eae479a55ba81e2b313798bf2d05c358f8efe86c0b4877e" => :high_sierra
+    sha256 "b25d169f4f883d99c02cc9ecf796b57611fb0f53f5df061cc20a2547c4b124a0" => :sierra
+    sha256 "89e6bf82254a0b29d623f9ed87008ab8f2ce194385d61e8482b9e06a1a2a481b" => :el_capitan
   end
 
   def install
