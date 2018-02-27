958e2ad927349c9c85239cd1d8741775071f64f0
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-27 00:59:11 +0000

    fasttext: add 0.1.0 bottle.

--- Formula/fasttext.rb
+++ Formula/fasttext.rb
@@ -5,6 +5,13 @@ class Fasttext < Formula
   sha256 "d6b4932b18d2c8b3d50905028671aadcd212b7aa31cbc6dd6cac66db2eff1397"
   head "https://github.com/facebookresearch/fastText.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "274a78c4f4cc2438351c22d688969150b2294104a636d0e27cb0d2c03e05e4d3" => :high_sierra
+    sha256 "eac4c0ea81c7a9898457d54b1498b9ea79d5de7ee28bacb8bb1f3adeb89980c3" => :sierra
+    sha256 "4e3bd4260d8b284c6c0af34a26b3819e41ff0b42fd107e792927a735ed28ab3d" => :el_capitan
+  end
+
   def install
     system "make"
     bin.install "fasttext"
