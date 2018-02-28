6b3492e286ce4d20cbd045f2cc95765aab1c76e2
BrewTestBot <brew-test-bot@googlegroups.com>
2018-02-28 10:32:23 +0000

    elastix: add 4.8 bottle.

--- Formula/elastix.rb
+++ Formula/elastix.rb
@@ -4,6 +4,13 @@ class Elastix < Formula
   url "http://elastix.isi.uu.nl/download/elastix_macosx64_v4.8.zip"
   sha256 "d9c49856674e7f39619b3dbbf85e6a9a902c660f9b68e37a9a7e381ebda9d29b"
 
+  bottle do
+    cellar :any
+    sha256 "3d1d5d41b7dcaffd957e686a8731705220bc3c6564191d920601df3beb9dd5a7" => :high_sierra
+    sha256 "3d1d5d41b7dcaffd957e686a8731705220bc3c6564191d920601df3beb9dd5a7" => :sierra
+    sha256 "3d1d5d41b7dcaffd957e686a8731705220bc3c6564191d920601df3beb9dd5a7" => :el_capitan
+  end
+
   def install
     prefix.install "LICENSE", "NOTICE", "bin", "lib"
   end
