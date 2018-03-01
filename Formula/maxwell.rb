b3c4c2e9a2913154f9a3126f19475db490f81653
Shashwat Anand <me@shashwat.me>
2018-03-01 09:51:49 +0530

    maxwell 1.13.1 (new formula)
    Fix review comments.
    -- Remove bottle blocks.
    -- Remove maven as build.
    -- Remove java dependency block.
    -- Fix test by using string over regex.

--- Formula/maxwell.rb
+++ Formula/maxwell.rb
@@ -4,20 +4,7 @@ class Maxwell < Formula
   url "https://github.com/zendesk/maxwell/releases/download/v1.13.1/maxwell-1.13.1.tar.gz"
   sha256 "3ab7489d47f2ec04677122c8786cf573572b9bbb511b184802d2dcfe82e83781"
 
-  bottle do
-    cellar :any
-    sha256 "3ab7489d47f2ec04677122c8786cf573572b9bbb511b184802d2dcfe82e83781" => :high_sierra
-    sha256 "3ab7489d47f2ec04677122c8786cf573572b9bbb511b184802d2dcfe82e83781" => :sierra
-    sha256 "3ab7489d47f2ec04677122c8786cf573572b9bbb511b184802d2dcfe82e83781" => :el_capitan
-  end
-
-  depends_on "maven" => :build
   depends_on :java => "1.8"
-
-  pour_bottle? do
-    reason "The bottle requires Java 1.8."
-    satisfy { quiet_system("/usr/libexec/java_home --version 1.8 --failfast") }
-  end
 
   def install
     libexec.install Dir["*"]
@@ -34,6 +21,6 @@ class Maxwell < Formula
       exec "#{bin}/maxwell --log_level=OFF > #{testpath}/maxwell.log 2>/dev/null"
     end
     sleep 15
-    assert_match /Using kafka version/, IO.read("#{testpath}/maxwell.log")
+    assert_match "Using kafka version", IO.read("#{testpath}/maxwell.log")
   end
 end
