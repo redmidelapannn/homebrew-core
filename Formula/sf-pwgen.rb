e8b3fa5b03d4e1b19582f8b924f16318a492879e
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-03 20:28:52 +0000

    sf-pwgen: update 1.4 bottle.

--- Formula/sf-pwgen.rb
+++ Formula/sf-pwgen.rb
@@ -8,10 +8,10 @@ class SfPwgen < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "05ac7530759e9f2cf3a0e94a5b1f4fea83a1710de8010fa9b3d9b60167018038" => :high_sierra
-    sha256 "ff59281df8d8c0e341233cdffafe67d0dc78101afb90a8597b0a83605ef578a3" => :sierra
-    sha256 "2c0e0523569aa25fe254012d3b86ae0bdc587c0f17c4a62d8d12917b7fa44fbf" => :el_capitan
-    sha256 "2c6d133b3c9b079dc8c81407107a3c1fb4d5cb3d654afa7acef6f23b9f9df9a6" => :yosemite
+    rebuild 1
+    sha256 "331f9f0d913cccd80cd617295a2dc1f02b00d8cf8ff8155e803d48e7228ab035" => :high_sierra
+    sha256 "1afb8dfd1c0b5f635232679435da8f6a299c941bf17185ec6f49aeff941d871c" => :sierra
+    sha256 "b625e9a25f8de6b733f01a80e8069683a0d028320fbc8d35e8b6e8e8d35cf5ea" => :el_capitan
   end
 
   depends_on :macos => :mountain_lion
