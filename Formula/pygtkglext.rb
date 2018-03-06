0e6038f74d56a66117cf7b04a3705195301ed268
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-06 18:56:34 +0000

    pygtkglext: update 1.1.0_2 bottle.

--- Formula/pygtkglext.rb
+++ Formula/pygtkglext.rb
@@ -7,10 +7,10 @@ class Pygtkglext < Formula
 
   bottle do
     cellar :any
-    sha256 "90695fa5442758d3ec47f6672a84bcdfb03c3a20df803bdc2aae7fd1e9139819" => :high_sierra
-    sha256 "c2a2ead95b65a79eecac385c7ec23dab04f8980cfba44e8d51c4cc2de80fd7f3" => :sierra
-    sha256 "d946f8f65dd31f5d8ee9e7032b54ec1f259ec11ab57908fd0c4c28f617bb0d21" => :el_capitan
-    sha256 "ffac080474197c23fca753fecba77f53f1d25b0b86856fade067f60dc70941d6" => :yosemite
+    rebuild 1
+    sha256 "4302acf41dd640c2e5eb3fa521136310cfb12ab2239d2c6a8ba8d98848ddf7ae" => :high_sierra
+    sha256 "a1dae615c373694bcbd289048d1c0876acec60a3b9629a2dd76382a32b676e23" => :sierra
+    sha256 "e5bbd5cfcbe2445602333be20dc48fe75d8d0cd118d45d7bdb629fbf378b3cd8" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
