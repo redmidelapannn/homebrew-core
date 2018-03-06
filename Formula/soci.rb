257f40224289cbf572acaa92fc079c58fc0d32ad
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-06 09:45:30 +0000

    soci: update 3.2.3 bottle.

--- Formula/soci.rb
+++ Formula/soci.rb
@@ -5,10 +5,10 @@ class Soci < Formula
   sha256 "ab0f82873b0c5620e0e8eb2ff89abad6517571fd63bae4bdcac64dd767ac9a05"
 
   bottle do
-    sha256 "b6186caa197c5111b9704cc4b4f133f0f5f656727ed3211c7351e2a97302f10f" => :high_sierra
-    sha256 "61ac3ada591371a743198251fa35d70a18cc6018d23e4a36e3ec45aa6ec99db2" => :sierra
-    sha256 "4b8d8acc29c2ed8e826be84c9dc777d947033396336885440fb823530460b470" => :el_capitan
-    sha256 "b802ee253ceb25ebfd2b5a90ef4dc8b229a90c7b1cae1a33533c9bd2ff9e7d50" => :yosemite
+    rebuild 1
+    sha256 "3f082affe2519ebb1ab60070722217f249a16b12431070b021f4eeaf71a6e7de" => :high_sierra
+    sha256 "174fd5b3530548cd4f2bea89da34c8cbfb9e5f7bbdd642731a2aadf0b08e2d05" => :sierra
+    sha256 "a3c942acf5fc54d8e95bc7bfcc375bc86ead8faf43a738f990c42f28b32d31f0" => :el_capitan
   end
 
   option "with-oracle", "Enable Oracle support."
