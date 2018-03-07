0fe68a544e8fca2a44c0623c6429a929ecf439c8
ilovezfs <ilovezfs@icloud.com>
2018-03-07 23:49:50 +0000

    Update git-sizer.rb

--- Formula/git-sizer.rb
+++ Formula/git-sizer.rb
@@ -3,17 +3,14 @@ class GitSizer < Formula
   homepage "https://github.com/github/git-sizer"
   url "https://github.com/github/git-sizer/archive/v1.0.0.tar.gz"
   sha256 "8ef6b2adb646ccb72f5ccfebd19a57371b119da1aac02ef06f1a46af6f7447a0"
-  head "https://github.com/github/git-sizer.git"
 
   depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = buildpath
-    dir = buildpath/"src/github.com/github/git-sizer"
-    dir.install buildpath.children
-
-    cd "src" do
-      system "go", "build", "-o", bin/"git-sizer", "github.com/github/git-sizer"
+    (buildpath/"src/github.com/github/git-sizer").install buildpath.children
+    cd "src/github.com/github/git-sizer" do
+      system "go", "build", "-o", bin/"git-sizer"
       prefix.install_metafiles
     end
   end
@@ -21,6 +18,6 @@ class GitSizer < Formula
   test do
     system "git", "init"
     output = shell_output("#{bin}/git-sizer")
-    assert_match(/No problems above the current threshold were found/, output)
+    assert_match "No problems above the current threshold were found", output
   end
 end
