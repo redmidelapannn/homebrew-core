2bdd8629be4510299c598326f1829866848fee9b
Alyssa Ross <hi@alyssa.is>
2018-03-08 18:37:11 +0000

    geckodriver: add comment to patch

--- Formula/geckodriver.rb
+++ Formula/geckodriver.rb
@@ -7,8 +7,11 @@ class Geckodriver < Formula
     url "https://github.com/mozilla/geckodriver/archive/v0.19.1.tar.gz"
     sha256 "f590ddfef42995a23e781b52befdbc2ac342bf829008e98d212f2e1e15d9f713"
 
+    # Remove for > 0.19.1
+    # Fixes E0277 when building slog with Rust > 1.23
+    # Fixed upstream in https://hg.mozilla.org/mozilla-central/rev/cbd3741a4bb0 on 2018-02-15.
     patch do
-      url "https://raw.githubusercontent.com/Homebrew/formula-patches/8920856c/geckodriver/bug-1435830.patch"
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/e89f1ad/geckodriver/bug-1435830.patch"
       sha256 "578cdb22803c2f6ee00e8e0b1ca6fcde622c743572aad7038dda0d63cbce4500"
     end
   end
