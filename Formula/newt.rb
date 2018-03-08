b22d76e25d4aba4d0f61670a4daa30a819aab3cf
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-08 12:16:40 +0000

    newt: update 0.52.20 bottle.

--- Formula/newt.rb
+++ Formula/newt.rb
@@ -6,10 +6,10 @@ class Newt < Formula
 
   bottle do
     cellar :any
-    sha256 "092e8f0a4603c337cd7e51d26d3721bd450a3a9ebf6eb0f55bf703f1a3c34cfa" => :high_sierra
-    sha256 "eb0b10566d3852909e6f11ad02dd09382cb494d05f1ea4a2371c15abda8cda1c" => :sierra
-    sha256 "1a3b16bafddded8ae06bcb5b261c50142b7b7752d4e4cf08d65709a2506edf82" => :el_capitan
-    sha256 "78f895d8ee19c343c5846a29699fd8be1f552f8d5c335081df353f8069cd9fa0" => :yosemite
+    rebuild 1
+    sha256 "64842fba919ff3a9cc2a1fd3a351d75d52d7ee319c0975cc56257e5927401543" => :high_sierra
+    sha256 "7161bd917081d7792d0085466c66a845683a925e90a9649dc66df71a7b88ce7e" => :sierra
+    sha256 "cee51137f061d1805b0233c597626eb1ab19a8fca8f19b74549e258ec499478a" => :el_capitan
   end
 
   depends_on "gettext"
