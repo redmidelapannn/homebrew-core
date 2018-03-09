162d657211b737574d1d51d5136d5a440f82ad63
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-09 23:56:15 +0000

    dhcpdump: update 1.8 bottle.

--- Formula/dhcpdump.rb
+++ Formula/dhcpdump.rb
@@ -7,12 +7,10 @@ class Dhcpdump < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d49aaa82bf92fd7adeb0edb91812d4c48aa20fb0af2f30f9b4238d032dbb99ce" => :high_sierra
-    sha256 "10e6565cdd5717666822ce9a0f77747d44969b5cbc7c3ccf1505aaa6ba95af85" => :sierra
-    sha256 "1f30cb4146a741b3523d674336854c665546e939af04f619e38623d9298cd4ef" => :el_capitan
-    sha256 "6df64653cfd4b118db43e2acb2f08a565ac3cba9e1b739a258eeb7655c1a6103" => :yosemite
-    sha256 "c549cb96db8e621b379b1a02cd4e743ff74b4d212f2d880649351e28c4c63684" => :mavericks
-    sha256 "bcf2658789655d4c518f9b666e8b46b205b3d1ac8aad3c6057fc0fac28a72cb1" => :mountain_lion
+    rebuild 1
+    sha256 "bca7b8747e256117a11b65d50c025ace9b0238fd49583ea9d2e9f115ab28db62" => :high_sierra
+    sha256 "0f79e02ec0b055ed2ca53479a399bb647fab50cdaba8c2b539c6927078200fe3" => :sierra
+    sha256 "ea6c78a94e24ba2b2f46eb3a61699d2f09d50eb7c9e0bb9ecf02aad0b6d7b635" => :el_capitan
   end
 
   def install
