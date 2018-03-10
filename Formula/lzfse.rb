f1d74f0db8abb5f06eb108f0731e161ca7f12135
FX Coudert <fxcoudert@gmail.com>
2018-03-10 16:45:34 +0100

    Update lzfse.rb

--- Formula/lzfse.rb
+++ Formula/lzfse.rb
@@ -1,21 +1,14 @@
 class Lzfse < Formula
   desc "Apple LZFSE compression library and command-line tool"
   homepage "https://github.com/lzfse/lzfse"
-
   url "https://github.com/lzfse/lzfse/archive/lzfse-1.0.tar.gz"
   sha256 "cf85f373f09e9177c0b21dbfbb427efaedc02d035d2aade65eb58a3cbf9ad267"
-
-  head "https://github.com/lzfse/lzfse"
-
-  option "without-test", "Skip build-time tests (not recommended)"
 
   depends_on "cmake" => :build
 
   def install
     mkdir "build" do
       system "cmake", "..", *std_cmake_args
-      system "make"
-      system "make", "test" if build.with? "test"
       system "make", "install"
     end
   end
