f748ea4073ffb2b23d4fc53e97ec7a35083bd8e6
qinxin0720 <qinxin_0720@live.com>
2018-03-10 14:38:06 +0800

    Update zlog.rb

--- Formula/zlog.rb
+++ Formula/zlog.rb
@@ -1,6 +1,6 @@
  class Zlog < Formula
   desc "A reliable, high-performance, thread safe, flexsible, clear-model, pure C logging library."
-  homepage "http://hardysimpson.github.io/zlog/"
+  homepage "https://hardysimpson.github.io/zlog/"
   url "https://github.com/lisongmin/zlog/archive/v1.2.12.1.tar.gz"
   sha256 "7127b4210728069667db39c6a163ae3fc070fe84a73bad642d5e707642b3b6d2"
 
