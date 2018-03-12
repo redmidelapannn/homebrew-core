a6ab68769ea65467f4c5d467b09c1216086cf183
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-12 10:53:39 +0000

    fselect: add 0.3.2 bottle.

--- Formula/fselect.rb
+++ Formula/fselect.rb
@@ -5,6 +5,12 @@ class Fselect < Formula
   sha256 "9aef280de65b8ffd91993c6eb6321c8fdec4281f3490b070da832b307f8ded1b"
   head "https://github.com/jhspetersson/fselect.git"
 
+  bottle do
+    sha256 "f766c8896858032abb1d65559ffa4b09b2e508e3de51a570c509abd98aeb7e29" => :high_sierra
+    sha256 "dde34dd1ee08a272a40452ee51028fcb06589f0cc817745af811bd2b00707647" => :sierra
+    sha256 "4a6ee623a2d226fe63f316dc52ea9f14ed152a2943122ff40a02f93174f1bd77" => :el_capitan
+  end
+
   depends_on "rust" => :build
 
   def install
