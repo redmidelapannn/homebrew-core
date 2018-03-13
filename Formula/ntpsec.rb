c964d5ed33479e5dc8bc31de631395783408c617
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-13 12:46:45 +0000

    ntpsec: add 1.0.0 bottle.

--- Formula/ntpsec.rb
+++ Formula/ntpsec.rb
@@ -4,6 +4,12 @@ class Ntpsec < Formula
   url "ftp://ftp.ntpsec.org/pub/releases/ntpsec-1.0.0.tar.gz"
   sha256 "7727e67679812200c52a4c8114bb140f217a4303ec52326c449c15cc470c1700"
 
+  bottle do
+    cellar :any
+    sha256 "dddd2e37e911f93666687bcabe50aedc3a9e01227ddaf75bc378e25b5134cfad" => :high_sierra
+    sha256 "eb1a9349fb41e4aa128fdd66edde1bdd16fa976e01e56cb3fe64b213f090bff2" => :sierra
+  end
+
   depends_on "pkg-config" => :build
   depends_on "openssl"
 
