206b9f78eadbd2f9088c3d1f752e308bd155e47d
John Cupitt <jcupitt@gmail.com>
2018-03-13 13:10:43 +0000

    revert removal of pygobject3
    pygobject3 had been accidentally removed -- revert

--- Formula/vips.rb
+++ Formula/vips.rb
@@ -25,6 +25,7 @@ class Vips < Formula
   depends_on "little-cms2"
   depends_on "orc"
   depends_on "pango"
+  depends_on "pygobject3"
   depends_on "fftw" => :recommended
   depends_on "poppler" => :recommended
   depends_on "graphicsmagick" => :optional
