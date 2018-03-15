06c34895df3c86cac813a7950134a09537cbce87
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-15 12:32:14 +0000

    par2tbb: add 20141125 bottle.

--- Formula/par2tbb.rb
+++ Formula/par2tbb.rb
@@ -4,6 +4,13 @@ class Par2tbb < Formula
   url "https://web.archive.org/web/20141212202746/http://chuchusoft.com/par2_tbb/par2cmdline-0.4-tbb-20141125.tar.gz"
   sha256 "17a5bb5e63c1b9dfcf5feb5447cee60a171847be7385d95f1e2193a7b59a01ad"
 
+  bottle do
+    cellar :any
+    sha256 "1b1e0c2395512030adcdbdfd4835a55f50d857c4c05c3d48830024e42da389dc" => :high_sierra
+    sha256 "9816c9c69de0c41e5f51c23e68481f13b2716a22e70a6ea5fa9d26829a3ecdde" => :sierra
+    sha256 "e17915ad514052f1aa9ebb246baebe6487cf0a18ef2a8f40448531ed25976a5f" => :el_capitan
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
