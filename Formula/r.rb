8042c82474a7241198dc2663bcf8e372a19715b0
Luis Puerto <luiss.puerto@gmail.com>
2018-03-15 17:07:42 +0200

    https added

--- Formula/r.rb
+++ Formula/r.rb
@@ -1,7 +1,7 @@
 class R < Formula
   desc "Software environment for statistical computing"
   homepage "https://www.r-project.org/"
-  url "http://cran.r-project.org/src/base/R-3/R-3.4.4.tar.gz"
+  url "https://cran.r-project.org/src/base/R-3/R-3.4.4.tar.gz"
   sha256 "b3e97d2fab7256d1c655c4075934725ba1cd7cb9237240a11bb22ccdad960337"
 
   bottle do
