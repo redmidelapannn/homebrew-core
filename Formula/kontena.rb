827f207297c31498b13d46bc020de3446256ebac
Kimmo Lehto <kimmo.lehto@kontena.io>
2018-03-16 10:50:35 +0200

    Replaced the online tests with offline one

--- Formula/kontena.rb
+++ Formula/kontena.rb
@@ -187,10 +187,11 @@ class Kontena < Formula
   test do
     assert_match "+homebrew", shell_output("#{bin}/kontena --version")
     assert_match "login", shell_output("#{bin}/kontena complete kontena master")
-    output = shell_output("#{bin}/kontena plugin search digitalocean")
-    assert_match "Kontena DigitalOcean plugin", output
-    output = shell_output("#{bin}/kontena stack reg show kontena/hello-world")
-    assert_match "description: Sample stack to test Kontena", output
+    test_yaml = "stack: test/test\nversion: 0.1.0\nservices:\n  redis:\n    image: redis:latest\n"
+    (testpath/"kontena.yml").write(test_yaml)
+    output = shell_output("#{bin}/kontena stack validate --format api-json")
+    assert_match "\"stack\": \"test/test\"", output
+    assert_match "\"expose\": null", output
     assert_match "NAME", shell_output("#{bin}/kontena master list")
   end
 end
