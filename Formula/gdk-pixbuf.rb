60eae6f5d3090a6a27f5b947652111c103c31d21
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-17 15:42:56 +0000

    gdk-pixbuf: update 2.36.11 bottle.

--- Formula/gdk-pixbuf.rb
+++ Formula/gdk-pixbuf.rb
@@ -5,9 +5,10 @@ class GdkPixbuf < Formula
   sha256 "ae62ab87250413156ed72ef756347b10208c00e76b222d82d9ed361ed9dde2f3"
 
   bottle do
-    sha256 "bd9e4d72a827f75ea2a1cd9463be0cf123ba1cda8f2e4d0a3ef0b1a1c46945f6" => :high_sierra
-    sha256 "a6280e13fe29c5c06548e4c8d0ed80755b50432778b6f668495327a289693cf3" => :sierra
-    sha256 "70aa88fda9b08b1cbd7fdd3c21d378ce1a95c1c936d5eba9dbe9efcd75254f04" => :el_capitan
+    rebuild 1
+    sha256 "fe620b7ad77b13b962e13b5f1fc498b9e48e016a93f009310adc8544291510eb" => :high_sierra
+    sha256 "28f1fb7235f253805bc061f97f7c4626ceb8ae0b8b9885139a4c8b79d9f2b60a" => :sierra
+    sha256 "fd7891bc9d256dfc2245e0e8c0f82ab9bb151a2496b30dae73a9c2f713a8830b" => :el_capitan
   end
 
   option "without-modules", "Disable dynamic module loading"
