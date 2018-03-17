585c39d57730310855e95d998c3df055f186f9f9
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-17 05:12:37 +0000

    autoconf@2.64: update 2.64 bottle.

--- Formula/autoconf@2.64.rb
+++ Formula/autoconf@2.64.rb
@@ -7,13 +7,10 @@ class AutoconfAT264 < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 4
-    sha256 "63957a3952b7af5496012b3819c9956822fd7d895d63339c23fdc65c502e1a40" => :high_sierra
-    sha256 "a76fca79a00f733c1c9f75600b906de4755dd3fbb595b1b55ded1347ac141607" => :sierra
-    sha256 "ded69c7dac4bc8747e52dca37d6d561e55e3162649d3805572db0dc2f940a4b8" => :el_capitan
-    sha256 "daf70656aa9ff8b2fb612324222aa6b5e900e2705c9f555198bcd8cd798d7dd0" => :yosemite
-    sha256 "d153b3318754731ff5e91b45b2518c75880993fa9d1f312a03696e2c1de0c9d5" => :mavericks
-    sha256 "37e77a2e7ca6d479f0a471d5f5d828efff621bd051c1884ff1363d77c5c4675e" => :mountain_lion
+    rebuild 1
+    sha256 "1d35948b6daef2bf2d04c00050d70ec3eb4d3f045ebfc619ee8064ddfce90cb7" => :high_sierra
+    sha256 "731168ac6c8d436221ca1f8ac75efa5222e8ca9bd0f1cff9c6b1f0aa46a143de" => :sierra
+    sha256 "579378099c4c38cc037496df079047e79486e9dca443f627d05a89609fa07dac" => :el_capitan
   end
 
   keg_only :provided_until_xcode43
