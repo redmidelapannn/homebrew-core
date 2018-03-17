f0786490bed014ab3d410e6e013a0b6687bb19a0
Olivier Halligon <olivier@halligon.net>
2018-03-17 22:37:45 +0100

    swiftgen 5.3.0

--- Formula/swiftgen.rb
+++ Formula/swiftgen.rb
@@ -2,8 +2,8 @@ class Swiftgen < Formula
   desc "Swift code generator for assets, storyboards, Localizable.strings, …"
   homepage "https://github.com/SwiftGen/SwiftGen"
   url "https://github.com/SwiftGen/SwiftGen.git",
-      :tag => "5.2.1",
-      :revision => "21107b1de6180cf5644e512e67e4d60a0d115ac4"
+      :tag => "5.3.0",
+      :revision => "c2b03152f31e779f1deba3811b158ac1670f5b3b"
   head "https://github.com/SwiftGen/SwiftGen.git"
 
   bottle do
@@ -26,17 +26,17 @@ class Swiftgen < Formula
     system "bundle", "exec", "rake", "cli:install[#{bin},#{lib},#{pkgshare}/templates]"
 
     fixtures = {
-      "Resources/Fixtures/XCAssets/Images.xcassets" => "Images.xcassets",
-      "Resources/Fixtures/XCAssets/Colors.xcassets" => "Colors.xcassets",
-      "Resources/Fixtures/Colors/colors.xml" => "colors.xml",
-      "Resources/Fixtures/Strings/Localizable.strings" => "Localizable.strings",
-      "Resources/Fixtures/Storyboards-iOS" => "Storyboards-iOS",
-      "Resources/Fixtures/Fonts" => "Fonts",
-      "Resources/Tests/Expected/XCAssets/swift3-context-all.swift" => "xcassets.swift",
-      "Resources/Tests/Expected/Colors/swift3-context-defaults.swift" => "colors.swift",
-      "Resources/Tests/Expected/Strings/structured-swift3-context-localizable.swift" => "strings.swift",
-      "Resources/Tests/Expected/Storyboards-iOS/swift3-context-all.swift" => "storyboards.swift",
-      "Resources/Tests/Expected/Fonts/swift3-context-defaults.swift" => "fonts.swift",
+      "Tests/Fixtures/Resources/XCAssets/Images.xcassets" => "Images.xcassets",
+      "Tests/Fixtures/Resources/XCAssets/Colors.xcassets" => "Colors.xcassets",
+      "Tests/Fixtures/Resources/Colors/colors.xml" => "colors.xml",
+      "Tests/Fixtures/Resources/Strings/Localizable.strings" => "Localizable.strings",
+      "Tests/Fixtures/Resources/Storyboards-iOS" => "Storyboards-iOS",
+      "Tests/Fixtures/Resources/Fonts" => "Fonts",
+      "Tests/Fixtures/Generated/XCAssets/swift3-context-all.swift" => "xcassets.swift",
+      "Tests/Fixtures/Generated/Colors/swift3-context-defaults.swift" => "colors.swift",
+      "Tests/Fixtures/Generated/Strings/structured-swift3-context-localizable.swift" => "strings.swift",
+      "Tests/Fixtures/Generated/Storyboards-iOS/swift3-context-all.swift" => "storyboards.swift",
+      "Tests/Fixtures/Generated/Fonts/swift3-context-defaults.swift" => "fonts.swift",
     }
     (pkgshare/"fixtures").install fixtures
   end
