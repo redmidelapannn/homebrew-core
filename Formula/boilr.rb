cfe599bdf65199ba34d41600c70648ad7a92a47b
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-17 11:48:58 +0000

    boilr: add 0.3.0 bottle.

--- Formula/boilr.rb
+++ Formula/boilr.rb
@@ -5,6 +5,13 @@ class Boilr < Formula
   version "0.3.0"
   sha256 "81c135073310adbfbfa1b46fabb8467c26e08053340491d604ee9bec886c18d8"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a95a142ae0e33da27edf80c1d5a85f23978435af16cc527405ffa935db8de1e1" => :high_sierra
+    sha256 "a95a142ae0e33da27edf80c1d5a85f23978435af16cc527405ffa935db8de1e1" => :sierra
+    sha256 "a95a142ae0e33da27edf80c1d5a85f23978435af16cc527405ffa935db8de1e1" => :el_capitan
+  end
+
   def install
     bin.install "boilr"
   end
