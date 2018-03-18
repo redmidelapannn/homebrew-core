191e978f7913d372bceb6af27894eff165d6890b
ilovezfs <ilovezfs@icloud.com>
2018-03-18 06:47:45 +0000

    Update fribidi.rb

--- Formula/fribidi.rb
+++ Formula/fribidi.rb
@@ -3,7 +3,6 @@ class Fribidi < Formula
   homepage "https://github.com/fribidi/fribidi"
   url "https://github.com/fribidi/fribidi/releases/download/v1.0.1/fribidi-1.0.1.tar.bz2"
   sha256 "c1b182d70590b6cdb5545bab8149de33b966800f27f2d9365c68917ed5a174e4"
-  revision 1
 
   bottle do
     cellar :any
