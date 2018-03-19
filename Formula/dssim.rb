397fab7a1bb34b40a330e1312a7974cb58f4e445
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-19 05:40:46 +0000

    dssim: update 2.9.7 bottle.

--- Formula/dssim.rb
+++ Formula/dssim.rb
@@ -5,9 +5,10 @@ class Dssim < Formula
   sha256 "4ee60e125efae43f49bf9c4ca849f9cef2b1f86ee1d538da84907faae853eeeb"
 
   bottle do
-    sha256 "cd9660967e876f49480ae7f8f1486ebd7f752edf2a22621b03f2b6bc8bf07f73" => :high_sierra
-    sha256 "923949599d0070fbe2c6938f5847ed6df69097921897ce118d7e9cc8ef48cb40" => :sierra
-    sha256 "34f0204a20f42a08c375b831669e8fdf8b2a8e54a77298f5c35a75403b1c69c7" => :el_capitan
+    rebuild 1
+    sha256 "3587efa781bc8dbfb2b12a35d7e23ac3fe889aac7de71bfda732e34e637507d2" => :high_sierra
+    sha256 "fbc5ba04b95590bf8b13754afc5f8119e643fc6995632b99b726987532d562f0" => :sierra
+    sha256 "231d88d0834c59af861c2dbbc386277556d9bdfb8e5726ab45d217ade54d37e5" => :el_capitan
   end
 
   depends_on "rust" => :build
