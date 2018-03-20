6feee86ede3bd5b575bfc84366125e0ac0cd60c0
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-20 10:31:01 +0000

    parallelstl: add 20171127 bottle.

--- Formula/parallelstl.rb
+++ Formula/parallelstl.rb
@@ -4,6 +4,13 @@ class Parallelstl < Formula
   url "https://github.com/intel/parallelstl/archive/20171127.tar.gz"
   sha256 "4d92881984fc476d382454c60f3d10f79605b54206d8fb332fd3287906163dc8"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "dbbcb3b7bf0509e91e0300cc621d367cb5db9037de587380f656e8b4c827a75a" => :high_sierra
+    sha256 "7c7eecc3c568ea8235b71e7f8a84d36178b8ec70f2606bc119e0ca954a08e2e9" => :sierra
+    sha256 "dbbcb3b7bf0509e91e0300cc621d367cb5db9037de587380f656e8b4c827a75a" => :el_capitan
+  end
+
   depends_on "tbb"
 
   def install
