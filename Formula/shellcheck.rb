9ce50e84ec41c2163fe7c0ca585109ad89519037
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-20 23:51:09 +0000

    shellcheck: update 0.4.7 bottle.

--- Formula/shellcheck.rb
+++ Formula/shellcheck.rb
@@ -11,9 +11,10 @@ class Shellcheck < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "4670b4952117677a171f4e3312d5b6660ad6b443c1e2c65aed8fb17fe46b2342" => :high_sierra
-    sha256 "8eb344f3f20995bd7210f12b337116b0295755bf78990ea824ba160e8e6b0bf9" => :sierra
-    sha256 "7136c486a8faf29e840b96abc02a68420b641261ba3ed252fbc5ac0da7161721" => :el_capitan
+    rebuild 1
+    sha256 "ef0fd41f7a1a10d7e9e8f68b224fb8a45e4311ad329a9fcd59be2ca30f452091" => :high_sierra
+    sha256 "502af78ec1bba372a6185c37e139b2cf7cfeff1c27a5d5b7b395d859783e3e8f" => :sierra
+    sha256 "8e4daf5b0b814c1e08a94770f6f4fb218dab35e3a96754397c98d9bb4b492eeb" => :el_capitan
   end
 
   depends_on "cabal-install" => :build
