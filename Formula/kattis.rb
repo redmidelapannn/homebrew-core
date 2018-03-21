b8cb38319db60f507e466878773ffc39ad83c0f4
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-21 01:58:54 +0000

    kattis: add 40d54c bottle.

--- Formula/kattis.rb
+++ Formula/kattis.rb
@@ -5,6 +5,13 @@ class Kattis < Formula
   version "40d54c"
   sha256 "e0077380c6e479c03c631caf652f02e0d85d5ae288b4f4a79c285bb22f3bc3f5"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "54800fd86d4dbd8f9f5f1ef658ff71cfde2cd8c0f79aee668910052a9bcde7b4" => :high_sierra
+    sha256 "54800fd86d4dbd8f9f5f1ef658ff71cfde2cd8c0f79aee668910052a9bcde7b4" => :sierra
+    sha256 "54800fd86d4dbd8f9f5f1ef658ff71cfde2cd8c0f79aee668910052a9bcde7b4" => :el_capitan
+  end
+
   def install
     bin.install "submit.py" => "kattis"
   end
