ec738383419634ffa3128b4ad82acc5097c95948
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-21 15:18:53 +0000

    elm-format: update 0.6.1-alpha bottle.

--- Formula/elm-format.rb
+++ Formula/elm-format.rb
@@ -12,10 +12,10 @@ class ElmFormat < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d63d07ef26edd91e1b10c4b1286dd271ac7f2958eb5e92aa78bb62ec49f4802a" => :high_sierra
-    sha256 "0d803f1ba6449fc85db9edac0bf55f14c9358868b015559ec2836c799bdf9cb4" => :sierra
-    sha256 "034a1da2a60646992a7571e1879f6ff31ebc43c3f43250689d4b6d6f1c12286d" => :el_capitan
-    sha256 "964df8c9e60c3ab2968fa6d6304beee5d0eefd993001a35e26da279b54e2e543" => :yosemite
+    rebuild 1
+    sha256 "e386920a6d6520e79f4fecd94a11e907c0873219bbe9b89330d6333ede92bb9e" => :high_sierra
+    sha256 "088469773fcd630e31fd196b21d45183cfe11dada5adaec1765b3005d1f99d6f" => :sierra
+    sha256 "1576e23a7fb68afccca88672b19c97a3a96a6e7c84baa842ec90c8378a05e6ef" => :el_capitan
   end
 
   depends_on "cabal-install" => :build
