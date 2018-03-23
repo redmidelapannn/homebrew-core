f6615f7643384c585d79f4534731852b9ecf28a5
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-23 15:22:41 +0000

    xapiand: add 1.0.0 bottle.

--- Formula/xapiand.rb
+++ Formula/xapiand.rb
@@ -5,6 +5,11 @@ class Xapiand < Formula
   sha256 "0cf3b349e99b48882e11ae0cc2985a7ea159dd24b82e7fd03b7d6072ce38b5e4"
   head "https://github.com/Kronuz/Xapiand.git"
 
+  bottle do
+    sha256 "3945530f00492ca45479ded1cd3ce5cc6761a9697c7fd9970506a0783b5942ec" => :high_sierra
+    sha256 "4d08bfb6b2354b7fa4bc58d6c5617307bfb3267df414f19ebe426817f1e6c508" => :sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "xapian"
