e2aa84e04eb8bd8aeae71126a418cdf68710aed6
lcdtyph <lcdtyph@gmail.com>
2018-03-24 21:46:17 +0800

    Update to v1.1.1

--- Formula/goquiet.rb
+++ Formula/goquiet.rb
@@ -1,24 +1,31 @@
+require "language/go"
+
 class Goquiet < Formula
   desc "Shadowsocks plugin that obfuscates the traffic"
   homepage "https://github.com/cbeuw/GoQuiet"
-  url "https://github.com/cbeuw/GoQuiet/archive/v1.0.2.tar.gz"
-  sha256 "bdd7644eb9af4130c5be461b55bb3f3eeb244e2ac2a172208f60e24f662c32c0"
+  url "https://github.com/cbeuw/GoQuiet/archive/v1.1.1.tar.gz"
+  sha256 "55f92df1dcf892d7aaccd0331d917a41286dafb5a7d8fa7b8d2d257dc385314f"
 
   depends_on "go" => :build
 
+  go_resource "github.com/cbeuw/gotfo" do
+    url "https://github.com/cbeuw/gotfo.git",
+        :revision => "e8588a8e0499d45affbdedf940842dbfb2805cd0"
+  end
+
   def install
     ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/cbeuw/GoQuiet").install buildpath.children
+    Language::Go.stage_deps resources, buildpath/"src"
+    (buildpath/"src/github.com/cbeuw").mkpath
+    ln_s buildpath, "src/github.com/cbeuw/GoQuiet"
 
-    cd "src/github.com/cbeuw/GoQuiet" do
-      system "make"
-      bin.install Dir["build/*"]
-      prefix.install_metafiles
-    end
+    system "make"
+    bin.install Dir["build/*"]
+    prefix.install_metafiles
   end
 
   test do
-    assert pipe_output("#{bin}/gq-client -h").start_with? ""
-    assert pipe_output("#{bin}/gq-server -h").start_with? ""
+    assert_match "Usage of", shell_output("#{bin}/gq-client -h 2>&1", 2)
+    assert_match "Usage of", shell_output("#{bin}/gq-server -h 2>&1", 2)
   end
 end
