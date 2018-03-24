cb50bb9dff71f8fc02972c9abbf1573ebe6b6e8b
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-24 13:52:59 +0000

    goquiet: add 1.1.1 bottle.

--- Formula/goquiet.rb
+++ Formula/goquiet.rb
@@ -6,6 +6,13 @@ class Goquiet < Formula
   url "https://github.com/cbeuw/GoQuiet/archive/v1.1.1.tar.gz"
   sha256 "55f92df1dcf892d7aaccd0331d917a41286dafb5a7d8fa7b8d2d257dc385314f"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "49aeaeac8cd8dd43db9b00a66741f033f0f24774fe061bd3de1fc07df01aa4bc" => :high_sierra
+    sha256 "a7d90cd4109b718c2c20f52a2a2fa7cb25f5359a277b20aeb4da5855852e441f" => :sierra
+    sha256 "53c517da00591bab6392373d4087f6bcf629af8af2d8f975f157b8c43a081b40" => :el_capitan
+  end
+
   depends_on "go" => :build
 
   go_resource "github.com/cbeuw/gotfo" do
