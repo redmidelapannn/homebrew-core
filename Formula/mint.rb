32b1ba43c0f4dd8ea16c79ac0359f848900dbd9a
Wolfgang Lutz <wlut@num42.de>
2018-03-25 20:57:15 +0200

    mint 0.7.1 (new formula): implement requested changes

--- Formula/mint.rb
+++ Formula/mint.rb
@@ -3,15 +3,17 @@ class Mint < Formula
   homepage "https://github.com/yonaskolb/Mint"
   url "https://github.com/yonaskolb/Mint/archive/0.7.1.tar.gz"
   sha256 "986dd967b818ee044ed0568135db8be2083ad6c3995fe6281ada9b42b224a2ec"
-  head "https://github.com/yonaskolb/Mint.git"
 
-  depends_on :xcode
+  depends_on :xcode => ["9.0", :build]
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
   end
 
   test do
-    system "#{bin}/mint"
+    # Test by showing the help scree
+    system "#{bin}/mint", "--help"
+    # Test by building and running mint by using mint itself and showing the help
+    system "#{bin}/mint", "run", "yonaskolb/mint@0.7.1", "mint", "--help"
   end
 end
