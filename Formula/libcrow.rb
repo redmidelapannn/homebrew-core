2f7b1ede4c854104901c8cc9ae8083ca52c78d7d
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-26 22:05:29 +0000

    libcrow: update 0.6.2 bottle.

--- Formula/libcrow.rb
+++ Formula/libcrow.rb
@@ -7,7 +7,10 @@ class Libcrow < Formula
   needs :cxx11
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
+    sha256 "a8785e891453af2136c0c78bca798fdcad573459ddfff647f9bd7dc11ffe9cb5" => :high_sierra
+    sha256 "6fd45b14984c8d385264118c1e6514bb6c6f9bfdd85eb9895f985346f62470c4" => :sierra
+    sha256 "f50a614f65b8c2eb72128ac19f999dd56d6fe40351705dff56ab7903eed38d71" => :el_capitan
   end
 
   head do
