c668387b1608a7e8faeaa2982988dcfd4b3ae300
FX Coudert <fxcoudert@gmail.com>
2018-03-27 16:13:00 +0200

    Update jbig2enc.rb

--- Formula/jbig2enc.rb
+++ Formula/jbig2enc.rb
@@ -3,6 +3,7 @@ class Jbig2enc < Formula
   homepage "https://github.com/agl/jbig2enc"
   url "https://github.com/agl/jbig2enc/archive/0.29.tar.gz"
   sha256 "bfcf0d0448ee36046af6c776c7271cd5a644855723f0a832d1c0db4de3c21280"
+  head "https://github.com/agl/jbig2enc.git"
 
   bottle do
     cellar :any
@@ -13,18 +14,13 @@ class Jbig2enc < Formula
     sha256 "0a34215dfcd908571ca6b65d3ca4d79c2758e3a4edbf4d3c8da944a8567fc02f" => :yosemite
   end
 
-  head do
-    url "https://github.com/agl/jbig2enc.git"
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
-
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on "leptonica"
 
   def install
-    system "./autogen.sh" if build.head?
+    system "./autogen.sh"
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
   end
