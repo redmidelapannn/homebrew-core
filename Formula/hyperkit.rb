18f16c05e515e8d11ffe5a56a4caa281a79ac10c
Antoine Lépée <alepee@gmail.com>
2018-03-28 18:22:49 +0200

    hyperkit: switch to git url with tagged release

--- Formula/hyperkit.rb
+++ Formula/hyperkit.rb
@@ -1,7 +1,7 @@
 class Hyperkit < Formula
   desc "Toolkit for embedding hypervisor capabilities in your application"
   homepage "https://github.com/moby/hyperkit"
-  url "https://github.com/moby/hyperkit/archive/v0.20180123.tar.gz"
+  url "https://github.com/moby/hyperkit.git", :tag => "v0.20180123"
   sha256 "382933118da3835056203d3d05923b554f36cc41a555a821516e11ccb7d16bf3"
 
   head do
