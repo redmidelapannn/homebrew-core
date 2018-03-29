225d471d17c07dfc64afea7a6f4dc33d31afaff1
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-29 09:49:37 +0000

    arabica: update 20160214 bottle.

--- Formula/arabica.rb
+++ Formula/arabica.rb
@@ -8,11 +8,10 @@ class Arabica < Formula
 
   bottle do
     cellar :any
-    sha256 "0d3da860ba0b953abb9f70665cc73620ae2b0cd0570dacb55bfe8954ae0d58e7" => :high_sierra
-    sha256 "6a08cb5b8af8d2034569451ad499260acebc610ab390383b20398fc10a6fb115" => :sierra
-    sha256 "185edd120b5759f25d1e69b9f24840eef6a404b1001c90e684546e359cf75928" => :el_capitan
-    sha256 "1882f30edf8da8d98df603a6006c3dce96e21941e4266103366930b3e5a922c2" => :yosemite
-    sha256 "5d247d4d5819106404bc7091e3b6141b4d298c77636bee39bfc524a3c5481e7f" => :mavericks
+    rebuild 1
+    sha256 "afa8421f72a5b6dd8cd8688f518feb173e50921a4d97a80ce98c88803f25453c" => :high_sierra
+    sha256 "f38616c7c9c51351e492b9b3e82228fe2565869c862eee9a5f2b2ac82bd323fd" => :sierra
+    sha256 "832992f0ca5cf85866269a637781de4a2b6c53402b6ddf28b6195926acbfb10d" => :el_capitan
   end
 
   option "without-test", "Skip compile-time make checks (Not Recommended)"
