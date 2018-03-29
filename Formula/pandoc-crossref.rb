622eddf3b184ed03ac47dfa9ee1c60c795083e57
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-29 14:33:33 +0000

    pandoc-crossref: update 0.3.0.3 bottle.

--- Formula/pandoc-crossref.rb
+++ Formula/pandoc-crossref.rb
@@ -10,9 +10,10 @@ class PandocCrossref < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e5863e9c46fa492be1b3f690fd1a20ec781c3607edfe44b28c9834069e7656b6" => :high_sierra
-    sha256 "42e54077bdc05637e1a21f707a9ffb2d57ed07448d71e2dabf3c0fa17ed0b26c" => :sierra
-    sha256 "480dfd80971bca83a1dd9124f5ceec5408a79470e479f2e1e047b73d09898733" => :el_capitan
+    rebuild 1
+    sha256 "56c45356eec9b5e08a620dc62af0923078391ac54d9882ef65de1c7756171112" => :high_sierra
+    sha256 "d5a01d206386410c309c22a479ecd357b5e1407661a8ff587a83230359c4cc93" => :sierra
+    sha256 "29c30bf01ac043f1a8fdcb9435da4d41f6c3a240f4897476497196c13d957437" => :el_capitan
   end
 
   depends_on "cabal-install" => :build
