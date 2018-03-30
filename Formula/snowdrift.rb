775c940badff1c214f2fb1442a3b9695e19f1524
Douglas Muth <douglas_muth@cable.comcast.com>
2018-03-30 19:12:11 -0400

    Snowdrift 1.0: Removed default comments.

--- Formula/snowdrift.rb
+++ Formula/snowdrift.rb
@@ -1,35 +1,14 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Snowdrift < Formula
   desc "App to perform testing and validation of firewall rules"
   homepage "https://comcast.github.io/"
   url "https://github.com/Comcast/snowdrift/archive/snowdrift-1.0.tar.gz"
   sha256 "f48b6beec8f6f93d1e57709f6ff8853352ccea7e21e28b53f9a31dacd3e27e96"
-  # depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    #system "./configure", "--disable-debug",
-    #                      "--disable-dependency-tracking",
-    #                      "--disable-silent-rules",
-    #                      "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    #system "make", "install" # if this fails, try separate make/make install steps
     bin.install "snowdrift"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test snowdrift`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
