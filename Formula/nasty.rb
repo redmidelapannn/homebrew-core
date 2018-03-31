a84d47047e371cc280880569fafa879e6802aad4
BrewTestBot <brew-test-bot@googlegroups.com>
2018-03-31 11:34:26 +0000

    nasty: add 0.6 bottle.

--- Formula/nasty.rb
+++ Formula/nasty.rb
@@ -3,6 +3,13 @@ class Nasty < Formula
   homepage "https://www.vanheusden.com/nasty/"
   url "https://www.vanheusden.com/nasty/nasty-0.6.tgz"
   sha256 "7607256d4672f1c52f2603d7b9691e7250bfe3a9b4f219fcbb61227172a7f6b7"
+  bottle do
+    cellar :any
+    sha256 "a39089e33f8a974750798dcad617939b19e3f9363615cc1ce7f0885e14328de3" => :high_sierra
+    sha256 "b399bd43697daac224fe2a1728b8fd9ad8695877d968e35c2ddc5965d940a057" => :sierra
+    sha256 "6b42dc11f2f4d3c3be12b9a94ceadb5a7047b12a07efd0a524e706244bbb4363" => :el_capitan
+  end
+
   depends_on "gpgme"
 
   def caveats
