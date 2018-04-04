af7a6e22831f41d20a90d5a0efb28862cf570c49
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-04 21:35:28 +0000

    oksh: add 20180111 bottle.

--- Formula/oksh.rb
+++ Formula/oksh.rb
@@ -4,6 +4,13 @@ class Oksh < Formula
   url "https://devio.us/~bcallah/oksh/oksh-20180111.tar.gz"
   sha256 "c15652b503123dd542144c36f12a076fdb89b1fc4c6a8807ce1ec83fa1e0d797"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4c65fd49e8d980a238866d5aa5447f9c4b72365125e8a2a3350b7a67d525a39a" => :high_sierra
+    sha256 "7b8a2407ac082f887fd600063f7820149c468397b55c878fa7cce9a97e24b6b8" => :sierra
+    sha256 "d33379bbfb7080fc43d7a578c3f5e51118fd3316b140a6aafdee8af99dab40a0" => :el_capitan
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}",
                           "--mandir=#{man}"
