4b10d29d6732d02f136d269c0157a2c2edf01f4c
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-04 15:03:50 +0000

    openexr: update 2.2.1 bottle.

--- Formula/openexr.rb
+++ Formula/openexr.rb
@@ -6,12 +6,9 @@ class Openexr < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "12c2585a07369da6668cacffe07ddd7a6b0f9750d107f0d053eefcb992e4df85" => :high_sierra
-    sha256 "0220d0edcb161cb56a1cbe8477cc2ecd45fee320ce48f99c114454421a527198" => :sierra
-    sha256 "a73d66076b03d20908db659b965cee768d82408ddca5196fef8164b779091765" => :el_capitan
-    sha256 "ad973fc780bb731a5ee8be347283903e8343448913e46a23fbdede0145ef980a" => :yosemite
-    sha256 "9c21b70caff58d8d699a58a50249f220b03c23f450a219276782827e6c03ff33" => :mavericks
+    sha256 "739e3acd1ee0a5a4ab6ac31dc6b188745a2724a8e4181804ed059ab2f919d6cd" => :high_sierra
+    sha256 "0415c10b837fb8f0c7c949eb3148a1f0d65b283c8e2a383f79188bf2724f5391" => :sierra
+    sha256 "728e352bef32449853515ce52ed6ec210e30d0115edcb6e530ce84c804307eef" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
