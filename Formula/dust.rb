303ddfa9a6cd75484c13654a16d7a4a632eeb30b
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-04 19:14:02 +0000

    dust: add 0.2.1 bottle.

--- Formula/dust.rb
+++ Formula/dust.rb
@@ -5,6 +5,12 @@ class Dust < Formula
   sha256 "ea597ad1fa36b1e05fc74b0b98d89e53e79f9d420959de8eadb0e8d05587e265"
   head "https://github.com/bootandy/dust.git"
 
+  bottle do
+    sha256 "7d2ad5235867a531518215be445d3933bf14603e56d9fce4141f728974539757" => :high_sierra
+    sha256 "610eb7669d52944d25686ebb043fb7a908e30ca014072576acd9494a421bd8a6" => :sierra
+    sha256 "463deb5da7eb3983e706cfd0e70cbc5139c22ce3e8675858fc43ea8ef2a1bb4e" => :el_capitan
+  end
+
   depends_on "rust" => :build
 
   def install
