c44998b37540c6142b7089bba5098e632f11fadc
Michael K. Werle <mkw-github@coruscations.com>
2018-04-05 09:02:37 -0500

    PR feedback

--- Formula/click.rb
+++ Formula/click.rb
@@ -3,12 +3,11 @@ class Click < Formula
   homepage "https://github.com/databricks/click"
   url "https://github.com/databricks/click/archive/v0.3.2.tar.gz"
   sha256 "eed648409bf78a05658a9d097e5099ca17bf19df70122e2067859ae94c5575d5"
-  head "https://github.com/databricks/click.git"
+
   depends_on "rust" => :build
 
   def install
-    system "cargo", "build", "--release"
-    bin.install "target/release/click"
+    system "cargo", "install", "--root", prefix
   end
 
   test do
