3c0c23ffbca3ba2fd1bd104a83c14d51fb725e30
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-06 01:59:16 +0000

    buildapp: update 1.5.6_1 bottle.

--- Formula/buildapp.rb
+++ Formula/buildapp.rb
@@ -7,9 +7,10 @@ class Buildapp < Formula
   head "https://github.com/xach/buildapp.git"
 
   bottle do
-    sha256 "aa14506cc1ab8eb712c9b1fdb71af45c9041b327c9ecdf9d48541b652e3da3be" => :high_sierra
-    sha256 "15e1be1e7928770abfdb801e67435b9d7c1bc36923bc6d9d5281d9ceab0d9abd" => :sierra
-    sha256 "49d62ce2b8564d5456725d1a7cedafb38690d9bcdf5e7b6f279805665ed65833" => :el_capitan
+    rebuild 1
+    sha256 "7e179a81db9bfcc85b20b310e1587c04d21179b0643045ea7f198661ca5dead8" => :high_sierra
+    sha256 "af5b56e6a8c82c8880510f6383435d7cc545ef168292183abe9da9ee1e6f1889" => :sierra
+    sha256 "191183a944537ffeb67fd7d2297e3dcfd1825ac3f1af4db629512f4bb8e9465f" => :el_capitan
   end
 
   depends_on "sbcl"
