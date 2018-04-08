932d41f9e6d60a81bf0821e09f8f098bf8f72bee
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-08 07:43:13 +0000

    cxxtest: update 4.4 bottle.

--- Formula/cxxtest.rb
+++ Formula/cxxtest.rb
@@ -7,12 +7,10 @@ class Cxxtest < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b6016aba933e8e047299e4a4dae83549eb8679a9e18e11937d90877b9e94af62" => :high_sierra
-    sha256 "09aa93c60544867a44c3ad711f7ad9207f3f097505ce658e12d4d8ae11287c82" => :sierra
-    sha256 "d35cfbbea5de989734e9f859531b203dffc870fdf931a5a7f12302adc7354c87" => :el_capitan
-    sha256 "a69d95d4c027024e6c14a999c679106cf6259e22bb748205d93dbc5d0596a8e3" => :yosemite
-    sha256 "2e8e487aac953d698f38f89ae9946572f8d072ec35b91683aa66bc147cec2fa4" => :mavericks
-    sha256 "de1e98e94198507c65ecb17ae240b995ae8f03dadeafbde27bb704df2e10737c" => :mountain_lion
+    rebuild 1
+    sha256 "39acd580087caf84ed99af51ff53ec005d9b7b9b1104f6db78168c96dacc7dba" => :high_sierra
+    sha256 "39acd580087caf84ed99af51ff53ec005d9b7b9b1104f6db78168c96dacc7dba" => :sierra
+    sha256 "39acd580087caf84ed99af51ff53ec005d9b7b9b1104f6db78168c96dacc7dba" => :el_capitan
   end
 
   depends_on "python@2"
