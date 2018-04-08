872ba56f171c1d14483eb53db1d9060ae4824f78
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-08 07:43:58 +0000

    spoof-mac: update 2.1.1 bottle.

--- Formula/spoof-mac.rb
+++ Formula/spoof-mac.rb
@@ -7,11 +7,10 @@ class SpoofMac < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f7972b66a491d8513262e3bdb9098850fe91e4c8e0e3d7a12efd24994e47dda5" => :high_sierra
-    sha256 "baa45eb53fc9e1a713ca7693351409f66c833834e7c7d0838115bcf5f38df555" => :sierra
-    sha256 "6c97eaa9a82f7eadb5c1127a0dcf9b0f9f1837e36d3e9978e989471322f42b4a" => :el_capitan
-    sha256 "927644491edf064dcc3145e05107737c3e571b55989ae8d539bf0b499da3685c" => :yosemite
-    sha256 "f7dc1529dd2c83d8bf8667d170299aa592910bb4918174b23f6a9b7d3555084e" => :mavericks
+    rebuild 1
+    sha256 "816a67857a56bc5cf84520aa8b3b4aea6ae5e4b107b49942be0b4ecae0007f69" => :high_sierra
+    sha256 "816a67857a56bc5cf84520aa8b3b4aea6ae5e4b107b49942be0b4ecae0007f69" => :sierra
+    sha256 "816a67857a56bc5cf84520aa8b3b4aea6ae5e4b107b49942be0b4ecae0007f69" => :el_capitan
   end
 
   depends_on "python@2"
