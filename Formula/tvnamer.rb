1fd22360770651685dbbfa6a1656c4bd03bc5b8a
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-08 07:43:22 +0000

    tvnamer: update 2.4 bottle.

--- Formula/tvnamer.rb
+++ Formula/tvnamer.rb
@@ -7,10 +7,10 @@ class Tvnamer < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "4f1ae20737bed54785fce92bbdd67c78f3b20553312d02051f72b8906211d507" => :high_sierra
-    sha256 "c1a03bc94b59d7e044445daa9c0037b46230271d457d658aba3e7e55e9291dd0" => :sierra
-    sha256 "546b13ef89b777d7076948d993adc9a57c664b2bbc9cb2f429cf042f3d61d165" => :el_capitan
-    sha256 "546b13ef89b777d7076948d993adc9a57c664b2bbc9cb2f429cf042f3d61d165" => :yosemite
+    rebuild 1
+    sha256 "7156ebb147673afb1147ff44c4f1a4d760f8f263fdc58382042db1c2d522b282" => :high_sierra
+    sha256 "7156ebb147673afb1147ff44c4f1a4d760f8f263fdc58382042db1c2d522b282" => :sierra
+    sha256 "7156ebb147673afb1147ff44c4f1a4d760f8f263fdc58382042db1c2d522b282" => :el_capitan
   end
 
   depends_on "python@2"
