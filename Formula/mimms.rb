3b92c1522363b2d30566c3e8777ef756749a18cb
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-08 07:42:51 +0000

    mimms: update 3.2.1 bottle.

--- Formula/mimms.rb
+++ Formula/mimms.rb
@@ -8,11 +8,10 @@ class Mimms < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "c3e7cc6b99acb9af2a0e80029f0f0e7e4714430e21af830ee5ec59cbd6c1e447" => :high_sierra
-    sha256 "a594aa43b8705e1fd8945ba78b9374eb82c805cc75e92a4ea10375fb8b443529" => :sierra
-    sha256 "9996da3980f7307ef745866b671fc56045dc951ff48333ea4e121acaa2d476ae" => :el_capitan
-    sha256 "88b2e83cfdf9abca4ff712bddacb76611ce29aa2c58218c05e8abf3ed7d0be2b" => :yosemite
+    rebuild 2
+    sha256 "c3c0e0f3129d1755ae2114efa2e9edb9e86adf2f326005d1816b23a3183b1372" => :high_sierra
+    sha256 "0e9deb9e54b6b488fc0ca08dc1b0a7e3e2f703d42951b8abaa315b48b2a78c2f" => :sierra
+    sha256 "e3180cfcdfeffb0f6a246e05cf66a88cb63afe0630fb869a30a12de70657a955" => :el_capitan
   end
 
   depends_on "python@2"
