13008e9520a2c6156ac217064cae8787a122123b
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-08 07:42:45 +0000

    hachoir-metadata: update 1.3.3 bottle.

--- Formula/hachoir-metadata.rb
+++ Formula/hachoir-metadata.rb
@@ -6,12 +6,10 @@ class HachoirMetadata < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "bdf181593e61e9a545f79f77b10674804f8fb63c14967af927a9211a0c98f422" => :high_sierra
-    sha256 "cbc769eacb82b386dd720090cee95538ec14f6c2bc947a248099a66ef6e123e4" => :sierra
-    sha256 "b2a11194217ff9b97338b9c10147fd25f3e83213d0579bc88cef9689895025a3" => :el_capitan
-    sha256 "316c9a2496302aef88844f80b8c7f63f69dd991c1f183b46228636b4780c7d7a" => :yosemite
-    sha256 "73c9aaf96521afa7bac1942ea16a5af6c0baaba1537c399c22e493ea24ae7186" => :mavericks
+    rebuild 2
+    sha256 "d06964f8ccde602a24fd246af7940c4f78b3cc604070ad9ab2174fdb3069d4e3" => :high_sierra
+    sha256 "d06964f8ccde602a24fd246af7940c4f78b3cc604070ad9ab2174fdb3069d4e3" => :sierra
+    sha256 "d06964f8ccde602a24fd246af7940c4f78b3cc604070ad9ab2174fdb3069d4e3" => :el_capitan
   end
 
   depends_on "python@2"
