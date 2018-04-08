0f47b448d6ed0f0e0bc049e9c364f03a3ce18c93
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-08 07:42:54 +0000

    muttils: update 1.3 bottle.

--- Formula/muttils.rb
+++ Formula/muttils.rb
@@ -7,12 +7,10 @@ class Muttils < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "cc13c555f3f6fe0527b1b2ef61f50d3e01fe99663d9ba76d8ea912c7114bb9f0" => :high_sierra
-    sha256 "5c8920713f0a4abc1e6834dda10b40295010dfb96589196ffb319476801f3178" => :sierra
-    sha256 "6843b7372098864c53199d84759724bcbf2c1ab36fc74c15e56405351e43fbc9" => :el_capitan
-    sha256 "d976f7445a3142ff1c311cf19302b5fcc2976b0a26c0bb48e57e157bd4c7002f" => :yosemite
-    sha256 "01f0c26274540336fa829a8718bb0c3a2a5b5aa3c96c1f4ec7cf79e6263b837b" => :mavericks
-    sha256 "63e819c0bb96a56ed0f159ab816aeac84805a52333bd23298a17cd2abddcb17b" => :mountain_lion
+    rebuild 1
+    sha256 "debc7a7c47071097852f59fb53fd253f76fe17896420137eb0f9090a61a1f307" => :high_sierra
+    sha256 "debc7a7c47071097852f59fb53fd253f76fe17896420137eb0f9090a61a1f307" => :sierra
+    sha256 "debc7a7c47071097852f59fb53fd253f76fe17896420137eb0f9090a61a1f307" => :el_capitan
   end
 
   depends_on "python@2"
