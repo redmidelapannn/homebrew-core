59f9e2b0a3b8c1dbe9765344381444ee2642e632
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-08 07:43:32 +0000

    clingo: update 5.2.2 bottle.

--- Formula/clingo.rb
+++ Formula/clingo.rb
@@ -5,10 +5,10 @@ class Clingo < Formula
   sha256 "da1ef8142e75c5a6f23c9403b90d4f40b9f862969ba71e2aaee9a257d058bfcf"
 
   bottle do
-    rebuild 1
-    sha256 "c6d1544f386880a1c16c81f56fddc3bef9efccda62d624dc646e6e785a236301" => :high_sierra
-    sha256 "69df2be35e41efc97550deef3f12a66210a77838b4238e9046edf2aab9a38a25" => :sierra
-    sha256 "7c9b87455116a0538ff4e8de9a2366a7ae7cb0020ef9c8a38450a72efd4903ce" => :el_capitan
+    rebuild 2
+    sha256 "fee978cde6f5ff447bed05af2f899055c4a577ec4d90f0e1df771649064a69c4" => :high_sierra
+    sha256 "ee227d1fa9affef0d589a9190adf99418e1b23aafc0a87423295ca5f603923da" => :sierra
+    sha256 "1edafd5e332f7dcb629a524300a03634aed319c03a768851d99a43c1a189c4e5" => :el_capitan
   end
 
   depends_on "cmake" => :build
