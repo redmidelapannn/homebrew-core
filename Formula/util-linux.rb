b47485a68071c04dcf10e12e5add6013232d5a9d
Peter van Dijk <peter@7bits.nl>
2018-04-09 21:08:11 +0200

    remove things that macOS already ships

--- Formula/util-linux.rb
+++ Formula/util-linux.rb
@@ -1,7 +1,7 @@
 class UtilLinux < Formula
   desc "Collection of Linux utilities"
   homepage "https://github.com/karelzak/util-linux"
-  url "https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v2.32/util-linux-2.32.tar.xz"
+  url "https://www.kernel.org/pub/linux/utils/util-linux/v2.32/util-linux-2.32.tar.xz"
   sha256 "6c7397abc764e32e8159c2e96042874a190303e77adceb4ac5bd502a272a4734"
 
   def install
@@ -9,12 +9,36 @@ class UtilLinux < Formula
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}",
-                          "--disable-ipcs",     # does not build on macOS
-                          "--disable-ipcrm",    # does not build on macOS
-                          "--disable-wall",     # already comes with macOS
-                          "--disable-libuuid"   # conflicts with ossp-uuid
+                          "--disable-ipcs",        # does not build on macOS
+                          "--disable-ipcrm",       # does not build on macOS
+                          "--disable-wall",        # already comes with macOS
+                          "--disable-libuuid",     # conflicts with ossp-uuid
+                          "--disable-libsmartcols" # macOS already ships 'column'
 
     system "make", "install"
+
+    ["cal",   # these already come with macOS
+     "col",
+     "colcrt",
+     "colrm",
+     "getopt",
+     "hexdump",
+     "logger",
+     "nologin",
+     "look",
+     "mesg",
+     "more",
+     "renice",
+     "rev",
+     "ul",
+     "whereis"
+    ].each do |prog|
+      rm_f bin/prog
+      rm_f sbin/prog
+      rm_f man1/"#{prog}.1"
+      rm_f man8/"#{prog}.8"
+      rm_f share/"bash-completion"/"completions"/prog
+    end
   end
 
   test do
