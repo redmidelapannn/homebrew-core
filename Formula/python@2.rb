25e23160fac3fbf21acf778b8cf77ce491b05ba2
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-09 10:06:53 +0000

    python@2: update 2.7.14_3 bottle.

--- Formula/python@2.rb
+++ Formula/python@2.rb
@@ -7,10 +7,10 @@ class PythonAT2 < Formula
   head "https://github.com/python/cpython.git", :branch => "2.7"
 
   bottle do
-    rebuild 1
-    sha256 "02d0b530c0fee00c7564a24ae9c0bde6c3b9028eb2a1b88e1b09a69481cb7ae5" => :high_sierra
-    sha256 "8ad21131c5fbc3843d5f9aa754ea5c352de0433c69f693f56e465ac3f7b96424" => :sierra
-    sha256 "99865efdb594d21e65bc74674267a73081fe1087043263b1f12675c046414d62" => :el_capitan
+    rebuild 2
+    sha256 "8425048e35e2d895047ede83dfd54fb53624d4bbfd23a615e9cfbe0c71a68b0f" => :high_sierra
+    sha256 "a0cef6d8c7b3ecefa96d6338b21e2b4ab64de254786b6404cf98051736e93711" => :sierra
+    sha256 "bb76ca169888c6ff8e07eb9735d98672629e9dde44b6c76caa5baf03f0eae8f0" => :el_capitan
   end
 
   # Please don't add a wide/ucs4 option as it won't be accepted.
