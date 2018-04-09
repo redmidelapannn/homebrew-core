ce61d7e9fb3ef3d79a159e2fdfdbe8f70bb834ea
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-09 12:01:44 +0000

    ansilove: add 3.0.8 bottle.

--- Formula/ansilove.rb
+++ Formula/ansilove.rb
@@ -4,6 +4,13 @@ class Ansilove < Formula
   url "https://github.com/ansilove/ansilove/archive/3.0.8.tar.gz"
   sha256 "655472d31a9ec5a5ba29f5c15485d8647526ff8b0c9968d71f4267a9aa9bb544"
 
+  bottle do
+    cellar :any
+    sha256 "0ebe22a6150177cef2fa89ab9da8719bcc584aa82b2dd8717d57e9f85e96be9f" => :high_sierra
+    sha256 "0fe022847878c9d37acd6dfbe6c3e5de530da675028c48fb861cb53aefad2d12" => :sierra
+    sha256 "74a38423d24b76a40af6220f8acdb0a079e5ab9a09df80d20d7d5d3f0a4887f6" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on "gd"
 
