370e594616be01410c178f472b3c8ca19065fd32
Stephen Belanger <stephen.belanger@elastic.co>
2018-04-09 20:29:54 -0700

    gjstest: patch for V8 6.5

--- Formula/gjstest.rb
+++ Formula/gjstest.rb
@@ -1,10 +1,19 @@
 class Gjstest < Formula
   desc "Fast JavaScript unit testing framework that runs on the V8 engine"
   homepage "https://github.com/google/gjstest"
-  url "https://github.com/google/gjstest/archive/v1.0.2.tar.gz"
-  sha256 "7bf0de1c4b880b771a733c9a5ce07c71b93f073e6acda09bec7e400c91c2057c"
-  revision 12
+  revision 13
   head "https://github.com/google/gjstest.git"
+
+  stable do
+    url "https://github.com/google/gjstest/archive/v1.0.2.tar.gz"
+    sha256 "7bf0de1c4b880b771a733c9a5ce07c71b93f073e6acda09bec7e400c91c2057c"
+
+    # Apply upstream patch for v8 6.5 compatibility
+    patch do
+      url "https://github.com/google/gjstest/commit/8218472140582e2efd1a9dfb2d7b969610a02d75.patch?full_index=1"
+      sha256 "736a9ac4e963801730ac8d482ed4817ebc5c57d5395b7a05236b7841423218dd"
+    end
+  end
 
   bottle do
     sha256 "23787738847f2dfbe0c3d6943c846ec14530b8b1a79441b3af8d118adc5b3671" => :high_sierra
