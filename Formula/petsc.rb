1c860c38a514b91702b8e7eb85ef7e63fe0bb736
Francois Hebert <fmh33@cornell.edu>
2018-04-10 13:13:18 -0700

    Update PETSc to v3.9, remove incompatible dep hypre

--- Formula/petsc.rb
+++ Formula/petsc.rb
@@ -1,8 +1,8 @@
 class Petsc < Formula
   desc "Scalable solution of models that use partial differential equations"
   homepage "https://www.mcs.anl.gov/petsc/index.html"
-  url "http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-lite-3.7.6.tar.gz"
-  sha256 "b07f7b4e57d75f982787bd8169f7b8debd5aee2477293da230ab6c80a52c6ef8"
+  url "http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-lite-3.9.0.tar.gz"
+  sha256 "a233e0d7f69c98504a1c3548162c6024f7797dde5556b83b0f98ce7326251ca1"
   head "https://bitbucket.org/petsc/petsc", :using => :git
 
   depends_on "cmake" => :build
@@ -11,7 +11,6 @@ class Petsc < Formula
 
   depends_on "hdf5"
   depends_on "hwloc"
-  depends_on "hypre"
   depends_on "metis"
   depends_on "netcdf"
   depends_on "scalapack"
@@ -32,7 +31,6 @@ class Petsc < Formula
               --with-x=0
               --with-hdf5-dir=#{Formula["hdf5"].opt_prefix}
               --with-hwloc-dir=#{Formula["hwloc"].opt_prefix}
-              --with-hypre-dir=#{Formula["hypre"].opt_prefix}
               --with-metis-dir=#{Formula["metis"].opt_prefix}
               --with-netcdf-dir=#{Formula["netcdf"].opt_prefix}
               --with-scalapack-dir=#{Formula["scalapack"].opt_prefix}
