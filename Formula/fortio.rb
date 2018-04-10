8860204a01e81f09331faa8c8b9d3988c8272171
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-10 16:22:52 +0000

    fortio: add 0.9.0 bottle.

--- Formula/fortio.rb
+++ Formula/fortio.rb
@@ -7,6 +7,12 @@ class Fortio < Formula
       :revision => "e50943e8e525197f36f9b4f81464615c063a0a65"
   head "https://github.com/istio/fortio.git"
 
+  bottle do
+    sha256 "85615927eebe8e6b7b6315e0c28108b082967bd0534fc1a102e7138b7c88cb02" => :high_sierra
+    sha256 "0a0ce6f9eb8eb7268fcfe8a76b3e853b44217bb8b2ea363869d748dc8e99dd32" => :sierra
+    sha256 "e16af70e0821c85b283f304c36a45b127e84c492a9c07eeac09bf384b823dbca" => :el_capitan
+  end
+
   depends_on "go" => :build
 
   def install
