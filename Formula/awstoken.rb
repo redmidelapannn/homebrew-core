0bfb1592bcf176cd12943b312fa0b9c3ec7bf0e5
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-11 01:09:04 +0000

    awstoken: add 0.1.0 bottle.

--- Formula/awstoken.rb
+++ Formula/awstoken.rb
@@ -3,6 +3,13 @@ class Awstoken < Formula
   homepage "https://github.com/vandot/awstoken"
   url "https://github.com/vandot/awstoken/archive/v0.1.0.tar.gz"
   sha256 "6c8389c344dcdb9455e4d8bbddaa41de5d2cb6681d2d947bcbf01501bc4a458e"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "54416a6e0ef5870f90bf1856ac5297f4f90010ad8426375524a704d63390d778" => :high_sierra
+    sha256 "54416a6e0ef5870f90bf1856ac5297f4f90010ad8426375524a704d63390d778" => :sierra
+    sha256 "54416a6e0ef5870f90bf1856ac5297f4f90010ad8426375524a704d63390d778" => :el_capitan
+  end
+
   depends_on "awscli"
   depends_on "jq"
 
