20e8efeebaf44974ba74a24fee5031c131b33d21
Ilya Puchka <ilyapuchka@gmail.com>
2018-04-11 22:31:59 +0100

    sourcery 0.12.0

--- Formula/sourcery.rb
+++ Formula/sourcery.rb
@@ -1,8 +1,8 @@
 class Sourcery < Formula
   desc "Meta-programming for Swift, stop writing boilerplate code"
   homepage "https://github.com/krzysztofzablocki/Sourcery"
-  url "https://github.com/krzysztofzablocki/Sourcery/archive/0.11.2.tar.gz"
-  sha256 "0be23addc7ae986158cbef263393539f9e07f67969a1d51b82aec78cb7d0d21a"
+  url "https://github.com/krzysztofzablocki/Sourcery/archive/0.12.0.tar.gz"
+  sha256 "2b8f4919aea02af61f8f875f0fb33872c0ec1ae5f4c7aebb2d6842c206aff6a6"
   head "https://github.com/krzysztofzablocki/Sourcery.git"
 
   bottle do
