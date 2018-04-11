839592f34ad5b9ad9ff31c069185730d03b3563e
Francois Hebert <fmh33@cornell.edu>
2018-04-11 02:20:55 -0700

    Fix per fxcoudert's suggestions

--- Formula/petsc.rb
+++ Formula/petsc.rb
@@ -3,7 +3,6 @@ class Petsc < Formula
   homepage "https://www.mcs.anl.gov/petsc/index.html"
   url "http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-lite-3.9.0.tar.gz"
   sha256 "a233e0d7f69c98504a1c3548162c6024f7797dde5556b83b0f98ce7326251ca1"
-  head "https://bitbucket.org/petsc/petsc", :using => :git
 
   depends_on "gcc"
   depends_on "hdf5"
@@ -15,24 +14,22 @@ class Petsc < Formula
   depends_on "suite-sparse"
 
   def install
-    args = %W[CC=mpicc
-              CXX=mpicxx
-              F77=mpif77
-              FC=mpif90
-              --prefix=#{prefix}
-              --with-debugging=0
-              --with-scalar-type=real
-              --with-shared-libraries=1
-              --with-ssl=0
-              --with-x=0
-              --with-hdf5-dir=#{Formula["hdf5"].opt_prefix}
-              --with-hwloc-dir=#{Formula["hwloc"].opt_prefix}
-              --with-metis-dir=#{Formula["metis"].opt_prefix}
-              --with-netcdf-dir=#{Formula["netcdf"].opt_prefix}
-              --with-scalapack-dir=#{Formula["scalapack"].opt_prefix}
-              --with-suitesparse-dir=#{Formula["suite-sparse"].opt_prefix}]
-
-    system "./configure", *args
+    system "./configure", "CC=mpicc",
+                          "CXX=mpicxx",
+                          "F77=mpif77",
+                          "FC=mpif90",
+                          "--prefix=#{prefix}",
+                          "--with-debugging=0",
+                          "--with-scalar-type=real",
+                          "--with-shared-libraries=1",
+                          "--with-ssl=0",
+                          "--with-x=0",
+                          "--with-hdf5-dir=#{Formula["hdf5"].opt_prefix}",
+                          "--with-hwloc-dir=#{Formula["hwloc"].opt_prefix}",
+                          "--with-metis-dir=#{Formula["metis"].opt_prefix}",
+                          "--with-netcdf-dir=#{Formula["netcdf"].opt_prefix}",
+                          "--with-scalapack-dir=#{Formula["scalapack"].opt_prefix}",
+                          "--with-suitesparse-dir=#{Formula["suite-sparse"].opt_prefix}"
     system "make", "all"
     system "make", "install"
   end
@@ -40,6 +37,7 @@ class Petsc < Formula
   test do
     test_case = "#{pkgshare}/examples/src/ksp/ksp/examples/tutorials/ex1.c"
     system "mpicc", test_case, "-I#{include}", "-L#{lib}", "-lpetsc"
-    assert (`./a.out | grep 'Norm of error' | awk '{print $4}'`.to_f < 1.0e-13)
+    output = shell_output("./a.out | grep 'Norm of error' | awk '{print $4}'")
+    assert(output.to_f < 1.0e-13)
   end
 end
