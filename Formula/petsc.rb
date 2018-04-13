7e0624ef47df0356c7d128de10386dd54447a861
Francois Hebert <fmh33@cornell.edu>
2018-04-13 14:02:13 -0700

    Fix PETSc description

--- Formula/petsc.rb
+++ Formula/petsc.rb
@@ -1,6 +1,6 @@
 class Petsc < Formula
-  desc "Scalable solution of models that use partial differential equations"
-  homepage "https://www.mcs.anl.gov/petsc/index.html"
+  desc "Tools for solving partial differential equations in parallel"
+  homepage "https://www.mcs.anl.gov/petsc/"
   url "http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-lite-3.9.0.tar.gz"
   sha256 "a233e0d7f69c98504a1c3548162c6024f7797dde5556b83b0f98ce7326251ca1"
 
