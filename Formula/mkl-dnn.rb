2b56b8d9adaed2f91acaf88d16dd1230a2ce7aa6
ilovezfs <ilovezfs@icloud.com>
2018-04-13 03:38:27 +0000

    Update mkl-dnn.rb

--- Formula/mkl-dnn.rb
+++ Formula/mkl-dnn.rb
@@ -3,21 +3,16 @@ class MklDnn < Formula
   homepage "https://01.org/mkl-dnn"
   url "https://github.com/intel/mkl-dnn/archive/v0.13.tar.gz"
   sha256 "d2cfd93a70cfe86ebe054477c530c9b5c1218b70f75856eb6d1956c68ee89e8f"
-  head "https://github.com/intel/mkl-dnn.git"
-
-  option "without-doxygen", "Do not build HTML documentation"
 
   depends_on "cmake" => :build
-  depends_on "doxygen" => [:build, :recommended]
+  depends_on "doxygen" => :build
   depends_on :arch => :x86_64
 
   def install
-    mkdir "build" do
-      system "cmake", "..", *std_cmake_args
-      system "make"
-      system "make", "doc" if build.with? "doxygen"
-      system "make", "install"
-    end
+    system "cmake", ".", *std_cmake_args
+    system "make"
+    system "make", "doc"
+    system "make", "install"
   end
 
   test do
