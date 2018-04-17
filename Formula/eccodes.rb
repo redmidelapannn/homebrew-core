eeb1a5539c0df30b164ad251cfe297f93f6d6824
Jens-Uwe Mager <jum@anubis.han.de>
2018-04-17 16:47:36 -0600

    Changes as per recommendations from @ilovezfs.

--- Formula/eccodes.rb
+++ Formula/eccodes.rb
@@ -3,26 +3,19 @@ class Eccodes < Formula
   homepage "https://software.ecmwf.int/wiki/display/ECC/ecCodes+Home"
   url "https://software.ecmwf.int/wiki/download/attachments/45757960/eccodes-2.7.0-Source.tar.gz"
   sha256 "118f46cf6f800585580a5bc838128537ab0879073e9fcded49cd374e4c8d8e6a"
+
   depends_on "cmake" => :build
   depends_on "gcc" # for gfortran
   depends_on "numpy"
-  depends_on "jasper" => :recommended
-  depends_on "libpng" => :optional
+  depends_on "libpng"
+  depends_on "jasper"
 
   def install
-    # Fix "no member named 'inmem_' in 'jas_image_t'"
-    inreplace "src/grib_jasper_encoding.c", "image.inmem_    = 1;", ""
 
     inreplace "CMakeLists.txt", "find_package( OpenJPEG )", ""
 
     mkdir "build" do
       args = std_cmake_args
-      args << "-DBUILD_SHARED_LIBS=OFF" if build.with? "static"
-
-      if build.with? "libpng"
-        args << "-DPNG_PNG_INCLUDE_DIR=#{Formula["libpng"].opt_include}"
-        args << "-DENABLE_PNG=ON"
-      end
 
       system "cmake", "..", "-DENABLE_NETCDF=OFF", *args
       system "make", "install"
