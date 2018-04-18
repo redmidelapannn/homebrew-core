0ec04288a31966914d66ca4d184d80e7c9946847
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-18 04:24:08 +0000

    jumanpp: update 1.02 bottle.

--- Formula/jumanpp.rb
+++ Formula/jumanpp.rb
@@ -5,10 +5,10 @@ class Jumanpp < Formula
   sha256 "01fa519cb1b66c9cccc9778900a4048b69b718e190a17e054453ad14c842e690"
 
   bottle do
-    sha256 "87555f0efa065cd86d3344c8304972567d4b8a84ef1c0e53acb09afabbd1fa9e" => :high_sierra
-    sha256 "afddd3445d86fa1969611b413d0ae460fdaa7b106cdf5edf6ce0bf9d14689a49" => :sierra
-    sha256 "4b4dd5ca55ba7d380a9a6bc7dda462c3825aa8650e9dc6b131e53fadbf64dc63" => :el_capitan
-    sha256 "d53d25e49f4bd8cddd2657ee09eeaec56844996b10445c15561be1a12977a888" => :yosemite
+    rebuild 1
+    sha256 "1cdaa052099a09388965e5f022fff97bb155329356a3d6d7e975efecb34b0261" => :high_sierra
+    sha256 "23f20b3639dce1af9671ba4a8eaee5ff7079646a71084a934fcb0b327a9e7e03" => :sierra
+    sha256 "5efc10a50990918dea354f444f156aebc033ac3e93a2a2c3c51ff21f4e4725c9" => :el_capitan
   end
 
   depends_on "boost-build" => :build
