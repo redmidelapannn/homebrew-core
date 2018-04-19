d865b1922776494c28f5c6a7177a5579d4ff3358
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-19 13:30:41 +0000

    eless: add 0.5 bottle.

--- Formula/eless.rb
+++ Formula/eless.rb
@@ -4,6 +4,13 @@ class Eless < Formula
   url "https://github.com/kaushalmodi/eless/archive/v0.5.tar.gz"
   sha256 "b4da2c7c223996681bb951d50e0d542d8df04baf765c16412fa0848cdb2b3a3d"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "df120f0598d3202b4aec9e8803bedb7591f2e96bf8016bcae6bd035a3801af21" => :high_sierra
+    sha256 "df120f0598d3202b4aec9e8803bedb7591f2e96bf8016bcae6bd035a3801af21" => :sierra
+    sha256 "df120f0598d3202b4aec9e8803bedb7591f2e96bf8016bcae6bd035a3801af21" => :el_capitan
+  end
+
   depends_on "emacs"
 
   def install
