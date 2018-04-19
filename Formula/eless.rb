ff06f93c4f253ca720bee9da99496bb8709b4ad8
ilovezfs <ilovezfs@icloud.com>
2018-04-19 06:21:49 -0700

    Update eless.rb

--- Formula/eless.rb
+++ Formula/eless.rb
@@ -3,7 +3,6 @@ class Eless < Formula
   homepage "https://eless.scripter.co/"
   url "https://github.com/kaushalmodi/eless/archive/v0.5.tar.gz"
   sha256 "b4da2c7c223996681bb951d50e0d542d8df04baf765c16412fa0848cdb2b3a3d"
-  head "https://github.com/kaushalmodi/eless.git"
 
   depends_on "emacs"
 
