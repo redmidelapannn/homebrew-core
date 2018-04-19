950a9d29d7e1dd3d740a394d057eab913f9787e6
ilovezfs <ilovezfs@icloud.com>
2018-04-19 06:20:08 -0700

    Update eless.rb

--- Formula/eless.rb
+++ Formula/eless.rb
@@ -1,6 +1,6 @@
 class Eless < Formula
-  desc "Better 'less' using Emacs view-mode and Bash"
-  homepage "https://eless.scripter.co"
+  desc "Better `less` using Emacs view-mode and Bash"
+  homepage "https://eless.scripter.co/"
   url "https://github.com/kaushalmodi/eless/archive/v0.5.tar.gz"
   sha256 "b4da2c7c223996681bb951d50e0d542d8df04baf765c16412fa0848cdb2b3a3d"
   head "https://github.com/kaushalmodi/eless.git"
