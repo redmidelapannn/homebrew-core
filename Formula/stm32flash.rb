3601978411d9482397e8077ccd03875a4907d6e9
Rickard von Essen <rickard.von.essen@gmail.com>
2018-04-20 16:41:05 +0200

    Fixed download url

--- Formula/stm32flash.rb
+++ Formula/stm32flash.rb
@@ -1,7 +1,7 @@
 class Stm32flash < Formula
   desc "Open source flash program for STM32 using the ST serial bootloader"
   homepage "https://sourceforge.net/projects/stm32flash/"
-  url "https://downloads.sourceforge.net/projects/stm32flash/files/stm32flash-0.5.tar.gz"
+  url "https://downloads.sourceforge.net/project/stm32flash/stm32flash-0.5.tar.gz"
   sha256 "97aa9422ef02e82f7da9039329e21a437decf972cb3919ad817f70ac9a49e306"
 
   def install
