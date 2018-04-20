f8599830276fe0e2f72c9056dc637616c4703ec5
Rickard von Essen <rickard.von.essen@gmail.com>
2018-04-20 15:30:19 +0200

    stm32flash 0.5 (new formula)
    Open source flash program for STM32 using the ST serial bootloader.
    See https://sourceforge.net/projects/stm32flash/

---
+++ Formula/stm32flash.rb
@@ -0,0 +1,14 @@
+class Stm32flash < Formula
+  desc "Open source flash program for STM32 using the ST serial bootloader"
+  homepage "https://sourceforge.net/projects/stm32flash/"
+  url "https://downloads.sourceforge.net/projects/stm32flash/files/stm32flash-0.5.tar.gz"
+  sha256 "97aa9422ef02e82f7da9039329e21a437decf972cb3919ad817f70ac9a49e306"
+
+  def install
+    system "make", "PREFIX=#{prefix}", "install"
+  end
+
+  test do
+    system "#{bin}/stm32flash", "-h"
+  end
+end
