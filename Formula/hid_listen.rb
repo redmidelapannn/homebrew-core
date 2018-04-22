508283be73766cfa8975e5981515bf9d1baa5160
Rickard von Essen <rickard.von.essen@gmail.com>
2018-04-22 19:53:11 +0200

    Updated hid_listen to use the new github repo

--- Formula/hid_listen.rb
+++ Formula/hid_listen.rb
@@ -1,15 +1,11 @@
 class HidListen < Formula
   desc "Prints out debugging information from usb hid devices"
   homepage "https://www.pjrc.com/teensy/hid_listen.html"
-
-  stable do
-    url "https://www.pjrc.com/teensy/hid_listen_1.01.zip"
-    sha256 "9cc73e325dc9265c032c295d93e16aad63e01214f69ee788b0b80cbc16d3b076"
-    patch :DATA
-  end
+  url "https://github.com/PaulStoffregen/hid_listen/archive/1.01.tar.gz"
+  sha256 "4cb44c8fb0b5dab1c7acee5920c298bd4b193ada25fa186a5b9aff08ed48ba43"
 
   def install
-    system "make", "hid_listen"
+    system "make", "OS=DARWIN", "hid_listen"
     bin.install "hid_listen"
   end
 
@@ -17,42 +13,3 @@ class HidListen < Formula
     system "true"
   end
 end
-
-__END__
-diff -ruN --exclude .git hid_listen/Makefile hid_listen-git/Makefile
---- hid_listen/Makefile	2009-09-21 17:52:39.000000000 +0200
-+++ hid_listen-git/Makefile	2018-04-12 09:55:31.000000000 +0200
-@@ -1,7 +1,7 @@
- PROG = hid_listen
-
--OS = LINUX
--#OS = DARWIN
-+#OS = LINUX
-+OS = DARWIN
- #OS = WINDOWS
-
-
-@@ -15,9 +15,8 @@
- TARGET = $(PROG)
- CC = gcc
- STRIP = strip
--SDK = /Developer/SDKs/MacOSX10.5.sdk
--CFLAGS = -O2 -Wall -isysroot $(SDK) -D$(OS) -arch ppc -arch i386
--LIBS = -Xlinker -syslibroot -Xlinker $(SDK) -framework IOKit -framework CoreFoundation
-+CFLAGS = -O2 -Wall -D$(OS) -arch x86_64
-+LIBS = -framework IOKit -framework CoreFoundation
- else ifeq ($(OS), WINDOWS)
- TARGET = $(PROG).exe
- CC = i586-mingw32msvc-gcc
-diff -ruN --exclude .git hid_listen/rawhid.c hid_listen-git/rawhid.c
---- hid_listen/rawhid.c	2008-12-18 00:05:57.000000000 +0100
-+++ hid_listen-git/rawhid.c	2018-04-12 09:54:42.000000000 +0200
-@@ -50,7 +50,7 @@
- /**                                                                     **/
- /*************************************************************************/
-
--#if defined(LINUX) || defined(__LINUX__) || #system(linux)
-+#if defined(LINUX) || defined(__LINUX__)
- #define OPERATING_SYSTEM linux
- #include <fcntl.h>
- #include <errno.h>
