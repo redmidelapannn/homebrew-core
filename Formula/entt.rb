7a1a2cadb681148da77d25339bc5476f51b0fe5a
Kerndog73 <kerndog73@gmail.com>
2018-04-23 23:56:23 +0930

    Embed a test
    The test compiles and runs a minimal C++ program that uses the library.

--- Formula/entt.rb
+++ Formula/entt.rb
@@ -9,6 +9,37 @@ class Entt < Formula
   end
 
   test do
-    system "true"
+    (testpath/"test.cpp").write <<~EOS
+      #include <entt/entt.hpp>
+
+      struct Position {
+        int x;
+        int y;
+      };
+
+      void moveSystem(entt::DefaultRegistry &reg, Position shift) {
+        auto view = reg.view<Position>();
+        for (const uint32_t entity : view) {
+          Position &pos = view.get(entity);
+          pos.x += shift.x;
+          pos.y += shift.y;
+        }
+      }
+
+      int main() {
+        entt::DefaultRegistry reg;
+        uint32_t entity = reg.create();
+        reg.assign<Position>(entity, 2, 6);
+        moveSystem(reg, {4, 4});
+
+        Position expected = {6, 10};
+        Position actual = reg.get<Position>(entity);
+        reg.destroy(entity);
+
+        return actual.x != expected.x || actual.y != expected.y;
+      }
+    EOS
+    system ENV.cxx, "-std=c++14", "test.cpp", "-o", "test"
+    system "./test"
   end
 end
