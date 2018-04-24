0ac3efbb0b5344dc095cd4d5c4e67b644785c33a
Ahmed Tolba <tolpa1@gmail.com>
2018-04-24 11:20:59 +0200

    map 0.1.1 (new formula)

---
+++ Formula/map.rb
@@ -0,0 +1,14 @@
+class Map < Formula
+  desc "Maps lines from stdin to commands"
+  homepage "https://github.com/soveran/map"
+  url "https://github.com/soveran/map/archive/0.1.1.tar.gz"
+  sha256 "6903d2bb6d7e0cddceee487cc35b442e5e78a459785aa5ae7c84f13090f193f0"
+
+  def install
+    system "make", "PREFIX=#{prefix}", "install"
+  end
+
+  test do
+    assert_equal "foobar", shell_output("printf 'foo\nbar' | map f 'printf $f'")
+  end
+end
