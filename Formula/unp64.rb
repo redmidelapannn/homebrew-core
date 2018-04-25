e2f9f06e66b7df248a46b0dc37ce9378fb52be83
Dennis Luxen <info@project-osrm.org>
2018-04-25 14:14:30 +0200

    Add version "2.34"

--- Formula/unp64.rb
+++ Formula/unp64.rb
@@ -3,7 +3,8 @@ class Unp64 < Formula
   homepage "http://iancoog.altervista.org"
   url "http://iancoog.altervista.org/C/unp64_234.7z"
   sha256 "86968afaa13b6c17fac7577041d5e3f3cc51cb534d818b5f360fddf41a05eaad"
-
+  version "2.34"
+  
   def install
     system "make", "-C", "unp64_234/src", "unp64"
     bin.install "unp64_234/src/Release/unp64"
