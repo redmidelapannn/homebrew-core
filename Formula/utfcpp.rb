294f941caeb388b75c82de9fe0cb8f6dbaa9f4de
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-25 13:35:49 +0000

    utfcpp: add 2.3.4 bottle.

--- Formula/utfcpp.rb
+++ Formula/utfcpp.rb
@@ -4,6 +4,13 @@ class Utfcpp < Formula
   url "https://downloads.sourceforge.net/project/utfcpp/utf8cpp_2x/Release%202.3.4/utf8_v2_3_4.zip"
   sha256 "3373cebb25d88c662a2b960c4d585daf9ae7b396031ecd786e7bb31b15d010ef"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6ab3e4a1b8212f3cbea82b9321d9b9362600b43c8c546e97036e3e31a24dcb56" => :high_sierra
+    sha256 "6ab3e4a1b8212f3cbea82b9321d9b9362600b43c8c546e97036e3e31a24dcb56" => :sierra
+    sha256 "6ab3e4a1b8212f3cbea82b9321d9b9362600b43c8c546e97036e3e31a24dcb56" => :el_capitan
+  end
+
   def install
     include.install Dir["source/*"]
   end
