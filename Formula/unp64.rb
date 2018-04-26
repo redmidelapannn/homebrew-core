de6e1d47bf4e12aced1dd6028e19611ea74ecbcb
Dennis Luxen <info@project-osrm.org>
2018-04-26 07:12:19 +0200

    remove lint

--- Formula/unp64.rb
+++ Formula/unp64.rb
@@ -2,9 +2,9 @@ class Unp64 < Formula
   desc "Generic C64 prg unpacker,"
   homepage "http://iancoog.altervista.org"
   url "http://iancoog.altervista.org/C/unp64_234.7z"
-  sha256 "86968afaa13b6c17fac7577041d5e3f3cc51cb534d818b5f360fddf41a05eaad"
   version "2.34"
-  
+  sha256 "86968afaa13b6c17fac7577041d5e3f3cc51cb534d818b5f360fddf41a05eaad"
+
   def install
     system "make", "-C", "unp64_234/src", "unp64"
     bin.install "unp64_234/src/Release/unp64"
