34c6a6603f2259066d20bf097f6dcd624cc45bd5
Benito van der Zander <benito@benibela.de>
2018-04-26 23:51:26 +0200

    Update xidel.rb

--- Formula/xidel.rb
+++ Formula/xidel.rb
@@ -1,27 +1,27 @@
 class Xidel < Formula
-  desc "Command line tool to download and extract data from HTML/XML pages or JSON-APIs, using CSS, XPath 3.0, XQuery 3.0, JSONiq or pattern templates. It can also create new or transformed XML/HTML/JSON documents."
+  desc "CLI XPath/XQuery 3.0, JSONiq interpreter to extract data from HTML/XML/JSON"
   homepage "http://www.videlibri.de/xidel.html"
   url "https://github.com/benibela/xidel/releases/download/Xidel_0.9.8/xidel-0.9.8.src.tar.gz"
   version "0.9.8"
   sha256 "72b5b1a2fc44a0a61831e268c45bc6a6c28e3533b5445151bfbdeaf1562af39c"
   head "https://github.com/benibela/xidel.git", :branch => "master"
 
-  depends_on "fpc" 
-  depends_on "bash" 
+  bottle :unneeded
+
+  depends_on "bash"
+  depends_on "fpc"
   depends_on "openssl" => :recommended
   
-  bottle :unneeded
-  
   def install
-    cd "xidel-#{version}-src/programs/internet/xidel" do
+    cd buildpath/"xidel-#{version}-src/programs/internet/xidel" do
       system "bash", "build.sh"
       bin.install "xidel"
       man1.install "meta/xidel.1"
-    end    
+    end
   end
 
-  test do  
-    cd "xidel-#{version}-src/programs/internet/xidel" do
+  test do
+    cd buildpath/"xidel-#{version}-src/programs/internet/xidel" do
       system "bash", "tests/tests.sh"
     end
   end
