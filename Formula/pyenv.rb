ca49077176ba371960ed6089e9b190522a596565
Ryan J Ollos <ryan.j.ollos@gmail.com>
2018-04-26 13:59:55 -0700

    pyenv: Add caveat on enabling shims and autocompletion

--- Formula/pyenv.rb
+++ Formula/pyenv.rb
@@ -30,6 +30,14 @@ class Pyenv < Formula
     end
   end
 
+  def caveats; <<~EOS
+      To enable shims and autocompletion add to your profile:
+        if command -v pyenv 1>/dev/null 2>&1; then eval "$(pyenv init -)" fi
+
+      See: https://github.com/pyenv/pyenv#basic-github-checkout
+    EOS
+  end
+
   test do
     shell_output("eval \"$(#{bin}/pyenv init -)\" && pyenv versions")
   end
