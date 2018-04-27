e6635c7122bc4ac41011dea7e6be6c7b39be111b
Andrew Erickson <aerickson@gmail.com>
2018-04-26 21:46:25 -0400

    move location where env var is set

--- Formula/mpv.rb
+++ Formula/mpv.rb
@@ -51,6 +51,11 @@ class Mpv < Formula
     # that's good enough for building the manpage.
     ENV["LC_ALL"] = "C"
 
+    # for the --with-bundle option:
+    #  - allow the build to work on older versions of OS X
+    # see https://github.com/mpv-player/mpv/commit/c176f1169f356059ee6e3c447e4546fcdb335ec2
+    ENV["MACOSX_DEPLOYMENT_TARGET"] = "10.3"
+
     # Prevents a conflict between python2 and python3 when
     # gobject-introspection is using brewed python.
     ENV.delete("PYTHONPATH") if MacOS.version <= :mavericks
@@ -85,9 +90,6 @@ class Mpv < Formula
     system "python3", "waf", "install"
 
     if build.with? "bundle"
-      # allow the build to work on older versions of OS X
-      # see https://github.com/mpv-player/mpv/commit/c176f1169f356059ee6e3c447e4546fcdb335ec2
-      ENV["MACOSX_DEPLOYMENT_TARGET"] = "10.3"
       system "python3", "TOOLS/osxbundle.py", "build/mpv"
       prefix.install "build/mpv.app"
     end
