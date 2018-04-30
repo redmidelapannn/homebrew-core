6a22d5d65447166ee081413097f0737b21cd688a
Recep Aslantas <info@recp.me>
2018-04-30 17:50:28 +0300

    drop pkg-config as build dep

--- Formula/cglm.rb
+++ Formula/cglm.rb
@@ -7,7 +7,6 @@ class Cglm < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
-  depends_on "pkg-config" => :build
 
   def install
     system "sh", "./autogen.sh"
