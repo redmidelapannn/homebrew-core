5051cd22352527a8a941dc3c479666a797f31df8
BrewTestBot <brew-test-bot@googlegroups.com>
2018-04-30 23:46:05 +0000

    insteadman: add 3.0.8 bottle.

--- Formula/insteadman.rb
+++ Formula/insteadman.rb
@@ -4,6 +4,13 @@ class Insteadman < Formula
   url "https://github.com/jhekasoft/insteadman3/archive/v3.0.8.tar.gz"
   sha256 "d60a46dbfbfcc99021fc2b8942aaa6af0e9d27501a11e401e1de3e70cc88993a"
 
+  bottle do
+    cellar :any
+    sha256 "2f0ccce7150b25d4c98edbe825bf60924941b5cacb0ed685f21ce3d4b594216f" => :high_sierra
+    sha256 "22ddc077eef4248874c4741b70dd78aa3147a774f3155ab3899259f283913282" => :sierra
+    sha256 "8c65d7d82da9e6fa51238240916af283e49db50e54459c6245643c4cd6c94c10" => :el_capitan
+  end
+
   depends_on "pkg-config" => :build
   depends_on "go" => :build
   depends_on "gtk+3"
