77db21f96a469d73e1c7b752d75d68ac54003a11
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-01 20:08:48 +0000

    viro: add 1.0.0 bottle.

--- Formula/viro.rb
+++ Formula/viro.rb
@@ -5,6 +5,12 @@ class Viro < Formula
   version "1.0.0"
   sha256 "eb22f144fe249e45332edca2a05bb40eccc81a4013b0b555915af74c1345e601"
 
+  bottle do
+    sha256 "b9d1832648a7778d87f572fed50d95ed53b7fb2f20bd2ef02d0af82e1a92c27c" => :high_sierra
+    sha256 "b9d1832648a7778d87f572fed50d95ed53b7fb2f20bd2ef02d0af82e1a92c27c" => :sierra
+    sha256 "b9d1832648a7778d87f572fed50d95ed53b7fb2f20bd2ef02d0af82e1a92c27c" => :el_capitan
+  end
+
   def install
     bin.install "viro"
   end
