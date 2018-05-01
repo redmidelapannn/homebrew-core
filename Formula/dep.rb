1a2a9f074f61a39dca4090a63742ad4b983c27a3
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-01 09:39:56 +0000

    dep: update 0.4.1_1 bottle.

--- Formula/dep.rb
+++ Formula/dep.rb
@@ -9,9 +9,10 @@ class Dep < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0b974c08cfb94c31a652908312620e6dac1a617ffd2d21816c5e9c8f259ea559" => :high_sierra
-    sha256 "4e6e39bb9678fd4ddf200a80ba657f09d3d91bd133f5795775153a3aa12688eb" => :sierra
-    sha256 "4e95ecf37819ce91f1e52a6b11e6c875c2e9b3b17edba1b919c55ffc98ba95cf" => :el_capitan
+    rebuild 1
+    sha256 "39dc339ed5fc052f27f277602ac495630ec5e356b1da0d8e1f11d7f213c7b881" => :high_sierra
+    sha256 "5474e2976b94072d2cb8ef78bda3c29a837f0619c92dec9f7475e186a9d138ea" => :sierra
+    sha256 "6fdc76ca548545403b28af6d10fa5be48a91dd0d6433834a7e92815ac2f2462c" => :el_capitan
   end
 
   depends_on "go"
