5c6f4ec21f20e4c48e4b188c9f4869e465d9637a
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-02 22:10:51 +0000

    unp64: add 2.34 bottle.

--- Formula/unp64.rb
+++ Formula/unp64.rb
@@ -5,6 +5,12 @@ class Unp64 < Formula
   version "2.34"
   sha256 "86968afaa13b6c17fac7577041d5e3f3cc51cb534d818b5f360fddf41a05eaad"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "0eb2bf4adb439ae39849eadec361f209cb180aea911bafa6acc3fcfbbd99e8b2" => :high_sierra
+    sha256 "5d9e19e33b2592f2644a101614143562f552099b0e5bad12e2d5267ec4760ade" => :el_capitan
+  end
+
   def install
     system "make", "-C", "unp64_234/src", "unp64"
     bin.install "unp64_234/src/Release/unp64"
