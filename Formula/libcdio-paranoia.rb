cd9150c692b6738e9729273fd2e481c6ecc8c556
Miguel Rivera <miguel@alphachronos.com>
2018-05-02 10:35:19 -0500

    Additional changes done per fxcoudert suggestions.

--- Formula/libcdio-paranoia.rb
+++ Formula/libcdio-paranoia.rb
@@ -1,15 +1,17 @@
 class LibcdioParanoia < Formula
-  desc "The cd-paranoia audio-cd ripper based on libcdio"
+  desc "Audio CD ripper based on libcdio"
   homepage "https://www.gnu.org/software/libcdio/"
   url "https://ftp.gnu.org/gnu/libcdio/libcdio-paranoia-10.2+0.94+2.tar.gz"
   mirror "https://ftpmirror.gnu.org/libcdio/libcdio-paranoia-10.2+0.94+2.tar.gz"
   sha256 "d60f82ece97eeb92407a9ee03f3499c8983206672c28ae5e4e22179063c81941"
 
-  depends_on "libcdio" => :build
   depends_on "pkg-config" => :build
+  depends_on "libcdio"
 
   def install
     system "./configure", "--without-versioned-libs",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
                           "--prefix=#{prefix}"
     system "make", "install"
   end
