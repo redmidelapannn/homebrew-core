e14952d017d2ce142ff526ac5b5f5c0206dea612
Miguel Rivera <miguel@alphachronos.com>
2018-05-02 09:31:00 -0500

    Fix problems with the initial submission.
    Changes to fix problems and coply with the standards required by homebrew.

--- Formula/libcdio-paranoia.rb
+++ Formula/libcdio-paranoia.rb
@@ -1,13 +1,12 @@
 class LibcdioParanoia < Formula
-  desc "This is a port of xiph.org's cdda paranoia to use libcdio for CDROM access. By doing this, cdparanoia runs on platforms othe
-r than GNU/Linux."
+  desc "The cd-paranoia audio-cd ripper based on libcdio."
   homepage "https://www.gnu.org/software/libcdio/"
   url "https://ftp.gnu.org/gnu/libcdio/libcdio-paranoia-10.2+0.94+2.tar.gz"
   mirror "https://ftpmirror.gnu.org/libcdio/libcdio-paranoia-10.2+0.94+2.tar.gz"
   sha256 "d60f82ece97eeb92407a9ee03f3499c8983206672c28ae5e4e22179063c81941"
 
-  depends_on "pkg-config" => :build
   depends_on "libcdio" => :build
+  depends_on "pkg-config" => :build
 
   def install
     system "./configure", "--without-versioned-libs",
@@ -16,6 +15,6 @@ r than GNU/Linux."
   end
 
   test do
-     assert_match /cdparanoia III release 10.2/, shell_output("#{bin}/cd-paranoia -V", 0)
+    assert_match /cdparanoia III release 10.2/, shell_output("#{bin}/cd-paranoia -V", 0)
   end
 end
