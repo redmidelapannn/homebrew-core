32dd86360b53df43133c176be1edc1b3f5cbf396
FX Coudert <fxcoudert@gmail.com>
2018-05-02 11:58:31 +0200

    Update unp64.rb

--- Formula/unp64.rb
+++ Formula/unp64.rb
@@ -1,6 +1,6 @@
 class Unp64 < Formula
   desc "Generic C64 prg unpacker,"
-  homepage "http://iancoog.altervista.org"
+  homepage "http://iancoog.altervista.org/"
   url "http://iancoog.altervista.org/C/unp64_234.7z"
   version "2.34"
   sha256 "86968afaa13b6c17fac7577041d5e3f3cc51cb534d818b5f360fddf41a05eaad"
@@ -16,7 +16,7 @@ class Unp64 < Formula
       output.write [code.join].pack("H*")
     end
 
-    output = shell_output("#{bin}/unp64 -i a.prg 2>&1", 0)
+    output = shell_output("#{bin}/unp64 -i a.prg 2>&1")
     assert_match "a.prg :  (Unknown)", output
   end
 end
