6e42fad23d890b311b24ea6dac78e6b2cbc87eef
Miguel Rivera <miguel@alphachronos.com>
2018-05-03 11:13:10 -0500

    Add the english module to use $CHILD_STATUS instead of $?.

--- Formula/libcdio-paranoia.rb
+++ Formula/libcdio-paranoia.rb
@@ -1,3 +1,5 @@
+require "English"
+
 class LibcdioParanoia < Formula
   desc "Audio CD ripper based on libcdio"
   homepage "https://www.gnu.org/software/libcdio/"
@@ -19,6 +21,6 @@ class LibcdioParanoia < Formula
   test do
     assert_match /cdparanoia/, shell_output("#{bin}/cd-paranoia -V 2>&1", 0).partition(" ").first
     system("#{bin}/cd-paranoia", "-V")
-    assert_equal $?.success?, true, "Execution of cd-paranoia failed."
+    assert_equal $CHILD_STATUS, true, "Execution of cd-paranoia failed."
   end
 end
