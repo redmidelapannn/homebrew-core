514cb13560092d8176544eeab3cd506c8ed8eac2
youmitsu <ymbullseye@gmail.com>
2018-05-08 00:02:26 +0900

    headの分岐をなくす

--- Formula/kotlinreplacesupporter.rb
+++ Formula/kotlinreplacesupporter.rb
@@ -7,12 +7,7 @@ class Kotlinreplacesupporter < Formula
   depends_on :java => "1.6+"
 
   def install
-    if build.head?
-      system "./gradlew", "packJar"
-      libexec.install "build/libs/kotlin_replace_supporter-1.0.jar"
-    else
-      libexec.install "kotlin_replace_supporter-1.0.jar"
-    end
+    libexec.install "kotlin_replace_supporter-1.0.jar"
     bin.write_jar_script libexec/"kotlin_replace_supporter-1.0.jar", "krs"
   end
   test do
