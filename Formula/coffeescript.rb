81d97121a78172a4e148c5490839549e73969d50
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-07 11:31:11 +0000

    coffeescript: update 2.3.0 bottle.

--- Formula/coffeescript.rb
+++ Formula/coffeescript.rb
@@ -9,9 +9,10 @@ class Coffeescript < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "aaca241c7e0891044ea1d21582b9df86813949ab4b93c6aedae6d97134a05ba1" => :high_sierra
-    sha256 "d855c47f212359ffdcd350d851c924124f22528b0b53709005bfaa28eac2c721" => :sierra
-    sha256 "50ba107ce918fe8d188ed577256d456fb7f007a92a3433f54ed6b29679ce2321" => :el_capitan
+    rebuild 1
+    sha256 "6b469c00505da4537c845607fd028e884831e16a7a75db9d92263ffce0448e22" => :high_sierra
+    sha256 "3125d2f9c3a646a7a462829e3f6c3f535736a57ca5ab024617c82c33296d9cf9" => :sierra
+    sha256 "947f795d52554508bdc2e5481f3038aee80a45bb1a65ef67ac085cc9f3011a74" => :el_capitan
   end
 
   depends_on "node"
