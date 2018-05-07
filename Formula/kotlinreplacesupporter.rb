0c7c7fd24fa79887c87b06648bb9bd9a5348808f
youmitsu <ymbullseye@gmail.com>
2018-05-07 23:02:09 +0900

    initial_commit

---
+++ Formula/kotlinreplacesupporter.rb
@@ -0,0 +1,21 @@
+class Kotlinreplacesupporter < Formula
+  desc "This is a tool of replacing"
+  homepage "https://github.com/youmitsu/KotlinReplaceSupporter"
+  url "https://github.com/youmitsu/KotlinReplaceSupporter/releases/download/1.0/kotlin_replace_supporter-1.0.jar"
+  sha256 "f7b5feafaea72057235809f6d587539863eb6f5785fa044ff42f891ef91d55e6"
+  head "https://github.com/youmitsu/KotlinReplaceSupporter.git"
+  depends_on :java => "1.6+"
+
+  def install
+    if build.head?
+      system "./gradlew", "packJar"
+      libexec.install "build/libs/kotlin_replace_supporter-1.0.jar"
+    else
+      libexec.install "kotlin_replace_supporter-1.0.jar"
+    end
+    bin.write_jar_script libexec/"kotlin_replace_supporter-1.0.jar", "krs"
+  end
+  test do
+    system "#{bin}/krs"
+  end
+end
