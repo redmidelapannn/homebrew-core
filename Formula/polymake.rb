6a3013022b822e69a8088787e09db51f449013ed
Andreas Paffenholz <paffenholz@mathematik.tu-darmstadt.de>
2018-05-08 14:37:22 +0200

    polymake 3.2 (new formula)

---
+++ Formula/polymake.rb
@@ -0,0 +1,60 @@
+class Polymake < Formula
+  desc "Tool for computations in algorithmic discrete geometry"
+  homepage "https://polymake.org"
+  url "https://polymake.org/lib/exe/fetch.php/download/polymake-3.2r3.tar.bz2"
+  version "3.2"
+  sha256 "8423dac8938dcd96e15b1195432f6a9844e8c34727c829395755a5067ce43440"
+
+  depends_on "ant"
+  depends_on "boost"
+  depends_on "gmp"
+  depends_on "mpfr"
+  depends_on "ninja"
+  depends_on "ppl"
+  depends_on "readline"
+  depends_on "singular" => :recommended
+
+  resource "Term::Readline::Gnu" do
+    url "http://search.cpan.org/CPAN/authors/id/H/HA/HAYASHI/Term-ReadLine-Gnu-1.35.tar.gz"
+    sha256 "575d32d4ab67cd656f314e8d0ee3d45d2491078f3b2421e520c4273e92eb9125"
+  end
+
+  resource "XML::LibXSLT" do
+    url "http://search.cpan.org/CPAN/authors/id/S/SH/SHLOMIF/XML-LibXSLT-1.96.tar.gz"
+    sha256 "2a5e374edaa2e9f9d26b432265bfea9b4bb7a94c9fbfef9047b298fce844d473"
+  end
+
+  def install
+    ENV.prepend_create_path "PERL5LIB", prefix/"perl5/lib/perl5"
+    ENV.prepend_path "PERL5LIB", prefix/"perl5/lib/perl5/darwin-thread-multi-2level"
+
+    resource("XML::LibXSLT").stage do
+      system "perl", "Makefile.PL", "INSTALL_BASE=#{prefix}/perl5"
+      system "make", "install"
+    end
+
+    resource("Term::Readline::Gnu").stage do
+      ENV.refurbish_args
+      system "perl", "Makefile.PL", "INSTALL_BASE=#{prefix}/perl5",
+                     "--includedir=#{Formula["readline"].opt_include}",
+                     "--libdir=#{Formula["readline"].opt_lib}"
+      system "make", "install"
+    end
+
+    system "./configure", "--prefix=#{prefix}",
+                          "--without-jreality"
+
+    system "ninja", "-C", "build/Opt", "-l2", "install"
+    bin.env_script_all_files(prefix/"perl5/bin", :PERL5LIB => ENV["PERL5LIB"])
+  end
+
+  test do
+    assert_match "1 23 23 1", shell_output("#{bin}/polymake 'print cube(3)->H_STAR_VECTOR'")
+    stdout, stderr, = Open3.capture3("#{bin}/polymake 'my $a=new Array<SparseMatrix<Float>>'")
+    assert_match "", stderr
+    assert_match "", stdout
+    recompiling_info = /^polymake:  WARNING: Recompiling in .* please be patient\.\.\.$/
+    _, stderr, = Open3.capture3("#{bin}/polymake 'my $a=new Array<SparseMatrix<Float>>'")
+    assert_match recompiling_info, stderr
+  end
+end
