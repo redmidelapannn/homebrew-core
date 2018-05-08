8bd1f286587cc15dab5b1a7a31c80cb70c8c8044
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-08 14:06:02 +0000

    kotlinreplacesupporter: add 1.0 bottle.

--- Formula/kotlinreplacesupporter.rb
+++ Formula/kotlinreplacesupporter.rb
@@ -3,6 +3,13 @@ class Kotlinreplacesupporter < Formula
   homepage "https://github.com/youmitsu/KotlinReplaceSupporter"
   url "https://github.com/youmitsu/KotlinReplaceSupporter/releases/download/1.0/kotlin_replace_supporter-1.0.jar"
   sha256 "f7b5feafaea72057235809f6d587539863eb6f5785fa044ff42f891ef91d55e6"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "bcaaadbfe641852130c212b54cf604e896749daa15aea9af4852422d245f2984" => :high_sierra
+    sha256 "bcaaadbfe641852130c212b54cf604e896749daa15aea9af4852422d245f2984" => :sierra
+    sha256 "bcaaadbfe641852130c212b54cf604e896749daa15aea9af4852422d245f2984" => :el_capitan
+  end
+
   depends_on :java => "1.6+"
 
   def install
