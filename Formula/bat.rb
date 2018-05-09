4b0a8eed6e5fb3f2ba3bf833c1e4135f4a3d6f3b
Xu Cheng <git@xuc.me>
2018-05-09 13:46:10 +0800

    bat 0.3.0 (new formula)

---
+++ Formula/bat.rb
@@ -0,0 +1,17 @@
+class Bat < Formula
+  desc "Clone of cat(1) with syntax highlighting and Git integration"
+  homepage "https://github.com/sharkdp/bat"
+  url "https://github.com/sharkdp/bat/archive/v0.3.0.tar.gz"
+  sha256 "ffb3f2b20fb831e503c3db224a563dc30e766c627a08c069cf60e5182845207c"
+
+  depends_on "cmake" => :build
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--root", prefix
+  end
+
+  test do
+    system bin/"bat", prefix/"README.md"
+  end
+end
