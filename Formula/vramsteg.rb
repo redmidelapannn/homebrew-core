b592c04911bab884140019ddf066c7440ee48ab6
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-09 07:56:23 +0000

    vramsteg: update 1.1.0 bottle.

--- Formula/vramsteg.rb
+++ Formula/vramsteg.rb
@@ -7,11 +7,10 @@ class Vramsteg < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0c9aff3582ad05a388cba8c43770ead295d921a8e419323a3c4115f09e609ba1" => :high_sierra
-    sha256 "7f65668b7bb036fb19e69bdc9cbc2ec48728bc8c1936253f6d5e8d74a113a3fd" => :sierra
-    sha256 "e4b3e2e66c2f772a38de529b884091a2ffa1f920af6604696129d21cc9e70b99" => :el_capitan
-    sha256 "9285766e0502b88c62d3d295402a41c46b8d9d2707a492bb5d70484b1080c212" => :yosemite
-    sha256 "f9ea5a2984d676db153d2128b1aa84a413edb93692e9c6be8147e5a856d42972" => :mavericks
+    rebuild 1
+    sha256 "27430ea6263ec4538e7d7d13280a2957894c0f6e478cc9a1c466d77517d9e879" => :high_sierra
+    sha256 "9a35de0ec2a81078961109db7d969178f546af6ed09138a35c20a5e39d588241" => :sierra
+    sha256 "ec67d1ffb8e726f18a79f876e6c7df562cc828de269993882726cc30e1d8b30a" => :el_capitan
   end
 
   depends_on "cmake" => :build
