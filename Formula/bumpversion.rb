d829f50bf5b5422b4816d8afa90b82fa927711bb
George Hartzell <hartzell@alerce.com>
2018-05-10 14:04:47 -0700

    bumpversion: update to 0.5.3 release

--- Formula/bumpversion.rb
+++ Formula/bumpversion.rb
@@ -3,8 +3,8 @@ class Bumpversion < Formula
 
   desc "Version-bump your software with a single command"
   homepage "https://pypi.python.org/pypi/bumpversion"
-  url "https://pypi.io/packages/source/b/bumpversion/bumpversion-0.5.0.tar.gz"
-  sha256 "030832b9b46848e1c1ac6678dba8242a021e35e908b65565800c9650291117dc"
+  url "https://pypi.io/packages/source/b/bumpversion/bumpversion-0.5.3.tar.gz"
+  sha256 "6744c873dd7aafc24453d8b6a1a0d6d109faf63cd0cd19cb78fd46e74932c77e"
 
   depends_on "python@2"
 
