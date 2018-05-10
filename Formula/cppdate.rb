e17d8fa0d7155698c8ed4aeb30c83d24081eef6c
carlos.alvaro <carlos.alvaro@meteologica.com>
2018-05-10 14:54:23 +0200

    Pass brew audit cppdate --online --new-formula

--- Formula/cppdate.rb
+++ Formula/cppdate.rb
@@ -1,5 +1,5 @@
 class Cppdate < Formula
-  desc "C++11/14/17 library for Date and Time operations based on <chrono> header"
+  desc "C++11/14/17 library for Date and Time operations based on <chrono>"
   homepage "https://github.com/HowardHinnant/date"
   url "https://github.com/HowardHinnant/date/archive/v2.4.1.tar.gz"
   sha256 "98907d243397483bd7ad889bf6c66746db0d7d2a39cc9aacc041834c40b65b98"
@@ -21,13 +21,9 @@ class Cppdate < Formula
       #include "date/tz.h"
       #include <iostream>
 
-      using namespace std;
-      using namespace std::chrono;
-      using namespace date;
-      
       int main() {
-        auto t = make_zoned(current_zone(), system_clock::now());
-        cout << t << endl;
+        auto t = date::make_zoned(date::current_zone(), std::chrono::system_clock::now());
+        std::cout << t << std::endl;
       }
     EOS
     system ENV.cxx, "test.cpp", "-std=c++1y", "-ltz", "-o", "test"
