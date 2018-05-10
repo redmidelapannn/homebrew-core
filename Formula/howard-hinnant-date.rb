0164d0aba863a42159aa8990007f2c9b5b82e2a0
Carlos <carlos.alvaro@citelan.es>
2018-05-10 20:36:43 +0200

    Rename cppdate to howard-hinnant-date

--- Formula/howard-hinnant-date.rb
+++ Formula/howard-hinnant-date.rb
@@ -1,5 +1,5 @@
-class Cppdate < Formula
-  desc "C++11/14/17 library for Date and Time operations based on <chrono>"
+class HowardHinnantDate < Formula
+  desc "C++ library for date and time operations based on <chrono>"
   homepage "https://github.com/HowardHinnant/date"
   url "https://github.com/HowardHinnant/date/archive/v2.4.1.tar.gz"
   sha256 "98907d243397483bd7ad889bf6c66746db0d7d2a39cc9aacc041834c40b65b98"
@@ -8,11 +8,7 @@ class Cppdate < Formula
   needs :cxx11
 
   def install
-    custom_cmake_args = ["-DENABLE_DATE_TESTING=OFF",
-                         "-DUSE_SYSTEM_TZ_DB=ON",
-                         "-DBUILD_SHARED_LIBS=ON"]
-
-    system "cmake", ".", *std_cmake_args, *custom_cmake_args
+    system "cmake", ".", *std_cmake_args, "-DENABLE_DATE_TESTING=OFF", "-DUSE_SYSTEM_TZ_DB=ON", "-DBUILD_SHARED_LIBS=ON"
     system "make", "install"
   end
 
