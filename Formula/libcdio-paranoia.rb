329fdb09c354ba26a89a2d2ab154c7d705ca04ca
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-11 12:52:13 +0000

    libcdio-paranoia: add 10.2+0.94+2 bottle.

--- Formula/libcdio-paranoia.rb
+++ Formula/libcdio-paranoia.rb
@@ -7,6 +7,13 @@ class LibcdioParanoia < Formula
   version "10.2+0.94+2"
   sha256 "d60f82ece97eeb92407a9ee03f3499c8983206672c28ae5e4e22179063c81941"
 
+  bottle do
+    cellar :any
+    sha256 "75e8d985622e89f9ab770cbb0b83858b10861fbc9013670a35059c64378390af" => :high_sierra
+    sha256 "2141a0c34c5ae27159d7f070ee8c8fa54d98e1d4ba163211fe5dd0de26796a29" => :sierra
+    sha256 "945356fecca26f9e16bf59a44aac79ac1dc4b03da2054a1e652cd51080e46865" => :el_capitan
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libcdio"
 
