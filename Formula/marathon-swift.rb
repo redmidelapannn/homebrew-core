f79137024085621138290934ddb32b3b98109a30
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-13 12:48:25 +0000

    marathon-swift: add 3.0.0 bottle.

--- Formula/marathon-swift.rb
+++ Formula/marathon-swift.rb
@@ -4,6 +4,11 @@ class MarathonSwift < Formula
   url "https://github.com/JohnSundell/Marathon/archive/3.0.0.tar.gz"
   sha256 "5dd1f495a8d69d9c3450735b771f816a838a040a45c682b0c42ac17105058a6e"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ec7d84b17ded694124ec3f790ba7a8c8e2755ce01d1016e9c0e7aa9ad02c3f1d" => :high_sierra
+  end
+
   depends_on :xcode => ["9.3", :build]
 
   def install
