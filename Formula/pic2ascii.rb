09b4b83e30a7359c6b4d3ca6d128e2bac7456091
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-15 05:43:18 +0000

    pic2ascii: add 1.4.3 bottle.

--- Formula/pic2ascii.rb
+++ Formula/pic2ascii.rb
@@ -4,6 +4,13 @@ class Pic2ascii < Formula
   url "https://github.com/wzshiming/pic2ascii/archive/v1.4.3.tar.gz"
   sha256 "15d9d21399ac3560b9d919b7d643d74c9c82bde5942e4d0ebed06109dd9236ae"
 
+  bottle do
+    cellar :any
+    sha256 "aefeb65eec51d2e0846322db0ac9dad00e8bf43de7263194012b1f26b8aff49d" => :high_sierra
+    sha256 "ef7eb72948d18d668fbb62b48bc37b9dd77030b704f47d135fdad59a3ffd1d89" => :sierra
+    sha256 "aaf40110851d033187d4e1c7b1559ec177cae6640f516d63e638689aff9ee2c7" => :el_capitan
+  end
+
   depends_on "ffmpeg" => :build
   depends_on "go" => :build
 
