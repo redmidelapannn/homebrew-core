13a601271768fb6c59c18d1dfa1907db45a2540a
George Hartzell <hartzell@alerce.com>
2018-05-14 17:31:41 -0700

    Use files.pythonhosted.org for downloads

--- Formula/bumpversion.rb
+++ Formula/bumpversion.rb
@@ -3,7 +3,7 @@ class Bumpversion < Formula
 
   desc "Version-bump your software with a single command"
   homepage "https://pypi.python.org/pypi/bumpversion"
-  url "https://pypi.io/packages/source/b/bumpversion/bumpversion-0.5.3.tar.gz"
+  url "https://files.pythonhosted.org/packages/source/b/bumpversion/bumpversion-0.5.3.tar.gz"
   sha256 "6744c873dd7aafc24453d8b6a1a0d6d109faf63cd0cd19cb78fd46e74932c77e"
 
   depends_on "python@2"
