3dc9f16748460bc5f39381aa1db3b97794faac62
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-16 13:12:12 +0000

    yafc: update 1.3.7_1 bottle.

--- Formula/yafc.rb
+++ Formula/yafc.rb
@@ -7,10 +7,10 @@ class Yafc < Formula
   revision 1
 
   bottle do
-    sha256 "40c44b84a96b4cf65ee0c1d09a2770ede71e5cfd7fdf9c24504723a8b1eaef3f" => :high_sierra
-    sha256 "05ea440c672b5283feaf2597a8c0525d3ff4228cd2347aabd3a69d282d245dd2" => :sierra
-    sha256 "9cbbd25ce9019d92e6ed3c8ce5af9533b0d79d79f96b01fc8741016a3bd4eb39" => :el_capitan
-    sha256 "3ac839135bac59b4d1d98cb662adc558109167fd21e69976dd39e1e3602de25b" => :yosemite
+    rebuild 1
+    sha256 "9403864494c0101dcdfede0d79a2b157826465a7ee32469c5e676ebeab483991" => :high_sierra
+    sha256 "f7c6f59a2bd28a4151c0090ac7d4b720d038b4287ff9a44510bb43736da8f529" => :sierra
+    sha256 "bf31ac04413d154d2337d2957609c92e53f066a4205582d8682bdbdc361d58d1" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
