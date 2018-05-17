096ce37700279e7fac7c55eaa504ba6dc8a37b2c
ilovezfs <ilovezfs@icloud.com>
2018-05-17 01:05:07 -0700

    Update icarus-verilog.rb

--- Formula/icarus-verilog.rb
+++ Formula/icarus-verilog.rb
@@ -3,6 +3,7 @@ class IcarusVerilog < Formula
   homepage "http://iverilog.icarus.com/"
   url "ftp://icarus.com/pub/eda/verilog/v10/verilog-10.2.tar.gz"
   sha256 "96dedbddb12d375edb45a144a926a3ba1e3e138d6598b18e7d79f2ae6de9e500"
+  revision 1
 
   bottle do
     sha256 "81e937d3d6a5f7b22271e79b73618e96db1e52ee491eaf3506671cfb5891c129" => :high_sierra
@@ -19,12 +20,11 @@ class IcarusVerilog < Formula
   depends_on "bison" => :build
 
   def install
-    bison = Formula["bison"].bin/"bison"
     system "autoconf" if build.head?
     system "./configure", "--prefix=#{prefix}"
     # https://github.com/steveicarus/iverilog/issues/85
     ENV.deparallelize
-    system "make", "install", "BISON=#{bison}"
+    system "make", "install", "BISON=#{Formula["bison"].opt_bin}/bison"
   end
 
   test do
@@ -42,7 +42,7 @@ class IcarusVerilog < Formula
 
     # test syntax errors do not cause segfaults
     (testpath/"error.v").write "error;"
-    assert_equal "-:1: error: variable declarations must be contained within a module.\n",
-      shell_output("#{bin}/iverilog error.v 2>&1", 1)
+    assert_equal "-:1: error: variable declarations must be contained within a module.",
+      shell_output("#{bin}/iverilog error.v 2>&1", 1).chomp
   end
 end
