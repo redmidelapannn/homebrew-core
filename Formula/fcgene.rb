76521d24d0bd3c75f3d1b82bf605d03d20cedb78
FX Coudert <fxcoudert@gmail.com>
2018-05-19 00:07:47 +0200

    Update fcgene.rb

--- Formula/fcgene.rb
+++ Formula/fcgene.rb
@@ -5,8 +5,7 @@ class Fcgene < Formula
   sha256 "4e1f85f2ec812e2528bd19b6c18ecf297666cd83046e003bc57d9ed5f25783d6"
 
   def install
-    # upstream Makefile ignores $CXX
-    ENV["AC_CXX"] = ENV["CXX"]
+    ENV["AC_CXX"] = ENV["CXX"] # otherwise CXX is ignored
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
@@ -19,6 +18,6 @@ class Fcgene < Formula
     (testpath/"test.ped").write("1 1000000000 0 0 1 1 0 0 1 1\n1 1000000001 0 0 1 2 1 1 1 2\n")
     (testpath/"test.map").write("1 rs0 0 1000\n1 rs10 0 1001\n")
     system "#{bin}/fcgene", "--ped", "test.ped", "--map", "test.map"
-    assert FileTest.exists?(testpath/"fcgene_out_fcgene.log")
+    assert_predicate testpath/"fcgene_out_fcgene.log", :exist?
   end
 end
