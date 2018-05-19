53cefb96118094bd7316283620a4ee8aa5eacc12
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-19 19:44:51 +0000

    rs-git-fsmonitor: add git bottle.

--- Formula/rs-git-fsmonitor.rb
+++ Formula/rs-git-fsmonitor.rb
@@ -5,6 +5,13 @@ class RsGitFsmonitor < Formula
   url "https://github.com/jgavris/rs-git-fsmonitor/releases/download/v0.1.0/rs-git-fsmonitor"
   sha256 "4faf1723ea75a76e0dd6187d5a3e9074fd5e93dd13f1024843d4ed256ff689e8"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d1830192aaf05bc24df1514c88cf5bf639bdef831884db0d61827fa8d17765cf" => :high_sierra
+    sha256 "d1830192aaf05bc24df1514c88cf5bf639bdef831884db0d61827fa8d17765cf" => :sierra
+    sha256 "d1830192aaf05bc24df1514c88cf5bf639bdef831884db0d61827fa8d17765cf" => :el_capitan
+  end
+
   depends_on "watchman"
 
   def install
