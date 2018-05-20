50123f017319f255b2cb2121ca5a4c126e3ac472
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-20 14:07:33 +0000

    ceu: add 0.30 bottle.

--- Formula/ceu.rb
+++ Formula/ceu.rb
@@ -2,6 +2,13 @@ class Ceu < Formula
   desc "Reactive language alternative to C"
   homepage "http://www.ceu-lang.org/"
   url "https://github.com/fsantanna/ceu.git", :tag => "v0.30"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ccb47980cdee7bbe4cb67cb84389f8615a691288f47a121c181da8781335169e" => :high_sierra
+    sha256 "a772ab2fd8d31743084c7b6f8b4596d4f0fcedc86b12685ccfa96334939435d9" => :sierra
+    sha256 "dab3da338cd203d6b3623ecd929eb70dfd955bd78a320ca4590527f30e672cd8" => :el_capitan
+  end
+
   depends_on "lua"
 
   resource "lpeg" do
