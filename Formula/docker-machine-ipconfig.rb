da737cea9c43ecaa6f2d9842dc373220fb4e0f21
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-20 08:41:10 +0000

    docker-machine-ipconfig: add 1.0.0 bottle.

--- Formula/docker-machine-ipconfig.rb
+++ Formula/docker-machine-ipconfig.rb
@@ -4,6 +4,13 @@ class DockerMachineIpconfig < Formula
   url "https://github.com/fivestars/docker-machine-ipconfig.git",
     :tag => "v1.0.0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "01ab66396c7e688e2424d07b0cb67c74db01e81d4afbebc48159716649e68bdb" => :high_sierra
+    sha256 "01ab66396c7e688e2424d07b0cb67c74db01e81d4afbebc48159716649e68bdb" => :sierra
+    sha256 "01ab66396c7e688e2424d07b0cb67c74db01e81d4afbebc48159716649e68bdb" => :el_capitan
+  end
+
   depends_on "docker-machine" => :recommended
 
   def install
