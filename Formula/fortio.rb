c85ab94c372acf2246ffda736a8022f139ac8fcd
Yossi Mesika <mesika@il.ibm.com>
2018-05-22 20:51:30 +0300

    Revert "Putting back a flag to keep built binary static"
    This reverts commit 9a6f9db319f5de98f055461b9b7d1afb4b52627d.

--- Formula/fortio.rb
+++ Formula/fortio.rb
@@ -11,7 +11,6 @@ class Fortio < Formula
     ENV["XC_OS"] = "darwin"
     ENV["XC_ARCH"] = "amd64"
     ENV["GOPATH"] = buildpath
-    ENV["CGO_ENABLED"] = "0"
 
     (buildpath/"src/istio.io/fortio").install buildpath.children
     cd "src/istio.io/fortio" do
