3a1e088d5656786d344edaa84001d1151d4a729c
Gabe Cohen <gabcoh2@nilesk12.org>
2018-05-22 17:05:42 -0500

    fix suggestions for soundpipe 1.7.0 (new-formula)
    move test and examples to `pkgshare`. Test install by compiling example
    program and checking output.

--- Formula/soundpipe.rb
+++ Formula/soundpipe.rb
@@ -3,21 +3,22 @@ class Soundpipe < Formula
   homepage "https://paulbatchelor.github.io/proj/soundpipe.html"
   url "https://github.com/PaulBatchelor/soundpipe/archive/v1.7.0.tar.gz"
   sha256 "2d6f6b155ad93d12f59ae30e2b0f95dceed27e0723147991da6defc6d65eadda"
+
   depends_on "libsndfile"
 
   def install
     system "make"
-    system "make", "-C", "test"
-    cp_r "test", prefix
-    cp_r "examples", prefix
-    system "make", "install", "PREFIX="+prefix
+    mkdir_p pkgshare
+    cp_r "test", pkgshare
+    cp_r "examples", pkgshare
+    system "make", "install", "PREFIX=#{prefix}"
   end
 
   test do
-    # TODO
-    # While this command does test the library, it does so by printing out ok or
-    # not ok for various tests. A more thorough test would check to make sure
-    # that everything is ok before returning success.
-    system "cd #{prefix}/test;#{prefix}/test/run.bin"
+    system ENV.cc, "#{pkgshare}/examples/ex_osc.c", "-o#{testpath}/test",
+          "-L#{lib}", "-L#{HOMEBREW_PREFIX}/lib", "-lsndfile", "-lsoundpipe"
+    system "cd #{testpath};./test"
+    hash = "07caba5db440b7442fbe8d40145e0dbc06ef52c0088380e581c6071a05c94bc6"
+    assert_equal hash, shell_output("sha256sum #{testpath}/test.wav").split(' ')[0]
   end
 end
