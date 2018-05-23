7012d03757ec9690d5fc080525ea5a5352cf4b6c
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-23 20:29:07 +0000

    bitwarden: add 1.0.0 bottle.

--- Formula/bitwarden.rb
+++ Formula/bitwarden.rb
@@ -4,6 +4,13 @@ class Bitwarden < Formula
   url "https://github.com/bitwarden/cli/releases/download/v1.0.0/bw-macos-1.0.0.zip"
   sha256 "4ed81213fe796025c171243ef9c6e66a95a40d9650e87fc86c7e401769cc63d8"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4792d95435843bbbe7d8de4fbb34ee088d284e8d03045daddd372dbbbd6aa02d" => :high_sierra
+    sha256 "4792d95435843bbbe7d8de4fbb34ee088d284e8d03045daddd372dbbbd6aa02d" => :sierra
+    sha256 "4792d95435843bbbe7d8de4fbb34ee088d284e8d03045daddd372dbbbd6aa02d" => :el_capitan
+  end
+
   def install
     libexec.install Dir["*"]
     chmod(0755, "#{libexec}/bw")
