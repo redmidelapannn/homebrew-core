614e31e8092ae3508197231498ac2b19083fe04d
ilovezfs <ilovezfs@icloud.com>
2018-05-24 12:02:15 +0000

    Update tunnel.rb

--- Formula/tunnel.rb
+++ Formula/tunnel.rb
@@ -1,6 +1,6 @@
 class Tunnel < Formula
   desc "Expose local servers to internet securely"
-  homepage "https://labstack.com/docs/diy/tunnel"
+  homepage "https://labstack.com/docs/tunnel"
   url "https://github.com/labstack/tunnel/archive/0.2.7.tar.gz"
   sha256 "6b4a6564732e2e86e49450629a72dc7ef647088ef66f568cb507d1e0c9b5588f"
 
