7888c5b23fed0505927fc8b55f11cca3d3c636cd
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-24 12:41:21 +0000

    capstone: update 3.0.4 bottle.

--- Formula/capstone.rb
+++ Formula/capstone.rb
@@ -7,12 +7,10 @@ class Capstone < Formula
 
   bottle do
     cellar :any
-    sha256 "82e40e06f3a41633326d3ceb7f268945441dcb7a0ae1caa88e06fc0504c73cc0" => :high_sierra
-    sha256 "7d0f04a49d42bd9c953a5ea6cb85159f72f8e948d6aea4d7c64b3e82a12459f1" => :sierra
-    sha256 "3aa8d8b679cc5261a3fbf44b191c61480cdb34576f71b769e63d68c5e27c19b1" => :el_capitan
-    sha256 "5bbd8f7d9e0ae0d3b23c7d478fdb02476e8cee847577576d543bf98649985975" => :yosemite
-    sha256 "0cfd7478b21360ffea1aac61ec64eeae612bce247a681b0205ffd14790f8f7dc" => :mavericks
-    sha256 "585042b1452fbeda9efd07da4b8400d56d166afd5e5f1120da20975e41001e88" => :mountain_lion
+    rebuild 1
+    sha256 "c8f21d476b2966ea2942e306f843760c37956bb0ed968df578021bcff849518d" => :high_sierra
+    sha256 "ee1df1070e95a189d7cde85456932226076f4adebd57719e042789749dafb7a5" => :sierra
+    sha256 "69adfe37a098117f4a7d526686cd821627f290ac77f358ec81f974a6d5bcf196" => :el_capitan
   end
 
   depends_on "pkg-config" => :build if build.head?
