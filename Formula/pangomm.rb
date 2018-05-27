8464efbc765ecf96588e7b1b0aed0ee7c82574d3
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-27 14:08:31 +0000

    pangomm: update 2.40.1_1 bottle.

--- Formula/pangomm.rb
+++ Formula/pangomm.rb
@@ -7,9 +7,10 @@ class Pangomm < Formula
 
   bottle do
     cellar :any
-    sha256 "eefd29c6f25ecaa2d5b6f93a5578eda9607a71283ad7c2613167bb21ad4c62d4" => :high_sierra
-    sha256 "d17ae0b9cdda0fe0145e53c890981aa088880befecc5973818170ca6f2b39c6b" => :sierra
-    sha256 "807631e6f412e16bbc94bf2cdae1d9f4bd61da40d27a741047e907739a73d311" => :el_capitan
+    rebuild 1
+    sha256 "78b43bc37347623c793517136ee44c8fd30d559a248de0d837fa2ca7d680d667" => :high_sierra
+    sha256 "a00b98fef0b712d4589e4cd0eb419b0e37dee0d0f3ff2490f6bd5dfd610f45ed" => :sierra
+    sha256 "665decefeb7ae357507c1a9839955ea30f529402ba46832f8b67f05a94ff2f44" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
