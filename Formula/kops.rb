de7d81053da2491597118b05891bd4c46120968b
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-28 07:28:50 +0000

    kops: update 1.9.1 bottle.

--- Formula/kops.rb
+++ Formula/kops.rb
@@ -7,9 +7,9 @@ class Kops < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "2f4045b67f382260fbb8d6656b064b73fb5cc530c75a9505e47c39c9f06a4c95" => :high_sierra
-    sha256 "53b20962447aef2d0844b443db45b41956f5e0d51bf87939634f7f3cd4ea7a69" => :sierra
-    sha256 "c67f4f62829834d028e8a56cdaf4b5ccf6aed26221892bf70374ac342f492f81" => :el_capitan
+    sha256 "6a4116f7b3251e353a8ad1184306706985f05d95dfdd1dd1c5afcd87180fb670" => :high_sierra
+    sha256 "fc446d7afff675d80377198f3de22c493e82d76b64961bbd9581ae45d4e86478" => :sierra
+    sha256 "7a5251b11e975fdd7b41a9782fc3aeb8ff2dd9ac76e72964d333f34fb68e6524" => :el_capitan
   end
 
   depends_on "go" => :build
