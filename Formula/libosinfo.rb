f0fd0162a4c2b31fa56128dc5f0e194f2f22dc3a
Roman Bolshakov <roolebo@gmail.com>
2018-05-28 13:59:04 +0300

    libosinfo: ship latest osinfo-db
    virt-manager has no elements in OS Version dropdown and virt-install
    doesn't accept --os-variant field without the database.
    
    The change vendors osinfo-db-tools with osinfo-db within libosinfo.

--- Formula/libosinfo.rb
+++ Formula/libosinfo.rb
@@ -3,7 +3,7 @@ class Libosinfo < Formula
   homepage "https://libosinfo.org/"
   url "https://releases.pagure.org/libosinfo/libosinfo-1.1.0.tar.gz"
   sha256 "600f43a4a8dae5086a01a3d44bcac2092b5fa1695121289806d544fb287d3136"
-  revision 1
+  revision 2
 
   bottle do
     sha256 "f4a54c53fe160f16818d4db569f8818a30c55e777c326982d140478fdd4c8b35" => :high_sierra
@@ -18,8 +18,19 @@ class Libosinfo < Formula
   depends_on "check"
   depends_on "gettext"
   depends_on "glib"
+  depends_on "libarchive" # for osinfo-db-tools
   depends_on "libsoup"
   depends_on "libxml2"
+
+  resource "osinfo-db-tools" do
+    url "https://releases.pagure.org/libosinfo/osinfo-db-tools-1.1.0.tar.gz"
+    sha256 "a141cd2fc07c30d84801b5dbf6b11f2c2e708b0e81216277d052ac0b57fe546b"
+  end
+
+  resource "osinfo-db" do
+    url "https://releases.pagure.org/libosinfo/osinfo-db-20180514.tar.xz", :using => :nounzip
+    sha256 "4246b093c2903b349abb5e7f729ba83d36135b73cc3dc88882490fcb5f42dfdf"
+  end
 
   def install
     # avoid wget dependency
@@ -47,6 +58,20 @@ class Libosinfo < Formula
     # Compilation of docs doesn't get done if we jump straight to "make install"
     system "make"
     system "make", "install"
+
+    resource("osinfo-db-tools").stage do
+      system "./configure", "--prefix=#{prefix}",
+                            "--localstatedir=#{var}",
+                            "--sysconfdir=#{etc}",
+                            "--disable-silent-rules"
+      system "make", "install"
+    end
+
+    resource("osinfo-db").stage do |s|
+      system "#{bin}/osinfo-db-import",
+             "--system",
+             "#{s.resource.name}-#{s.resource.version}.tar.xz"
+    end
   end
 
   test do
@@ -75,5 +100,7 @@ class Libosinfo < Formula
     ]
     system ENV.cc, "test.c", "-o", "test", *flags
     system "./test"
+    assert_match "#{share}/osinfo", shell_output("#{bin}/osinfo-db-path --system")
+    system "#{bin}/osinfo-query", "os", "short-id=win10"
   end
 end
