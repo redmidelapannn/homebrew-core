4492723ecdb310d0c5fcce2c06443b91d2329656
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-30 23:03:31 +0000

    confluent-oss: add 4.1.0 bottle.

--- Formula/confluent-oss.rb
+++ Formula/confluent-oss.rb
@@ -5,6 +5,13 @@ class ConfluentOss < Formula
   version "4.1.0"
   sha256 "32c3aff688d6f24e1ea573efdc4e16a3a5f466f1641b770ae4ba0350e7be583a"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "df6810e081f32667c527fba07da921301fbda0fd83dfcc29aead214b6bcc1890" => :high_sierra
+    sha256 "df6810e081f32667c527fba07da921301fbda0fd83dfcc29aead214b6bcc1890" => :sierra
+    sha256 "df6810e081f32667c527fba07da921301fbda0fd83dfcc29aead214b6bcc1890" => :el_capitan
+  end
+
   depends_on :java => "1.8"
 
   conflicts_with "kafka", :because => "kafka also ships with identically named Kafka related executables"
