85ee7d95e832e309c6cae3696604585242bb1ed0
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-30 20:24:49 +0000

    phpmyadmin: add 4.8.1 bottle.

--- Formula/phpmyadmin.rb
+++ Formula/phpmyadmin.rb
@@ -4,6 +4,13 @@ class Phpmyadmin < Formula
   url "https://files.phpmyadmin.net/phpMyAdmin/4.8.1/phpMyAdmin-4.8.1-all-languages.tar.gz"
   sha256 "4f7771e96a1637797e0dc710d36345d8327a612d8c08ac305018ef7185f2217d"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "28181b5c2b4fd43ad2ad13ada89696ac84f423a195cb9f0de86bac5fed853d60" => :high_sierra
+    sha256 "28181b5c2b4fd43ad2ad13ada89696ac84f423a195cb9f0de86bac5fed853d60" => :sierra
+    sha256 "28181b5c2b4fd43ad2ad13ada89696ac84f423a195cb9f0de86bac5fed853d60" => :el_capitan
+  end
+
   depends_on "php" => :test
 
   def install
