431660ac06919697cd1a95c2f8069b82aa870328
Geoff Appleby <geoff@gapple.ca>
2018-05-30 13:15:17 -0700

    Update phpmyadmin to 4.8.1

--- Formula/phpmyadmin.rb
+++ Formula/phpmyadmin.rb
@@ -1,8 +1,8 @@
 class Phpmyadmin < Formula
   desc "Web interface for MySQL and MariaDB"
   homepage "https://www.phpmyadmin.net"
-  url "https://files.phpmyadmin.net/phpMyAdmin/4.8.0.1/phpMyAdmin-4.8.0.1-all-languages.tar.gz"
-  sha256 "a78c413fef9387b25ab589e0411443c51849510e37c65708dd7f728f095134b0"
+  url "https://files.phpmyadmin.net/phpMyAdmin/4.8.1/phpMyAdmin-4.8.1-all-languages.tar.gz"
+  sha256 "4f7771e96a1637797e0dc710d36345d8327a612d8c08ac305018ef7185f2217d"
 
   depends_on "php" => :test
 
