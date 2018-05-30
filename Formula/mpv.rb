3f6813e51d7e989b0eb9c14f2481787cba9676d9
BrewTestBot <brew-test-bot@googlegroups.com>
2018-05-30 09:37:12 +0000

    mpv: update 0.28.2 bottle.

--- Formula/mpv.rb
+++ Formula/mpv.rb
@@ -6,9 +6,10 @@ class Mpv < Formula
   head "https://github.com/mpv-player/mpv.git"
 
   bottle do
-    sha256 "1dee05ae50b43c059c11c667a2e70b51bccfb373248ad8f877eeef9a8afe7c67" => :high_sierra
-    sha256 "9c43d85b504b5edd2cbcf87ed219dc7a75c771b763e66fd64305b93c5070c993" => :sierra
-    sha256 "301203ddeb374ecce692a47b59ff8396b63e2f40530f6f82dc037992a53a0ebc" => :el_capitan
+    rebuild 1
+    sha256 "9be5ac7cd239fc50abff5ba617b8f9bf76336b48ae0050b9435c56f3c2cdadd5" => :high_sierra
+    sha256 "ecb253532cfdec4bf4da87499a8dd275cc3600a4a0d6c624507502ae7ade2412" => :sierra
+    sha256 "41a8fc843116483a24b011446898cef6de2b866741461d6024662db90894cfd0" => :el_capitan
   end
 
   option "with-bundle", "Enable compilation of the .app bundle."
