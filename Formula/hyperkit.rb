b59a4e1ee36d5195312464d462d348a453857489
Antoine Lepee <alepee@MacBook-Pro-de-Antoine-2.local>
2018-05-31 12:04:11 +0200

    hyperkit: build with qcow support

--- Formula/hyperkit.rb
+++ Formula/hyperkit.rb
@@ -5,40 +5,33 @@ class Hyperkit < Formula
     :tag => "v0.20180123",
     :revision => "8bbe1da0166553a5b0ccff170bf9ccbd7d10753e"
 
-  option "with-qcow", "Enable support for qcow disk image format"
-  if build.with? "qcow"
-    depends_on "ocaml" => :build
-    depends_on "opam" => :build
-    depends_on "libev"
-  end
+  depends_on "ocaml" => :build
+  depends_on "opam" => :build
+  depends_on "libev"
 
   depends_on :xcode => ["9.0", :build]
   depends_on :macos => :yosemite
 
   def install
-    if build.with? "qcow"
-      system "opam", "init", "--no-setup"
-      opam_dir = "#{buildpath}/.brew_home/.opam"
-      ENV["CAML_LD_LIBRARY_PATH"] = "#{opam_dir}/system/lib/stublibs:/usr/local/lib/ocaml/stublibs"
-      ENV["OPAMUTF8MSGS"] = "1"
-      ENV["PERL5LIB"] = "#{opam_dir}/system/lib/perl5"
-      ENV["OCAML_TOPLEVEL_PATH"] = "#{opam_dir}/system/lib/toplevel"
-      ENV.prepend_path "PATH", "#{opam_dir}/system/bin"
+    system "opam", "init", "--no-setup"
+    opam_dir = "#{buildpath}/.brew_home/.opam"
+    ENV["CAML_LD_LIBRARY_PATH"] = "#{opam_dir}/system/lib/stublibs:/usr/local/lib/ocaml/stublibs"
+    ENV["OPAMUTF8MSGS"] = "1"
+    ENV["PERL5LIB"] = "#{opam_dir}/system/lib/perl5"
+    ENV["OCAML_TOPLEVEL_PATH"] = "#{opam_dir}/system/lib/toplevel"
+    ENV.prepend_path "PATH", "#{opam_dir}/system/bin"
 
-      inreplace "#{opam_dir}/compilers/4.05.0/4.05.0/4.05.0.comp",
-        '["./configure"', '["./configure" "-no-graph"' # Avoid X11
+    inreplace "#{opam_dir}/compilers/4.05.0/4.05.0/4.05.0.comp",
+      '["./configure"', '["./configure" "-no-graph"' # Avoid X11
 
-      ENV.deparallelize { system "opam", "switch", "4.05.0" }
+    ENV.deparallelize { system "opam", "switch", "4.05.0" }
 
-      system "opam", "config", "exec", "--",
-             "opam", "install", "-y", "uri", "qcow", "conduit.1.0.0", "lwt.3.1.0",
-             "qcow-tool", "mirage-block-unix.2.9.0", "conf-libev", "logs", "fmt",
-             "mirage-unix", "prometheus-app"
+    system "opam", "config", "exec", "--",
+           "opam", "install", "-y", "uri", "qcow", "conduit.1.0.0", "lwt.3.1.0",
+           "qcow-tool", "mirage-block-unix.2.9.0", "conf-libev", "logs", "fmt",
+           "mirage-unix", "prometheus-app"
 
-      system "opam", "config", "exec", "--", "make"
-    else
-      system "make"
-    end
+    system "opam", "config", "exec", "--", "make"
 
     bin.install "build/hyperkit"
   end
