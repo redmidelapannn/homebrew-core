ea30895e72cecbccf7380183f80c4d93d65fde0b
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-03 04:20:02 +0000

    openapi-generator: add 3.0.0 bottle.

--- Formula/openapi-generator.rb
+++ Formula/openapi-generator.rb
@@ -4,6 +4,13 @@ class OpenapiGenerator < Formula
   url "https://github.com/OpenAPITools/openapi-generator/archive/v3.0.0.tar.gz"
   sha256 "01b4fde626b96c66edd8be71c34027eeda9ad132cc03890e6f051f505e600207"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "049206808ca9aaee80dd5ae643bad630989d97529eb6457064e9ca8a865703b1" => :high_sierra
+    sha256 "ec38fd47ae0eb4363b48a4349457c514c46f90eb14fbc8b187eee6b8aa0d81b1" => :sierra
+    sha256 "4c98536e312f3fe3363ed52480e23b30c25fcd6f941adad4c656ac36f131aa7b" => :el_capitan
+  end
+
   depends_on "maven" => :build
   depends_on :java => "1.8"
 
