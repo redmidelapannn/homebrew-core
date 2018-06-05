5d8a013d3a96445d67ce47c038eaf355c3fcdce5
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-05 21:21:25 +0000

    comply: add 1.2.3 bottle.

--- Formula/comply.rb
+++ Formula/comply.rb
@@ -4,6 +4,13 @@ class Comply < Formula
   url "https://github.com/strongdm/comply/archive/v1.2.3.tar.gz"
   sha256 "d818536eb1fe84a211399c6252a761babb29695c2036f69950fb6d74eeeb7d64"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a4c6ae7d775bca367055c67f2526235033e068f2d4c913593b9a9b034dd53704" => :high_sierra
+    sha256 "7fbef649f84ceb1654eb710a98dfd4ad3504143deae37f5c9c26315441a22c96" => :sierra
+    sha256 "4e6e4c09b706997647b931bf54af842f66fb89cd07285d80cf17b0c36371be93" => :el_capitan
+  end
+
   depends_on "go" => :build
 
   def install
