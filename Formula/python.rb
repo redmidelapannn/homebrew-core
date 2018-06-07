98c04a069cbd7928eb34c35b9d3eab34c92824b8
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-07 03:17:00 +0000

    python: update 3.6.5 bottle.

--- Formula/python.rb
+++ Formula/python.rb
@@ -6,10 +6,10 @@ class Python < Formula
   head "https://github.com/python/cpython.git"
 
   bottle do
-    rebuild 1
-    sha256 "7e0fc1b078b51d9478ab08660d5df01611976a7af0f6c24054bda58264bb506c" => :high_sierra
-    sha256 "2fe5ca9be0f1596798927c4aa1d4d187ca7f83adc4681483cec2cc52d7c95386" => :sierra
-    sha256 "bccf50de973644608af29652f2660124d033f3213d422fe44a7f012a47643a95" => :el_capitan
+    rebuild 2
+    sha256 "13d8d57687d7dd817040ae34e40936a74b47e7f1ca21212f9941d7b4b52c1995" => :high_sierra
+    sha256 "7283c42cbe46142c5f6e46cc5a204a11d379f27b7f849481b5d4767b92f38eb2" => :sierra
+    sha256 "006e214a4f9713903c9301b448a31b0bdc2e4c5f27b37d93eb4c2d4d6ec04de3" => :el_capitan
   end
 
   devel do
