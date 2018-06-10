d50fb65bd08e80af503e9cc86e04debea5cc6852
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-10 12:38:00 +0000

    just: update 0.3.11 bottle.

--- Formula/just.rb
+++ Formula/just.rb
@@ -5,9 +5,10 @@ class Just < Formula
   sha256 "2ded5cbb140955e87e0edee5c9728146316a34fa0a23a4de1f7a28df569b25d0"
 
   bottle do
-    sha256 "a250bafb3b21f931cf075f354f8ed77ff90f7f5be9188f14ad4c44b882a908c5" => :high_sierra
-    sha256 "540606e67eedc2c95d19133a613fa40152e8ca0b693dc764c9b3c491a5564d71" => :sierra
-    sha256 "4d2980f96306ec681a1de6c99864a632b72014b5d0c3971308ad6b5ea4baafe4" => :el_capitan
+    rebuild 1
+    sha256 "c56c1bb9f616ff5ee9c9af6272035e6c742228d9a91ea23e8410322f192cf7be" => :high_sierra
+    sha256 "cb30c1a71e711fb5e73719d705be54d814769a7bb8bd2f672ca8d765d2eb0c4c" => :sierra
+    sha256 "45e900e36227d3c8ffb2c8f39dcc6efb9e59a10b70772640bcaca32adea9ec97" => :el_capitan
   end
 
   depends_on "rust" => :build
