0910548d409936a0c14286e1308ea01d3234ccf9
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-12 22:45:09 +0000

    valgrind: update 3.13.0 bottle.

--- Formula/valgrind.rb
+++ Formula/valgrind.rb
@@ -23,9 +23,9 @@ class Valgrind < Formula
   end
 
   bottle do
-    sha256 "1ae24d4988d010b407bb344c953dd8a7696876a3c2793f2548648be4cfc61db2" => :sierra
-    sha256 "8dcc652676c3ec2f1fd3dfc710298b4da0985b1a056f86c429e68b5b8bcf74a6" => :el_capitan
-    sha256 "02f118896232618cb6319e22f21cea21d29f8985be4807547bb49bc397307730" => :yosemite
+    rebuild 1
+    sha256 "35cdb554106a966eca546da5a0f85edf1c90c76f9adc8c97f1828fcaccc1d61e" => :sierra
+    sha256 "86b1018d1a2534426dc7fb339a4607a2b2f689a8fe03745899bb7b9bc1b29ed4" => :el_capitan
   end
 
   head do
