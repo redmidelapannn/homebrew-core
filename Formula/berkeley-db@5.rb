58b2a6fb07bd12c21788a7d11bd6639295b1fcf2
Steven Willis <onlynone@gmail.com>
2018-06-14 13:37:25 -0400

    berkeley-db@5 5.3.28 (new formula)

---
+++ Formula/berkeley-db@5.rb
@@ -0,0 +1,100 @@
+class BerkeleyDbAT5 < Formula
+  desc "High performance key/value database"
+  homepage "https://www.oracle.com/technology/products/berkeley-db/index.html"
+  url "https://download.oracle.com/berkeley-db/db-5.3.28.tar.gz"
+  sha256 "e0a992d740709892e81f9d93f06daf305cf73fb81b545afe72478043172c3628"
+
+  keg_only :versioned_formula
+
+  option "with-java", "Compile with Java support."
+  option "with-sql", "Compile with SQL support."
+
+  deprecated_option "enable-sql" => "with-sql"
+
+  # Fix build under Xcode 4.6
+  # Double-underscore names are reserved, and __atomic_compare_exchange is now
+  # a built-in, so rename this to something non-conflicting.
+  patch :DATA
+
+  def install
+    # BerkeleyDB dislikes parallel builds
+    ENV.deparallelize
+    # --enable-compat185 is necessary because our build shadows
+    # the system berkeley db 1.x
+    args = %W[
+      --disable-debug
+      --prefix=#{prefix}
+      --mandir=#{man}
+      --enable-cxx
+      --enable-compat185
+    ]
+    args << "--enable-java" if build.with? "java"
+    args << "--enable-sql" if build.with? "sql"
+
+    # BerkeleyDB requires you to build everything from the build_unix subdirectory
+    cd "build_unix" do
+      system "../dist/configure", *args
+      system "make", "install"
+
+      # use the standard docs location
+      doc.parent.mkpath
+      mv prefix/"docs", doc
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <assert.h>
+      #include <string.h>
+      #include <db_cxx.h>
+      int main() {
+        Db db(NULL, 0);
+        assert(db.open(NULL, "test.db", NULL, DB_BTREE, DB_CREATE, 0) == 0);
+
+        const char *project = "Homebrew";
+        const char *stored_description = "The missing package manager for macOS";
+        Dbt key(const_cast<char *>(project), strlen(project) + 1);
+        Dbt stored_data(const_cast<char *>(stored_description), strlen(stored_description) + 1);
+        assert(db.put(NULL, &key, &stored_data, DB_NOOVERWRITE) == 0);
+
+        Dbt returned_data;
+        assert(db.get(NULL, &key, &returned_data, 0) == 0);
+        assert(strcmp(stored_description, (const char *)(returned_data.get_data())) == 0);
+
+        assert(db.close(0) == 0);
+      }
+    EOS
+    flags = %W[
+      -I#{include}
+      -L#{lib}
+      -ldb_cxx
+    ]
+    system ENV.cxx, "test.cpp", "-o", "test", *flags
+    system "./test"
+    assert_predicate testpath/"test.db", :exist?
+  end
+end
+
+__END__
+diff --git a/src/dbinc/atomic.h b/src/dbinc/atomic.h
+index 096176a..561037a 100644
+--- a/src/dbinc/atomic.h
++++ b/src/dbinc/atomic.h
+@@ -144,7 +144,7 @@ typedef LONG volatile *interlocked_val;
+ #define	atomic_inc(env, p)	__atomic_inc(p)
+ #define	atomic_dec(env, p)	__atomic_dec(p)
+ #define	atomic_compare_exchange(env, p, o, n)	\
+-	__atomic_compare_exchange((p), (o), (n))
++	__atomic_compare_exchange_db((p), (o), (n))
+ static inline int __atomic_inc(db_atomic_t *p)
+ {
+ 	int	temp;
+@@ -176,7 +176,7 @@ static inline int __atomic_dec(db_atomic_t *p)
+  * http://gcc.gnu.org/onlinedocs/gcc-4.1.0/gcc/Atomic-Builtins.html
+  * which configure could be changed to use.
+  */
+-static inline int __atomic_compare_exchange(
++static inline int __atomic_compare_exchange_db(
+ 	db_atomic_t *p, atomic_value_t oldval, atomic_value_t newval)
+ {
+ 	atomic_value_t was;
