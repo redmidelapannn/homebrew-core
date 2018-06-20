f8989f4b7f93a1dbf3a9dc6f6c94bb5c4533cf15
Anderson Lucas C. Ramos <aramos@horizonte.tv.br>
2018-06-20 20:08:00 -0300

    buildcannon 0.2.1

--- Formula/buildcannon.rb
+++ Formula/buildcannon.rb
@@ -1,14 +1,14 @@
 class Buildcannon < Formula
-  desc "buildcannon is a terminal automation buildtool for iOS"
+  desc "buildcannon is a automation buildtool for iOS"
   homepage "https://github.com/andersonlucasg3/buildcannon"
-  url "https://github.com/andersonlucasg3/buildcannon/releases/download/0.2.0/buildcannon.zip"
-  sha256 "a8a28eaacc67abf557f4437588137fa932f45bc2466883ead1b4775529655190"
-  
+  url "https://github.com/andersonlucasg3/buildcannon/releases/download/0.2.1/buildcannon.zip"
+  sha256 "5019dd36b439037355f8105915d135b0a5bb1071801ddd55bd0d8459ac0d27b7"
+
   def install
     bin.install "buildcannon"
   end
 
-  def test
+  test do
     system "#{bin}/buildcannon", "--help"
   end
 end
