45f5e1f5ff3b9366269e79e9e5ef455474938047
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-21 06:14:22 +0000

    hfstospell: update 0.5.0_1 bottle.

--- Formula/hfstospell.rb
+++ Formula/hfstospell.rb
@@ -7,9 +7,10 @@ class Hfstospell < Formula
 
   bottle do
     cellar :any
-    sha256 "de622d1a6a04d7f4de2525d8717d06e5c5c4c920eac54b86d4a9296966a891e7" => :high_sierra
-    sha256 "a15f6eb7855860c888079fcbd7a0e4ee10e52101db5b7923092c0f9883f144ec" => :sierra
-    sha256 "70dbedbf094447921f05004f7aac65831e288eda106b633773001d4e68c5f5b4" => :el_capitan
+    rebuild 1
+    sha256 "9f57404244a9c075280b50d193a1d9cd78ab4c3af033fe91b931993fdb606cf4" => :high_sierra
+    sha256 "a91c54dab9df897fee235229b7b8a382d6bcce2223aa604999429308cc52f364" => :sierra
+    sha256 "40ae66776752fbc1475e33a798de7a9246f25dfb9e208db3a6ecdeee64b73fc6" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
