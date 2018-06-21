b0290ab4c757b4eee68351ed1a7e410363b2c54c
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-21 16:36:38 +0000

    sstp-client: update 1.0.12 bottle.

--- Formula/sstp-client.rb
+++ Formula/sstp-client.rb
@@ -5,9 +5,10 @@ class SstpClient < Formula
   sha256 "487eb406579689803ce0397f6102b18641e4572ac7bc9b9e5f3027c84dcf67ff"
 
   bottle do
-    sha256 "f910157864b32a5cd004366b0e8ee6cefd09b0edd36afb89d751ede34e27df96" => :high_sierra
-    sha256 "1bbbe9f5558b4757f8244064144106b0bda636c2adcc3da734634895b960ff68" => :sierra
-    sha256 "c4b0b6b1ebc16783d22327173bfe1e90bd5fd329786a613422c858fc352672eb" => :el_capitan
+    rebuild 1
+    sha256 "9214db286259fd2c7d7127542b4a247a6059186fd22731409475114d4ddd0860" => :high_sierra
+    sha256 "12b5cd82cfb991999c8c151f4d1b6e2c0da45979dc1b6275099aeb0325ddcdc8" => :sierra
+    sha256 "cd1f3e4b99d653f9086b32a76addf6547d8e07aeac9c774d838cbb163de1001d" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
