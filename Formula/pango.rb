ad3840aa836e95baa978f49ec51d066520138dbc
BrewTestBot <brew-test-bot@googlegroups.com>
2018-06-21 14:11:32 +0000

    pango: update 1.42.1 bottle.

--- Formula/pango.rb
+++ Formula/pango.rb
@@ -5,9 +5,10 @@ class Pango < Formula
   sha256 "915a6756b298578ff27c7a6393f8c2e62e6e382f9411f2504d7af1a13c7bce32"
 
   bottle do
-    sha256 "74825fefb505c6d581915966256fbd11039e4e6ff169a49dcd0ffa67dbb970f6" => :high_sierra
-    sha256 "e437556a5d9271325d90a82680919673889809d0b5da758830230f27ee0f98c8" => :sierra
-    sha256 "27a07f281416d178c5178cb0d63fd8a80c72b2c54aad68f0508c52850fc6289a" => :el_capitan
+    rebuild 1
+    sha256 "121d216e455dfbbfa0c186670add67d3cc0fccea19bc6ba664c69f6c8ef1b1f4" => :high_sierra
+    sha256 "de9d815a8bdcc5d95091143f4ef3f8bfaa56341b2aa2c8c1c37e9c79e19a273c" => :sierra
+    sha256 "ca508b8aad94d42db18dc1bd236dbe7b011544ce720315cd258aee0f7265dd64" => :el_capitan
   end
 
   head do
