01abdc3e2fc3b27319db1957085269346a4864b6
Drew J. Sonne <drew.sonne@gmail.com>
2018-06-23 12:31:18 +0100

    Remove redundant version tag

--- Formula/ansi.rb
+++ Formula/ansi.rb
@@ -2,7 +2,6 @@ class Ansi < Formula
   desc "This bash script is designed to help you colorize words and bits of text"
   homepage "https://github.com/fidian/ansi"
   url "https://github.com/fidian/ansi/archive/1.1.0.tar.gz"
-  version "1.1.0"
   sha256 "0930acff725a505e430c68784e12731e8329998d60efcd759a7ff52e6baac959"
 
   def install
