bd8fec8659fd142b3050aed44dc0d87152c8fd81
Tom Parker <palfrey@tevp.net>
2018-06-24 16:04:42 +0100

    bashate: pypi -> files.pythonhosted for pbr

--- Formula/bashate.rb
+++ Formula/bashate.rb
@@ -9,7 +9,7 @@ class Bashate < Formula
   depends_on "python@2" if MacOS.version <= :snow_leopard
 
   resource "pbr" do
-    url "https://pypi.python.org/packages/source/p/pbr/pbr-4.0.4.tar.gz"
+    url "https://files.pythonhosted.org/packages/source/p/pbr/pbr-4.0.4.tar.gz"
     sha256 "a9c27eb8f0e24e786e544b2dbaedb729c9d8546342b5a6818d8eda098ad4340d"
   end
 
