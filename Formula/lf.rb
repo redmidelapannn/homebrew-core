eb81e2f78b673f796c1eda0d6f799fa30a4f511c
ptzz <ptzzptzzp@gmail.com>
2018-06-26 05:16:09 +0200

    Ask upstream to implement Go vendoring

--- Formula/lf.rb
+++ Formula/lf.rb
@@ -1,35 +1,24 @@
-require "language/go"
-
 class Lf < Formula
   desc "Terminal file manager"
   homepage "https://godoc.org/github.com/gokcehan/lf"
-  url "https://github.com/gokcehan/lf/archive/da126a3959d3c1695b67d93a517a72617cc740c4.tar.gz"
+  url "https://github.com/gokcehan/lf/archive/a607e53e5110b3ed3fd28bdc8e8c87a48f2e5b85.tar.gz"
   version "5"
-  sha256 "821c08f8cbb27657e8cdeeab00ca9ec3123ae678479b4a3b3e028d0c540e4182"
+  sha256 "0d624a5a2c71a59c91402e5759315d51ccaf92fc2ece1ab4244003b0e02c378a"
 
+  depends_on "dep" => :build
   depends_on "go" => :build
-
-  go_resource "github.com/mattn/go-runewidth" do
-    url "https://github.com/mattn/go-runewidth.git",
-      :revision => "9e777a8366cce605130a531d2cd6363d07ad7317"
-  end
-
-  go_resource "github.com/nsf/termbox-go" do
-    url "https://github.com/nsf/termbox-go.git",
-      :revision => "5c94acc5e6eb520f1bcd183974e01171cc4c23b3"
-  end
 
   def install
     ENV["GOPATH"] = buildpath
     bin_path = buildpath/"src/github.com/gokcehan/lf"
     bin_path.install Dir["*"]
-    Language::Go.stage_deps resources, buildpath/"src"
     cd bin_path do
+      system "dep", "ensure", "-vendor-only"
       system "go", "build", "-o", bin/"lf", "."
     end
   end
 
   test do
-    system "true"
+    system bin/"lf", "-doc"
   end
 end
