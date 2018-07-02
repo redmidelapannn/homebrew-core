253916b847c7658c2ea46b9f2a1b78fdefa42e5d
ageekymonk <ramzthecoder@gmail.com>
2018-07-02 20:20:31 +1000

    Adding support for kubernetes service catalog
    This formula is for kubernetes service catalog client called svcat

---
+++ Formula/kubernetes-service-catalog.rb
@@ -0,0 +1,30 @@
+class KubernetesServiceCatalog < Formula
+  desc "Consume services in K8s using the OSB API"
+  homepage "https://svc-cat.io/"
+  url "https://github.com/kubernetes-incubator/service-catalog.git",
+      :tag => "v0.1.23",
+      :revision => "87a5db0e1e0359ce372037a235ce4448944c8611"
+
+  depends_on "coreutils" => :build
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["NO_DOCKER"] = "1"
+    dir = buildpath/"src/github.com/kubernetes-incubator/service-catalog"
+
+    dir.install buildpath.children
+
+    cd dir do
+      ldflags = "-s -w -X github.com/kubernetes-incubator/service-catalog/pkg.VERSION=#{version}"
+      system "make", ".generate_files"
+      system "go", "build", "-ldflags", ldflags, "-o", "bin/svcat", "./cmd/svcat"
+      bin.install "bin/svcat"
+    end
+  end
+
+  test do
+    version_output = shell_output("#{bin}/svcat version --client 2>&1")
+    assert_match "Client Version: 0.1.23", version_output
+  end
+end
