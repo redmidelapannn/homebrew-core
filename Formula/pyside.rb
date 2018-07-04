1a2d578f4f8e2645b1750298ad8e81bff1b7e9ad
albertosottile <alby128@gmail.com>
2018-07-04 15:09:52 +0200

    pyside: use inreplace instead of a patch to fix build

--- Formula/pyside.rb
+++ Formula/pyside.rb
@@ -9,14 +9,10 @@ class Pyside < Formula
   depends_on "python@2"
   depends_on "qt"
 
-  # Patch to correct clang header include issue in PySide 5.11.0
-  # https://bugreports.qt.io/browse/PYSIDE-731
-  patch do
-    url "http://syncplay.s3.amazonaws.com/pyside-clang-header.diff"
-    sha256 "d3472787b0b67b60d1f7882f2871404b1826677cd96188de9cb0b54f1f4285fc"
-  end
-
   def install
+    inreplace "sources/shiboken2/ApiExtractor/clangparser/clangbuilder.cpp", '|| cStringStartsWith("/usr/include/sys/_types", cFileName)', \
+    "|| cStringStartsWith(\"/usr/include/sys/_types\", cFileName)\n                || cStringStartsWith(\"#{HOMEBREW_PREFIX}/opt\", cFileName)"
+
     dest_path=lib/"python2.7/site-packages"
     dest_path.mkpath
 
