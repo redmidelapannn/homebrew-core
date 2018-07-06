d7552f164f1beee9c5823a131c15a66f68f8dd76
BrewTestBot <brew-test-bot@googlegroups.com>
2018-07-06 11:37:58 +0000

    csv-localizer: add 1.2.1 bottle.

--- Formula/csv-localizer.rb
+++ Formula/csv-localizer.rb
@@ -4,6 +4,12 @@ class CsvLocalizer < Formula
   url "https://github.com/rogermolas/csv-localizer/archive/v1.2.1.tar.gz"
   sha256 "9ed12ed932673521659bee64edb9ff727c845a3a948aac00a3354b5f96a825e8"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9066e8de986bcc6f4e873c2d61a6d394a2bacc3d36fc86d0371f7daddfbc49e4" => :high_sierra
+    sha256 "9066e8de986bcc6f4e873c2d61a6d394a2bacc3d36fc86d0371f7daddfbc49e4" => :el_capitan
+  end
+
   depends_on "python@2"
 
   def install
