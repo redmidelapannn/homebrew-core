2391b39e7dd30b9976fce23cfcd4756f2b7d91ca
Claudia <claui@users.noreply.github.com>
2018-07-06 15:09:58 +0200

    gptfdisk: fix brew audit issues

--- Formula/gptfdisk.rb
+++ Formula/gptfdisk.rb
@@ -1,6 +1,6 @@
 class Gptfdisk < Formula
   desc "Text-mode partitioning tools"
-  homepage "http://www.rodsbooks.com/gdisk/"
+  homepage "https://www.rodsbooks.com/gdisk"
   url "https://downloads.sourceforge.net/project/gptfdisk/gptfdisk/1.0.0/gptfdisk-1.0.0.tar.gz"
   sha256 "5b66956743a799fc0471cdb032665c1391e82f9c5b3f1d7d726d29fe2ba01d6c"
   revision 1
@@ -13,8 +13,8 @@ class Gptfdisk < Formula
     sha256 "95593d9ce977a9529b11c9de8ee1089e56c67d76a642e13bdac31097aa5c7f69" => :mountain_lion
   end
 
-  depends_on "popt"
   depends_on "icu4c"
+  depends_on "popt"
 
   def install
     system "make", "-f", "Makefile.mac"
