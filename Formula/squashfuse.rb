c1a537218630ea0787fa38e38678874e7799f9d8
Sean Karlage <skarlage@fb.com>
2018-07-06 17:48:08 -0700

    Updates
    * Change download link to be pre-configurd tar.gz to avoid build-time
    deps.
    * Removed specific prefixes for compression algorithms, it's able to
    find them.
    * Fixed test to ensure the process is killed if it sticks around.

--- Formula/squashfuse.rb
+++ Formula/squashfuse.rb
@@ -1,16 +1,11 @@
 class Squashfuse < Formula
   desc "FUSE filesystem to mount squashfs archives"
   homepage "https://github.com/vasi/squashfuse"
-  url "https://github.com/vasi/squashfuse/archive/0.1.103.tar.gz"
-  sha256 "bba530fe435d8f9195a32c295147677c58b060e2c63d2d4204ed8a6c9621d0dd"
+  url "https://github.com/vasi/squashfuse/releases/download/0.1.103/squashfuse-0.1.103.tar.gz"
+  sha256 "42d4dfd17ed186745117cfd427023eb81effff3832bab09067823492b6b982e7"
 
-  # Build deps
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
   depends_on "pkg-config" => :build
 
-  # Compression algorithms supported by squashfuse
   depends_on "lz4"
   depends_on "lzo"
   depends_on :osxfuse
@@ -25,10 +20,6 @@ class Squashfuse < Formula
       "--prefix=#{prefix}",
     ]
 
-    %w[lz4 lz0 xz zlib zstd].each do |algorithm|
-      configure_args << "--with-#{algorithm}=#{HOMEBREW_PREFIX}"
-    end
-    system "./autogen.sh"
     system "./configure", *configure_args
     system "make", "install"
   end
@@ -36,10 +27,19 @@ class Squashfuse < Formula
   # Unfortunately, making/testing a squash mount requires sudo priviledges, so
   # just test that squashfuse execs for now.
   test do
-    pid = fork { exec "squashfuse", "--help" }
-    _, status = Process.wait2 pid
+    begin
+      pid = fork { exec "squashfuse", "--help" }
+      _, status = Process.wait2 pid
 
-    # Returns 254 after running --help.
-    assert_equal 254, status.exitstatus
+      # Returns 254 after running --help.
+      assert_equal 254, status.exitstatus
+    ensure
+      begin
+        Process.kill "TERM", pid
+
+      # The process already exited.
+      rescue Errno::ESRCH
+      end 
+    end
   end
 end
