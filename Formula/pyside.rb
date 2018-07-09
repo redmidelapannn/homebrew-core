90d4d9f1a18661bba95f2b3597f6b00dea050c86
albertosottile <alby128@gmail.com>
2018-07-09 17:03:41 +0200

    pyside: use patches for homebrew and 3.7 compatibility from upstream

--- Formula/pyside.rb
+++ Formula/pyside.rb
@@ -6,42 +6,58 @@ class Pyside < Formula
 
   depends_on "cmake" => :build
   depends_on "llvm" => :build
-  depends_on "python@2"
   depends_on "qt"
+  depends_on "python" => :recommended
+  depends_on "python@2" => :recommended
+
+  # Edited patch to fix clang header include issue in 5.11.0, safe to remove once 5.11.1 is released
+  # http://code.qt.io/cgit/pyside/pyside-setup.git/commit/?h=5.11.0&id=5662706937bd6a1449538539e3a503c6cbc45399
+  patch do
+    url "http://syncplay.s3.amazonaws.com/pyside-homebrew.patch"
+    sha256 "3a6a62ae8d4a7ab34f9ca66b5358e45a5e08c66f327f635f901bb68d6f97c8a4"
+  end
+
+  # Patch to add Python 3.7 support on 5.11.0, safe to remove once 5.11.1 is released
+  # https://code.qt.io/cgit/pyside/pyside-setup.git/commit/?h=5.11&id=4a32f9d00b043b7255b590b95e9b35e9de44c4ed
+  patch do
+    url "https://code.qt.io/cgit/pyside/pyside-setup.git/patch/?id=4a32f9d00b043b7255b590b95e9b35e9de44c4ed"
+    sha256 "3b88c02242172c7f58626bcb670fd0e2ef3259f628d50b755967c5dfb37e8a3b"
+  end
 
   def install
-    inreplace "sources/shiboken2/ApiExtractor/clangparser/clangbuilder.cpp", '|| cStringStartsWith("/usr/include/sys/_types", cFileName)', \
-    "|| cStringStartsWith(\"/usr/include/sys/_types\", cFileName)\n                || cStringStartsWith(\"#{HOMEBREW_PREFIX}/opt\", cFileName)"
+    Language::Python.each_python(build) do |python, version|
+      dest_path=lib/"python#{version}/site-packages"
+      dest_path.mkpath
 
-    dest_path=lib/"python2.7/site-packages"
-    dest_path.mkpath
-
-    args = %W[
-      install
-      --ignore-git
-      --no-examples
-      --macos-use-libc++
-      --jobs=#{ENV.make_jobs}
-      --install-lib #{lib}/python2.7/site-packages
-      --install-scripts #{bin}
-    ]
-    system "python", *Language::Python.setup_install_args(prefix), *args
-    (dest_path/"homebrew-pyside.pth").write "#{dest_path}\n"
+      args = %W[
+        --ignore-git
+        --no-examples
+        --macos-use-libc++
+        --jobs=#{ENV.make_jobs}
+        --install-lib #{lib}/python#{version}/site-packages
+        --install-scripts #{bin}
+        --skip-modules=UiTools
+      ]
+      system python, *Language::Python.setup_install_args(prefix), *args
+      (dest_path/"homebrew-pyside.pth").write "#{dest_path}\n"
+    end
   end
 
   test do
-    system "python", "-c", "import PySide2"
-    %w[
-      Core
-      Gui
-      Location
-      Multimedia
-      Network
-      Quick
-      Svg
-      WebEngineWidgets
-      Widgets
-      Xml
-    ].each { |mod| system "python", "-c", "import PySide2.Qt#{mod}" }
+    Language::Python.each_python(build) do |python, _version|
+      system python, "-c", "import PySide2"
+      %w[
+        Core
+        Gui
+        Location
+        Multimedia
+        Network
+        Quick
+        Svg
+        WebEngineWidgets
+        Widgets
+        Xml
+      ].each { |mod| system python, "-c", "import PySide2.Qt#{mod}" }
+    end
   end
 end
