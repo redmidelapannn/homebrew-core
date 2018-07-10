2e91ca368d7b553c2e2a56be653a400ec892a381
Francesco Durighetto <work@francescodurighetto.com>
2018-07-10 18:08:46 +0200

    update log4cxx to follow the current latest_stable branch which points to: https://github.com/apache/logging-log4cxx/commit/a0a28253d9c3b8da0d15aa41a8f5b6375c50c96e
    0.10.0 build suffers from many problems like the one that is not thread safe and many other problem.
    This is not the official 0.11.0 build but should contains these fixes
    https://logging.apache.org/log4cxx/next_stable/changes-report.html#a0.11.0

--- Formula/log4cxx.rb
+++ Formula/log4cxx.rb
@@ -1,9 +1,9 @@
 class Log4cxx < Formula
   desc "Library of C++ classes for flexible logging"
   homepage "https://logging.apache.org/log4cxx/index.html"
-  url "https://www.apache.org/dyn/closer.cgi?path=logging/log4cxx/0.10.0/apache-log4cxx-0.10.0.tar.gz"
-  sha256 "0de0396220a9566a580166e66b39674cb40efd2176f52ad2c65486c99c920c8c"
-  revision 1
+  url "https://github.com/kekkokk/logging-log4cxx/releases/download/0.10.0_with_fixes/log4cxx_latest_stable.tar.gz"
+  sha256 "d0e57ecaa61980f450677783736a6d71f549ef0c75adfd209e87fb0db6119f8c"
+  revision 2
 
   bottle do
     cellar :any
@@ -19,25 +19,8 @@ class Log4cxx < Formula
 
   depends_on "apr-util"
 
-  # Incorporated upstream, remove on next version update
-  # https://issues.apache.org/jira/browse/LOGCXX-400 (r1414037)
-  # https://issues.apache.org/jira/browse/LOGCXX-404 (r1414037)
-  patch :p0 do
-    url "https://gist.githubusercontent.com/cawka/b4a79f6b883c46ac1672/raw/be8b4e610a1e21b34aaaf8fb4151362dcfb782ff/LOGCXX-400,LOGCXX-404---r1414037.patch"
-    sha256 "822c24f4eebd970aa284672eec2f71c6f8e192a85d78edb15a232c15011a52d4"
-  end
-
-  # https://issues.apache.org/jira/browse/LOGCXX-417 (r1556413)
-  patch :p0 do
-    url "https://gist.githubusercontent.com/cawka/b4a79f6b883c46ac1672/raw/4188731bd771a961a91fcfbe561f3999b555b9c3/LOG4CXX-417---r1556413.patch"
-    sha256 "eca194ec349b4925d0ad53d2b67c18b6a1aa7a979e7bd8729cfd1ed1ef4994c7"
-  end
-
-  # https://issues.apache.org/jira/browse/LOGCXX-400 (reported)
-  patch :p1 do
-    url "https://gist.githubusercontent.com/cawka/b4a79f6b883c46ac1672/raw/f33998566cccf91fb84133e101f5a92a14b31aed/LOGCXX-404---domtestcase.cpp.patch"
-    sha256 "3eaf321e1df8e8e4a0a507a96646727180e7e721b2c42af22a5d40962d3dbecc"
-  end
+  # Fix to build LOGCXX-225
+  patch :DATA
 
   def install
     ENV.O2 # Using -Os causes build failures on Snow Leopard.
@@ -92,3 +75,21 @@ class Log4cxx < Formula
     assert_match /ERROR.*Foo/, shell_output("./test", 1)
   end
 end
+
+__END__
+diff --git a/src/main/include/log4cxx/helpers/simpledateformat.h b/src/main/include/log4cxx/helpers/simpledateformat.h
+index 9c27f68..39da173 100644
+--- a/src/main/include/log4cxx/helpers/simpledateformat.h
++++ b/src/main/include/log4cxx/helpers/simpledateformat.h
+@@ -27,10 +27,9 @@
+
+ #include <log4cxx/helpers/dateformat.h>
+ #include <vector>
++#include <locale>
+ #include <time.h>
+
+-namespace std { class locale; }
+-
+ namespace log4cxx
+ {
+         namespace helpers
