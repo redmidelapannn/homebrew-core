c726affd8ebb9768b0ec9fe4d703084402381f9a
BrewTestBot <brew-test-bot@googlegroups.com>
2018-07-12 21:54:44 +0000

    torxakis: add 0.7.0 bottle.

--- Formula/torxakis.rb
+++ Formula/torxakis.rb
@@ -6,6 +6,13 @@ class Torxakis < Formula
   sha256 "46a78b4303b170aa37f8da36bbe2e8ff27952575e98dcca608e1123a8d74c5d0"
   head "https://github.com/TorXakis/TorXakis.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "39f8cccbf22a9c3ac0d6c6f683532cecc5b61a7068ca2abee9868210d3473fcd" => :high_sierra
+    sha256 "adeeee80220f393cb87a0bb132a56b69922380c64aa78b23350feafa29b74f8b" => :sierra
+    sha256 "d2bdedcbeeecdc21fdae691edb87edec6599eede460fdd62b2966759639af5b0" => :el_capitan
+  end
+
   depends_on "haskell-stack" => :build
   depends_on "z3"
 
