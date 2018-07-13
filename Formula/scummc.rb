b12bb9648327c9e45c549ba605bd8d9686a056f7
idleberg <jan@idleberg.com>
2018-07-13 23:01:12 +0200

    drop checksums

--- Formula/scummc.rb
+++ Formula/scummc.rb
@@ -3,13 +3,6 @@ class Scummc < Formula
   homepage "https://github.com/AlbanBedel/scummc#readme"
   url "https://github.com/AlbanBedel/scummc.git", :revision => "b947f61fc9f550bcdcf66e771fc36795b6cd3687"
   version "0.2.0+20171206"
-  sha256 "6eed5d773a2b7de912c15cbfd3409d82939e08769dd559a22c63558e541093ac"
-
-  bottle do
-    sha256 "1de93783f408c2cfc634d8f13c8cb7db1e2544d021f15e0484e64a92af2ed3db" => :high_sierra
-    sha256 "bd2da4d91eb4e0f8fcf69ad9383a89979c73e539f1b1a5df8d7d99dc01fb67c5" => :sierra
-    sha256 "26dce34185c67fe8034effb99e248b9b68344063194ccb20c8c166b2f6c58dfb" => :el_capitan
-  end
 
   depends_on "bison" => :build
   depends_on "freetype" => :build
