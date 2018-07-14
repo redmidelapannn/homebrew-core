2843071c8954af40f00749657bd19291969dd7b2
ilovezfs <ilovezfs@icloud.com>
2018-07-14 19:09:02 +0000

    Update dartsim.rb

--- Formula/dartsim.rb
+++ Formula/dartsim.rb
@@ -32,7 +32,6 @@ class Dartsim < Formula
   def install
     ENV.cxx11
     system "cmake", ".", *std_cmake_args
-    system "make", "all"
     system "make", "install"
 
     # Avoid revision bumps whenever fcl's or libccd's Cellar paths change
