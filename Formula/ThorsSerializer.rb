d4baf922a1d53c238162dd791fb77e582077ad40
Loki Astari <Loki.Astari@gmail.com>
2018-07-16 23:24:57 -0700

    Fixed audit Issues
        Added "test do" block
        Removed redundant ":using" value in url

--- Formula/ThorsSerializer.rb
+++ Formula/ThorsSerializer.rb
@@ -1,7 +1,7 @@
 class Thorsserializer < Formula
   desc "Declarative Serialization Library (Json/Yaml) for C++"
   homepage "https://github.com/Loki-Astari/ThorsSerializer"
-  url "https://github.com/Loki-Astari/ThorsSerializer.git", :using => GitHubGitDownloadStrategy, :tag => "1.5.7"
+  url "https://github.com/Loki-Astari/ThorsSerializer.git", :tag => "1.5.7"
 
   ENV["COV"] = "gcov"
 
@@ -10,5 +10,45 @@ class Thorsserializer < Formula
   def install
     system "./configure", "--disable-binary", "--disable-vera", "--with-thor-build-on-travis", "--prefix=#{prefix}"
     system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include "ThorSerialize/JsonThor.h"
+      #include "ThorSerialize/SerUtil.h"
+      #include <sstream>
+      #include <iostream>
+      #include <string>
+
+      struct Block
+      {
+          std::string             key;
+          int                     code;
+      };
+      ThorsAnvil_MakeTrait(Block, key, code);
+
+      int main()
+      {
+          using ThorsAnvil::Serialize::jsonImport;
+          using ThorsAnvil::Serialize::jsonExport;
+
+          std::stringstream   inputData(R"({"key":"XYZ","code":37373})");
+
+          Block    object;
+          inputData >> jsonImport(object);
+
+          if (object.key != "XYZ" || object.code != 37373) {
+              std::cerr << "Fail\n";
+              return 1;
+          }
+          std::cerr << "OK\n";
+          return 0;
+      }
+    EOS
+    system ENV.cc, "-std=c++14", "test.cpp", "-o", "test",
+                   "-I#{include}",
+                   "-L#{lib}",
+                   "-lThorSerialize17"
+    system "./test"
   end
 end
