0e4f159f657709fcd1a132aaadb02b9595e8136c
ilovezfs <ilovezfs@icloud.com>
2018-07-18 21:46:23 -0700

    cquery 20180718

--- Formula/cquery.rb
+++ Formula/cquery.rb
@@ -2,8 +2,8 @@ class Cquery < Formula
   desc "C/C++ language server"
   homepage "https://github.com/cquery-project/cquery"
   # pull from git tag to get submodules
-  url "https://github.com/cquery-project/cquery.git", :tag => "v20180302",
-                                                      :revision => "f3e9e756e182b122bef8826a77047f6ccf5529b6"
+  url "https://github.com/cquery-project/cquery.git", :tag => "v20180718",
+                                                      :revision => "b523aa928acf8ffb3de6b22c79db7366a9672489"
   head "https://github.com/cquery-project/cquery.git"
 
   bottle do
