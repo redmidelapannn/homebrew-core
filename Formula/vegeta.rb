cf047cbe118553b97cf1ee42be6e166aa14101f5
ilovezfs <ilovezfs@icloud.com>
2018-07-22 05:34:53 +0000

    Update vegeta.rb

--- Formula/vegeta.rb
+++ Formula/vegeta.rb
@@ -17,8 +17,8 @@ class Vegeta < Formula
 
   def install
     ENV["GOPATH"] = buildpath
-    ENV.prepend_create_path "PATH", buildpath/"bin"
     (buildpath/"src/github.com/tsenart/vegeta").install buildpath.children
+    ENV.prepend_create_path "PATH", buildpath/"bin"
     cd "src/github.com/tsenart/vegeta" do
       system "make", "vegeta"
       bin.install "vegeta"
