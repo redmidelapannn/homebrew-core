462ac6386d21d8c4cc6457f235281db37f374094
yinheli <me@yinheli.com>
2018-07-24 01:05:40 +0800

    sshw 1.0.2

---
+++ Formula/sshw.rb
@@ -0,0 +1,14 @@
+class Sshw < Formula
+  desc "🐝  ssh client wrapper for automatic login"
+  homepage "https://github.com/yinheli/sshw"
+  url "https://github.com/yinheli/sshw/releases/download/v1.0.2/sshw-darwin-amd64.tar.gz"
+  sha256 "4dd11ea34bfcac3c4d318a917557b109c8001bd4157abf6ed7c777c0ada0b48d"
+
+  def install
+    bin.install "sshw"
+  end
+
+  test do
+    system "sshw -version"
+  end
+end
