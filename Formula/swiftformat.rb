ea0c4643e0add80e53e115612dc87a82bdb5e934
Nick Lockwood <nick@charcoaldesign.co.uk>
2018-07-25 00:05:55 +0200

    swiftformat 0.33.13

--- Formula/swiftformat.rb
+++ Formula/swiftformat.rb
@@ -1,13 +1,14 @@
 class Swiftformat < Formula
   desc "Formatting tool for reformatting Swift code"
   homepage "https://github.com/nicklockwood/SwiftFormat"
-  url "https://github.com/nicklockwood/SwiftFormat/archive/0.33.12.tar.gz"
-  sha256 "2d2a489b0719e62467d98abebb445b1c922c8f5263cccdcefd22909c8892e813"
+  url "https://github.com/nicklockwood/SwiftFormat/archive/0.33.13.tar.gz"
+  sha256 "f2141305bebe54f6cf9ea24067f0e3a083b12de16abdbaa0f4dfde197791d905"
   head "https://github.com/nicklockwood/SwiftFormat.git", :shallow => false
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e0f849e7cd6059deefbb162bf2b354406ec5d22514921f9e16012ebcd70b8d79" => :high_sierra
+    sha256 "55fe805e7e5727d2635e91c72c3cb65be363873dc8e8bd7624ae33a6a2472043" => :high_sierra
+    sha256 "cf50765e6cf0c8e84cc28103012a696bd8ac6b4487a613256428d26382d9d6d1" => :sierra
   end
 
   depends_on :xcode => ["9.3", :build]
