abd1547eab997707883326f8c8d2876bfa1c325b
Chonlasith Jucksriporn <chonlasith@gmail.com>
2018-07-24 20:15:07 +0700

    cotton 0.1.17 (new formula)

--- Formula/cotton.rb
+++ Formula/cotton.rb
@@ -4,14 +4,13 @@ class Cotton < Formula
     url "https://github.com/chonla/cotton/archive/0.1.17.tar.gz"
     sha256 "f199d6672b75dcefa8c517ddbbc4b94a8df2289597f6b598e0c37b211a3f6766"
     depends_on "go" => :build
-  
+
     def install
       system "bash", "build.sh"
       bin.install "/tmp/.gobuild/bin/cotton" => "cotton"
     end
-  
+
     test do
       system "#{bin}/cotton", "-v"
     end
-  end
-  
+end
