72a4be9b6a1e90d386f0b70104e83a2cf7d1e1c1
Chonlasith Jucksriporn <chonlasith@gmail.com>
2018-07-24 20:12:05 +0700

    cotton 0.1.17

---
+++ Formula/cotton.rb
@@ -0,0 +1,17 @@
+class Cotton < Formula
+    desc "Markdown Test Specification Runner"
+    homepage "https://github.com/chonla/cotton"
+    url "https://github.com/chonla/cotton/archive/0.1.17.tar.gz"
+    sha256 "f199d6672b75dcefa8c517ddbbc4b94a8df2289597f6b598e0c37b211a3f6766"
+    depends_on "go" => :build
+  
+    def install
+      system "bash", "build.sh"
+      bin.install "/tmp/.gobuild/bin/cotton" => "cotton"
+    end
+  
+    test do
+      system "#{bin}/cotton", "-v"
+    end
+  end
+  
