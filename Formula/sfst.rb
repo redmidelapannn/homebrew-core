f6022d2150bc582f86d13904608291a299fd604e
Nathan Glenn <nathan-gary.glenn@trivago.com>
2018-07-24 21:38:40 +0200

    Editing following @DomT4's suggestions

--- Formula/sfst.rb
+++ Formula/sfst.rb
@@ -3,24 +3,24 @@ class Sfst < Formula
   homepage "http://www.cis.uni-muenchen.de/~schmid/tools/SFST/"
   url "http://www.cis.uni-muenchen.de/~schmid/tools/SFST/data/SFST-1.4.7d.tar.gz"
   sha256 "5a13c6a45298197216a6299eb6cdf96595d2036572bb518b9e1c1893cb1a6d5f"
-  # dependencies are bison, flex and ncurses, but they are included in MacOS
 
   def install
-    cd "src"
-    system "make"
-    system "make", "DESTDIR=#{prefix}/", "install"
-    system "make", "DESTDIR=#{share}/", "maninstall"
+    cd "src" do
+      system "make"
+      system "make", "DESTDIR=#{prefix}/", "install"
+      system "make", "DESTDIR=#{share}/", "maninstall"
+    end
   end
 
   test do
+    require "open3"
     # this simple example is from the manual.
     # first compile a simple grammar
-    File.write("foo.fst", "Hello")
+    (testpath/"foo.fst").write "Hello"
     system "#{bin}/fst-compiler", "foo.fst", "foo.a"
-    assert_true File.file?("foo.a")
+    assert_predicate testpath/"foo.a", :exist?, "Foo.a should exist but does not!"
 
     # then parse simple input using that grammar
-    require "open3"
     Open3.popen3("#{bin}/fst-mor", "foo.a") do |stdin, stdout, _|
       stdin.write("Hello")
       stdin.close
