eada6ed81d4fb8c19f449d8d9318052f2c4acd40
ilovezfs <ilovezfs@icloud.com>
2018-07-25 07:20:32 +0000

    Update prettier.rb

--- Formula/prettier.rb
+++ Formula/prettier.rb
@@ -1,8 +1,8 @@
 require "language/node"
 
 class Prettier < Formula
-  desc "Code formatter for JavaScript, CSS, JSON, GraphQL, Markdown, YAML. "
-  homepage "https://prettier.io"
+  desc "Code formatter for JavaScript, CSS, JSON, GraphQL, Markdown, YAML"
+  homepage "https://prettier.io/"
   url "https://registry.npmjs.org/prettier/-/prettier-1.13.7.tgz"
   sha256 "a380bed5eda687946afceb09ed56ca960dc88292cc512303f426d9640522b47a"
 
@@ -15,6 +15,7 @@ class Prettier < Formula
 
   test do
     (testpath/"test.js").write("const arr = [1,2];")
-    assert_equal "const arr = [1, 2];\n", shell_output("#{bin}/prettier test.js")
+    output = shell_output("#{bin}/prettier test.js")
+    assert_equal "const arr = [1, 2];", output.chomp
   end
 end
