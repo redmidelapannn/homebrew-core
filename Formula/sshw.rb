5a829f8a4a0140441f1dd15244868ed66f64f74e
BrewTestBot <brew-test-bot@googlegroups.com>
2018-07-29 02:05:13 +0000

    sshw: add 1.0.4 bottle.

--- Formula/sshw.rb
+++ Formula/sshw.rb
@@ -4,6 +4,13 @@ class Sshw < Formula
   url "https://github.com/yinheli/sshw/archive/v1.0.4.tar.gz"
   sha256 "7b48c8126f4fca50c186f864c5e9db1578d7670e2b9b0887922d5a2c56051a71"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "32f7655c539287d256adf7096fd498988f5caef6066a25fb3e52c6ff1cad8091" => :high_sierra
+    sha256 "5844af60527c90659f94c410f7a72656502cd3585a0fdc8948ed5a3469710ab5" => :sierra
+    sha256 "db647aa7d540a34442aa9b905cbf8095803335e45b2c0793fac410d8595b0e2f" => :el_capitan
+  end
+
   depends_on "go" => :build
 
   def install
