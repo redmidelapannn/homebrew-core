646f48c6c9bb13f968d736bf2c578fbf56c4e313
BrewTestBot <brew-test-bot@googlegroups.com>
2018-07-30 22:53:30 +0000

    ekg2: update 0.3.1_2 bottle.

--- Formula/ekg2.rb
+++ Formula/ekg2.rb
@@ -7,10 +7,10 @@ class Ekg2 < Formula
   revision 2
 
   bottle do
-    sha256 "59cef56ba8d42aa5f198e11b7c342ddeabe8382543562ef9a13fc169ab5d4efa" => :high_sierra
-    sha256 "77d62ccb6a0ff4e9126850aa1c5287e583cc5f643f6fbe2505199e694de7a337" => :sierra
-    sha256 "34a9b94c6161be85b1b42a57395528982965357cffd7f4771456c01309fb402e" => :el_capitan
-    sha256 "dafaf67dca8fefd62b5e76ec03c8d71a85542d04157e53e0ceb915475a2c5067" => :yosemite
+    rebuild 1
+    sha256 "83d47d0f1a979464ae78c5c9c3cb11bec6c1f0cebc1f1d1256cb04bfac2a9cbe" => :high_sierra
+    sha256 "b3aa7b8dce8a31a193e31e1037cc401b4fe6c3bedc0dbb28d281bc1437af3fa4" => :sierra
+    sha256 "e4e2d486fd05370e4106f4b1b61906b20f56272fc69580f5b075942bed48394c" => :el_capitan
   end
 
   depends_on "pkg-config" => :build
