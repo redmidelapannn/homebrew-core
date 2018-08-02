35b700e3d5d5d97cc2f7a49fba7612a78d4d88cb
BrewTestBot <brew-test-bot@googlegroups.com>
2018-08-02 18:56:13 +0000

    libcanberra: update 0.30 bottle.

--- Formula/libcanberra.rb
+++ Formula/libcanberra.rb
@@ -10,11 +10,10 @@ class Libcanberra < Formula
   end
 
   bottle do
-    sha256 "c30a642b46b4da40690fd0c49b77d9ca2cea2c95d8e1f2e39f92d655b9b324c6" => :high_sierra
-    sha256 "066b33288fa7e3741ef45fc2d59578b4f1cf8141b5e6f9847635555a3436ef00" => :sierra
-    sha256 "dc9f8e76c81bb21afedd4deb9b9d9bbf3382f012192e5796658845e2083ca018" => :el_capitan
-    sha256 "3d820f8c9747e8658c226483267536c76b3dfdf44a9e14ea1327cb5cf0385ba8" => :yosemite
-    sha256 "10c6f3c931a349848b6436d10893e9b1bc5051658f125f6b67ddc1f9319396a0" => :mavericks
+    rebuild 1
+    sha256 "941d57ed8a02742d86757bfd6e5ad0dc17aa2e2dbd28f701f998e7d511c40b7c" => :high_sierra
+    sha256 "f51b6ec82af7070abe17dbc4ecc43079c006a15ae513f2873b0471feae0e1333" => :sierra
+    sha256 "c12ca2c05f84d70faafd3fdc7231b3f3a80ef7b66b35403a8dc35bdda1328db0" => :el_capitan
   end
 
   head do
