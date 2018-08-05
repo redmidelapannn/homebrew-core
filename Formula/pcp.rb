160309316db6c8d5c718e3770cefcc0b0a7da359
BrewTestBot <brew-test-bot@googlegroups.com>
2018-08-05 05:07:16 +0000

    pcp: add 4.1.1 bottle.

--- Formula/pcp.rb
+++ Formula/pcp.rb
@@ -3,6 +3,12 @@ class Pcp < Formula
   homepage "https://pcp.io/"
   url "https://dl.bintray.com/pcp/source/pcp-4.1.1.src.tar.gz"
   sha256 "430c3ce05db5e475dc0d962ac584a61a91c69b791e66d5cc684a7aada3e21f52"
+  bottle do
+    sha256 "8533d5d2babe46b969c03006741778afac3bd529c6011b4c6d418354dd5274fb" => :high_sierra
+    sha256 "3508fa4d30ab218787b2ab75e51c9b56d9d1b28f0f1be9fb31e76a84fb845748" => :sierra
+    sha256 "54e7b3b98ceda5bea177bb83fed868be5cfcb18d3ea5bd3862c7c86adbaa5da2" => :el_capitan
+  end
+
   depends_on "pkg-config" => :build
   depends_on "nss"
   depends_on "python"
