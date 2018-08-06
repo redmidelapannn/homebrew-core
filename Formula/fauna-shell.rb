08ba6a722f923cb5c56c75705decb58b125026b8
Alvaro Videla <alvaro.videla@fauna.com>
2018-08-06 23:28:55 +0200

    adds tests for endpoints functionality

--- Formula/fauna-shell.rb
+++ Formula/fauna-shell.rb
@@ -14,6 +14,53 @@ class FaunaShell < Formula
   end
 
   test do
-    system bin/"fauna --help"
+    # just making sure the program was properly installed
+    system bin/"fauna", "--help"
+
+    require "open3"
+
+    # there are no endpoints, so an error is raised
+    o, e, s = Open3.capture3("#{bin}/fauna list-endpoints")
+    assert_equal false, s.success?
+
+    o, e, s = Open3.capture3("#{bin}/fauna add-endpoint https://endpoint1:8443", :stdin_data=>"secret\nendpoint1\n")
+    assert s.success?
+
+    # the endpoint 'endpoint1' was added and is listed as default (*)
+    o, e, s = Open3.capture3("#{bin}/fauna list-endpoints")
+    assert_equal "endpoint1 *\n", o
+    assert s.success?
+
+    # by no providing an endpoint alias, it's taken from the hostname
+    o, e, s = Open3.capture3("#{bin}/fauna add-endpoint https://endpoint2:8443", :stdin_data=>"secret\n\n")
+    assert s.success?
+
+    # the endpoint 'endpoint2' was added, and 'endpoint1' is still listed as default (*)
+    o, e, s = Open3.capture3("#{bin}/fauna list-endpoints")
+    assert_equal "endpoint1 *\nendpoint2 \n", o
+    assert s.success?
+
+    # sets 'endpoint2' as the default endpoint
+    o, e, s = Open3.capture3("#{bin}/fauna default-endpoint endpoint2")
+    assert s.success?
+
+    # endpoint 'endpoint2' is now the default endpoint (*)
+    o, e, s = Open3.capture3("#{bin}/fauna list-endpoints")
+    assert_equal "endpoint1 \nendpoint2 *\n", o
+    assert s.success?
+
+    o, e, s = Open3.capture3("#{bin}/fauna delete-endpoint endpoint1", :stdin_data=>"y\n")
+    assert s.success?
+
+    o, e, s = Open3.capture3("#{bin}/fauna list-endpoints")
+    assert_equal "endpoint2 *\n", o
+    assert s.success?
+
+    o, e, s = Open3.capture3("#{bin}/fauna delete-endpoint endpoint2", :stdin_data=>"y\n")
+    assert s.success?
+
+    # there are no endpoints, so an error is raised
+    o, e, s = Open3.capture3("#{bin}/fauna list-endpoints")
+    assert_equal false, s.success?
   end
 end
