bf6646ad49b5d4799cfdf60604173916a4a4e55a
Antoine Lépée <alepee@gmail.com>
2018-08-07 16:06:02 +0200

    hyperkit: add test when virtualization is supported

--- Formula/hyperkit.rb
+++ Formula/hyperkit.rb
@@ -10,6 +10,11 @@ class Hyperkit < Formula
 
   depends_on "libev"
 
+  resource "tinycorelinux" do
+    url "https://dl.bintray.com/markeissler/homebrew/hyperkit-kernel/tinycorelinux_8.x.tar.gz"
+    sha256 "560c1d2d3a0f12f9b1200eec57ca5c1d107cf4823d3880e09505fcd9cd39141a"
+  end
+
   def install
     system "opam", "init", "--no-setup"
     opam_dir = "#{buildpath}/.brew_home/.opam"
@@ -39,5 +44,41 @@ class Hyperkit < Formula
 
   test do
     assert_match(version.to_s, shell_output("#{bin}/hyperkit -v 2>&1"))
+
+    if Hardware::CPU.features.include? :vmx
+      resource("tinycorelinux").stage do |context|
+        tmpdir = context.staging.tmpdir
+        path_resource_versioned = Dir.glob(tmpdir.join("tinycorelinux_[0-9]*"))[0]
+        cp(File.join(path_resource_versioned, "vmlinuz"), testpath)
+        cp(File.join(path_resource_versioned, "initrd.gz"), testpath)
+      end
+
+      (testpath / "test_hyperkit.exp").write <<-EOS
+        #!/usr/bin/env expect -d
+        set KERNEL "./vmlinuz"
+        set KERNEL_INITRD "./initrd.gz"
+        set KERNEL_CMDLINE "earlyprintk=serial console=ttyS0"
+        set MEM {512M}
+        set PCI_DEV1 {0:0,hostbridge}
+        set PCI_DEV2 {31,lpc}
+        set LPC_DEV {com1,stdio}
+        set ACPI {-A}
+        spawn #{bin}/hyperkit $ACPI -m $MEM -s $PCI_DEV1 -s $PCI_DEV2 -l $LPC_DEV -f kexec,$KERNEL,$KERNEL_INITRD,$KERNEL_CMDLINE
+        set pid [exp_pid]
+        set timeout 20
+        expect {
+          timeout { puts "FAIL boot"; exec kill -9 $pid; exit 1 }
+          "\\r\\ntc@box:~$ "
+        }
+        send "sudo halt\\r\\n";
+        expect {
+          timeout { puts "FAIL shutdown"; exec kill -9 $pid; exit 1 }
+          "reboot: System halted"
+        }
+        expect eof
+        puts "\\nPASS"
+      EOS
+      system "expect", "test_hyperkit.exp"
+    end
   end
 end
