2c180eac2e64cf2a3c3c1b9a6cf931cf9e842cd7
ageekymonk <ramzthecoder@gmail.com>
2018-08-08 06:59:38 +1000

    Updating to latest version, fixing the alignment for better readability and optimizing the bin install to remove extra lines

--- Formula/kubernetes-service-catalog-client.rb
+++ Formula/kubernetes-service-catalog-client.rb
@@ -2,16 +2,16 @@ class KubernetesServiceCatalogClient < Formula
   desc "Consume Services in k8s using the OSB API"
   homepage "https://svc-cat.io/"
   url "https://github.com/kubernetes-incubator/service-catalog.git",
-      :tag => "v0.1.23",
-      :revision => "87a5db0e1e0359ce372037a235ce4448944c8611"
+      :tag => "v0.1.28",
+      :revision => "0aaf02aba556b945a357fce4e82f62122d158f2a"
 
   depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = buildpath
     ENV["NO_DOCKER"] = "1"
-    dir = buildpath/"src/github.com/kubernetes-incubator/service-catalog"
 
+    dir = buildpath/"src/github.com/kubernetes-incubator/service-catalog"
     dir.install buildpath.children
 
     cd dir do
@@ -20,8 +20,7 @@ class KubernetesServiceCatalogClient < Formula
         github.com/kubernetes-incubator/service-catalog/pkg.VERSION=v#{version}
       ]
       system "go", "build", "-ldflags", ldflags.join(" "), "-o",
-             "bin/svcat", "./cmd/svcat"
-      bin.install "bin/svcat"
+             bin/"svcat", "./cmd/svcat"
       prefix.install_metafiles
     end
   end
