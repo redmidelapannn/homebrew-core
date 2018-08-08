32fa26ba5758f9ed612cda56f87bdd36dba499e3
Sebastian Schmidt <publicarray@users.noreply.github.com>
2018-08-09 09:00:50 +1000

    Code review

--- Formula/infrakit.rb
+++ Formula/infrakit.rb
@@ -4,9 +4,7 @@ class Infrakit < Formula
   url "https://github.com/docker/infrakit.git",
       :tag => "v0.5",
       :revision => "3d2670e484176ce474d4b3d171994ceea7054c02"
-  head "https://github.com/docker/infrakit.git"
 
-  depends_on "automake" => :build
   depends_on "go" => :build
   depends_on "libvirt" => :build
   depends_on "pkg-config" => :build
@@ -22,9 +20,8 @@ class Infrakit < Formula
   end
 
   test do
-    ENV["INFRAKIT_DIR"] = ENV["HOME"]
+    ENV["INFRAKIT_HOME"] = ENV["HOME"]
     ENV["INFRAKIT_CLI_DIR"] = ENV["HOME"]
-    ENV["INFRAKIT_PLUGINS_DIR"] = ENV["HOME"]
     assert_match revision.to_s, shell_output("#{bin}/infrakit version")
   end
 end
