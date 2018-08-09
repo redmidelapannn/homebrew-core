b57ba10cee71074465bea3e3be550d081c192672
Ron Mordechai <ronmrdechai@gmail.com>
2018-08-10 00:20:53 +0300

    Updated for comments

--- Formula/range-v3.rb
+++ Formula/range-v3.rb
@@ -3,15 +3,16 @@ class RangeV3 < Formula
   homepage "https://ericniebler.github.io/range-v3/"
   url "https://github.com/ericniebler/range-v3/archive/0.3.6.tar.gz"
   sha256 "ce6e80c6b018ca0e03df8c54a34e1fd04282ac1b068cd39e902e2e5201ac117f"
+
   depends_on "cmake" => :build
 
   def install
     system "cmake", ".",
-      "-DRANGE_V3_TESTS=OFF",
-      "-DRANGE_V3_HEADER_CHECKS=OFF",
-      "-DRANGE_V3_EXAMPLES=OFF",
-      "-DRANGE_V3_PERF=OFF",
-      *std_cmake_args
+                    "-DRANGE_V3_TESTS=OFF",
+                    "-DRANGE_V3_HEADER_CHECKS=OFF",
+                    "-DRANGE_V3_EXAMPLES=OFF",
+                    "-DRANGE_V3_PERF=OFF",
+                    *std_cmake_args
     system "make", "install"
   end
 
@@ -28,6 +29,6 @@ class RangeV3 < Formula
       }
     EOS
     system ENV.cc, "-std=c++11", "-o", "test", "test.cpp"
-    system "./test"
+    assert_equal "./test", 0
   end
 end
