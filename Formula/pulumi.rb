fa27a92f289a1913cfb8662272194f70c7546fd8
Thomas Schersach <tschersach@gmail.com>
2018-08-10 19:59:07 +0200

    Updated formula to build from source, and improved the test

--- Formula/pulumi.rb
+++ Formula/pulumi.rb
@@ -1,37 +1,38 @@
 class Pulumi < Formula
   desc "Define cloud applications and infrastructure in your favorite language"
   homepage "https://pulumi.io/"
-  url "https://github.com/pulumi/pulumi.git",
-      :tag => "v0.14.3"
+  version "666"
+  url "https://github.com/Tirke/pulumi.git",
+      :branch => "tirke-test"
+  # url "https://github.com/pulumi/pulumi.git",
+  #     :tag => "v0.15.0-rc1",
+  #     :revision => "cf93373a84a257e732efa96cc54291eccee87b21"
+  head "https://github.com/pulumi/pulumi.git",
+       :shallow => false
 
   depends_on "dep" => :build
   depends_on "go" => :build
-  depends_on "node@8" => :build
-  depends_on "pandoc" => :build
-  depends_on "pipenv" => :build
-  depends_on "python@2" => :build
-  depends_on "yarn" => :build
 
   def install
-    ENV.deparallelize
     ENV["GOPATH"] = buildpath
-    # Remove once opt/pulumi/bin is not needed anymore
-    ENV["PULUMI_ROOT"] = buildpath / "opt/pulumi/bin"
-    (buildpath / "opt/pulumi/bin").mkpath
 
     contents = Dir["{*,.git,.gitignore}"]
     (buildpath / "src/github.com/pulumi/pulumi").install contents
     (buildpath / "bin").mkpath
 
     cd "src/github.com/pulumi/pulumi" do
-      system "make", "ensure"
-      system "make", "only_build"
+      system "dep", "ensure", "-v"
+      system "make", "dist"
       bin.install Dir["#{buildpath}/bin/*"]
     end
   end
 
   test do
-    output = shell_output("#{bin}/pulumi version")
-    assert_match "v0.14.3", output
+    ENV["PULUMI_ACCESS_TOKEN"] = "local://"
+    ENV["PULUMI_TEMPLATE_PATH"] = testpath
+    system "pulumi", "new", "aws-typescript", "--generate-only", "-y"
+    # assert_equal "v0.14.3", output
+    # system bin/"ng", "new", "angular-homebrew-test", "--skip-install"
+    # assert_predicate testpath/"angular-homebrew-test/package.json", :exist?, "Project was not created"
   end
 end
