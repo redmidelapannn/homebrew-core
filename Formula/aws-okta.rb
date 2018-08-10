3dda300bd4d9e71f2c62914e66bb295656b5fe26
Jose Luis Salas <josacar@users.noreply.github.com>
2018-08-10 17:56:49 +0200

    Apply feedback to initial aws-okta formula

--- Formula/aws-okta.rb
+++ Formula/aws-okta.rb
@@ -1,12 +1,12 @@
 class AwsOkta < Formula
-  desc "Aws-vault like tool for Okta authentication"
+  desc "Authenticate with AWS using your Okta credentials"
   homepage "https://github.com/segmentio/aws-okta"
   url "https://github.com/segmentio/aws-okta/archive/v0.19.0.tar.gz"
 
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = p buildpath
+    ENV["GOPATH"] = buildpath
     (buildpath/"src/github.com/segmentio/aws-okta").install buildpath.children
     cd "src/github.com/segmentio/aws-okta" do
       system "go", "build", "-ldflags", "-X main.Version=#{version}"
@@ -16,6 +16,15 @@ class AwsOkta < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/aws-okta version", 0)
+    require 'pty'
+
+    PTY.spawn("#{bin}/aws-okta add") do |input, output, pid|
+      output.puts "organization\n"; input.gets
+      output.puts "username\n"; input.gets
+      output.puts "password\n"; input.gets
+      input.gets
+      assert_match "Added credentials for user username", input.gets.chomp
+      input.close
+    end
   end
 end
