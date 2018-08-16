f3490f65a8c3487da9a8f0238985824bc82bfde0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-16 21:07:51 +0000

    xcselect: add 1.1.0.14 bottle.

--- Formula/xcselect.rb
+++ Formula/xcselect.rb
@@ -4,6 +4,13 @@ class Xcselect < Formula
   url "https://github.com/pandawebsoft/xcselect/archive/v1.1.0.14.tar.gz"
   sha256 "75639d12e5d91af4d4e0806518727d779bbf72b76197f44f7c08bf0cd71c4015"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "cd91bc5b36428a91105179f091575932f2dd1c3d7978826d19e26cbf0458da43" => :high_sierra
+    sha256 "cd91bc5b36428a91105179f091575932f2dd1c3d7978826d19e26cbf0458da43" => :sierra
+    sha256 "cd91bc5b36428a91105179f091575932f2dd1c3d7978826d19e26cbf0458da43" => :el_capitan
+  end
+
   def install
     bin.install "bin/xcselect"
   end
