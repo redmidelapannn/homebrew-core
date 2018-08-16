52d3da20a4d06068b5e20a433ef8937846439828
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-16 19:18:39 +0000

    pinentry-mac: update 0.9.4 bottle.

--- Formula/pinentry-mac.rb
+++ Formula/pinentry-mac.rb
@@ -7,12 +7,10 @@ class PinentryMac < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "aa7a00ec594541e43a74cf7ae16cb05317d5b73c7b6b6647ca349584280eaad7" => :high_sierra
-    sha256 "3da6a88abbd4bb04eb2455d8e6998f4fc4db77c3f765d52b7eadf364e82aeaa7" => :sierra
-    sha256 "c3d508c96256c50b6a62f9e64fc4cb28810a910927c21f7defbe8af11a3c5961" => :el_capitan
-    sha256 "b96a51a263a9447101d4bb8dc4247f324531bd4fd96218f6e170edfc983a87f7" => :yosemite
-    sha256 "c2538b57edce2eb7ccc10a32e16ccfbbbe8e61c384c4db8d5a62b04d3815c0ed" => :mavericks
-    sha256 "0d6eb6b84a6389e208d4ec055d5f10f84b48bad94d00e9599b9aafb3dfaf71d8" => :mountain_lion
+    rebuild 1
+    sha256 "22f2eb8913fcd4241885a7739119d604791fcbe78f72a54d2d8981bd1103536a" => :high_sierra
+    sha256 "5b89032e22a908676d1ab5544334442be7c5066340f8287d3e4270db9c29c314" => :sierra
+    sha256 "92692077ad0b2de626aa6b2035a44c0f0ed7fd40105359c1ba9a71979a25665c" => :el_capitan
   end
 
   depends_on :xcode => :build
