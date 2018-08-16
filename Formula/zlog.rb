907ca74c49dd14777afa24f33bd6ebf010bbc6e6
秦新 <qinxin_0720@live.com>
2018-08-16 16:00:50 +0800

    zlog 1.2.12 (new formula)

---
+++ Formula/zlog.rb
@@ -0,0 +1,15 @@
+class Zlog < Formula
+  desc "High-performance C logging library"
+  homepage "https://github.com/HardySimpson/zlog"
+  url "https://github.com/HardySimpson/zlog/archive/1.2.12.tar.gz"
+  sha256 "9c6014a3f74d136c70255539beba11f30e1d3617d07ce7ea917b35f3e52bac20"
+
+  def install
+    system "make"
+    system "make", "install"
+  end
+  
+  test do
+    system "#{bin}/zlog-chk-conf", "-h"
+  end
+end
