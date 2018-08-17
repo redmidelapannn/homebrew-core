7663cf787b4a483a18b3287f6221d39b23ec6fd8
Dominyk Tiller <dominyktiller@gmail.com>
2018-08-17 01:41:07 +0100

    Sigh

--- Formula/docbook-xsl.rb
+++ Formula/docbook-xsl.rb
@@ -3,6 +3,7 @@ class DocbookXsl < Formula
   homepage "https://docbook.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/docbook/docbook-xsl/1.79.1/docbook-xsl-1.79.1.tar.bz2"
   sha256 "725f452e12b296956e8bfb876ccece71eeecdd14b94f667f3ed9091761a4a968"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
