4be419339f71980f7a9ac3fef769eb5f42782e32
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-19 21:50:16 +0000

    lua: update 5.3.5 bottle.

--- Formula/lua.rb
+++ Formula/lua.rb
@@ -6,10 +6,10 @@ class Lua < Formula
 
   bottle do
     cellar :any
-    sha256 "e4878844eb5b248ac72df3fead9d5dac8b432ad4385c1956d8577884fb2520c1" => :mojave
-    sha256 "0519eedf38f18d868842cead8c66ea1784ead938e0eed940d89e7d50c8259884" => :high_sierra
-    sha256 "e93b77f260956a5f16ff0df19c8bdaa6047ff6fe7cde806f65624c728db63ae9" => :sierra
-    sha256 "79caa0554afffb84b0827f50e65dc57d7e7d34ff83db76341427ed5293b3a21c" => :el_capitan
+    rebuild 1
+    sha256 "84e784b33b445eedd6d15e1ab1dab12335edce19a3b786d5bc33fad7db74ab51" => :high_sierra
+    sha256 "fa9579e5ef63647e4a0fc388942cf4e6fc332284fce2a7026ce0567a45a952e0" => :sierra
+    sha256 "0553106d20a1579b22edcf3f75910f3aa1c16c96adc3d1c3b8b8e356fdc8e582" => :el_capitan
   end
 
   option "without-luarocks", "Don't build with Luarocks support embedded"
