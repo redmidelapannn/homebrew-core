860c71ba8102a2989fcbe2d9e5f5618c736442c7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-20 08:04:36 +0000

    python@2: update 2.7.15_1 bottle.

--- Formula/python@2.rb
+++ Formula/python@2.rb
@@ -8,6 +8,7 @@ class PythonAT2 < Formula
 
   bottle do
     rebuild 2
+    sha256 "e0a012ca945ee15158c402e211d8549c2e69d342ea2c0c398ae04c3b219c7026" => :mojave
     sha256 "d7363d4915adde267e383547dc10d946434a2e0ce596a644ee475a2923380d5d" => :high_sierra
     sha256 "8b778a6c52f90a8c54c91174b05c1044fbf720771124468ceb2b4183f81d9780" => :sierra
     sha256 "f39c5bff2b9d7c901d0dc12a70eb067d7123afbfc17e2643898b112fcd938359" => :el_capitan
