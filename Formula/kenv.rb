f3e9c7f33de5c5e817f5004f6e19691ebf2e9a21
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-21 15:16:44 +0000

    kenv: add 0.3.1 bottle.

--- Formula/kenv.rb
+++ Formula/kenv.rb
@@ -3,6 +3,13 @@ class Kenv < Formula
   homepage "https://github.com/thisendout/kenv"
   url "https://github.com/thisendout/kenv/archive/v0.3.1.tar.gz"
   sha256 "c245583aee13cb0e1b6ec44e7d16fe83455fbca2657b73f49f1d7328b8594204"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a4888ec5a5adbc0ac806af814f157f93ff82b7152eaa37a43ecd46ae832228e8" => :high_sierra
+    sha256 "dbb41d102b47bb6789a836d02445cbed2c6b050fe11893537d058c519d706941" => :sierra
+    sha256 "ad5c37d4641ff0be873aa64d666a25b31efc63df88d6fa151b379c63bc934fe4" => :el_capitan
+  end
+
   depends_on "go" => :build
 
   def install
