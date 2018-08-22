0fcf5b9dc6a6fbe6f482d1993b31cd52df6240dd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 13:34:30 +0000

    pdal: update 1.7.2_1 bottle.

--- Formula/pdal.rb
+++ Formula/pdal.rb
@@ -7,9 +7,9 @@ class Pdal < Formula
   head "https://github.com/PDAL/PDAL.git"
 
   bottle do
-    sha256 "9d05cf0f554fead70d78c89b153cd70b424d7682ee0842770e6dfdbc1dea300d" => :high_sierra
-    sha256 "a3ca061671f7a7883a7c0aa5497676356fd449283e8b18d65830ae4c30b8e995" => :sierra
-    sha256 "4d68eb0946cbe5853fbc80229e3b40d1f9eeb0d16fdef5a70724733fb46b5d0e" => :el_capitan
+    sha256 "86bd198f9ddadfd92e694e2bacdd65f27a85ddacd353a0d21495c87fcaf76405" => :high_sierra
+    sha256 "0ab04d198c9a0dfc189c423a0538bd2dfdeec3558526bd541d9682fd068bb288" => :sierra
+    sha256 "0ada5584d3945b2392b999cc5b8a50a19a7b9984a15554e20b3c3914863dd372" => :el_capitan
   end
 
   depends_on "cmake" => :build
