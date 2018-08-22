25e5e2cdfabad1b8092a92835e15435bd9c10756
alswl <alswlx@gmail.com>
2018-08-22 12:00:20 +0800

    feat: add luit 20180628

---
+++ Formula/luit.rb
@@ -0,0 +1,25 @@
+class Luit < Formula
+  desc "Filter run between arbitrary application and UTF-8 terminal emulator"
+  homepage "http://invisible-island.net/luit/"
+  url "ftp://ftp.invisible-island.net/luit/luit-20180628.tgz"
+  sha256 "7b84f63072589e9b03bb3e99e01ef344bb37793b76ad1cbb7b11f05000d64844"
+  depends_on "cmake" => :build
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test luit`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
