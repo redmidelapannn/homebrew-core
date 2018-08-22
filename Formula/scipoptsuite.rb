6dcddb0e1345536801146abb69a4ba5f20e0aa54
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-22 14:13:24 +0000

    scipoptsuite: add 6.0.0 bottle.

--- Formula/scipoptsuite.rb
+++ Formula/scipoptsuite.rb
@@ -4,6 +4,12 @@ class Scipoptsuite < Formula
   url "http://scip.zib.de/download/release/scipoptsuite-6.0.0.tgz"
   sha256 "a91119687e521575070c43eaa30bc15ce461e9dc414651793ea4c4ca33b89750"
 
+  bottle do
+    sha256 "f08cbf2c79bb2bf7d8e5a315624c2f04489400c91e2012db09658b5ebef7ba14" => :high_sierra
+    sha256 "3d750c2a3fdf827ffb0f2bb987604211a1a4114706461c4072fb32aae4d8004a" => :sierra
+    sha256 "15c2d2b99b4645f06ce4cde07e52de001d78e4e279f6872f276dd36eaac38b15" => :el_capitan
+  end
+
   depends_on "cmake" => :build
   depends_on "gmp"
   depends_on "readline"
