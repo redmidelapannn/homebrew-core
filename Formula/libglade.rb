3a8c5c334353807cfaaf62475f7905ef3dd88068
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-25 18:00:22 +0000

    libglade: update 2.6.4_3 bottle.

--- Formula/libglade.rb
+++ Formula/libglade.rb
@@ -6,6 +6,7 @@ class Libglade < Formula
   revision 3
 
   bottle do
+    sha256 "4cba57fae8d8fa94b32ff0e8cca52331884d8d36000edf791668705e3545e1f3" => :mojave
     sha256 "2639d4af6b770ed782d7080b2b6105a0190eae1d8fdfd8076cdf3d89862b9364" => :high_sierra
     sha256 "a26aa0e778ef074f9ba9726945297885fbfd49acab0ab392096990964e844fed" => :sierra
     sha256 "057e25800e73c6233f353ed97ae4f7dfe1ca1f5eada9858d6527e03d4632ba87" => :el_capitan
