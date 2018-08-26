b1a092f3559098935162a01f31e5434ffaad6bb5
Dominyk Tiller <dominyktiller@gmail.com>
2018-08-26 04:22:44 +0100

    geogram: style tweaks

--- Formula/geogram.rb
+++ Formula/geogram.rb
@@ -5,6 +5,7 @@ class Geogram < Formula
   sha256 "08211b1d6f21e14701e3fd5c79adbe331cdf66b8af84efdb54cd7048244691b5"
 
   depends_on "cmake" => :build
+  depends_on "glfw"
 
   resource "bunny" do
     url "https://raw.githubusercontent.com/FreeCAD/Examples/be0b4f9/Point_cloud_ExampleFiles/PointCloud-Data_Stanford-Bunny.asc"
@@ -12,22 +13,25 @@ class Geogram < Formula
   end
 
   def install
-    # don't write directly into /usr/local/lib/cmake/modules
-    inreplace "CMakeLists.txt", "lib/cmake/modules", "#{share}/cmake/Modules"
-    # workaround for undefined _mm_set_pd1 in clang
+    # Workaround for undefined _mm_set_pd1 in clang; fixed in next release.
+    # https://lists.gforge.inria.fr/pipermail/geogram-users/2018-August/000158.html
     inreplace "src/lib/geogram/numerics/predicates.cpp", "_mm_set_pd1", "_mm_set1_pd"
 
-    (buildpath/"CMakeOptions.txt").append_lines "set(CMAKE_INSTALL_PREFIX #{prefix})"
+    (buildpath/"CMakeOptions.txt").append_lines <<~EOS
+      set(CMAKE_INSTALL_PREFIX #{prefix})
+      set(GEOGRAM_USE_SYSTEM_GLFW3 ON)
+    EOS
 
-    system "sh", "-f", "configure.sh"
+    system "./configure.sh"
     cd "build/Darwin-clang-dynamic-Release" do
       system "make", "install"
     end
+
+    (share/"cmake/Modules").install Dir[lib/"cmake/modules/*"]
   end
 
   test do
-    testpath.install resource("bunny")
-    mv "PointCloud-Data_Stanford-Bunny.asc", "bunny.xyz"
+    resource("bunny").stage { testpath.install Dir["*"].first => "bunny.xyz" }
     system "#{bin}/vorpalite", "profile=reconstruct", "bunny.xyz", "bunny.meshb"
     assert_predicate testpath/"bunny.meshb", :exist?, "bunny.meshb should exist!"
   end
