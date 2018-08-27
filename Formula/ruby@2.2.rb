f430a4cac7ac3f4000a122ddd39800201dfb2417
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-27 03:00:40 +0000

    ruby@2.2: update 2.2.10 bottle.

--- Formula/ruby@2.2.rb
+++ Formula/ruby@2.2.rb
@@ -5,6 +5,7 @@ class RubyAT22 < Formula
   sha256 "bf77bcb7e6666ccae8d0882ea12b05f382f963f0a9a5285a328760c06a9ab650"
 
   bottle do
+    sha256 "5b32ccb51fd40b2a8abb11ee3819ce9f02bcf00afe13b1047fdda0b8c2ab5e80" => :mojave
     sha256 "b31c95511243da91d1049e32fbe660447f9a6920369fff9f1fc9c854f14f7f20" => :high_sierra
     sha256 "3391108f892cbe37ca39b5ac4e79a31eaaee321d7001f0380f8c7282b737a22b" => :sierra
     sha256 "394e22d1904fb9f59a3b34a8a4ac71608c0c3f24b73d58dbaa3fa4f4e65785ad" => :el_capitan
