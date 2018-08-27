8d8fb9da23f70557303c4bba57e0e1a1f553dce0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-27 05:46:58 +0000

    eslint: update 5.4.0 bottle.

--- Formula/eslint.rb
+++ Formula/eslint.rb
@@ -8,6 +8,7 @@ class Eslint < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b30c6a1742b47cb1970d732e5a482d31c2e3e5fc19c51b4f6d6102e974500a30" => :mojave
     sha256 "a8fa91afbc63e27f0687e0a7dae5ada8b02d1eef03a32752fc4f7f458e9a0951" => :high_sierra
     sha256 "c2e4c24651b34fa120287ddff0682e6c4f1d6d0e64afaf694e40f1160503f6b0" => :sierra
     sha256 "dd3a4d89d1dfe385a4c553e88e928ed4013c6e54f1f73cd034e206605955bf45" => :el_capitan
