134482a6b330fa24171b1836e459e5b94f730065
Samuel Bétrisey <samuel@sam.ninja>
2018-08-27 03:26:50 +0200

    certgraph: fix formula
    - fix formatting
    - install meta files
    - add test

--- Formula/certgraph.rb
+++ Formula/certgraph.rb
@@ -2,8 +2,8 @@ class Certgraph < Formula
   desc "Intelligence tool to crawl the graph of certificate Alternate Names"
   homepage "https://lanrat.github.io/certgraph"
   url "https://github.com/lanrat/certgraph.git",
-    :tag => "20180217",
-    :revision => "41927387b1544a0d668c57b9883ed18381494ec4"
+      :tag => "20180217",
+      :revision => "41927387b1544a0d668c57b9883ed18381494ec4"
 
   depends_on "dep" => :build
   depends_on "go" => :build
@@ -17,10 +17,12 @@ class Certgraph < Formula
       system "make", "dep"
       system "make"
       bin.install "certgraph"
+      prefix.install_metafiles
     end
   end
 
   test do
-    system "#{bin}/certgraph", "-version"
+    cmd = "#{bin}/certgraph -depth 0 example.com"
+    assert_equal "example.com", shell_output(cmd).strip
   end
 end
