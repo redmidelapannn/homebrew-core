70118deafe81169a0f0d36e0e5c1b2f43f8a779f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-28 21:04:49 +0000

    libofx: update 0.9.13 bottle.

--- Formula/libofx.rb
+++ Formula/libofx.rb
@@ -5,10 +5,11 @@ class Libofx < Formula
   sha256 "57eaf97cddbaf82c24f26b8f5cf8b2fbfd4969c74500a2c9acc9082b83bcc0e4"
 
   bottle do
-    sha256 "8474a3f5f575c9950791274b365452ca0cc6e36b11134373b7918616ae76b958" => :mojave
-    sha256 "76ec57f4127bd219529ab60bff89e86e8779479424f69e2f485aba05cf4ca63c" => :high_sierra
-    sha256 "6a59abeb58d3b9f5659fa0e6544540a50fb6521c7b149e4aa2f677d91d5093e8" => :sierra
-    sha256 "c59e5f4445b49e59cc869aa9c01c0dbcb6d8cc5eb7822c3b1c2500f10fd4dacc" => :el_capitan
+    rebuild 1
+    sha256 "afd55589d1acf2b93fa139c7035122b7934ca3c312c13c79640240b08ae04d5d" => :mojave
+    sha256 "aa6e514c67895c53e01da6660c9b7a6976502cced51bae161f0d9d6f9d7a9a6b" => :high_sierra
+    sha256 "1fdf11e014eb69bb0b2be2dbb8f6f93ac57f9bdb640da5f186d98015ab2b85f5" => :sierra
+    sha256 "ac483a60c6b80762107876a5039a9d0e693092d738a3c8e1489702fcdd815798" => :el_capitan
   end
 
   depends_on "open-sp"
