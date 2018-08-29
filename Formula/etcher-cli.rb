bef21e288592fead481fb023a675396ad54fce1f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-29 19:03:46 +0000

    etcher-cli: add 1.4.4 bottle.

--- Formula/etcher-cli.rb
+++ Formula/etcher-cli.rb
@@ -6,6 +6,12 @@ class EtcherCli < Formula
   url "https://github.com/resin-io/etcher/archive/v1.4.4.tar.gz"
   sha256 "02082bc1caac746e1cdcd95c2892c9b41ff8d45a672b52f8467548cad4850f5d"
 
+  bottle do
+    sha256 "90488f77093e9b43c0e57ae0a18b35fa08e4bec51aba67d2ae0e600771bb9b1c" => :high_sierra
+    sha256 "b78d1372e60919891d7349e16a6217af3fc988cc2bde824e2716c47cd122442a" => :sierra
+    sha256 "d803153d15c61ee9eb54d2d7093b4a5e9ca0faa66382f0006ee354926477fd8b" => :el_capitan
+  end
+
   depends_on "node@6" => :build # must be abi compatiable to the node version used with pkg
   depends_on "python" => :build
 
