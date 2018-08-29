f18b1214042501f0a779958ccb41a600a09a754c
Kotaro Yamazaki <s1411420@u.tsukuba.ac.jp>
2018-08-29 16:54:07 +0900

    modify desc

--- Formula/cassandra-reaper.rb
+++ Formula/cassandra-reaper.rb
@@ -1,5 +1,5 @@
 class CassandraReaper < Formula
-  desc "Manager for Cassandra"
+  desc "A management interface for Cassandra"
   homepage "http://cassandra-reaper.io"
   url "https://github.com/thelastpickle/cassandra-reaper/releases/download/1.2.2/cassandra-reaper-1.2.2-release.tar.gz"
   sha256 "720aff69e3205301bc07399afc46dae3568d8effffa3712f1852a169ce9801db"
