cc1aaf228fac624af0635e5e79774559dafd516a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-29 13:28:14 +0000

    badtouch: update 0.6.1 bottle.

--- Formula/badtouch.rb
+++ Formula/badtouch.rb
@@ -5,10 +5,11 @@ class Badtouch < Formula
   sha256 "62181ac05a68a552e1984dd42206f6a5ca195e51addc48cbfdf55a60afc7c3ae"
 
   bottle do
-    sha256 "0adb1b9292c93963822e090e4860c6deadbdd2cc7da9d0e49639fa4a181cb425" => :mojave
-    sha256 "4965a902bbe8a22452039b145d5937f785fdac750674dc3c44ada3ca822c4aa4" => :high_sierra
-    sha256 "0d24c83275c2988cb09befda551377ee9b803fffdfca094bb4649dbdc2624899" => :sierra
-    sha256 "f579fb45cbfd1ee3908f5afd25c91c017d68ab380a8d2d36dc125fcd5031b293" => :el_capitan
+    rebuild 1
+    sha256 "310dac17430016c0ce60e9e0abdca4fd18b2a913af6f6f37726e999ae6fd1d60" => :mojave
+    sha256 "b3062463bb281299bd13b6706e6dbb7eaf6934a31aa3d93b04a099a97304d1a6" => :high_sierra
+    sha256 "47bfe9dfa0a2fc1b2979af8d4def7682e27fa4d3bf26530441557f14543f033e" => :sierra
+    sha256 "6673e8b7d42b910d24847ea2b814d81cc415fd487b60c310bbee8995fe33f9ff" => :el_capitan
   end
 
   depends_on "rust" => :build
