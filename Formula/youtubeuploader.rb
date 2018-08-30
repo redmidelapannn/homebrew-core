c5e6fe9c50b3142c3027adf509fe53649de03947
sidneys <sidneys.github.io@outlook.com>
2018-08-30 18:49:49 +0200

    youtubeuploader: fix formatting

--- Formula/youtubeuploader.rb
+++ Formula/youtubeuploader.rb
@@ -3,10 +3,9 @@ class Youtubeuploader < Formula
   homepage "https://github.com/porjo/youtubeuploader"
   url "https://github.com/porjo/youtubeuploader/archive/18.12.tar.gz"
   sha256 "c76fa3c1a021d38a1faf6ba46479b11888bdc892bff1a7e8902a8b4c2dc5d875"
-  head "https://github.com/porjo/youtubeuploader.git"
 
-  depends_on "go" => :build
   depends_on "dep" => :build
+  depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = buildpath
