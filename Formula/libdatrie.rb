2cf747ea63718bf23f67371c3412ad11888a1af1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-08-31 03:47:43 +0000

    libdatrie: add 0.2.12 bottle.

--- Formula/libdatrie.rb
+++ Formula/libdatrie.rb
@@ -4,6 +4,14 @@ class Libdatrie < Formula
   url "https://github.com/tlwg/libdatrie/releases/download/v0.2.12/libdatrie-0.2.12.tar.xz"
   sha256 "452dcc4d3a96c01f80f7c291b42be11863cd1554ff78b93e110becce6e00b149"
 
+  bottle do
+    cellar :any
+    sha256 "55ec3287b4d4ae186f29f481d125df648eb7d11180676c18fa865f31d5fd62fa" => :mojave
+    sha256 "7f2cc96620d013833a25574fb122a5f6a61d509afb6f4e4594a4b85620b0629b" => :high_sierra
+    sha256 "174421980f65077fc93b7dd506c8751ea12b2aacb39ec608f291257876290ca5" => :sierra
+    sha256 "44a7411495a05da3a2e52539fdb14976d1c9d04c0085eb34d014e06c8fc0d54d" => :el_capitan
+  end
+
   depends_on "autoconf" => :build
 
   def install
