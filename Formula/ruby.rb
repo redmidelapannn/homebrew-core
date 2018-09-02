0cac5c1c95b5b7d54552898858d5255a3f999c27
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-02 01:34:32 +0000

    ruby: update 2.5.1 bottle.

--- Formula/ruby.rb
+++ Formula/ruby.rb
@@ -5,9 +5,11 @@ class Ruby < Formula
   sha256 "886ac5eed41e3b5fc699be837b0087a6a5a3d10f464087560d2d21b3e71b754d"
 
   bottle do
-    sha256 "8e0435a0c5b066866799464e6603ceb2afe20f5dc92ed98e1057dd5a319f5cc2" => :high_sierra
-    sha256 "3bfecdbe9caf5cf8e13bfccdfb2cf1ad1f8d1ce6efb62152d73717ac63479eaf" => :sierra
-    sha256 "91f06a229b8f8a89cdb3870517d1f5d22a4ba915e43bc7c2b20d3224778492cd" => :el_capitan
+    rebuild 1
+    sha256 "3e46f6136d0c80c54e19a10b3720030b2fea67fd3c2744dde6fa5c368bfb3a0d" => :mojave
+    sha256 "88829cd00de05d51254d915db58b49c9e01c5499a556df2aa9ef2871d89df05d" => :high_sierra
+    sha256 "1d4e95416a40425532a478c172dad807e827981217f02804fdec4a68f5be78de" => :sierra
+    sha256 "3f770d9227e6d1c09b3cd07dd3fc4964520c2298ed20d68634d5b653c8670356" => :el_capitan
   end
 
   devel do
