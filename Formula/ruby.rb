7ee3e37253aed1e44d4530a8bfed6c77c60d0a98
Chongyu Zhu <i@lembacon.com>
2018-09-02 22:51:12 +0800

    ruby: update HEAD

--- Formula/ruby.rb
+++ Formula/ruby.rb
@@ -17,7 +17,7 @@ class Ruby < Formula
   end
 
   head do
-    url "https://svn.ruby-lang.org/repos/ruby/trunk/"
+    url "https://github.com/ruby/ruby.git"
     depends_on "autoconf" => :build
   end
 
