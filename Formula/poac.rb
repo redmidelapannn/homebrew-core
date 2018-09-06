ecbb387fd956066aa8d18d5c2625ad8725b6a19d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-06 21:11:54 +0000

    poac: add 0.0.1 bottle.

--- Formula/poac.rb
+++ Formula/poac.rb
@@ -5,6 +5,12 @@ class Poac < Formula
       :tag => "0.0.1-beta",
       :revision => "e0edca899587f4cc5ac82874fff2ad66f75e342e"
 
+  bottle do
+    cellar :any
+    sha256 "31c160b4ac4906c40067ae575bfbbbfacf9f72f5159536bb65ea3d2750275e7f" => :mojave
+    sha256 "3665c9f9c9e7889b99d9b9e156c13efc4dbab3e0e4df64261110f42f3c6afcf5" => :high_sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "openssl"
