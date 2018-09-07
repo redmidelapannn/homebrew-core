2a948100c42d616826c15b780dc05f0c5825f61d
kotaro <k_yamzaki@kde.cs.tsukuba.ac.jp>
2018-09-07 14:58:05 +0900

    update according to the feedback

--- Formula/cassandra-reaper.rb
+++ Formula/cassandra-reaper.rb
@@ -5,19 +5,18 @@ class CassandraReaper < Formula
   sha256 "720aff69e3205301bc07399afc46dae3568d8effffa3712f1852a169ce9801db"
 
   def install
-    prefix.install "bin/cassandra-reaper"
-    mv "server/target", "cassandra-reaper"
-    share.install "cassandra-reaper"
+    inreplace "bin/cassandra-reaper", "etc", etc
+    libexec.install "bin/cassandra-reaper"
+    share.install "server/target" => "cassandra-reaper"
     etc.install "resource" => "cassandra-reaper"
-    inreplace prefix/"cassandra-reaper", "etc", etc
-    env={ :CLASS_PATH => "#{pkgshare}/cassandra-reaper-?.?.?.jar" }
-    (bin/"cassandra-reaper").write_env_script(prefix/"cassandra-reaper", env)
+    env = { :CLASS_PATH => "#{pkgshare}/cassandra-reaper-?.?.?.jar" }
+    (bin/"cassandra-reaper").write_env_script(libexec/"cassandra-reaper", env)
   end
 
   test do
     begin
       pid = fork do
-        exec "/usr/local/bin/cassandra-reaper"
+        exec "#{bin}/cassandra-reaper"
       end
       sleep 10
       output = shell_output("curl -Im3 -o- http://localhost:8080/webui/")
