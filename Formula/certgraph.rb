1219df083c714c16ff5059d02fe25080f05623c9
Samuel Bétrisey <samuel@sam.ninja>
2018-09-12 21:27:23 +0200

    certgraph 20180911 (new formula)

--- Formula/certgraph.rb
+++ Formula/certgraph.rb
@@ -2,23 +2,14 @@ class Certgraph < Formula
   desc "Intelligence tool to crawl the graph of certificate Alternate Names"
   homepage "https://lanrat.github.io/certgraph"
   url "https://github.com/lanrat/certgraph.git",
-      :tag => "20180217",
-      :revision => "41927387b1544a0d668c57b9883ed18381494ec4"
+      :tag => "20180911",
+      :revision => "98e0ef1f86b117a84a34c92a6b9197fa7b1d9a58"
 
-  depends_on "dep" => :build
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    dir = buildpath/"src/github.com/lanrat/certgraph"
-    dir.install Dir["*"]
-
-    cd dir do
-      system "make", "dep"
-      system "make"
-      bin.install "certgraph"
-      prefix.install_metafiles
-    end
+    system "make"
+    bin.install "certgraph"
   end
 
   test do
