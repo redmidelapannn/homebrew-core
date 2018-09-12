bc2ffba654d9e59cb96c0223126b3d14b51207b5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-12 10:25:55 +0000

    teem: update 1.11.0 bottle.

--- Formula/teem.rb
+++ Formula/teem.rb
@@ -6,13 +6,8 @@ class Teem < Formula
   head "http://svn.code.sf.net/p/teem/code/teem/trunk"
 
   bottle do
-    sha256 "439d02dd7f54d7f307b5984d00448a4e77309660e8f1c52e998ef9ea40fdcaa1" => :mojave
-    sha256 "4cb2692b42e79880161879605c3990cd5d0c4fbb171c7ccd003bb9d6bb0fee09" => :high_sierra
-    sha256 "31d19cd9e0e4c064fb743c41a286736503e61b1d5e4b81f29140fcebf2cde2c8" => :sierra
-    sha256 "5ade8dc18d0c66ac154d802df6c64e88222781b6fc427a841fb1f4047f8c4e49" => :el_capitan
-    sha256 "3974a9a565044cb4de798eb1bec2b8980eef03eb6bd7ec6c98cddd606f7c8a29" => :yosemite
-    sha256 "c340d18c157b81be663636ff72326ecb946313ea1dfc533a6ba95b9efdb6bf44" => :mavericks
-    sha256 "96733ab04a4a3a7feb5db5c95f58b0a0c1ef418b91988d1000898a46c142a3ec" => :mountain_lion
+    rebuild 1
+    sha256 "7e7a50d62ab7c44d10d6449d02427f1f067e79be14934c698d94fb95fcea0122" => :mojave
   end
 
   depends_on "cmake" => :build
