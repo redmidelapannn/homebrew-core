bf127ad633eba646edbe9be0a6049657fbe07c91
Song Li <boltomli@users.noreply.github.com>
2018-09-12 18:40:29 +0800

    Resolve a couple of comments
    <https://github.com/Homebrew/homebrew-core/pull/32011>

--- Formula/picat.rb
+++ Formula/picat.rb
@@ -4,17 +4,14 @@ class Picat < Formula
   url "http://picat-lang.org/download/picat24_src.tar.gz"
   version "2.4.8"
   sha256 "72b452a8ba94d6187d837dcdb46aab0d7dc724651bac99a8cf2ada5c0a3543dd"
-  depends_on "gcc" => :build
+  depends_on "gcc"
 
   def install
     inreplace "emu/Makefile.picat.mac64", "/usr/local/bin/gcc", HOMEBREW_PREFIX/"bin/gcc-8"
     system "make", "-C", "emu", "-f", "Makefile.picat.mac64"
     mv "lib", "pi_lib"
     prefix.install Dir["doc", "emu", "exs", "pi_lib"]
-  end
-
-  def post_install
-    ln_sf prefix/"emu/picat_macx", HOMEBREW_PREFIX/"bin/picat"
+    bin.install_symlink prefix/"emu/picat_macx" => "picat"
   end
 
   test do
