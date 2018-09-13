c7d3e09560a9237dc13b80467b72a663af1c7894
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-13 15:57:47 +0000

    assimp: update 4.1.0 bottle.

--- Formula/assimp.rb
+++ Formula/assimp.rb
@@ -7,10 +7,11 @@ class Assimp < Formula
 
   bottle do
     cellar :any
-    sha256 "97457d0943085b11a105b95a4799c73f624db843b9de7e0f81515add33539f6e" => :mojave
-    sha256 "2a3c4f77532717d3cd6b8de75a4cdb033b26fc4d64736f17e90d836e11b90fe4" => :high_sierra
-    sha256 "632ab4d0bd3f3aaa002945ace7e90362b396154ef3c4536b884872f82f3dd30d" => :sierra
-    sha256 "5991caf1877f8193889d0929399aa24790e8eeab96736c3c1d28800966d75169" => :el_capitan
+    rebuild 1
+    sha256 "6f40b398996b3a54e96e7f3043d67259783272c7c7758ef0cd3707cd444ed203" => :mojave
+    sha256 "3dddf9c8cf4d9bf00873d344abe9bea1428a3ac9e4c4f55a5fa920a398f3b93d" => :high_sierra
+    sha256 "fb93a6e9e13c5084bd51e4984c3e5400f6176ef750c23d8ec04e2996ed3c95a2" => :sierra
+    sha256 "41c9c0d164cb610180285e825e531836a1fad2e60e432127ecdf70bd0f434df5" => :el_capitan
   end
 
   depends_on "boost" => :build
