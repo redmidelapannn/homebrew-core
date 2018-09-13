1680fcfa80b2136f848c88fb6f0d1365b3505438
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-13 15:57:59 +0000

    jp2a: update 1.0.6_1 bottle.

--- Formula/jp2a.rb
+++ Formula/jp2a.rb
@@ -7,11 +7,11 @@ class Jp2a < Formula
 
   bottle do
     cellar :any
-    sha256 "12a78b015bc8204f0c8375f06bf45fb71120d3b7b51ef9e695807e65b313143f" => :mojave
-    sha256 "c68f18d28eb4799c5aa6a74c5f2c98a21b9c0968ed2cd9af6b4ea16a7f206fa6" => :high_sierra
-    sha256 "2d5c4358454d3fa29f6bf4ebb9f43992f7fd0fd68f9161476ae2cd79f51a652a" => :sierra
-    sha256 "ff1455c705893442d357efb79b275c6337b24a9e11c959e38f06df5cde4367ae" => :el_capitan
-    sha256 "2a175fef16afed3e74c834ff250f0278eedc60c4deb0296e62f073954605d97d" => :yosemite
+    rebuild 1
+    sha256 "066b881ae84ce86c6fc146d6e285a49fef8dd54746cd1fcdce4ca6c23ddbc7e3" => :mojave
+    sha256 "4f7e887d3b7bfe64a9becee7476e218f03c7fb075a3c0d39c123601c41982ebb" => :high_sierra
+    sha256 "7a9197d80c279dd1c5e534d0c17b41d5ce2dc8558ccb8ef750b741eb0435634d" => :sierra
+    sha256 "383c2532b9be2b332dde0f29d88b738c3fef29946afc4cbd91e4c82b9eeba077" => :el_capitan
   end
 
   depends_on "jpeg"
