4dee635a7f694b74a93ec76a2042311c90268cab
FX Coudert <fxcoudert@gmail.com>
2018-09-13 12:07:25 +0200

    opensc: remove options

--- Formula/opensc.rb
+++ Formula/opensc.rb
@@ -12,27 +12,22 @@ class Opensc < Formula
     sha256 "8be12043d847a94cd56524734f99e49f5bfa884d84ea3c70fa9347d30b8a7ea1" => :el_capitan
   end
 
-  option "without-man-pages", "Skip building manual pages"
-
   depends_on "autoconf" => :build
   depends_on "automake" => :build
+  depends_on "docbook-xsl" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
-  depends_on "docbook-xsl" => :build if build.with? "man-pages"
   depends_on "openssl"
 
   def install
     args = %W[
       --disable-dependency-tracking
       --prefix=#{prefix}
-      --enable-sm
       --enable-openssl
       --enable-pcsc
+      --enable-sm
+      --with-xsl-stylesheetsdir=#{Formula["docbook-xsl"].opt_prefix}/docbook-xsl
     ]
-
-    if build.with? "man-pages"
-      args << "--with-xsl-stylesheetsdir=#{Formula["docbook-xsl"].opt_prefix}/docbook-xsl"
-    end
 
     system "./bootstrap"
     system "./configure", *args
