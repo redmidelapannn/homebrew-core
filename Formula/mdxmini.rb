a55bd1105164152ec0ded63d23b12e02ba9f56e6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-14 16:24:26 +0000

    mdxmini: update 1.0.0 bottle.

--- Formula/mdxmini.rb
+++ Formula/mdxmini.rb
@@ -6,13 +6,11 @@ class Mdxmini < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "e93281dc0c64642e33763f0dc2a4cfa0a6da0dd4739222b0411e54913435ee27" => :mojave
-    sha256 "5bf36e82084146ab4604b4746bcf6634cfe4268f2044712e4d13519b21ab5165" => :high_sierra
-    sha256 "8e0daf3d508dad59074c567b8c8e60bd88c8026b7dfe1305e4e9c50ec5d8fbbd" => :sierra
-    sha256 "d20b94107c25833096401be6336544f283e6956758d4238e207e6a4e34fa5fdf" => :el_capitan
-    sha256 "55cf6b84d9a0e649e25db7626db954a47bf1061afd20a959972470b6e5cc9fa2" => :yosemite
-    sha256 "d08a617e3a8791b9e5dc93426f3d471408550a4a0bab85e33a726ccdcdcb683c" => :mavericks
+    rebuild 2
+    sha256 "a91b19016e672ccbf70c6b48883a7e5b355217ad6a8a70f4b1c5dcd500ecf256" => :mojave
+    sha256 "704eede8b099b489b29ef01f4240b9d5dd178e1d1cdc1d0e977f31678bbfd112" => :high_sierra
+    sha256 "5a7128481bfe24c5998371cc52c88b9545d38772603ce94afbe27ebab24a7280" => :sierra
+    sha256 "6bdd4f06f0940746c6958cd118e87f7418d7042d2ada0fa3ae2c8bb61938e8af" => :el_capitan
   end
 
   depends_on "sdl"
