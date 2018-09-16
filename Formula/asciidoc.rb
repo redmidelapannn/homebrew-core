96bdfe1042961a9f23cf7fff4e1dfdd9578b9329
Yurii Kolesnykov <root@yurikoles.com>
2018-09-17 00:42:46 +0300

    asciidoc: add python2 dependecy for head

--- Formula/asciidoc.rb
+++ Formula/asciidoc.rb
@@ -5,6 +5,7 @@ class Asciidoc < Formula
   url "https://github.com/asciidoc/asciidoc/archive/8.6.10.tar.gz"
   sha256 "9e52f8578d891beaef25730a92a6e723596ddbd07bfe0d2a56486fcf63a0b983"
   revision 1
+
   head "https://github.com/asciidoc/asciidoc.git"
 
   bottle do
@@ -20,6 +21,7 @@ class Asciidoc < Formula
   depends_on "docbook"
 
   def install
+    ENV.prepend_path "PATH", "/System/Library/Frameworks/Python.framework/Versions/2.7/bin"
     ENV["XML_CATALOG_FILES"] = etc/"xml/catalog"
 
     system "autoconf"
