ec4e07485c2478f2e819a1ec8fb25e6767e9d615
Paul Bunkham <paul@dobit.co.uk>
2018-09-18 16:57:55 +0100

    Unison@2.48
    Some servers haven't been updated to 2.51 and there are problems if the
    server and client versions don't match. This is a versioned formula to
    allow 2.48 to be installed.

---
+++ Formula/unison@2.48.rb
@@ -0,0 +1,23 @@
+class UnisonAT248 < Formula
+  desc "File synchronization tool for OSX"
+  homepage "https://www.cis.upenn.edu/~bcpierce/unison/"
+  url "https://github.com/bcpierce00/unison/archive/v2.48.15v4.tar.gz"
+  sha256 "f8c7e982634bbe1ed6510fe5b36b6c5c55c06caefddafdd9edc08812305fdeec"
+
+  keg_only :versioned_formula
+
+  depends_on "ocaml" => :build
+
+  def install
+    ENV.deparallelize
+    ENV.delete "CFLAGS" # ocamlopt reads CFLAGS but doesn't understand common options
+    ENV.delete "NAME" # https://github.com/Homebrew/homebrew/issues/28642
+    system "make", "UISTYLE=text"
+    bin.install "src/unison"
+    prefix.install_metafiles "src"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/unison -version")
+  end
+end
