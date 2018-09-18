737da9af07f19aa30bb463a09888e43f3368751d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-18 13:13:57 +0000

    corkscrew-rs: add 0.1.0 bottle.

--- Formula/corkscrew-rs.rb
+++ Formula/corkscrew-rs.rb
@@ -4,6 +4,14 @@ class CorkscrewRs < Formula
   url "https://github.com/rustch/corkscrew-rs/releases/download/0.1.0/corkscrew-rs.apple_x86_64.tar.bz2"
   version "0.1.0"
   sha256 "47ad58bf7dc4e521223865b6db83f0de77be2dcc0ffc2801a1e0273edc31085e"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "321a0ad8e2b12d09b20612184279ed00ba228733d4bb6620227498d839f169a6" => :mojave
+    sha256 "2b757d80ab39d77a2e1ffc05ebb6eb8a48666b4dbd4d863e590560b7f551ddc4" => :high_sierra
+    sha256 "2b757d80ab39d77a2e1ffc05ebb6eb8a48666b4dbd4d863e590560b7f551ddc4" => :sierra
+    sha256 "2b757d80ab39d77a2e1ffc05ebb6eb8a48666b4dbd4d863e590560b7f551ddc4" => :el_capitan
+  end
+
   def install
     bin.install "corkscrew-rs"
   end
