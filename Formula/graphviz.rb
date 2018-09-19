adb4f2b3b68698e34ddb0a051c193a54f7527593
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-19 23:19:03 +0000

    graphviz: update 2.40.1 bottle.

--- Formula/graphviz.rb
+++ Formula/graphviz.rb
@@ -8,12 +8,11 @@ class Graphviz < Formula
   version_scheme 1
 
   bottle do
-    rebuild 1
-    sha256 "668c64749620f556cf7d26ac96088005b4439acd9488be4c640fdc9cfe66d563" => :mojave
-    sha256 "b592ce51c2a929c3da82e96ec856571ebfc54cf4dac90c2924cd3845078d7082" => :high_sierra
-    sha256 "41b5811054f03978db12525919540fe41e073fb2c20e899247ed9c2a191f7a66" => :sierra
-    sha256 "cab27f92a59d543e2f2c1494c28c7563a4c2d7e0dce4c4fbc22587db91cafc5b" => :el_capitan
-    sha256 "6bd4c01e724cfc965871e1aad9a4fb2a6afef90a1e254d81e2fe33a997f50aaa" => :yosemite
+    rebuild 2
+    sha256 "e1c9760375fdf1e60c88ca5ba2e2e71313e056940dceb2480b0b94d4156d2286" => :mojave
+    sha256 "2e7818b66eb8b382d8e23c6d1bd5db88af342e2475429d5f943288f7d42ceee5" => :high_sierra
+    sha256 "b0965d65e766455fd8bb4c093ff8c875ebc2544611e5ef06f7e1b65282ab3d8f" => :sierra
+    sha256 "885307a0cd152e25c45c6e7920ccbfcbe7bca58cb23b6e8ea99bf3d25fd6a7f2" => :el_capitan
   end
 
   head do
