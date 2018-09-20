8e66578bbed3dafc90538d980d3b0eccc44c6c48
Nathan Kent <nkent2@cs.rochester.edu>
2018-09-19 23:18:52 -0400

    Remove options fron nng formula

--- Formula/nng.rb
+++ Formula/nng.rb
@@ -3,23 +3,14 @@ class Nng < Formula
   homepage "https://nanomsg.github.io/nng/"
   url "https://github.com/nanomsg/nng/archive/v1.0.1.tar.gz"
   sha256 "c08ef670d472eb6fd50a2f863c6a4432b2963addd47f35d54cfb9fd7c543895b"
-  head "https://github.com/nanomsg/nng.git"
-
-  option "with-docs", "Include man pages in install"
-  option "with-test", "Run tests after building"
-  option "with-shared-library", "Build as a shared library instead of static"
 
   depends_on "asciidoctor" => :build
   depends_on "cmake" => :build
   depends_on "ninja" => :build
 
   def install
-    args = std_cmake_args + ["-GNinja"]
-    args << "-DNNG_ENABLE_DOC=ON" if build.with? "docs"
-    args << "-DBUILD_SHARED_LIBS=ON" if build.with? "shared-library"
-
     mkdir "build" do
-      system "cmake", "..", *args
+      system "cmake", "..", "-GNinja", "-DNNG_ENABLE_DOC=ON", "-DBUILD_SHARED_LIBS=ON", *std_cmake_args
       system "ninja"
       system "ninja", "test" if build.with? "test"
       system "ninja", "install"
