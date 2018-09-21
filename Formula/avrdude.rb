6b7c502a522b6a7af6913c490b2d1c3e6944d4b2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-21 13:55:46 +0000

    avrdude: update 6.3 bottle.

--- Formula/avrdude.rb
+++ Formula/avrdude.rb
@@ -6,12 +6,8 @@ class Avrdude < Formula
   sha256 "0f9f731b6394ca7795b88359689a7fa1fba818c6e1d962513eb28da670e0a196"
 
   bottle do
-    sha256 "65f04843495b83a0840c7d1eceae4c1604761083dc9568f267a864881f2c76ad" => :mojave
-    sha256 "cf4744484312894ba0feb9380fae6bf13967655db74bd24c52ccc30c7eacd26c" => :high_sierra
-    sha256 "45c59c4d7821476baa9354c5d57254ee2e7a589f4928f4ecc68f55c93dd648c6" => :sierra
-    sha256 "4e5318b6c2022a2c33f2cd7528c02a23599097227a390855b8ad5f0680c85dd3" => :el_capitan
-    sha256 "5abc4587ae12d475dd95a1b94e1c3045c4c4a64037d7be3dc935ff3a41d0285c" => :yosemite
-    sha256 "17916229c901c02efd79616a053fd6910a1e6c07c90027d868b43c5b536f86d5" => :mavericks
+    rebuild 1
+    sha256 "0fc0eb38ee4ed88b2c651b35bf728bcf1bae9deacba07f9ba013ac79c422c7e1" => :mojave
   end
 
   head do
