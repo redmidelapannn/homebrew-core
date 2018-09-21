be68d19428d15b0acdb1ddfdfc44654708d9ad26
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-21 19:33:12 +0000

    wdiff: update 1.2.2 bottle.

--- Formula/wdiff.rb
+++ Formula/wdiff.rb
@@ -7,13 +7,8 @@ class Wdiff < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "6efa01d5b01a7b3c1dfe4803c310e0cb1619fa93432ba0b34ee33ea310cb04b9" => :mojave
-    sha256 "1c0d329ea782f64396dcef2420b9c90517dd8e8203fbddc924c37a5211e34a4d" => :high_sierra
-    sha256 "aaeab2c70d214deb69451922276ea6d5450b78784cdea803c9cd1220a47998ed" => :sierra
-    sha256 "2e3e40ebdb98e11d783fd5e8e9f5c7c553ae06c739b47a4cf3aa3c4c9483cdf2" => :el_capitan
-    sha256 "1e34ac95a5aa21146f93c5bd0d7d1b22c48941101dc684d019d6d9700da90e8f" => :yosemite
-    sha256 "6cf8260aaa5f0da951bf405f3ed05e1660f8ca7d585c11324319b0c1e6371d56" => :mavericks
-    sha256 "06da8b4a640ef51d0dd884b436d3909c4bd2c5c00ea5da9e81158554a00f0dbe" => :mountain_lion
+    rebuild 1
+    sha256 "ab6e0fa8fb7b69667ea7d6976925b717d3ba110110293e395913144ac95330ab" => :mojave
   end
 
   depends_on "gettext" => :optional
