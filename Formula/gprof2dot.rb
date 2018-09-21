c50d6be59dd3fc26f60db62d30e58e7251a24585
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-21 19:33:10 +0000

    gprof2dot: update 2017.9.19 bottle.

--- Formula/gprof2dot.rb
+++ Formula/gprof2dot.rb
@@ -7,10 +7,11 @@ class Gprof2dot < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "3141494b024f2303016fc172921c489551acf416074c31d5b94908aa2bb133da" => :mojave
-    sha256 "b557363c9161ff1cd9de06cfb116eac29886c31f655c57d031ef2c6641fe876c" => :high_sierra
-    sha256 "4cbf98d1320170ed6287d0b58bc7c810e29529de2ea5d5c4c2545443c6f5e3e2" => :sierra
-    sha256 "2fe7080ddad320a9f73638f8d1ad072e97d6ba80825221414bae015019672852" => :el_capitan
+    rebuild 1
+    sha256 "529f36eb2fd92cf1c3f3ac46252f46caba75f89cf089a0c2522b63fe3066097b" => :mojave
+    sha256 "c6a27491ebff94b12a9639b8ee4d2310a6adfd5bb2dcde10cd7044b8b3b11433" => :high_sierra
+    sha256 "c6a27491ebff94b12a9639b8ee4d2310a6adfd5bb2dcde10cd7044b8b3b11433" => :sierra
+    sha256 "c6a27491ebff94b12a9639b8ee4d2310a6adfd5bb2dcde10cd7044b8b3b11433" => :el_capitan
   end
 
   depends_on "graphviz"
