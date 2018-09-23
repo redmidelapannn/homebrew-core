5c7e127a9765b26860a8fcdc490830b9355af372
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-23 00:54:31 +0000

    tor: update 0.3.4.8 bottle.

--- Formula/tor.rb
+++ Formula/tor.rb
@@ -6,10 +6,11 @@ class Tor < Formula
   sha256 "826a4cb2c099a29c7cf91516ffffcfcb5aace7533b8853a8c8bddcfe2bfb1023"
 
   bottle do
-    sha256 "4e0a8a84df687a6d22052011839197c7167ed8a65b7e81c8018cf4a39b2f45a7" => :mojave
-    sha256 "1da5cb32b5f311a0aee51a982af8742d1d222df47b55b57537f846d1a8404fb2" => :high_sierra
-    sha256 "964684408cdad27d79ae6521e72db62c1d2875c92215f996416e6a580f829c1e" => :sierra
-    sha256 "7e7908fd91af63e0bdb5553d1c4b21eed201211baf87239d32ea4ac7e7940459" => :el_capitan
+    rebuild 1
+    sha256 "a6503d0c2a5c1634b30bab48764e91d13634556b48d55e300eb96cb6ce1d2284" => :mojave
+    sha256 "6c4b0031249bc91c7e458c20879968396712d3cb78b5295b045719fe19f18479" => :high_sierra
+    sha256 "4c1fe55f70037d087227f17b0af12eb58c370444ac43ba60ac03e22cdd0dfed9" => :sierra
+    sha256 "e4dcf12f1b14c4267b8779040caabec78e849a6a2ebfb89e3f476fd606563e9b" => :el_capitan
   end
 
   devel do
