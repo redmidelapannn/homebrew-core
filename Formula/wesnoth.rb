e0f3ca5f7a6559cd09c9e16aeca720b075e6a57e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-26 03:19:42 +0000

    wesnoth: update 1.12.6_8 bottle.

--- Formula/wesnoth.rb
+++ Formula/wesnoth.rb
@@ -7,9 +7,9 @@ class Wesnoth < Formula
   head "https://github.com/wesnoth/wesnoth.git"
 
   bottle do
-    sha256 "415c02b6c6ff9664fb11f41ff9a7441356b3fb70eb4efc1d9dd21c3a9de7b370" => :high_sierra
-    sha256 "d1440a43271ccec672400bd6daefae1c448da134c35d4c282dffa038e934ed33" => :sierra
-    sha256 "f4de6ae40204fa43e16e0a970e508d7b9869965b06b1170e3a8b1c935f187c9c" => :el_capitan
+    sha256 "8ff628f68c0d53d741842b3e9420c239d7af1e0bfe2797785c6a7afcdb71239f" => :mojave
+    sha256 "851874f5cf9843fae20448c29565357d6ad791e52ff4eba287856ac4998f1d99" => :high_sierra
+    sha256 "8508cfb53a817c32ec3a0da05fc9c8e0fb8e02f9164a50ecfa98d13959396558" => :sierra
   end
 
   depends_on "gettext" => :build
