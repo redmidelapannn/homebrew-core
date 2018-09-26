869919d5dbe2d3b6643816d66f49a9add2e5d375
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-26 03:19:43 +0000

    source-highlight: update 3.1.8_10 bottle.

--- Formula/source-highlight.rb
+++ Formula/source-highlight.rb
@@ -8,10 +8,9 @@ class SourceHighlight < Formula
   revision 10
 
   bottle do
-    sha256 "ed1d149a9f97855caee3f659c371a1478b68454efdadee059e34597218b982fe" => :mojave
-    sha256 "88e245af7dfafc7cc0419fab616aa7d1ee16e3f47a36eab9e66bed809bbcd079" => :high_sierra
-    sha256 "2889da5f2a4ce82b711fc2903e888e9410a8ae4d93fd26885b8d6b0b6b178505" => :sierra
-    sha256 "85f7a45cd5ebde9700d332e72395a116287579e29ac000c80f8cb3fd6c8f843f" => :el_capitan
+    sha256 "81826982318da13cca56a7001a77aeed1cfe86dfd13cf8f5662bcff48df46e01" => :mojave
+    sha256 "73e5f7f909b45d7d9a188ff0397aebfd60bfc7e50a6d7a7e5baa543bd78452f9" => :high_sierra
+    sha256 "b102851a6a5f10c04445d41c1842641673b4b2f83466af2ba40ce9283a9e2ec2" => :sierra
   end
 
   depends_on "boost"
