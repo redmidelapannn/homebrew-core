d81f1a8fe403eb5c407badd940697b18a68d53ef
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-09-26 03:19:40 +0000

    cclive: update 0.7.16_3 bottle.

--- Formula/cclive.rb
+++ Formula/cclive.rb
@@ -7,11 +7,9 @@ class Cclive < Formula
 
   bottle do
     cellar :any
-    sha256 "8001f6a23cfef448ee8d58eb05b6d1e497520b8b21851d0f2d2859fc09f74f6d" => :mojave
-    sha256 "65a00aa88ea96cff176c5bdfd29d97686548cfaa362aaa99ff2ed9fe10d9c624" => :high_sierra
-    sha256 "a589260223c5b92d0b29551e98e068814311de8ddde82e158063bec6f834aa2d" => :sierra
-    sha256 "e77474769e3fa5b12b81fba7d476a9feeb237a9b6ba9bf9cf275e4783203c67d" => :el_capitan
-    sha256 "933a9b935bb868923702489a97780998c450c2c220a63786542bf65bfb2db155" => :yosemite
+    sha256 "01a6a9d427ddfa8e08451f19872eb341a771346d886d4739490ca03b5e4519fe" => :mojave
+    sha256 "bea921eda3695efea705a472db7851d2ce3b0578e35f0385897eaa1e9e16e72e" => :high_sierra
+    sha256 "8856be145c903d54f1211a3e14b1bb19ad78aedbaf7041658c43cef23578698a" => :sierra
   end
 
   depends_on "pkg-config" => :build
