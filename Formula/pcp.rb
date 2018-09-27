932e8bec41f87fc64f8bf4f62fcb1fea6f5b7324
JARVIS <parth.verma96@gmail.com>
2018-09-27 15:24:52 +0530

    New Formula PCP

--- Formula/pcp.rb
+++ Formula/pcp.rb
@@ -1,8 +1,8 @@
 class Pcp < Formula
   desc "Performance Co-Pilot is a system performance and analysis framework"
   homepage "https://pcp.io/"
-  url "https://dl.bintray.com/pcp/source/pcp-4.1.3.src.tar.gz"
-  sha256 "ca593d65ccaf6f481d55720019e317679afeadd589aa37c878ec1e8629a1e75c"
+  url "https://github.com/performancecopilot/pcp/archive/4.1.3.tar.gz"
+  sha256 "346caea9df3610319b191b11abdd86c3312eaa20f9db1d9b34bbd9e0204f3939"
 
   depends_on "pkg-config" => :build
   depends_on "nss"
@@ -12,7 +12,7 @@ class Pcp < Formula
   depends_on "xz"
 
   def install
-    system "./configure", "--prefix=#{prefix}", "CC=clang", "CXX=clang", "--without-manager"
+    system "./configure", "--prefix=#{prefix}"
     system "make", "install"
   end
 
@@ -26,29 +26,26 @@ class Pcp < Formula
         <key>RunAtLoad</key>         <true/>
         <key>KeepAlive</key>         <true/>
         <key>Label</key>             <string>io.pcp.startup</string>
-
         <key>ProgramArguments</key>
             <array>
               <string>ln</string>
-                <string>-s</string>
-                <string>/usr/local/Cellar/pcp/4.1.1/etc/pcp.env</string>
-                <string>/etc/pcp.env</string>
+              <string>-s</string>
+              <string>/usr/local/Cellar/pcp/4.1.1/etc/pcp.env</string>
+              <string>/etc/pcp.env</string>
             </array>
-
         <key>ProgramArguments</key>
             <array>
               <string>ln</string>
-                <string>-s</string>
-                <string>/usr/local/Cellar/pcp/4.1.1/etc/pcp.conf</string>
-                <string>/etc/pcp.conf</string>
+              <string>-s</string>
+              <string>/usr/local/Cellar/pcp/4.1.1/etc/pcp.conf</string>
+              <string>/etc/pcp.conf</string>
             </array>
-
     </dict>
     </plist>
   EOS
   end
 
   test do
-    system "/usr/local/Cellar/pcp/4.1.1/etc/init.d/pmcd", "start"
+    system "#{etc}/init.d/pmcd", "start"
   end
 end
