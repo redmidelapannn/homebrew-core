3ab39c5b70bf6505b6ad6c292f6ace67cc4ef5a3
Matthias Schmieder <schmieder.matthias@gmail.com>
2018-10-01 18:08:16 +0200

    azure-storage-cpp 5.1.1 (new formula)
    added azure-storage-cpp that enables communication with azure storages. This formula is dependend on newer versions of cpprestsdk > 2.10.5 and therefore will only build successfully if the the merge request for cpprestsdk 2.10.6 is accepted first

---
+++ Formula/azure-storage-cpp.rb
@@ -0,0 +1,20 @@
+class AzureStorageCpp < Formula
+  desc "Microsoft Azure Storage Client Library for C++"
+  homepage "https://azure.github.io/azure-storage-cpp"
+  url "https://github.com/Azure/azure-storage-cpp/archive/v5.1.1.tar.gz"
+  sha256 "29b4b0c47784ae541b0ef1b3693acba54e0d06bac78d139105a379ab1fd20333"
+
+  depends_on "cmake" => :build
+  depends_on "boost"
+  depends_on "cpprestsdk"
+  depends_on "openssl"
+
+  def install
+    system "cmake", "-DBUILD_SAMPLES=OFF", "-DBUILD_TESTS=OFF", "Microsoft.WindowsAzure.Storage", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
