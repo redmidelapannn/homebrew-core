a05173d0fe9f4cd3beacf71019c377bb919dd260
FX Coudert <fxcoudert@gmail.com>
2018-10-01 23:15:23 +0200

    Update cpprestsdk.rb

--- Formula/cpprestsdk.rb
+++ Formula/cpprestsdk.rb
@@ -1,8 +1,8 @@
 class Cpprestsdk < Formula
   desc "C++ libraries for cloud-based client-server communication"
   homepage "https://github.com/Microsoft/cpprestsdk"
-  sha256 "5fecccc779b077f18acf0f7601b19b39c3da963498ed5b10bb2700dccfe66c5a"
   url "https://github.com/Microsoft/cpprestsdk/archive/v2.10.6.tar.gz"
+  sha256 "5fecccc779b077f18acf0f7601b19b39c3da963498ed5b10bb2700dccfe66c5a"
   head "https://github.com/Microsoft/cpprestsdk.git", :branch => "development"
 
   bottle do
