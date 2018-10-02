2e6925657fb273e588878b565e675e62fd85fb29
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-02 20:32:49 +0000

    cityhash: update 1.1.1 bottle.

--- Formula/cityhash.rb
+++ Formula/cityhash.rb
@@ -6,12 +6,10 @@ class Cityhash < Formula
 
   bottle do
     cellar :any
-    sha256 "4d7f25360b715d36177c70f06f7c21f39d38b6b8aa9f8a5befe80818baa3545f" => :mojave
-    sha256 "37e8244399c42c6f3bdb2fad91562607e96bc3380378d318ceecbc16ec8d52be" => :high_sierra
-    sha256 "62d8d1409dfe744d4de7a1727824b06c5a80b248433c2d8bd8a4efcd444346cb" => :sierra
-    sha256 "b09962ca43b3bb3321e1e57bf74a0936142ec5c94e198113ac3aa14e669e4d28" => :el_capitan
-    sha256 "2b155183e2422811593d91b415ac2e90a00b7d6972f284e54b3214940250935e" => :yosemite
-    sha256 "6c361a421b5f59c32c1098d4c29dd0c8f3048cf288c8880e954448926ed26184" => :mavericks
+    rebuild 1
+    sha256 "bbe6898750b18f411cf7e7fa4be367cc10bf3e9bb8506e87d4d5d5ded154137c" => :mojave
+    sha256 "ca91d419f80d02e09b404e574b249361e63b5f5d2441dc8163d67ecd7c82506a" => :high_sierra
+    sha256 "f7e913b2c7a14fd218f44adc9fe220b02de5e892cbb968d19561dafe5f919ad0" => :sierra
   end
 
   def install
