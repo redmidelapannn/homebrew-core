6267b2748de2040e93e0184242b828b60f95844f
John Sockwell <johnsockwell@users.noreply.github.com>
2018-10-02 19:49:01 -0400

    Create formula template for RKE

---
+++ Formula/rke.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Rke < Formula
+  desc "Rancher Kubernetes Engine, an extremely simple, lightning fast Kubernetes installer that works everywhere."
+  homepage ""
+  url "https://github.com/rancher/rke/archive/v0.1.9.tar.gz"
+  sha256 "b7d289e7481f11a0b3ecf5237ccc3e9ef2f791047af5bdc9eb07484c7fd69c5e"
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test rke`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
