d5b8758ecfd3bbf60a56b32b38c1ba72817f1e10
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-02 02:05:59 +0000

    certgraph: add 20180911 bottle.

--- Formula/certgraph.rb
+++ Formula/certgraph.rb
@@ -5,6 +5,13 @@ class Certgraph < Formula
       :tag => "20180911",
       :revision => "98e0ef1f86b117a84a34c92a6b9197fa7b1d9a58"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d22173305e900b60cc3837c9ccc8f0fc24ae6a96cee51fc61a090ea7b43fda62" => :mojave
+    sha256 "8eb2593465a64017260a22b6f0028199a5078b67a8120f64d2be4ca8fd8bf043" => :high_sierra
+    sha256 "a962a0a9805078d7874fea87e715435b6727e353436f0451b67ee1ee12d1295e" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
