ebd1bc197abded7fcef905775a0e1254151ed1f9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-02 01:18:39 +0000

    boost-build@1.68: add 1.68.0 bottle.

--- Formula/boost-build@1.68.rb
+++ Formula/boost-build@1.68.rb
@@ -4,6 +4,13 @@ class BoostBuildAT168 < Formula
   url "https://github.com/boostorg/build/archive/boost-1.68.0.tar.gz"
   sha256 "0b8612f45c43da57f370234f52018d4a6a22e11e0fcd0dff4050a1650d82e294"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "884b3aad0836edf093a5396c4e92a3e4d3d7a519b05946692ee5490bc2c2309b" => :mojave
+    sha256 "09a123c4d54f1025bf3742fcb32522cbe2784b09a5b3c6275db5833edeb434ae" => :high_sierra
+    sha256 "9734f2f06c002865ddc519ba08e3f5941e7b7318260196f25e36473a122a0de9" => :sierra
+  end
+
   keg_only :versioned_formula
 
   def install
