3a268470d583f19713648930389755d4721126c5
Markus Reiter <me@reitermark.us>
2018-10-04 15:56:02 +0200

    rustup-init: add patch for relative symlinks.

--- Formula/rustup-init.rb
+++ Formula/rustup-init.rb
@@ -2,7 +2,8 @@ class RustupInit < Formula
   desc "The Rust toolchain installer"
   homepage "https://github.com/rust-lang-nursery/rustup.rs"
   url "https://github.com/rust-lang-nursery/rustup.rs/archive/1.14.0.tar.gz"
-  sha256 "a19310b180aaa373cf01007ab19836ba6bb1360aebad01471c81698140afbe55"
+  sha256 "ab125d9b12bf0f3f7e7ad98e826035fa1ae3dbe6ba8b78be4c82f9cde00bc59f"
+  revision 1
 
   bottle do
     sha256 "11ccb4a96d2927f6509534e699cc9302e1fba469162fe6c8a236cf54b1d0064c" => :mojave
@@ -12,6 +13,14 @@ class RustupInit < Formula
   end
 
   depends_on "rust" => :build
+
+  # Fixes `rustup-init` not working when it is relative symlink.
+  # https://github.com/rust-lang-nursery/rustup.rs/issues/1512
+  # https://github.com/rust-lang-nursery/rustup.rs/pull/1521
+  patch do
+    url "https://github.com/rust-lang-nursery/rustup.rs/pull/1521/commits/ebbfced7ee4aeb5be4c775f8f85e351093d5818f.diff?full_index=1"
+    sha256 "34dc006d75d747b99dd9fe49bdde396a8bf5443d1e3df1a47c4776c06ec3f3c7"
+  end
 
   def install
     cargo_home = buildpath/"cargo_home"
