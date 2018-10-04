f2d606c31c7e6b45aa97b9fb9d419fcfcac202f1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-04 15:05:42 +0000

    gitlab-time-tracker: add 1.7.27 bottle.

--- Formula/gitlab-time-tracker.rb
+++ Formula/gitlab-time-tracker.rb
@@ -6,6 +6,13 @@ class GitlabTimeTracker < Formula
   url "https://registry.npmjs.org/gitlab-time-tracker/-/gitlab-time-tracker-1.7.27.tgz"
   sha256 "a29b7be0069201688dab67280fa681a1698db80f1f2abab18af962bedf960871"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "43d9f8a886daa613ac86c71417a339e8f714f04591ad8354f55168d7e731167d" => :mojave
+    sha256 "7f4c5a4e8a1dfaaad3eb7a6dc329262699185b8f77d4f3f0bf07c08901a23f4a" => :high_sierra
+    sha256 "33d0bc49737a965bb2c2cd349c096fce8b897fe84f4266937ce3bc89ee5c646a" => :sierra
+  end
+
   depends_on "node"
 
   def install
