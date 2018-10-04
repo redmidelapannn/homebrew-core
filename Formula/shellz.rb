f370ee534046d062f2312664e25efec6ebdcd947
Jérémy CHABERNAUD <djerfy@gmail.com>
2018-10-04 07:17:48 +0200

    Remove bottle block

--- Formula/shellz.rb
+++ Formula/shellz.rb
@@ -4,14 +4,6 @@ class Shellz < Formula
   url "https://github.com/evilsocket/shellz/archive/v1.1.0.tar.gz"
   sha256 "8bec29e04a54adb8b9d161c88422d324e1f3d52d743f4fc0d72ab89ca54f9c1e"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "586429477cb29a4947f2a2dff5727ae818957d330f78a8c28838f0c68fc9745f" => :mojave
-    sha256 "9543e2e0ce043a3a36a27ee17db633a05b1183c4e159eb9ad06338e68e7d5aff" => :high_sierra
-    sha256 "f84673840eebbeacc1e0e17ac6ce6467019373c8615120f5fe990aafdb059fda" => :sierra
-    sha256 "6f0ec386ee44e427b0f95dc12c3f047c47eb6a1072501169df4529aef7f7af60" => :el_capitan
-  end
-
   depends_on "dep" => :build
   depends_on "go" => :build
 
