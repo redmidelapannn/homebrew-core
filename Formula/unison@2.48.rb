1f73b486b12a9c340222aedee2ed829278291998
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-04 12:45:08 +0000

    unison@2.48: add 4 bottle.

--- Formula/unison@2.48.rb
+++ Formula/unison@2.48.rb
@@ -4,6 +4,13 @@ class UnisonAT248 < Formula
   url "https://github.com/bcpierce00/unison/archive/v2.48.15v4.tar.gz"
   sha256 "f8c7e982634bbe1ed6510fe5b36b6c5c55c06caefddafdd9edc08812305fdeec"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e0dcbc445e52c36e8855a246e1e02f3518e988ffb2477782f859734d23ebd2c3" => :mojave
+    sha256 "21e115b208141a0cd00a93c8f9706c548392571cca291108fbf5a6f9ea90473e" => :high_sierra
+    sha256 "8ad3323b5597820faebca4833255470ca73cb212d8747522d4a6826c1ebd9bfe" => :sierra
+  end
+
   keg_only :versioned_formula
 
   depends_on "ocaml" => :build
