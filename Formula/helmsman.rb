9022cb1b9398e4e941e5d89bc6dfffe5bc4db236
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-05 18:07:03 +0000

    helmsman: add 1.6.2 bottle.

--- Formula/helmsman.rb
+++ Formula/helmsman.rb
@@ -4,6 +4,13 @@ class Helmsman < Formula
   url "https://github.com/Praqma/helmsman/archive/v1.6.2.tar.gz"
   sha256 "587ae86423df96fd5f5409f1af1a1ab763274d780c0b3f8651202caf840050dc"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5b9b7c5a095cc52bad65526b0efec885994de99df9bd12313f5fe17a5bad4a9f" => :mojave
+    sha256 "8207af9f47852124e8c8b9fc1de9ce8666f1db30f879e5b5ebc4aa3209cb1785" => :high_sierra
+    sha256 "d5b446e3bd33ddb9f752144985471a5edd8327b494814ec88cc3234555fa4ec9" => :sierra
+  end
+
   depends_on "go" => :build
   depends_on "kubernetes-cli"
   depends_on "kubernetes-helm"
