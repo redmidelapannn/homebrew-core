4b3cbf721ed54506eb088b0c16a84dc55711c8cc
John Sockwell <johnsockwell@users.noreply.github.com>
2018-10-05 10:20:09 -0400

    Add revision to url

--- Formula/rke.rb
+++ Formula/rke.rb
@@ -2,7 +2,8 @@ class Rke < Formula
   desc "Rancher Kubernetes Engine, a Kubernetes installer that works everywhere"
   homepage "https://rancher.com/docs/rke/v0.1.x/en/"
   url "https://github.com/rancher/rke.git",
-    :tag => "v0.1.9"
+    :tag => "v0.1.9",
+    :revision => "bff7e0a2c3b792704020263927fb7d49b55a3017"
 
   depends_on "go" => :build
 
