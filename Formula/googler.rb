8440621f3fa74562941498e6d2dfe76ff068544e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-05 18:58:08 +0000

    googler: update 3.7 bottle.

--- Formula/googler.rb
+++ Formula/googler.rb
@@ -7,10 +7,9 @@ class Googler < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "971af061566b3156a2f69fff34217f2373cee171c90b98ab8b39e78fa3d16f38" => :mojave
-    sha256 "543a26a00407a7677a9f568c37887acf16d2efab36f86df00f17b2c87ed0e7ea" => :high_sierra
-    sha256 "543a26a00407a7677a9f568c37887acf16d2efab36f86df00f17b2c87ed0e7ea" => :sierra
-    sha256 "543a26a00407a7677a9f568c37887acf16d2efab36f86df00f17b2c87ed0e7ea" => :el_capitan
+    sha256 "4825fcfe0c8e5073f34e758c53d3c654e3f1670217acc282536f89ae845a1dc1" => :mojave
+    sha256 "fed450ae4bf96923a8e346cf1a46bbdc5aa23b0e00af05c6fc9d65c0361d9b1b" => :high_sierra
+    sha256 "fed450ae4bf96923a8e346cf1a46bbdc5aa23b0e00af05c6fc9d65c0361d9b1b" => :sierra
   end
 
   depends_on "python"
