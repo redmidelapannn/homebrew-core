149d96e848eba569495e05323bc7cf1ded1abf4c
John Sockwell <johnsockwell@users.noreply.github.com>
2018-10-04 20:09:33 -0400

    Fixup to pass audit
    - Remove template comments
    - Shorten description
    - Remove period from end of description

--- Formula/rke.rb
+++ Formula/rke.rb
@@ -1,8 +1,5 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Rke < Formula
-  desc "Rancher Kubernetes Engine, an extremely simple, lightning fast Kubernetes installer that works everywhere."
+  desc "Rancher Kubernetes Engine, a Kubernetes installer that works everywhere"
   homepage "https://rancher.com/docs/rke/v0.1.x/en/"
   url "https://github.com/rancher/rke.git",
     :tag => "v0.1.9"
