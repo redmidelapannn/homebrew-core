7384e4e74a2c91cb68a79dd7240ca348f4733997
Scott Crooks <scott.crooks@gmail.com>
2018-10-06 18:59:44 +0200

    Changes after `brew audit`

--- Formula/sonobuoy.rb
+++ Formula/sonobuoy.rb
@@ -1,15 +1,8 @@
 class Sonobuoy < Formula
-  desc "Diagnostic tool that makes it easier to understand the state of a Kubernetes cluster by running a set of Kubernetes conformance tests in an accessible and non-destructive manner"
+  desc "Kubernetes component that generates reports on cluster conformance"
   homepage "https://github.com/heptio/sonobuoy"
   url "https://github.com/heptio/sonobuoy/archive/v0.12.0.tar.gz"
   sha256 "068def4fedb8d9a951444a83759a9c3eafef7b968a3d8b1135f63ff982571fdf"
-
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "" => :mojave
-    sha256 "" => :high_sierra
-    sha256 "" => :sierra
-  end
 
   depends_on "go" => :build
 
@@ -20,8 +13,8 @@ class Sonobuoy < Formula
     cd "src/github.com/heptio/sonobuoy" do
       system "go", "build", "-o", bin/"sonobuoy", "-installsuffix", "static",
                    "-ldflags",
-                   "-X github.com/heptio/sonobuoy/pkg/buildinfo.Version=#{version}",
-                   "./cmd/sonobuoy"
+                   "-s -w -X github.com/heptio/sonobuoy/pkg/buildinfo.Version=#{version}",
+                   "./"
       prefix.install_metafiles
     end
   end
@@ -29,7 +22,7 @@ class Sonobuoy < Formula
   test do
     output = shell_output("#{bin}/sonobuoy 2>&1")
     assert_match "Sonobuoy is an introspective kubernetes component that generates reports on cluster conformance", output
-    assert_match "#{version}", shell_output("#{bin}/sonobuoy version 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/sonobuoy version 2>&1")
     output = shell_output("#{bin}/sonobuoy gen 2>&1")
     assert_match "name: heptio-sonobuoy", output
   end
