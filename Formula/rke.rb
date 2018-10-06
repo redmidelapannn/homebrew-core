13e9d82d58a8752abc43acd5ca78a0720bdf0568
commitay <commitay@users.noreply.github.com>
2018-10-06 10:09:36 +1000

    Update rke.rb

--- Formula/rke.rb
+++ Formula/rke.rb
@@ -2,22 +2,25 @@ class Rke < Formula
   desc "Rancher Kubernetes Engine, a Kubernetes installer that works everywhere"
   homepage "https://rancher.com/docs/rke/v0.1.x/en/"
   url "https://github.com/rancher/rke.git",
-    :tag => "v0.1.9",
-    :revision => "bff7e0a2c3b792704020263927fb7d49b55a3017"
+      :tag => "v0.1.9",
+      :revision => "bff7e0a2c3b792704020263927fb7d49b55a3017"
 
   depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/rancher/rke/").install Dir["*"]
-    system "go", "build", "-ldflags",
-           "-w -X main.VERSION=v#{version}",
-           "-o", "#{bin}/rke",
-           "github.com/rancher/rke/"
+    (buildpath/"src/github.com/rancher/rke").install buildpath.children
+
+    cd "src/github.com/rancher/rke" do
+      system "go", "build", "-ldflags",
+             "-w -X main.VERSION=v#{version}",
+             "-o", bin/"rke"
+      prefix.install_metafiles
+    end
   end
 
   test do
-    system "#{bin}/rke", "config", "-e"
-    assert File.file? "#{testpath}/cluster.yml"
+    system bin/"rke", "config", "-e"
+    assert_predicate testpath/"cluster.yml", :exist?
   end
 end
