aeba69652085e24cfc69f743b55fc7c302c56491
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-09 19:35:12 +0000

    liblognorm: add 2.0.5 bottle.

--- Formula/liblognorm.rb
+++ Formula/liblognorm.rb
@@ -4,6 +4,13 @@ class Liblognorm < Formula
   url "http://www.liblognorm.com/files/download/liblognorm-2.0.5.tar.gz"
   sha256 "c8151da83b21031f088bb2a8ea674e4f7ee58551829e985028245841330db190"
 
+  bottle do
+    cellar :any
+    sha256 "0158e0621b417cb3846d3dde61a21bf14ffb2a16cb859b7e2fcb36d1869ec94d" => :mojave
+    sha256 "da4a717f06236524b971cf80aa87ac930e244a7075d0c94b7bee1633346b32f0" => :high_sierra
+    sha256 "c42356e025d647be01346579da4f085c4a317000c6c1bef2d2b48d52566d56e0" => :sierra
+  end
+
   depends_on "pkg-config" => :build
   depends_on "libestr"
   depends_on "libfastjson"
