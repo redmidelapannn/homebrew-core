7b2797ac42a923963a5f6b1b542770516aba6c77
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-09 07:45:21 +0000

    nu: update 2.2.2 bottle.

--- Formula/nu.rb
+++ Formula/nu.rb
@@ -6,11 +6,10 @@ class Nu < Formula
 
   bottle do
     cellar :any
-    sha256 "1fad2a9ee6b36b1c5afb989e9c4f2282fb3e4cdd7476f24febf561f17d4a5ef2" => :mojave
-    sha256 "2b2b032b705ea26fc837bb621ce54a319594112708ce2c960ee2d440723667b3" => :high_sierra
-    sha256 "1508d2c0376f54e1108568f79fa907244877f1d3981bbe6db69c6efcc7460c54" => :sierra
-    sha256 "183c89418f6803a6f1395545739da9012b4e049d160038d6d5c00e242243284a" => :el_capitan
-    sha256 "ba5bd173433144dbf6141cfced1c04f17f81c1cb014ea2f794090e6c5a5f8f4b" => :yosemite
+    rebuild 1
+    sha256 "dd4ad8f32f6dede0a197db5025971e1f49307c69ee4899f8d538f7f9fbb61183" => :mojave
+    sha256 "8c5bc675f15111f4fa99aac6b83c1870988508e7f0d08cbe32ea9d420bdc9229" => :high_sierra
+    sha256 "3edc9761b9cf8627e6843ec7b982130430822da765afe7219759070f5f8cf116" => :sierra
   end
 
   depends_on :macos => :lion
