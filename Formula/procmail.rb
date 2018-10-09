6d55a89e26fdb645a16b9a65c92aebb4d9175d17
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-09 07:45:21 +0000

    procmail: update 14 bottle.

--- Formula/procmail.rb
+++ Formula/procmail.rb
@@ -7,12 +7,10 @@ class Procmail < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "48be3e5215b4ac296ef1f9150b313964112e5c7d04fe20489f336342548656e0" => :mojave
-    sha256 "c64920b1989d941d9aa4de7c275cf2e80306cb8bd2ee5d8263e883ddab7ef2e3" => :high_sierra
-    sha256 "c64ccf998d9c71d1b73004abe4c96a8c35993cf4c1a899cd6d92bfab82b9272a" => :sierra
-    sha256 "3328bcda4649612afba606950e59f4cb0c22e10fe97a4f1e38f190e3e4115800" => :el_capitan
-    sha256 "cd5a5cdfbe9d03067533df0ef3f09cc2c05bd16a9b75d2d19cd9c2d1da2986e5" => :yosemite
-    sha256 "9e476567851a38caedbbb894afb83d3f5575bb494aaab296f884387feca9bf54" => :mavericks
+    rebuild 1
+    sha256 "c8f2a7330c1c9bd444f0a328ae752555248c996b7c47ecf7ddc3bfe7c844a2c4" => :mojave
+    sha256 "60a451948ced32078b37a4b0ffc4a8eb0e7e524ccd81a5f572636e748aeebea4" => :high_sierra
+    sha256 "d1b82d8dfa8b3bb7005f596472ae95ca524127fae18e39a51b196b293a5bebc1" => :sierra
   end
 
   def install
