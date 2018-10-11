98fb4657061712fb55a5c769808ec483aed448d2
Alex Merkel <mail@alexmerkel.com>
2018-10-11 19:04:38 +0200

    sundials 3.2.0

--- Formula/sundials.rb
+++ Formula/sundials.rb
@@ -1,16 +1,8 @@
 class Sundials < Formula
   desc "Nonlinear and differential/algebraic equations solver"
   homepage "https://computation.llnl.gov/casc/sundials/main.html"
-  url "https://computation.llnl.gov/projects/sundials/download/sundials-3.1.2.tar.gz"
-  sha256 "a8985bb1e851d90e24260450667b134bc13d71f5c6effc9e1d7183bd874fe116"
-
-  bottle do
-    cellar :any
-    sha256 "395df599c4b62d2db1cc65c1823b0711f7d4171f2bc41815354b9ec6a476409f" => :mojave
-    sha256 "85d26a0fe45cc223074d986abfcdb70c06363a6cdbf5df8bff1f4f8a2ff7103d" => :high_sierra
-    sha256 "9b679f316ad602b3b5057bb39a81b398f65bdae5fd7f80cb8d8ec3cba08cf7fa" => :sierra
-    sha256 "2213f3ae580feabe0874cf229f39e9d9434cbaefe960f88616496a66a11512be" => :el_capitan
-  end
+  url "https://computation.llnl.gov/projects/sundials/download/sundials-3.2.0.tar.gz"
+  sha256 "d2b690afecadf8b5a048bb27ab341de591d714605b98d3518985dfc2250e93f9"
 
   depends_on "cmake" => :build
   depends_on "gcc" # for gfortran
