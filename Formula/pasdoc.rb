bdfc78b3f91817fee35394511d0b933df6eb9985
Alex Merkel <mail@alexmerkel.com>
2018-10-14 17:36:46 +0200

    pasdoc 0.15.0 (new formula)

---
+++ Formula/pasdoc.rb
@@ -0,0 +1,26 @@
+class Pasdoc < Formula
+  desc "Documentation tool for ObjectPascal (Free Pascal, Lazarus, Delphi) source code"
+  homepage "https://github.com/pasdoc/pasdoc/wiki"
+  url "https://github.com/pasdoc/pasdoc/archive/v0.15.0.tar.gz"
+  sha256 "9863c56a199d12bfc22f2ff06d387831e01f052a59c5ad087717756d9193ed6d"
+  head "https://github.com/pasdoc/pasdoc.git"
+  depends_on "fpc" => :build
+
+  def install
+    system "make", "build-fpc-default"
+    bin.install Dir["bin/*"]
+  end
+
+  test do
+    hello = <<~EOS
+      program Hello;
+      uses GL;
+      begin
+        writeln('Hello Homebrew')
+      end.
+    EOS
+    (testpath/"hello.pas").write(hello)
+    system "#{bin}/pasdoc", "--format=html", "hello.pas"
+    assert_path_exist(testpath/"Hello.html")
+  end
+end
