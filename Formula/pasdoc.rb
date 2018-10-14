e52fe34a2270233f890127ad9bdbd870657ae398
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-14 15:50:07 +0000

    pasdoc: add 0.15.0 bottle.

--- Formula/pasdoc.rb
+++ Formula/pasdoc.rb
@@ -4,6 +4,13 @@ class Pasdoc < Formula
   url "https://github.com/pasdoc/pasdoc/archive/v0.15.0.tar.gz"
   sha256 "9863c56a199d12bfc22f2ff06d387831e01f052a59c5ad087717756d9193ed6d"
   head "https://github.com/pasdoc/pasdoc.git"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1998776d0d9e1d341928d3f3f3812fe0eae4fbdbb12cf0171df89fd7d832e00d" => :mojave
+    sha256 "b75b6e98dad5b23fa0ee6d56a26f4e3f3a7121a613155e47cfd529628433b183" => :high_sierra
+    sha256 "57ecac32f9da8a978e9afe6c9ad54f9e811a7f0de02b01ec8c89c1245c5b68fb" => :sierra
+  end
+
   depends_on "fpc" => :build
 
   def install
