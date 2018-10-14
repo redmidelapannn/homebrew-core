f012c4e86daa8d2a55088af17b50a7f78eaa68e4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-14 06:59:16 +0000

    conjure-up: update 2.6.0 bottle.

--- Formula/conjure-up.rb
+++ Formula/conjure-up.rb
@@ -8,10 +8,10 @@ class ConjureUp < Formula
 
   bottle do
     cellar :any
-    sha256 "f979ba47dc174f8ba0c54fe5aef6788218e54b2c47d3e92895af6fb6f61819b8" => :mojave
-    sha256 "30f18ecddf24005e85b4f155b6fb1acfae01b71bccd669d505d4946985eb99f1" => :high_sierra
-    sha256 "2fac93758693efeedfecf58cda5fff8f2ce2cd6608096d378699792d63e65cc5" => :sierra
-    sha256 "4f06009d8c2cc5ae0e58f8f3fb91a340e46b6964c2462463ad362632c51c982d" => :el_capitan
+    rebuild 1
+    sha256 "ce79f4261dcc3913f99d0acce2fb39f51b88abc89f138131ef92ae4e3d00b94a" => :mojave
+    sha256 "dee78499a17e7d7c98c2df29dd9fbd1ee8e25a32305790f609a4321d51b4e270" => :high_sierra
+    sha256 "988090b33bc5ff513d717204e129ec8b6ed072f2bcd1d53ab4c99df66103394f" => :sierra
   end
 
   depends_on "awscli"
