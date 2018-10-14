ee5d1427971464b714bf72cf418ef374d33de0f0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-14 09:24:41 +0000

    libtorrent-rakshasa: add 0.13.7 bottle.

--- Formula/libtorrent-rakshasa.rb
+++ Formula/libtorrent-rakshasa.rb
@@ -4,6 +4,12 @@ class LibtorrentRakshasa < Formula
   url "https://github.com/rakshasa/rtorrent/releases/download/v0.9.7/libtorrent-0.13.7.tar.gz"
   sha256 "c738f60f4d7b6879cd2745fb4310bf24c9287219c1fd619706a9d5499ca7ecc1"
 
+  bottle do
+    cellar :any
+    sha256 "8a627c2b6228b6f2ac59f7d9f99e0e198f1e671159033f7a9b8ed5f2033fccbf" => :mojave
+    sha256 "eafd1112699f4bb3721df5e2aef7e4f6e1015bbafdf8b51daeb4e33563fac0ea" => :sierra
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
