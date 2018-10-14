75322eccf641c4b73b956ff510f053106f32f345
Brandon Valentine <brandon@brandonvalentine.com>
2018-10-14 15:10:32 -0500

    resin-cli: remove redundant version string

--- Formula/resin-cli.rb
+++ Formula/resin-cli.rb
@@ -4,7 +4,6 @@ class ResinCli < Formula
   desc "The official resin.io CLI tool"
   homepage "https://docs.resin.io/reference/cli/"
   url "https://registry.npmjs.org/resin-cli/-/resin-cli-7.10.6.tgz"
-  version "7.10.6"
   sha256 "011f79bfb1a75ad077b82afdc42695ce6061a522ccb1671a4e6d47e3e39f0889"
 
   depends_on "node"
