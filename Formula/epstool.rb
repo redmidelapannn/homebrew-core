6ea52fcb50b9fa7efb8f1954de145826ed378d7f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-15 22:59:41 +0000

    epstool: update 3.08 bottle.

--- Formula/epstool.rb
+++ Formula/epstool.rb
@@ -7,13 +7,10 @@ class Epstool < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "99cd88b7e087c3f55b3a0fc49ef08901a9e79745191e5ce551f268148a011dd1" => :mojave
-    sha256 "4e41615a63d60d963029768b0618d43e29142e5495fb693a7f5844d2cb6f0d6b" => :high_sierra
-    sha256 "d53d1f3d353d0f68f52b87603a67ca97942fb714f766a54636c142550568cbd0" => :sierra
-    sha256 "92efa66cd268f0447dc52c14e9da04ae8af01b1691ec8eec3df61bbeb947b713" => :el_capitan
-    sha256 "bb2aefa17b699127f2f6ed65004c9acbf7e5e5122f6f4b920d1e03fb9bd87b2e" => :yosemite
-    sha256 "ae3c4b14dd19d3ac43947eff025a0fa3eabbe832333922359f9f74e0fe5e1d3d" => :mavericks
-    sha256 "408c2744b730a01eed68f19d4f643b060f026bbbc2616cf4b6afcb08ef608b90" => :mountain_lion
+    rebuild 1
+    sha256 "d1c60f6a58de10ffc56c97ea8e43aa8770a10e72951c27889303e4ae399063bd" => :mojave
+    sha256 "5e844d50cc7e054fd91d7d1ac12479e24d5efea3c2ead558fa3adf0dbad3ffc9" => :high_sierra
+    sha256 "f278c23d8f397e08999244c375786073dc800606c4119a4ef3e31f1c5580620a" => :sierra
   end
 
   depends_on "ghostscript"
