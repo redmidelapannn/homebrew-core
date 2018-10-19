0482a75feb269e201a246e7b1393385285e76948
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-19 07:54:08 +0000

    packet-cli: add 0.0.2 bottle.

--- Formula/packet-cli.rb
+++ Formula/packet-cli.rb
@@ -5,6 +5,13 @@ class PacketCli < Formula
   homepage "https://packet.net"
   url "https://github.com/packethost/packet-cli/archive/0.0.2.tar.gz"
   sha256 "1312a9b40a8178e6c106efe887e886b46cf4ff90dd80597e7bc7e46059392e8f"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "51bb48c5da1feae61d75f3ede04e1c589db9f455fd8b6e4e6978b605a259fe1f" => :mojave
+    sha256 "ce5c8e52e73b387f990a930eb439dfd430034f9f20bb6dadd05493dd83468572" => :high_sierra
+    sha256 "ad73359cdbdb340ec56820bbbaa28c649f5f83bdbcbb4b3e1dc765f1cb67be46" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
