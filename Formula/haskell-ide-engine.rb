c286c1be5a1203d22b79b3249388978cd2f5f7c3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-19 10:36:00 +0000

    haskell-ide-engine: add 0.2.1.0 bottle.

--- Formula/haskell-ide-engine.rb
+++ Formula/haskell-ide-engine.rb
@@ -4,6 +4,12 @@ class HaskellIdeEngine < Formula
 
   url "https://github.com/haskell/haskell-ide-engine.git",
     :revision => "0.2.1.0"
+  bottle do
+    sha256 "d99f7ce39dc78e1e5a9a863641d011894ee2b6f69ff0ecbc53248c87d2629e67" => :mojave
+    sha256 "8bd8e61aae40048386ea49176c58bb69d75e41883d198ef6884ce37dc9fddddd" => :high_sierra
+    sha256 "c1d1e714c4f1fc1577869354857d895244f77c5489b12a43a821b3e25c5c4af1" => :sierra
+  end
+
   
   head "https://github.com/haskell/haskell-ide-engine.git"
 
