32544d409afdaf3a365a50eb9927f71f69393d9b
FX Coudert <fxcoudert@gmail.com>
2018-10-19 14:51:24 +0200

    skafos 1.7.4

--- Formula/skafos.rb
+++ Formula/skafos.rb
@@ -1,8 +1,8 @@
 class Skafos < Formula
   desc "CLI for the Metis Machine A.I. and machine learning deployment platform"
   homepage "https://metismachine.com/"
-  url "https://github.com/MetisMachine/skafos/archive/1.7.3.tar.gz"
-  sha256 "c10474132f58c397b529fd4b566f799fd9e85cdc1b4105fb97acd8787f86785a"
+  url "https://github.com/MetisMachine/skafos/archive/1.7.4.tar.gz"
+  sha256 "e6b9ce8b68ce03dbff0438ddd51f724a431ace23e2de3884f02463678832a73f"
 
   bottle do
     cellar :any
