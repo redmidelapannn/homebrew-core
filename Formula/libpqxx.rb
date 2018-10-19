34cff5c019ee76026df324106bf10c227b92f5e8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-19 18:35:14 +0000

    libpqxx: update 6.2.4_3 bottle.

--- Formula/libpqxx.rb
+++ Formula/libpqxx.rb
@@ -7,10 +7,9 @@ class Libpqxx < Formula
 
   bottle do
     cellar :any
-    sha256 "70c47a06792e042ec1039b98fd7cf3bc5dd6398f84bd6bf8e89bbc25e8c29706" => :mojave
-    sha256 "8017994020ce366492b79055e349730ffb2964e5fac11b3e734360e22b9e7986" => :high_sierra
-    sha256 "599b8ac667629d5704a1822f64d0609bd0c4833c6b61b6164088beda8a7ceaa7" => :sierra
-    sha256 "24c11214d49a0f493871beadbf8cbaac859b4b4393f74f8f0a880e0f269154a7" => :el_capitan
+    sha256 "42a7b14e48060e0480179ae2383542eda0a95a03222b7542dd80babd341f29f0" => :mojave
+    sha256 "b68bc4b41b49fc86b1b69796712ca83a35c7b0e7f9410268ceae529401bba89e" => :high_sierra
+    sha256 "b12c3439ee2bb5b595bf772210f3b1e9f975541e53b8067cbee1ef4c9429e95e" => :sierra
   end
 
   depends_on "pkg-config" => :build
