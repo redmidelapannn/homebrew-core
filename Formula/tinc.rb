0fd51bc4ea02b53e7bd1ba48617b7ce97b0fcc70
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-20 12:39:07 +0000

    tinc: update 1.0.35 bottle.

--- Formula/tinc.rb
+++ Formula/tinc.rb
@@ -5,9 +5,10 @@ class Tinc < Formula
   sha256 "18c83b147cc3e2133a7ac2543eeb014d52070de01c7474287d3ccecc9b16895e"
 
   bottle do
-    sha256 "98c73cd30d30b74363b2093621006361da5a8738e040d9c6bf329016d7f18c3f" => :mojave
-    sha256 "9c76119b5f3116772bff828dfb3032d44d1ee6d232236789c03871cf605d9f37" => :high_sierra
-    sha256 "0c78ab7901f43d6f2dd2406157308377722df4d225737fb193dcd7e3f3ef7714" => :sierra
+    rebuild 1
+    sha256 "9c1346ef93e3948ab511cdd6c139994c51eb95350a934f0a8ee6e7bdd9b52e44" => :mojave
+    sha256 "79ad67610c45593b0b7532e46a934bbb0e892b3881c3cad537cd6065ae900430" => :high_sierra
+    sha256 "89639ad03be8c9a18ba54c085d2497c6c3a6cc789ed371ab2bbba21cf122b4e5" => :sierra
   end
 
   depends_on "lzo"
