4735faed4603da197e7472224de5a1e9eaa308ee
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-20 08:53:05 +0000

    abook: update 0.5.6_1 bottle.

--- Formula/abook.rb
+++ Formula/abook.rb
@@ -7,11 +7,10 @@ class Abook < Formula
   head "https://git.code.sf.net/p/abook/git.git"
 
   bottle do
-    sha256 "aa37eab8b488f96dd3d54236309ed7841aaeede27a179b4de7415fb8711dc904" => :mojave
-    sha256 "3eb9e2e7003ef3501c63d87a5a632791769c84ac4d81a2c5fe1d40c04986e19f" => :high_sierra
-    sha256 "e32cff277928e0b5cd24f201b1b5f94faf5469f263856b48c78f85b539018c86" => :sierra
-    sha256 "fc5e09a73519a20dbe90258d6779bfddb1a02b2fc277fd54b4cd8c80c378539d" => :el_capitan
-    sha256 "2f3a8d37fd17ecdda801f8de53e4048f19d824748e11a34c6f9abca0aae06c3b" => :yosemite
+    rebuild 1
+    sha256 "b72177e86ac2a9fbbc3541374c296d9841a42a73c4150e96160617c910e9e16a" => :mojave
+    sha256 "fa09fe607c2b60760f7d5b133a4689d2ae7bd5c70963aab5daa5be25badefede" => :high_sierra
+    sha256 "61c75529f187a65ec5158a06e82f4227ff5a6bfba59c921776fb2bb692a78ee3" => :sierra
   end
 
   depends_on "readline"
