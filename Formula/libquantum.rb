445c731d2211ea1fe25a4269161b0782b3eda3b7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-20 12:39:07 +0000

    libquantum: update 1.0.0 bottle.

--- Formula/libquantum.rb
+++ Formula/libquantum.rb
@@ -6,13 +6,10 @@ class Libquantum < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0a74bf2e856af7db63821d9ed7fbbd569977c04f808d52380fe841ad1e855cac" => :mojave
-    sha256 "3e5896712c2c1a35d230aad235f312eac36946fbac1f605e6c5b90963c6c22c2" => :high_sierra
-    sha256 "ac518e4460bb259e294f1eabbfeb85c9e996ccab05f6e97a915ec34d21ae4e5f" => :sierra
-    sha256 "14c3e392521c20d45a993639de7f561bc0d2b2718158636074bac6f0a2d41581" => :el_capitan
-    sha256 "ce31c2a7df81599bc4930ad4aef206f22e006db41d32d05ef1f2f3e72ff6d29d" => :yosemite
-    sha256 "2347b6f64ac6a2463cded1679de4390f5bda4b07a74f304efd4ea3bc536af3df" => :mavericks
-    sha256 "a7df989c22406155638d24ac755e851208d4cb7f72a8f1f17a985d172270006f" => :mountain_lion
+    rebuild 1
+    sha256 "589e1a2024e60baace2c99588197e3d938d2534df1d36fdfcf14055c60836b12" => :mojave
+    sha256 "0a995f5328a9911ede2de5afb2600918ff260ab5858e5537dd098741273981f8" => :high_sierra
+    sha256 "531fe5bb36f29471b9979b2ab52c3333598ac58f464c469811bdc071bb1a40da" => :sierra
   end
 
   def install
