30897586ceea81b61b19ffc2ddfc0cee4a94dfbc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-21 10:54:16 +0000

    simgrid: update 3.20 bottle.

--- Formula/simgrid.rb
+++ Formula/simgrid.rb
@@ -5,10 +5,10 @@ class Simgrid < Formula
   sha256 "4d4757eb45d87cf18d990d589c31d223b0ea8cf6fcd8c94fca4d38162193cef6"
 
   bottle do
-    sha256 "a9c5cb5da50ab2b224e5d1882834d1e7c4428393e05bec3731823597a2da30e9" => :mojave
-    sha256 "75ce382a395f963425946d7f53361fa1143a2b6ca9292c63193e76139bcef91e" => :high_sierra
-    sha256 "58895cb0aed9235972377d2460a18c8bc03203212ae8246450c429e837a01bcf" => :sierra
-    sha256 "c0a6cf0fba37da1c281cac508bdde38aa4145fe7c50bcc9f466f5521cf95d722" => :el_capitan
+    rebuild 1
+    sha256 "1d7b9eb544d3839c0f6ad6e958ebcb0e35042e68b0464aadc907b23be3be0182" => :mojave
+    sha256 "506beed39e34164877d1be6a56d376acf7febe75933f99ac686961b46fcdc2f4" => :high_sierra
+    sha256 "ef32b071d6bb42032e5a1905a398685976c3576d03d7e069c610112dc3b150d3" => :sierra
   end
 
   depends_on "cmake" => :build
