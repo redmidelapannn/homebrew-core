11232ff316f4e59a9ab9230af19573d2b6e31339
MRNIU <a6813140@hotmail.com>
2018-10-24 01:29:03 +0800

    Fixed some error.
    About i386-elf-gcc.rb,
    
        mkdir "i386-elf-gcc-build" do
        "--without-isl",
        binutils = Formulary.factory "i386-elf-binutils"
        ln_sf binutils.prefix/"i386-elf", prefix/"i386-elf"
    
    are necessary, otherwise it can't be compiled.
    The last two lines created an i386-elf-binutils link in i386-elf-gcc, otherwise an error
    will occur when compiling programs with it.
    
    Thanks.

--- Formula/i386-elf-binutils.rb
+++ Formula/i386-elf-binutils.rb
@@ -1,24 +1,18 @@
 class I386ElfBinutils < Formula
   desc "FSF Binutils for i386-elf cross development"
   homepage "https://www.gnu.org/software/binutils/"
-  url "https://ftp.gnu.org/gnu/binutils/binutils-2.31.tar.gz"
-  sha256 "5a9de9199f22ca7f35eac378f93c45ead636994fc59f3ac08f6b3569f73fcf6f"
-  depends_on "gettext" => :build
-  depends_on "xz" => :build
-
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.31.1.tar.xz"
+  sha256 "5d20086ecf5752cc7d9134246e9588fa201740d540f7eb84d795b1f7a93bca86"
   def install
-    mkdir "binutils-build" do
-      system "../configure", "--target=i386-elf",
-                             "--disable-multilib",
-                             "--disable-nls",
-                             "--disable-werror",
-                             "--prefix=#{prefix}"
-      system "make"
-      system "make", "install"
-    end
+    system "./configure", "--target=i386-elf",
+                          "--disable-multilib",
+                          "--disable-nls",
+                          "--disable-werror",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
   end
-
   test do
-    system "#{bin}/i386-elf-ar", "--version"
+    assert_match "f()", shell_output("#{bin}/i386-elf-c++filt _Z1fv")
   end
 end
--- Formula/i386-elf-gcc.rb
+++ Formula/i386-elf-gcc.rb
@@ -1,25 +1,22 @@
 class I386ElfGcc < Formula
   desc "The GNU compiler collection for i386-elf"
   homepage "https://gcc.gnu.org"
-  url "https://mirrors.nju.edu.cn/gnu/gcc/gcc-8.2.0/gcc-8.2.0.tar.gz"
-  sha256 "1b0f36be1045ff58cbb9c83743835367b860810f17f0195a4e093458b372020f"
-  depends_on "gettext" => :build
-  depends_on "gmp" => :build
-  depends_on "i386-elf-binutils" => :build
-  depends_on "libmpc" => :build
-  depends_on "mpfr" => :build
-  depends_on "xz" => :build
-
+  url "https://mirrors.nju.edu.cn/gnu/gcc/gcc-8.2.0/gcc-8.2.0.tar.xz"
+  sha256 "196c3c04ba2613f893283977e6011b2345d1cd1af9abeac58e916b1aab3e0080"
+  depends_on "gmp"
+  depends_on "i386-elf-binutils"
+  depends_on "libmpc"
+  depends_on "mpfr"
   def install
-    mkdir "gcc-build" do
+    mkdir "i386-elfgcc-build" do
       system "../configure", "--target=i386-elf",
                              "--prefix=#{prefix}",
+                             "--without-isl",
                              "--disable-multilib",
                              "--disable-nls",
                              "--disable-werror",
                              "--without-headers",
-                             "--enable-languages=c,c++",
-                             "--without-isl"
+                             "--enable-languages=c, c++"
       system "make", "all-gcc"
       system "make", "install-gcc"
       system "make", "all-target-libgcc"
@@ -28,8 +25,16 @@ class I386ElfGcc < Formula
       ln_sf binutils.prefix/"i386-elf", prefix/"i386-elf"
     end
   end
-
   test do
-    system "#{bin}/i386-elf-gcc", "--version"
+    (testpath/"test-c.c").write <<~EOS
+      #include <stdio.h>
+      int main()
+      {
+        puts("Hello, world!");
+        return 0;
+      }
+    EOS
+    system "#{bin}/i386-elf-gcc", "-E", "-o", "test-c.i", "test-c.c"
+    assert_not_empty "test-c.i"
   end
 end
