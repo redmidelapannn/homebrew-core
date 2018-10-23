c54c3dd5d5d1812a641d8b3ab3e63736be7f0496
Luis Santana <noncetonic@blacksunhackers.club>
2018-10-23 14:21:52 -0700

    sng 1.1.0 (new formula)
    Adds a new formula for the Scriptable Network Graphics compiler/decompiler, `sng`

---
+++ Formula/sng.rb
@@ -0,0 +1,16 @@
+class Sng < Formula
+  desc "Enable lossless editing of PNGs via a textual representation"
+  homepage "https://sng.sourceforge.io/"
+  url "https://downloads.sourceforge.net/project/sng/sng-1.1.0.tar.gz"
+  sha256 "119c55870c1d1bdc65f7de9dbc62929ccb0c301c2fb79f77df63f5d477f34619"
+  depends_on "libpng"
+
+  def install
+    system "./configure", "--prefix=/usr/local/Cellar/sng/1.1.0"
+    system "make", "install"
+  end
+
+  test do
+    system "curl", "-o", "test.png", "https://upload.wikimedia.org/wikipedia/en/2/2c/WikiPNGRenderingTestB_Thumb_RGB.png", "&&", "sng", "test.png", "&&", "true"
+  end
+end
