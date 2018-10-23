56f79eeb1acea175fb5322eb66669813c53eb5e0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-23 22:50:30 +0000

    lockgit: add 0.5.0 bottle.

--- Formula/lockgit.rb
+++ Formula/lockgit.rb
@@ -5,6 +5,13 @@ class Lockgit < Formula
   url "https://github.com/jswidler/lockgit/archive/v0.5.0.tar.gz"
   sha256 "5bfc50eebf7d846c1c227ffb9c92cd45e0f6c15b33316997104b5cc700ea4dfa"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8c144e639341155dc0bfb08e9e13ff0d5581cdd7680e88926aa0690710146d5a" => :mojave
+    sha256 "e1a8d6c043b35050f0f71d7ffdaae72b2a954e9b7a6e9aeb8dc853c769cd7270" => :high_sierra
+    sha256 "d660d3b75090792a78e053e20ac125ffec1775a8c9f459ef969ed79c84919471" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
