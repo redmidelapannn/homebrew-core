2fef3631d4e82556e82357883516424f40d5c805
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-23 19:58:48 +0000

    sfk: update 1.9.3.2 bottle.

--- Formula/sfk.rb
+++ Formula/sfk.rb
@@ -7,9 +7,7 @@ class Sfk < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "31be7680a1df8fa2fb4de468fc67e8393aad87c8919cf3b9c08bac70b3c05ba1" => :high_sierra
-    sha256 "42cd7ac69141a671fa741ff9ca9b4dacaa4530ed9e2c969f91803b68ed6167c8" => :sierra
-    sha256 "151596ee2a4392118faf49ed6b56f7b4408ea800d6e212afc6c67941690b735d" => :el_capitan
+    sha256 "004cc739484e6b0d1ed83b178a9a3f72976c5b5b57a721e66148e821423abb14" => :sierra
   end
 
   def install
