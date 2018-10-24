cb4a33a6aa99d6053ddd6e3d2bb9bae1bcf5e268
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-24 05:33:55 +0000

    terminus: add 1.9.0 bottle.

--- Formula/terminus.rb
+++ Formula/terminus.rb
@@ -4,6 +4,13 @@ class Terminus < Formula
   url "https://github.com/pantheon-systems/terminus/archive/1.9.0.tar.gz"
   sha256 "4e315e950affbf0c95e7b1225b80151c85ff8ae04b2aad053c2adb5c30380f93"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a6419319ae5f6103f5596b42f582fd6834215f32896c19ee5baaf2456e2b2467" => :mojave
+    sha256 "f0c5f1d59679cdf94dd74f50f0645f1ca5ac91dfa9b7ee14b26f5d86d477b6d2" => :high_sierra
+    sha256 "32c1a134533445345a06052a657ed5029fd95b1cd64ab9a1f08c347e9521436c" => :sierra
+  end
+
   depends_on "composer"
 
   def install
