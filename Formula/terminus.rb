dbe979fa24457538bd78b3284ea8b0d2ba3a85a2
Wesley Turner-Harris <wturnerharris@gmail.com>
2018-10-24 00:17:46 -0400

    Fix case for dependency name

--- Formula/terminus.rb
+++ Formula/terminus.rb
@@ -4,7 +4,7 @@ class Terminus < Formula
   url "https://github.com/pantheon-systems/terminus/archive/1.9.0.tar.gz"
   sha256 "4e315e950affbf0c95e7b1225b80151c85ff8ae04b2aad053c2adb5c30380f93"
 
-  depends_on "Composer"
+  depends_on "composer"
 
   def install
     system "composer", "install"
