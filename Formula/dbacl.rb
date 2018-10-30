c5bfa45060158771eb9827b5e879cf115b06f6cc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-30 09:33:13 +0000

    dbacl: update 1.14.1 bottle.

--- Formula/dbacl.rb
+++ Formula/dbacl.rb
@@ -6,12 +6,10 @@ class Dbacl < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "8a64ac80e91d8d5b2366046096098b851d503c58af65ef0858834c5794d039a5" => :mojave
-    sha256 "42c1c03e8df0b4db91dc99ace3ec87f3901f1aa6975430d597240ab5f9182c1f" => :high_sierra
-    sha256 "c6e6d74e2f2a86325ee895f8ef6893d99e1463d0018ead0d0da46e0dfd95c272" => :sierra
-    sha256 "750c29761c5784ddbd0d46643f2d462d8b22c14822773e2366db01be17a3e310" => :el_capitan
-    sha256 "b3d759bba5d1e478bc3df9e829594f49fb37cdd96e589937ebf4a7da87416855" => :yosemite
-    sha256 "09d4e7e62bc73cf25f16d366c768519e10f3ce33894c4a7eaa327046cc05889a" => :mavericks
+    rebuild 1
+    sha256 "6effe73086dabdf61c584f88275b86b512d4a85e02612cd6ef7318b2e83ddf5c" => :mojave
+    sha256 "abdde448f6354be6f05b6d61b5a3e2e391182e8f706d52d1397e021552ca4209" => :high_sierra
+    sha256 "f9bd9ab031463d6fac0b811f47bb1758d2af474f82cdeebd8db83e5d3a73ce0f" => :sierra
   end
 
   def install
