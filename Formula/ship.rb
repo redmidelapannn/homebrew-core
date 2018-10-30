6e9dbc21430e98d428cf90037af0a68d3c584b2a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-10-30 21:58:39 +0000

    ship: add 0.21.0 bottle.

--- Formula/ship.rb
+++ Formula/ship.rb
@@ -5,6 +5,13 @@ class Ship < Formula
   version "0.21.0"
   sha256 "31aff6c15e6b3cf964f724d5e74e4199db57954c2d7650fce03638138c04df6c"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "3d5e609a976d65b65a36c39f951e0a3685ab0edf4c1b21b2a68755973228860a" => :mojave
+    sha256 "9f31ad70abef06cdc4b6f8258dd28f4e903de0850ee0eff4776185ce9f5947ce" => :high_sierra
+    sha256 "9f31ad70abef06cdc4b6f8258dd28f4e903de0850ee0eff4776185ce9f5947ce" => :sierra
+  end
+
   def install
     bin.install "ship"
   end
