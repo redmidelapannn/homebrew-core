55f51f51e05f62d18f3c48aadb6fc303128f5d74
Kotaro Yamazaki <k_yamazaki@kde.cs.tsukuba.ac.jp>
2018-10-31 13:53:12 +0900

    update for v1.3.0

--- Formula/cassandra-reaper.rb
+++ Formula/cassandra-reaper.rb
@@ -1,16 +1,13 @@
 class CassandraReaper < Formula
   desc "Management interface for Cassandra"
   homepage "http://cassandra-reaper.io"
-  url "https://github.com/thelastpickle/cassandra-reaper/releases/download/1.2.2/cassandra-reaper-1.2.2-release.tar.gz"
-  sha256 "720aff69e3205301bc07399afc46dae3568d8effffa3712f1852a169ce9801db"
+  url "https://github.com/thelastpickle/cassandra-reaper/releases/download/1.3.0/cassandra-reaper-1.3.0-release.tar.gz"
+  sha256 "79c190c51c3404c2efc7f7f1aafa7cfd91f2280cbb1fe719e668966836904efd"
 
   def install
-    inreplace "bin/cassandra-reaper", "etc", etc
-    libexec.install "bin/cassandra-reaper"
+    prefix.install "bin"
     share.install "server/target" => "cassandra-reaper"
     etc.install "resource" => "cassandra-reaper"
-    env = { :CLASS_PATH => "#{pkgshare}/cassandra-reaper-?.?.?.jar" }
-    (bin/"cassandra-reaper").write_env_script(libexec/"cassandra-reaper", env)
   end
 
   test do
