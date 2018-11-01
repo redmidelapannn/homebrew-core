6e7d862c28651c8575fc1782852ddd59187d37a0
Johnny Willemsen <jwillemsen@remedy.nl>
2018-11-01 14:00:22 +0100

    Version should be before sha256
        * Formula/ace.rb:

--- Formula/ace.rb
+++ Formula/ace.rb
@@ -2,8 +2,8 @@ class Ace < Formula
   desc "ADAPTIVE Communication Environment: OO network programming in C++"
   homepage "https://www.dre.vanderbilt.edu/~schmidt/ACE.html"
   url "https://github.com/DOCGroup/ACE_TAO/releases/download/ACE%2BTAO-6_5_2/ACE.tar.bz2"
-  sha256 "f0393d6df25ee92e0cbc6539c68ccf122caae0ffd5ae9a786163403bb2306cc5"
   version "6.5.2"
+  sha256 "f0393d6df25ee92e0cbc6539c68ccf122caae0ffd5ae9a786163403bb2306cc5"
 
   bottle do
     cellar :any
