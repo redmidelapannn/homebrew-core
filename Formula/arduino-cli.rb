38a5b3c4ba8bef2c6761b8832829838da61f6736
Steven Conaway <Steven.conaway@icloud.com>
2018-11-02 20:43:50 -0700

    Add arduino-cli Formula

---
+++ Formula/arduino-cli.rb
@@ -0,0 +1,17 @@
+class ArduinoCli < Formula
+  desc "Arduino command-line interface"
+  homepage "https://github.com/arduino/arduino-cli"
+  url "https://github.com/arduino/arduino-cli/archive/0.3.2-alpha.preview.tar.gz"
+  sha256 "fe0a2766dbe9410e7b9873778c0fe7e693cb82e29db1ce246e9a7eeff2569ed7"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "get"
+    system "go", "build"
+  end
+
+  test do
+    system "#{bin}/arduino-cli", "version"
+  end
+end
