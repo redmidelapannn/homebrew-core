be6e147adcacd708ea46e6df4b24438ff6dba01f
Thierry Moisan <thierry.moisan@gmail.com>
2018-11-04 11:43:22 -0500

    dnsdist: revision for mojave

--- Formula/dnsdist.rb
+++ Formula/dnsdist.rb
@@ -3,6 +3,7 @@ class Dnsdist < Formula
   homepage "https://www.dnsdist.org/"
   url "https://downloads.powerdns.com/releases/dnsdist-1.3.2.tar.bz2"
   sha256 "0be7704e5a418a8ed6908fc110ecfb9bc23f270b5af8a5525f1fa934ef0e6bc4"
+  revision 1
 
   bottle do
     sha256 "62320372f4328e35695e03165f4565a2a229ecbc6b9d4a9a8943fbe68a010ff9" => :high_sierra
@@ -18,7 +19,7 @@ class Dnsdist < Formula
     # error: unknown type name 'mach_port_t'
     ENV["SDKROOT"] = MacOS.sdk_path if MacOS.version == :sierra
 
-    if MacOS.version == :high_sierra
+    if MacOS.version >= :high_sierra
       sdk = MacOS::CLT.installed? ? "" : MacOS.sdk_path
       ENV["LIBEDIT_CFLAGS"] = "-I#{sdk}/usr/include -I#{sdk}/usr/include/editline"
       ENV["LIBEDIT_LIBS"] = "-L/usr/lib -ledit -lcurses"
