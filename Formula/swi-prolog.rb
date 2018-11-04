2c9641bc378b937b048d58ac7d9a2c3c2445637d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-04 11:07:01 +0000

    swi-prolog: update 7.6.4 bottle.

--- Formula/swi-prolog.rb
+++ Formula/swi-prolog.rb
@@ -5,10 +5,10 @@ class SwiProlog < Formula
   sha256 "2d3d7aabd6d99a02dcc2da5d7604e3500329e541c6f857edc5aa06a3b1267891"
 
   bottle do
-    sha256 "5076f120b7f2775fc0968885d2d0e82cb7a93f3040c1c39243abdd8ec3ba1e59" => :mojave
-    sha256 "7a1a76d4b9160e0fea1899a8af0dcd448f71efef8476b1732d75e8d0339ac419" => :high_sierra
-    sha256 "af00bfcc0da68a800dd50e608aabc6620db00de1a7bf1b986a7bc49ae58ea234" => :sierra
-    sha256 "2016d9b076b252805f48f705181d03cd26183b0f74a026c029cd34f9e8afb79d" => :el_capitan
+    rebuild 1
+    sha256 "0ca7d11c9bd7a55329e6858c428f671b3d35e5d2e8c28bfe639a87c0a8af5099" => :mojave
+    sha256 "7e9a96b62a531e3787a7ea318e71241b05292040053cee427264d65f8ae9f32e" => :high_sierra
+    sha256 "00f37afa2e50d4b214de7a46ad2ec166a00fe2974c3745ef262f8f3e802e7bd4" => :sierra
   end
 
   head do
