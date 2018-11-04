543a17c1cd92857aea57182c472ba451ff397868
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-04 19:21:02 +0000

    xmrig-amd: add 2.8.4 bottle.

--- Formula/xmrig-amd.rb
+++ Formula/xmrig-amd.rb
@@ -4,6 +4,13 @@ class XmrigAmd < Formula
   url "https://github.com/xmrig/xmrig-amd/archive/v2.8.4.tar.gz"
   sha256 "3bc3669724c67e47a88e3878c1e345a1697aaa28e779afe9b972221bbece9a9d"
 
+  bottle do
+    cellar :any
+    sha256 "3e7698b292cd666fdca5bfa0a65129aca09a5734090840586e47db2ec5f9533e" => :mojave
+    sha256 "d352e0a76a345022278589cb1da21d63ab8c5c1e57b7dd4f63533c7da37bf0b5" => :high_sierra
+    sha256 "94686fa387497d97ba39366214217e6d5bba40c8edc00023a1f5e38ee04b1049" => :sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "libmicrohttpd"
   depends_on "libuv"
