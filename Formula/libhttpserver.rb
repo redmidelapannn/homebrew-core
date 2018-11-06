5f340c480f39e1d9ef73d0f61cd01ea6e645fc2a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-06 02:31:12 +0000

    libhttpserver: update 0.14.0 bottle.

--- Formula/libhttpserver.rb
+++ Formula/libhttpserver.rb
@@ -7,12 +7,9 @@ class Libhttpserver < Formula
 
   bottle do
     cellar :any
-    sha256 "8aae09b1c7c4a8eae4c7e9ebb109c9c0fa251dea72eeaf7939c4206ef3e8b48b" => :mojave
-    sha256 "5dc4f569cf86961564e5954220d9049af40ef28796eefa350c12c6df8b32e45e" => :high_sierra
-    sha256 "aac717fe37c9fa0491185e237100288f2d8ebe8544bfc16115f20b13fd242782" => :sierra
-    sha256 "ee7c3025c9678a97f326c69a8a9faa4963eefc8f972c78096b3f237cf7368945" => :el_capitan
-    sha256 "d6ec883a992e348d69b90c37b3c0f1ab2329cc9bae3cb8d1f1db7d112ca65200" => :yosemite
-    sha256 "aec3bba3f8db0cb1e9fd99d66aafb1f2ed399197f11af43654f911205b62d5ee" => :mavericks
+    sha256 "24f60c5ae04d52c13de56c095b65d4a5ec2645c7de25663b0f2dba5c4e7e315a" => :mojave
+    sha256 "118277615a80f3056530dac07d3bb0c4dcb8ebaa318d317b7795f97e21337ce8" => :high_sierra
+    sha256 "0665789e905350a6088688caea50c5770c5fd821d22bfa4a761dac2993e0b28a" => :sierra
   end
 
   depends_on "autoconf" => :build
