d7b4592c03bfab06026a3ad49144ca27c0ddeeee
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-06 04:10:21 +0000

    libbtbb: update 2018-08-R1 bottle.

--- Formula/libbtbb.rb
+++ Formula/libbtbb.rb
@@ -8,9 +8,9 @@ class Libbtbb < Formula
 
   bottle do
     cellar :any
-    sha256 "94949c6626fc47d9879b00ab5f259a3bc09ffa3aab3b9be93c56e80cdb19d5db" => :mojave
-    sha256 "e044f611d480089eb2206cfdb6d7289c33cd9043642f024270957e6ac3f2fa5f" => :high_sierra
-    sha256 "ce36c7d48d1ca6ebf1087f2edab17be8ce4f381afe652f03a5fab0d6f6c9f7db" => :sierra
+    sha256 "bbec306e4b2331bea56c78a34b58c26bda9fd9934d74b37da0f66f07e2447117" => :mojave
+    sha256 "9c618053e9402ad3b96b016905dda2aa4a187202b161307c65146d0af2d9ce5b" => :high_sierra
+    sha256 "7e054b8afbee98c21b9573ddaa926d809ac159894ef9f4fdcf18b34cb94ba302" => :sierra
   end
 
   depends_on "cmake" => :build
