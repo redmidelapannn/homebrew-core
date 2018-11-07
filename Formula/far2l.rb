deafc075dfa6087cf8eee2b633247130fe3f222f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-07 13:12:41 +0000

    far2l: add 0.2-alpha bottle.

--- Formula/far2l.rb
+++ Formula/far2l.rb
@@ -6,6 +6,13 @@ class Far2l < Formula
   sha256 "971d170698d9567d28c547ba5d49234f6d0f9e5b1dece0ab366037daea87e675"
   head "https://github.com/elfmz/far2l.git"
 
+  bottle do
+    cellar :any
+    sha256 "de7fdd69209432ac70d3b6c6378c95ebdd3b7dbb06cd6e78d123c8fc09aced68" => :mojave
+    sha256 "a647c1037126f3c0c8b301cc6053dd7a75396e4bf09d6459f05ba4cb2b8d6f49" => :high_sierra
+    sha256 "bc13c4e2905d8c8b9ae3c1e1b118f7f4952ba54868d3d0ce3759f397b50e00f1" => :sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "gawk"
   depends_on "glib"
