76111f7cb1dc46dd9e0cae807c11e9ae3550e89a
Igor Kapkov <igasgeek@me.com>
2018-11-07 12:08:55 +1100

    termrec 0.18

--- Formula/termrec.rb
+++ Formula/termrec.rb
@@ -1,8 +1,8 @@
 class Termrec < Formula
   desc "Record \"videos\" of terminal output"
   homepage "https://angband.pl/termrec.html"
-  url "https://github.com/kilobyte/termrec/archive/0.17.tar.gz"
-  sha256 "e3496dcb520b63036423cc72f3eaf66f690e869ef4ae508f027923062c34d84f"
+  url "https://github.com/kilobyte/termrec/archive/v0.18.tar.gz"
+  sha256 "b74aeea0526b606d0c0a8dfe4149670f7d34be9d4369e974a15abc67509a02ac"
   head "https://github.com/kilobyte/termrec.git"
 
   bottle do
