c30e6bc9d8f03efc78d46cfb89a56d86ebff9eb8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-10 22:40:05 +0000

    awsweeper: add 0.2.0 bottle.

--- Formula/awsweeper.rb
+++ Formula/awsweeper.rb
@@ -4,6 +4,13 @@ class Awsweeper < Formula
   url "https://github.com/cloudetc/awsweeper/archive/v0.2.0.tar.gz"
   sha256 "e867ecc3df01fb799fe900bc587676460ade1752f63c9176542bb66d27a1833a"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1fcf668dd4de7e0b25901a90c297746d7d14b9dfdbaf9dcd1f62486f7f0ba3f7" => :mojave
+    sha256 "0961aa160e0ef538f8ce0523ac3ac09f9d90c68433eba689982983d2c0270ee0" => :high_sierra
+    sha256 "d85872d1c9bac0090cb3a788eb6e6d2767e1923b556f92f91bc3911dda23051e" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
