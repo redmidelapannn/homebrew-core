fa35486752f97a40a79d0d1dac13a951366126fb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-10 03:04:24 +0000

    pazpar2: update 1.13.1_1 bottle.

--- Formula/pazpar2.rb
+++ Formula/pazpar2.rb
@@ -7,10 +7,9 @@ class Pazpar2 < Formula
 
   bottle do
     cellar :any
-    sha256 "75e4e23993c4a0051b8f02133c825cfc7874db10fd4780161ff2413fc2b2153a" => :mojave
-    sha256 "8116479eacfc5e7d9d463a98b0bbef1eca4f89b878a9ffd8ef37a02b149f68f8" => :high_sierra
-    sha256 "234f0ac36ece60e82c0fd808df4aba614222c231db5181014e8a4e694456809c" => :sierra
-    sha256 "2e691d6a0a7f5bf18c69d6eb624af3e4c7166b4197ce9918c02d6a12686316e3" => :el_capitan
+    sha256 "4e5d8a3e89b2c6c60047481baa9821c030473d33355f025f936ab79b6ef6eea7" => :mojave
+    sha256 "3833bff5a7de29ebc97d4b18e7c311cae100e2ddb9a1ede39f6165198f104d2e" => :high_sierra
+    sha256 "5c29945513d59e563fc88cd6a0ec258a4b1fe9e0c983e8228eeb10de838097b4" => :sierra
   end
 
   head do
