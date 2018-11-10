ef2f499ee460426faee59923d7dde9d260eae381
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-10 03:04:24 +0000

    mimic: update 1.2.0.2_5 bottle.

--- Formula/mimic.rb
+++ Formula/mimic.rb
@@ -7,10 +7,9 @@ class Mimic < Formula
 
   bottle do
     cellar :any
-    sha256 "b88a35a402c068c4efa17d9ca35a502292ff3d0ee162dfbe4fe3b48e192d76cb" => :mojave
-    sha256 "20fc7273db85a315903304d758aa74edf0f7bfc5947a9ee261661fc2b38e699f" => :high_sierra
-    sha256 "8072f200df17d949891fce6fec60489527a3ae4e9f4f3a59cae6b42f86e19b0c" => :sierra
-    sha256 "e91d1999945a78d5f88d20c47702585630e61d010ba52742bec6af159d1ca47c" => :el_capitan
+    sha256 "cbeca8df6f3ccf07164b8133af86dffac08ff6433847847cf6aead89fa920a79" => :mojave
+    sha256 "fde8d25df74ce4b06294066cc2e162167089cb7f3972020429cb63ea0c360397" => :high_sierra
+    sha256 "68faa0e11d7ffa277696b4df934eac8724d32882123e86a6e9dbb71fd77f50d3" => :sierra
   end
 
   depends_on "autoconf" => :build
