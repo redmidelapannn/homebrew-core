5a274089ab5f4ce98bc2dcc49693ec7033bd8674
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-11 00:45:55 +0000

    cloud-nuke: add 0.1.4 bottle.

--- Formula/cloud-nuke.rb
+++ Formula/cloud-nuke.rb
@@ -4,6 +4,13 @@ class CloudNuke < Formula
   url "https://github.com/gruntwork-io/cloud-nuke/archive/v0.1.4.tar.gz"
   sha256 "ce2f5caff4db80accc9ceaeef859382135e772dfd08d1bf981e52098512c45f7"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "169f15604288077c6ca42719c5ff359fe5dbe7a20cb1fde557fafb0eca6f8769" => :mojave
+    sha256 "0caeb48e16635cd22bcc4e665144930838ec3f655987165696773a463c26a5e3" => :high_sierra
+    sha256 "043fb1a8e94709f542f9a68b5b680b64c0e9c78d305c1face1516c0cd7151e2e" => :sierra
+  end
+
   depends_on "dep" => :build
   depends_on "go" => :build
 
