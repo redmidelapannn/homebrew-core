75128ca3ad6d2d7964c2d6cd7827691371f37496
Matthias Winkelmann <MatthiasWinkelmann@users.noreply.github.com>
2018-11-14 15:11:22 +0100

    Updated Resources

--- Formula/ocrmypdf.rb
+++ Formula/ocrmypdf.rb
@@ -39,18 +39,18 @@ class Ocrmypdf < Formula
   end
 
   resource "pikepdf" do
-    url "https://files.pythonhosted.org/packages/09/ef/db0bc644097f52382f8040633ca8bc49b04a54dfbf76147c495398e2949b/pikepdf-0.3.5.tar.gz"
-    sha256 "661d38fd54bf419549bb162b9dab6699395896a52e10ca5d0b52610806122d69"
+    url "https://files.pythonhosted.org/packages/ea/8d/01f66685772025ac82c63c759b032d5df2a0714a7f59292c393a084cb42e/pikepdf-0.3.7.tar.gz"
+    sha256 "870c930e3450656b6da793968124ea9c509370deb413479652e0787b655d3290"
   end
 
   resource "Pillow" do
-    url "https://files.pythonhosted.org/packages/d3/c4/b45b9c0d549f482dd072055e2d3ced88f3b977f7b87c7a990228b20e7da1/Pillow-5.2.0.tar.gz"
-    sha256 "f8b3d413c5a8f84b12cd4c5df1d8e211777c9852c6be3ee9c094b626644d3eab"
+    url "https://files.pythonhosted.org/packages/1b/e1/1118d60e9946e4e77872b69c58bc2f28448ec02c99a2ce456cd1a272c5fd/Pillow-5.3.0.tar.gz"
+    sha256 "2ea3517cd5779843de8a759c2349a3cd8d3893e03ab47053b66d5ec6f8bc4f93"
   end
 
   resource "pycparser" do
-    url "https://files.pythonhosted.org/packages/8c/2d/aad7f16146f4197a11f8e91fb81df177adcc2073d36a17b1491fd09df6ed/pycparser-2.18.tar.gz"
-    sha256 "99a8ca03e29851d96616ad0404b4aad7d9ee16f25c9f9708a11faf2810f7b226"
+    url "https://files.pythonhosted.org/packages/68/9e/49196946aee219aead1290e00d1e7fdeab8567783e83e1b9ab5585e6206a/pycparser-2.19.tar.gz"
+    sha256 "a988718abfad80b6b157acce7bf130a30876d27603738ac39f140993246b25b3"
   end
 
   resource "python-xmp-toolkit" do
@@ -59,18 +59,18 @@ class Ocrmypdf < Formula
   end
 
   resource "pytz" do
-    url "https://files.pythonhosted.org/packages/ca/a9/62f96decb1e309d6300ebe7eee9acfd7bccaeedd693794437005b9067b44/pytz-2018.5.tar.gz"
-    sha256 "ffb9ef1de172603304d9d2819af6f5ece76f2e85ec10692a524dd876e72bf277"
+    url "https://files.pythonhosted.org/packages/cd/71/ae99fc3df1b1c5267d37ef2c51b7d79c44ba8a5e37b48e3ca93b4d74d98b/pytz-2018.7.tar.gz"
+    sha256 "31cb35c89bd7d333cd32c5f278fca91b523b0834369e757f4c5641ea252236ca"
   end
 
   resource "reportlab" do
-    url "https://files.pythonhosted.org/packages/70/4c/19fe74b800e7d74b3dd636137aac6e8df4b19286e318c1a5b6d8ca4b17fd/reportlab-3.5.6.tar.gz"
-    sha256 "3836a49e7ea7bce458f437cbc094633c7fd4ac027180565875c18ecc726f261e"
+    url "https://files.pythonhosted.org/packages/6d/40/e0230c95234a16accad10bb50228b3b274c92797c92bcc423db70e0396c0/reportlab-3.5.9.tar.gz"
+    sha256 "f92f81314807cd860f29fe07a1a4100b03910ae6bbfca20a07e02c3b460f4f20"
   end
 
   resource "ruffus" do
-    url "https://files.pythonhosted.org/packages/ea/32/5048607dd7a9104406789b15fb4078e774121b23190c9e464d4dd1f7ed89/ruffus-2.7.0.tar.gz"
-    sha256 "4bd46461d31aa532357019a33d8045f4e57e52f4ee41643b5b3a7372e380cae0"
+    url "https://files.pythonhosted.org/packages/ac/c7/5652e44a3644dfdf8cdfa17c2bb7933896dddec99888f57bf2adbbcca226/ruffus-2.8.0.tar.gz"
+    sha256 "609e76e88de2d11362ed2a312917508f14daf56413fd1b3c7d5a96aaa14bb5e9"
   end
 
   def install
