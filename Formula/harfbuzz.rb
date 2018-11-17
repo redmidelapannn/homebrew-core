a4b8995555ceba8a1e1ce466fe9129c64646b7c1
Chongyu Zhu <i@lembacon.com>
2018-11-17 08:57:25 +0800

    harfbuzz 2.1.2

--- Formula/harfbuzz.rb
+++ Formula/harfbuzz.rb
@@ -1,8 +1,8 @@
 class Harfbuzz < Formula
   desc "OpenType text shaping engine"
   homepage "https://wiki.freedesktop.org/www/Software/HarfBuzz/"
-  url "https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-2.1.1.tar.bz2"
-  sha256 "83bf5d552df72c52969332f294cd97d8f6b46b77b41b61346ca56ebb81884e14"
+  url "https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-2.1.2.tar.bz2"
+  sha256 "18b70c2fa3c757361ba5e6185a64a59a00e1c1392b90ac21eccde09bacc6bcc1"
 
   bottle do
     sha256 "e3f8391baaf751e545b9757db5d76dcdbc56cece41732cd27df67b923b4886fa" => :mojave
