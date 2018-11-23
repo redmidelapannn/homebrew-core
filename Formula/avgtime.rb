1dc4f35d5005f32be7be6bfdf91ffe2b825bd6b7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-23 17:26:29 +0000

    avgtime: add 0.6.0-dev bottle.

--- Formula/avgtime.rb
+++ Formula/avgtime.rb
@@ -4,6 +4,11 @@ class Avgtime < Formula
   url "https://github.com/jmcabo/avgtime.git", :revision => "ffdf20049db7a6aa6074354d95265869c6aad65b"
   version "0.6.0-dev"
 
+  bottle do
+    sha256 "254e179967e9edf7460403d86e6e14e474a427c4fa86aeb27919963f7109d66d" => :high_sierra
+    sha256 "465e1823784549e9059810194e2b0f6ae6f484bab0606e343ceacf7dac818558" => :sierra
+  end
+
   depends_on "dmd" => :build
 
   def install
