9cebcf92e5a170a2ebe5091f1726a1ac1461b70d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-23 13:50:43 +0000

    pict: add 33 bottle.

--- Formula/pict.rb
+++ Formula/pict.rb
@@ -5,6 +5,12 @@ class Pict < Formula
   version "33"
   sha256 "73d36bc93d72dfbc936e11774c5615b23689cab1a3507995dcb9266d406f5e11"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e46de6c979d7740afc6181fa1a836c422e54b41051a77561aa4dc16bc6e4a780" => :high_sierra
+    sha256 "ab476c261c4dd34327b17fbf33d5882abac72559f4f1a99055acaa7efc1bace8" => :sierra
+  end
+
   def install
     system "make"
     bin.mkpath
