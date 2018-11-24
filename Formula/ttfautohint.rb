5c7acc1cc87a0c446d543e7a41ca8d048c79955b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-24 12:49:49 +0000

    ttfautohint: update 1.8.2 bottle.

--- Formula/ttfautohint.rb
+++ Formula/ttfautohint.rb
@@ -6,10 +6,10 @@ class Ttfautohint < Formula
 
   bottle do
     cellar :any
-    sha256 "5a56efdcfabb79895db80fd244c6578f5042e1aa30196a48fa51e530a9586899" => :mojave
-    sha256 "d44712b0e6a341fad7a535ab42f9f131994528352ce98bd838697110d7cfd67a" => :high_sierra
-    sha256 "754ef442537be790bc6e1fea4723cf007ba5a9bb78a56e5933c9c7026ff38fec" => :sierra
-    sha256 "f94c3bd7520d4af65b157c124957e3fbe765c55e2b316c45d761f4e441c3a273" => :el_capitan
+    rebuild 1
+    sha256 "a62918efda8b5577d511603305508045a86d8777274dc86db139ef9f351b85e8" => :mojave
+    sha256 "f81ec5b4421d4fbd7cba01356033626c5556271080956e32722888309190a8f6" => :high_sierra
+    sha256 "9247905a058d75071afae5be0395edc5086a3e43f8e21d1ca69b1bfd06a32891" => :sierra
   end
 
   head do
