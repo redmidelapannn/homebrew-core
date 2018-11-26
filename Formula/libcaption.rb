62c0e4c937fb809d665ed1474cc9c871d73e7e25
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-26 04:42:04 +0000

    libcaption: add 0.7 bottle.

--- Formula/libcaption.rb
+++ Formula/libcaption.rb
@@ -5,6 +5,13 @@ class Libcaption < Formula
   url "https://github.com/szatmary/libcaption/archive/0.7.tar.gz"
   sha256 "125c9c55e1d5f0dc37ef151fa9583dd2fcfaefe7699d348c7292e634859e527e"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "168b3b0a20913aa18df1a9dd9af93644497574f9221d57030c405768a082dd77" => :mojave
+    sha256 "23ccd33a7007d02796b6750fbd7bf526487dd8b450c475493cb7e3fd6f22f7ad" => :high_sierra
+    sha256 "cfc5c5cb0f7446001f159ec71f7e417556851c4ef3c5544bbf5b747e575c4421" => :sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "ffmpeg" => [:build, :test]
   depends_on "re2c" => :build
