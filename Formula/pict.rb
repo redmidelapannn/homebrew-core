f6a223f80d1e6adb7f3d4de4c7d446248040faea
Giuseppe Sorrentino <uhpnou@gmail.com>
2018-11-28 17:51:48 +0100

    changed resource sha256

--- Formula/pict.rb
+++ Formula/pict.rb
@@ -6,7 +6,7 @@ class Pict < Formula
 
   resource "testfile" do
     url "https://gist.githubusercontent.com/glsorre/9f67891c69c21cbf477c6cedff8ee910/raw/84ec65cf37e0a8df5428c6c607dbf397c2297e06/pict.txt"
-    sha256 "4fc7939c708f9c8d6346430b3b90f122f2cc5e341f172f94eb711b1c48f2518a"
+    sha256 "ac5e3561f9c481d2dca9d88df75b58a80331b757a9d2632baaf3ec5c2e49ccec"
   end
 
   def install
