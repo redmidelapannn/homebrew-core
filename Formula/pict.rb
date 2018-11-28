45e4b9e34a4bdfac24b7447f274e1f44139e2cd0
Giuseppe Sorrentino <uhpnou@gmail.com>
2018-11-28 13:01:07 +0100

    used resources instead than curl

--- Formula/pict.rb
+++ Formula/pict.rb
@@ -4,15 +4,19 @@ class Pict < Formula
   url "https://github.com/Microsoft/pict/archive/v3.7.1.tar.gz"
   sha256 "4fc7939c708f9c8d6346430b3b90f122f2cc5e341f172f94eb711b1c48f2518a"
 
+  resource "testfile" do
+    url "https://gist.githubusercontent.com/glsorre/9f67891c69c21cbf477c6cedff8ee910/raw/84ec65cf37e0a8df5428c6c607dbf397c2297e06/pict.txt"
+    sha256 "4fc7939c708f9c8d6346430b3b90f122f2cc5e341f172f94eb711b1c48f2518a"
+  end
+
   def install
     system "make"
-    bin.mkpath
     bin.install "pict"
   end
 
   test do
-    system "curl", "https://gist.githubusercontent.com/glsorre/9f67891c69c21cbf477c6cedff8ee910/raw/84ec65cf37e0a8df5428c6c607dbf397c2297e06/pict.txt", "-o", "prova.txt"
-    output = `#{bin}/pict prova.txt`
+    variables = resource("testfile").fetch
+    output = shell_output("#{bin}/pict " + variables)
     output = output.split("\n")
     assert_equal output[0], "LANGUAGES\tCURRIENCIES"
     assert_equal output[4], "en_US\tGBP"
