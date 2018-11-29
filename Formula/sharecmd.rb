24cf327c960a91b2f2e780c71ebcc21c0b464650
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-11-29 10:44:59 +0000

    sharecmd: add 0.0.26 bottle.

--- Formula/sharecmd.rb
+++ Formula/sharecmd.rb
@@ -5,6 +5,13 @@ class Sharecmd < Formula
   version "0.0.26"
   sha256 "adcf3f4f4c0edb2811bc9985936d5343a04e19fcca5a1e622e772f91a3560f3e"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4d8b1293bcc351a88c87494a07ee779456cab85dafbedfbb1167fcc0281a3595" => :mojave
+    sha256 "e56ce597a24555f07e851ea9ee05bef78c1bf77ea7d7faac2f85af78bcada8be" => :high_sierra
+    sha256 "e56ce597a24555f07e851ea9ee05bef78c1bf77ea7d7faac2f85af78bcada8be" => :sierra
+  end
+
   def install
     bin.install "share"
   end
