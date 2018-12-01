56fa87b601c7b85a1bd729d8f76c25a5ef3c0a53
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-01 08:39:03 +0000

    skim: add 0.5.2 bottle.

--- Formula/skim.rb
+++ Formula/skim.rb
@@ -4,6 +4,12 @@ class Skim < Formula
   url "https://github.com/lotabout/skim/archive/v0.5.2.tar.gz"
   sha256 "41280bee2138afefff95f76a640b753d1cb9215e8391ef37eab7ccb3517d9019"
 
+  bottle do
+    sha256 "4f3fa07825b9a9a1835b72cecf4b1f5e13ec03bb53bf42b38e5f01f14b3ee667" => :mojave
+    sha256 "e4ce983171199d05f35c27bfff3035ad494fc113bfd60c4d486d55ea7820c2fc" => :high_sierra
+    sha256 "c943bb63cdf4324394d93eecda07e764f7e938fd51a34958bf954753f42da5d5" => :sierra
+  end
+
   depends_on "rust" => :build
 
   def install
