49b21c25b77f44b33595adaf4235762b7cd6b2d2
Igor Kapkov <igasgeek@me.com>
2018-12-01 21:38:00 +1100

    node@6 6.15.1

--- Formula/node@6.rb
+++ Formula/node@6.rb
@@ -1,9 +1,8 @@
 class NodeAT6 < Formula
   desc "Platform built on V8 to build network applications"
   homepage "https://nodejs.org/"
-  url "https://nodejs.org/dist/v6.14.4/node-v6.14.4.tar.xz"
-  sha256 "9a4bfc99787f8bdb07d5ae8b1f00ec3757e7b09c99d11f0e8a5e9a16a134ec0f"
-  revision 1
+  url "https://nodejs.org/dist/v6.15.1/node-v6.15.1.tar.xz"
+  sha256 "c3bde58a904b5000a88fbad3de630d432693bc6d9d6fec60a5a19e68498129c2"
 
   bottle do
     sha256 "5c493aa31e3950ef73fc825c60a152a2653d89ae6a2cad8e4632502ed4738a5d" => :mojave
@@ -31,6 +30,12 @@ class NodeAT6 < Formula
   end
 
   def install
+    # Switches standard libary for native addons from libstdc++ to libc++ to
+    # match the superenv enforced one for the node binary itself. This fixes
+    # incompatibilities between native addons built with our node-gyp and our
+    # node binary and makes building native addons with XCode 10.1+ possible.
+    inreplace "common.gypi", "'CLANG_CXX_LANGUAGE_STANDARD': 'gnu++0x',  # -std=gnu++0x",
+                             "'CLANG_CXX_LANGUAGE_STANDARD': 'gnu++0x',  # -std=gnu++0x\n'CLANG_CXX_LIBRARY': 'libc++',"
     resource("icu4c").stage buildpath/"deps/icu"
     system "./configure", "--prefix=#{prefix}", "--with-intl=full-icu"
     system "make", "install"
