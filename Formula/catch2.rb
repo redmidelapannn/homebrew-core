7b47aeb916ed2fd82787dddd1ec2c9f752d46f53
ncihnegn <ncihnegn@users.noreply.github.com>
2018-12-04 02:30:27 -0800

    Fix for brew audit

--- Formula/catch2.rb
+++ Formula/catch2.rb
@@ -1,8 +1,7 @@
 class Catch2 < Formula
-  desc "A modern, C++-native, header-only, test framework for unit-tests, TDD and BDD - using C++11, C++14, C++17 and later (or C++03 on the Catch1.x branch)"
+  desc "Modern, C++-native, header-only, test framework"
   homepage "https://discord.gg/4CWS9zD"
   url "https://github.com/catchorg/Catch2/archive/v2.5.0.tar.gz"
-  version "2.5.0"
   sha256 "720c84d18f4dc9eb23379941df2054e7bcd5ff9c215e4d620f8533a130d128ae"
   head "https://github.com/catchorg/Catch2.git"
   depends_on "cmake" => :build
@@ -13,4 +12,21 @@ class Catch2 < Formula
     system "cmake", "--build", "build", "--target", "install"
   end
 
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #define CATCH_CONFIG_MAIN
+      #include <catch2/catch.hpp>
+      unsigned int Factorial( unsigned int number ) {
+          return number <= 1 ? number : Factorial(number-1)*number;
+      }
+      TEST_CASE( "Factorials are computed", "[factorial]" ) {
+          REQUIRE( Factorial(1) == 1 );
+          REQUIRE( Factorial(2) == 2 );
+          REQUIRE( Factorial(3) == 6 );
+          REQUIRE( Factorial(10) == 3628800 );
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-std=c++1y", "-o", "test"
+    system "./test"
+  end
 end
