70d44098cd71d9bc7461a77d2501fe210c7ab240
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-04 02:37:10 +0000

    expect: update 5.45.4_1 bottle.

--- Formula/expect.rb
+++ Formula/expect.rb
@@ -6,10 +6,9 @@ class Expect < Formula
   revision 1
 
   bottle do
-    sha256 "668b4fb12eed5bbf783e8b4ec52dad24b88f38af5577ba1e45ed9947e50e50ef" => :mojave
-    sha256 "a0c6ffe797dc0bbe512b628819acee67a7a9b00573b6433fe0672285d41a9df1" => :high_sierra
-    sha256 "fc9ad781caaf8d45f47a87d4303645faa2e600852c73fd5432f0be2e588e95f2" => :sierra
-    sha256 "4fcb163b1b1e7e209b632c43ba03106ca1c4e4d6a745260b813d28a803581e58" => :el_capitan
+    sha256 "3addaf0de76ba525dcfe2dcb9820578020dd39f32f8117d294f02ded6d321bef" => :mojave
+    sha256 "0d8f581af9c0c723ab0a1ed9e4f183265338e13c940740fd2483c362bf3d7a67" => :high_sierra
+    sha256 "648945349b7f19e768e0771800220dcd1aff0acad0e29715ae0baa1c156fca5d" => :sierra
   end
 
   option "with-threads", "Build with multithreading support"
