17e5b42d3aba264a9a48c280f4c63c344700fe94
Yvo Mulder <7681555+ytbmulder@users.noreply.github.com>
2018-12-05 07:32:49 +0100

    aiccu: remove mirror

--- Formula/aiccu.rb
+++ Formula/aiccu.rb
@@ -3,7 +3,6 @@ class Aiccu < Formula
   homepage "https://www.sixxs.net/tools/aiccu/"
   # Upstream 402s when passed a non-standard User-Agent such as Homebrew's
   url "https://cdn-aws.deb.debian.org/debian/pool/main/a/aiccu/aiccu_20070115.orig.tar.gz"
-  mirror "http://ftp.nl.debian.org/debian/pool/main/a/aiccu/aiccu_20070115.orig.tar.gz"
   version "20070115"
   sha256 "d23cf50a16fa842242c97683c3c1c1089a7a4964e3eaba97ad1f17110fdfe3cc"
 
