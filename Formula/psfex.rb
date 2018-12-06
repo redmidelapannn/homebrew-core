76e2c9a02218d9d5c017e86ca47ce70c81259b2c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-06 17:38:42 +0000

    psfex: add 3.21.1 bottle.

--- Formula/psfex.rb
+++ Formula/psfex.rb
@@ -4,6 +4,13 @@ class Psfex < Formula
   url "https://github.com/astromatic/psfex/archive/3.21.1.zip"
   sha256 "0716e6be8d701ada0500c3e5e40091d4ac15f85ebd670dc4f937f92812b26b70"
 
+  bottle do
+    cellar :any
+    sha256 "bbea204dae0dbb015f5696595cf33431cd8d014300e4cad7ca9021bb1ee66d75" => :mojave
+    sha256 "40ba2e32224b4ef2fc601e965f4117c96ab9949645abb5448c0d2f707994ee95" => :high_sierra
+    sha256 "3e62244cf0371fe7c36e93293a144319f3964f881d6febf0d93caab25e709c7c" => :sierra
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
