d328a500c04a80a329c325e6853d90f3ebfe1cbf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-08 00:49:05 +0000

    synfig: update 1.0.2_5 bottle.

--- Formula/synfig.rb
+++ Formula/synfig.rb
@@ -7,10 +7,10 @@ class Synfig < Formula
   head "https://svn.code.sf.net/p/synfig/code/"
 
   bottle do
-    sha256 "7051446f2836f7de2f71508639cd7e82de5b71013e55801124f6e5ecf426cca4" => :mojave
-    sha256 "2f05123f7d5b6cd2d2e0ae7ba1bb070e5bf5bd69f71a85553ea45858ea18b7ab" => :high_sierra
-    sha256 "ef83f8b014f601eeed4e7810d5edd5c41838ef0cf7df918674aa8975a670bcb7" => :sierra
-    sha256 "42f7ab274f30645bd06e707a9e7962cabf7ba8766728c781e066efa2cd1a53ac" => :el_capitan
+    rebuild 1
+    sha256 "0ce520d89c628c7f43af95d43448db7fc9096c67c7fd7660eb128a37b9d0f578" => :mojave
+    sha256 "eb4ea4a665ec80e307a4bba0abb5585cabfdfd71dbd1b395674c9eddd6cf1249" => :high_sierra
+    sha256 "af743f415d72009de7098228a585b5fceb7b65bc49696dadbc590ddb553424ec" => :sierra
   end
 
   depends_on "pkg-config" => :build
