0ee5937ce423c317bdec4ce3649bd573155967c1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-08 00:49:03 +0000

    sz81: update 2.1.7 bottle.

--- Formula/sz81.rb
+++ Formula/sz81.rb
@@ -6,11 +6,10 @@ class Sz81 < Formula
   head "https://svn.code.sf.net/p/sz81/code/sz81"
 
   bottle do
-    sha256 "b90dc9986a1f3f6fa93967745f331d55d4e8837e05e47b9b28d3ee9245e561d3" => :mojave
-    sha256 "c23507f4f58b7144b2b4c0dd42ed6ae22a6d65661d15ea024ab8b65fd2a774ba" => :high_sierra
-    sha256 "853475dfc7991beea12b01669e81fc35ce10e6a9b067716eb026e0ff693d5c4c" => :sierra
-    sha256 "7a9b6ffa108486dea9514df6fbdd820a0e7b829c893ecb1b76a1b69ca8f39a21" => :el_capitan
-    sha256 "a7f7cc5af1a1a42449da3169e18587df907369c94debf6bb15edba62acf0e199" => :yosemite
+    rebuild 1
+    sha256 "411faa3977506e3eb5656b57b61d93b8010567d25f499c5536fdec6b468b5a0e" => :mojave
+    sha256 "9702256a769b45a100d2c301e0add713eb910c8be4a8c06d07bc44383b39d38b" => :high_sierra
+    sha256 "0c234aba8025b5ab0596a19a42db854b59bb4c323528b0e8bedea9585ff55f16" => :sierra
   end
 
   depends_on "sdl"
