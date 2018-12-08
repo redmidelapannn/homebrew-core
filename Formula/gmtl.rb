3797d92aacd000182c6a1d098822a17992ec126a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-08 00:49:05 +0000

    gmtl: update 0.6.1 bottle.

--- Formula/gmtl.rb
+++ Formula/gmtl.rb
@@ -15,13 +15,10 @@ class Gmtl < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "f69dd084cade396219047d066439043e0aaa689797082ee207db7a1335787286" => :mojave
-    sha256 "66ae5e3ccd2a0cbf4608b4ffee45bccb9c3be33148af25787c76652c1c0967ac" => :high_sierra
-    sha256 "ee8d0c9f5f52453421a189c040459b5126a5b739231493a3e39d331c934c6478" => :sierra
-    sha256 "8aa9f0f1fb77376dd333bb03e9c5a07f6457b76008a74018a932dca930148606" => :el_capitan
-    sha256 "5e6d70f957f11e58d8b3cd24d5474a8bedc73e0aec6df13f85322f4fda8a1164" => :mavericks
-    sha256 "ffeb26dd58a9b05a4427ca02392f93f9d5b352af790e536e4d2989baa81e4faf" => :mountain_lion
-    sha256 "568a43df4aebd32ab9638d2725721b9c062bca0ecb778dbffb67fafd926d4a1a" => :lion
+    rebuild 1
+    sha256 "a71a2b32e61ea9a2611613fdb52ab655c49b605cdb0c663bb96f431175b6dba8" => :mojave
+    sha256 "48a81079220c66a9f423f93387da2a1290705289605ab5dbdc329bae098e98f2" => :high_sierra
+    sha256 "48a81079220c66a9f423f93387da2a1290705289605ab5dbdc329bae098e98f2" => :sierra
   end
 
   depends_on "scons" => :build
