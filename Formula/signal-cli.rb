b6bf8e93ebd8afc3075e2999c6cdf6d32ecbc4a6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-11 23:10:24 +0000

    signal-cli: add 0.6.1 bottle.

--- Formula/signal-cli.rb
+++ Formula/signal-cli.rb
@@ -3,6 +3,13 @@ class SignalCli < Formula
   homepage "https://github.com/AsamK/signal-cli"
   url "https://github.com/AsamK/signal-cli/releases/download/v0.6.1/signal-cli-0.6.1.tar.gz"
   sha256 "550b389a25089ef2251d97997e06d57c70e9d4023684ff8682afef23ed2f1822"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9f78f519c436a3feaf038a20f5eddbd38f5d864ca44d471ed23d40687d454571" => :mojave
+    sha256 "e13ce729d63e4f364f617e8dd7ee0239063265c1212accb6742de29c070005e0" => :high_sierra
+    sha256 "e13ce729d63e4f364f617e8dd7ee0239063265c1212accb6742de29c070005e0" => :sierra
+  end
+
   depends_on :java => "1.7+"
 
   def install
