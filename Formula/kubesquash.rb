ad54a184dde81f49ebb4fc154cfd3dacf51df819
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-13 22:36:30 +0000

    kubesquash: add 0.1.6 bottle.

--- Formula/kubesquash.rb
+++ Formula/kubesquash.rb
@@ -5,6 +5,13 @@ class Kubesquash < Formula
   version "0.1.6"
   sha256 "acfd077d186202e4fd88ec6c96aa26130d86b2e6eae308ff7b000cc307ab1b2c"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "91557a2ad5dc07105551f897cecea70d4418df5e0893c2593d8e16fd70870b4c" => :mojave
+    sha256 "2b81bc5f650c6f1e6a30e873bc63cd54cbcf49f1cc110a3f16eed885313a7d98" => :high_sierra
+    sha256 "2b81bc5f650c6f1e6a30e873bc63cd54cbcf49f1cc110a3f16eed885313a7d98" => :sierra
+  end
+
   def install
     mv "kubesquash-osx", "kubesquash"
     bin.install "kubesquash"
