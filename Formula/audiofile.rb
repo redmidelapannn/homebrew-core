22ce17448ee2e18022758ffd4ec8db6d906c6648
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-18 00:57:03 +0000

    audiofile: update 0.3.6_1 bottle.

--- Formula/audiofile.rb
+++ Formula/audiofile.rb
@@ -18,11 +18,10 @@ class Audiofile < Formula
 
   bottle do
     cellar :any
-    sha256 "9d1038463e8eaa68f1cee8c447d566dc5acd32e2697f41837a9c08fedb0b2088" => :mojave
-    sha256 "cf1f732ca5565a0e5d24a8d90714e554f95b4fbd3662da18ec843c0c356fff16" => :high_sierra
-    sha256 "0a6cc39d6cce2c4436008f3d5679dbac6a8e0c0a1a91ea5db34597737fd5fb54" => :sierra
-    sha256 "8e725b2809f539e2382b07a2fb64a551cbb09fdbaad168dd05784142e07ce495" => :el_capitan
-    sha256 "b7c1a9815937840419cec79513d908a5e7ddb34699c34d656a6f1f85cb08b90f" => :yosemite
+    rebuild 1
+    sha256 "a4c973cefe6fd7501ffe0d01350be146a61022aa996fed1015f2f52d3f433250" => :mojave
+    sha256 "1d571c0e4c6a8b497309e8eede32fd0b319197ea178443759362b5aacd770df8" => :high_sierra
+    sha256 "ff7c5d46ec36387c9dd949bf9b4798927f4485076a97478481f6f3982ea1e5ad" => :sierra
   end
 
   head do
