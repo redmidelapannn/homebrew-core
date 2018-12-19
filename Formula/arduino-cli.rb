76ed1baee300cd5deb62345b8e4d9ed47f1b86af
Isaac Webb <6345267+isaac-webb@users.noreply.github.com>
2018-12-18 21:33:18 -0500

    arduino-cli 0.3.3-alpha

--- Formula/arduino-cli.rb
+++ Formula/arduino-cli.rb
@@ -1,22 +1,17 @@
 class ArduinoCli < Formula
   desc "Arduino command-line interface"
   homepage "https://github.com/arduino/arduino-cli"
-  url "https://github.com/arduino/arduino-cli/archive/0.3.2-alpha.preview.tar.gz"
-  sha256 "fe0a2766dbe9410e7b9873778c0fe7e693cb82e29db1ce246e9a7eeff2569ed7"
-
-  depends_on "go" => :build
+  url "https://github.com/arduino/arduino-cli/releases/download/0.3.3-alpha.preview/arduino-cli-0.3.3-alpha.preview-osx.zip"
+  version "0.3.3-alpha"
+  sha256 "cc5b2b53fba4b518ab3f3332635645f1149aef6e1a5a9a6e6b91ee2b382f49ad"
 
   def install
-    ENV["GOPATH"] = buildpath
-    (buildpath/"src/github.com/arduino/arduino-cli").install buildpath.children
-
-    cd "src/github.com/arduino/arduino-cli" do
-      system "go", "build", "-o", bin/"arduino-cli"
-      prefix.install_metafiles
-    end
+    mv "./arduino-cli-0.3.3-alpha.preview-osx", "./arduino-cli"
+    bin.install "arduino-cli"
   end
 
   test do
-    system bin/"arduino-cli", "version"
+    assert_equal "arduino-cli version #{version}.preview", shell_output("#{bin}/arduino-cli version").strip
+    system "#{bin}/arduino-cli", "core", "search", "arduino"
   end
 end
