a20e573822c300318a097f709621179d379c47cd
Steven Conaway <Steven.conaway@icloud.com>
2018-12-18 18:59:06 -0800

    Arduino-cli: update tests, update version

--- Formula/arduino-cli.rb
+++ Formula/arduino-cli.rb
@@ -1,8 +1,9 @@
 class ArduinoCli < Formula
   desc "Arduino command-line interface"
   homepage "https://github.com/arduino/arduino-cli"
-  url "https://github.com/arduino/arduino-cli/archive/0.3.2-alpha.preview.tar.gz"
-  sha256 "fe0a2766dbe9410e7b9873778c0fe7e693cb82e29db1ce246e9a7eeff2569ed7"
+  url "https://github.com/arduino/arduino-cli/archive/0.3.3-alpha.preview.tar.gz"
+  version "0.3.3-alpha"
+  sha256 "bd47c46efa9e3c72e1fa915f96e3aa9742fbf220e75ef254b44b3e6be8332d0e"
 
   depends_on "go" => :build
 
@@ -17,6 +18,7 @@ class ArduinoCli < Formula
   end
 
   test do
-    system bin/"arduino-cli", "version"
+    assert_equal "arduino-cli version #{version}.preview", shell_output("#{bin}/arduino-cli version").strip
+    system "#{bin}/arduino-cli", "core", "search", "arduino"
   end
 end
