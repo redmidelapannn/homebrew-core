59f42764ffa4a0275d725d14e30ba1e707f8993c
Philipp Haussleiter <phaus@users.noreply.github.com>
2018-12-19 13:15:14 +0100

    Updating termtosvg to 0.7.0

--- Formula/termtosvg.rb
+++ Formula/termtosvg.rb
@@ -3,20 +3,21 @@ class Termtosvg < Formula
 
   desc "Record terminal sessions as SVG animations"
   homepage "https://nbedos.github.io/termtosvg"
-  url "https://github.com/nbedos/termtosvg/archive/0.6.0.tar.gz"
-  sha256 "09ccefb75a6a4f8be186b6efdc3cf355cf8de7dc2222b9e4d841d54e623dd261"
+  url "https://github.com/nbedos/termtosvg/archive/0.7.0.tar.gz"
+  sha256 "a90cbd2ea29e1bdf4059aa8feb8f19e9f32da5620b447289ac78cde38682c7c2"
+
+  head "https://github.com/nbedos/termtosvg.git"
 
   depends_on "python"
 
   def install
     venv = virtualenv_create(libexec, "python3")
-    system libexec/"bin/pip", "install", "-U", "-e", ".[dev]"
+    system libexec/"bin/pip", "install", "-U", "-e", "."
     venv.pip_install_and_link buildpath
     bin.install_symlink libexec/"bin/termtosvg"
   end
 
   test do
-    testfile = testpath/"test.svg"
-    exec("echo \"termtosvg-test\nexit\" | #{bin}/termtosvg  #{testfile} && grep -q termtosvg-test #{testfile}")
+    system libexec/"bin/python", "-m", "unittest", "termtosvg.tests.suite"
   end
 end
