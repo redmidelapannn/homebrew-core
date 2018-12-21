80e020c3834d00e06a1b823540d16a00b416f822
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-21 12:06:49 +0000

    figlet: update 2.2.5 bottle.

--- Formula/figlet.rb
+++ Formula/figlet.rb
@@ -6,12 +6,9 @@ class Figlet < Formula
   sha256 "bf88c40fd0f077dab2712f54f8d39ac952e4e9f2e1882f1195be9e5e4257417d"
 
   bottle do
-    sha256 "906556c44706889c0170f4dfe7d7427f27122cee425042c3911f7266f9fc2e4c" => :mojave
-    sha256 "3047847adef9cb5bd5588cf65f64bfcc0549ed44d4370a862071aba2f9d98ba6" => :high_sierra
-    sha256 "c53966c742bf88b8481f6ed0bde1a951ea11185af2c631fb02b84fa7120f2e17" => :sierra
-    sha256 "943067dae95de58518b20334aec401cf5fd24866ff77315c0d7bd8b5d4ab0011" => :el_capitan
-    sha256 "0a1b051fb0143dbfca1da36c83eca8580c215ff155e0dc755a924ce1f53a4b46" => :yosemite
-    sha256 "3d33cf3ee819346dc431c37f07e2051c9f92d222cb35d330a41ca88bd5153e2d" => :mavericks
+    rebuild 1
+    sha256 "f40fbd6bbf4dfdbade1221a9bdce8555967c69bd60c9d82b6a83573240a0088d" => :mojave
+    sha256 "c9cf1541823a526f043317ade683e43fcee4fa189ed7e691ee15766f5c3c7bbc" => :high_sierra
   end
 
   resource "contrib" do
