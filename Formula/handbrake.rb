e18e3598edd9e2c72a981abc5b268a84abc70802
Chongyu Zhu <i@lembacon.com>
2018-12-23 09:08:02 +0800

    handbrake 1.2.0

--- Formula/handbrake.rb
+++ Formula/handbrake.rb
@@ -1,8 +1,8 @@
 class Handbrake < Formula
   desc "Open-source video transcoder available for Linux, Mac, and Windows"
   homepage "https://handbrake.fr/"
-  url "https://download.handbrake.fr/releases/1.1.2/HandBrake-1.1.2-source.tar.bz2"
-  sha256 "ba9a4a90a7657720f04e4ba0a2880ed055be3bd855e99c0c13af944c3904de2e"
+  url "https://download.handbrake.fr/releases/1.2.0/HandBrake-1.2.0-source.tar.bz2"
+  sha256 "113b398a50147d48c8777e6ff2c4de6825af5f1079b3822e41bf0eacec9c940d"
   head "https://github.com/HandBrake/HandBrake.git"
 
   bottle do
