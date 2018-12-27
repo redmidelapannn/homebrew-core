b01e0e7e0c175f3246abc388449cd6c083b0949b
kpcyrd <git@rxv.cc>
2018-12-27 02:06:27 +0100

    sn0int 0.7.0 (new formula)

---
+++ Formula/sn0int.rb
@@ -0,0 +1,37 @@
+class Sn0int < Formula
+  desc "OSINT framework and package manager"
+  homepage "https://github.com/kpcyrd/sn0int"
+  url "https://github.com/kpcyrd/sn0int/archive/v0.7.0.tar.gz"
+  sha256 "3074f1adbdc6f15ff2d9f8c8b5c5ada19266d7efaf9a21dfc044be08c220ab03"
+
+  depends_on "rust" => :build
+  depends_on "sphinx-doc" => :build
+
+  def install
+    system "cargo", "install", "--root", prefix, "--path", "."
+
+    system "#{bin}/sn0int completions bash > sn0int.bash"
+    system "#{bin}/sn0int completions fish > sn0int.fish"
+    system "#{bin}/sn0int completions zsh > _sn0int"
+
+    bash_completion.install "sn0int.bash"
+    fish_completion.install "sn0int.fish"
+    zsh_completion.install "_sn0int"
+
+    system "make", "-C", "docs", "man"
+    man1.install "docs/_build/man/sn0int.1"
+  end
+
+  test do
+    (testpath/"true.lua").write <<~EOS
+      -- Description: basic selftest
+      -- Version: 0.1.0
+      -- License: GPL-3.0
+
+      function run()
+          -- nothing to do here
+      end
+    EOS
+    system "#{bin}/sn0int", "run", "-vvf", testpath/"true.lua"
+  end
+end
