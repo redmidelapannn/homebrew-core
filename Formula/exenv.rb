03bd5b3d4309b2a60430575baaf4a1357b9fa150
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2018-12-28 22:39:39 +0000

    exenv: add 1.1.1 bottle.

--- Formula/exenv.rb
+++ Formula/exenv.rb
@@ -5,6 +5,13 @@ class Exenv < Formula
   sha256 "d8318ed11ddf6f8a215f982677f4166035efe02bc67993b216d01dec11add2ef"
   head "https://github.com/exenv/exenv.git"
 
+  bottle do
+    cellar :any
+    sha256 "eca8849c0dbe0829cb8c0a07044a618e6d148028127249a71c1ad79f9881b8e4" => :mojave
+    sha256 "7f05cf732a8c3ee52e3f2d1f4408f6bde00f62f9a91eabb7d1f1a11d2a9b1ecf" => :high_sierra
+    sha256 "1adc3dc71d48db98d9c2df4d07073a00bc8c2f63f54a1f6c16788d3b1244345c" => :sierra
+  end
+
   depends_on "elixir-build"
 
   def install
