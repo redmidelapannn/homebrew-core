0992f1e50fa096eea4ce2e78cc97f09ce9088f01
Rafael Gaspar <rafael.gaspar@me.com>
2018-12-28 20:34:08 -0200

    exenv 1.1.1

--- Formula/exenv.rb
+++ Formula/exenv.rb
@@ -1,18 +1,35 @@
 class Exenv < Formula
-  desc "Elixir versions management tool"
-  homepage "https://github.com/mururu/exenv"
-  url "https://github.com/mururu/exenv/archive/v0.1.0.tar.gz"
-  sha256 "368095760ecc386a0930954f5f0ce7cea977641fe6d27b1beff032f512598a58"
-  head "https://github.com/mururu/exenv.git"
+  desc "Elixir version manager"
+  homepage "https://github.com/exenv/exenv"
+  url "https://github.com/exenv/exenv/archive/v1.1.1.tar.gz"
+  sha256 "d8318ed11ddf6f8a215f982677f4166035efe02bc67993b216d01dec11add2ef"
+  head "https://github.com/exenv/exenv.git"
 
-  bottle :unneeded
+  depends_on "elixir-build"
 
   def install
-    inreplace "libexec/exenv", "/usr/local", HOMEBREW_PREFIX
-    prefix.install Dir["*"]
+    inreplace "libexec/exenv" do |s|
+      s.gsub! '"${BASH_SOURCE%/*}"/../libexec', libexec
+      if HOMEBREW_PREFIX.to_s != "/usr/local"
+        s.gsub! ":/usr/local/etc/exenv.d", ":#{HOMEBREW_PREFIX}/etc/exenv.d\\0"
+      end
+    end
+
+    # Compile optional bash extension.
+    system "src/configure"
+    system "make", "-C", "src"
+
+    if build.head?
+      # Record exact git revision for `rbenv --version` output
+      git_revision = `git rev-parse --short HEAD`.chomp
+      inreplace "libexec/exenv---version", /^(version=)"([^"]+)"/,
+                                           %Q(\\1"\\2-g#{git_revision}")
+    end
+
+    prefix.install ["bin", "completions", "libexec", "exenv.d"]
   end
 
   test do
-    system "#{bin}/exenv", "init", "-"
+    shell_output("eval \"$(#{bin}/exenv init -)\"")
   end
 end
