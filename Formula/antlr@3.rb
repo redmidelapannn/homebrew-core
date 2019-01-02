807aa15f7ea70ce17255e15b5bc5f6bb67fde59d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-02 17:12:34 +0000

    antlr@3: add 3.5.2 bottle.

--- Formula/antlr@3.rb
+++ Formula/antlr@3.rb
@@ -4,6 +4,14 @@ class AntlrAT3 < Formula
   url "https://www.antlr3.org/download/antlr-3.5.2-complete.jar"
   sha256 "26ca659f47d77384f518cf2b6463892fcd4f0b0d4d8c0de2addf697e63e7326b"
 
+  bottle do
+    cellar :any_skip_relocation
+    rebuild 1
+    sha256 "ebcb4dff9dde5894ad7c6a079daf65357ddda0f2cdb9e09b9bfe886036a1ffb9" => :mojave
+    sha256 "81a00c106554d5a1fe06fa50d752ed45a73b8c4aaa670aac7f34141726337716" => :high_sierra
+    sha256 "81a00c106554d5a1fe06fa50d752ed45a73b8c4aaa670aac7f34141726337716" => :sierra
+  end
+
   keg_only :versioned_formula
   depends_on :java => "1.5+"
 
