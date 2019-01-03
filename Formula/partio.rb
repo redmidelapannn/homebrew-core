ddb8b6f28fca9a7da59760cf48f19b5a6c0cc770
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-03 18:33:58 +0000

    partio: update 1.5.5 bottle.

--- Formula/partio.rb
+++ Formula/partio.rb
@@ -5,13 +5,10 @@ class Partio < Formula
   sha256 "25352a6af0ccc20794a8c85715d5a6645c3f8ddcb6bbaaf20136670b5abd4727"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "553577c6a9293fbfc098cea8153e6b273e2dc6584db96aa0ea7071be4f6ddd12" => :mojave
-    sha256 "42d4fe9271be76bca99f13bc73146328265995f707fdf50d3c274f5a65193cdd" => :high_sierra
-    sha256 "1db67357f3ce32f14c84788605a167838753433a1a81e17f40758fb2f2630445" => :sierra
-    sha256 "da106b6a4b5667f84b6528081510b12d0da2acb1bfd74afbf3f7af72316afe63" => :el_capitan
-    sha256 "a496ac6afbd60f605e2d3347d06a1850ae2617651b748e28c33a7c4c9c3bf957" => :yosemite
-    sha256 "78e2ac329d90feb8c0211135d2337b5e754b0cc5d70a4d58ebae3acc8442c32e" => :mavericks
+    cellar :any
+    sha256 "22109983d7b5b04aafdfe7fb4294b58cd1057dca16a17e9490829b8516715dd6" => :mojave
+    sha256 "78384ed10f70a6634f56f4dcb461708c5ad95ea17db7df7090ade05557c04751" => :high_sierra
+    sha256 "fd0a725f1ce1590370a84bdd6b3f537fe1b5df2330490db198199f5994d25489" => :sierra
   end
 
   depends_on "cmake" => :build
