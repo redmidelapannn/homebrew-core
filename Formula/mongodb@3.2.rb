9b2df811db5f2a1fb21ad0af6730f2b9854601b0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-04 03:50:30 +0000

    mongodb@3.2: update 3.2.21 bottle.

--- Formula/mongodb@3.2.rb
+++ Formula/mongodb@3.2.rb
@@ -7,10 +7,11 @@ class MongodbAT32 < Formula
   sha256 "8263befc10319809ea14e5cbf230c55113de7b38510b42a6ad27125dfa674371"
 
   bottle do
-    sha256 "6068932526af487dd50264aa1ead11dc199ed2bab849fd0adc04b67f90a89113" => :mojave
-    sha256 "3cc0ca816b495f169afc2d5f4b92463808424f5993ee9d3ec3f8b70354a89fcd" => :high_sierra
-    sha256 "d36d4031c58cee0a35b3f0445f995efad701eb519da8e3b66cf8eeb558092e6e" => :sierra
-    sha256 "8fc6948abb95eca2ecdfdf4411e9ff958616dc5a917376786546e981222852c8" => :el_capitan
+    cellar :any
+    rebuild 1
+    sha256 "403f6f273f16d343c2ad1347a482db98e05ed59649e1316d72c21746b9b8d787" => :mojave
+    sha256 "f30cd2033ad8543cc2ff424d7d5018f13974b2d898d76bb2d7475ed7c1f05124" => :high_sierra
+    sha256 "a134ddc630443ab71203a4986743efe08e678bd917304cef55ba5ac3abfe4aff" => :sierra
   end
 
   keg_only :versioned_formula
