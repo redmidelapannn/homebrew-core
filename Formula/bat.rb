df4591310a39ace140b873b308f7f64d95d852aa
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-04 01:01:34 +0000

    bat: update 0.9.0 bottle.

--- Formula/bat.rb
+++ Formula/bat.rb
@@ -5,9 +5,11 @@ class Bat < Formula
   sha256 "4ce9c118cf5da1159a882dea389f3c5737b5d98192e9a619b0fe8c1730341cc6"
 
   bottle do
-    sha256 "feb7b5af916d972b796c19cae512445b55072b22d6306df646b2c073b37ed081" => :mojave
-    sha256 "35447bd7311bdb54056cbd470dd9449b88ccecc4c62f45d237d51445f1a2a88e" => :high_sierra
-    sha256 "c7ccfa8aeda9b8d2a4e2f8dd6cdc6af2667c0bf036708a7c676fe915e9929ef5" => :sierra
+    cellar :any_skip_relocation
+    rebuild 1
+    sha256 "c3c5bb54b826028ba81220ef1b7b95dc3b24cf5052c03d6d9823f223c092f968" => :mojave
+    sha256 "046af2ce296e01d2c47d0e10e6762d87c303743b65f68b47baad80cb5e785946" => :high_sierra
+    sha256 "914a83043bf8527127462fab799a9eefdf2f9e623b74246fe87d3f202aa0cca3" => :sierra
   end
 
   depends_on "rust" => :build
