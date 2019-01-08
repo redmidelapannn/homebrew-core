a262e6beb242fd531461bd15ffc20288cabdee25
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-08 16:44:13 +0000

    drafter: update 3.2.7 bottle.

--- Formula/drafter.rb
+++ Formula/drafter.rb
@@ -5,11 +5,11 @@ class Drafter < Formula
   sha256 "a2b7061e2524804f153ac2e80f6367ae65dfcd367f4ee406eddecc6303f7f7ef"
 
   bottle do
-    cellar :any_skip_relocation
-    sha256 "886a7b2595eebd07cee223f5d5a53a4748e91719c7f4f3a37c236c8476b3c532" => :mojave
-    sha256 "5d350d1a7fb4aa5e9561933f0e9638ee59fd93b0d9168d07e665a806e472d5f1" => :high_sierra
-    sha256 "a90e9d4493f252d29d69e186dcc49eb116f6bc506d07769f03864f56d5840f08" => :sierra
-    sha256 "dc86d4e8dc44c2dead52e57c3bf6403d691926b9abf274f16b94c9649dd562fd" => :el_capitan
+    cellar :any
+    rebuild 1
+    sha256 "0a5733048972f618798474f84d3c8d17382e4c04231d1cd603f8877e6ccd2f74" => :mojave
+    sha256 "1d2cce9f3e2b882fa33e7871efbcc0b1217f4632f00bbc6fdd45a3fedbc4f053" => :high_sierra
+    sha256 "2d1810ca95f1553c75cf7f2fdc63d05d593e64d2d2cd8f177cf0fa7bd1f4c10b" => :sierra
   end
 
   def install
