fc3af6203f43d590f8aa09d36f5a79d88c79336f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-10 04:44:39 +0000

    sipsak: update 0.9.6 bottle.

--- Formula/sipsak.rb
+++ Formula/sipsak.rb
@@ -7,13 +7,10 @@ class Sipsak < Formula
 
   bottle do
     cellar :any
-    sha256 "ec6959b5778c091626354d80a76fcd96fb1c9ad62661818bca1a724d2e27f7b1" => :mojave
-    sha256 "7590f9703c8c1a70bcc03274f278a398a2bc0d0259c4e7a7fb91c524ec4153ec" => :high_sierra
-    sha256 "4725693dba5edcb68df030b63cb738795e96d29668bc9bf512ba8e2800ec862a" => :sierra
-    sha256 "9f42a09240891ecd9aa62dea1dd75dc1c5362d2f946fefecf13ce975a6c05626" => :el_capitan
-    sha256 "873d8cd50cce684ad55abbdf834157b4464c70877de9d1c37ad3c4ec9aaf6e10" => :yosemite
-    sha256 "d70729739fcfe770fdfa997dc33cd04370a6cd2f6916e63adfed60473c4bfc55" => :mavericks
-    sha256 "09d0961004d525dfc5f81bfe111884b401a09993fd83ff2f426016feb99607d4" => :mountain_lion
+    rebuild 1
+    sha256 "ec09fbad8db21be911af14c791ce615ab9c1c09c54291ff9d06e68d6c36c80f1" => :mojave
+    sha256 "1dddab1931a734faba13716ee041b990e9dddad987a22601dbd14fc63a3ccc94" => :high_sierra
+    sha256 "b130367046fa44a12b34924f67ad95d69584ec4a1e29259e56e7b1105100960c" => :sierra
   end
 
   depends_on "openssl"
