d2c33c19a9e8411f47318db09a029cf95fd3789e
Sameer Naik <sameer@bitnami.com>
2019-01-11 12:38:14 +0530

    kubeprod 1.1.0 (new formula)
    Signed-off-by: Sameer Naik <sameer@bitnami.com>

---
+++ Formula/kubeprod.rb
@@ -0,0 +1,25 @@
+class Kubeprod < Formula
+  desc "Installer for the Bitnami Kubernetes Production Runtime (BKPR)"
+  homepage "https://kubeprod.io"
+  url "https://github.com/bitnami/kube-prod-runtime/archive/v1.1.0.tar.gz"
+  sha256 "fb238bd46fe5177c976640746b3ff51c3961388a4e10edd9581880e0ab670e22"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["TARGETS"] = "darwin/amd64"
+    dir = buildpath/"src/github.com/bitnami/kube-prod-runtime"
+    dir.install buildpath.children
+
+    cd dir do
+      system "make", "-C", "kubeprod", "release", "VERSION=v#{version}"
+      bin.install "kubeprod/_dist/darwin-amd64/bkpr-v#{version}/kubeprod"
+    end
+  end
+
+  test do
+    version_output = shell_output("#{bin}/kubeprod version")
+    assert_match "Installer version: v#{version}", version_output
+  end
+end
