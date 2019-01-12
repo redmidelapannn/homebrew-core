74b6f7caee9aa32a4bfbfe38ef77b0e4e19c0592
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-12 02:31:23 +0000

    miskweb: add 0.1.3 bottle.

--- Formula/miskweb.rb
+++ Formula/miskweb.rb
@@ -10,6 +10,13 @@ class Miskweb < Formula
     stable do
       url 'https://github.com/square/misk-web/raw/master/misk-web/web/packages/@misk/cli/bin/linux/miskweb', :using => :curl
     end
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b5526628fad4a0bd370f8c2aa5a0bde4d360887eafece3cfd0b2fc3b9daf1e38" => :mojave
+    sha256 "a53378d96180818a66b578e8d4c91110cdcceb3dcb7f934e86aed38a3f07da4d" => :high_sierra
+    sha256 "86797dd4891f835167ffe503d6a81c5c02bbfcd91aa5aedf0eae6508c9b30944" => :sierra
+  end
+
   else
     stable do
       url 'https://github.com/square/misk-web/raw/master/misk-web/web/packages/@misk/cli/bin/macos/miskweb', :using => :curl
