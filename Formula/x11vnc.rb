9b44c6c4cfb3250f0c18c2616d08a653824ded5e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-13 19:23:02 +0000

    x11vnc: update 0.9.13_1 bottle.

--- Formula/x11vnc.rb
+++ Formula/x11vnc.rb
@@ -7,11 +7,10 @@ class X11vnc < Formula
 
   bottle do
     cellar :any
-    sha256 "388752257323e1376251e32dea385830afd3a11215b3661dc928111442cb2b5c" => :mojave
-    sha256 "3ed573fd3d02c34f407858af3f6e01386d1af2fc9fee16258eea94a4b2c19137" => :high_sierra
-    sha256 "3f840749807f57b4248c9ae202a214209c4bf4c4e7386d47da83b424153abe2a" => :sierra
-    sha256 "f4a33301592ef159be8f999fce086875bf88afd6ad2b48d6709c2f32d4ab3be1" => :el_capitan
-    sha256 "38e07e6c3a26cf1e8d60f1a4e7061da400afb9bb2803f0dd79566c5a3bfd0d22" => :yosemite
+    rebuild 1
+    sha256 "8ab0e37150405399c3e8e8158888173cc5296e99cf708d488ef77b30d9926594" => :mojave
+    sha256 "b7f0198a06c5a490ecc0bbba063e89258b4cff456b0293cb038032b25fcccd8c" => :high_sierra
+    sha256 "15384d64f10f562b9a1c18eb74edb0c5c80a567d96025167031b35a18d1b1399" => :sierra
   end
 
   depends_on "jpeg"
