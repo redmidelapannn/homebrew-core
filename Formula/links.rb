d1c4651ae7b7c493e9bf14920c8f3dd73fac6790
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-13 19:23:01 +0000

    links: update 2.17_1 bottle.

--- Formula/links.rb
+++ Formula/links.rb
@@ -7,9 +7,10 @@ class Links < Formula
 
   bottle do
     cellar :any
-    sha256 "7946cea648a8453c0d4bd3ec1be3a3b0ef885691c51123dd1e5e252b95e0a06d" => :mojave
-    sha256 "4383c9c0681964915b3a3fe31d26178724e0f856725271e491dbf400dfeffa7e" => :high_sierra
-    sha256 "e6c4e0b96b72c727a01f3b35cb743215a1ab4540c509c694d5f0e5ead1293244" => :sierra
+    rebuild 1
+    sha256 "f9004405755b2d4de642e217f6846596f02fdf021138b88172ea29bb26b7b753" => :mojave
+    sha256 "febe84a620b4ef88d0347bd74b6a0e79df466d58e9d745912bbf798aa9a9ff84" => :high_sierra
+    sha256 "7eae131755d945051bc468691daf4430816f96ee8eaaa66569904d53d5bcf1aa" => :sierra
   end
 
   depends_on "pkg-config" => :build
