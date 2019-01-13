197e53a204ecc8257e5c00774f06014efcc8410a
FX Coudert <fxcoudert@gmail.com>
2019-01-13 09:13:13 +0100

    Update joplin.rb

--- Formula/joplin.rb
+++ Formula/joplin.rb
@@ -5,7 +5,6 @@ class Joplin < Formula
   homepage "https://joplin.cozic.net/"
   url "https://registry.npmjs.org/joplin/-/joplin-1.0.120.tgz"
   sha256 "80a96934f099ef45a35389184de718d683aabdd9b3371c179c0ae6fee5db9ab2"
-  revision 1
 
   bottle do
     sha256 "d9b4ce17add87eab53f2068a9dbad86cd502f30860b4212d59b2c348e99fd861" => :mojave
