8310305921a049688ab1fa0dfbf757b7bc4d03b9
lougehrig10 <lougehrig10@gmail.com>
2019-01-16 07:14:37 -0600

    Removed expletives and added capitalization to description

--- Formula/fff.rb
+++ Formula/fff.rb
@@ -1,5 +1,5 @@
 class Fff < Formula
-  desc "fucking fast file-manager (written in bash)"
+  desc "Fast file-manager (written in bash)"
   homepage "https://github.com/dylanaraps/fff"
   url "https://github.com/dylanaraps/fff/archive/1.2.tar.gz"
   sha256 "a77095950c2fe1eca82c12c41273ed987e295e522c7e85365192f9056e4f80d8"
