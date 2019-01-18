93e213ad84e13ce8618c4a8408d302d680d3866a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-18 10:38:38 +0000

    tzdiff: add 1.0 bottle.

--- Formula/tzdiff.rb
+++ Formula/tzdiff.rb
@@ -4,6 +4,13 @@ class Tzdiff < Formula
   url "https://github.com/belgianbeer/tzdiff/archive/1.0.tar.gz"
   sha256 "a02aa5a5c97471dcdc7588cfd3f3f216c01ff0d518cb2ed2df1553959849ea34"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "60957a8ed101e2f890aaa552767ae0cd0e738e2ab0e4959cec24cb3f207e043b" => :mojave
+    sha256 "917e486bc04abc8bdeb00e7c9cbb31f7e63940601c6d7e26ede6c9c09d064c0f" => :high_sierra
+    sha256 "917e486bc04abc8bdeb00e7c9cbb31f7e63940601c6d7e26ede6c9c09d064c0f" => :sierra
+  end
+
   def install
     bin.install "tzdiff"
     man1.install "tzdiff.1"
