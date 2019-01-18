45c9044a93d0b9768fb8b7730564731ea7c12721
adtennant <alex@adtennant.co.uk>
2019-01-18 12:04:28 +0000

    physfs 2.0.3 (new formula)

---
+++ Formula/physfs@2.rb
@@ -0,0 +1,28 @@
+class PhysfsAT2 < Formula
+    desc "Library to provide abstract access to various archives"
+    homepage "https://icculus.org/physfs/"
+    url "https://icculus.org/physfs/downloads/physfs-2.0.3.tar.bz2"
+    sha256 "ca862097c0fb451f2cacd286194d071289342c107b6fe69079c079883ff66b69"
+  
+    depends_on "cmake" => :build
+  
+    def install
+      mkdir "macbuild" do
+        args = std_cmake_args
+        args << "-DPHYSFS_BUILD_TEST=TRUE"
+        args << "-DPHYSFS_BUILD_WX_TEST=FALSE"
+        system "cmake", "..", *args
+        system "make", "install"
+      end
+    end
+  
+    test do
+      (testpath/"test.txt").write "homebrew"
+      system "zip", "test.zip", "test.txt"
+      (testpath/"test").write <<~EOS
+        addarchive test.zip 1
+        cat test.txt
+      EOS
+      assert_match /Successful\.\nhomebrew/, shell_output("#{bin}/test_physfs < test 2>&1")
+    end
+  end
