4437e53d3ff3f296d1d04ef092e8dbcdb91703b4
lougehrig10 <lougehrig10@gmail.com>
2019-01-18 10:29:37 -0600

    Update to version 1.4

--- Formula/fff.rb
+++ Formula/fff.rb
@@ -1,8 +1,8 @@
 class Fff < Formula
   desc "Fast file-manager (written in bash)"
   homepage "https://github.com/dylanaraps/fff"
-  url "https://github.com/dylanaraps/fff/archive/1.2.tar.gz"
-  sha256 "a77095950c2fe1eca82c12c41273ed987e295e522c7e85365192f9056e4f80d8"
+  url "https://github.com/dylanaraps/fff/archive/1.4.tar.gz"
+  sha256 "1a9d0f2ca1920d0ade41c91f27ca5af0f114b35d438cf67b87d8bfb6f6453bda"
 
   def install
     bin.install "fff"
