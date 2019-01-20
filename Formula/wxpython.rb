0b77de37bc1f17a66c2019278599f9758764a263
Davide `rainbow` Gerhard <rainbow@irh.it>
2019-01-20 19:07:21 +0100

    wxpython: update wxmac to 3.1.2 so bump up revision
    Signed-off-by: Davide `rainbow` Gerhard <rainbow@irh.it>

--- Formula/wxpython.rb
+++ Formula/wxpython.rb
@@ -3,7 +3,7 @@ class Wxpython < Formula
   homepage "https://www.wxwidgets.org/"
   url "https://downloads.sourceforge.net/project/wxpython/wxPython/3.0.2.0/wxPython-src-3.0.2.0.tar.bz2"
   sha256 "d54129e5fbea4fb8091c87b2980760b72c22a386cb3b9dd2eebc928ef5e8df61"
-  revision 1
+  revision 2
 
   bottle do
     cellar :any
