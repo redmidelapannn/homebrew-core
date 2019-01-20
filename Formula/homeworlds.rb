95ca349de7b9b5882270bcef9de494f876589b4f
Davide `rainbow` Gerhard <rainbow@irh.it>
2019-01-20 19:05:34 +0100

    homeworlds: update wxmac to 3.1.2 so bump up revision
    Signed-off-by: Davide `rainbow` Gerhard <rainbow@irh.it>

--- Formula/homeworlds.rb
+++ Formula/homeworlds.rb
@@ -4,6 +4,7 @@ class Homeworlds < Formula
   url "https://github.com/Quuxplusone/Homeworlds.git",
     :revision => "917cd7e7e6d0a5cdfcc56cd69b41e3e80b671cde"
   version "20141022"
+  revision 1
 
   bottle do
     cellar :any
