53dad78041a26ae6ff0a6ee6343b7cbcee45fc8c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-20 10:24:36 +0000

    libpulsar: update 2.2.1 bottle.

--- Formula/libpulsar.rb
+++ Formula/libpulsar.rb
@@ -6,9 +6,10 @@ class Libpulsar < Formula
 
   bottle do
     cellar :any
-    sha256 "63b0d3fcf20e333a2fe5d60fddd103de2597156e1339aa7450d157341de19dbf" => :mojave
-    sha256 "58d7ece9b66f7ec7b7a3874e721fba493b7b18f0dfc2b5e12202d45c1a36c527" => :high_sierra
-    sha256 "a4fb175e3280ca1f3e40befdf8568e3f43191c093bafe90aa30d743ce6ef5dc1" => :sierra
+    rebuild 1
+    sha256 "d5889a6b1b18368d035844401e9f71b2ef54544a37059f13249fa330a3f4410c" => :mojave
+    sha256 "1cbfce37675dd459e7ee2c12ac3d244e18c584727c9aef04c68e259b857a6a55" => :high_sierra
+    sha256 "21e078c82058d72f6cc48421fd1d4bca19ff348ebdc25cd7258e0b5b6e2feaa5" => :sierra
   end
 
   depends_on "cmake" => :build
