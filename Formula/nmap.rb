bcc07aeaf2f69158f4ded8a96a0062a0e69f41c0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-21 21:39:04 +0000

    nmap: update 7.70 bottle.

--- Formula/nmap.rb
+++ Formula/nmap.rb
@@ -6,10 +6,10 @@ class Nmap < Formula
   head "https://svn.nmap.org/nmap/"
 
   bottle do
-    sha256 "9a9bfb7842cb631f4d48384e7f0624540c109c1fbf16dc1df3a2bab521392f61" => :mojave
-    sha256 "ef7ef98c6b83c013727eea37c37dcfa04eb6a572dc03699920cd7fc76a7f358a" => :high_sierra
-    sha256 "a39669b4c391823e7f42407654475539d7b4b58bc343817c6bfb96bc4063e848" => :sierra
-    sha256 "a597fa10396be4a782a198f4af51565c15dc8ae59cbe8c367bb78fd3babd972e" => :el_capitan
+    rebuild 1
+    sha256 "b90ab9ae74530d87292cbd89a67572719a1650daad1f3c279308986332c39bf6" => :mojave
+    sha256 "2f4245aa40ab11d77e3ef9cf7496465258aa1bc599cc8bc77f4986c38cbce4d2" => :high_sierra
+    sha256 "91f5fd86e43316ca9306ece64978a62d7c1ce76bf5e0d5e7850290b099d79cd3" => :sierra
   end
 
   depends_on "openssl"
