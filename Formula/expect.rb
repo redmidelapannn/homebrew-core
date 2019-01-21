29c799f4ae0369722e2af089088c7fd867beb9b0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-21 16:05:59 +0000

    expect: update 5.45.4 bottle.

--- Formula/expect.rb
+++ Formula/expect.rb
@@ -5,11 +5,10 @@ class Expect < Formula
   sha256 "49a7da83b0bdd9f46d04a04deec19c7767bb9a323e40c4781f89caf760b92c34"
 
   bottle do
-    rebuild 1
-    sha256 "668b4fb12eed5bbf783e8b4ec52dad24b88f38af5577ba1e45ed9947e50e50ef" => :mojave
-    sha256 "a0c6ffe797dc0bbe512b628819acee67a7a9b00573b6433fe0672285d41a9df1" => :high_sierra
-    sha256 "fc9ad781caaf8d45f47a87d4303645faa2e600852c73fd5432f0be2e588e95f2" => :sierra
-    sha256 "4fcb163b1b1e7e209b632c43ba03106ca1c4e4d6a745260b813d28a803581e58" => :el_capitan
+    rebuild 2
+    sha256 "aae3ed7d25f45423ca616745a51665d2f82c70af497fb11e2508ef3a78539b2d" => :mojave
+    sha256 "0fefb45893954c2716e3b7ec11448b2695d57f438ab0ed76ebc2974fc6b6a796" => :high_sierra
+    sha256 "cc2fd564861b4055ed131588a9295abd6fd3c24f0ef7e8b4ae77815ee0455d08" => :sierra
   end
 
   # Autotools are introduced here to regenerate configure script. Remove
