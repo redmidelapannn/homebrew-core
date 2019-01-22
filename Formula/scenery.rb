c88cd2f5d03f4005fc08021b5abe8ac7765b03df
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-22 01:56:52 +0000

    scenery: add 0.1.1 bottle.

--- Formula/scenery.rb
+++ Formula/scenery.rb
@@ -4,6 +4,13 @@ class Scenery < Formula
   url "https://github.com/dmlittle/scenery/archive/v0.1.1.tar.gz"
   sha256 "ae1c662ebe70c63baa3ddd8c0875fd2d132e3d0511619257c60369fd2ea18893"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6c423ae3db081123d2c904a6bedc32a2c1a20bb27cafbfcc6571135bd2fc7ab1" => :mojave
+    sha256 "966199caeffe4a6dcebffc7076c48f6be2084ae6aee3446741e86118be74e8d1" => :high_sierra
+    sha256 "0f4d033b2b694b3d4bf276983c5409f84470dc06af418cf2800d134ab9b9096b" => :sierra
+  end
+
   depends_on "dep" => :build
   depends_on "go" => :build
 
