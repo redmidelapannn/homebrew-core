abf5a7f5326914a285864be2c6ca4ca23bb4cadf
FX Coudert <fxcoudert@gmail.com>
2019-01-22 22:48:04 +0100

    Update bluetoothconnector.rb

--- Formula/bluetoothconnector.rb
+++ Formula/bluetoothconnector.rb
@@ -1,5 +1,5 @@
 class Bluetoothconnector < Formula
-  desc "Simple CLI to connect/disconnect Bluetooth devices"
+  desc "Connect and disconnect Bluetooth devices"
   homepage "https://github.com/lapfelix/BluetoothConnector"
   url "https://github.com/lapfelix/BluetoothConnector/archive/1.1.1.tar.gz"
   sha256 "8ea61b26ebab18b6ee8b8960d016d41f514d26ef9a7b4452c467017ab1859056"
@@ -13,8 +13,8 @@ class Bluetoothconnector < Formula
 
   test do
     output = shell_output("#{bin}/BluetoothConnector")
-    assert_match /Usage: BluetoothConnector/, output
-    assert_match /Get the MAC address from the list below/, output
+    assert_match "Usage: BluetoothConnector", output
+    assert_match "Get the MAC address from the list below", output
 
     output_fail = shell_output("#{bin}/BluetoothConnector --connect 00-00-00-00-00-00", 252)
     assert_equal "Not paired to device\n", output_fail
