cd8f2d91f058f913aa7136f57f0094b92ccd3e83
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-22 16:22:50 +0000

    xapian: update 1.4.7 bottle.

--- Formula/xapian.rb
+++ Formula/xapian.rb
@@ -7,10 +7,10 @@ class Xapian < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "424ac432e892cfc455f6fcd8ae7608b44d76d39a73361b3b56d143ac30921b8c" => :mojave
-    sha256 "32e3b96b619d8f39e6f2d5a32fb0f9b9fb0e1378fcfdbb2a163eb549798e10e1" => :high_sierra
-    sha256 "adfbefe2380111b93f1ebce01311bf8a1d07f91715e90b33134457604772d41d" => :sierra
+    rebuild 2
+    sha256 "e4581faff92db58dde7c348d92604999d675cd51c69fdbd3a4f13ef0f7831165" => :mojave
+    sha256 "a326ccaba91a0b95abb04b64c08e1e06e8fca4daebacfa867ff82470fe1831d6" => :high_sierra
+    sha256 "cfbe65900073b4e95b2d43502168fd13453f7b1efc8c488149ec7cffc96f6750" => :sierra
   end
 
   skip_clean :la
