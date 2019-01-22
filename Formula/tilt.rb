23b4e6c75a0fdf30165c23b3c0f5b90953c359d9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-22 19:56:53 +0000

    tilt: add 0.5.1 bottle.

--- Formula/tilt.rb
+++ Formula/tilt.rb
@@ -4,6 +4,13 @@ class Tilt < Formula
   url "https://github.com/windmilleng/tilt/archive/v0.5.1.tar.gz"
   sha256 "a5f33bbdaca92a7842f5f056fe30260cbb9412deca71634a81596dd981ce5399"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e21dc9a566ecbd029370dcbad42da6dba647b9411b82c07a99aa9cba380e1991" => :mojave
+    sha256 "010bf3d42b9fc9f7a3780e261b27153d3c9f2ec009cf20f21e1427843f0e2d5c" => :high_sierra
+    sha256 "8da1ed0450f1be2a904d320013421b5e30e07887096daaa6a2dbaf769e3a6851" => :sierra
+  end
+
   depends_on "go" => :build
   depends_on "docker"
   depends_on "kubernetes-cli"
