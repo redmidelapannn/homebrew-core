164264fb2a10ccbf522dfb72c5bc17c0f56e8e93
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-23 10:32:35 +0000

    gnumeric: update 1.12.43 bottle.

--- Formula/gnumeric.rb
+++ Formula/gnumeric.rb
@@ -5,10 +5,9 @@ class Gnumeric < Formula
   sha256 "87c9abd6260cf29401fa1e0fcce374e8c7bcd1986608e4049f6037c9d32b5fd5"
 
   bottle do
-    sha256 "1317c0b0a36c781ea587b3411a2b7c7ccff9206fed085e002e4bbef339130e99" => :mojave
-    sha256 "f4a007bcb9422d69d6217c08f9af877ed61cdf51d919fc88eb6d905c1aedaf3f" => :high_sierra
-    sha256 "d7db9d5d98fb58473c411d22683e8de5e73fe6e965dae5bdbd8d3fd243a59826" => :sierra
-    sha256 "c987c86ec64c80322d7f602234ca1e3e144889bf72d6144f9d1b5ffa76879ec8" => :el_capitan
+    rebuild 1
+    sha256 "c4d85b0bcff851f947af78f11be6a61a20e2b006e86132845809ad7019bab8dc" => :high_sierra
+    sha256 "b83ac26834bdba5857ff7d824126e698350798508d693caf4d6437171e94bd49" => :sierra
   end
 
   depends_on "intltool" => :build
