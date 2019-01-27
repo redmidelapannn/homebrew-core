60cf94ec20d8892279a751a133c6158b8a1f165f
ray851107 <16625236+ray851107@users.noreply.github.com>
2019-01-27 18:23:42 +0800

    mblaze 0.4 (new formula)

---
+++ Formula/mblaze.rb
@@ -0,0 +1,26 @@
+class Mblaze < Formula
+  desc "Maildir-focused command-line mail client"
+  homepage "https://github.com/chneukirchen/mblaze"
+  url "https://github.com/chneukirchen/mblaze/archive/v0.4.tar.gz"
+  sha256 "83dffd7fe74fa871642e97db94e27c04506f746fa4d6ba2f95c1a880638d6e0c"
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
+
+    bin.install "contrib/mencrypt"
+    bin.install "contrib/menter"
+    bin.install "contrib/mgpg"
+    bin.install "contrib/mp7m"
+    bin.install "contrib/mraw"
+    bin.install "contrib/mrecode"
+    bin.install "contrib/msign"
+    bin.install "contrib/mverify"
+    bin.install "contrib/mvi"
+  end
+
+  test do
+    maildir = testpath/"maildir"
+    system "#{bin}/mmkdir", maildir
+    assert_equal maildir.to_s, shell_output("#{bin}/mdirs #{testpath}").strip
+  end
+end
