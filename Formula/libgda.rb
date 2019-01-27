2bb37312a5ba6dc2700f46a480c7dc078f8b584a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-27 18:25:37 +0000

    libgda: update 5.2.8_2 bottle.

--- Formula/libgda.rb
+++ Formula/libgda.rb
@@ -6,9 +6,7 @@ class Libgda < Formula
   revision 2
 
   bottle do
-    sha256 "31bc1f2cecf14e6a2d751f257a09426d5f00ef0c46910154fe2ec1180dbe059e" => :mojave
-    sha256 "04afe02f130d01741467427001c3fa1f6a0dbd09767653ea5eccbd8326421471" => :high_sierra
-    sha256 "0e770b424256d46f816041357aa27bef22ab5366133e6b20fe685793ca3bd49e" => :sierra
+    sha256 "df387af481a7b0fa56935ab5aa257e9efc4c657905c0a26819e70a0a6f524695" => :high_sierra
   end
 
   depends_on "gobject-introspection" => :build
