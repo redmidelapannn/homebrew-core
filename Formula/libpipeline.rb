3f0b4e51087862dcf9ad0b32cf9127e8f8d6a4de
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-29 21:29:38 +0000

    libpipeline: add 1.5.1 bottle.

--- Formula/libpipeline.rb
+++ Formula/libpipeline.rb
@@ -4,6 +4,13 @@ class Libpipeline < Formula
   url "https://download.savannah.nongnu.org/releases/libpipeline/libpipeline-1.5.1.tar.gz"
   sha256 "d633706b7d845f08b42bc66ddbe845d57e726bf89298e2cee29f09577e2f902f"
 
+  bottle do
+    cellar :any
+    sha256 "ab420abd51374fa093179566ed0fca4cfe83306cedd9ec3027f22d42a7e7e066" => :mojave
+    sha256 "595a6803077f7f30268523cd8c8d0b9dc3d1ef26d4939a1fd247d4ee4b193333" => :high_sierra
+    sha256 "2fec168915b15541fc44e0c0faee8e48c4d78e269d93496e2a6ed7f249ddf780" => :sierra
+  end
+
   depends_on "pkg-config"
 
   def install
