5caafeff12dd0e379495c1c417e9d9b2e5115f2d
Sameer Naik <sameer@bitnami.com>
2019-01-29 12:28:19 +0530

    Improve test coverage of compiled binary
    Signed-off-by: Sameer Naik <sameer@bitnami.com>

--- Formula/kubeprod.rb
+++ Formula/kubeprod.rb
@@ -21,5 +21,50 @@ class Kubeprod < Formula
   test do
     version_output = shell_output("#{bin}/kubeprod version")
     assert_match "Installer version: v#{version}", version_output
+
+    (testpath/"kube-config").write <<~EOS
+      apiVersion: v1
+      clusters:
+      - cluster:
+          certificate-authority-data: test
+          server: http://127.0.0.1:8080
+        name: test
+      contexts:
+      - context:
+          cluster: test
+          user: test
+        name: test
+      current-context: test
+      kind: Config
+      preferences: {}
+      users:
+      - name: test
+        user:
+          token: test
+    EOS
+
+    authzDomain = "castle-black.com"
+    project = "white-walkers"
+    oauthClientId = "jon-snow"
+    oauthClientSecret = "king-of-the-north"
+    contactEmail = "jon@castle-black.com"
+
+    ENV["KUBECONFIG"] = testpath/"kube-config"
+    system "#{bin}/kubeprod", "install", "gke",
+              "--authz-domain", "#{authzDomain}",
+              "--project", "#{project}",
+              "--oauth-client-id", "#{oauthClientId}",
+              "--oauth-client-secret", "#{oauthClientSecret}",
+              "--email", "#{contactEmail}",
+              "--only-generate"
+
+    assert File.file? "kubeprod-autogen.json"
+    assert_match "\"authz_domain\": \"#{authzDomain}\"", File.read("kubeprod-autogen.json")
+    assert_match "\"client_id\": \"#{oauthClientId}\"", File.read("kubeprod-autogen.json")
+    assert_match "\"client_secret\": \"#{oauthClientSecret}\"", File.read("kubeprod-autogen.json")
+    assert_match "\"contactEmail\": \"#{contactEmail}\"", File.read("kubeprod-autogen.json")
+
+    assert File.file? "kubeprod-manifest.jsonnet"
+    assert_match "https://releases.kubeprod.io/files/v#{version}/manifests/platforms/gke.jsonnet", File.read("kubeprod-manifest.jsonnet")
   end
 end
