fc745a19277707b787617976ba91220379e0233c
Norbert Preining <norbert@preining.info>
2019-01-30 06:19:33 +0900

    fixes for audit

--- Formula/cafeobj.rb
+++ Formula/cafeobj.rb
@@ -1,7 +1,7 @@
 class Cafeobj < Formula
   desc "New generation algebraic specification and programming language"
   homepage "https://cafeobj.org/"
-  url "http://cafeobj.org/files/1.5.9/cafeobj-1.5.9.tar.gz"
+  url "https://cafeobj.org/files/1.5.9/cafeobj-1.5.9.tar.gz"
   sha256 "a4085e9ee060a8a0a22cb7c522c17aa1ccadb5bdce8f90085e08ded3794498d4"
 
   depends_on "sbcl"
@@ -12,6 +12,6 @@ class Cafeobj < Formula
   end
 
   test do
-    system "cafeobj", "-batch"
+    system "#{bin}/cafeobj", "-batch"
   end
 end
