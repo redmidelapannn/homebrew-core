270cfdb1504948a6ba9f0fc78f116685d2d36909
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-30 21:06:02 +0000

    ucon64: update 2.1.0 bottle.

--- Formula/ucon64.rb
+++ Formula/ucon64.rb
@@ -6,10 +6,10 @@ class Ucon64 < Formula
   head "https://svn.code.sf.net/p/ucon64/svn/trunk/ucon64"
 
   bottle do
-    sha256 "84609c8e92dae09a76f12eebe5c19d1769eb22d28029db5d15c14949800c358f" => :mojave
-    sha256 "3fc97894b8edaa28fa90a2cb494976be49fa7a28fb875797bd5f56ddc3b2fd69" => :high_sierra
-    sha256 "6fe6e1cf1db14e4592eb3486c6629bff06813a42c34b9ae8e188a5ad02745344" => :sierra
-    sha256 "015cfd4803ea86f02429fc9a1f5de59d72012c9d0e32a92c9f996922ef422084" => :el_capitan
+    rebuild 1
+    sha256 "c205b44f1ed2e3c1eef8e5d7b1e57caecb9078a7e9869f28d6ee5923b06bd933" => :mojave
+    sha256 "c46de103a88159ca01cb4d55145944a1617dc369249bda5ba0538790fed3506b" => :high_sierra
+    sha256 "3a188c1dcdd6b81cffde6713c7d407f07d392b3606849d5fae9bc3b8fce6c8ea" => :sierra
   end
 
   resource "super_bat_puncher_demo" do
