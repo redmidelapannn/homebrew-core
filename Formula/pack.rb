ab3273418079c30af4768ea5f2cd4198364162c2
lucperkins <lucperkins@gmail.com>
2019-01-30 08:27:47 -0800

    Pack 0.0.9 (new formula)

---
+++ Formula/pack.rb
@@ -0,0 +1,11 @@
+class Pack < Formula
+  desc "A CLI tool for building applications with Cloud Native Buildpacks"
+  homepage "https://buildpacks.io"
+  url "https://github.com/buildpack/pack/releases/download/v0.0.9/pack-0.0.9-macos.tar.gz"
+  sha256 "6b8ed842d91ddff35f6aa96fefc4ecde767a4ee7a3bdc772eba0a14c123cd47b"
+  head "https://github.com/buildpack/pack.git"
+
+  def install
+    bin.install "pack"
+  end
+end
