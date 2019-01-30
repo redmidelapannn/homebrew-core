21bb7b3a3135c28270d1c1bdb671bf873d100539
ober <jaimef@linbsd.org>
2019-01-30 14:33:55 -0800

    Bump GambitScheme to 4.9.2

--- Formula/gambit-scheme.rb
+++ Formula/gambit-scheme.rb
@@ -1,14 +1,8 @@
 class GambitScheme < Formula
   desc "Gambit Scheme is an implementation of the Scheme Language"
   homepage "http://gambitscheme.org"
-  url "https://github.com/gambit/gambit/archive/v4.9.1.tar.gz"
-  sha256 "667ae2ee657c22621a60b3eda6e242224d41853adb841e6ff9bc779f19921c18"
-
-  bottle do
-    sha256 "61c261221bd5f04afff4c3c5a88d0dd34674f58052c6a958f97179cd060a46cd" => :mojave
-    sha256 "667d14dc806cccc5170dbba686dea5b8042fe940edd372696296d06e3ba88b52" => :high_sierra
-    sha256 "8b77b5315032fce450b7e6b39e05533817fdb9995351f083f358a1ec1e5f6e06" => :sierra
-  end
+  url "https://github.com/gambit/gambit/archive/v4.9.2.tar.gz"
+  sha256 "237aa7c4b4e020d7fdc70481aab54dbe3d72e7551a490ae93b2bc45d4638688b"
 
   depends_on "openssl"
 
@@ -28,7 +22,13 @@ class GambitScheme < Formula
     system "make", "install"
   end
 
+  def caveats; <<~EOS
+    Due to conflicts with other packages. (gsi from ghostscript)
+    Please ensure to add /usr/local/opt/gambit-scheme-ssl/current/bin to your PATH.
+  EOS
+  end
+
   test do
-    assert_equal "0123456789", shell_output("#{prefix}/v4.9.1/bin/gsi -e \"(for-each write '(0 1 2 3 4 5 6 7 8 9))\"")
+    assert_equal "0123456789", shell_output("#{prefix}/current/bin/gsi -e \"(for-each write '(0 1 2 3 4 5 6 7 8 9))\"")
   end
 end
