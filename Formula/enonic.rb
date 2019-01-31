86ee1af464d6486bcd4f61c8104c538e7c7f05de
Pavel Milkevich <pavel.milkevich@gmail.com>
2019-01-31 14:34:19 +0300

    enonic 0.3 build fixes

--- Formula/enonic.rb
+++ Formula/enonic.rb
@@ -1,11 +1,15 @@
 class Enonic < Formula
-  desc "Enonic XP command line interface."
-  homepage "http://enonic.com/"
-  url "http://repo.enonic.com/public/com/enonic/cli/enonic/0.3/enonic_0.3_Mac_64-bit.tar.gz"
+  desc "Command-line interface for Enonic XP"
+  homepage "https://enonic.com/"
+  url "https://repo.enonic.com/public/com/enonic/cli/enonic/0.3/enonic_0.3_Mac_64-bit.tar.gz"
   version "0.3"
-  sha256 "54611984883d9c6c04124b41a0d29d2c226f06808c17722eaf6aa28187ea1ca5"
+  sha256 "9893534de4f97117ee5d2c2d960bcd4cbdbc28e847f76ceabbe9cf72153cfdac"
 
   def install
     bin.install "enonic"
+  end
+
+  test do
+    assert_match "Enonic CLI version #{version}", pipe_output("#{bin}/enonic -v")
   end
 end
