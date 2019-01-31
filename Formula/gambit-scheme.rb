a7a67fb9fbdb8b12f8c980597fc42c87239f24af
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-01-31 01:35:32 +0000

    gambit-scheme: add 4.9.2 bottle.

--- Formula/gambit-scheme.rb
+++ Formula/gambit-scheme.rb
@@ -4,6 +4,12 @@ class GambitScheme < Formula
   url "https://github.com/gambit/gambit/archive/v4.9.2.tar.gz"
   sha256 "237aa7c4b4e020d7fdc70481aab54dbe3d72e7551a490ae93b2bc45d4638688b"
 
+  bottle do
+    sha256 "03c1c5fb7a51938b0ceb7fe6bec144e2bfde05a7830407b10beda3cba362c1f3" => :mojave
+    sha256 "0741bdbc2d20a4f0ec4d1ea899c4c9652ebf7fee07df4d4e7f2a5b0ec5790aad" => :high_sierra
+    sha256 "8c571925f1dd6513b7481b9d6e0c057e1c108d2e358836f8c2761108a4856449" => :sierra
+  end
+
   depends_on "openssl"
 
   def install
