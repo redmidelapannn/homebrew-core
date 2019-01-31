d5d32980568fe0b104aba7b4bd7741f884c6dbcd
Pavel Milkevich <pavel.milkevich@gmail.com>
2019-01-31 18:07:42 +0300

    enonic 0.3 updated latest binary hash

--- Formula/enonic.rb
+++ Formula/enonic.rb
@@ -3,7 +3,7 @@ class Enonic < Formula
   homepage "https://enonic.com/"
   url "https://repo.enonic.com/public/com/enonic/cli/enonic/0.3/enonic_0.3_Mac_64-bit.tar.gz"
   version "0.3"
-  sha256 "9893534de4f97117ee5d2c2d960bcd4cbdbc28e847f76ceabbe9cf72153cfdac"
+  sha256 "cce94ac4999bda7e69be2edadbeebd7f012aed64e9628d3b5681e5a4e0025026"
 
   def install
     bin.install "enonic"
