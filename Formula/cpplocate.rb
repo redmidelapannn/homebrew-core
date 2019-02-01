4e8dfc9aa66538c2dd669f806c860a273a0d2b26
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-01 23:12:05 +0000

    cpplocate: add 2.1.0 bottle.

--- Formula/cpplocate.rb
+++ Formula/cpplocate.rb
@@ -3,6 +3,13 @@ class Cpplocate < Formula
   homepage "https://cpplocate.org/"
   url "https://github.com/cginternals/cpplocate/archive/v2.1.0.tar.gz"
   sha256 "da70ff59e490dd05434db093b06bb0775582d859da691cd3d1a917abb24b5fc8"
+  bottle do
+    cellar :any
+    sha256 "73fb5f333e001a791dbf556fea8f1f308fd2281577f09bf5aeb3f8c15eb299ca" => :mojave
+    sha256 "47d612a728c36b27e5fac1cd68b133951a7778205e772639c391fd9a4ea0d4d4" => :high_sierra
+    sha256 "dfaffefcfcabdfe690b117426be7d9317f8fdf1066ef2d1fb13f2c1e576e03b6" => :sierra
+  end
+
   
   depends_on "cmake" => :build
 
