ff42998367a23900eabea38b0afe0351913311c5
Jeffry Hesse <jhesse@sonatype.com>
2019-01-31 15:57:05 -0900

    Remove HEAD

--- Formula/nancy.rb
+++ Formula/nancy.rb
@@ -4,21 +4,19 @@ class Nancy < Formula
     url "https://github.com/sonatype-nexus-community/nancy.git",
     :tag      => "0.0.1",
     :revision => "0ddc0132e8b231d26d25d4eb71d9f273e4b01e3e"
-    head "https://github.com/sonatype-nexus-community/nancy.git"
-  
+
     depends_on "go"
-  
+
     def install
-      ENV["GO_PATH"] = buildpath
-      bin_path = buildpath/"src/github.com/sonatype-nexus-community/nancy"
-      bin_path.install Dir["*"]
-      cd bin_path do
+        ENV["GO_PATH"] = buildpath
+        bin_path = buildpath/"src/github.com/sonatype-nexus-community/nancy"
+        bin_path.install Dir["*"]
+        cd bin_path do
         system "go", "build", "-o", bin/"nancy", "."
-      end
+        end
     end
-  
+
     test do
-      assert_match "0.0.1", shell_output("#{bin}/nancy -version")
+        assert_match "0.0.1", shell_output("#{bin}/nancy -version")
     end
-  end
-  
+end
