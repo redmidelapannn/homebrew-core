5763b746c671372a024e1b8a126f611c4cd2aa3f
Michael Stringer <stringbean@users.noreply.github.com>
2019-02-01 11:07:38 +0000

    async-profiler 1.5 (new formula)

---
+++ Formula/async-profiler.rb
@@ -0,0 +1,24 @@
+class AsyncProfiler < Formula
+  desc "Sampling CPU and HEAP profiler for Java"
+  homepage "https://github.com/jvm-profiling-tools/async-profiler"
+  url "https://github.com/jvm-profiling-tools/async-profiler/archive/v1.5.tar.gz"
+  sha256 "ffe8281f745a8e55bf9485c9ed9d30e5c4934174275cdb73b32e47cde35fffdd"
+
+  depends_on :java
+
+  def install
+    system "make"
+
+    libexec.install "build", "profiler.sh"
+
+    (bin/"async-profiler").write <<~EOS
+      #!/bin/bash
+      "#{Formula["async-profiler"].libexec}/profiler.sh" "$@"
+    EOS
+    chmod 0555, bin/"async-profiler"
+  end
+
+  test do
+    system bin/"async-profiler", "-v"
+  end
+end
