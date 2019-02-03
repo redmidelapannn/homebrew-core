8381dc927ebd2147420b01e598bae568609e3009
lougehrig10 <lougehrig10@gmail.com>
2019-02-03 17:02:05 -0600

    Fixed formatting as well

--- Formula/fff.rb
+++ Formula/fff.rb
@@ -1,12 +1,12 @@
 class Fff < Formula
-  desc "a simple file manager written in bash"
+  desc "Simple file manager written in bash"
   homepage "https://github.com/dylanaraps/fff"
   url "https://github.com/dylanaraps/fff/archive/2.0.tar.gz"
   sha256 "a202bde184724239786ec93ccced8054e9d5788239312ef91d74593469ebb10a"
 
   def install
-  bin.install "fff"
-  man1.install "fff.1"
+    bin.install "fff"
+    man1.install "fff.1"
   end
 
   test do
