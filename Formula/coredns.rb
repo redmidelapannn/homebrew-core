6620a31dabe04e327f104fbc597a90186a39b712
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-03 09:37:11 +0000

    coredns: add 1.3.1 bottle.

--- Formula/coredns.rb
+++ Formula/coredns.rb
@@ -5,6 +5,13 @@ class Coredns < Formula
     :tag => "v1.3.1",
     :revision => "6b56a9c92130d50cee9bd92aaee500dbccff395f"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1a6e45cbbbca8d49140f0932b630752c1b7c829297dd50ed9d18bbd5a9b9b4f6" => :mojave
+    sha256 "b45397c250dbdbbea33936ed1b1185dbaaad50cbd1adfe998441a000336ac25b" => :high_sierra
+    sha256 "cc2418cd639ffd34987a5887dd8c82b3d7f633c6351125646f790d780662c621" => :sierra
+  end
+
   depends_on "dep" => :build
   depends_on "go" => :build
 
