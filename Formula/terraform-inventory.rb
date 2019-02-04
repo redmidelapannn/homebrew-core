a53d7f254693d9b64b26579ce67e2a047d7fa9d1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-04 15:31:16 +0000

    terraform-inventory: update 0.6.1 bottle.

--- Formula/terraform-inventory.rb
+++ Formula/terraform-inventory.rb
@@ -8,12 +8,10 @@ class TerraformInventory < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "29b5351cf96762d28878a118d6bd30d3165985dbc6e6fa5115f44bdbad0cc776" => :mojave
-    sha256 "15e8799c163c87affb5f4a48ffbb3e1f0392f837df4fba1db174088fe8159c6b" => :high_sierra
-    sha256 "6c423700ad3cb2a27295cbbca5de08a5a47b3f42a7c9cc53abfe9c3d1061b6b3" => :sierra
-    sha256 "d876b7acee1bfaaeaaf284bfd8518195b10005f7de504c8aaf39be3164b93dac" => :el_capitan
-    sha256 "77b4cfd96088f6019ad5bd0f0faf6757d1fb8c34ea3b327490ff33fde03538c1" => :yosemite
-    sha256 "19fed440ba3c95ef85fcd529f151131136fa684c9a091d17177ac8f74426e7d5" => :mavericks
+    rebuild 1
+    sha256 "907ed35425cbd31c9e71eb93059484bdf1f0983a359dee0d32ce4a1b0cd8bd72" => :mojave
+    sha256 "728a0cbfe7421c634c7488fce9d08f3405e421d0b4eeb9c21492f7bc8546ace4" => :high_sierra
+    sha256 "88066750ed6f5e4fcca430e9feb931ce15a9ef3889ac583d06e36d7b736bbab1" => :sierra
   end
 
   head do
