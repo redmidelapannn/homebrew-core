bffb57a7a468e387ed21408c4cfd2f97608e3ba5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-05 15:10:08 +0000

    mboxgrep: update 0.7.9 bottle.

--- Formula/mboxgrep.rb
+++ Formula/mboxgrep.rb
@@ -6,12 +6,10 @@ class Mboxgrep < Formula
 
   bottle do
     cellar :any
-    sha256 "93f800f8bae502815c85bac9dd2d7ec85599caa1ee08b6a4860ca8db11ab7276" => :mojave
-    sha256 "431d64ae6c1387c69b052942ee6b268ac9afbcd5bfff9d02bc21aeeaaa9807dd" => :high_sierra
-    sha256 "44a294d075cb08e577c4d1c1e45c222bea93f3c8488f9bed54ded5b36797f536" => :sierra
-    sha256 "ecc1d1a83a7ffbc3414feb24c970b89509737f7d1de5c0d8dbd71ba55e008220" => :el_capitan
-    sha256 "08cbb574005db6e34cc191ae4f46670ca35af252e4e6bbc2041a92e32397bc49" => :yosemite
-    sha256 "bb5cefa83e2fb8b37dac858f3119aedb338b29e0eb2715cbff08b4644689ad86" => :mavericks
+    rebuild 1
+    sha256 "54968a34daff4c8e0ab23051a51f464d6d9836cb55ab6020778ae7e8fe398780" => :mojave
+    sha256 "4790da274e8f711f2a1416772e0b095dbd6e7d18ca44293cf2c9e4b7515fe57c" => :high_sierra
+    sha256 "6e39c8a3d6830da568cd98b67ffd10b64c19291f4198fddab88deaccc729397d" => :sierra
   end
 
   depends_on "pcre"
