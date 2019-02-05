4930280991d2dfafb5dba7c368e40f2bb6b01aca
mabari <mabari@thoughtworks.com>
2019-02-05 13:02:56 +0530

    talisman v0.4.3

--- Formula/talisman.rb
+++ Formula/talisman.rb
@@ -1,8 +1,8 @@
 class Talisman < Formula
   desc "Detects secrets checked into git repository before commit or push"
   homepage "https://thoughtworks.github.io/talisman/"
-  url "https://github.com/thoughtworks/talisman/archive/v0.3.5.tar.gz"
-  sha256 "7c71d5e4a98a5061598fadc9155ee02ae27d0a85f912273360d987a65d2c7362"
+  url "https://github.com/thoughtworks/talisman/archive/v0.4.3.tar.gz"
+  sha256 "3aa40773885cbdab4ef5aa5cdc2465879cfe2d61e91619be4a2fb034fb001ae1"
 
   depends_on "go" => :build
   depends_on "gox" => :build
@@ -14,17 +14,22 @@ class Talisman < Formula
     talisman_path.install buildpath.children
 
     cd talisman_path do
-      arch = MacOS.prefer_64_bit? ? "amd64" : "386"
+      arch = "amd64"
       system "gox", "-osarch=darwin/#{arch}", "-ldflags=\"-X main.Version=v#{version}\"", "-output", bin/"talisman"
       bin.install "global_install_scripts/setup_talisman.bash"
-      bin.install "global_install_scripts/uninstall_talisman.bash"
-      onoe("The installation is not complete.")
-      ohai("Please run the following command to complete the installation of Talisman:\n")
-      ohai("\t\t /usr/local/Cellar/talisman/#{version}/bin/setup_talisman.bash\n")
+      bin.install "global_install_scripts/remove_hooks.bash"
     end
   end
 
+  def caveats
+    <<~EOS
+      \x1b[33mTalisman has been setup in your machine.
+      To configure your git repositories to use Talisman, please follow instructions at
+      https://github.com/thoughtworks/talisman
+    EOS
+  end
+
   test do
-    assert_equal "talisman Development Build\n", shell_output("#{bin}/talisman -v")
+    assert_equal "talisman Development Build\n", shell_output("#{bin}/talisman --v")
   end
 end
