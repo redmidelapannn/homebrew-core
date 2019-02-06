d61e75a488c16f5d9f5fbec6457cb1111852aa3a
lougehrig10 <lougehrig10@gmail.com>
2019-02-06 09:39:52 -0600

    Updated to version 2.1

--- Formula/fff.rb
+++ Formula/fff.rb
@@ -1,15 +1,15 @@
 class Fff < Formula
-  desc "Simple file manager written in bash"
+  desc "Simple file manager written in bash."
   homepage "https://github.com/dylanaraps/fff"
-  url "https://github.com/dylanaraps/fff/archive/2.0.tar.gz"
-  sha256 "a202bde184724239786ec93ccced8054e9d5788239312ef91d74593469ebb10a"
+  url "https://github.com/dylanaraps/fff/archive/2.1.tar.gz"
+  sha256 "776870d11c022fa40468d5d582831c0ab5beced573489097deaaf5dd690e7eab"
 
-  def install
-    bin.install "fff"
-    man1.install "fff.1"
-  end
+    def install
+      bin.install "fff"
+      man1.install "fff.1"
+    end
 
-  test do
-    system bin/"fff", "-v"
+    test do
+      system bin/"fff", "-v"
+    end
   end
-end
