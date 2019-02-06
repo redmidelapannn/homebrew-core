cddf0221d242fe5674f283ed26d5e53a6f08af86
Michael Goodness <mgoodness@gmail.com>
2019-02-06 17:06:13 -0600

    kustomize 2.0.0

--- Formula/kustomize.rb
+++ Formula/kustomize.rb
@@ -2,8 +2,8 @@ class Kustomize < Formula
   desc "Template-free customization of Kubernetes YAML manifests"
   homepage "https://github.com/kubernetes-sigs/kustomize"
   url "https://github.com/kubernetes-sigs/kustomize.git",
-      :tag      => "v1.0.11",
-      :revision => "8f701a00417a812558a7b785e8354957afa469ae"
+      :tag      => "v2.0.0",
+      :revision => "0184d5b69700607a1b9280f303152e2959bc19e7"
   head "https://github.com/kubernetes-sigs/kustomize.git"
 
   bottle do
