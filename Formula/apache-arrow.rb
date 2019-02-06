cb951426027d797977a1c7ed013f9249701e63cb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-06 01:08:23 +0000

    apache-arrow: add 0.12.0 bottle.

--- Formula/apache-arrow.rb
+++ Formula/apache-arrow.rb
@@ -4,6 +4,13 @@ class ApacheArrow < Formula
   url "https://archive.apache.org/dist/arrow/arrow-0.12.0/apache-arrow-0.12.0.tar.gz"
   sha256 "34dae7e4dde9274e9a52610683e78a80f3ca312258ad9e9f2c0973cf44247a98"
 
+  bottle do
+    cellar :any
+    sha256 "a9d9924c925b95727ed9b80c39754b6dbfb4e0440d92b17a14f55681913b06fc" => :mojave
+    sha256 "3c10303415e9aac2e97fcfe62ff4c8f4ce6c9f33da6732fc980e2532043aa7e3" => :high_sierra
+    sha256 "5b22da1e00749ca1844da837917efb9098959bdb9d75a28fb9527b738a4ea731" => :sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "python" => :optional
