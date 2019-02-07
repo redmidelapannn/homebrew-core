d6b1a7c42ad98ce0fad86b50fbbffd7b7cbb4ffa
Yurii Kolesnykov <root@yurikoles.com>
2019-02-07 19:38:15 +0200

    vulkan-headers: fix formatting

--- Formula/vulkan-headers.rb
+++ Formula/vulkan-headers.rb
@@ -3,6 +3,7 @@ class VulkanHeaders < Formula
   homepage "https://github.com/KhronosGroup/Vulkan-Headers"
   url "https://github.com/KhronosGroup/Vulkan-Headers/archive/v1.1.99.tar.gz"
   sha256 "37506deaf56b254193e4fa83195add2617e881efcac3d2ff5f0cf49353eb9aa8"
+
   depends_on "cmake" => :build
 
   def install
