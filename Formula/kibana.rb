fec3b9fba2adf7e0d65948fd0d333068fac58c3d
Tiago Costa <tiagoffcc@hotmail.com>
2019-02-08 05:11:03 +0000

    fix(na): preffix install dir glob.

--- Formula/kibana.rb
+++ Formula/kibana.rb
@@ -50,7 +50,8 @@ class Kibana < Formula
     system "yarn", "kbn", "bootstrap"
     system "yarn", "build", "--oss", "--release", "--skip-os-packages", "--skip-archives"
 
-    prefix.install Dir["build/oss/kibana-#{version}-darwin-x86_64/{[!node,!data,!plugins]**/*}"]
+    prefix.install Dir["build/oss/kibana-#{version}-darwin-x86_64/**/*"]
+                   .reject{ |f| File.fnmatch("build/oss/kibana-#{version}-darwin-x86_64/{node, data, plugins}/**/*", f) }
     mv "licenses/APACHE-LICENSE-2.0.txt", "LICENSE.txt" # install OSS license
 
     inreplace "#{bin}/kibana", %r{/node/bin/node}, "/libexec/node/bin/node"
