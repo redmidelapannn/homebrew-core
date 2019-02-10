00841a47e3ad76fd0e7dd075bac8297764df8a63
FX Coudert <fxcoudert@gmail.com>
2019-02-10 18:22:40 +0100

    Update mingw-w64.rb

--- Formula/mingw-w64.rb
+++ Formula/mingw-w64.rb
@@ -20,9 +20,9 @@ class MingwW64 < Formula
   depends_on "mpfr"
 
   resource "binutils" do
-    url "https://ftp.gnu.org/gnu/binutils/binutils-2.31.1.tar.gz"
-    mirror "https://ftpmirror.gnu.org/binutils/binutils-2.31.1.tar.gz"
-    sha256 "e88f8d36bd0a75d3765a4ad088d819e35f8d7ac6288049780e2fefcad18dde88"
+    url "https://ftp.gnu.org/gnu/binutils/binutils-2.32.tar.gz"
+    mirror "https://ftpmirror.gnu.org/binutils/binutils-2.32.tar.gz"
+    sha256 "9b0d97b3d30df184d302bced12f976aa1e5fbf4b0be696cdebc6cca30411a46e"
   end
 
   resource "gcc" do
