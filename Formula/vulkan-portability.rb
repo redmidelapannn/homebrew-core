c55e78efc03aa1161139413dd423cadb84703ae3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-11 17:57:13 +0000

    vulkan-portability: add 0.2 bottle.

--- Formula/vulkan-portability.rb
+++ Formula/vulkan-portability.rb
@@ -4,6 +4,13 @@ class VulkanPortability < Formula
   url "https://github.com/KhronosGroup/Vulkan-Portability/archive/v0.2.tar.gz"
   sha256 "2508a2dd0e111cb7d4636a54d454e7172ab33c48cba0e6e86e6fdb7cf48becb5"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "aa19c491dcca62db6bd6bd722f4e0d13b45f2aa88607d7ad4406beebdecd2d23" => :mojave
+    sha256 "aa19c491dcca62db6bd6bd722f4e0d13b45f2aa88607d7ad4406beebdecd2d23" => :high_sierra
+    sha256 "59b4732986e80478fee87789395d290b6df8ca1f228dde1e6db133b2babb68af" => :sierra
+  end
+
   depends_on "vulkan-headers" => :test
 
   def install
