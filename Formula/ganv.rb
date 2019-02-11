341e8b35210bdd230f5141f2d31caba90ea3477a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-11 16:48:36 +0000

    ganv: add 1.4.2 bottle.

--- Formula/ganv.rb
+++ Formula/ganv.rb
@@ -3,6 +3,13 @@ class Ganv < Formula
   homepage "https://drobilla.net/software/ganv"
   url "https://download.drobilla.net/ganv-1.4.2.tar.bz2"
 
+  bottle do
+    cellar :any
+    sha256 "65cd4f1d3d9a6d6412f77b0a3ed3169c8fad61044c76356f4c7599fdabb65606" => :mojave
+    sha256 "228e440a74f26e06499dd1e063d0afcf904d506843b837d14d92a14f980d8ead" => :high_sierra
+    sha256 "067ab2c1a423ae0b6d653fab34b5558a4141fbd7bdcd5846e8b52c0c5d400503" => :sierra
+  end
+
   depends_on "pkg-config" => [:build, :test]
   depends_on "glib"
   depends_on "glibmm"
