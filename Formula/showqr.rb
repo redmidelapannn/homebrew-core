9bcd78032e161ed0305d899d8451d6b4fd1a1434
Riccardo Bruno <ricsxn@hotmail.com>
2019-02-12 18:06:32 +0100

    Fixing audit

--- Formula/showqr.rb
+++ Formula/showqr.rb
@@ -1,11 +1,11 @@
 class Showqr < Formula
   desc "Show QR code from any selected text on your Mac"
-  homepage "http://ricsxn.github.io/ShowQR/"
+  homepage "https://ricsxn.github.io/ShowQR/"
   url "https://github.com/ricsxn/ShowQR/archive/1.3.1.tar.gz"
   sha256 "6eb4d14dca99b562d994ad8371ab7577443679e4ef8f54e8accaa946db5fd75f"
-  depends_on "python@2"
   depends_on "pipenv"
-  
+  depends_on "python@2"
+
   def install
     system "make", "install"
   end
