725f3b773ef34e1b38a82806eb50ead1ef976b69
Riccardo Bruno <ricsxn@hotmail.com>
2019-02-12 17:40:06 +0100

    ShowQR v1.3.1

---
+++ Formula/showqr.rb
@@ -0,0 +1,16 @@
+class Showqr < Formula
+  desc "Show QR code from any selected text on your Mac"
+  homepage "http://ricsxn.github.io/ShowQR/"
+  url "https://github.com/ricsxn/ShowQR/archive/1.3.1.tar.gz"
+  sha256 "6eb4d14dca99b562d994ad8371ab7577443679e4ef8f54e8accaa946db5fd75f"
+  depends_on "python@2"
+  depends_on "pipenv"
+  
+  def install
+    system "make", "install"
+  end
+
+  test do
+    system "make", "test"
+  end
+end
