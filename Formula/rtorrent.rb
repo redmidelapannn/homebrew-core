0833d9ce60542af2451105933025f2a0a8ff6757
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-15 23:15:36 +0000

    rtorrent: add 0.9.7 bottle.

--- Formula/rtorrent.rb
+++ Formula/rtorrent.rb
@@ -4,6 +4,12 @@ class Rtorrent < Formula
   url "https://github.com/rakshasa/rtorrent/releases/download/v0.9.7/rtorrent-0.9.7.tar.gz"
   sha256 "5d9842fe48c9582fbea2c7bf9f51412c1ccbba07d059b257039ad53b863fe8bb"
 
+  bottle do
+    cellar :any
+    sha256 "af2896d1457694c30e3d7ca1db6873bd9cade78327fa281942bbcb8e2879cee3" => :mojave
+    sha256 "82ad3b7e83a8f9fbe6bb87d31021816d9cb0583c991d003cc92325b7b40aac3d" => :sierra
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
