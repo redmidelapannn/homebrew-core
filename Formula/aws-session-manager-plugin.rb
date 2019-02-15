e3fc57a716ba2f0f2484800bae0b33bc3bc918c6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-15 18:18:57 +0000

    aws-session-manager-plugin: add 1.0.37.0 bottle.

--- Formula/aws-session-manager-plugin.rb
+++ Formula/aws-session-manager-plugin.rb
@@ -5,6 +5,13 @@ class AwsSessionManagerPlugin < Formula
   version "1.0.37.0"
   sha256 "4c04afb88081a23984cea5ca9fe2060045f8f4c96785e2771585b797631b6f16"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4befe7a18d211169b5c9b1e1f9462be46ffb4d7d4d0aa9d1bb649daecd651e39" => :mojave
+    sha256 "4befe7a18d211169b5c9b1e1f9462be46ffb4d7d4d0aa9d1bb649daecd651e39" => :high_sierra
+    sha256 "28f524d12e19a4f933476fb6bae4761e92a1dde27b6485f6378eb82b79bbf33e" => :sierra
+  end
+
   def install
     bin.install "bin/session-manager-plugin"
     prefix.install "seelog.xml.template"
