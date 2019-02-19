ac4e303c88b57de7b668eae825b5dfd240f98d8f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-19 08:52:28 +0000

    psu-cse-login: add 0.4 bottle.

--- Formula/psu-cse-login.rb
+++ Formula/psu-cse-login.rb
@@ -5,6 +5,13 @@ class PsuCseLogin < Formula
   sha256 "1b2c50060140f3cdcece908363dc429c7a259ca954eb433640019ec88359c53e"
   head "https://github.com/Seancheey/psu-cse-login.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ace13b3bc548bb6ac89b1cdc591fec4df726cc1026faaa93f48ccd57abdf0f59" => :mojave
+    sha256 "ace13b3bc548bb6ac89b1cdc591fec4df726cc1026faaa93f48ccd57abdf0f59" => :high_sierra
+    sha256 "0d21c65de77c5fe195673795237b6e8c779e860ad510195d5c7cce460a469f59" => :sierra
+  end
+
   def install
     system "make", "install", "PREFIX=#{prefix}"
   end
