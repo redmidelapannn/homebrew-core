2457776ed7bb012d55accbf45332e90bad8fe679
xykong <xy.kong@gmail.com>
2019-02-19 12:51:20 +0800

    frps: 0.24.1 (new formula)
    frps is the server of frp (https://github.com/fatedier/frp)

---
+++ Formula/frps.rb
@@ -0,0 +1,73 @@
+class Frps < Formula
+  desc "This is the server of frp (https://github.com/fatedier/frp)"
+  homepage "https://github.com/fatedier/frp"
+  url "https://github.com/fatedier/frp.git",
+      :tag      => "v0.24.1",
+      :revision => "cbf9c731a096ce33ec282ed913a25cb2e5f05303"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["XC_OS"] = "darwin"
+    ENV["XC_ARCH"] = "amd64"
+    ENV["GOPATH"] = buildpath
+    contents = Dir["{*,.git,.gitignore}"]
+    (buildpath/"src/github.com/fatedier/frp").install contents
+
+    (buildpath/"bin").mkpath
+    (etc/"frp").mkpath
+
+    cd "src/github.com/fatedier/frp" do
+      system "make", "frps"
+      bin.install "bin/frps"
+      etc.install "conf/frps.ini" => "frp/frps.ini"
+      etc.install "conf/frps_full.ini" => "frp/frps_full.ini"
+      prefix.install_metafiles
+    end
+  end
+
+  plist_options :manual => "frps -c #{HOMEBREW_PREFIX}/etc/frp/frps.ini"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>KeepAlive</key>
+        <true/>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/frps</string>
+          <string>-c</string>
+          <string>#{etc}/frp/frps.ini</string>
+        </array>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/frps.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/frps.log</string>
+      </dict>
+    </plist>
+  EOS
+  end
+
+  test do
+    system bin/"frps", "-v"
+    assert_match "Flags", shell_output("#{bin}/frps --help")
+
+    begin
+      read, write = IO.pipe
+      pid = fork do
+        exec bin/"frps", :out => write
+      end
+      sleep 1
+
+      output = read.gets
+      assert_match "frps tcp listen on", output
+    ensure
+      Process.kill(9, pid)
+      Process.wait(pid)
+    end
+  end
+end
