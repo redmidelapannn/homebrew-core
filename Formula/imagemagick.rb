7020045b62c7e95e408133427f9874e4487a4649
Chongyu Zhu <i@lembacon.com>
2019-02-19 18:57:50 +0800

    Update imagemagick.rb

--- Formula/imagemagick.rb
+++ Formula/imagemagick.rb
@@ -4,7 +4,8 @@ class Imagemagick < Formula
   # Please always keep the Homebrew mirror as the primary URL as the
   # ImageMagick site removes tarballs regularly which means we get issues
   # unnecessarily and older versions of the formula are broken.
-  url "https://www.imagemagick.org/download/ImageMagick-7.0.8-28.tar.xz"
+  url "https://dl.bintray.com/homebrew/mirror/ImageMagick-7.0.8-28.tar.xz"
+  mirror "https://www.imagemagick.org/download/ImageMagick-7.0.8-28.tar.xz"
   sha256 "d7aeb906b1c6b8d3292d4a8a94f6ac58683e25d972bbb8ebae8b300d8d707a1b"
   head "https://github.com/ImageMagick/ImageMagick.git"
 
