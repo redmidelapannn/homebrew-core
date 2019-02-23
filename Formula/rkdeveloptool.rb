75ddedbc4808bc4625d440afcb53e5f9b6eff6f8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-23 22:42:32 +0000

    rkdeveloptool: add 1.3 bottle.

--- Formula/rkdeveloptool.rb
+++ Formula/rkdeveloptool.rb
@@ -5,6 +5,13 @@ class Rkdeveloptool < Formula
   version "1.3"
   sha256 "c2c3a3cc325ccf840e4054d05a794144417dabed8725d9f1e66125b493124865"
 
+  bottle do
+    cellar :any
+    sha256 "94d91709830ad2ab02d7e1ee03e0b4dfb9bbde18f03410abb1f112436b0b6ffe" => :mojave
+    sha256 "6c4e7160b8581d8bbb19caec4c31aa766a58cd48352064a681cf11d31b36b080" => :high_sierra
+    sha256 "28e9ecf7292c9d18c5c943a1ef68ab34e91c4e17ee92b45662bebb90760a8ef7" => :sierra
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
