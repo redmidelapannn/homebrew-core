ff40d62288cd4c83cd4612de5c449054a5530997
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-23 00:16:29 +0000

    motion: add 4.2.2 bottle.

--- Formula/motion.rb
+++ Formula/motion.rb
@@ -3,6 +3,12 @@ class Motion < Formula
   homepage "https://motion-project.github.io/"
   url "https://github.com/Motion-Project/motion/archive/release-4.2.2.tar.gz"
   sha256 "c8d40976b41da8eb9f9f7128599403a312fc26b7226bf3787d75f78cb5a6cc6e"
+  bottle do
+    sha256 "8cfbb3887432d9978aa2cf5d2b6b56c5a11d90c9aea2cf9596bf9b17c0c197b8" => :mojave
+    sha256 "0b28d18b3966105ec17f6a4a1862d3a5b7c654f94fa69dcf88e2dc2aa8f0308f" => :high_sierra
+    sha256 "d3bcec7d8998ef1767ea4499116f2983e442f1f48a7037de81ce394a6a93b7a1" => :sierra
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "pkg-config" => :build
