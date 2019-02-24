7cf8e466690b16ad7abcbb017e09204b10fa1cf2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-02-24 20:59:02 +0000

    dhall-to-ssh-config: add 0.1.0.0 bottle.

--- Formula/dhall-to-ssh-config.rb
+++ Formula/dhall-to-ssh-config.rb
@@ -4,6 +4,12 @@ class DhallToSshConfig < Formula
   url "https://github.com/robertjlooby/dhall-ssh-config/archive/0.1.0.0.tar.gz"
   sha256 "6cbf72f80640d56d37e6c1ddc40b9c23082b8628027004e343cb7d62321c1a82"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "83523c43ad71aa7f4853a8683c411c2090bdd4fbec547965bdf6b29e0eb82c25" => :high_sierra
+    sha256 "be2dbf50c8a7715a15186192065abf880754eb1d0d2d2b2b140097fee6c004b8" => :sierra
+  end
+
   head do
     url "https://github.com/robertjlooby/dhall-ssh-config.git"
   end
