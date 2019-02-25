9796b1e6ebc83380798f116474614e27b43f9f6e
greyfenrir <grey.fenrir@gmail.com>
2019-02-25 10:23:12 +0300

    fix test url

--- Formula/bzt.rb
+++ Formula/bzt.rb
@@ -245,7 +245,7 @@ class Bzt < Formula
   end
 
   test do
-    cmd = "#{bin}/bzt -o execution.executor=nose -o execution.iterations=1 -o execution.scenario.requests.0=https://gettaurus.org/"
+    cmd = "#{bin}/bzt -o execution.executor=nose -o execution.iterations=1 -o execution.scenario.requests.0=http://gettaurus.org/"
     assert_match "INFO: Samples count: 1, 0.00% failures", shell_output(cmd)
   end
 end
