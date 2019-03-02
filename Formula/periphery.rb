b438b480c17646f72bee67b75c494645a80adcb7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-02 09:59:47 +0000

    periphery: update 1.4.0 bottle.

--- Formula/periphery.rb
+++ Formula/periphery.rb
@@ -5,6 +5,9 @@ class Periphery < Formula
   sha256 "a47920cde9584547d46c8b1b9207ba06577ecb2cb25b21dceaeb5cad8d55a82d"
 
   bottle do
+    cellar :any_skip_relocation
+    sha256 "c2decb5311abfd64fa92c739dd39455234d9ce992f9a38ec3d9f75929e39138c" => :mojave
+    sha256 "dfcd4f79eb4e6053c02e4c4d23c69e3c054aa09e9597397c7dbc3f02e87b685a" => :high_sierra
   end
 
   depends_on :xcode => "10.1"
