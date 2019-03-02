e13a6e3946bd30125f8bfcc9d6df56b7fa898a25
Ian Leitch <port001@gmail.com>
2019-03-02 10:39:35 +0100

    Remove head spec.

--- Formula/periphery.rb
+++ Formula/periphery.rb
@@ -3,7 +3,6 @@ class Periphery < Formula
   homepage "https://github.com/peripheryapp/periphery"
   url "https://github.com/peripheryapp/periphery/archive/1.4.0.tar.gz"
   sha256 "a47920cde9584547d46c8b1b9207ba06577ecb2cb25b21dceaeb5cad8d55a82d"
-  head "https://github.com/peripheryapp/periphery.git"
 
   bottle do
   end
