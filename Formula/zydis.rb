4bbbe339d258f1435aad92e5923df9c62de42fff
FX Coudert <fxcoudert@gmail.com>
2019-03-05 14:44:37 +0100

    Update zydis.rb

--- Formula/zydis.rb
+++ Formula/zydis.rb
@@ -1,5 +1,5 @@
 class Zydis < Formula
-  desc "Fast and lightweight x86/x86-64 disassembler library"
+  desc "Fast and lightweight x86/x86_64 disassembler library"
   homepage "https://zydis.re"
   url "https://github.com/zyantific/zydis/archive/v2.0.3.tar.gz"
   sha256 "9a49b179ee2c787e1887e789867ca5c3a6c5e1fc929548c0a64f81272990ab01"
@@ -8,13 +8,14 @@ class Zydis < Formula
   depends_on "cmake" => :build
 
   def install
-    mkdir "zydis-build" do
+    mkdir "build" do
       system "cmake", "..", *std_cmake_args
       system "make", "install"
     end
   end
 
   test do
-    assert_match("xrelease lock add qword ptr gs:[rax+rbx*4+0x12C], rsp", shell_output("#{bin}/ZydisInfo -64 66 3E 65 2E F0 F2 F3 48 01 A4 98 2C 01 00 00"))
+    output = shell_output("#{bin}/ZydisInfo -64 66 3E 65 2E F0 F2 F3 48 01 A4 98 2C 01 00 00")
+    assert_match "xrelease lock add qword ptr gs:[rax+rbx*4+0x12C], rsp", output
   end
 end
