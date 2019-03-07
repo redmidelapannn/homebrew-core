2e001e68876c4e58f56c471c0c8a387375a279f4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-07 20:14:36 +0000

    ghidra: add 9.0 bottle.

--- Formula/ghidra.rb
+++ Formula/ghidra.rb
@@ -4,6 +4,13 @@ class Ghidra < Formula
   url "https://ghidra-sre.org/ghidra_9.0_PUBLIC_20190228.zip"
   version "9.0"
   sha256 "3b65d29024b9decdbb1148b12fe87bcb7f3a6a56ff38475f5dc9dd1cfc7fd6b2"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8a3b65b5f3b3b0a09f58d6a79eb9284dd15c9f5f6f845d36d1824bf650c74919" => :mojave
+    sha256 "8a3b65b5f3b3b0a09f58d6a79eb9284dd15c9f5f6f845d36d1824bf650c74919" => :high_sierra
+    sha256 "06042e5da4324afb378b60182a64add002ffd0bdefc635481b6762c11d98f16d" => :sierra
+  end
+
   depends_on :java => "11.0+"
 
   def install
