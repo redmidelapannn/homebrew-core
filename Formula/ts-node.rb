ed7dc62ccac18cc5ea701471c23469ff9246a413
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-07 07:53:27 +0000

    ts-node: add 8.0.3 bottle.

--- Formula/ts-node.rb
+++ Formula/ts-node.rb
@@ -7,6 +7,13 @@ class TsNode < Formula
   url "https://registry.npmjs.org/ts-node/ts-node-8.0.3.tgz"
   sha256 "e3cb69b68d4974c456579cc48ba7d5f967e4a54e67e414ed672a30269e755054"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "58c639ef5131ebca23dfb8ba85f05471eb8d2a37f33ec532dd2a50405a7ba5e9" => :mojave
+    sha256 "c5dc69509c2b563098adc7e02261ba8ce3062e73dc94886019de7296d8f001cd" => :high_sierra
+    sha256 "336e78ea428704d23b602b91e1757adc5975ad6eb9f50a7d7abcf2bfd3c42503" => :sierra
+  end
+
   depends_on "node"
 
   resource "typescript" do
