e3e4a56b2023462c255618e7f0881d15ff1bc5c1
zricethezav <zricer@protonmail.com>
2019-03-10 15:51:12 -0500

    using tarball from github archive

--- Formula/gitleaks.rb
+++ Formula/gitleaks.rb
@@ -1,12 +1,22 @@
 class Gitleaks < Formula
   desc "Audit git repos for secrets"
   homepage "https://github.com/zricethezav/gitleaks"
-  url "https://github.com/zricethezav/gitleaks/releases/download/v1.24.0/gitleaks-darwin-amd64"
+  url "https://github.com/zricethezav/gitleaks/archive/v1.24.0.tar.gz"
   version "1.24.0"
-  sha256 "f81d98a002b85a12abb6d6441736c95a7ef416c97d9fbd55113ff380913146f3"
+  sha256 "6ba812be47976ca49bc2f5ab888c44ef41b824dd20fa9be5687f4ff6d185c2b1"
+
+  depends_on "dep" => :build
+  depends_on "go" => :build
 
   def install
-    bin.install "gitleaks-darwin-amd64" => "gitleaks"
+    ENV["GOPATH"] = buildpath
+    ENV["GOBIN"] = bin
+    dir = buildpath/"src/github.com/zricethezav/gitleaks"
+    dir.install buildpath.children
+    cd dir do
+      system "dep", "ensure"
+      system "go", "install"
+    end
   end
 
   test do
