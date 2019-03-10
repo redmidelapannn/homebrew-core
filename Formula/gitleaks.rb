4fdd127c9c2f6b0177a5caace6fea04ba10083a0
zricethezav <zricer@protonmail.com>
2019-03-10 15:57:19 -0500

    audit changes

--- Formula/gitleaks.rb
+++ Formula/gitleaks.rb
@@ -2,7 +2,6 @@ class Gitleaks < Formula
   desc "Audit git repos for secrets"
   homepage "https://github.com/zricethezav/gitleaks"
   url "https://github.com/zricethezav/gitleaks/archive/v1.24.0.tar.gz"
-  version "1.24.0"
   sha256 "6ba812be47976ca49bc2f5ab888c44ef41b824dd20fa9be5687f4ff6d185c2b1"
 
   depends_on "dep" => :build
@@ -14,7 +13,7 @@ class Gitleaks < Formula
     dir = buildpath/"src/github.com/zricethezav/gitleaks"
     dir.install buildpath.children
     cd dir do
-      system "dep", "ensure"
+      system "dep", "ensure", "-vendor-only"
       system "go", "install"
     end
   end
