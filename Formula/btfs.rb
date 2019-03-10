1c7e935c395267d5eff002de8d1aa270f777fbf8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-10 07:45:04 +0000

    btfs: update 2.18_3 bottle.

--- Formula/btfs.rb
+++ Formula/btfs.rb
@@ -8,9 +8,7 @@ class Btfs < Formula
 
   bottle do
     cellar :any
-    sha256 "e03434b33a6b3f27436b7e31e22696d3c645c27840729559b156e42ac4f99715" => :mojave
-    sha256 "cd5cffcddc4a7a7013fd7a38edba49b70c6bb9dc6ef965db0d003682d2a032c3" => :high_sierra
-    sha256 "b5bda19961ac5e3e9729c16ee6b39de5370fc462cbffac63823f7ba0f174548e" => :sierra
+    sha256 "c143cd021ee62b6ad7c17a46c0e3fea0da04bc63193396ed6e1fea6c79969ab1" => :mojave
   end
 
   depends_on "autoconf" => :build
