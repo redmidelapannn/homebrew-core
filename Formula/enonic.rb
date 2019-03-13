e0529f4a87cdaecc443f83f7f5150de9ba6c7af2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-13 09:39:21 +0000

    enonic: add 1.0.0 bottle.

--- Formula/enonic.rb
+++ Formula/enonic.rb
@@ -5,6 +5,13 @@ class Enonic < Formula
       :tag      => "1.0.0",
       :revision => "4d549122f2f0b137bd801a3b8ddb24a00a083376"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "28f1b70e9e3e9f7b2b554e0ae3ad648eb5dcab1686a4145970531350dc3b0ba3" => :mojave
+    sha256 "365aafbc47a60858540a3b0206a6ecf7b78d1cb54f775bfcdacff99d1817249b" => :high_sierra
+    sha256 "91b07cf5fc44e4a83876bb9c94cccbe822da2a7b9b65deea8873a1c1b0202860" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
