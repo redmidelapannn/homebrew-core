bb511243476e540eb291864f9cf65350f7db8c0a
Loki <Loki.Astari@gmail.com>
2019-03-14 01:03:56 -0700

    thors-serializer 1.10.3

--- Formula/thors-serializer.rb
+++ Formula/thors-serializer.rb
@@ -2,8 +2,8 @@ class ThorsSerializer < Formula
   desc "Declarative serialization library (JSON/YAML) for C++"
   homepage "https://github.com/Loki-Astari/ThorsSerializer"
   url "https://github.com/Loki-Astari/ThorsSerializer.git",
-      :tag      => "1.10.2",
-      :revision => "163fb907e8a4e0c3e05b8e15c1fdf6e0a02ca2c9"
+      :tag      => "1.10.3",
+      :revision => "27f85cd7fba7fce9a92569bf0298a14b33cd2544"
 
   bottle do
     cellar :any
