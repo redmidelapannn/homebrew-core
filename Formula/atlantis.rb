d231de3f4c623841dded4d7da5c63e966741e876
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-15 04:32:46 +0000

    atlantis: add 0.5.1 bottle.

--- Formula/atlantis.rb
+++ Formula/atlantis.rb
@@ -5,6 +5,13 @@ class Atlantis < Formula
   sha256 "4c0615e4979c9bda4343043056667bd14dcb3dc829e4bfd37ed04e00edb71fbb"
   head "https://github.com/runatlantis/atlantis.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "578437241c197a1936791db2778b2c72032e07d8039d196b2ddd42d09be27893" => :mojave
+    sha256 "5adfb40dfc884efd2f0bc480a28961d915451a963d714482775dc41d27231102" => :high_sierra
+    sha256 "73bef0cf71e6b37805324d2b4830253ce0125aef2cff7564a0683b1e59d970fc" => :sierra
+  end
+
   depends_on "dep" => :build
   depends_on "go" => :build
   depends_on "terraform"
