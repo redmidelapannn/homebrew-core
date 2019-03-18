43b4c8dd26e68a69b454c5b2f9b72827b4233ce7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-18 07:14:53 +0000

    hex: add 0.1.3 bottle.

--- Formula/hex.rb
+++ Formula/hex.rb
@@ -4,6 +4,13 @@ class Hex < Formula
   url "https://github.com/sitkevij/hex/archive/v0.1.3.tar.gz"
   sha256 "7c5397b1f435d2e3b5280cf6186ea8fcc44690fda812e303b6b51b6bbececc5e"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "795f3f0785f4b1d0b87f2014eeefddf23f555ea3ceca967eb94f0856d2ef9366" => :mojave
+    sha256 "18ab515b89b1c6b52fe1a280dbd6bcee4c48f76e29bfa1903a71a83964549f49" => :high_sierra
+    sha256 "aa654db2dce71aa8517a05afa7b97d1c091098ed3567a2c0d62aac10ef234864" => :sierra
+  end
+
   depends_on "rust" => :build
 
   def install
