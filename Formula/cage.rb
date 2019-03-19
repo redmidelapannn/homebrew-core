3d8340ab34878d98b9ea8ed0e68d68400c51ddc8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-19 16:45:48 +0000

    cage: add 0.2.7 bottle.

--- Formula/cage.rb
+++ Formula/cage.rb
@@ -5,6 +5,13 @@ class Cage < Formula
   sha256 "d438383f997a3c5e31ee8c739f35fc2fdd371a186ef295e945b56475842eebd6"
   head "https://github.com/faradayio/cage.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d0d5ababc3e8e5c59631b08ecc788bfe3adb1725eaf885e1bd47dbb2acba37ff" => :mojave
+    sha256 "d0d5ababc3e8e5c59631b08ecc788bfe3adb1725eaf885e1bd47dbb2acba37ff" => :high_sierra
+    sha256 "08852ea530b5e2416a3b64879a5abd59fd84248bb458b3f48eb10458f766bd8c" => :sierra
+  end
+
   depends_on "docker-compose"
 
   def install
