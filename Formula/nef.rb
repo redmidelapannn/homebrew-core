b8a901b9f719e1516340280d23164217ba6b1e73
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-20 08:39:37 +0000

    nef: add 0.1.0 bottle.

--- Formula/nef.rb
+++ Formula/nef.rb
@@ -4,6 +4,12 @@ class Nef < Formula
   url "https://github.com/bow-swift/nef/archive/0.1.0.tar.gz"
   sha256 "ec982a271c55e0003f03821dbf8f11306b28f232c8a6df3f5cbfdb11b8d4cbf5"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d0d13c3d49ecce7b358db3e77e6575561c6f871176e95d96cafd65fdd8d05586" => :mojave
+    sha256 "1324d423b61861e1677c85ed705e53b1fdcadb3310359988057af0145d9f2d0c" => :high_sierra
+  end
+
   depends_on "cocoapods"
   depends_on :xcode => "10.0"
 
