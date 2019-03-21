6fa36c1b5a5b9f6e40f10b6f5a96a754468f0806
Dimitar Haralanov <dharalanov@me.com>
2019-03-21 21:20:31 +0200

    Drop the `caveats` method
    This method should contain additional information that is relevant to
    Homebrew. Anything unrelated should not be included.

--- Formula/proper.rb
+++ Formula/proper.rb
@@ -11,12 +11,6 @@ class Proper < Formula
     prefix.install Dir["ebin", "include"]
   end
 
-  def caveats; <<~EOS
-    Add the following line to your shell startup file (~/.bashrc in the case of the Bash shell):
-      export ERL_LIBS=#{opt_prefix}
-  EOS
-  end
-
   test do
     assert_not_equal "non_existing", shell_output("erl -noshell -pa /usr/local/opt/proper/ebin -eval 'io:write(code:which(proper))' -s init stop")
   end
