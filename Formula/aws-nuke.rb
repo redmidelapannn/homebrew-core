6f433e5a51129eb1a4b14e0bff2b3639d3c05817
FX Coudert <fxcoudert@gmail.com>
2019-03-22 09:49:04 +0100

    Update aws-nuke.rb

--- Formula/aws-nuke.rb
+++ Formula/aws-nuke.rb
@@ -4,6 +4,7 @@ class AwsNuke < Formula
   homepage "https://github.com/rebuy-de/aws-nuke"
   url "https://github.com/rebuy-de/aws-nuke/archive/v2.8.0.tar.gz"
   sha256 "3539303b864e983e7b8b3e43f83ae9a4900b8361170aac0cd123132f16d4acc4"
+
   depends_on "glide" => :build
   depends_on "go" => :build
 
@@ -14,9 +15,7 @@ class AwsNuke < Formula
 
     cd srcpath do
       system "make", "build"
-      rm "aws-nuke"
-      mv "aws-nuke--darwin-amd64", "aws-nuke"
-      bin.install "aws-nuke"
+      bin.install "aws-nuke--darwin-amd64" => "aws-nuke"
       prefix.install_metafiles
     end
   end
