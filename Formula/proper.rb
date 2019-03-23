91d4a6b6fa9370de87d0d2e167ec5f123822a00c
FX Coudert <fxcoudert@gmail.com>
2019-03-23 18:37:17 +0100

    Update proper.rb

--- Formula/proper.rb
+++ Formula/proper.rb
@@ -1,5 +1,5 @@
 class Proper < Formula
-  desc "PropEr: a QuickCheck-inspired property-based testing tool for Erlang"
+  desc "QuickCheck-inspired property-based testing tool for Erlang"
   homepage "https://proper-testing.github.io"
   url "https://github.com/proper-testing/proper/archive/v1.3.tar.gz"
   sha256 "7e59eeaef12c07b1e42b0891238052cd05cbead58096efdffa3413b602cd8939"
@@ -12,6 +12,7 @@ class Proper < Formula
   end
 
   test do
-    assert_not_equal "non_existing", shell_output("erl -noshell -pa /usr/local/opt/proper/ebin -eval 'io:write(code:which(proper))' -s init stop")
+    output = shell_output("erl -noshell -pa #{opt_prefix}/ebin -eval 'io:write(code:which(proper))' -s init stop")
+    assert_not_equal "non_existing", output
   end
 end
