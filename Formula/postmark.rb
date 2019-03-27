ef3ad8e4b0d4de652f7f1c5eeb92c723d7360f17
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-27 05:24:56 +0000

    postmark: update 1.53 bottle.

--- Formula/postmark.rb
+++ Formula/postmark.rb
@@ -6,12 +6,10 @@ class Postmark < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "df5e68b280b40151c28cefb46a97605044af5c0e879f73a6d972676007aeffe6" => :mojave
-    sha256 "0e7f59c257967fd72381d86fca353c88f4af9d4843c0aa9e3eb4f606a6619fc4" => :high_sierra
-    sha256 "9d702b7bc49f646fd1623b316fd62c84ee6413e035a81e95866b0ca7240f100e" => :sierra
-    sha256 "784b46fe9883d27d347a44da73413ccf5c589088c0b57da577ebc1c79e64e1e6" => :el_capitan
-    sha256 "7fb38c3960e124a836cdc48650fd5f4d1fc446897b590e0dd6b6b6b5cbdec522" => :yosemite
-    sha256 "2ccb3812b371bc02e66d84ff853cb9684f8941485af3287424b4c183205bc649" => :mavericks
+    rebuild 1
+    sha256 "c9017f95626031f01c30d59e43d27a99e4b25e02099dc3a2fda8dd518327261c" => :mojave
+    sha256 "e310f3d75423cb4470c27fa093f17c9896e659a8d541cecd64043eb279328fe0" => :high_sierra
+    sha256 "ab70126798dc1e67e0d1fba4ec892078af8900c811058d5525f6257b43a86b3c" => :sierra
   end
 
   def install
