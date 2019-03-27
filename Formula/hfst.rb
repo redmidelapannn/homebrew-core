09f2cc22a3c65e87685405838529a1f9eeecce82
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-27 22:10:22 +0000

    hfst: add 3.15.0 bottle.

--- Formula/hfst.rb
+++ Formula/hfst.rb
@@ -4,6 +4,13 @@ class Hfst < Formula
   url "https://github.com/hfst/hfst/releases/download/v3.15.0/hfst-3.15.0.tar.gz"
   sha256 "bf50099174a0e14a53ab4d37d514bec2347dea3d4f2ff69d652659357cdd667f"
 
+  bottle do
+    cellar :any
+    sha256 "1a236a2fde407c6724eace023837afdbb0710d4069330bb537a0d95638387c1a" => :mojave
+    sha256 "6eba09ae81de182fb77f1023d684c47c57e7ea4ffda6fed4adddabf0b68ff46d" => :high_sierra
+    sha256 "3fd42b40bd06b6fe5f9e0692e0e3c916eb379150373b227903e70060a3b5c3a2" => :sierra
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
