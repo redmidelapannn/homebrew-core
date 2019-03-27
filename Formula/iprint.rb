16bc5cb2615b07856a7a56a87915eb7b5e913ecc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-27 05:24:56 +0000

    iprint: update 1.3-9 bottle.

--- Formula/iprint.rb
+++ Formula/iprint.rb
@@ -7,13 +7,10 @@ class Iprint < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 2
-    sha256 "8b1752455e0ff26b804070e3eb710493342fc2b2897a132a26433f4cabf5ec17" => :mojave
-    sha256 "c71f0b21d59a21fdc1e86e0a2016f79d862e838eb0fb7c92c50ed56e8aa1a163" => :high_sierra
-    sha256 "3fc40e5d2ee26c7b8709bf61e651ec3506561b98fcbf6ca52b8d353dd4be356d" => :sierra
-    sha256 "caa018741bb84409295f4fec33bcf427df199e717abf1323c9325d44238548ff" => :el_capitan
-    sha256 "eb0a1df1375a29fd3a88cddbe844820c9650b4ee14406245ee5d93ad41e48586" => :yosemite
-    sha256 "dfc0ad66122de0187db789cdafde75a367dc02748eede381567ca8f8a9208bde" => :mavericks
+    rebuild 3
+    sha256 "7f9ee6cb46090dad5937ff28c67501977c440a4ce3321559ca533cbd815397dd" => :mojave
+    sha256 "a00169ac99620e8215c9c154a56893eff1813b1628283055efadccbdbf3622f6" => :high_sierra
+    sha256 "9ab6ee097fc52f0017ead9a5d0b21569dda229411b76de5d4c3bf90a34f8d216" => :sierra
   end
 
   patch do
