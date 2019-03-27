16260fba44de233b10ffcaedc829b7f655089cd5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-27 05:25:02 +0000

    minicom: update 2.7.1 bottle.

--- Formula/minicom.rb
+++ Formula/minicom.rb
@@ -5,11 +5,10 @@ class Minicom < Formula
   sha256 "532f836b7a677eb0cb1dca8d70302b73729c3d30df26d58368d712e5cca041f1"
 
   bottle do
-    sha256 "77cc043be40a99634caa99d5f309741d85309fed07c1c21f313c6d99c4732966" => :mojave
-    sha256 "ec4beca9c9816db86a3bb7a11d7507fe0740feb62461341232a425a5156cd4a1" => :high_sierra
-    sha256 "63584b5ee8463dfb6cef69ad32308c51a4e83778dd44b80fc4c1e7c40cb48b2e" => :sierra
-    sha256 "820aae10f1c298350f51f7571d4d6becb4b0cfc876fb77126ea1e43bec8466e4" => :el_capitan
-    sha256 "5f17b6f15c2417acbda3a91b64f7df166b29fd2389adc52f011e2541f1fdbcb9" => :yosemite
+    rebuild 1
+    sha256 "898e103fbc8066c0c7ae410194b3e6184cb1959a89efd380ebfc70a3c7615ed8" => :mojave
+    sha256 "600365142bae63d6da1a75bf7d6f7768d78f7ce08694800353b000eda60bcd1b" => :high_sierra
+    sha256 "d6f35d553f94dc6a659fc390948b0436f54bf4955623b153c5d285a10340004a" => :sierra
   end
 
   def install
