f12fa891148707f260eacb7a2af1cd210a34e4a8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-03-27 05:25:02 +0000

    pipebench: update 0.40 bottle.

--- Formula/pipebench.rb
+++ Formula/pipebench.rb
@@ -8,12 +8,10 @@ class Pipebench < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "7489b10153744c61c74be048fb8c5d0acb4abae2f03437a0a633a312253c3345" => :mojave
-    sha256 "9ece6aaf7dcf0e1dbdbba28979ffbb6384f1d69aee8d194db2e009994c655cf2" => :high_sierra
-    sha256 "213e31962005a876277c6f8edd3c9cd8964c253496f7945d48aef7338c76277e" => :sierra
-    sha256 "353cabdaf04a41e2169c1e489cd038f9fbe7f33cfd24a5a0b3068449ccc3446d" => :el_capitan
-    sha256 "a999c7ba2978e60d38bdeec63d1f1a8b2667cb0d77d35c4da2e57212a37b85ac" => :yosemite
-    sha256 "c5b0805588d8a8d9047d30b9330dd0be0215bfe694ed662cf11fa84595bba85c" => :mavericks
+    rebuild 1
+    sha256 "4a84cb6768eba72b85cc5140c915c3cf74ffc4a3deff9a33c2fff2cb1f738636" => :mojave
+    sha256 "c7be5e8ec12b9f1ab0a496425054667ba1b81ef3194bad7dbee6ab70b43d4212" => :high_sierra
+    sha256 "d64b355efed693ace95359bb0d0093e19e843a3f9d48cae63040e3dc97038aac" => :sierra
   end
 
   def install
