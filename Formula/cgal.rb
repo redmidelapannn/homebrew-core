60d9cf73bafb20b1abccde66ed85ebdee7f65d9c
Maxime Gimeno <maxime.gimeno@gmail.com>
2019-04-02 09:30:03 +0200

    Update to 4.14 release

--- Formula/cgal.rb
+++ Formula/cgal.rb
@@ -1,8 +1,8 @@
 class Cgal < Formula
   desc "Computational Geometry Algorithm Library"
   homepage "https://www.cgal.org/"
-  url "https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.14-beta4/CGAL-4.14-beta4.tar.xz"
-  sha256 "83a50c61835a029f4a7b6fe885e77b83f4f6dc928d7d5acb00e3381f11f23733"
+  url "https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.14/CGAL-4.14.tar.xz"
+  sha256 "59464b1eaee892f2223ba570a7642892c999e29524ab102a6efd7c29c94a29f7"
 
   bottle do
     cellar :any
