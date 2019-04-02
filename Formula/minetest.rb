920ebf6df2f0220b54e3dbd8414bbb526be2ccaf
johnlockard <48885497+johnlockard@users.noreply.github.com>
2019-04-02 17:29:55 -0400

    Update minetest.rb to version 5.0.1
    Updated source and resource to current version.
    Removed unnecessary X11 dependency.
    Added simple Test Block.
    
    To do: Figure out how to update the Bottle Block.
    To do: Figure out how to run unit tests in the Test Block.

--- Formula/minetest.rb
+++ Formula/minetest.rb
@@ -3,12 +3,12 @@ class Minetest < Formula
   homepage "https://www.minetest.net/"
 
   stable do
-    url "https://github.com/minetest/minetest/archive/0.4.17.1.tar.gz"
-    sha256 "cd25d40c53f492325edabd2f6397250f40a61cb9fe4a1d4dd6eb030e0d1ceb59"
+    url "https://github.com/minetest/minetest/archive/5.0.1.tar.gz"
+    sha256 "aa771cf178ad1b436d5723e5d6dd24e42b5d56f1cfe9c930f6426b7f24bb1635"
 
     resource "minetest_game" do
-      url "https://github.com/minetest/minetest_game/archive/0.4.17.tar.gz"
-      sha256 "f0ab07cb47c1540b2016bf76a36e2eec28b0ea7827bf66fc5447e0c5e5d4495d"
+      url "https://github.com/minetest/minetest_game/archive/5.0.1.tar.gz"
+      sha256 "965d2cf3ac8c822bc9e60fb8f508182fb2f24dde46f46b000caf225ebe2ec519"
     end
   end
 
@@ -35,7 +35,6 @@ class Minetest < Formula
   depends_on "libogg"
   depends_on "libvorbis"
   depends_on "luajit"
-  depends_on :x11
 
   def install
     (buildpath/"games/minetest_game").install resource("minetest_game")
@@ -64,5 +63,15 @@ class Minetest < Formula
       If you would like to start the Minetest server from a terminal, run
       "/Applications/minetest.app/Contents/MacOS/minetest --server".
     EOS
+  end
+    
+  test do
+    #
+    # Commands --help, --version will produce a readable output without creating a file.
+    #
+    # Commands --info, --run-unittests, --speedtests, --trace, and
+    # --videomodes require creating user data directory for output.
+    #
+    system "#{prefix}/minetest.app/Contents/MacOS/minetest", "--version"
   end
 end
