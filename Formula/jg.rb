190d333c581b39d41b98c677522fdb597140cf8e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-03 00:13:42 +0000

    jg: add 0.1.3 bottle.

--- Formula/jg.rb
+++ Formula/jg.rb
@@ -3,6 +3,13 @@ class Jg < Formula
   desc "A command-line JSON processor in Rust. jg searches for selector patterns in json input, jg prints each json object that matches a pattern."
   homepage "https://github.com/gmmorris/jg"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6dbcc146126c0aaba8c8bc996f0c03e6bb9999e7c8d7536db4815cd2883362ef" => :mojave
+    sha256 "6dbcc146126c0aaba8c8bc996f0c03e6bb9999e7c8d7536db4815cd2883362ef" => :high_sierra
+    sha256 "f12cc0a17ca3b4564738c040420d03bdb33974e74f61b13370e2f672c465b916" => :sierra
+  end
+
   if OS.mac?
       url "https://github.com/gmmorris/jg/releases/download/#{version}/jg-#{version}-x86_64-apple-darwin.tar.gz"
       sha256 "4f36a8cebfd51cf97d99f415910c09e4354f847fde56bdc3e55311872aad0ed9"
