22242b07d77c6b3f825c9b96b7d8a8f29fb46a82
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-06 06:49:45 +0000

    accio: add 0.5.5 bottle.

--- Formula/accio.rb
+++ Formula/accio.rb
@@ -4,6 +4,11 @@ class Accio < Formula
   url "https://github.com/JamitLabs/Accio.git", :tag => "0.5.5", :revision => "3b8a910fc4e627c81ffa2e4eafeff450136bb0e4"
   head "https://github.com/JamitLabs/Accio.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6afc78cb2176ef875a09a5e91a9ef2ff10dfe1b55cb8dcd19b3831e0671c82aa" => :mojave
+  end
+
   depends_on "carthage"
   depends_on :xcode => "10.2"
 
