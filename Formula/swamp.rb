d8d610dfcca1563b8d47c272c8def610e6c0998a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-08 10:20:43 +0000

    swamp: add 0.10.0 bottle.

--- Formula/swamp.rb
+++ Formula/swamp.rb
@@ -4,6 +4,13 @@ class Swamp < Formula
   url "https://github.com/felixb/swamp/archive/v0.10.0.tar.gz"
   sha256 "4cdcdd3e7b9085de5c0f53a9783e3392680727372d89f0e38270ab8da889b1d8"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a4f86ab48c0b226a2f187467d94b5bc91556b97fb2ee4305762bad00a17d780d" => :mojave
+    sha256 "15504457ac1edc81be5bf10be5dce6e071e123ebc9a36ffbacf92bbf5a6b9726" => :high_sierra
+    sha256 "9a1fabd8962eef0b60f578b83088040777d4d088929e18d890c4361af17dd70c" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
