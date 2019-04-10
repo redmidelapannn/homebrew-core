059574ee6941b0dad0a1d8d5b940fc1e079a5eb0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-10 00:51:37 +0000

    gfile: add 86.64 bottle.

--- Formula/gfile.rb
+++ Formula/gfile.rb
@@ -6,6 +6,13 @@ class Gfile < Formula
   homepage "https://github.com/Antonito/gfile"
   url "https://github.com/Antonito/gfile/releases/download/v0.1.0/gfile_Darwin_x86_64.tar.gz"
   sha256 "a386af795bd1dfa4c1132390c0eb91b82c661bd4c49f176626746cea1f5767ce"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "3cdb8fa06a3f7cf8e2c71a1aa74411511dac35d8f0a76ec8673fa79d43780a54" => :mojave
+    sha256 "3cdb8fa06a3f7cf8e2c71a1aa74411511dac35d8f0a76ec8673fa79d43780a54" => :high_sierra
+    sha256 "d51e50e494d0269682384898a6f06f14ea21e3ebf22f0422e827e3da2a0921a9" => :sierra
+  end
+
   # depends_on "cmake" => :build
 
   def install
