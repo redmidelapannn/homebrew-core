17e6c8fa69cdcbb3cee3b13f4f3ff45e9d07320a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-12 17:12:38 +0000

    cypher-shell: add 1.1.9 bottle.

--- Formula/cypher-shell.rb
+++ Formula/cypher-shell.rb
@@ -5,6 +5,13 @@ class CypherShell < Formula
   version "1.1.9"
   sha256 "027dd83ff10ac24077dc9ab7e1be4a624f8cadeab4039bb6800584d94fecf49f"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5f7f49876322fe23df3fcda839ba3efdbd3e3d58f7ecdc3466e687d84a81dc84" => :mojave
+    sha256 "5f7f49876322fe23df3fcda839ba3efdbd3e3d58f7ecdc3466e687d84a81dc84" => :high_sierra
+    sha256 "02cd34433d5c7dd6c3e70f00c61fc2d5dbcccae4f71df683dfdca0e8c19e46a6" => :sierra
+  end
+
   depends_on :java => "1.8"
 
   def install
