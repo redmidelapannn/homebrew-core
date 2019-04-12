d713dab45618e8fc1e4152a184890b977000471b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-12 18:55:58 +0000

    getstream-cli: add 0.0.1-beta.58 bottle.

--- Formula/getstream-cli.rb
+++ Formula/getstream-cli.rb
@@ -7,6 +7,13 @@ class GetstreamCli < Formula
   version "0.0.1-beta.58"
   sha256 "3a976e56dd976baf071bcd4c9478b1c2ff67fa55010fb89dddbbbde49df95eea"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a716dc041f5d7592281d8c7382332d736d1bd8eed422d3f66a024fb6c1e999e5" => :mojave
+    sha256 "6f80433ad1892885908082d2a03c8fc0c55e75b1e6c860b37f510895f2623fd8" => :high_sierra
+    sha256 "2674f37478d1d65f4c4c55faab370d7883446af1f389b28c24f13eccf5f4801b" => :sierra
+  end
+
   depends_on "node"
 
   def install
