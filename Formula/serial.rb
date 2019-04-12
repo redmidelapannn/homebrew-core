63d373c308102f3ba47e21df39f6e45757e10df0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-12 04:58:22 +0000

    serial: add 1.3.2 bottle.

--- Formula/serial.rb
+++ Formula/serial.rb
@@ -3,6 +3,12 @@ class Serial < Formula
   homepage "https://gitlab.com/johnbarton/serial"
   url "https://gitlab.com/johnbarton/serial/-/archive/1.3.2/serial-1.3.2.tar.gz"
   sha256 "381ed4e4b76b414232b89ae044e3d6c1b6c2e2c99fdbea2625ce9888089fee72"
+  bottle do
+    sha256 "79d0a6fe904c8c2b5492602eac8c357781996791f8ae1ac5a4b72255b64b2dfa" => :mojave
+    sha256 "27bb27ba066cdf9819952e6291da9e9ad33887d7510d8d04159ba8940ef67940" => :high_sierra
+    sha256 "93d78117a6453ff9538a037d7cfaf89db44376e47ed01c3b20fd3c4f57575b42" => :sierra
+  end
+
   depends_on "cmake" => :build
 
   def install
