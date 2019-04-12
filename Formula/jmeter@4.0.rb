856f664ae05f6cf46e25bbaaf634e10775d8e7f8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-12 02:09:29 +0000

    jmeter@4.0: add 4.0 bottle.

--- Formula/jmeter@4.0.rb
+++ Formula/jmeter@4.0.rb
@@ -4,6 +4,13 @@ class JmeterAT40 < Formula
   url "https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-4.0.tgz"
   sha256 "845b8325726171a991cc13072275ec64b2ce4bbc6fa8e2aa350b2369e27e76b7"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "74960093fbb383f1cf3359548528a90b4dcee1c5d790f165d7f65d6ba3813fe3" => :mojave
+    sha256 "74960093fbb383f1cf3359548528a90b4dcee1c5d790f165d7f65d6ba3813fe3" => :high_sierra
+    sha256 "5030c34e8dcfed245401857efc60c7c94bb3fb0d17293437c3f8e75b9f823e10" => :sierra
+  end
+
   keg_only :versioned_formula
 
   def install
