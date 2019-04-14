f8cd0a964356ba96edbe954c6404e40027385b9d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-14 09:53:41 +0000

    pachi: update 11.00_1 bottle.

--- Formula/pachi.rb
+++ Formula/pachi.rb
@@ -8,11 +8,9 @@ class Pachi < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0b415ef29c6636bcdff11cb56dba0d3822d826cf6f2313427337695ba249e47d" => :mojave
-    sha256 "0d32845e7628104c0748c8263c4da549766cfada77adcdb7d02141a72b29c69a" => :high_sierra
-    sha256 "738716a5b3cbb1d23ff52db5a4375a014060ed9d836f938be9200bfd25b83324" => :sierra
-    sha256 "0296a8eab88a76533da8c45630d53bf54c98236b061666ebba72a0065d32ca7c" => :el_capitan
-    sha256 "88480b1583b55e3eb05dd1f3b32617024873465f09d19e5998c2c81afb4d9dba" => :yosemite
+    rebuild 1
+    sha256 "60ee32ed07e4038bdb5311695473bd85147fb03216986c491a5884153fa77728" => :mojave
+    sha256 "126704132dbe9f706a1881039a46043f525c0317f7d221875e77f74eeedad01f" => :high_sierra
   end
 
   fails_with :clang if MacOS.version == :mavericks
