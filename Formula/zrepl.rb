9e88a0c3b897b17728992364da5d3f56979c3500
Justin Scholz <git@justinscholz.de>
2019-04-15 09:35:48 +0200

    making zrepl

--- Formula/zrepl.rb
+++ Formula/zrepl.rb
@@ -2,13 +2,11 @@ class Zrepl < Formula
   desc "One-stop ZFS backup & replication solution"
   homepage "https://zrepl.github.io"
   url "https://github.com/zrepl/zrepl/archive/v0.1.1.tar.gz"
+  sha256 "0c16554e4527d14a390d78cf95bce759da425019a83ec63acfed5b4c50d68c9c"
   head "https://github.com/zrepl/zrepl.git"
-  sha256 ""
+
   depends_on "go" => :build
 
-  devel do
-    url "https://github.com/zrepl/zrepl/archive/problame/fix-io-timeout-despite-heartbeats.zip"
-  end
   def install
     contents = Dir["{*,.git,.gitignore}"]
     gopath = buildpath/"gopath"
@@ -19,7 +17,7 @@ class Zrepl < Formula
     
     cd gopath/"src/github.com/zrepl/zrepl" do
       system "./lazy.sh",  "godep"
-      system "make"
+      system "make", "ZREPL_VERSION=0.1.1"
       bin.install "artifacts/zrepl"
     end      
   end
