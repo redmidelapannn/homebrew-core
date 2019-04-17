7dc9df7da3862466823948cfdff845994d197482
Roberto Salas <wala.rsalas@gmail.com>
2019-04-16 23:37:53 -0400

    libpostal 1.1-alpha (new formula)

--- Formula/libpostal.rb
+++ Formula/libpostal.rb
@@ -1,5 +1,5 @@
 class Libpostal < Formula
-  desc "A C library for parsing/normalizing street addresses around the world. Powered by statistical NLP and open geo data."
+  desc "Library for parsing/normalizing street addresses around the world using NLP and open geodata"
   homepage "https://github.com/openvenues/libpostal"
   url "https://github.com/openvenues/libpostal/archive/v1.1-alpha.tar.gz"
   sha256 "c8a88eed70d8c09f68e1e69bcad35cb397e6ef11b3314e18a87b314c0a5b4e3a"
@@ -7,8 +7,8 @@ class Libpostal < Formula
 
   depends_on "autoconf" => :build
   depends_on "libtool" => :build
-  depends_on "pkg-config" => :build
   depends_on "automake" => :build
+  depends_on "pkg-config" => :build
 
   def install
     system "./bootstrap.sh"
