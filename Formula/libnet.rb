5b8bd9dd5233efbb4a18d8c6c5306a758ac5fc99
Adam Bene <adambene@adambene.com>
2019-04-20 17:39:38 +0200

    libnet 1.2

--- Formula/libnet.rb
+++ Formula/libnet.rb
@@ -1,8 +1,8 @@
 class Libnet < Formula
   desc "C library for creating IP packets"
   homepage "https://github.com/sam-github/libnet"
-  url "https://downloads.sourceforge.net/project/libnet-dev/libnet-1.1.6.tar.gz"
-  sha256 "d392bb5825c4b6b672fc93a0268433c86dc964e1500c279dc6d0711ea6ec467a"
+  url "https://github.com/sam-github/libnet/archive/libnet-1.2.tar.gz"
+  sha256 "a4fd8022abc9a279fa38abb5409d232d3969cce86cf67e64ea72ca264af71e05"
 
   bottle do
     cellar :any
@@ -17,19 +17,17 @@ class Libnet < Formula
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
+  depends_on "doxygen" => :build
   depends_on "libtool" => :build
 
-  # Fix raw sockets support
-  patch :p0 do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/a689647/libnet/patch-configure.in.diff"
-    sha256 "3c1ca12609d83372cf93223d69e903eb6e137ed7a4749a8ee19c21bd43f97f18"
-  end
-
   def install
-    system "autoreconf", "-fvi"
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
-    inreplace "src/libnet_link_bpf.c", "#include <net/bpf.h>", "" # Per MacPorts
-    system "make", "install"
+    chdir "libnet" do
+      system "autoreconf", "-fvi"
+      system "./configure", "--disable-dependency-tracking",
+                            "--prefix=#{prefix}"
+      inreplace "src/libnet_link_bpf.c", "#include <net/bpf.h>", "" # Per MacPorts
+      system "make -C doc doc"
+      ENV.deparallelize { system "make", "install" }
+    end
   end
 end
