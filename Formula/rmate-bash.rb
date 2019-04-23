32cbaed01bc09c689456c3c99c0da95100d55e8b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-23 22:22:23 +0000

    rmate-bash: add 1.0.2 bottle.

--- Formula/rmate-bash.rb
+++ Formula/rmate-bash.rb
@@ -6,6 +6,13 @@ class RmateBash < Formula
   version "1.0.2"
   head "https://github.com/aurora/rmate.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "bd3ede1209dd03ad4f214c8875367678361be93b668f560d14b560f51da16377" => :mojave
+    sha256 "bd3ede1209dd03ad4f214c8875367678361be93b668f560d14b560f51da16377" => :high_sierra
+    sha256 "f5ccbc0ed32ccce84186ec596dea4ed8610f7ab3ae40277c5aa98643d3fd51d6" => :sierra
+  end
+
   conflicts_with "rmate", :because => "both install `rmate` scripts"
 
   def install
