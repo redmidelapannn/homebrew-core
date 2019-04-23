c5a637cdd1c8e050dba4e09ab2c60dfe59f0d352
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-23 06:26:27 +0000

    ktlint: add 0.32.0 bottle.

--- Formula/ktlint.rb
+++ Formula/ktlint.rb
@@ -4,6 +4,13 @@ class Ktlint < Formula
   url "https://github.com/pinterest/ktlint/releases/download/0.32.0/ktlint"
   sha256 "95d292cb45271c733640a3d24add7db4f9f2fac998fb5a15679e6da79fb04441"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "81a5728bbf0306c4567e3a7b8f6d3fe45d4776bd33b327f7fe0324c6f4e17f4b" => :mojave
+    sha256 "81a5728bbf0306c4567e3a7b8f6d3fe45d4776bd33b327f7fe0324c6f4e17f4b" => :high_sierra
+    sha256 "46dd348ff3ca90f776b5d30488a756c8462530b2a3f2789a59d85721636e79eb" => :sierra
+  end
+
   def install
     bin.install "ktlint"
   end
