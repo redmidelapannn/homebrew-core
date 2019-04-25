3d35aeadddd9d2639c2b2caab2a087a7f4a5c80f
Clouds <Clouds.Flowing@gmail.com>
2019-04-25 19:42:02 +0800

    fix comment

--- Formula/spirv-tools.rb
+++ Formula/spirv-tools.rb
@@ -6,19 +6,19 @@ class SpirvTools < Formula
   depends_on "cmake" => :build
 
   resource "re2" do
-    # DEPS
+    # revision number could be found in ./DEPS
     url "https://github.com/google/re2.git",
         :revision => "6cf8ccd82dbaab2668e9b13596c68183c9ecd13f"
   end
 
   resource "effcee" do
-    # DEPS
+    # revision number could be found in ./DEPS
     url "https://github.com/google/effcee.git",
         :revision => "04b624799f5a9dbaf3fa1dbed2ba9dce2fc8dcf2"
   end
 
   resource "spirv-headers" do
-    # DEPS
+    # revision number could be found in ./DEPS
     url "https://github.com/KhronosGroup/SPIRV-Headers.git",
         :revision => "e74c389f81915d0a48d6df1af83c3862c5ad85ab"
   end
@@ -39,6 +39,6 @@ class SpirvTools < Formula
   test do
     cp libexec/"examples"/"main.cpp", testpath/"test.cpp"
     system ENV.cc, "-o", "test", "test.cpp", "-std=c++11", "-I#{include}", "-L#{lib}", "-lSPIRV-Tools", "-lSPIRV-Tools-link", "-lSPIRV-Tools-opt", "-lc++"
-    # system "./test" # note the example is not work now, https://github.com/KhronosGroup/SPIRV-Tools/issues/2538
+    system "./test" # note the example is not work now, https://github.com/KhronosGroup/SPIRV-Tools/issues/2538
   end
 end
