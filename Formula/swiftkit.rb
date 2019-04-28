2e15a0f9b504e796fd1a100838dca6dd09fd61cf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-04-28 15:00:04 +0000

    swiftkit: add 1.0.0 bottle.

--- Formula/swiftkit.rb
+++ Formula/swiftkit.rb
@@ -5,6 +5,11 @@ class Swiftkit < Formula
   sha256 "e30269929901c75d2f0f20a188336cf4e6b18e41eeb38fb45b10c73dd6c91944"
   head "https://github.com/SvenTiigi/SwiftKit.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ac78335b8ed487f05fb8c898a85914bbec73d84233ca4b6ab0c8412336f59d06" => :mojave
+  end
+
   depends_on :xcode => ["8.0", :build]
 
   def install
