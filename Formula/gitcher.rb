dccad29793238683916ea0dd8dfaff1ed5d69a43
Borja González Seoane <dev@glezseoane.com>
2019-04-29 15:52:52 +0200

    gitcher: Fix formula after audit

--- Formula/gitcher.rb
+++ Formula/gitcher.rb
@@ -1,13 +1,12 @@
 class Gitcher < Formula
   include Language::Python::Virtualenv
 
-  desc "The git profile switcher. It facilitates the switching between git profiles, importing configuration settings such as name, email and user signatures."
+  desc "The git profile switcher."
   homepage "https://gitlab.com/GlezSeoane/gitcher"
   url "https://gitlab.com/GlezSeoane/gitcher/-/archive/v2.0/gitcher-v2.0.tar.gz"
   sha256 "0c649235c78e0698fb062b758a12c9c74722eba2db484983fb85f080dbaa29b9"
-  version "2.0"
 
-  depends_on "python3"
+  depends_on "python"
 
   resource "prettytable" do
     url "https://files.pythonhosted.org/packages/e0/a1/36203205f77ccf98f3c6cf17cf068c972e6458d7e58509ca66da949ca347/prettytable-0.7.2.tar.gz#sha256=2d5460dc9db74a32bcc8f9f67de68b2c4f4d2f01fa3bd518764c69156d9cacd9"
@@ -22,7 +21,7 @@ class Gitcher < Formula
     virtualenv_install_with_resources
   end
 
-  def test
-    system "python3", "-c", "'import gitcher'"
+  test do
+    system "python", "-c", "'import gitcher'"
   end
 end
