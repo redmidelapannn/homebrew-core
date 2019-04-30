abe2836ffaef4725f508cb27023452bd982b6a7e
Paul Klemm <paulklemm@t-online.de>
2019-04-30 16:33:24 +0200

    proteinortho 6.0 (new formular)

--- Formula/proteinortho.rb
+++ Formula/proteinortho.rb
@@ -3,16 +3,15 @@ class Proteinortho < Formula
   homepage "https://gitlab.com/paulklemm_PHD/proteinortho"
   url "https://gitlab.com/paulklemm_PHD/proteinortho/-/archive/v6.0/proteinortho-v6.0.tar.gz"
   sha256 "b28f05c8c90a98bd65f2f1c1d9ed78e585959428c9f1a8deac1f53e72912de2c"
-  depends_on "cmake" => :build  
-  depends_on "pkg-config" => :build  
-  depends_on "lapack" => :build  
-  depends_on "gcc" => :build  
+  depends_on "cmake" => :build
+  depends_on "gcc" => :build
+  depends_on "pkg-config" => :build
 
   def install
-    system "mkdir", "#{prefix}/bin"
+    mkdir "#{bin}"
     ENV["LIBRARY_PATH"] = `pkg-config --libs --static lapack`.chomp
     ENV["CXX"] = `ls #{Formula["gcc"].opt_bin.realpath}/g++-* | sort | tail -n1`.chomp
-    system "make", "CXX=#{ENV["CXX"]}", "CXXLIBRARY='#{ENV["LIBRARY_PATH"]}'" , "install", "PREFIX=#{prefix}/bin"
+    system "make", "CXX=#{ENV["CXX"]}", "CXXLIBRARY='#{ENV["LIBRARY_PATH"]}'", "install", "PREFIX=#{bin}"
   end
 
   test do
