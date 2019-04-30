3e2379d5991d16176dca3f3b683f4d4833636a4b
Paul Klemm <paulklemm@t-online.de>
2019-04-30 09:31:43 +0200

    proteinortho

---
+++ Formula/proteinortho.rb
@@ -0,0 +1,18 @@
+class Proteinortho < Formula
+  desc "Proteinortho is a tool to detect orthologous genes within different species"
+  homepage "https://gitlab.com/paulklemm_PHD/proteinortho"
+  url "https://gitlab.com/paulklemm_PHD/proteinortho/-/archive/v6.0b/proteinortho-v6.0b.tar.gz"
+  sha256 "f59c436f6461f318ccebc1177a67eb70c29ef9ba222ad724de1fb2a769a120b5"
+  depends_on "cmake" => :build
+  depends_on "gcc"
+
+  def install
+    system "make", "clean"
+    system "make", "CPP=/usr/local/Cellar/gcc/*/bin/g++*" , "install"
+  end
+
+  test do
+    system "#{bin}/proteinortho", "-help"
+    system "#{bin}/proteinortho_clustering", "-test"
+  end
+end
