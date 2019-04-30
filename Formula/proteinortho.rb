2eac8bac72c0e8abc9560f9ec54006a96a665df5
Paul Klemm <paulklemm@t-online.de>
2019-04-30 16:46:54 +0200

    proteinortho 6.0 (new formular)

--- Formula/proteinortho.rb
+++ Formula/proteinortho.rb
@@ -2,9 +2,9 @@ class Proteinortho < Formula
   desc "Detecting orthologous genes within different species"
   homepage "https://gitlab.com/paulklemm_PHD/proteinortho"
   url "https://gitlab.com/paulklemm_PHD/proteinortho/-/archive/v6.0/proteinortho-v6.0.tar.gz"
-  sha256 "b28f05c8c90a98bd65f2f1c1d9ed78e585959428c9f1a8deac1f53e72912de2c"
+  sha256 "400e5a6e147b43e9bb0e6bc977715516043b3e5035586b58b7b48748975c178c"
   depends_on "cmake" => :build
-  depends_on "gcc"
+  depends_on "gcc" # for gfortran
   depends_on "pkg-config" => :build
 
   def install
