43cc3af0f3b7305ccae258d266b3a7d4f859d6dd
Paul Klemm <paulklemm@t-online.de>
2019-04-30 15:44:47 +0200

    proteinortho 6.0 (new formular)

--- Formula/proteinortho.rb
+++ Formula/proteinortho.rb
@@ -1,22 +1,19 @@
 class Proteinortho < Formula
-  desc "Proteinortho is a tool to detect orthologous genes within different species"
+  desc "Detecting orthologous genes within different species"
   homepage "https://gitlab.com/paulklemm_PHD/proteinortho"
   url "https://gitlab.com/paulklemm_PHD/proteinortho/-/archive/v6.0b/proteinortho-v6.0b.tar.gz"
   sha256 "f59c436f6461f318ccebc1177a67eb70c29ef9ba222ad724de1fb2a769a120b5"
-  depends_on "cmake" => :build
+  depends_on "cmake" => :build  
   depends_on "gcc"
 
-  # get the latest g++ compiler from brew (Cellar)
-  CXX = `ls /usr/local/Cellar/gcc/*/bin/g++* | sort | tail -n1 | tr -d '\n'`
-
   def install
-    system "make", "clean"
     system "mkdir", "#{prefix}/bin"
-    system "make", "CPP=#{CXX}" , "install", "PREFIX=#{prefix}/bin"
+    ENV["CXX"] = `ls #{Formula["gcc"].opt_bin.realpath}/g++-* | sort | tail -n1`.chomp
+    system "make", "CPP=$CXX" , "install", "PREFIX=#{prefix}/bin"
   end
 
   test do
-    system "#{bin}/proteinortho -help"
-    system "#{bin}/proteinortho_clustering -test"
+    system "#{bin}/proteinortho", "-help"
+    system "#{bin}/proteinortho_clustering", "-test"
   end
 end
