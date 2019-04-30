53fefd5dce966f2d57b1a372756a80d56fd16f0d
Paul Klemm <paulklemm@t-online.de>
2019-04-30 15:45:07 +0200

    proteinortho 6.0 (new formular)

--- Formula/proteinortho.rb
+++ Formula/proteinortho.rb
@@ -1,15 +1,18 @@
 class Proteinortho < Formula
   desc "Detecting orthologous genes within different species"
   homepage "https://gitlab.com/paulklemm_PHD/proteinortho"
-  url "https://gitlab.com/paulklemm_PHD/proteinortho/-/archive/v6.0b/proteinortho-v6.0b.tar.gz"
-  sha256 "f59c436f6461f318ccebc1177a67eb70c29ef9ba222ad724de1fb2a769a120b5"
+  url "https://gitlab.com/paulklemm_PHD/proteinortho/-/archive/v6.0/proteinortho-v6.0.tar.gz"
+  sha256 "b28f05c8c90a98bd65f2f1c1d9ed78e585959428c9f1a8deac1f53e72912de2c"
   depends_on "cmake" => :build  
-  depends_on "gcc"
+  depends_on "pkg-config" => :build  
+  depends_on "lapack" => :build  
+  depends_on "gcc" => :build  
 
   def install
     system "mkdir", "#{prefix}/bin"
+    ENV["LIBRARY_PATH"] = `pkg-config --libs --static lapack`.chomp
     ENV["CXX"] = `ls #{Formula["gcc"].opt_bin.realpath}/g++-* | sort | tail -n1`.chomp
-    system "make", "CPP=$CXX" , "install", "PREFIX=#{prefix}/bin"
+    system "make", "CXX=#{ENV["CXX"]}", "CXXLIBRARY='#{ENV["LIBRARY_PATH"]}'" , "install", "PREFIX=#{prefix}/bin"
   end
 
   test do
