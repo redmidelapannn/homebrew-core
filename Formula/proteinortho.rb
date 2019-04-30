0cf698634f16c02469f9516509d993a5dcbdf301
Paul Klemm <paulklemm@t-online.de>
2019-04-30 19:32:29 +0200

    proteinortho 6.0b (new formular)

--- Formula/proteinortho.rb
+++ Formula/proteinortho.rb
@@ -2,13 +2,14 @@ class Proteinortho < Formula
   desc "Detecting orthologous genes within different species"
   homepage "https://gitlab.com/paulklemm_PHD/proteinortho"
   url "https://gitlab.com/paulklemm_PHD/proteinortho/-/archive/v6.0/proteinortho-v6.0.tar.gz"
-  sha256 "400e5a6e147b43e9bb0e6bc977715516043b3e5035586b58b7b48748975c178c"
+  sha256 "b28f05c8c90a98bd65f2f1c1d9ed78e585959428c9f1a8deac1f53e72912de2c"
   depends_on "cmake" => :build
-  depends_on "gcc" # for gfortran
+  depends_on "lapack" => :build
   depends_on "pkg-config" => :build
+  depends_on "gcc" # for gfortran
 
   def install
-    mkdir "#{bin}"
+    mkdir bin.to_s
     ENV["LIBRARY_PATH"] = `pkg-config --libs --static lapack`.chomp
     ENV["CXX"] = `ls #{Formula["gcc"].opt_bin.realpath}/g++-* | sort | tail -n1`.chomp
     system "make", "CXX=#{ENV["CXX"]}", "CXXLIBRARY='#{ENV["LIBRARY_PATH"]}'", "install", "PREFIX=#{bin}"
