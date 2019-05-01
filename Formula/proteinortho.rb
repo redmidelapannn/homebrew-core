41c44936edc0d0f963e2d1ce77960c20bda6f324
Paul Klemm <paulklemm@t-online.de>
2019-05-01 13:56:05 +0200

    proteinortho 6.0b (new formular)

--- Formula/proteinortho.rb
+++ Formula/proteinortho.rb
@@ -2,7 +2,7 @@ class Proteinortho < Formula
   desc "Detecting orthologous genes within different species"
   homepage "https://gitlab.com/paulklemm_PHD/proteinortho"
   url "https://gitlab.com/paulklemm_PHD/proteinortho/-/archive/v6.0/proteinortho-v6.0.tar.gz"
-  sha256 "5fbfe0e7cb28f5f614a97fc2286d5df55f4c355ebbf3590a61005c56b915b53a"
+  sha256 "1df4460f7cd5654c1e0c0c7abf546b50288265c895b1bf28dde618d7f080e805"
 
   depends_on "cmake" => :build
   fails_with :clang # needs fortran
@@ -10,6 +10,7 @@ class Proteinortho < Formula
   def install
     mkdir bin.to_s
     system "make", "install", "PREFIX=#{bin}"
+    doc.install "manual.html"
   end
 
   test do
