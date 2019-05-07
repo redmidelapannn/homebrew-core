e853df1a04d6d9de8619225fd91fe62ba874a152
Joseph Henry <josephjah@gmail.com>
2019-05-07 16:14:19 -0700

    Removed bottle section. Fixed formula to conform to standards

--- Formula/libzt.rb
+++ Formula/libzt.rb
@@ -2,36 +2,17 @@ class Libzt < Formula
   desc "ZeroTier: libzt -- An encrypted P2P networking library for applications"
   homepage "https://www.zerotier.com"
 
-  version "1.3.0"
-
-  stable do
-    url "https://github.com/zerotier/libzt.git", :branch => "master", :revision => "3d1159882117278fcb5fabb623bd62175b6c7e6c"
-  end
-
-  bottle do
-    root_url "https://download.zerotier.com/dist/homebrew"
-    cellar :any
-    sha256 "e1ac8425fd0ea510c7db734af8d6c41cd3650b12f66a571f9d818c0121422eee" => :mojave
-  end
-
-  devel do
-    version "1.3.1"
-    url "https://github.com/zerotier/libzt.git", :branch => "dev"
-  end
-
-  head do
-    url "https://github.com/zerotier/libzt.git"
-  end
+  url "https://download.zerotier.com/dist/sdk/libzt-1.3.0-source.tar.gz"
+  sha256 "0db5e1df43275d17162b986be29fa8c530695e5503f207d27739703730b5b23c"
 
   depends_on "cmake" => :build
   depends_on "make" => :build
 
   def install
-    system "make", "update"
     system "cmake", ".", *std_cmake_args
     system "cmake", "--build", "."
     system "make", "install"
-    cp "LICENSE.GPL-3", "#{prefix}/LICENSE"
+    cp "licenses/ZEROTIER-LICENSE.GPL-3", "#{prefix}/LICENSE"
   end
 
   def caveats
