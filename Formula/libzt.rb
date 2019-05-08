6a5eec2b84e46e9740b036f5afde77f6193799b4
Joseph Henry <josephjah@gmail.com>
2019-05-08 01:02:34 -0700

    Removed comments and added proper newlines

--- Formula/libzt.rb
+++ Formula/libzt.rb
@@ -17,22 +17,11 @@ class Libzt < Formula
     cp "LICENSE.GPL-3", "#{prefix}/LICENSE"
   end
 
-  def caveats
-    <<~EOS
-      Visit https://my.zerotier.com to create virtual networks and authorize devices.
-      Visit https://www.zerotier.com/manual.shtml to learn more about how ZeroTier works.
-      Visit https://github.com/zerotier/ZeroTierOne/tree/master/controller to learn how to run your own network controller (advanced).
-    EOS
-  end
-
   test do
-    # Writes a simple test program to test.cpp which calls a library function. The expected output of this
-    # function is -2. This test verifies the following:
-    # - The library was installed correctly
-    # - The library was linked correctly
-    # - Library code executes successfully and sends the proper error code to the test program
     (testpath/"test.cpp").write <<-EOS
-      #include<cstdlib>\n#include<ZeroTier.h>\nint main(){return zts_socket(0,0,0)!=-2;}
+      #include<cstdlib>
+      #include<ZeroTier.h>
+      int main(){return zts_socket(0,0,0)!=-2;}
     EOS
 
     system ENV.cc, "-v", "test.cpp", "-o", "test", "-L#{lib}/Release", "-lzt"
