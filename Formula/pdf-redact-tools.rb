01e6338d561413460712692d72e6ef6fc957a349
Caleb Xu <calebcenter@live.com>
2019-05-11 03:21:14 -0400

    pdf-redact-tools: bump revision for ghostscript update

--- Formula/pdf-redact-tools.rb
+++ Formula/pdf-redact-tools.rb
@@ -3,6 +3,7 @@ class PdfRedactTools < Formula
   homepage "https://github.com/firstlookmedia/pdf-redact-tools"
   url "https://github.com/firstlookmedia/pdf-redact-tools/archive/v0.1.2.tar.gz"
   sha256 "5874a7b76be15ccaa4c20874299ef51fbaf520a858229a58678bc72a305305fc"
+  revision 1
   head "https://github.com/firstlookmedia/pdf-redact-tools.git"
 
   bottle do
