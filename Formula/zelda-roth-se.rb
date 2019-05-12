c4e9ac0c9d667941a4a3d60156da2ffd5938210f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-12 23:10:09 +0000

    zelda-roth-se: update 1.1.0 bottle.

--- Formula/zelda-roth-se.rb
+++ Formula/zelda-roth-se.rb
@@ -16,11 +16,10 @@ class ZeldaRothSe < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b9f77d702fa6c412eec4b5d1c602ae1bc0ca05d01421d792110057bd79e53742" => :mojave
-    sha256 "8b76f5c62bca90258a3e0c3d930551befe9fdd0f0814d1f2026f32dae1027998" => :high_sierra
-    sha256 "2db05a8f1209a0f039c6bea92ffefb362d94497bc9f659e8057eca1539fc132e" => :sierra
-    sha256 "7aa852ad416766a72aad6b254f86f21a53bd67df29f20b7eaa612ba709f8c0cd" => :el_capitan
-    sha256 "069a42db0b88121a77233d3860a5191af80ad335270cc4723b1bb4ef1c48ccc3" => :yosemite
+    rebuild 1
+    sha256 "3c32a1b07bb6de4bac669d91d0077c0ab95f0e0d710e5eb1f47970ed005d1ec9" => :mojave
+    sha256 "2bfe5c59bc1e0226822fc63f59e0f54558426e7a3399ce1fd394524da8bda330" => :high_sierra
+    sha256 "783a8e72d2498f371433cc66cbb0638febe6aa6d6bc4c2c8cf3de65340dc9250" => :sierra
   end
 
   depends_on "cmake" => :build
