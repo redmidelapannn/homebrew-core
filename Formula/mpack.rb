54ee4638da4134a67e8b175475c47d646af2005b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-12 23:10:10 +0000

    mpack: update 1.6 bottle.

--- Formula/mpack.rb
+++ Formula/mpack.rb
@@ -7,12 +7,10 @@ class Mpack < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "edbef02feb0f06f807c864f872cf7f5ac42bbf65a40249e92a3e990f5c1bfa4e" => :mojave
-    sha256 "3010b6b97b6388a250e18278f1ec45b27244898876856d53e776b75ecd0c6bf3" => :high_sierra
-    sha256 "4dac8e937f170ddcae76e90143e7b1a6b0bd66e730d683ab83693d55dd670f2b" => :sierra
-    sha256 "f0f9bd526ff3c7e8a1abea377e3716fee96916c0c54234d96b46a475b50b8c1e" => :el_capitan
-    sha256 "f5455f95c52ffc59181037ea3fd8151006178dfd6abb9674be65b2996f876766" => :yosemite
-    sha256 "9090f5b1263e27adfdd359ee1a052a71edb681d4305cfd921488b1533f8bfcf8" => :mavericks
+    rebuild 1
+    sha256 "4e9fa6bb6ed44e500daae00d3211635b45842edb8ff3fd88e79a4f397ee5b879" => :mojave
+    sha256 "51f4bd4480a9d00cf001861c6ddd6d198b65756bf100082d5545aa33cfdd29b3" => :high_sierra
+    sha256 "8ec5211543f8310d2fbdffd8aae93048fdaf863df3f813b9ace885b68cfdf78a" => :sierra
   end
 
   # Fix missing return value; clang refuses to compile otherwise
