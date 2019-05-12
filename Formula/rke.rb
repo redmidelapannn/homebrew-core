f096cfa9b71e9cfe476f5ed9ee276f5afcfb366d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-12 17:10:09 +0000

    rke: update 0.2.3 bottle.

--- Formula/rke.rb
+++ Formula/rke.rb
@@ -7,9 +7,9 @@ class Rke < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d26a87a3faf94f16da808f0f133bb7a6560846b53c8e2058504c03522c9db9b5" => :mojave
-    sha256 "51c3641dedaaec277a005dcd1bfca3e73fb7bee37492e7360a7ce5e3abb37e4b" => :high_sierra
-    sha256 "c638e82372f31fd465b59b23b035aa8a97c0f5eedbe02492ee456c396ddcb3e6" => :sierra
+    sha256 "029d033f86f215800c344cd37061e4f8e4479ff5c1d81112ecf4c54c85bee1b5" => :mojave
+    sha256 "7f1dd8df336cea7a61d34a2ad740e095bc534aee1669e76ee1dbd310bdb9b4f3" => :high_sierra
+    sha256 "161f2bb017f71d66c71e0aa4732e1f8181791b4341d0889ac5cea48b9f730ed2" => :sierra
   end
 
   depends_on "go" => :build
