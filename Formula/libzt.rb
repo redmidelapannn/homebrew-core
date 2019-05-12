87b1ff27a41ad174f7a6983e65f72e31f3cdd5bd
Joseph Henry <josephjah@gmail.com>
2019-05-12 15:34:06 -0700

    Fixed license install, indentation, and changed url to a tag

--- Formula/libzt.rb
+++ Formula/libzt.rb
@@ -3,22 +3,18 @@ class Libzt < Formula
   homepage "https://www.zerotier.com"
 
   url "https://github.com/zerotier/libzt.git",
-    :branch   => "master",
-    :revision => "9dbcc8eb45bf8863261599962c799b1de3557d14"
-
-  version "1.3.0"
+    :tag   => "1.3.0-13-g9dbcc8e"
 
   depends_on "cmake" => :build
 
   def install
-    system "git", "submodule", "update", "--init"
     system "git", "-C", "ext/lwip", "apply", "../lwip.patch"
     system "git", "-C", "ext/lwip-contrib", "apply", "../lwip-contrib.patch"
     system "git", "-C", "ext/ZeroTierOne", "apply", "../ZeroTierOne.patch"
     system "cmake", ".", *std_cmake_args
     system "cmake", "--build", "."
     system "make", "install"
-    cp "LICENSE.GPL-3", "#{prefix}/LICENSE"
+    prefix.install "LICENSE.GPL-3" => "LICENSE"
   end
 
   test do
@@ -27,7 +23,7 @@ class Libzt < Formula
       #include <ZeroTier.h>
       int main()
       {
-        return zts_socket(0,0,0)!=-2;
+        return zts_socket(0,0,0) != -2;
       }
     EOS
     system ENV.cxx, "-v", "test.cpp", "-o", "test", "-L#{lib}", "-lzt"
