4ab5a66c969d0b189c9d17f70cac58a43d734d13
Rui Chen <chenrui333@gmail.com>
2019-05-12 23:41:34 -0400

    kibana 6.7.2

--- Formula/kibana.rb
+++ Formula/kibana.rb
@@ -2,8 +2,8 @@ class Kibana < Formula
   desc "Analytics and search dashboard for Elasticsearch"
   homepage "https://www.elastic.co/products/kibana"
   url "https://github.com/elastic/kibana.git",
-      :tag      => "v6.7.0",
-      :revision => "e09a026ba3803569800034af6ce923cdc1864a08"
+      :tag      => "v6.7.2",
+      :revision => "c8ecbda57ae7c780ef7d870f2e4d163b5f093e3b"
   head "https://github.com/elastic/kibana.git"
 
   bottle do
@@ -14,13 +14,13 @@ class Kibana < Formula
   end
 
   resource "node" do
-    url "https://nodejs.org/dist/v10.15.2/node-v10.15.2.tar.xz"
-    sha256 "b8bb2da7cb016e895bc2f70009a420f6b8d519e66548624b6130bbfbd5118c59"
+    url "https://nodejs.org/dist/v10.15.3/node-v10.15.3.tar.xz"
+    sha256 "4e22d926f054150002055474e452ed6cbb85860aa7dc5422213a2002ed9791d5"
   end
 
   resource "yarn" do
-    url "https://yarnpkg.com/downloads/1.15.2/yarn-v1.15.2.tar.gz"
-    sha256 "c4feca9ba5d6bf1e820e8828609d3de733edf0e4722d17ed7ce493ed39f61abd"
+    url "https://yarnpkg.com/downloads/1.16.0/yarn-v1.16.0.tar.gz"
+    sha256 "df202627d9a70cf09ef2fb11cb298cb619db1b958590959d6f6e571b50656029"
   end
 
   def install
