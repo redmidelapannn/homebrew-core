220f56652583ae9f93e066feec54b87527a48bea
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-13 04:04:37 +0000

    librtlsdr: update 0.6.0 bottle.

--- Formula/librtlsdr.rb
+++ Formula/librtlsdr.rb
@@ -7,10 +7,8 @@ class Librtlsdr < Formula
 
   bottle do
     cellar :any
-    sha256 "0e9b14804b722d9efc959940e40ebcef7bf716eb636f0bb0dc600770cb005531" => :mojave
-    sha256 "71f28a8abd8e9e0245a61f841fcebcb7a179d952be786199bf21fae0edd11f6c" => :high_sierra
-    sha256 "d1b83b24f32d4857205be289f7c632ee3bd77af802e3445d7565bb9ba9e4f3b1" => :sierra
-    sha256 "f5196572498f20ff0ea38d4e7ceed95aea1199a558f29dd6aac5cec9db65ce33" => :el_capitan
+    rebuild 1
+    sha256 "24b27e2274146b237729b6809a5feb2a4aa70646dae972250b28e9686436c181" => :mojave
   end
 
   depends_on "cmake" => :build
