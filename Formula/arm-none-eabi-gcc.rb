7e3ad6dd4e1ce54609c8a5007c29e1b2ae029ccb
kousu <nick@kousu.ca>
2019-05-15 00:53:56 -0400

    arm-none-eabi-gcc 8-2018-q4-major (new formula)
    This imports https://github.com/ARMmbed/homebrew-formulae/ verbatim.
    I realize formulas are supposed to build from (it's casks from binaries),
    but this is just to get me started.

---
+++ Formula/arm-none-eabi-gcc.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class ArmNoneEabiGcc < Formula
+
+  homepage 'https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads'
+  version '8-2018-q4-major'
+
+  url 'https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/8-2018q4/gcc-arm-none-eabi-8-2018-q4-major-mac.tar.bz2'
+  sha256 '0b528ed24db9f0fa39e5efdae9bcfc56bf9e07555cb267c70ff3fee84ec98460'
+
+  def install
+    (prefix/"gcc").install Dir["./*"]
+    Dir.glob(prefix/"gcc/bin/*") { |file| bin.install_symlink file }
+  end
+end
+
