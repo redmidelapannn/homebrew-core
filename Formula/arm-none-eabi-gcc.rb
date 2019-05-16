239f396d7913b45c6041d92de653d5ee9678e2af
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-16 01:27:29 +0000

    arm-none-eabi-gcc: add 8-2018-q4-major bottle.

--- Formula/arm-none-eabi-gcc.rb
+++ Formula/arm-none-eabi-gcc.rb
@@ -5,6 +5,13 @@ class ArmNoneEabiGcc < Formula
   version "8-2018-q4-major"
   sha256 "0b528ed24db9f0fa39e5efdae9bcfc56bf9e07555cb267c70ff3fee84ec98460"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "58b5d859b8cfe78cfdea9fe3df33e8c6181b7092320d7193d11cd9294cd4c621" => :mojave
+    sha256 "58b5d859b8cfe78cfdea9fe3df33e8c6181b7092320d7193d11cd9294cd4c621" => :high_sierra
+    sha256 "677e3ca5af06637ac7187066ae349dbc1e795f590f67a54c71595422f996d9d1" => :sierra
+  end
+
   depends_on "expat" => :build
   depends_on "libiconv" => :build
   depends_on "libmpc" => :build
