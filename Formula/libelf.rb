a6867a3adf3dff6eb17000538ad8becc0bcec8d2
kousu <nick@kousu.ca>
2019-05-15 20:29:26 -0400

    libelf 0.8.12 (downgrade to solve linkrot)
    http://www.mr511.de/software/ is dead; it 301s to http://leere.seite/ which is not a website.
    Apparently linux has moved on from this libelf; now libelf is part of https://sourceware.org/elfutils/
    However that library depends on glibc, so it can't work on Darwin.
    
    This points libelf at a mirror someone made 10 years ago on github,
    which is one point release behind where libelf was, but that should...be...okay...hopefully.

--- Formula/libelf.rb
+++ Formula/libelf.rb
@@ -1,9 +1,9 @@
 class Libelf < Formula
   desc "ELF object file access library"
-  homepage "http://www.mr511.de/software/"
-  url "http://www.mr511.de/software/libelf-0.8.13.tar.gz"
-  sha256 "591a9b4ec81c1f2042a97aa60564e0cb79d041c52faa7416acb38bc95bd2c76d"
-  revision 1
+  homepage "https://github.com/WolfgangSt/libelf"
+  url "https://github.com/WolfgangSt/libelf.git",
+      :revision => "dbbf0eddb1f7d243f5f909cba13597ad76fc4fcb"
+  version "0.8.12"
 
   bottle do
     cellar :any_skip_relocation
