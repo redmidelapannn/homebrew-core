32bef9933bbc77ebce802bbdd28f207b173077f9
Steve McQuaid <smcquaid@salesforce.com>
2019-05-22 13:14:02 -0400

    Update tests

--- Formula/kubebuilder.rb
+++ Formula/kubebuilder.rb
@@ -24,17 +24,8 @@ class Kubebuilder < Formula
 
   test do
     ENV["GOPATH"] = testpath
-    ENV["GO111MODULE"] = "on"
-    dir = testpath/"src/example.com/test-kubebuilder"
-    dir.mkpath
 
-    run_output = shell_output("#{bin}/kubebuilder 2>&1")
+    run_output = shell_output("#{bin}/kubebuilder --help 2>&1")
     assert_match "Development kit for building Kubernetes extensions and tools.", run_output
-
-    version_output = shell_output("#{bin}/kubebuilder version 2>&1")
-    assert_match "GitTreeState:\"clean\"", version_output
-    if build.stable?
-      assert_match stable.instance_variable_get(:@resource).instance_variable_get(:@specs)[:revision], version_output
-    end
   end
 end
