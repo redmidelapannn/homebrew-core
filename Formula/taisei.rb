25e5f2aae9c293549b71aebef40654b11ba069d5
Igor Kapkov <igasgeek@me.com>
2019-05-23 16:25:31 +1000

    taisei 1.3

--- Formula/taisei.rb
+++ Formula/taisei.rb
@@ -2,9 +2,8 @@ class Taisei < Formula
   desc "Clone of Touhou Project shoot-em-up games"
   homepage "https://taisei-project.org/"
   url "https://github.com/taisei-project/taisei.git",
-      :tag      => "v1.2",
-      :revision => "46fb0f894ad269528ac7fda533c7994eddd9b758"
-  revision 1
+      :tag      => "v1.3",
+      :revision => "f8ef67224f47e85f4095e32736dc21e0d46ae5b7"
 
   bottle do
     sha256 "5a63d77e6ba51f8907f1d6a08ca6fc3c94b7f4317afd746c91f0c8011d767c20" => :mojave
@@ -26,11 +25,8 @@ class Taisei < Formula
   depends_on "sdl2_mixer" => :build
   depends_on "sdl2_ttf" => :build
 
-  # Fixes a bug in the .app bundle build script.
-  # # Will be in the next release.
-  patch do
-    url "https://github.com/taisei-project/taisei/commit/68b0d4f5c6f2015704e1ed1b4098be1c4336db74.patch?full_index=1"
-    sha256 "cb1f79826e632a61daa271cb59d0a80ab77dea876d384c381ab66d5eb9b9bd27"
+  if MacOS.version <= :sierra
+    depends_on "openssl" => :build
   end
 
   def install
@@ -47,6 +43,6 @@ class Taisei < Formula
 
   test do
     output = shell_output("#{prefix}/Taisei.app/Contents/MacOS/Taisei -h", 1)
-    assert_match "Touhou clone", output
+    assert_match "Taisei is an open source Tōhō Project fangame.", output
   end
 end
