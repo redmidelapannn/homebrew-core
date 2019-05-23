43609e61d0f379a995f5b103baacfc5ae3ef2c6e
Rui Chen <rchen@meetup.com>
2019-05-22 20:56:48 -0400

    Fix the build and upgrade to 0.10.3 (will squash)

--- Formula/wtfutil.rb
+++ Formula/wtfutil.rb
@@ -1,20 +1,19 @@
 class Wtfutil < Formula
   desc "The personal information dashboard for your terminal"
   homepage "https://wtfutil.com"
-  url "https://github.com/wtfutil/wtf/archive/v0.10.2.tar.gz"
-  sha256 "7ed8c63bd7126b39b35026bf25df3ebd4404aa804c54eb47b6fa04c6fe1e478b"
+  url "https://github.com/wtfutil/wtf/archive/v0.10.3.tar.gz"
+  sha256 "8afa15a5729bfa5a8d009e3d03423cf7472e797740b92e6f3e66ca8fd05c8454"
 
   depends_on "go" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    ENV["GO111MODULE"] = "on"
+    # ENV["GOPATH"] = buildpath
+    ENV["GOPATH"] = HOMEBREW_CACHE/"go_cache"
 
     dir = buildpath/"src/github.com/wtfutil/wtf"
     dir.install buildpath.children
 
     cd dir do
-      system "go", "mod", "vendor"
       system "go", "build", "-o", bin/"wtf"
       prefix.install_metafiles
     end
