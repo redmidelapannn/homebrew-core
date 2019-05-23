28cacf0cedcf0d811b9f096e67a4170ee0219210
Igor Kapkov <igasgeek@me.com>
2019-05-23 18:00:47 +1000

    inspircd 3.1.0

--- Formula/inspircd.rb
+++ Formula/inspircd.rb
@@ -1,13 +1,17 @@
 class Inspircd < Formula
   desc "Modular C++ Internet Relay Chat daemon"
   homepage "https://www.inspircd.org/"
-  url "https://github.com/inspircd/inspircd/archive/v2.0.27.tar.gz"
-  sha256 "6bc1956bd6a7d2d463c646f1563c99cb507f2f214e51d6ac9c70906ac27aae73"
+  url "https://github.com/inspircd/inspircd/archive/v3.1.0.tar.gz"
+  sha256 "5fd6b30e0285dd2bcf6fd135ffce52a08d8452f820a02e0068ac471e99d465ba"
 
   bottle do
     sha256 "de7c0627908828d4ec8b317cbf174f043598b38c1916fa64dab5c8b76411ac2d" => :mojave
     sha256 "679569e694341552a1cc4d50240ebbd28f77850f0adfc482d0e3ddc1a7736495" => :high_sierra
     sha256 "45b3a6258e87954cb82f98d97ca87d54f4063a06f5b8cdff645ba0be4bf994bb" => :sierra
+  end
+
+  if MacOS.version <= :sierra
+    depends_on "openssl" => :build
   end
 
   depends_on "pkg-config" => :build
@@ -17,12 +21,8 @@ class Inspircd < Formula
 
   def install
     system "./configure", "--enable-extras=m_ldapauth.cpp,m_ldapoper.cpp"
-    system "./configure", "--prefix=#{prefix}", "--with-cc=#{ENV.cc}"
+    system "./configure", "--prefix=#{prefix}", "CXX=#{ENV.cxx}"
     system "make", "install"
-  end
-
-  def post_install
-    inreplace "#{prefix}/org.inspircd.plist", "ircdaemon", ENV["USER"]
   end
 
   test do
