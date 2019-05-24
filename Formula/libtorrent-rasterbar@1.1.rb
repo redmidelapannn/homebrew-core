90bd4e8291851c36e4e2ca52eef99c83d9a72e4c
Calum Lind <calumlind@gmail.com>
2019-05-24 10:52:53 +0100

    libtorrent@1.1 1.1.13 (new formula)
    Although libtorrent 1.2 is the latest release with new features, 1.1 is
    the most stable version, especially with regards the the Python bindings
    so it is useful to have the older version still available e.g. for Deluge.
    
    Add an Alias for 1.2 to allow picking specific version.

---
+++ Formula/libtorrent-rasterbar@1.1.rb
@@ -0,0 +1,48 @@
+class LibtorrentRasterbarAT11 < Formula
+  desc "C++ bittorrent library with Python bindings"
+  homepage "https://www.libtorrent.org/"
+  url "https://github.com/arvidn/libtorrent/releases/download/libtorrent-1_1_13/libtorrent-rasterbar-1.1.13.tar.gz"
+  sha256 "30040719858e3c06634764e0c1778738eb42ecd0b45e814afa746329a948ead7"
+
+  keg_only :versioned_formula
+
+  depends_on "pkg-config" => :build
+  depends_on "boost"
+  depends_on "boost-python3"
+  depends_on "openssl"
+  depends_on "python"
+
+  def install
+    args = %W[
+      --disable-debug
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --prefix=#{prefix}
+      --enable-encryption
+      --enable-python-binding
+      --with-boost=#{Formula["boost"].opt_prefix}
+      --with-boost-python=boost_python37-mt
+      PYTHON=python3
+    ]
+
+    if build.head?
+      system "./bootstrap.sh", *args
+    else
+      system "./configure", *args
+    end
+
+    system "make", "install"
+    libexec.install "examples"
+  end
+
+  test do
+    system ENV.cxx, "-std=c++11", "-I#{Formula["boost"].include}/boost",
+                    "-L#{lib}", "-ltorrent-rasterbar",
+                    "-L#{Formula["boost"].lib}", "-lboost_system",
+                    "-framework", "SystemConfiguration",
+                    "-framework", "CoreFoundation",
+                    libexec/"examples/make_torrent.cpp", "-o", "test"
+    system "./test", test_fixtures("test.mp3"), "-o", "test.torrent"
+    assert_predicate testpath/"test.torrent", :exist?
+  end
+end
