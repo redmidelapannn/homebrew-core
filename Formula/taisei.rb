a67c8ff05e7f7037396e87e1a3638830aa01d6be
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-24 02:28:56 +0000

    taisei: update 1.3 bottle.

--- Formula/taisei.rb
+++ Formula/taisei.rb
@@ -6,10 +6,10 @@ class Taisei < Formula
       :revision => "f8ef67224f47e85f4095e32736dc21e0d46ae5b7"
 
   bottle do
-    sha256 "5a63d77e6ba51f8907f1d6a08ca6fc3c94b7f4317afd746c91f0c8011d767c20" => :mojave
-    sha256 "162eb10eaf78191aeb474f75095cb35b5840dd4b4ad1e0452ec65ab19590cf80" => :high_sierra
-    sha256 "0065ca15927c95e455dd3a42c4cc1dfcb9365069c914a8d1bffca11e41dbde00" => :sierra
-    sha256 "b9eae75e261940cba3bf33aec8fea1904c6de4648a0dea1f32f517289e0d9a1d" => :el_capitan
+    cellar :any
+    sha256 "7e3d15a3aa18590b614c047a5a35b67286913e8d8dc0dcd7f431706a268b3aec" => :mojave
+    sha256 "78ec2efedf734e4894c11d922b5d6c2aef8234e4d0c24f06011c41b5ee86050d" => :high_sierra
+    sha256 "b7d9cb66d263e7998d7c078fcf616555f423526958e4cbcc50fcda0d7b6f28b2" => :sierra
   end
 
   # Yes, these are all build deps; the game copies them into the app bundle,
