0c23f39242c01051ca8734a4548967c03881cf53
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-25 20:58:36 +0000

    bash-completion: update 1.3_3 bottle.

--- Formula/bash-completion.rb
+++ Formula/bash-completion.rb
@@ -9,11 +9,10 @@ class BashCompletion < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "9219c2b46362677e9ae6e19b344b774c3e9f163ae6bf6cf2686da06419aaec89" => :mojave
-    sha256 "b069be5574bdf6d12fd1fda17c3162467b68165541166d95d1a9474653a63abc" => :high_sierra
-    sha256 "58be92ef01d5068f37b1c00af8e9b202bdb409c93121bb0e07dcbb5e55dc3be2" => :sierra
-    sha256 "58be92ef01d5068f37b1c00af8e9b202bdb409c93121bb0e07dcbb5e55dc3be2" => :el_capitan
-    sha256 "58be92ef01d5068f37b1c00af8e9b202bdb409c93121bb0e07dcbb5e55dc3be2" => :yosemite
+    rebuild 1
+    sha256 "1cb9eb845f4734b406082f5206f584e0b70b8780da06ee7e1d23e34ec98831e6" => :mojave
+    sha256 "1cb9eb845f4734b406082f5206f584e0b70b8780da06ee7e1d23e34ec98831e6" => :high_sierra
+    sha256 "2edbff881ff24fa801a78cd65adf425435b7770fb4eca18ee03a5e93e2ebf4c1" => :sierra
   end
 
   conflicts_with "bash-completion@2", :because => "Differing version of same formula"
