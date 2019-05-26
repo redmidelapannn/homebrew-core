785ecfcf38968dba26d2764c02750f8417257b4b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-26 20:58:09 +0000

    libtorrent-rasterbar@1.1: add 1.1.13 bottle.

--- Formula/libtorrent-rasterbar@1.1.rb
+++ Formula/libtorrent-rasterbar@1.1.rb
@@ -4,6 +4,13 @@ class LibtorrentRasterbarAT11 < Formula
   url "https://github.com/arvidn/libtorrent/releases/download/libtorrent-1_1_13/libtorrent-rasterbar-1.1.13.tar.gz"
   sha256 "30040719858e3c06634764e0c1778738eb42ecd0b45e814afa746329a948ead7"
 
+  bottle do
+    cellar :any
+    sha256 "4eecbeae2d042dc70b65b01b48a46655ba16a1cddfe10472f339e2bc0c8fd0ea" => :mojave
+    sha256 "af8a80a3c356b3127e089537db082155890640412c7bcaf731af567d4379b762" => :high_sierra
+    sha256 "e3a762b4cd976dd6c4e7d575755e605791a04226ab1d379c737803acc41cd93b" => :sierra
+  end
+
   keg_only :versioned_formula
 
   depends_on "pkg-config" => :build
