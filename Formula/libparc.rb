8a7ca3c0af4c11f8f2e27c22bb78a370d32efbfc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-29 16:09:04 +0000

    libparc: update 1.0-65~g7944543~b3 bottle.

--- Formula/libparc.rb
+++ Formula/libparc.rb
@@ -5,7 +5,9 @@ class Libparc < Formula
   version "1.0-65~g7944543~b3"
 
   bottle do
-    sha256 "d915407ef1a9a22321a8fc4e8a4186b305d901b31858136cfa4d7f885c2bb0b9" => :mojave
+    sha256 "ee95df36e2f6629a3ae1e0722ce6c667e795d82788481f8c9a459588c21ba4da" => :mojave
+    sha256 "0dd78a4b07a6c373994a22aefe45149bb12031fdb346b5ad85275c91659c4210" => :high_sierra
+    sha256 "2741f7b08acc564359524e69eb9362c2c5fee2c16db9fbf555aaf359c6f5ac20" => :sierra
   end
 
   depends_on "cmake" => :build
