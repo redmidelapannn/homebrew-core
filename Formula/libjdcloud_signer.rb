b142a86d68df00a85ae6c0af15ed77c210a75d59
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-29 06:36:44 +0000

    libjdcloud_signer: add 0.2.0 bottle.

--- Formula/libjdcloud_signer.rb
+++ Formula/libjdcloud_signer.rb
@@ -5,6 +5,13 @@ class LibjdcloudSigner < Formula
   sha256 "f78ac6cfc8bdf539eaca11f13966671e38749feb46abac57db36bb24a8d12e36"
   head "https://github.com/jdcloud-api/jdcloud-sdk-cpp-signer.git"
 
+  bottle do
+    cellar :any
+    sha256 "c1eb331f810dfef4892ef86fe6b01f3082b3e33ec9628749a3bed6f97f0b8051" => :mojave
+    sha256 "c154916eb302b7258f7ffae9a03fa055d4ac3931d4c4d27d08d8f59aeee44ed5" => :high_sierra
+    sha256 "c9d4ca9693cb250de40f57843a1c11c957b7ba28ef3b8b80255adb289f9ff939" => :sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "openssl"
 
