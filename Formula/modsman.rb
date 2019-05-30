19d2b49465e99c62d511d13866397be7a2cf1ab1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-30 06:57:51 +0000

    modsman: add 0.20.1 bottle.

--- Formula/modsman.rb
+++ Formula/modsman.rb
@@ -6,6 +6,13 @@ class Modsman < Formula
   url "https://dev.azure.com/sargunvohra/modsman/_apis/build/builds/37/artifacts?artifactName=modsman-cli-0.20.1&api-version=5.0&%24format=zip"
   version "0.20.1"
   sha256 "136609552bbddbe3dbbd64c6a7731e8ff7cb8fcf8b92532172142f3b0d3d9da2"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "0eb4ce816c901d5405bb92b276c5a2587784da1b79fd2803560c320ffafa3ecb" => :mojave
+    sha256 "0eb4ce816c901d5405bb92b276c5a2587784da1b79fd2803560c320ffafa3ecb" => :high_sierra
+    sha256 "a40193b320400c101e26c9484bade5bcd041325beb5edbd8699748d96794612a" => :sierra
+  end
+
   
   head "https://gitlab.com/sargunv-mc-mods/modsman.git"
 
