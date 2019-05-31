d74ad3bc432fd02e1f79c703060fddd9c7c66f21
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-05-31 13:26:55 +0000

    libcapn: update 2.0.0 bottle.

--- Formula/libcapn.rb
+++ Formula/libcapn.rb
@@ -14,12 +14,8 @@ class Libcapn < Formula
   end
 
   bottle do
-    sha256 "63e06837f263b3c535d8e332dbb98800662bb58f31b1f95c8280e56e306a4db2" => :mojave
-    sha256 "0e2232cdfceb24cffcc403e97056e3be96e7aa20b6b2aaeced40a68df97ff5c2" => :high_sierra
-    sha256 "2a7e5dbc8969be74fd4e7d86ae6b8d639583ccd2de8fef81895775de584206f6" => :sierra
-    sha256 "1ea44f9bd53e6729a874c0b55a4031e0ff806ae967ab20267d0b0ec87219ceda" => :el_capitan
-    sha256 "e046c34011f734511cbaa38f5defeec5da3ed3ad3d2f7c11e8c5eddc92e8e596" => :yosemite
-    sha256 "a9405089ccb42d828415d5ce60a9da5c6d39c28d89d599244317c37634a6e580" => :mavericks
+    rebuild 1
+    sha256 "f72c1a75767b684c7ba2d990eb6131156bec31db0dcc3cf5c43cd4ca1563794d" => :mojave
   end
 
   depends_on "cmake" => :build
