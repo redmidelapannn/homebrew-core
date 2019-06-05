5fdf2db2a02f8ac6a3c36af6ba66f044f4f40cbf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-05 17:34:28 +0000

    nats-server: add 2.0.0 bottle.

--- Formula/nats-server.rb
+++ Formula/nats-server.rb
@@ -5,6 +5,13 @@ class NatsServer < Formula
   sha256 "e74e9b79c89d24e19aa4d755be02f57e63eefb501c053b7a9bfd373246370b47"
   head "https://github.com/nats-io/nats-server.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9ef86ba54e2113d09cd36d0fc0adebceb406468931d763414b6cca9418aec5b9" => :mojave
+    sha256 "3cf56b26bdb81e2eb6fc35bb0753c9931b29e3573cbb08ead4293b81d30bfa36" => :high_sierra
+    sha256 "144b0bd1e4b5deaef8287c20ebedd0b7a732fed76debfe5a10e846c2e2899997" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
