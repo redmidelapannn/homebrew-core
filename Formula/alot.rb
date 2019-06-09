b7debc97a17dbae5acf174c0edc93643d8aad638
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-09 02:36:18 +0000

    alot: update 0.8.1 bottle.

--- Formula/alot.rb
+++ Formula/alot.rb
@@ -9,10 +9,9 @@ class Alot < Formula
 
   bottle do
     cellar :any
-    sha256 "adbbdac0378bf3c5a1d5cf86190b0bdd7bff2569fd7fb3d254fd2bd5bae1e7fe" => :mojave
-    sha256 "79c1bdd07fa75983d06f4a6c27e81741d0553e3635dd3836c65dc6f3c08c0954" => :high_sierra
-    sha256 "227895c908fda25c07555e5514b067d086d6d961f71a40c2098763cfd63deccd" => :sierra
-    sha256 "ba94977585c50e29025be7e592a31809d2f520d50fdb8f023a445dc78ca1bd9d" => :el_capitan
+    sha256 "9c6b4e6397de78906ef8bb8f4890734db29061bbbfe0057b6a7f96f7a8deb60f" => :mojave
+    sha256 "cf9b582e4bee560127d4a225c2c7acff0b8365e4e6c3da78081f8aed5b831ffb" => :high_sierra
+    sha256 "a363ff4c9dbdb7ed68cc4d0c005115737aa36454bb2345df511cb3ee675ed4f7" => :sierra
   end
 
   depends_on "sphinx-doc" => :build
