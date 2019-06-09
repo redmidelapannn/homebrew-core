061d13177eed1f7c6695d30ee84d65d16d895959
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-09 05:06:35 +0000

    oksh: add 6.5 bottle.

--- Formula/oksh.rb
+++ Formula/oksh.rb
@@ -6,6 +6,13 @@ class Oksh < Formula
 
   head "https://github.com/ibara/oksh.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9abf6ee14cbd997bb052171db8875260f0cbfdc0ad45e921d9924c058ff382cc" => :mojave
+    sha256 "009fc7998ca23617ba13d0e1a68ffe96a41bcdfdfbd701900c22dbef34606bbd" => :high_sierra
+    sha256 "3593b4c6e1aeea9295c9b83edbb5ff3a4373af8239746475154cd7fdd20db874" => :sierra
+  end
+
   def install
     args = %W[
       --prefix=#{prefix}
