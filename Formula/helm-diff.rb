e355f95a7b2113a7c9bd63e122824c930de065c8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-10 01:38:43 +0000

    helm-diff: add 2.11.0 bottle.

--- Formula/helm-diff.rb
+++ Formula/helm-diff.rb
@@ -5,6 +5,13 @@ class HelmDiff < Formula
     :tag      => "v2.11.0+5",
     :revision => "33ee5a32d005db46d9efefeecb97d7b9aa125428"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "353400e8e5a4a41ebb0101ccf0e45822e64ca79f4cda35f21a1a2fd9ae60cff2" => :mojave
+    sha256 "e847c966f5c07a0389337f98e2d3c819b3ac084ab7867b62f8eeec087c162e4d" => :high_sierra
+    sha256 "8319bda38db207041b193d88c9f291cc6e2b09fb373a43b9ef0c59f21fa559d6" => :sierra
+  end
+
   depends_on "glide" => :build
   depends_on "go" => :build
   depends_on "kubernetes-cli"
