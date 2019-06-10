c4c8290bf5d2d6d44b41345ac2e8243002a292ac
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-10 06:49:24 +0000

    mtn: add 3.3.2 bottle.

--- Formula/mtn.rb
+++ Formula/mtn.rb
@@ -4,6 +4,13 @@ class Mtn < Formula
   url "https://gitlab.com/movie_thumbnailer/mtn/-/archive/3.3.2/mtn-3.3.2.tar.gz"
   sha256 "471ec0172a1753d684032edb8296ef01af115538619daa417678ac037328daf5"
 
+  bottle do
+    cellar :any
+    sha256 "10cc46631649299f0ff385e440531298b659e7035dfc039ebf348a5089c30f66" => :mojave
+    sha256 "0a39b13209580dea4c75d2ffaad77b4c46e139cae26b5e5685316289c90230d9" => :high_sierra
+    sha256 "92cf3c396477016008079d49d2c3bacc54ecd6fb84b88b4e7cfa86e6d3ad1286" => :sierra
+  end
+
   depends_on "ffmpeg"
   depends_on "gd"
 
