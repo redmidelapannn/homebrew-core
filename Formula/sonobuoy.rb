3756e35d76a0e5131b5679e8375672795969b489
Scott Crooks <scott.crooks@gmail.com>
2019-06-14 19:17:07 +0200

    sonobuoy 0.14.3

--- Formula/sonobuoy.rb
+++ Formula/sonobuoy.rb
@@ -1,8 +1,8 @@
 class Sonobuoy < Formula
   desc "Kubernetes component that generates reports on cluster conformance"
   homepage "https://github.com/heptio/sonobuoy"
-  url "https://github.com/heptio/sonobuoy/archive/v0.14.2.tar.gz"
-  sha256 "0c84da21a31c4cb386e45e28daed6b0ae6ec7fdd4507c0321fdfce92f7b72c3e"
+  url "https://github.com/heptio/sonobuoy/archive/v0.14.3.tar.gz"
+  sha256 "94727a3e82f1a89d1933b6724861829b113202063fdd738f0ab6456a27530a4a"
 
   bottle do
     cellar :any_skip_relocation
