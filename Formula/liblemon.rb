4e95ece9fc537336b95d41096b603fed80fa5e2c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-15 06:28:47 +0000

    liblemon: add 1.3.1 bottle.

--- Formula/liblemon.rb
+++ Formula/liblemon.rb
@@ -3,6 +3,13 @@ class Liblemon < Formula
   homepage "https://lemon.cs.elte.hu"
   url "https://lemon.cs.elte.hu/pub/sources/lemon-1.3.1.tar.gz"
   sha256 "71b7c725f4c0b4a8ccb92eb87b208701586cf7a96156ebd821ca3ed855bad3c8"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "705bae4e313695ab0941a87526fe32d2298c8e3cd17deb569b73069a8c0aa960" => :mojave
+    sha256 "ca35ca236801ac3a7bae658895365f93b8e1e95ffcdb10a074acfafad63876b5" => :high_sierra
+    sha256 "231f0d46905623b7afaf70f118fa5a2efe22631211930403a54dadcfe367703d" => :sierra
+  end
+
   depends_on "cmake" => :build
   def install
     system "cmake", ".", *std_cmake_args
