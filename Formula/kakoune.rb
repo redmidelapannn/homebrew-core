afa5f4dda6ab560ec86340648eec3e0b783de2a3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-15 01:08:31 +0000

    kakoune: update 2019.01.20 bottle.

--- Formula/kakoune.rb
+++ Formula/kakoune.rb
@@ -7,9 +7,10 @@ class Kakoune < Formula
 
   bottle do
     cellar :any
-    sha256 "898cd671f9407a36f4f0a039113251fcb0c18839415b25067de7b823cbcf7122" => :mojave
-    sha256 "a5992a3e250758a4026927218c78b387cc7054de11353f59c28278f2236a8e17" => :high_sierra
-    sha256 "225f222b1abe79c31cfaebf60756eef4f47d79a365e75c03261e143f21ea52af" => :sierra
+    rebuild 1
+    sha256 "18214520f6c2e1ad806510aa511bd3a558dc59de254a598750dbb1f2c941bb02" => :mojave
+    sha256 "cdeee9f4c311e51b551ac4ce64f5c4c4f38040daf537a9a3c61fceea6b8bb857" => :high_sierra
+    sha256 "07f850f2f951308992bd7856304470aaae934d45a3f395507f3a077fd7f72e0a" => :sierra
   end
 
   depends_on "asciidoc" => :build
