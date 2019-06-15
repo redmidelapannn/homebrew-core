671b8a9b6dd0181fc39395e9bc34db8a53fa4794
Jarry Shaw <jarryshaw@icloud.com>
2019-06-15 21:41:00 +0800

    vermin 0.6.0 (new formula)
    `vermin` is a utility tool to detect the minimum Python versions needed to run code.

--- Formula/vermin.rb
+++ Formula/vermin.rb
@@ -3,9 +3,10 @@ class Vermin < Formula
 
   desc "Concurrently detect the minimum Python versions needed to run code"
   homepage "https://github.com/netromdk/vermin"
-  url "https://github.com/netromdk/vermin/archive/v0.4.10.tar.gz"
-  sha256 "2b653a9440257867144bdec24ffa3493ac45c8f8ea4dabe4e5441991817b1f77"
-  head "https://github.com/netromdk/vermin.git"
+  url "https://github.com/netromdk/vermin/archive/v0.6.0.tar.gz"
+  sha256 "2271537cb54376481b852c852a35e9b09f6170f848dd8397f3c2dee4e0f07db8"
+
+  head "https://github.com/netromdk/vermin.git", :branch => "master"
 
   depends_on "python"
 
@@ -14,7 +15,8 @@ class Vermin < Formula
   end
 
   test do
-    path = Pathname.glob(libexec/"lib/python?.?/site-packages/vermin")[0]
+    version = Language::Python.major_minor_version "python3"
+    path = libexec/"lib/python#{version}/site-packages/vermin"
     assert_match "Minimum required versions: 2.7, 3.0", shell_output("#{bin}/vermin -q #{path}")
   end
 end
