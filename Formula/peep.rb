584ca1cfc87efe29f66558aa1d7106578a46cd3f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-17 16:10:22 +0000

    peep: add 0.1.4 bottle.

--- Formula/peep.rb
+++ Formula/peep.rb
@@ -5,6 +5,13 @@ class Peep < Formula
   sha256 "f96aefc41d041402f185b4ab14c0f4a31db62a6ed49eae015427b4e2d50b4b6b"
   head "https://github.com/ryochack/peep.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "dcc8f26f09c981b38a000af4c4ed0797076e0b05d95ad017da02df3e27411e47" => :mojave
+    sha256 "df5b6ac13093d14e43b2e4e533e95b063d90544744d4087011978a21fae1818b" => :high_sierra
+    sha256 "07c7837a2473f88bfe02769ef2b9f6faa32cc8cfa86b4836d488890df159e979" => :sierra
+  end
+
   depends_on "rust" => :build
 
   def install
