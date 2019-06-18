5e02b4587010660b8a0516e9a766c7d261d4ba5c
Chris Lawrence <lordsutch@gmail.com>
2019-06-17 22:01:04 -0400

    update sha256sum

--- Formula/pypy3.rb
+++ Formula/pypy3.rb
@@ -1,8 +1,8 @@
 class Pypy3 < Formula
   desc "Implementation of Python 3 in Python"
   homepage "https://pypy.org/"
-  url "https://bitbucket.org/pypy/pypy/downloads/pypy3.6-v7.1.1-src.tar.bz2"
-  sha256 "7ccbf81db5c647fa0c27636c7d18d059d2570fff7eaffc03857c67bee84b8a26"
+  url "https://bitbucket.org/pypy/pypy/downloads/pypy2.7-v7.1.1-src.tar.bz2"
+  sha256 "6a3ef876e3691a54f4cff045028ec3be94ab9beb2e99f051b83175302c1899a8"
 
   bottle do
     cellar :any
