0db92e1dedc67312eb5466dbf9160da1cf5b89bc
Claudio Satriano <satriano@gmail.com>
2019-06-19 17:40:16 +0200

    Shorten description

--- Formula/gmt.rb
+++ Formula/gmt.rb
@@ -1,5 +1,5 @@
 class Gmt < Formula
-  desc "Tools for manipulating geographic and Cartesian data and creating plots, maps, and 3D views"
+  desc "Tools for manipulating and plotting geographic and Cartesian data"
   homepage "https://www.generic-mapping-tools.org"
   url "https://github.com/GenericMappingTools/gmt/releases/download/6.0.0rc1/gmt-6.0.0rc1-src.tar.xz"
   # url "ftp://ftp.soest.hawaii.edu/gmt/gmt-5.4.5-src.tar.gz"
