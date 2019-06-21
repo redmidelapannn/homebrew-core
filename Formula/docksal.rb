5d5f7b8a0eb6c7ee145caa185e229d77a0db753a
Grant Steuart <gmsteuart@gmail.com>
2019-06-21 15:09:41 -0500

    docksal 1.12.3 (new formula)

---
+++ Formula/docksal.rb
@@ -0,0 +1,27 @@
+class Docksal < Formula
+  desc "Docker powered web development environments for macOS, Windows & Linux"
+  homepage "https://docksal.io"
+  url "https://github.com/docksal/docksal/archive/v1.12.3.tar.gz"
+  sha256 "252a8ce0194fbcacb0a2b7dd6d578843f4ada2cd31c1859350a53791827932a0"
+
+  def install
+    mkdir_p "~/.docksal"
+    touch "~/.docksal/docksal.env"
+    system "echo", "'DOCKER_NATIVE=1'", ">>", "~/.docksal/docksal.env"
+    bin.install "bin/fin"
+  end
+
+  def caveats
+    s = <<-EOS.undent
+      VirtualBox can be installed via `brew cask install virtualbox`
+      Docker can be installed via `brew cask install docker`
+      For Sandbox Server support run: `echo 'CI=1' >> ~/.docksal/docksal.env`
+      For Katacoda mode support run: `echo 'KATACODA=1' >> ~/.docksal/docksal.env`
+    EOS
+    s
+  end
+
+  test do
+    system "#{bin}/fin", "--version"
+  end
+end
