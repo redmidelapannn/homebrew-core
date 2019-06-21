19cde38b1d467d52177af917ca4b5ad07223f23f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-21 20:45:38 +0000

    docksal: add 1.12.3 bottle.

--- Formula/docksal.rb
+++ Formula/docksal.rb
@@ -4,6 +4,13 @@ class Docksal < Formula
   url "https://github.com/docksal/docksal/archive/v1.12.3.tar.gz"
   sha256 "252a8ce0194fbcacb0a2b7dd6d578843f4ada2cd31c1859350a53791827932a0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9702169bb884bbdd11d6fb4165ab4559526ff2e0f27ef791fe403004bc485c5b" => :mojave
+    sha256 "9702169bb884bbdd11d6fb4165ab4559526ff2e0f27ef791fe403004bc485c5b" => :high_sierra
+    sha256 "a9b798cd64d7fdcaac72b9cfd1ce1ca38bd4445bc98ef9dc6af168cd2a4b0d62" => :sierra
+  end
+
   def install
     mkdir_p "~/.docksal"
     touch "~/.docksal/docksal.env"
