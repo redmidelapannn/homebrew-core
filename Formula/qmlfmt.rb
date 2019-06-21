2b0510774181e8568514511fe4c6478e025b97ca
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-21 11:12:35 +0000

    qmlfmt: add 1.0.85 bottle.

--- Formula/qmlfmt.rb
+++ Formula/qmlfmt.rb
@@ -4,6 +4,13 @@ class Qmlfmt < Formula
   version "1.0.85"
   url "https://github.com/jesperhh/qmlfmt/archive/#{version}.tar.gz"
   sha256 "c78a79781f2499ffeadacfa3beea7752f8b7e511c45d163007aca73fbd066b2e"
+  bottle do
+    cellar :any
+    sha256 "43e3173f6a8ab7be3814747a71f6dd1443ee045870a17f6c48df02734e2b4fad" => :mojave
+    sha256 "6aaf6430e88458db8fe7d005f8a1f5c48d1517bd8e1eccf230bd90cd281e766f" => :high_sierra
+    sha256 "2d8005a6cfba645fa5c160a67d437cd03e8c27a4976ee60b78d5728792ebbc23" => :sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "qt" => :build
 
