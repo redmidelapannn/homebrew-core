3d64504f3ba017fcf53918eb0c50acbae5bcba4a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-22 14:19:16 +0000

    dish: add 1.19.1 bottle.

--- Formula/dish.rb
+++ Formula/dish.rb
@@ -4,6 +4,13 @@ class Dish < Formula
   url "https://freeshell.de/~drimiks/gnu/download.cgi/progs/dish/dish-1.19.1.tar.gz"
   sha256 "eaddd3f97240f00714879cfa6e279a652bc2afad106ff1da6c5083ff27af3bf0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "76dd30eff53b36fbff1e15b52689ee0cc165dc3062ba124a774c4bc19e1129e8" => :mojave
+    sha256 "76dd30eff53b36fbff1e15b52689ee0cc165dc3062ba124a774c4bc19e1129e8" => :high_sierra
+    sha256 "7db8a8f01aa712540d70a4af84a9644c4a36f255101cfff906d910aaf652deda" => :sierra
+  end
+
   def install
     mkdir_p bin
     mkdir_p man1
