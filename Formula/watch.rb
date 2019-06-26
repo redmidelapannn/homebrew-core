697dc3559f221b7a3ad8b75d0f7a1256c34ecbb4
Thomas Frank <t.frank@tum.de>
2019-06-26 16:00:00 +0200

    Update watch.rb

--- Formula/watch.rb
+++ Formula/watch.rb
@@ -17,6 +17,7 @@ class Watch < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
+  depends_on "ncurses" => :build
   depends_on "pkg-config" => :build
 
   depends_on "gettext"
