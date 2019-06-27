3efd2276a3b776ba919819a242c6dc8c0a391c2f
Diogo Azevedo <diogoazevedos@gmail.com>
2019-06-27 12:06:35 -0300

    node 12.5.0

--- Formula/node.rb
+++ Formula/node.rb
@@ -1,8 +1,8 @@
 class Node < Formula
   desc "Platform built on V8 to build network applications"
   homepage "https://nodejs.org/"
-  url "https://nodejs.org/dist/v12.4.0/node-v12.4.0.tar.gz"
-  sha256 "cf39befb8ea486363cac6f297d8f451fd0d5dd8ad21dca51e9c2ea86b242baa0"
+  url "https://nodejs.org/dist/v12.5.0/node-v12.5.0.tar.gz"
+  sha256 "3bdae2fdd8f176de2b3225d073847cff9c8ea4374b2f5ed17b07d87ace64a029"
   head "https://github.com/nodejs/node.git"
 
   bottle do
