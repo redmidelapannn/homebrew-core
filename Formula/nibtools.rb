94ba28440f5907fb3d02e18a9e1191a2609f857f
Percival41 <50451009+percival41@users.noreply.github.com>
2019-06-28 00:19:19 -0400

    nibtools 2014r649 (new formula) Commodore 1541/1571 disk image nibbler

---
+++ Formula/nibtools.rb
@@ -0,0 +1,25 @@
+class Nibtools < Formula
+  desc "Commodore 1541/1571 disk image nibbler"
+  homepage "https://c64preservation.com/dp.php?pg=nibtools"
+  url "https://c64preservation.com/svn/nibtools/trunk", :using => :svn, :revision => "649"
+  version "2014"
+  sha256 "4e4bb0d2872084ae45d208ccf0868e1901fab99eb17e972e2f0351de63ac840a"
+
+  depends_on "cc65" => :build
+  depends_on "opencbm" => :build
+
+  def install
+    system "make", "-f", "GNU/Makefile", "linux"
+    mkdir bin.to_s
+    cp %w[nibread nibwrite nibconv nibscan nibrepair nibsrqtest], bin.to_s
+  end
+
+  test do
+    assert_path_exist("#{bin}/nibread")
+    assert_path_exist("#{bin}/nibwrite")
+    assert_path_exist("#{bin}/nibconv")
+    assert_path_exist("#{bin}/nibscan")
+    assert_path_exist("#{bin}/nibrepair")
+    assert_path_exist("#{bin}/nibsrqtest")
+  end
+end
