f06900b793a8741c853c60da14a7923312fba68b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-28 16:59:21 +0000

    nibtools: add 2014 bottle.

--- Formula/nibtools.rb
+++ Formula/nibtools.rb
@@ -5,6 +5,13 @@ class Nibtools < Formula
   version "2014"
   sha256 "4e4bb0d2872084ae45d208ccf0868e1901fab99eb17e972e2f0351de63ac840a"
 
+  bottle do
+    cellar :any
+    sha256 "21e93da68f3981f2775c2005a124bb7c5f6860722f0c675ebe5547a936c3bf26" => :mojave
+    sha256 "bf621d61c718e1975c2a7406a6c9355b3bee26f443133aff67d100a05c6a42f3" => :high_sierra
+    sha256 "b5b33d8c840fc87d9fc6ffb780026b47fdcacede406dd9205fbeffa32eb91c31" => :sierra
+  end
+
   depends_on "cc65" => :build
   depends_on "opencbm" => :build
   depends_on "opencbm"
