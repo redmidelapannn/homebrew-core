3e20ec875134f8d342ae485e2201dc44636f6be2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-29 12:37:58 +0000

    icecast: update 2.4.3 bottle.

--- Formula/icecast.rb
+++ Formula/icecast.rb
@@ -6,9 +6,9 @@ class Icecast < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "f5d7003e198e5106a08c7ecdad8124c1bfba72a82e7d6ef7ffa479ca59aacc1b" => :mojave
-    sha256 "29b910add5cd848b1cbfb91ef13632d7b6a0df8dc42fc8425b6a73062168efbc" => :high_sierra
+    rebuild 2
+    sha256 "3d4628446598c24013c09ac338fff79169145b4202dd0ba2f39454e7f2b3054d" => :mojave
+    sha256 "1c7b30580cb6e0e540c6f4bb7928493093dda65ba2cd714f80d72c5065336ea6" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
