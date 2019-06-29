174ff42395129e55f8b365958286bfcd64b1689a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-06-29 12:37:54 +0000

    avro-c: update 1.9.0 bottle.

--- Formula/avro-c.rb
+++ Formula/avro-c.rb
@@ -5,9 +5,10 @@ class AvroC < Formula
   sha256 "80568644384828b7f926efe76965907c8fff0c49e02301bfe8b2cb7965fa08b2"
 
   bottle do
-    sha256 "72fbe9c22be502351d5dcffcb37a97c754f3bb3057742bf31b185b0a0f1d5eb4" => :mojave
-    sha256 "6a8b708a052cd04ffb7135b4d69b0f6b6a60ac83dc071622dfaf241f26998c79" => :high_sierra
-    sha256 "e194efdbbebb86f6f553b039251165fa87646a4e21d29916560f6ce73ef8281b" => :sierra
+    rebuild 1
+    sha256 "a7189199ac9c54216b81278bb20f56243c98a276934b03b630f137d35fdc51b5" => :mojave
+    sha256 "7711830f2dc159938f5b074edb80f7c6161cd7208d2f0cb43231f3b48350b6fe" => :high_sierra
+    sha256 "4735fc0a5d163ad3f730cc9067ff255959ee86b4a24402d01c55c94cb397f2c8" => :sierra
   end
 
   depends_on "cmake" => :build
