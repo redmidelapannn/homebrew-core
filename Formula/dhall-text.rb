511d00acc20accc2f49b5b44487178889625532a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-02 11:03:44 +0000

    dhall-text: add 1.0.18 bottle.

--- Formula/dhall-text.rb
+++ Formula/dhall-text.rb
@@ -9,6 +9,13 @@ class DhallText < Formula
   sha256 "4d6f9477806cfe32ee415ca5763c1c0ded7b3137215a619a2fd663fa8e919bdb"
   head "https://github.com/dhall-lang/dhall-haskell.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d7640e83d6ae129321477428b49b6e4c2ab57a7f58adb2b6988d774fed153d5e" => :mojave
+    sha256 "7a7028d74908e12d298d68424aedeb6c6e28ad074b7beb7eb450d3820f6c03d9" => :high_sierra
+    sha256 "97cfb0b71e72ccde6bb364c7a3f11181f92a3df19e6682ab10865ff2800b732d" => :sierra
+  end
+
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
 
