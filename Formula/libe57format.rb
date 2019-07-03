e2b2ef998ead9d15ed9252ec24c2faae0629215d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-03 09:36:19 +0000

    libe57format: add 2.0.1 bottle.

--- Formula/libe57format.rb
+++ Formula/libe57format.rb
@@ -3,6 +3,13 @@ class Libe57format < Formula
   homepage "https://github.com/asmaloney/libE57Format"
   url "https://github.com/asmaloney/libE57Format/archive/v2.0.1.tar.gz"
   sha256 "35a8e2990ad51623373a55dc63f8af997debc5c77703594c6b9b6fb4e08e1052"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "88cf82fc93ab7416a61a62e5cc6d819a43eaa601377dfbaeff2354f52cd8fdcc" => :mojave
+    sha256 "19ddf5c41f0f96ed7b97a181d753b36d7d9e34cce4ae03bcff16fe61b973287a" => :high_sierra
+    sha256 "f3c2dcb761b8838f5c5c391116428813f21cffbc6f7a78762bbca0fdf7f25c23" => :sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "xerces-c"
 
