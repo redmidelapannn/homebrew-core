b0f9cabd8b5d3f5fe43db43b6981d8ef4b8740c4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-04 13:51:05 +0000

    hashcat: update 5.1.0 bottle.

--- Formula/hashcat.rb
+++ Formula/hashcat.rb
@@ -9,10 +9,9 @@ class Hashcat < Formula
   head "https://github.com/hashcat/hashcat.git"
 
   bottle do
-    sha256 "e3b79b41df423b3574012ffa580e92b6b4e38d24bdfc652692af128e6cafb1ce" => :mojave
-    sha256 "4f16d55d6180cf0f4dc8bb93c5c747919ff81ca73f49235c9a906020508ae44e" => :high_sierra
-    sha256 "d03c80e77174389dc5c6983a7400810abcf07ecd8cdc88de546a185a9014c727" => :sierra
-    sha256 "63ca34c2ed34998d779e906210e06f3f46c4becc9410c00985dfdecf0daf5f8f" => :el_capitan
+    sha256 "121d2afe61eb40be19f340576842e48167ca6d43578768e9c4be1218130e27ba" => :mojave
+    sha256 "65802e7fffbc34a77dced2d684c56794605303874d23a44cddc18f9fdc65c16d" => :high_sierra
+    sha256 "a5093a3ab89e162f0201f6f3d2bd06ecc44591301256afa068d84bc876dd527a" => :sierra
   end
 
   depends_on "gnu-sed" => :build
