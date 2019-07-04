1a98dd4c4474af5b6ca3b67ad74e697e16fca342
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-04 12:03:27 +0000

    gitfit: add 0.5.3 bottle.

--- Formula/gitfit.rb
+++ Formula/gitfit.rb
@@ -4,6 +4,13 @@ class Gitfit < Formula
   url "https://github.com/cjpatoilo/gitfit/archive/v0.5.3.zip"
   sha256 "4b8fe1e7a451d140462ea983408859160ec0bff35055f484cb1656c078ba04f5"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "306585bc997fc6e4b2fd42d404e5d66917f01d1a7646576c6441c983f696ea44" => :mojave
+    sha256 "306585bc997fc6e4b2fd42d404e5d66917f01d1a7646576c6441c983f696ea44" => :high_sierra
+    sha256 "d9fe581d07345635e4bc1bc56a42e119b23a8b990a9dd9a2a33b5fae6f3614e9" => :sierra
+  end
+
   depends_on "git"
 
   def install
