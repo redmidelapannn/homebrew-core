be22edc4262e0b0ea602bdf92ec01308de0f0a2a
ak2k <akirby@gmail.com>
2019-07-11 15:25:22 -0400

    wordgrinder 0.7.2: fix head
    fix make install --HEAD by correcting head location

--- Formula/wordgrinder.rb
+++ Formula/wordgrinder.rb
@@ -3,7 +3,7 @@ class Wordgrinder < Formula
   homepage "https://cowlark.com/wordgrinder"
   url "https://github.com/davidgiven/wordgrinder/archive/0.7.2.tar.gz"
   sha256 "4e1bc659403f98479fe8619655f901c8c03eb87743374548b4d20a41d31d1dff"
-  head "https://github.com/davidgiven/wordgrinder"
+  head "https://github.com/davidgiven/wordgrinder.git"
 
   depends_on "lua"
   depends_on "ninja"
