ae366839dde378f8d6dfbd6878c23bc96dfe049b
David Dymko <dymkod@gmail.com>
2019-07-15 12:50:55 -0400

    vultr-cli formula

---
+++ Formula/vultr-cli.rb
@@ -0,0 +1,26 @@
+class VultrCli < Formula
+  desc "Official command line tool for Vultr services"
+  homepage "https://github.com/vultr/vultr-cli"
+  url "https://github.com/vultr/vultr-cli/archive/v0.1.2.tar.gz"
+  sha256 "ebf98ab885e0bb5de0f4448af26919df3d2db2114672aea4d2941f88e0f87410"
+  head "https://github.com/vultr/vultr-cli.git"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["GO111MODULE"] = "on"
+    dir = buildpath / "src/github.com/vultr/vultr-cli"
+    dir.install buildpath.children
+
+    cd dir do
+      system "go", "mod", "vendor"
+      system "go", "build"
+      bin.install "vultr-cli"
+    end
+  end
+
+  test do
+    system "vultr-cli", "version"
+  end
+end
