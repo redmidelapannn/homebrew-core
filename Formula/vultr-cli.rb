76fb5babe3bda6e7fa7fa9d5ded2254614471828
David Dymko <dymkod@gmail.com>
2019-07-15 13:40:12 -0400

    updatse to vultr-cli formula

--- Formula/vultr-cli.rb
+++ Formula/vultr-cli.rb
@@ -1,5 +1,5 @@
 class VultrCli < Formula
-  desc "Official command line tool for Vultr services"
+  desc "Official command-line tool for Vultr services"
   homepage "https://github.com/vultr/vultr-cli"
   url "https://github.com/vultr/vultr-cli/archive/v0.1.2.tar.gz"
   sha256 "ebf98ab885e0bb5de0f4448af26919df3d2db2114672aea4d2941f88e0f87410"
@@ -10,17 +10,17 @@ class VultrCli < Formula
   def install
     ENV["GOPATH"] = buildpath
     ENV["GO111MODULE"] = "on"
-    dir = buildpath / "src/github.com/vultr/vultr-cli"
+    dir = buildpath/"src/github.com/vultr/vultr-cli"
     dir.install buildpath.children
 
     cd dir do
-      system "go", "mod", "vendor"
-      system "go", "build"
-      bin.install "vultr-cli"
+      system "go", "build", "-o", bin/"vultr-cli", "-ldflags", "-X main.Version=v#{version}"
+      prefix.install_metafiles
     end
   end
 
   test do
-    system "vultr-cli", "version"
+    output = shell_output("#{bin}/vultr-cli version 2>&1", 1)
+    assert_match "Please export your VULTR API Key", output
   end
 end
