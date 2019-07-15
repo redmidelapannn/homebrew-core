87f9bb1ead916b9615dc44ca38626bc17afd2ee5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-15 18:53:07 +0000

    vultr-cli: add 0.1.2 bottle.

--- Formula/vultr-cli.rb
+++ Formula/vultr-cli.rb
@@ -5,6 +5,13 @@ class VultrCli < Formula
   sha256 "ebf98ab885e0bb5de0f4448af26919df3d2db2114672aea4d2941f88e0f87410"
   head "https://github.com/vultr/vultr-cli.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7ca3b13a81bdc92d474c931648d1b9dc659d1516c5582b641202645287c24950" => :mojave
+    sha256 "16d90e1f17699abc04100bde5878de7565e9dffbf2be14e51779262512e6352a" => :high_sierra
+    sha256 "55f01456e0749b88f603c968e8deedca5cdd781f1c07e24aa629136573709097" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
