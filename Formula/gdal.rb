3410ed79e78f24933a4063ed9445150ced2907f4
luz.paz <luzpaz@users.noreply.github.com>
2019-07-16 13:34:19 -0400

    gdal: revision for netcdf

--- Formula/gdal.rb
+++ Formula/gdal.rb
@@ -3,7 +3,7 @@ class Gdal < Formula
   homepage "https://www.gdal.org/"
   url "https://download.osgeo.org/gdal/2.4.2/gdal-2.4.2.tar.xz"
   sha256 "dcc132e469c5eb76fa4aaff238d32e45a5d947dc5b6c801a123b70045b618e0c"
-  revision 2
+  revision 3
 
   bottle do
     sha256 "c7ad8cc7d44e6604063e85f3b6d68a2537b65805e2956ec5d690ee5d179cbb4f" => :mojave
