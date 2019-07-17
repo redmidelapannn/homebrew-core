175e4000dfdf8d9ed04d0eaa647aaad27663de8d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-17 11:02:12 +0000

    nsaname: add 0.1.0 bottle.

--- Formula/nsaname.rb
+++ Formula/nsaname.rb
@@ -7,6 +7,13 @@ class Nsaname < Formula
   url "https://github.com/mdoza/nsaname/archive/v0.1.0.tar.gz"
   version "0.1.0"
   sha256 "358973ca504d56261ea6f5d2c422699aeabfb25d0f24afa9ed4066ab4f9fc697"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "463ee4fa905a95515fba411656d3e17255ec94072b3003e61baa47b7ce6cc3ad" => :mojave
+    sha256 "de670de45a3e106fc792d1ae1d8898dadffd586ce7ecb43c314c05bf53715c40" => :high_sierra
+    sha256 "6078647d65436d1f99b633160e37ad6269d5afea7b35171cbb1259a56c9e75c6" => :sierra
+  end
+
   # depends_on "cmake" => :build
 
   def install
