6906f78edcab717164818fd3e7532c7eada0c9a7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-18 23:53:18 +0000

    miniconda: add 3 bottle.

--- Formula/miniconda.rb
+++ Formula/miniconda.rb
@@ -8,6 +8,12 @@ class Miniconda < Formula
 
   sha256 "2ec958508139289df3b5e2c10257311af4f0ebf39242f61d39f11e7fa14ebb40"
 
+  bottle do
+    sha256 "ef2c903dbf5c4d63161bd459a06bdf7316ef624116d4bcae56e6510bba95d946" => :mojave
+    sha256 "6f71f204bcd1d558aefcdfc74dc8ece1ca63dcea72ab3ec255e5067ffd89fba7" => :high_sierra
+    sha256 "98030873b5a43971aef6225e16d4ab57735a9a89e10a312fe2ddf0257c2d07ff" => :sierra
+  end
+
   def install
     system "/bin/bash",
       "Miniconda3-4.6.14-MacOSX-x86_64.sh",
