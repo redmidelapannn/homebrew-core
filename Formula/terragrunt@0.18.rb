fdbe96d8f1df81769978956c8043c6786ce8f179
Sam Chowdhury <sam.chowdhury@techholding.co>
2019-07-18 19:29:51 -0700

    terragrunt@0.18 0.18.7

---
+++ Formula/terragrunt@0.18.rb
@@ -0,0 +1,24 @@
+class TerragruntAT018 < Formula
+  desc "Thin wrapper for Terraform e.g. for locking state"
+  homepage "https://github.com/gruntwork-io/terragrunt"
+  url "https://github.com/gruntwork-io/terragrunt/archive/v0.18.7.tar.gz"
+  sha256 "1db9838f2f774599938eca25d7f8266da48693bcfd814292d083ad320f72c742"
+
+  keg_only :versioned_formula
+  depends_on "dep" => :build
+  depends_on "go" => :build
+  depends_on "terraform"
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/gruntwork-io/terragrunt").install buildpath.children
+    cd "src/github.com/gruntwork-io/terragrunt" do
+      system "dep", "ensure", "-vendor-only"
+      system "go", "build", "-o", bin/"terragrunt", "-ldflags", "-X main.VERSION=v#{version}"
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/terragrunt --version")
+  end
+end
