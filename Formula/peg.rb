97fa8d65c65a6b1523aa74172a5ada9afe5ef9eb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-20 20:01:20 +0000

    peg: update 0.1.18 bottle.

--- Formula/peg.rb
+++ Formula/peg.rb
@@ -8,12 +8,10 @@ class Peg < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "335fda7dd0c4cbd0a2c929daf19693729b3e1592f1880f5a1cb2ebd5ae587c3c" => :mojave
-    sha256 "622cd7695294bcac63049e45e934ea1936dfc0f9373046dd028f63a3fe6fa2a4" => :high_sierra
-    sha256 "15dfb147f388a8a486714d17d519a1ad1195f79bad5843d37726e8efaab1ae79" => :sierra
-    sha256 "44d0ab83d1bc3ee71294d328dc70dd14206b8d8ddf05a195f2cdf354d746d5dc" => :el_capitan
-    sha256 "9abe69e43c8e2672aa7b5b26df5c80976c2d0365b5d85d672e8948cebe88646f" => :yosemite
-    sha256 "bbe71ecc8acb17bdf2538f41ae56472bc104a69e310cfd533565507c3468c53c" => :mavericks
+    rebuild 1
+    sha256 "5aa55f5e92e1fb439229e2087f1aaf99bb6d319cf0917f8547459e6b7c2eb207" => :mojave
+    sha256 "0ecc89d2930154bc31c84f14cb030a29d11a7488f74b6a40da28f3884bc54371" => :high_sierra
+    sha256 "87d8094406c8373d6554d9208d04bddd55afaa7e7af74e3b3f44863e2d9a479c" => :sierra
   end
 
   def install
