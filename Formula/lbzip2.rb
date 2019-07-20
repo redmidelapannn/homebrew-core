8a5cf670af06f39cee88df8baa1a77d8c639a0b3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-20 20:01:21 +0000

    lbzip2: update 2.5 bottle.

--- Formula/lbzip2.rb
+++ Formula/lbzip2.rb
@@ -7,12 +7,10 @@ class Lbzip2 < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e9a936e34f1b42f2b7c57deb4eebfa76ee9dd8967960fad81babef5ac8f23867" => :mojave
-    sha256 "d297c1d65f6d255c8fec3afa0b79b9b24e92b5462f47673ba8950a8c81a06b90" => :high_sierra
-    sha256 "e30a2f0d1bb044183857ea0f6c0b314f97b66b800916f71c0788cbdcf58fcdbc" => :sierra
-    sha256 "91c1fb0593205365ff4ada30a34fe7f3afcb1c4e684a3bf239e9168d9fdfc4f7" => :el_capitan
-    sha256 "983c8fe1c23dbfdb73d9e7320e776521c4998169a2d17cd1c6f3035674d8a147" => :yosemite
-    sha256 "7e521c70fadae71ad2e7807cc844183c05751e4a2433d9f1210069fb2a34333e" => :mavericks
+    rebuild 1
+    sha256 "a711b0927f78bd14934f0174b5d027818f87639b949e63edcf65517a90d1485f" => :mojave
+    sha256 "87d72b48c4d6bf9287b71a8d8bcea28e758051320c2f3f1f446c7580622646ce" => :high_sierra
+    sha256 "3d543faba0a62d63005fea8352a17b92248a5b53785871baf8ab15391d42d142" => :sierra
   end
 
   def install
