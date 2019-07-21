8cbbd354f9b8f7efcc9dfc818344ff1d539bb521
Filip Sobalski <pinkeen@gmail.com>
2019-07-21 22:09:52 +0200

    quickjs 2019-07-21 | fix issues reported in review

--- Formula/quickjs.rb
+++ Formula/quickjs.rb
@@ -10,18 +10,8 @@ class Quickjs < Formula
     sha256 "3fbf3dfc902c9e68415088325ec7ec6f16e1c69204b3cd3432046d6bed14896e" => :sierra
   end
 
-  patch :DATA
-
   def install
-    system "make", "prefix=#{prefix}", "CONFIG_M32="
-    # Tests are dependent on having a TTY, so fake it with `script`
-    system "script", "-q", "/dev/stdout", "make", "test", "prefix=#{prefix}", "CONFIG_M32="
     system "make", "install", "prefix=#{prefix}", "CONFIG_M32="
-
-    mkdir_p pkgshare
-
-    cp_r "examples", pkgshare
-    cp_r "doc", pkgshare
   end
 
   test do
@@ -35,39 +25,3 @@ class Quickjs < Formula
     assert_equal "hello", output
   end
 end
-
-__END__
-diff --git a/Makefile b/Makefile.patched
-index 7ca93f0..dde78ff 100644
---- a/Makefile
-+++ b/Makefile.patched
-@@ -120,2 +120,8 @@ endif
- 
-+# LDFLAGS for building dynamically-loadable JS binary modules
-+LDFLAGS_DYNLOAD=$(LDFLAGS)
-+ifdef CONFIG_DARWIN
-+LDFLAGS_DYNLOAD += -undefined dynamic_lookup
-+endif
-+
- PROGS=qjs$(EXE) qjsbn$(EXE) qjsc qjsbnc run-test262 run-test262-bn
-@@ -370,7 +376,3 @@ doc/%.html: doc/%.texi
- 
--ifndef CONFIG_DARWIN
--test: bjson.so
--endif
--
--test: qjs qjsbn
-+test: qjs qjsbn bjson.so
- 	./qjs tests/test_closure.js
-@@ -380,5 +382,3 @@ test: qjs qjsbn
- 	./qjs -m tests/test_std.js
--ifndef CONFIG_DARWIN
- 	./qjs -m tests/test_bjson.js
--endif
- 	./qjsbn tests/test_closure.js
-@@ -460,3 +460,3 @@ bench-v8: qjs qjs32
- bjson.so: $(OBJDIR)/bjson.pic.o
--	$(CC) $(LDFLAGS) -shared -o $@ $^ $(LIBS)
-+	$(CC) $(LDFLAGS_DYNLOAD) -shared -o $@ $^ $(LIBS)
- 
-
