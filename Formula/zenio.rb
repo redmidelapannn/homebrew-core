a526dffcac660b8d65c8eb3ad7a109be4e424ca7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-22 10:47:05 +0000

    zenio: add 0.8.1 bottle.

--- Formula/zenio.rb
+++ Formula/zenio.rb
@@ -4,6 +4,13 @@ class Zenio < Formula
   url "https://dev.zenio.co/cli/zenio-0.8.1.tar"
   sha256 "8838c70497d0d4e99365c32a7a197f90f2d3e67a8b1cab5e5a6ebac4f6d1bc92"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "14119a89fe4e82ac6ba4fc36d0b12d671d701e9566e73793674e48b9d51ee0b1" => :mojave
+    sha256 "14119a89fe4e82ac6ba4fc36d0b12d671d701e9566e73793674e48b9d51ee0b1" => :high_sierra
+    sha256 "b78903195c00b44411e14426863d20d4e76e72d3faffe5508003744619e3770f" => :sierra
+  end
+
   depends_on :java
 
   def install
