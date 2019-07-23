20f4e1ceb71f702d1623e6929cb9d370a3812718
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-23 16:34:35 +0000

    flat: add 0.0.2 bottle.

--- Formula/flat.rb
+++ Formula/flat.rb
@@ -4,6 +4,13 @@ class Flat < Formula
   url "https://github.com/tlopo-ruby/flat.git", :using => :git, :revision => "bde2c6e47d055f7c7f27c06d6438bc2a6a6e3967"
   version "0.0.2"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "355ca4b49b912e96107c87f48685d5bf15981b364de2e2fbed4863fcbec6ddf0" => :mojave
+    sha256 "355ca4b49b912e96107c87f48685d5bf15981b364de2e2fbed4863fcbec6ddf0" => :high_sierra
+    sha256 "663a0a22ed623e94e7a890747b3c46a947ae541fb0a86f119db5a8df6f9aba8d" => :sierra
+  end
+
   def install
     mv "flat.rb", "flat"
     bin.install "flat"
