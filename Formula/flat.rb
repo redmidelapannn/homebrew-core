a8f71a5442c34acf28eeaebd429cc89b8804ace2
Tiago Lopo <tiago_lopo_da_silva@external.mckinsey.com>
2019-07-23 18:28:47 +0200

    Moved flat to github, so we do not need a custom downloader

--- Formula/flat.rb
+++ Formula/flat.rb
@@ -1,22 +1,12 @@
-class CustomDownloadStrategy < CurlDownloadStrategy
-  def fetch
-    CurlDownloadStrategy.new(url, name, version, **meta).fetch
-    ENV["flat_download_location"] = cached_location
-  end
-end
-
 class Flat < Formula
   desc "Flattens json or yaml structure"
-  homepage "https://bit.ly/tiago-flat-script-v0-0-2"
-  url "https://bit.ly/tiago-flat-script-v0-0-2", :using => CustomDownloadStrategy
+  homepage "https://github.com/tlopo-ruby/flat"
+  url "https://github.com/tlopo-ruby/flat.git", :using => :git, :revision => "bde2c6e47d055f7c7f27c06d6438bc2a6a6e3967"
   version "0.0.2"
-  sha256 "97654f6f3d5084c7f8aef147c50be20a2272f4c0c048c731f635dfce38cdabcb"
 
   def install
-    downloaded_file = File.read ENV["flat_download_location"]
-    file = "flat"
-    File.write file, downloaded_file
-    bin.install file
+    mv "flat.rb", "flat"
+    bin.install "flat"
   end
 
   test do
