cfd82322a5e0dbe50be508292b39b6886c319ee9
Yosuke Shiro <yosuke.shiro615@gmail.com>
2019-07-24 20:13:15 +0900

    apache-arrow: support gandiva

--- Formula/apache-arrow.rb
+++ Formula/apache-arrow.rb
@@ -3,6 +3,7 @@ class ApacheArrow < Formula
   homepage "https://arrow.apache.org/"
   url "https://www.apache.org/dyn/closer.cgi?path=arrow/arrow-0.14.1/apache-arrow-0.14.1.tar.gz"
   sha256 "9948ddb6d4798b51552d0dca3252dd6e3a7d0f9702714fc6f5a1b59397ce1d28"
+  revision 1
   head "https://github.com/apache/arrow.git"
 
   bottle do
@@ -20,12 +21,14 @@ class ApacheArrow < Formula
   depends_on "flatbuffers"
   depends_on "glog"
   depends_on "grpc"
+  depends_on "llvm@7"
   depends_on "lz4"
   depends_on "numpy"
   depends_on "openssl"
   depends_on "protobuf"
   depends_on "python"
   depends_on "rapidjson"
+  depends_on "re2"
   depends_on "snappy"
   depends_on "thrift"
   depends_on "zstd"
@@ -34,6 +37,7 @@ class ApacheArrow < Formula
     ENV.cxx11
     args = %W[
       -DARROW_FLIGHT=ON
+      -DARROW_GANDIVA=ON
       -DARROW_ORC=ON
       -DARROW_PARQUET=ON
       -DARROW_PLASMA=ON
