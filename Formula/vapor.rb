9c43322b3f94a4bc58970bcc61fe79208e6b14c8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-25 19:05:18 +0000

    vapor: add 18.0.0-beta.17 bottle.

--- Formula/vapor.rb
+++ Formula/vapor.rb
@@ -10,6 +10,12 @@ class Vapor < Formula
     url "https://github.com/vapor/toolbox/archive/18.0.0-beta.17.tar.gz"
     sha256 "202208026843dbf68ec54a13fe08db1c41d3d39e8c060fdbb723f192b88ecd8d"
   end
+  bottle do
+    cellar :any
+    sha256 "ac691641bd97f761f6022aa7987a35ec9c0f5662146f28b39a434de616860f78" => :mojave
+    sha256 "0ec0e6297c1f3d71ba133a0cff5a5819015002ac8246664e1abf9a021a6cb3d7" => :high_sierra
+  end
+
 
   def install
     system "swift", "build", "--disable-sandbox"
