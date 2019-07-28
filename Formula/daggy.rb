1b08290e123c1d943f8567297d03eb964aad7509
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-07-28 18:59:02 +0000

    daggy: add 1.1.3 bottle.

--- Formula/daggy.rb
+++ Formula/daggy.rb
@@ -4,6 +4,12 @@ class Daggy < Formula
   url "https://github.com/synacker/daggy/archive/1.1.3/daggy-1.1.3.tar.gz"
   sha256 "559a9d7916988edf67be50625d89b38c311edaa322c3238e0107883dc315f0c6"
 
+  bottle do
+    cellar :any
+    sha256 "e6ead2e5971ed38bd6fbacedf67a14a2273d3f74d2395943c4ae3fee48bb1964" => :mojave
+    sha256 "28b869daf423886b9d622ed931ca211660980982dda509bd7275da12c8f08fd8" => :sierra
+  end
+
   depends_on "botan"
   depends_on "qt"
   depends_on "yaml-cpp"
