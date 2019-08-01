c156073450f7a72d4e29ffef86dde575e0c7ab0f
Arthur Factor <46137336+DaFuqtor@users.noreply.github.com>
2019-08-02 00:19:06 +0300

    fix according to audit
    moved sha256 before head

--- Formula/wifi-pass.rb
+++ Formula/wifi-pass.rb
@@ -2,8 +2,8 @@ class WifiPass < Formula
   desc "Copy, QR encode or just get the password of current Wi-Fi connection"
   homepage "https://wifi-pass.ru"
   url "https://github.com/DaFuqtor/wifi-pass/archive/0.2.7.tar.gz"
-  head "https://github.com/DaFuqtor/wifi-pass.git"
   sha256 "c315d03693d707c47ba58e32f3bf9aaf0aa17f799b3838f68a1aa4f77ea9a5cc"
+  head "https://github.com/DaFuqtor/wifi-pass.git"
 
   depends_on "qrencode" => :recommended
 
