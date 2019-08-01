2db07b92f7471605e7757df22b66b639fd60e879
Rui Chen <chenrui333@gmail.com>
2019-07-31 20:49:10 -0400

    convox 20190730145854

--- Formula/convox.rb
+++ Formula/convox.rb
@@ -1,8 +1,8 @@
 class Convox < Formula
   desc "Command-line interface for the Rack PaaS on AWS"
   homepage "https://convox.com/"
-  url "https://github.com/convox/rack/archive/20190717145705.tar.gz"
-  sha256 "7e1557f0284044e373bf094da1cd2465aa5aa0d1be3dc0941d072260977ea6f6"
+  url "https://github.com/convox/rack/archive/20190730145854.tar.gz"
+  sha256 "d5cdf69f2ee0bc4840e58da8f035a51e7a62707eb57286f4a1f51c86bff92b73"
 
   bottle do
     cellar :any_skip_relocation
