69f63bd257f5905d475fe34bd8cc665672d0016f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-01 13:09:27 +0000

    cuts: add 20180815 bottle.

--- Formula/cuts.rb
+++ Formula/cuts.rb
@@ -6,6 +6,13 @@ class Cuts < Formula
   version "20180815"
   sha256 "fda9850588f8090895fd6e68085d6481c8c51ce2eacd5c6dc9736dddf2f488bf"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "0869413c610d5ae8f46cfe7b9683267ffb108229b680ae43232006fa1b642a39" => :mojave
+    sha256 "0869413c610d5ae8f46cfe7b9683267ffb108229b680ae43232006fa1b642a39" => :high_sierra
+    sha256 "b1420fe8fb25f0b1da4efc989da84fe9a714fc63e8f4901c129ded6080aadefc" => :sierra
+  end
+
   def install
     bin.install "cuts"
   end
