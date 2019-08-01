c4110297a728aa0ec89f2fdcfe16b08b376b44b6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-01 23:52:26 +0000

    wifi-pass: add 0.2.7 bottle.

--- Formula/wifi-pass.rb
+++ Formula/wifi-pass.rb
@@ -5,6 +5,13 @@ class WifiPass < Formula
   sha256 "c315d03693d707c47ba58e32f3bf9aaf0aa17f799b3838f68a1aa4f77ea9a5cc"
   head "https://github.com/DaFuqtor/wifi-pass.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ed369572f890fbbddd82e1608deae3e849afab8b780fefea15f5c554e5c361c8" => :mojave
+    sha256 "ed369572f890fbbddd82e1608deae3e849afab8b780fefea15f5c554e5c361c8" => :high_sierra
+    sha256 "e6572d5ed0fcd6e8b2eb9d1a67873ae0f911b98c6bf0c6378fc3fe917b91b209" => :sierra
+  end
+
   depends_on "qrencode"
 
   def install
