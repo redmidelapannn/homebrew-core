a65310be1ffac5b0f70bb5259b487cc699ee4e28
Cedric Beust <cedric@beust.com>
2019-08-02 10:09:28 -0700

    Attempt to fix the compilation error.

--- Formula/kash.rb
+++ Formula/kash.rb
@@ -1,15 +1,16 @@
 class Kash < Formula
   desc "Shell powered by Kotlin"
   homepage "https://github.com/cbeust/kash"
-  url "https://github.com/cbeust/kash/archive/v1.9.tar.gz"
-  sha256 "59211d1844f83416faed5fb388670d909793099bf4e195791651b46df2b097b1"
+  version "1.11"
+  url "https://github.com/cbeust/kash/archive/v#{version}.tar.gz"
+  sha256 "74d5ab5f7b464e36164e360558d628c8bf934818fe1358b0b0bdde112ce0b057"
 
   depends_on :java
 
   def install
     system "./gradlew", "assemble"
-    libexec.install "build/libs/kash-1.9.jar"
-    bin.write_jar_script libexec/"kash-1.9.jar", "kash", :java_version => "11.0"
+    libexec.install "build/libs/kash-#{version}.jar"
+    bin.write_jar_script libexec/"kash-#{version}.jar", "kash", :java_version => "11.0"
   end
 
   test do
