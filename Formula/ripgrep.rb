2816958c91462aeebd3a50f309d51e1b13a30ac2
chenrui <rchen@meetup.com>
2019-08-01 21:38:45 -0400

    Update Formula/ripgrep.rb
    Co-Authored-By: David Yantis <404NetworkError@users.noreply.github.com>

--- Formula/ripgrep.rb
+++ Formula/ripgrep.rb
@@ -2,7 +2,7 @@ class Ripgrep < Formula
   desc "Search tool like grep and The Silver Searcher"
   homepage "https://github.com/BurntSushi/ripgrep"
   url "https://github.com/BurntSushi/ripgrep/archive/11.0.2.tar.gz"
-  sha256 "b218bb70b0546bfb4aa94a5206dc528e96f194eb7b5c5de8e7db23e852788c35"
+  sha256 "0983861279936ada8bc7a6d5d663d590ad34eb44a44c75c2d6ccd0ab33490055"
   head "https://github.com/BurntSushi/ripgrep.git"
 
   bottle do
