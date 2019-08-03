a1de1b5160ed1e3ce9ced3f5da30860abcf7634d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-03 01:55:00 +0000

    ksh: add 2020.0.0.1-alpha bottle.

--- Formula/ksh.rb
+++ Formula/ksh.rb
@@ -5,6 +5,13 @@ class Ksh < Formula
   version "2020.0.0.1-alpha"
   sha256 "48b150392038b0592bd0b07817ff4838dc038f53bb564d4a73a35357fc88802e"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8882183ea6964b3bbbe20dc2b648dafe9188e8c6d7fdc92fd43aa86edec01fc0" => :mojave
+    sha256 "cc662f29a239e8282a7c2b2b1152244f424f125e46b5d57cdefd0919f3d98790" => :high_sierra
+    sha256 "2f8c5c2d6f02e88e343fcdaaee21daa0ca216deeea2350136cde80800ec4dafb" => :sierra
+  end
+
   depends_on "meson" => :build
   depends_on "ninja" => :build
 
