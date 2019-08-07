a0095b92b079017bb97e8577215f52c187eab0ab
Morgan Walker <morgan@skafos.ai>
2019-08-07 11:05:52 -0400

    parago 1.0.0

--- Formula/parago.rb
+++ Formula/parago.rb
@@ -1,38 +1,20 @@
 class Parago < Formula
   desc "A command line tool to help you rapidly create machine learning models"
 	homepage "https://github.com/skafos/parago-cli"
-	url "https://github.com/skafos/parago-cli/<PACKAGE>"
-  sha256 ""
-  depends_on "heroku/brew/heroku-node"
+	url "https://github.com/skafos/parago-cli/releases/download/1.0.0/pgo-v1.0.0.tar.gz"
+  sha256 "0c404cff981a6798e2e1bf866c41efdf36e91d2f8fd6c939293b04657691338f"
+  depends_on "node@12"
 
   def install
-    inreplace "bin/heroku", /^CLIENT_HOME=/, "export HEROKU_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
-    inreplace "bin/heroku", "\"$DIR/node\"", "#{Formula["heroku-node"].opt_share}/node"
+    inreplace "bin/pgo", /^CLIENT_HOME=/, "export pgo_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
     libexec.install Dir["*"]
-    bin.install_symlink libexec/"bin/heroku"
+    bin.install_symlink libexec/"bin/pgo"
+    bin.install_symlink libexec/"bin/parago"
 
-    bash_completion.install "#{libexec}/node_modules/@heroku-cli/plugin-autocomplete/autocomplete/brew/bash"
-    zsh_completion.install "#{libexec}/node_modules/@heroku-cli/plugin-autocomplete/autocomplete/brew/zsh/_heroku"
-  end
-
-  def caveats; <<~EOS
-    To use the Heroku CLI's autocomplete --
-      Via homebrew's shell completion:
-        1) Follow homebrew's install instructions https://docs.brew.sh/Shell-Completion
-            NOTE: For zsh, as the instructions mention, be sure compinit is autoloaded
-                  and called, either explicitly or via a framework like oh-my-zsh.
-        2) Then run
-          $ heroku autocomplete --refresh-cache
-
-      OR
-
-      Use our standalone setup:
-        1) Run and follow the install steps:
-          $ heroku autocomplete
-  EOS
   end
 
   test do
-    system bin/"heroku", "version"
+    system bin/"pgo", "version"
+    system bin/"parago", "version"
   end
 end
