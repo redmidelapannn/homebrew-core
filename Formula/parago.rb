17aaed728aaccdd16ee0f6041109d3652a25dcb6
Morgan Walker <morgan@skafos.ai>
2019-08-07 11:51:44 -0400

    fixing for strict audit

--- Formula/parago.rb
+++ Formula/parago.rb
@@ -1,24 +1,23 @@
-class Parago < Formula
-  desc "A command line tool to help you rapidly create machine learning models"
-	homepage "https://github.com/skafos/parago-cli"
-	url "https://github.com/skafos/parago-cli/releases/download/1.0.0/pgo-v1.0.0.tar.gz"
+class Parago < Formula 
+  desc "command-line tool to help you rapidly create machine learning models"
+  homepage "https://github.com/skafos/parago-cli"
+  url "https://github.com/skafos/parago-cli/releases/download/1.0.0/pgo-v1.0.0.tar.gz"
   sha256 "168ef6b7c655c746feb686344abbc3fd5f1657efdcebba9db82cc70499a2b33c"
-  depends_on "node@12"
+  depends_on "node"
 
   def install
     inreplace "bin/pgo", /^CLIENT_HOME=/, "export pgo_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/pgo"
-
   end
 
   def caveat; <<~EOS
-    
+
     To begin using Parago, please run `pgo setup`.
 
   EOS
   end
-  
+
   test do
     system bin/"pgo", "version"
   end
