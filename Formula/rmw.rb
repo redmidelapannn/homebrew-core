c1b3d73c3b2a5a48cc48e108a9456f40c6073b72
andy5995 <andy400-dev@yahoo.com>
2019-08-07 18:13:03 -0500

    rmw 0.7.03 (new formula)

---
+++ Formula/rmw.rb
@@ -0,0 +1,26 @@
+class Rmw < Formula
+  desc "Safe-remove utility for the command-line"
+  homepage "https://remove-to-waste.info/"
+  url "https://github.com/theimpossibleastronaut/rmw/releases/download/v0.7.03/rmw-0.7.03.tar.gz"
+  sha256 "60750279980b450df5b62f12a7c7153584b41acba7bdb4ecf96def043d50f71a"
+  head "https://github.com/theimpossibleastronaut/rmw.git"
+
+  depends_on "ncurses"
+
+  def install
+    system "./configure", "--enable-debug=no",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    touch("foo")
+    system "#{bin}/rmw"
+    system "#{bin}/rmw", "-v", "foo"
+    system "ls", "foo"
+    system "#{bin}/rmw", "-u"
+    system "ls", "foo"
+  end
+end
