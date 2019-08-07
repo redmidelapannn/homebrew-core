ea223bb22f183e5251acc25c07ea393ad31ebc51
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-07 17:07:34 +0000

    parago: add 1.0.0 bottle.

--- Formula/parago.rb
+++ Formula/parago.rb
@@ -3,6 +3,13 @@ class Parago < Formula
   homepage "https://github.com/skafos/parago-cli"
   url "https://github.com/skafos/parago-cli/releases/download/1.0.0/pgo-v1.0.0.tar.gz"
   sha256 "168ef6b7c655c746feb686344abbc3fd5f1657efdcebba9db82cc70499a2b33c"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a12729de30369d64b729d701ae79b16f161842170f2b76792992015b9f2211f5" => :mojave
+    sha256 "a12729de30369d64b729d701ae79b16f161842170f2b76792992015b9f2211f5" => :high_sierra
+    sha256 "7444419e2e82d974c412e99a1cbe0778d586b406b8cf7804731c3c129a3e73c0" => :sierra
+  end
+
   depends_on "node"
 
   def install
