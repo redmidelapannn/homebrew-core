a56c602d10200ffab25d2557a1677c2e8af73f50
Morgan Walker <morgan@skafos.ai>
2019-08-07 11:53:49 -0400

    buttoning up audits

--- Formula/parago.rb
+++ Formula/parago.rb
@@ -1,5 +1,5 @@
-class Parago < Formula 
-  desc "Command-line tool to help you rapidly create machine learning models."
+class Parago < Formula
+  desc "Command-line tool to help you rapidly create machine learning models"
   homepage "https://github.com/skafos/parago-cli"
   url "https://github.com/skafos/parago-cli/releases/download/1.0.0/pgo-v1.0.0.tar.gz"
   sha256 "168ef6b7c655c746feb686344abbc3fd5f1657efdcebba9db82cc70499a2b33c"
