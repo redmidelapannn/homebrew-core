4073f3ce44b26b54657117a383c553aefb766038
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-12 16:08:50 +0000

    popt: update 1.16 bottle.

--- Formula/popt.rb
+++ Formula/popt.rb
@@ -6,13 +6,11 @@ class Popt < Formula
   sha256 "e728ed296fe9f069a0e005003c3d6b2dde3d9cad453422a10d6558616d304cc8"
 
   bottle do
-    rebuild 1
-    sha256 "20b8ee08d436d2d37782ecddc315cfa1598bd50765e8089070c8b1d1e8355c69" => :mojave
-    sha256 "242ffcbf7f9796f970d45612b025d8be787f739aa5f16a02ce097196d3f56941" => :high_sierra
-    sha256 "ceae94fc8e588309670a7a045186eee8ff3a9966a68650f044a14d101267b7b2" => :sierra
-    sha256 "60a7f19e8fecafd92a5beb7d6438efac915e8f3afe3d83575fb64bb4a6190aab" => :el_capitan
-    sha256 "56d1104516e23bb314a248904b8ec85afe2fdbf71555417eb8f91edc1286e6da" => :yosemite
-    sha256 "ba122e7f34b9b03ab5a32ab01124b61eb608c29e0c0d023462953ed03782dd2a" => :mavericks
+    cellar :any
+    rebuild 2
+    sha256 "e3e96875a6420b31cb5e3642fd4d157dbff084287d4cb686f99f22adbba1af91" => :mojave
+    sha256 "8b72bd4a9c1f1392a094ac1112e35e07088b277fbae310a1082a5f60ff368152" => :high_sierra
+    sha256 "499505e490070071812848e421cce113a84db5dbe1e7d1e40cef11d36791295a" => :sierra
   end
 
   def install
