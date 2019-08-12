539cf15fe01f01b6ee08644f1ee09712fc64efe6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-12 16:08:49 +0000

    libnfc: update 1.7.1 bottle.

--- Formula/libnfc.rb
+++ Formula/libnfc.rb
@@ -5,12 +5,10 @@ class Libnfc < Formula
   sha256 "945e74d8e27683f9b8a6f6e529557b305d120df347a960a6a7ead6cb388f4072"
 
   bottle do
-    sha256 "a6f032e306967d4df3177c22363c8c93336ba314216431c5e4ff3380b3823456" => :mojave
-    sha256 "6123f4b6cba07ac11b327e378b9b774fca3609cec3e7ef58d6dcb6f6270dc36e" => :high_sierra
-    sha256 "18ebd76ab92ace056da2f014b3bbfb872d152ff6dc5d66ae274c7bd67474defa" => :sierra
-    sha256 "792064362ef2b224a15190b6fcf97066c7a1d47a3bee13495134aafb067cc11d" => :el_capitan
-    sha256 "3f2e7a57fca1f12b4b938c6136036f889562540e6c6a8e5188cd32fabd927a9c" => :yosemite
-    sha256 "cabb3f773d92c2cd95af437d6f4c567529229b26b82d568af1c89ec50b674f59" => :mavericks
+    rebuild 1
+    sha256 "9841eaad09019a00406d509922fd6b9c3f104ac132fb75211eb92c1399640054" => :mojave
+    sha256 "f40349e8d6a13842c3c5819e45de698e18d9717013d74bef7819acac5d92af4f" => :high_sierra
+    sha256 "f54a004ccea3be3832e52ef2237489eb7d4a2841ca732aa3302e0a24e005940c" => :sierra
   end
 
   head do
