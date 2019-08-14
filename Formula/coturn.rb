a8c6f21dcafb8eef89aea4650f10c18a94f48044
Minho Ryang <minhoryang@gmail.com>
2019-08-14 14:54:57 +0900

    Update coturn.rb for adding a dependency: hiredis.
    Without hiredis, test command will be failed `turnadmin -l`.
    
    ```bash
    $ turnadmin -l
    dyld: Library not loaded: /usr/local/opt/hiredis/lib/libhiredis.0.14.dylib
      Referenced from: /usr/local/bin/turnadmin
      Reason: image not found
    Abort trap: 6
    ```

--- Formula/coturn.rb
+++ Formula/coturn.rb
@@ -3,7 +3,7 @@ class Coturn < Formula
   homepage "https://github.com/coturn/coturn"
   url "http://turnserver.open-sys.org/downloads/v4.5.1.1/turnserver-4.5.1.1.tar.gz"
   sha256 "e020ce90ea0301213451d37099185ff25d93f97fa0f2b48bf21b2946fc3696a4"
-  revision 1
+  revision 2
 
   bottle do
     sha256 "40ae9111624e581b825f1fb3a7701512f1b8bf86d26ca17c59e5e9d673cd3270" => :mojave
@@ -11,6 +11,7 @@ class Coturn < Formula
     sha256 "2b2ef532930f01cf3b5a2deb3d22e0f706e19febfb60dd8e5ef270f6a2000d12" => :sierra
   end
 
+  depends_on "hiredis"
   depends_on "libevent"
 
   def install
