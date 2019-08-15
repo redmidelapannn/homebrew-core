5e2cb3ff0fbe8c2235858c710e3bef0da2fcdc30
jnozsc <jnozsc@gmail.com>
2019-08-15 12:08:30 -0700

    desc is too long, short it

--- Formula/terragrunt@0.18.rb
+++ Formula/terragrunt@0.18.rb
@@ -1,5 +1,5 @@
 class TerragruntAT018 < Formula
-  desc "Thin wrapper for Terraform e.g. for locking state. v0.18 is the last version supports terraform < 0.12"
+  desc "Thin wrapper for Terraform e.g. for locking state."
   homepage "https://github.com/gruntwork-io/terragrunt"
   url "https://github.com/gruntwork-io/terragrunt/archive/v0.18.7.tar.gz"
   sha256 "1db9838f2f774599938eca25d7f8266da48693bcfd814292d083ad320f72c742"
