72aba1fabf10cafc72259b881adb93e240c3c055
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-16 16:58:06 +0000

    secreter: add 0.0.2 bottle.

--- Formula/secreter.rb
+++ Formula/secreter.rb
@@ -6,6 +6,13 @@ class Secreter < Formula
       :revision => "e3e1c8f0c0e5d17a134786a96a1a2ca6cec8de84"
   head "https://github.com/amaizfinance/secreter.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9fb82eb107ac4263d82d819665439b2606cbcaa7b8cba558d18597330efe61c2" => :mojave
+    sha256 "775c9d9671ce684072c4bec5bb7eccbcb7501110fc76519e5fdbf96f72d4b490" => :high_sierra
+    sha256 "c1fee1858b28028f6b70fb5e9d06c8cb8317243f99fa9149ce1e91333f03e021" => :sierra
+  end
+
   depends_on "bazel" => :build
 
   def install
