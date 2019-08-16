e7e285c90c2843ac84c8a5d5aeeed870085136d2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-16 02:34:31 +0000

    noobaa: add 1.0.2 bottle.

--- Formula/noobaa.rb
+++ Formula/noobaa.rb
@@ -6,6 +6,13 @@ class Noobaa < Formula
       :revision => "e97aff3759afc0d08f20f42484ac475570e9cd4d"
   head "https://github.com/noobaa/noobaa-operator.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5a9113bdf050e582e3df4ad60ae267c3c1a3854be99500e300ea932776e6a08c" => :mojave
+    sha256 "b22f24679a7d11dc00cbbcce76eb47256dc9c0445a2a97dfdd47139fb5ceb2f6" => :high_sierra
+    sha256 "94ec26be9e42c65aaba40231028c8491a85a638c3ea5de08870ae9dfab4e6d63" => :sierra
+  end
+
   depends_on "go"
 
   def install
