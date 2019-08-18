19cfc6c0dad2ddd7ab39b26cee3082c536ca3331
sidneys <sidneys.github.io@outlook.com>
2019-08-18 13:16:35 +0200

    pulseaudio: use Formula name as log file name

--- Formula/pulseaudio.rb
+++ Formula/pulseaudio.rb
@@ -79,9 +79,9 @@ class Pulseaudio < Formula
         <string>--verbose</string>
       </array>
       <key>StandardErrorPath</key>
-      <string>#{home}/Library/Logs/pulseaudio.log</string>
+      <string>#{home}/Library/Logs/#{name}.log</string>
       <key>StandardOutPath</key>
-      <string>#{home}/Library/Logs/pulseaudio.log</string>
+      <string>#{home}/Library/Logs/#{name}.log</string>
     </dict>
     </plist>
   EOS
