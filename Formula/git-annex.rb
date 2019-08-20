e0c42b6808265280ae6c997c892f50728b3f8268
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-20 18:06:26 +0000

    git-annex: update 7.20190819 bottle.

--- Formula/git-annex.rb
+++ Formula/git-annex.rb
@@ -11,9 +11,9 @@ class GitAnnex < Formula
 
   bottle do
     cellar :any
-    sha256 "747b903fe607e86e7e78e089fdc367d7fb08bcd87907af83cd5fbf4eadaea8bd" => :mojave
-    sha256 "90d6130b9f60a4820115803cc19ae28d07443db1eabd2c8a4b01853e88e3fce2" => :high_sierra
-    sha256 "5e5160a60abd6c5cfa08b965d1da079f52f4c9fa972a4d7becb9e3c41b3f82e2" => :sierra
+    sha256 "947b290d4f2ed7a8938bcf2064d80c3fc392a84a4897b7e317d5ef3a8d86f854" => :mojave
+    sha256 "00bceac85471d211bbca6b216f1915aa6883fe96cd76659cd9ad78fc3585ec84" => :high_sierra
+    sha256 "ffb6c7f9e2046dadf82128e0bc3808db0e56b16b0f66ea63c8a5563fd660f544" => :sierra
   end
 
   depends_on "cabal-install" => :build
