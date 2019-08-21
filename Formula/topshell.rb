5e3228a642f23252e1d911485ccf41a5a7dfcdc6
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2019-08-21 01:11:04 +0100

    topshell 0.7.7 (new formula)
    Fixes from audit

---
+++ Formula/topshell.rb
@@ -0,0 +1,33 @@
+require "language/node"
+
+class Topshell < Formula
+  desc "Purely functional, reactive scripting language"
+  homepage "http://show.ahnfelt.net/topshell/"
+  url "https://github.com/topshell-language/topshell/archive/v0.7.7.tar.gz"
+  sha256 "a24c759b231fbd5aaed3409ed5d12e98ae2987cc27672fef27964a21017dccd6"
+
+  depends_on "node@8" => :build
+  depends_on "sbt" => :build
+
+  def install
+    system "sbt", "fullOptJS"
+    cd "node" do
+      system "npm", "install", *Language::Node.local_npm_install_args
+      system "node_modules/.bin/pkg", "-t", "node8-macos-x64", "package.json"
+      bin.install "topshell"
+    end
+  end
+
+  test do
+    begin
+      pid = fork do
+        exec "#{bin}/topshell"
+      end
+      sleep 1
+      output = shell_output("curl -o /dev/null -s -w '%{http_code}\\n' http://localhost:7070/topshell/index.html")
+      assert_match(/200/m, output)
+    ensure
+      Process.kill("HUP", pid)
+    end
+  end
+end
