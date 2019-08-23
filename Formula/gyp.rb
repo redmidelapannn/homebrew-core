0865543568935073904f1b62a2f34d68a54bc25a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-23 00:23:22 +0000

    gyp: add 1.0 bottle.

--- Formula/gyp.rb
+++ Formula/gyp.rb
@@ -2,6 +2,13 @@ class Gyp < Formula
   desc "Meta-Build system: a build system that generates other build systems"
   homepage "https://gyp.gsrc.io"
   url "https://chromium.googlesource.com/external/gyp", :using => :git
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "65767051a16a27d335e947d6174467eeaf0c146a9a517d58992d26d0bcb3c153" => :mojave
+    sha256 "65767051a16a27d335e947d6174467eeaf0c146a9a517d58992d26d0bcb3c153" => :high_sierra
+    sha256 "1c115fe52d5d28d963fadf885f302558e66586b72461a6fcaff4c354e368e91e" => :sierra
+  end
+
   version "1.0" # gyp doesn't actually have any published versions.
 
   depends_on "python"
