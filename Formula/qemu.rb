32dffabacbacf8157b7db11554190595082766d8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-25 14:29:10 +0000

    qemu: update 4.1.0 bottle.

--- Formula/qemu.rb
+++ Formula/qemu.rb
@@ -6,9 +6,10 @@ class Qemu < Formula
   head "https://git.qemu.org/git/qemu.git"
 
   bottle do
-    sha256 "3b35079b1729b9b1bd58087794ccd9d40848b69d22a601c12b451791709298d6" => :mojave
-    sha256 "0ef458cc2c387f8bbb2bdbd0e4d6e2a21574eb735c1621ad8e0cd3e094288298" => :high_sierra
-    sha256 "68d05c70584e722c8109682ddc041a6271e0c36d102c7d3f55553ba3b4f39480" => :sierra
+    rebuild 1
+    sha256 "02658e4d1ca22932865b0361f1fe0e64661cbbbee05c28f0b878e76af8976c60" => :mojave
+    sha256 "a5425646bf831660d6cea9c04f93442ca67fd45236ccbcf3ac4d8e2822dadd82" => :high_sierra
+    sha256 "3ccac605c673e5a33ef31e8cdda1b2b3347297ad289e50b6a25da297e50d5c0e" => :sierra
   end
 
   depends_on "libtool" => :build
