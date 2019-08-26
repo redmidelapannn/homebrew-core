76b3e2ebae5379c3573ced0ca7963fbfa1422b90
Geoff Garbers <geoff@garbers.co.za>
2019-08-26 09:53:19 +0200

    envkey-fetch 1.2.8 (new formula)

--- Formula/envkey-fetch.rb
+++ Formula/envkey-fetch.rb
@@ -1,5 +1,5 @@
 class EnvkeyFetch < Formula
-  desc "Core EnvKey fetch/decryption/verification logic. Give it an ENVKEY, get back decrypted config as json. Compiles static cross platform binaries that can be easily built upon."
+  desc "Give it an ENVKEY, get back decrypted config as json"
   homepage "https://www.envkey.com"
   url "https://github.com/envkey/envkey-fetch/archive/v1.2.8.tar.gz"
   sha256 "4d1f55ba8d1c024ddc49752979439d035beb890ddd1fe8b40805aa048c5a5bee"
@@ -7,16 +7,15 @@ class EnvkeyFetch < Formula
 
   def install
     ENV["GOPATH"] = buildpath/"src"
-    ENV["GOBIN"] = buildpath/"bin"
+    ENV["GO111MODULE"] = "on"
 
-    system "go", "get", "-v"
-    system "go", "build", "-o", "envkey-fetch"
+    system "go", "build", "-o", "envkey-fetch", "main.go"
 
     bin.install "envkey-fetch"
   end
 
   test do
-    shell_output "#{bin}/envkey-fetch", 0
+    shell_output "#{bin}/envkey-fetch"
     shell_output "#{bin}/envkey-fetch 000", 1
   end
 end
