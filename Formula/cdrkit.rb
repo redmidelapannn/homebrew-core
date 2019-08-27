87e0ecc55046a68597af1e1cfb851e5c409587e3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-27 07:02:44 +0000

    cdrkit: add 2.0.0 bottle.

--- Formula/cdrkit.rb
+++ Formula/cdrkit.rb
@@ -5,6 +5,13 @@ class Cdrkit < Formula
   sha256 "bf07b0e011532c1f96bbab71efe75537c5ec1614fd9636ce551fcaf56b03579c"
   head "https://github.com/sidneys/cdrkit.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5ec48a37516f7b40ca61d6cdc24619522f3b6f971a408d3ead02609cfa470931" => :mojave
+    sha256 "30bc7e57b178e230692b648a7966802ab3cd693b4eff28ac373d274b00681038" => :high_sierra
+    sha256 "aa6359ad119f329179f3678934e29223d893c5c03198025d69afe746581aacf4" => :sierra
+  end
+
   depends_on "cmake" => :build
 
   def install
