9da6ce6f1ee1513844464db43ca78c77eef0c218
Andrew Kane <andrew@chartkick.com>
2019-08-27 12:52:18 -0700

    onnxruntime 0.5.0 (new formula)

---
+++ Formula/onnxruntime.rb
@@ -0,0 +1,34 @@
+class Onnxruntime < Formula
+  desc "Cross-platform, high performance scoring engine for ML models"
+  homepage "https://github.com/microsoft/onnxruntime"
+  url "https://github.com/microsoft/onnxruntime.git",
+      :tag      => "v0.5.0",
+      :revision => "1f8019bc9208c5ddf3c59561570f654888369746"
+
+  depends_on "cmake" => :build
+  depends_on "python" => :build
+
+  def install
+    system "#{Formula["python"].opt_bin}/python3", "tools/ci_build/build.py",
+           "--build_dir", "build/Linux", "--config", "Release",
+           "--build_shared_lib", "--skip_submodule_sync"
+
+    lib.install Dir["build/Linux/Release/*.dylib"]
+    include.install "include/onnxruntime/core/session/onnxruntime_c_api.h"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <onnxruntime_c_api.h>
+      #include <stdio.h>
+      int main()
+      {
+        printf("%s\\n", OrtGetVersionString());
+        return 0;
+      }
+    EOS
+    system ENV.cc, "-I#{include}", "-L#{lib}", "-lonnxruntime",
+           testpath/"test.c", "-o", testpath/"test"
+    assert_equal version, shell_output("./test").strip
+  end
+end
