667b580ada2bcb8a06c07065ec8338e50d9c14a2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-27 07:02:44 +0000

    chntpw: add 0.99.6 bottle.

--- Formula/chntpw.rb
+++ Formula/chntpw.rb
@@ -5,6 +5,13 @@ class Chntpw < Formula
   sha256 "e915f5addc2673317285c6f022c94da7fdee415d9800cd38540a13706706786b"
   head "https://github.com/sidneys/chntpw.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "26df7f7cb620531be04a76eb8d60dfdb20a5109c25899a2e63952e4547b89dcf" => :mojave
+    sha256 "c478dfd506e695a44ae6dfb2fd1ea90821674bb5e043e0c5615c0dd7bd6a1cd4" => :high_sierra
+    sha256 "1424ac6d7339aac3d37c0d525002ab76b4dd435392916c4305b6eb3905d1bc89" => :sierra
+  end
+
   depends_on "openssl"
 
   def install
