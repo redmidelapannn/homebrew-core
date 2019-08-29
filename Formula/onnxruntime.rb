9c0263ddcb461fc41d4fd429794b92b103fc605f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-08-29 03:26:42 +0000

    onnxruntime: add 0.5.0 bottle.

--- Formula/onnxruntime.rb
+++ Formula/onnxruntime.rb
@@ -5,6 +5,12 @@ class Onnxruntime < Formula
       :revision => "14eae293bf84956f371bfac50628e15b73751940"
   version "0.5.0"
 
+  bottle do
+    cellar :any
+    sha256 "bbfe0e53a0c1d3b60df9f04221f4f4d1bd98caa41e7d5fc0e0b1b8d5d02de0d9" => :mojave
+    sha256 "6ca146c701309e055b6930d4fb6fe362d5f89dfeb05aa2d6cda4c595c4f21b57" => :high_sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "python" => :build
 
