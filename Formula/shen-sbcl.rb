db0ac10c3581cd9d47e87d1920cfbd307f9b3f0b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-01 00:08:25 +0000

    shen-sbcl: add 2.5.0 bottle.

--- Formula/shen-sbcl.rb
+++ Formula/shen-sbcl.rb
@@ -4,6 +4,13 @@ class ShenSbcl < Formula
   url "https://github.com/Shen-Language/shen-cl/archive/v2.5.0.tar.gz"
   sha256 "366225b7df60be56d7622180332bf2db9ddac087c4497ed14eeb7533a765964d"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1391d4071b0b0b5807473ef7705cbb1b412c5262845944473d53e8ce445aa0de" => :mojave
+    sha256 "8294b2b0dd51cd9a2c7af926df1a9f7016e982984aaeccf68812c2e85ee32e0f" => :high_sierra
+    sha256 "9d0caa31d2241922482e38dc4541eac12e80e6b7e63f7e144cd93d8e8a75ce3c" => :sierra
+  end
+
   depends_on "sbcl" => :build
 
   resource "shen-kernel" do
