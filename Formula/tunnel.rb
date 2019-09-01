e760e5f5e65b6f7a9cf8853748f0fe51f106af19
Vishal Rana <vr@labstack.com>
2019-08-31 18:07:38 -0700

    tunnel 0.4.0

--- Formula/tunnel.rb
+++ Formula/tunnel.rb
@@ -1,8 +1,8 @@
 class Tunnel < Formula
   desc "Expose local servers to the internet securely"
   homepage "https://tunnel.labstack.com/docs"
-  url "https://github.com/labstack/tunnel-client/archive/v0.3.4.tar.gz"
-  sha256 "4a0d6030e12bba77cb0f1fe9e39b641347377e9f4898bf4c8407336f602c4056"
+  url "https://github.com/labstack/tunnel-client/archive/v0.4.0.tar.gz"
+  sha256 "3c88b74b60646569269641bd66b9f3f22d5196ef22049d724a821a41b61edcca"
 
   bottle do
     cellar :any_skip_relocation
@@ -19,8 +19,8 @@ class Tunnel < Formula
   end
 
   test do
-    system bin/"tunnel", "start", "8080"
-    system bin/"tunnel", "kill"
+    system bin/"tunnel", "connect", "8080"
+    system bin/"tunnel", "daemon", "stop"
     assert_predicate testpath/".tunnel/daemon.log", :exist?
   end
 end
