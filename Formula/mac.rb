b2bedac26cceb66487cec56d22882ae508527ecd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-02 10:49:00 +0000

    mac: add 4.92 bottle.

--- Formula/mac.rb
+++ Formula/mac.rb
@@ -5,6 +5,13 @@ class Mac < Formula
   version "4.92"
   sha256 "65170024af5c6518afdf31a3533131473a2fd29341626a315f21ce8d048ecf08"
 
+  bottle do
+    cellar :any
+    sha256 "416dffca65600c06d58a9ebbafe1c32f6198fc0d1bda163b1b09517ac16c778c" => :mojave
+    sha256 "079692ca459b58f40efda6e77b21f6b7f8543c6ab831362d5b04f7fd81452d71" => :high_sierra
+    sha256 "d7fb7532aff66e0d97c1028b3a2ccf16f0d8105a9ecbca6a915918283e74eccd" => :sierra
+  end
+
   patch :DATA
 
   def install
