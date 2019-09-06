89b15fc99bab8b53dcc640346b39f7232b7d017f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-06 23:01:34 +0000

    kash: add 1.11 bottle.

--- Formula/kash.rb
+++ Formula/kash.rb
@@ -4,6 +4,13 @@ class Kash < Formula
   url "https://github.com/cbeust/kash/archive/v1.11.tar.gz"
   sha256 "74d5ab5f7b464e36164e360558d628c8bf934818fe1358b0b0bdde112ce0b057"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7e1d4b72d536d3af74b45dc0dd3d69f45a6d5a3b39e9788fedb15881fcc26cec" => :mojave
+    sha256 "2e71547c8e219a07dd25599e4afd3715d06a2def51ea7641791be971dbc3b3d9" => :high_sierra
+    sha256 "0b8f65319b5625bfbd710a42e1c170314eba9313feb90aea1639bec31b2d153e" => :sierra
+  end
+
   depends_on :java => "11.0+"
 
   def install
