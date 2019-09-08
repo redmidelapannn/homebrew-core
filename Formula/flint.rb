7c783e2b92ee0e5326dcdd300ce9d38a4f6aeee5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-08 00:36:55 +0000

    flint: add 2.5.2 bottle.

--- Formula/flint.rb
+++ Formula/flint.rb
@@ -4,6 +4,13 @@ class Flint < Formula
   url "http://flintlib.org/flint-2.5.2.tar.gz"
   sha256 "cbf1fe0034533c53c5c41761017065f85207a1b770483e98b2392315f6575e87"
 
+  bottle do
+    cellar :any
+    sha256 "7c47103e4ce98c6b18ff5beb329f779c480c427e389ee7749a2f2665a09a5bc4" => :mojave
+    sha256 "078c1e6b8b2d746798f9e3b55b937d779776f4ed8aaade1e450080374796631b" => :high_sierra
+    sha256 "e819fbe16a6be03e1434f1db290eb8f9d8bc476144b9cbb2285ba216ee925bba" => :sierra
+  end
+
   depends_on "gmp"
   depends_on "mpfr"
 
