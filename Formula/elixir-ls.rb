30e680e6308f62a0fdff4de95a1a802bef9bf483
Oleksii Filonenko <brightone@protonmail.com>
2019-09-10 17:41:34 +0300

    Update elixir-ls formula
    After looking at https://github.com/Homebrew/homebrew-core/pull/35918,
    I made a few improvements to my original take on this.
    
    This also enables the debugger script.

--- Formula/elixir-ls.rb
+++ Formula/elixir-ls.rb
@@ -4,8 +4,7 @@ class ElixirLs < Formula
   url "https://github.com/JakeBecker/elixir-ls/archive/v0.2.25.tar.gz"
   sha256 "48aef4a0e795627962bde7e9c5a4fd201f92ef816fc823738d6a1702ee911ee0"
 
-  depends_on "elixir" => :build
-  depends_on "erlang" => :build
+  depends_on "elixir"
 
   def install
     # Ensure Hex and Rebar3 are installed
@@ -20,28 +19,33 @@ class ElixirLs < Formula
     # Fix the launcher script
     # it depends on being in the same directory as .ez files
     bin.mkpath
-    (bin/"elixir-ls").write <<~EOF
-      #!/bin/sh
-
-      export ERL_LIBS=#{prefix}:$ERL_LIBS
-      elixir -e "ElixirLS.LanguageServer.CLI.main()"
-    EOF
-    chmod("+x", bin/"elixir-ls")
+    bin.install_symlink(
+      prefix/"language_server.sh" => "elixir-language-server",
+      prefix/"debugger.sh"        => "elixir-debugger",
+    )
   end
 
   test do
-    require "Open3"
+    begin
+      io = IO.popen("#{bin}/elixir-language-server", "r+")
+      io.write("\n")
+      sleep 2
+    ensure
+      Process.kill("SIGINT", io.pid)
+      Process.wait(io.pid)
+
+      assert_match "Started ElixirLS", io.read
+    end
 
     begin
-      stdin, stdout, _, wait_thr = Open3.popen3("#{bin}/elixir-ls")
-      pid = wait_thr.pid
-      stdin.write <<~EOF
-        Content-Length: 58
-        {"jsonrpc":"2.0","method":"initialize","params":{},"id":1}
-      EOF
-      assert_match "Content-Length", stdout.gets("\n")
+      io = IO.popen("#{bin}/elixir-debugger", "r+")
+      io.write("\n")
+      sleep 2
     ensure
-      Process.kill "SIGKILL", pid
+      Process.kill("SIGINT", io.pid)
+      Process.wait(io.pid)
+
+      assert_match "Started ElixirLS debugger", io.read
     end
   end
 end
