225a4887e43b64cd74171994e98a19dd3f8d994d
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2019-09-16 20:47:54 +0100

    Updates dependency on Node to latest

--- Formula/topshell.rb
+++ Formula/topshell.rb
@@ -6,14 +6,14 @@ class Topshell < Formula
   url "https://github.com/topshell-language/topshell/archive/v0.7.7.tar.gz"
   sha256 "a24c759b231fbd5aaed3409ed5d12e98ae2987cc27672fef27964a21017dccd6"
 
-  depends_on "node@8" => :build
+  depends_on "node" => :build
   depends_on "sbt" => :build
 
   def install
     system "sbt", "fullOptJS"
     cd "node" do
       system "npm", "install", *Language::Node.local_npm_install_args
-      system "node_modules/.bin/pkg", "-t", "node8-macos-x64", "package.json"
+      system "node_modules/.bin/pkg", "-t", "macos-x64", "package.json"
       bin.install "topshell"
     end
   end
