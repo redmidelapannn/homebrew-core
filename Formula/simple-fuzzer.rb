89ed7926563e7bce6f33eed32b0e28282fb90622
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-18 21:33:32 +0000

    simple-fuzzer: add 0.7.1 bottle.

--- Formula/simple-fuzzer.rb
+++ Formula/simple-fuzzer.rb
@@ -6,6 +6,12 @@ class SimpleFuzzer < Formula
   version "0.7.1"
   head "https://github.com/orgcandman/Simple-Fuzzer.git"
 
+  bottle do
+    sha256 "5e363394851fced83bc31931d605c36c404388d6c9dd1cb8504915ed3d175b43" => :mojave
+    sha256 "35659ad6d11c86d48c7440459b98f55f206455735a361a6922b526e7363ffd85" => :high_sierra
+    sha256 "043624b30db5eda18d6df69b31458c7063490be9ad6b8bfe4c9db0a582c492fd" => :sierra
+  end
+
   depends_on "openssl"
 
   def install
