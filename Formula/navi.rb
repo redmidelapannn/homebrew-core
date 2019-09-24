ca0fdfcc1d0531f205f41c8df05234df65de2179
Denis Isidoro <denisidoro@users.noreply.github.com>
2019-09-23 21:16:54 -0300

    Remove whitespace

--- Formula/navi.rb
+++ Formula/navi.rb
@@ -3,7 +3,7 @@ class Navi < Formula
   homepage "https://github.com/denisidoro/navi"
   url "https://github.com/denisidoro/navi/archive/v0.8.1.tar.gz"
   sha256 "fb453cddb7c15ffdabe62a88f80948b9335fc6dbe79547ed68dbc55236d241ee"
-  
+
   depends_on "fzf" => :build
 
   def install
