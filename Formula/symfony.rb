cfd3e57ff910487d7bb41a7ee55b5cebdc07653a
Alfredo Lopez <alfredlopezf@hotmail.com>
2019-09-28 14:32:50 +0200

    symfony v4.7.0 (new formula)

---
+++ Formula/symfony.rb
@@ -0,0 +1,19 @@
+class Symfony < Formula
+  desc "Installs the Symfony framework for PHP"
+  homepage "https://symfony.com/"
+  url "https://github.com/symfony/cli/releases/download/v4.7.0/symfony_darwin_amd64.gz"
+  version "v4.7.0"
+  sha256 "c9be77cda0ee6b2f144bf8c5648a7500b01d3f4bd19d838d7a11a32be0f648b3"
+
+  depends_on "php"
+  depends_on "composer"
+
+  def install
+    bin.install "symfony" => "symfony"
+  end
+
+  test do
+    symfony new testpath/"test_project"
+    assert_predicate testpath/"test_project", :exist?
+  end
+end
