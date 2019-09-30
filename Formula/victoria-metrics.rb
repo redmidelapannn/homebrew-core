afdaefe92cab10a61baa0dea2f0f19a968bef556
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-09-30 10:04:41 +0000

    victoria-metrics: add 1.27.3 bottle.

--- Formula/victoria-metrics.rb
+++ Formula/victoria-metrics.rb
@@ -4,6 +4,13 @@ class VictoriaMetrics < Formula
   url "https://github.com/VictoriaMetrics/VictoriaMetrics/archive/v1.27.3.tar.gz"
   sha256 "648bf0bbf78357465346923ea5f25b9313dc066419ae30aa67fcfe884fca978a"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6ed4b0582b3a42aae2f1cb2647e1c33a7c54b9bdbf0f200bbc98c352851b1058" => :catalina
+    sha256 "8c2596cddadefd60024d2b2bd435c9f185e0a92a04a7216c3576a4e17518c1b4" => :mojave
+    sha256 "108c9e7b4cd174ae448e54a7bf3777368b2fc471844ab88bbdf0d707305adb04" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
