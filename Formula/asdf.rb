fa1400fb6d2d3cf8f99e1f31a0aa642b07196972
Sourav Karjee <35899626+srvk-99@users.noreply.github.com>
2019-10-01 22:21:39 +0530

    asdf 3.3.3

--- Formula/asdf.rb
+++ Formula/asdf.rb
@@ -1,8 +1,8 @@
 class Asdf < Formula
   desc "Extendable version manager with support for Ruby, Node.js, Erlang & more"
   homepage "https://github.com/asdf-vm"
-  url "https://github.com/asdf-vm/asdf/archive/v0.7.4.tar.gz"
-  sha256 "25c3c26e667eaf96686db340279acfa1b40a924ed2ecabc4db66a04b12211f7e"
+  url "https://common-lisp.net/project/asdf/archives/asdf-3.3.3.tar.gz"
+  sha256 "efcd05120132aa3173ff8102ed9534ba3cae192a59d0c27a5c0dac3a0b21c784"
   head "https://github.com/asdf-vm/asdf.git"
 
   bottle :unneeded
