ee89dbaf75ae58b4fbb4805df971fff3b0963691
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-01 19:50:12 +0000

    aom: update 1.0.0 bottle.

--- Formula/aom.rb
+++ Formula/aom.rb
@@ -7,6 +7,7 @@ class Aom < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "6d2df5fd18d91f1f708f074a0049925cad705458f178cd19c111e5246dc510f6" => :catalina
     sha256 "fdcfd3f69fbf8c9d5d3277a9cc0aabe6e4d708e3c505724828078ef93d3c82f7" => :mojave
     sha256 "7ab120d51096c0b9211588e0241f6e3da2cb76487fa92ed3fba97ccefab6608b" => :high_sierra
     sha256 "6059c30278e7c195ca7bd6487e21b7f8177d1320c32ab7d1e3202649b4680a3b" => :sierra
