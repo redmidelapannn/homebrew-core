255b892dfb6fefbbb77cdf8e1d122f8b57089d99
Sourav Karjee <35899626+srvk-99@users.noreply.github.com>
2019-10-01 22:10:31 +0530

    abook 0.6.1

--- Formula/abook.rb
+++ Formula/abook.rb
@@ -1,8 +1,8 @@
 class Abook < Formula
   desc "Address book with mutt support"
   homepage "https://abook.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/abook/abook/0.5.6/abook-0.5.6.tar.gz"
-  sha256 "0646f6311a94ad3341812a4de12a5a940a7a44d5cb6e9da5b0930aae9f44756e"
+  url "http://abook.sourceforge.net/devel/abook-0.6.1.tar.gz"
+  sha256 "f0a90df8694fb34685ecdd45d97db28b88046c15c95e7b0700596028bd8bc0f9"
   revision 2
   head "https://git.code.sf.net/p/abook/git.git"
 
