5426f71759337b0260d6f1890b2e547b3bf77c6c
Mark Siemers <mark.siemers@gmail.com>
2019-10-03 15:25:14 -0700

    postgresql 12.0

--- Formula/postgresql@11.rb
+++ Formula/postgresql@11.rb
@@ -1,8 +1,9 @@
-class Postgresql < Formula
+class PostgresqlAT11 < Formula
   desc "Object-relational database system"
   homepage "https://www.postgresql.org/"
-  url "https://ftp.postgresql.org/pub/source/v12.0/postgresql-12.0.tar.bz2"
-  sha256 "cda2397215f758b793f741c86be05468257b0e6bcb1a6113882ab5d0df0855c6"
+  url "https://ftp.postgresql.org/pub/source/v11.5/postgresql-11.5.tar.bz2"
+  sha256 "7fdf23060bfc715144cbf2696cf05b0fa284ad3eb21f0c378591c6bca99ad180"
+  revision 1
   head "https://github.com/postgres/postgres.git"
 
   bottle do
