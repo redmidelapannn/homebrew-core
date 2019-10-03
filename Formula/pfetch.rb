035b16e2a566aef203341ad73f2ec697788c7e83
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-03 13:12:07 +0000

    pfetch: add 0.3 bottle.

--- Formula/pfetch.rb
+++ Formula/pfetch.rb
@@ -4,6 +4,13 @@ class Pfetch < Formula
   url "https://github.com/dylanaraps/pfetch/archive/0.3.tar.gz"
   sha256 "e65694db6e49c9850d15bf9948a4d19ce9a1807966bd2e4cea7b0df202c2d14d"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d39a9b93ac765a276a22c59bddd6918ec4f1c5c5644f72bea1071c180610de7c" => :catalina
+    sha256 "d39a9b93ac765a276a22c59bddd6918ec4f1c5c5644f72bea1071c180610de7c" => :mojave
+    sha256 "d39a9b93ac765a276a22c59bddd6918ec4f1c5c5644f72bea1071c180610de7c" => :high_sierra
+  end
+
   def install
     bin.install "pfetch"
   end
