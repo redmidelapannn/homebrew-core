5df0563bd7e811617b3ba061c00938837f57f2af
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-03 04:19:29 +0000

    sass: add 1.22.10 bottle.

--- Formula/sass.rb
+++ Formula/sass.rb
@@ -6,6 +6,13 @@ class Sass < Formula
   url "https://registry.npmjs.org/sass/-/sass-1.22.10.tgz"
   sha256 "ecea121912cc18b742ae62e7473be26ac3783d95ce1f304deecc80e65a676a3a"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c18680ef290f752b80b3373058f994231ca5342fdc53771a60f3b6d39931b4ca" => :catalina
+    sha256 "ecddcb61579a0cae910a909bc2e8dcc6e8f4053ddf1b4c541ed6291ab1b2a7bb" => :mojave
+    sha256 "dfed14e3d6fd058453eb56717c8c03f9ccc780f4d5da4aee0a418c7ce292fef9" => :high_sierra
+  end
+
   depends_on "node"
 
   def install
