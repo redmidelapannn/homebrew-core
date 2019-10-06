56e9da43b441417104850eeca42bd3574819db09
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-06 19:16:49 +0000

    arduino-cli: add 0.5.0-showports bottle.

--- Formula/arduino-cli.rb
+++ Formula/arduino-cli.rb
@@ -5,6 +5,13 @@ class ArduinoCli < Formula
   version "0.5.0-showports"
   sha256 "96373b59e004f277474eb5a11e2a585633976c9939056b833e65e670e7393e77"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "08c0cd11ce824ac3fb83380d6c5649ac0fcce4fd308b1baf55ec037ac68db384" => :catalina
+    sha256 "8ef55a520435a57646f4fc3bda00aa4724917aebdfe51f23b9e4ab385290702a" => :mojave
+    sha256 "9bd1eb7045a7f34ff4b8ac5ffe56b448941627274033eea86a22493aa07682d3" => :high_sierra
+  end
+
   depends_on "go"
 
   def install
