8b0e1a39a89f892a8565c64d9124a80fc4f03571
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-07 23:46:34 +0000

    stoken: update 0.92_1 bottle.

--- Formula/stoken.rb
+++ Formula/stoken.rb
@@ -7,11 +7,9 @@ class Stoken < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "a802cd05fc1de00af961c7de284078a6472c8327a58ecdc4cad8479c812d1175" => :catalina
-    sha256 "8b558486eeb55f39205d26201a1613eaaadc9b6615d1e5f24b3487749acfb89b" => :mojave
-    sha256 "2aeef625d9594a2fc26890500d1c8ff611e8c5c69df6f8ac905b6a72f179caf0" => :high_sierra
-    sha256 "0896359e6966f067248616b2393e5e0f24a05639c4059c7f6bbd025acb867714" => :sierra
+    sha256 "ab6798dfe9bdb43eb98fdef5f21c9b45a53e65e26c16fffbf6de862e7606e6e2" => :catalina
+    sha256 "d1b01f80fee156b9128320ecd5bd88cd07b0dbbe0f4331cfa132b4af98e20f81" => :mojave
+    sha256 "89554cfa0484f2351b3bceafb7ccb6a3f0f391e2101c663465e6251b74b715c3" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
