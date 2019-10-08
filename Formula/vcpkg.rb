96e061dca3deba83432249560fbf50cd6aa2411f
Flier Lu <flier.lu@gmail.com>
2019-10-08 13:30:54 +0800

    vcpkg 2019.09 (new formula)

---
+++ Formula/vcpkg.rb
@@ -0,0 +1,29 @@
+class Vcpkg < Formula
+  desc "C++ Library Manager for Windows, Linux, and MacOS"
+  homepage "https://docs.microsoft.com/en-us/cpp/vcpkg"
+
+  stable do
+    url "https://github.com/microsoft/vcpkg/archive/2019.09.tar.gz"
+    sha256 "16c1b151cc3cf2022b03b699970e548f48cd9cd1e68db0b9c23a1d0d39391e56"
+  end
+
+  head do
+    url "https://github.com/microsoft/vcpkg.git"
+  end
+
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
+  depends_on "gcc"
+  
+  def install
+    ENV["CC"] = Formula["gcc"].opt_bin/"gcc-#{Formula["gcc"].version_suffix}"
+    ENV["CXX"] = Formula["gcc"].opt_bin/"g++-#{Formula["gcc"].version_suffix}"
+    system "./bootstrap-vcpkg.sh", "-useSystemBinaries"
+    libexec.install ["ports", "scripts", "triplets", "vcpkg", ".vcpkg-root"]
+    bin.install_symlink libexec/"vcpkg"
+  end
+
+  test do
+    assert_match version.to_s, pipe_output("#{bin}/vcpkg version")
+  end
+end
