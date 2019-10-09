403b62f5f202145fe6825323a108aedbb9927d7f
Yanis Zafirópulos <1265028+drkameleon@users.noreply.github.com>
2019-10-09 15:34:21 +0200

    Update arturo.rb

--- Formula/arturo.rb
+++ Formula/arturo.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Arturo < Formula
   desc "Simple, modern and powerful interpreted programming language for super-fast scripting."
   homepage "http://arturo-lang.io"
@@ -21,15 +18,6 @@ class Arturo < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test Arturo`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
