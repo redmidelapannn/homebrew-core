65bd9868884a9f23b40c76a1200a2c96171a6ab2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-10 12:11:53 +0000

    arturo: add 0.3.6 bottle.

--- Formula/arturo.rb
+++ Formula/arturo.rb
@@ -4,6 +4,12 @@ class Arturo < Formula
   url "https://github.com/arturo-lang/arturo/archive/v0.3.6.tar.gz"
   sha256 "c6c584f9e0fed0d82ecb81f2bf7481fe1b6b2918fe6bc588f8c9f851c68ea18c"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c177890fa4baf58def9bbb4e6fe9295868a757594702ea160ef9e913cd9fe65b" => :mojave
+    sha256 "7068963b635129ef5335af1ed5c7022ed3133ac76429094a1eddbc1c708c7a1a" => :high_sierra
+  end
+
   depends_on "dmd" => :build
   depends_on "bison" => :build
   depends_on "dub" => :build
