09d18cb2d4cc3e043ca460769b46482b4c2c3e81
Yanis Zafirópulos <1265028+drkameleon@users.noreply.github.com>
2019-10-10 09:10:59 +0200

    Update arturo.rb
    fixed issues with formula

--- Formula/arturo.rb
+++ Formula/arturo.rb
@@ -1,23 +1,20 @@
 class Arturo < Formula
-  desc "Simple, modern and powerful interpreted programming language for super-fast scripting."
-  homepage "http://arturo-lang.io"
-  url "https://github.com/arturo-lang/arturo/tarball/master"
-  version "0.3.6"
-  sha256 ""
+  desc "Simple, modern and powerful interpreted programming language"
+  homepage "https://arturo-lang.io"
+  url "https://github.com/arturo-lang/arturo/archive/v0.3.6.tar.gz"
+  sha256 "c6c584f9e0fed0d82ecb81f2bf7481fe1b6b2918fe6bc588f8c9f851c68ea18c"
 
-  depends_on "bison" => :build
-  depends_on "curl" => :build
   depends_on "dmd" => :build
   depends_on "dub" => :build
-  depends_on "flex" => :build
   depends_on "gtk+" => :build
+  depends_on "bison" => :build
 
   def install
-    system "dub build --build=release --compiler=dmd"
+    system "dub", "build", "--build=release", "--compiler=dmd"
     bin.install "arturo"
   end
 
   test do
-    system "false"
+    system bin/"arturo", "--version"
   end
 end
