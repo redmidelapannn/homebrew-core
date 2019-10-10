a09d8680032df1936061ebf399b094a8c22d1997
Yanis Zafirópulos <1265028+drkameleon@users.noreply.github.com>
2019-10-10 13:58:36 +0200

    Update arturo.rb

--- Formula/arturo.rb
+++ Formula/arturo.rb
@@ -4,7 +4,6 @@ class Arturo < Formula
   url "https://github.com/arturo-lang/arturo/archive/v0.3.6.tar.gz"
   sha256 "c6c584f9e0fed0d82ecb81f2bf7481fe1b6b2918fe6bc588f8c9f851c68ea18c"
 
-  depends_on macos: [:high_sierra, :mojave]
   depends_on "dmd" => :build
   depends_on "bison" => :build
   depends_on "dub" => :build
@@ -12,6 +11,8 @@ class Arturo < Formula
   depends_on "atk" => :build
   depends_on "gtk+3" => :build
   depends_on "jpeg" => :build
+  
+  depends_on macos: [:high_sierra, :mojave]
 
   def install
     system "dub", "build", "--build=release", "--compiler=dmd"
