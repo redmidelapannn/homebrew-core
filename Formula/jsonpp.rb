2f6d8a73c50aac5c947b638c0aa2ff85e8fd0a3b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-11 21:29:17 +0000

    jsonpp: update 1.3.0_1 bottle.

--- Formula/jsonpp.rb
+++ Formula/jsonpp.rb
@@ -7,6 +7,7 @@ class Jsonpp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "8303f62ad1a616e45c2d44d5b1fe60562d6ee13700ad3d25870b0c37f8296626" => :catalina
     sha256 "7c8dd8c69321e42c29e075a658840167138d16820ba6a0dd5f2f2425c87a569b" => :mojave
     sha256 "d81995103192bb58f66d7089939eb6682f117a7044d3a84804db62b4c31a3c81" => :high_sierra
     sha256 "219f8a6bfdf1d0e8435fa1c1fdf0cc22b91cae8ec7d62581d312927fabcf9388" => :sierra
