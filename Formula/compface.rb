b92bf7c1897dd393af3f456f027b2bb1e8a659b3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-12 00:20:02 +0000

    compface: update 1.5.2 bottle.

--- Formula/compface.rb
+++ Formula/compface.rb
@@ -8,6 +8,7 @@ class Compface < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "42b934f4c25a729267425ca85959a556ee3aa4cdef729c815a29dbc76e814e4f" => :catalina
     sha256 "10d7c5d38196576ac2d21278ead512819c9e393fa8caf81d75a70d7b09c7aaa8" => :mojave
     sha256 "15f3ed9a165fa2f4966fde4de5b8b1c62d583425e0c3d9961b26348f6355bfcc" => :high_sierra
     sha256 "092d90367b0fa75ff8a1be3982cda127226fb9805c681170f66fe27c148c8d1b" => :sierra
