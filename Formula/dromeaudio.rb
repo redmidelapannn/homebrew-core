dc9fc9a4bd3737a28850f27fca08815d0c484f3b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-12 00:20:02 +0000

    dromeaudio: update 0.3.0 bottle.

--- Formula/dromeaudio.rb
+++ Formula/dromeaudio.rb
@@ -8,6 +8,7 @@ class Dromeaudio < Formula
   bottle do
     cellar :any_skip_relocation
     rebuild 1
+    sha256 "b9aabc72e92dc1ae0a6babfe58d050d4f637b1e489009e3ec64c568fa051b72f" => :catalina
     sha256 "537401eb1346b32ad7474408f24853224d1874d7782c4980b0883855be8e153b" => :mojave
     sha256 "17e070192156e0e50f983a857110b1b9efcb5476dca7047adcdd151f9b14b81e" => :high_sierra
     sha256 "6a8617ee09ea859c079db275005a89d38738e497f07383ec2ba96b8df5c657f7" => :sierra
