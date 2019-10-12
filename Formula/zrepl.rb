5bf3be7acc82f7e476134c72d3c3aa6c55422e25
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-12 21:39:40 +0000

    zrepl: update 0.1.1 bottle.

--- Formula/zrepl.rb
+++ Formula/zrepl.rb
@@ -7,6 +7,7 @@ class Zrepl < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "0c125e8d2634b7a24c3791cace5d6181ee67cb2dd70798ef252067131133b87f" => :catalina
     sha256 "9cec0ea416a2a394452fac82be895e8931c8e882465518e237a2b847e3e168d7" => :mojave
     sha256 "2fd02af7892953417e8a4e4322c40ea2e1e2ac0181719d5afeaa88df62a91029" => :high_sierra
     sha256 "05827e523e9e3c6e25579abc41d7367cd877694d6f4a90174dd4f801a775f9e3" => :sierra
