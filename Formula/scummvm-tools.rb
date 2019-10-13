762a4e753bcd58848c7b1d7662daed8103f96c67
Igor Kapkov <igasgeek@me.com>
2019-10-13 22:30:46 +1100

    scummvm-tools: revision bump for icu4c

--- Formula/scummvm-tools.rb
+++ Formula/scummvm-tools.rb
@@ -3,6 +3,7 @@ class ScummvmTools < Formula
   homepage "https://www.scummvm.org/"
   url "https://www.scummvm.org/frs/scummvm-tools/2.0.0/scummvm-tools-2.0.0.tar.xz"
   sha256 "c2042ccdc6faaf745552bac2c00f213da382a7e382baa96343e508fced4451b3"
+  revision 1
   head "https://github.com/scummvm/scummvm-tools.git"
 
   bottle do
