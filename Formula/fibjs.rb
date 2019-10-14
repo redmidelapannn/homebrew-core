5e757b457c8d745ea309b641410b345a49fe3793
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 13:53:59 +0000

    fibjs: update 0.28.0 bottle.

--- Formula/fibjs.rb
+++ Formula/fibjs.rb
@@ -7,6 +7,7 @@ class Fibjs < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "15b5b40c7bc597ed87a8eaeddf7dfe64b5a6cd55086734cc0b1e39a4693a7561" => :catalina
     sha256 "88607ac1b27e38088880ab703f2abd76a88b38ddcae2da62ff6108e6bf9f3305" => :mojave
     sha256 "a332e5c7b8e75b627ff346bcca19d5fb167ffb05ba5ab8e62bc4234b54276aef" => :high_sierra
     sha256 "c66dc2e015e907055d655dcaa2843a5d638e97afd3f5ac7dcff87d830b233e92" => :sierra
