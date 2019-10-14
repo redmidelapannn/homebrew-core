09249d87118beb6d463c526ad9b9e5c916b9ae85
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 10:27:29 +0000

    doublecpp: update 0.6.3 bottle.

--- Formula/doublecpp.rb
+++ Formula/doublecpp.rb
@@ -6,6 +6,7 @@ class Doublecpp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "cf024c9ea1833ab381e78f02c64c2e77d3231d215037cdd52ef75be8addf173c" => :catalina
     sha256 "eed3920bd4e85e32542ce2a67fc9d928f8d8ddfceb0b48e80ddd9db30090e9e6" => :mojave
     sha256 "ca161369434cba6763add99e4e470a495662c866a328b374c5d6184e687361cc" => :high_sierra
     sha256 "748af7fb63392453cc4b648cea20786173202f5c891b45765dbf374e4ac2c2d5" => :sierra
