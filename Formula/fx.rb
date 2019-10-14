60c35d458d16d26874e51730225da515a68e6f15
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 15:33:21 +0000

    fx: update 14.0.1 bottle.

--- Formula/fx.rb
+++ Formula/fx.rb
@@ -8,6 +8,7 @@ class Fx < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b8a594f7248ab60de33a2c5235220fc0bf49ab7553dc0dbaeed7cc8e7c6ca7e7" => :catalina
     sha256 "1b7b95e489a1e63f52412bbd0fb053e0debe8c52ffbb3f8cf039510f32361b3c" => :mojave
     sha256 "bb119118bdee2f95492a21515064644fa9954ab72d4174798b21ef8fbeaf4729" => :high_sierra
     sha256 "a50e8d3910abb9888f08a1a8e91c5555d2ebb8bd5cc5750a8ee903dd83434684" => :sierra
