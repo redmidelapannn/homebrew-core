da7f9650a6212d744b61cdd7cec874142a5058ce
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 20:13:43 +0000

    libtommath: update 1.1.0 bottle.

--- Formula/libtommath.rb
+++ Formula/libtommath.rb
@@ -7,6 +7,7 @@ class Libtommath < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "19b1567ef5308f1aa63a744c42b822379fcd2ca3fe1c33a932dfad10a9fac9eb" => :catalina
     sha256 "e69fa7fa3b1ff2e85209b6719ad17192942c9d2954321f30b7e039745e4e9ffb" => :mojave
     sha256 "291e7d5f7f5ecace41fc8d9a402f8bb630004700f264339ad013df713f9b33eb" => :high_sierra
     sha256 "fba7bbbc5efe8f09f7a23b93e5d168134703f3ce14bf2a5b8b7473cad0f7826f" => :sierra
