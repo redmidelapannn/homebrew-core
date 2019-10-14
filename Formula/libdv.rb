d4dd2e56c55d3b39027024a03b9164483399dc83
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-14 17:32:33 +0000

    libdv: update 1.0.0 bottle.

--- Formula/libdv.rb
+++ Formula/libdv.rb
@@ -7,6 +7,7 @@ class Libdv < Formula
   bottle do
     cellar :any
     rebuild 1
+    sha256 "d7178d7e34035286a66f16b969171922924e8c855ca395726e26721e473c6b30" => :catalina
     sha256 "ec2564e27b321ebb35a3278d6e7411d17d1b1acad5dbcd40106cbcaf24205b53" => :mojave
     sha256 "0f7c7db1baa95682ad66b9d628d51978f162558f6d8296715a38150f83a7c72f" => :high_sierra
     sha256 "9ea1a006d7aa954c5a1d61497f9f7f43e0b1bd5bce911b6d334a693d8af58671" => :sierra
