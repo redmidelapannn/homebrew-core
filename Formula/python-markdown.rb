3360f1e56ee1031cabbd1832d4bfce9ee476f57d
Alexander Bayandin <a.bayandin@gmail.com>
2019-10-15 09:32:19 +0100

    python-markdown: revision bump for python

--- Formula/python-markdown.rb
+++ Formula/python-markdown.rb
@@ -3,6 +3,7 @@ class PythonMarkdown < Formula
   homepage "https://pypi.python.org/pypi/Markdown"
   url "https://files.pythonhosted.org/packages/ac/df/0ae25a9fd5bb528fe3c65af7143708160aa3b47970d5272003a1ad5c03c6/Markdown-3.1.1.tar.gz"
   sha256 "2e50876bcdd74517e7b71f3e7a76102050edec255b3983403f1a63e7c8a41e7a"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
