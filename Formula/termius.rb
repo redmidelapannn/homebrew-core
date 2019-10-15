d8118ccfdd4db813e0315c81b210e2cf13e27d4d
Alexander Bayandin <a.bayandin@gmail.com>
2019-10-15 09:21:18 +0100

    termius: revision bump for python

--- Formula/termius.rb
+++ Formula/termius.rb
@@ -5,7 +5,7 @@ class Termius < Formula
   homepage "https://termius.com"
   url "https://github.com/Crystalnix/termius-cli/archive/v1.2.12.tar.gz"
   sha256 "89be6d35e5c4918c0d9e3f2410620d3a84c7108e52c2c87cfa6166c5612e08ee"
-  revision 1
+  revision 2
   head "https://github.com/Crystalnix/termius-cli.git", :branch => "master"
 
   bottle do
