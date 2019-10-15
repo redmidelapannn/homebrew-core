ba27713c7da9c2116e1e8e9c9d597cec28e391e7
Alexander Bayandin <a.bayandin@gmail.com>
2019-10-15 09:21:23 +0100

    vdirsyncer: revision bump for python

--- Formula/vdirsyncer.rb
+++ Formula/vdirsyncer.rb
@@ -6,6 +6,7 @@ class Vdirsyncer < Formula
   url "https://github.com/pimutils/vdirsyncer.git",
       :tag      => "0.16.7",
       :revision => "dcf5f701b7b5c21a8f4e8c80243db3e0baff1313"
+  revision 1
   head "https://github.com/pimutils/vdirsyncer.git"
 
   bottle do
