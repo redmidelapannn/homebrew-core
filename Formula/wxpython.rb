47e64c0189c4702cd313c92c1f42a7f36dc809f7
Alexander Bayandin <a.bayandin@gmail.com>
2019-10-15 12:16:10 +0100

    wxpython: revision bump for python

--- Formula/wxpython.rb
+++ Formula/wxpython.rb
@@ -6,6 +6,7 @@ class Wxpython < Formula
   url "https://files.pythonhosted.org/packages/b9/8b/31267dd6d026a082faed35ec8d97522c0236f2e083bf15aff64d982215e1/wxPython-4.0.7.post2.tar.gz"
   version "4.0.7.post2"
   sha256 "5a229e695b64f9864d30a5315e0c1e4ff5e02effede0a07f16e8d856737a0c4e"
+  revision 1
 
   bottle do
     cellar :any
