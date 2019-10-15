9a885973459e11cf5483a1ddbb2abde7784f3b35
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 21:56:59 +0000

    vpcs: update 0.8 bottle.

--- Formula/vpcs.rb
+++ Formula/vpcs.rb
@@ -6,6 +6,7 @@ class Vpcs < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "1e9179eacb14b97e4565b772038ee35505ed4d1f102d3910253cb82373e09957" => :catalina
     sha256 "5728bc8e33f81a307c74fe625305c42363a493ff1dc612d604feec971374385d" => :mojave
     sha256 "ac52b231d875679e7bd4da3a09c6b5bc833e5b93fe5a77749dc834b1d82d21d5" => :high_sierra
     sha256 "78c7e415e9bcbdf28cfdda5d37fce9cc7d735b01d61400b41239e0cdee17ada5" => :sierra
