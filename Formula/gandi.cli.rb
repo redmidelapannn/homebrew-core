06e1f34df08a7bf93c4af2f02a52b16760dd5280
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 10:40:41 +0000

    gandi.cli: update 1.5 bottle.

--- Formula/gandi.cli.rb
+++ Formula/gandi.cli.rb
@@ -8,6 +8,7 @@ class GandiCli < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "a025ecc6696a2c9812784d8148453637a330e0e9f40a339289b18b7ef383ca5e" => :catalina
     sha256 "fd37e0329ef88f7b334ddb4d31128c7167f11c7f33334aca4cd8b954766b9bb9" => :mojave
     sha256 "9ab5c20d1af6e6083ebb211f1bc07dbeafa53a5ff5670452ef00fc26f68a843c" => :high_sierra
     sha256 "f63f0c8c8a4d924f12589b8eb1c85e57ccd86ad09ee96c6d570191b07816a2e8" => :sierra
