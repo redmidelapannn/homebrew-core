3d5f4db1b090c9cd04f3a5d9ba087db906d205bf
Alexander Bayandin <a.bayandin@gmail.com>
2019-10-15 12:16:01 +0100

    pyside: revision bump for python

--- Formula/pyside.rb
+++ Formula/pyside.rb
@@ -3,6 +3,7 @@ class Pyside < Formula
   homepage "https://wiki.qt.io/Qt_for_Python"
   url "https://download.qt.io/official_releases/QtForPython/pyside2/PySide2-5.13.2-src/pyside-setup-opensource-src-5.13.2.tar.xz"
   sha256 "3e255d64df08880d0281ebe86009d5ea45f24332b308954d967c33995f75e543"
+  revision 1
 
   bottle do
     sha256 "fea86f39d15b29aac8ed23fda31d69eb46a63f76b8a70da89b64f4398a97f32a" => :catalina
@@ -14,6 +15,9 @@ class Pyside < Formula
   depends_on "llvm" => :build
   depends_on "python"
   depends_on "qt"
+
+  # Allow pyside to be built on python 3.8
+  patch :p0, :DATA
 
   def install
     ENV.remove "HOMEBREW_LIBRARY_PATHS", Formula["llvm"].opt_lib
@@ -54,3 +58,15 @@ class Pyside < Formula
     ].each { |mod| system "python3", "-c", "import PySide2.Qt#{mod}" }
   end
 end
+
+__END__
+--- build_scripts/config.py	2019-10-18 12:31:32.000000000 +0100
++++ build_scripts/config.py	2019-10-18 12:32:28.000000000 +0100
+@@ -93,6 +93,7 @@
+             'Programming Language :: Python :: 3.5',
+             'Programming Language :: Python :: 3.6',
+             'Programming Language :: Python :: 3.7',
++            'Programming Language :: Python :: 3.8',
+         ]
+
+         self.setup_script_dir = None
