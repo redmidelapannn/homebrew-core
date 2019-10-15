20a315c288f3c5f4724f8d66e0596b94680e6cd1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-15 19:37:57 +0000

    warp: update 0.0.3 bottle.

--- Formula/warp.rb
+++ Formula/warp.rb
@@ -6,6 +6,7 @@ class Warp < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "211e4ca1493700d38a4eb62b17573320b9b753c54dbea2dbf6373147e56a3fc7" => :catalina
     sha256 "0d48b2519458f997d8591c1f3bc645e9877486b95849656651865376450bf2d0" => :mojave
     sha256 "214fdbd64070317a1a5026b54a590d1783c295ac7240ac442092314ab861a252" => :high_sierra
     sha256 "562aed9e482aa4423476a0a83e9ecb36822ac19968f4440362a3b273bdcaf4dc" => :sierra
