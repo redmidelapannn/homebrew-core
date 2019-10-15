eb319403d36b255ec583168677b71a25fd7ee6d8
Alexander Bayandin <a.bayandin@gmail.com>
2019-10-15 09:20:45 +0100

    legit: revision bump for python

--- Formula/legit.rb
+++ Formula/legit.rb
@@ -5,6 +5,7 @@ class Legit < Formula
   homepage "https://frostming.github.io/legit/"
   url "https://files.pythonhosted.org/packages/6b/65/819f84ef3caa4743c834122acb897df29167f6178c9c095d7896f2181387/legit-1.1.0.tar.gz"
   sha256 "25bd3809d657c9b0bd45a6a230dc7f58a56e6e068159d471ffeadb0cf4677b4d"
+  revision 1
   head "https://github.com/frostming/legit.git", :branch => "master"
 
   bottle do
