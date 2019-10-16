5f43e4086a127e8b02861879cebf5cf504ec7dc2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-16 23:45:05 +0000

    rtmidi: update 4.0.0 bottle.

--- Formula/rtmidi.rb
+++ Formula/rtmidi.rb
@@ -6,6 +6,7 @@ class Rtmidi < Formula
 
   bottle do
     cellar :any
+    sha256 "0e22aa0ab469af17eb75a9ea9d4d69ceae98fe38d1047501d1796a0bf8ea0ade" => :catalina
     sha256 "5cb51ca3774a1e22d2388f01ccb514d091519b9d8cbc5f4805437be1be7dba30" => :mojave
     sha256 "8744fb2c8d9952b0e14e50f2fed7982e715843746506378645211a178a3163e0" => :high_sierra
     sha256 "4eab0eb4ede3d1035d7918bd84e2aede8f648c2ebcf449ac6f9ce15c0c744988" => :sierra
