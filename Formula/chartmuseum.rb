2fcd49265e53e5650471caa8bf7002f0db8995df
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-17 23:17:20 +0000

    chartmuseum: add 0.9.0 bottle.

--- Formula/chartmuseum.rb
+++ Formula/chartmuseum.rb
@@ -4,6 +4,13 @@ class Chartmuseum < Formula
   url "https://github.com/helm/chartmuseum/archive/v0.9.0.tar.gz"
   sha256 "e5fed41b098141a56037340ad3b0857c7defae6d143af56b6003a270cc75631f"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "70e9580c7ab001cfa1eab8369888b4d85e276d2244fda1b592ef7c23f98a9a2a" => :catalina
+    sha256 "95e9b5179d3de896220f44666e8f80cb44cf4c0e6a946b3bad2cbc05a3ece6ec" => :mojave
+    sha256 "1b74daf5e4b7579390f017486d19ccadfbb8ade5fed56371350d8b978ce813c5" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
