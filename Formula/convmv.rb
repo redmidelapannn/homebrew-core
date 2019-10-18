41f903696582b3276217faa02fc6dd2e1a50d8e5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 03:06:54 +0000

    convmv: update 2.05 bottle.

--- Formula/convmv.rb
+++ Formula/convmv.rb
@@ -6,6 +6,7 @@ class Convmv < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "b27a22e4ebeb51a8633060850dc19176c2249fc6ae96e2bddd47250995c4e86a" => :catalina
     sha256 "8e5a0c8207cb0c57f1640c5a8c9a4a05cd9c8ffc68d922774cae4bfa56b691e6" => :mojave
     sha256 "856021a73afb22052e496ced9eb1a7386d810a6d75903aac99feb98298801ea8" => :high_sierra
     sha256 "cc6cf7ff9cfd8909a76e29dd6ddbdddc9ad95638e154b72a36fe6c255e3a367d" => :sierra
