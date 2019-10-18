c289d18ed977f899c0717fbc6a30f3893fe198af
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 20:29:59 +0000

    ttf2eot: update 0.0.3 bottle.

--- Formula/ttf2eot.rb
+++ Formula/ttf2eot.rb
@@ -6,11 +6,10 @@ class Ttf2eot < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "05b1f397b4784a77f36a3d3138e812932db4419d8d03e0f0735e58591677e918" => :catalina
-    sha256 "54d328636bcb7d9fe1e28bf46115f0b718fc9f4d8e18c48b39d5b2e87bb3930b" => :mojave
-    sha256 "7b44ec925ee2bbeeaba775befc77c0c22f2f690ecd94edb72e471c631da80f43" => :high_sierra
-    sha256 "26f40d7a58de2ee396fc04dd47c41e9b65640570fa1ca8b71134dd88e6e88c06" => :sierra
-    sha256 "5fc89e642b7d51c0c7965d9a952d1b697f94b4ec16d7711ff37387979ce47f5d" => :el_capitan
+    rebuild 1
+    sha256 "8d8967fef8cd57d8232d0786e3e05dbed719858e9f34715b300e12a102094e8f" => :catalina
+    sha256 "86ce34888f43ec26b543b738b818a3fa638367d967185acf3d452938a2f51d6e" => :mojave
+    sha256 "aa253ec8ae9652509eb22aadb2a49fc1b5ffc736f5c268a57406c92a3f2a75ad" => :high_sierra
   end
 
   def install
