d545b4e1c729415cab33f547544ff8800c4fd4a6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 08:51:41 +0000

    latexml: update 0.8.4 bottle.

--- Formula/latexml.rb
+++ Formula/latexml.rb
@@ -7,6 +7,7 @@ class Latexml < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "104d0a5860ea00a2766f2760265156685b8d23ecfe602ad2a5b0dcadabb40aef" => :catalina
     sha256 "e7c78acf6bb580fdb949777719972a806c1d7d349d9e826b338572bedde6cf5c" => :mojave
     sha256 "388dbf99df85e55879cccfa48eed9b6ef362d13f3ffe83dbfd09b1e7fb12fa1f" => :high_sierra
     sha256 "b911ac9897012edcc7c32d96785e4ca3830ce8cbddff78da0942263c7fb0d0bb" => :sierra
