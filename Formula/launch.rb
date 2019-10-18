3fabc677cb42aa26d3595a1df35332f1b9d2d8c6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-18 08:51:40 +0000

    launch: update 1.2.5 bottle.

--- Formula/launch.rb
+++ Formula/launch.rb
@@ -7,6 +7,7 @@ class Launch < Formula
 
   bottle do
     cellar :any_skip_relocation
+    sha256 "43c3726bc3c2cf3f9b70cf0f55a4ea9556d1760228fb4c3e5077925ac19b0692" => :catalina
     sha256 "39473462b7b66e86f4d3abfef40f6b9314793ae6d621dba3ca61ccf9f06f1d0f" => :mojave
     sha256 "7ea743ebff2392770ebb7bd7ff0a420ad9a3f6bc50d1181df7518a5fe46a8000" => :high_sierra
     sha256 "4fa06c0d934752695a0c823c51569063b50f8826c7fb9cbd302f731b059e4225" => :sierra
