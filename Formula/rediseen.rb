6d75a758cfedc405f7480246923e06ea71da3f48
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-19 06:54:02 +0000

    rediseen: add 1.0.0 bottle.

--- Formula/rediseen.rb
+++ Formula/rediseen.rb
@@ -4,6 +4,13 @@ class Rediseen < Formula
   url "https://github.com/XD-DENG/rediseen/archive/1.0.0.tar.gz"
   sha256 "50b60cdf3724ef7d4b526f5d5c9806a87d54c91010c336e398541f017a44b3f2"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7d13a516d6a791302ce894e226c7cc8b5df10d4f2a7a71785bdba93dfeef16d9" => :catalina
+    sha256 "fffed7fa35c5bdc7d8fe5032e5421efe46c951d9ee353539c7a783dbd8ad491d" => :mojave
+    sha256 "317d0817d2a9a9f310302e01b5ef71c14d1e5c101c62cb40db1d007af72f82a7" => :high_sierra
+  end
+
   head do
     url "https://github.com/XD-DENG/rediseen.git"
   end
