b3d58d48e5f02c49fa0affabe32e3890a16db62d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-21 07:31:55 +0000

    osmcoastline: add 2.2.4 bottle.

--- Formula/osmcoastline.rb
+++ Formula/osmcoastline.rb
@@ -4,6 +4,13 @@ class Osmcoastline < Formula
   url "https://github.com/osmcode/osmcoastline/archive/v2.2.4.tar.gz"
   sha256 "7a399661b46e4e700b11d6a5163ec7bdc8ad49f0837b524f1f2535cca7b9ee43"
 
+  bottle do
+    cellar :any
+    sha256 "83e62c7b47703385c47615ef84a4c0b35ecce5fd0a53e299e7111dd0d377578d" => :catalina
+    sha256 "2c47dd8a8b33920e60bc14ed9bf43eb09c638c35f4ec245af2a994082fefa258" => :mojave
+    sha256 "ebf1dbec28cb28865e6fe451547eb8398c431264b3f595e1ac106a5f471c97bb" => :high_sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "libosmium" => :build
   depends_on "gdal"
