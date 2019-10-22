f00ee75ed43fcd7df8579a638e70424a5979e9b9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-22 13:41:10 +0000

    vip: update 19971113 bottle.

--- Formula/vip.rb
+++ Formula/vip.rb
@@ -7,13 +7,10 @@ class Vip < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d49d0ecf58de93d03369024f165aae99210c2b72cffe4aadff7a2299236d7420" => :catalina
-    sha256 "da936f8d9a839a1235962c772ae957563c13f089d5953df7c1ba64b694cb0687" => :mojave
-    sha256 "5622623485848fc1e4238404c3491f056f4220c6a80fbe9342ec89cd34b15bcb" => :high_sierra
-    sha256 "12eec6f5294a94f2fb09c54f218470aab2fb7bad58570e8a82c789d8ba5e9639" => :sierra
-    sha256 "1bf2041f43bcea1e8c503119a9b34f8849b751da767ec5b5094fd5fa8fe5f297" => :el_capitan
-    sha256 "8e60ec9a240192f872f5d730ca93c9bc9e73d4644e685173554ff786b634ef7c" => :yosemite
-    sha256 "96ae6a94171da559b1762970dc99b1c458ccd68c061d40248879d16bb6df8511" => :mavericks
+    rebuild 1
+    sha256 "d4d4e43e16a975074c70fa52e2aa6b693c8b89493e91d566aed78255130dad3a" => :catalina
+    sha256 "d4d4e43e16a975074c70fa52e2aa6b693c8b89493e91d566aed78255130dad3a" => :mojave
+    sha256 "d4d4e43e16a975074c70fa52e2aa6b693c8b89493e91d566aed78255130dad3a" => :high_sierra
   end
 
   resource "man" do
