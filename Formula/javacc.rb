4c4dbf25d3b5b127bf2dcf801b17fcc1648c29aa
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-22 15:24:43 +0000

    javacc: add 7.0.5 bottle.

--- Formula/javacc.rb
+++ Formula/javacc.rb
@@ -4,6 +4,13 @@ class Javacc < Formula
   url "https://github.com/javacc/javacc/archive/7.0.5.tar.gz"
   sha256 "d1502f8a7ed607de17427a1f33e490a33b0c2d5612879e812126bf95e7ed11f4"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "bddd6aa1083daa875485e1fa612d476a68d2c8d1c1b9ba339c1c190bc29b2246" => :catalina
+    sha256 "ba78c2e54814a789fe4bf2cf03bbcbc66e29326ae2b2c7b605b3f1c5ce9b6bb5" => :mojave
+    sha256 "1113cb1594867773777497caf05e2183892e8d365aabeceb4d08dd8a75767b88" => :high_sierra
+  end
+
   depends_on "ant" => :build
   depends_on :java
 
