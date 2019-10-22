f11645ae71ad4b940394ba515c4bfe5717630511
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-22 13:41:11 +0000

    tiny-fugue: update 5.0b8_1 bottle.

--- Formula/tiny-fugue.rb
+++ Formula/tiny-fugue.rb
@@ -7,10 +7,10 @@ class TinyFugue < Formula
   revision 1
 
   bottle do
-    sha256 "24ef105267f27da0182f9f179ab73c2388230f3886b20697ff98495a235feeb4" => :catalina
-    sha256 "2196e7b32acfb7604f19b8734d5425b32650ff1154f6c327313b8d18c52ffda1" => :mojave
-    sha256 "32fcc0d6629da120ee1698b6185aaea0387519861182a4d6f10e6c67e9455405" => :high_sierra
-    sha256 "cbdc761189ce2c20a0a00b2ba2bfe930d0b149ea932dda7c562fa36d6b199e7e" => :sierra
+    rebuild 1
+    sha256 "7a51e1401af0fc0e8d61f0648b991f51bd5589ff8e2f7421f3b5c9085f2e8118" => :catalina
+    sha256 "962bd4e3d293078dc0930343b077a96ecc55d7cb1447301790e142dd8cbb949d" => :mojave
+    sha256 "15b5de4edc4a70b5f4c112f380fd28741ceccafa9c25ba060522f452df8ebaa0" => :high_sierra
   end
 
   depends_on "libnet"
