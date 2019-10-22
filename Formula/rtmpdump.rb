1334af664bc06085041a30b3554ce3a614238955
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-22 13:41:12 +0000

    rtmpdump: update 2.4+20151223_1 bottle.

--- Formula/rtmpdump.rb
+++ Formula/rtmpdump.rb
@@ -9,10 +9,10 @@ class Rtmpdump < Formula
 
   bottle do
     cellar :any
-    sha256 "f39d714005d28ed61728832877433a68dd256796bc225bac68b505b2c1d97ef4" => :catalina
-    sha256 "97cf25d61d474c2115f6448940f924324d630b60776396398662b1368b4544da" => :mojave
-    sha256 "7e95dc18fc03a6c1f19385e1507448f23e2e570c9b3ad60bd3fbc05c65295fb8" => :high_sierra
-    sha256 "2118d007922d98ae71169be417106f594636e6ff979611b9e51dd2cf09c002b7" => :sierra
+    rebuild 1
+    sha256 "eea445ef751981cc96372e498f6711e437d0eae3d875c0e163bad7d93339dea3" => :catalina
+    sha256 "fdd359a601d28119dfd35151569bcb7dadd566d5ed2e5eed56eb915878f3a013" => :mojave
+    sha256 "979f2d98ceda50fee10b95265b8873de82dd292cc5362796ccb4cf8e56886a46" => :high_sierra
   end
 
   depends_on "openssl@1.1"
