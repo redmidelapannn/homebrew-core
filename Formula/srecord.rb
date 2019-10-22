6a0df873c27db9cf8f8fca18f0453031b5c741bc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-22 13:41:13 +0000

    srecord: update 1.64 bottle.

--- Formula/srecord.rb
+++ Formula/srecord.rb
@@ -6,13 +6,10 @@ class Srecord < Formula
 
   bottle do
     cellar :any
-    sha256 "cc4e1e89835954876853f5f7bcccbfd172adbb5651c1f2790ea3da10e4347845" => :catalina
-    sha256 "6b3b825b501d1ea1635d107fb62021dde713f6da375f53f1a1fdcb59070df63a" => :mojave
-    sha256 "f6341ba9022e6cbc057c519fcdc7c7518247c850025777b80d2463341315d88c" => :high_sierra
-    sha256 "0601896fc392a13f7ef861fc3840fadfc7ddc7313763c1d374555129f4301c0d" => :sierra
-    sha256 "6a0df3e5fb40699d9b1198562b3b3a4e1745c3a0d12923c461246b7784b8324c" => :el_capitan
-    sha256 "c3c29b357c44bc3da2dbb8f23a6d83aeb637aa374fe0564eb9454e5e6b53d54c" => :yosemite
-    sha256 "10a04c2aca5e6f554c00aa57bd05f9c3cbe46238c9af66678dc1e6a3323c5cdb" => :mavericks
+    rebuild 1
+    sha256 "a59f54c53aacff53d35f1f34c2e58de279557a9f30100f74841fe3cd7b25049b" => :catalina
+    sha256 "afa8be1d741be6b60599540cfd70cc23bb2f5c0d126a41ccb1ef1d389fa0e103" => :mojave
+    sha256 "8d731a8cb7f4c03cdb7f6ff44467314147f497c67fce7a9ad684faee8ae287cb" => :high_sierra
   end
 
   depends_on "libtool" => :build
