d7afa3ea202eba86f95f4777751bb2977fedcb7b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-23 16:55:38 +0000

    fdm: add 2.0 bottle.

--- Formula/fdm.rb
+++ Formula/fdm.rb
@@ -4,6 +4,13 @@ class Fdm < Formula
   url "https://github.com/nicm/fdm/releases/download/2.0/fdm-2.0.tar.gz"
   sha256 "06b28cb6b792570bc61d7e29b13d2af46b92fea77e058b2b17e11e8f7ed0cea4"
 
+  bottle do
+    cellar :any
+    sha256 "c5b128c154604f5f2af18c2e4799f4a0b1e3577d28ceec071403b3b5648427bd" => :catalina
+    sha256 "d9778135ad2965c20c34065e3441a675efed74f2a10cfdeb94de055d64fe744a" => :mojave
+    sha256 "0be1de236753f148cea46b05256c265e0cf1f5ed65efe9937a2a2e02d7e9d173" => :high_sierra
+  end
+
   depends_on "openssl@1.1"
   depends_on "pcre"
   depends_on "tdb"
