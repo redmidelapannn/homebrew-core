509d5600f68f918cf272c381aea176a61c23af0f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-24 03:27:46 +0000

    libserdes: add 5.3.1 bottle.

--- Formula/libserdes.rb
+++ Formula/libserdes.rb
@@ -4,6 +4,13 @@ class Libserdes < Formula
   url "http://packages.confluent.io/deb/5.3/pool/main/c/confluent-libserdes/confluent-libserdes_5.3.1.orig.tar.gz"
   sha256 "06bcb4066f6f13d2f4ece7987188c1feb9510c36bb5828e3283c267267c3d470"
 
+  bottle do
+    cellar :any
+    sha256 "c0349579280ea9c2a09387e288557af6e4e92fe6310abbd1608540900257c03d" => :catalina
+    sha256 "4698e76f52c30d4c5235213e83a7d7c034b9596f960c637f877394de59f81306" => :mojave
+    sha256 "2ba600f3997ec3d5ef1a58601915a79982cec1fd82f5ade0d068fd0b20643d93" => :high_sierra
+  end
+
   depends_on "avro-c"
   depends_on "jansson"
   uses_from_macos "curl"
