aea745d082f7c5680a4908e671a45d2dfd28560b
JP Simard <jp@jpsim.com>
2019-10-24 15:33:37 -0700

    swiftlint 0.36.0

--- Formula/swiftlint.rb
+++ Formula/swiftlint.rb
@@ -2,8 +2,8 @@ class Swiftlint < Formula
   desc "Tool to enforce Swift style and conventions"
   homepage "https://github.com/realm/SwiftLint"
   url "https://github.com/realm/SwiftLint.git",
-      :tag      => "0.35.0",
-      :revision => "8dc8421a49f2b74f79da3ef514a26249027309b9"
+      :tag      => "0.36.0",
+      :revision => "652e5dd59c44f00ef89549a1d923f2a72378b85f"
   head "https://github.com/realm/SwiftLint.git"
 
   bottle do
