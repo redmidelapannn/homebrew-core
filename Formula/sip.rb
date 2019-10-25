c06d857e1cc86efb9081ee940e5a4b715f54be54
Alexander Bayandin <a.bayandin@gmail.com>
2019-10-25 19:41:33 +0100

    sip 5.0.0

--- Formula/sip.rb
+++ Formula/sip.rb
@@ -1,9 +1,10 @@
 class Sip < Formula
+  include Language::Python::Virtualenv
+
   desc "Tool to create Python bindings for C and C++ libraries"
   homepage "https://www.riverbankcomputing.com/software/sip/intro"
-  url "https://www.riverbankcomputing.com/static/Downloads/sip/4.19.19/sip-4.19.19.tar.gz"
-  sha256 "5436b61a78f48c7e8078e93a6b59453ad33780f80c644e5f3af39f94be1ede44"
-  revision 1
+  url "https://files.pythonhosted.org/packages/c6/7f/b0830b133e6fac3902ecea99acc99e41972d9b565cda5d68ae33ae37909c/sip-5.0.0.tar.gz"
+  sha256 "c034d427f52584d63277dd7df4d16d00c5df0b5cb838e0c256865b854b7e074b"
   head "https://www.riverbankcomputing.com/hg/sip", :using => :hg
 
   bottle do
@@ -15,37 +16,28 @@ class Sip < Formula
 
   depends_on "python"
 
+  resource "toml" do
+    url "https://files.pythonhosted.org/packages/b9/19/5cbd78eac8b1783671c40e34bb0fa83133a06d340a38b55c645076d40094/toml-0.10.0.tar.gz"
+    sha256 "229f81c57791a41d65e399fc06bf0848bab550a9dfd5ed66df18ce5f05e73d5c"
+  end
+
+  resource "packaging" do
+    url "https://files.pythonhosted.org/packages/5a/2f/449ded84226d0e2fda8da9252e5ee7731bdf14cd338f622dfcd9934e0377/packaging-19.2.tar.gz"
+    sha256 "28b924174df7a2fa32c1953825ff29c61e2f5e082343165438812f00d3a7fc47"
+  end
+
+  resource "pyparsing" do
+    url "https://files.pythonhosted.org/packages/7e/24/eaa8d7003aee23eda270099eeec754d7bf4399f75c6a011ef948304f66a2/pyparsing-2.4.2.tar.gz"
+    sha256 "6f98a7b9397e206d78cc01df10131398f1c8b8510a2f4d97d9abd82e1aacdd80"
+  end
+
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/dd/bf/4138e7bfb757de47d1f4b6994648ec67a51efe58fa907c1e11e350cddfca/six-1.12.0.tar.gz"
+    sha256 "d16a0141ec1a18405cd4ce8b4613101da75da0e9a7aec5bdd4fa804d0e0eba73"
+  end
+
   def install
-    ENV.prepend_path "PATH", Formula["python"].opt_libexec/"bin"
-    ENV.delete("SDKROOT") # Avoid picking up /Application/Xcode.app paths
-
-    if build.head?
-      # Link the Mercurial repository into the download directory so
-      # build.py can use it to figure out a version number.
-      ln_s cached_download/".hg", ".hg"
-      # build.py doesn't run with python3
-      system "python", "build.py", "prepare"
-    end
-
-    version = Language::Python.major_minor_version "python3"
-    system "python3", "configure.py",
-                      "--deployment-target=#{MacOS.version}",
-                      "--destdir=#{lib}/python#{version}/site-packages",
-                      "--bindir=#{bin}",
-                      "--incdir=#{include}",
-                      "--sipdir=#{HOMEBREW_PREFIX}/share/sip"
-    system "make"
-    system "make", "install"
-    system "make", "clean"
-  end
-
-  def post_install
-    (HOMEBREW_PREFIX/"share/sip").mkpath
-  end
-
-  def caveats; <<~EOS
-    The sip-dir for Python is #{HOMEBREW_PREFIX}/share/sip.
-  EOS
+    virtualenv_install_with_resources
   end
 
   test do
