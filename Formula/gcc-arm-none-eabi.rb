45a31b564035834c47816611f94bae3a91f0f764
Zsolt Prontvai <prozsolt@gmail.com>
2019-10-27 19:32:29 +0100

    One step back

--- Formula/gcc-arm-none-eabi.rb
+++ Formula/gcc-arm-none-eabi.rb
@@ -5,15 +5,17 @@ class GccArmNoneEabi < Formula
   version "8-2019-q3-update"
   sha256 "e8a8ddfec47601f2d83f1d80c0600f198476f227102121c8d6a5a781d0c2eeef"
 
-  depends_on "gmp"
-  depends_on "isl"
-  depends_on "libelf"
-  depends_on "libmpc"
-  depends_on "mpfr"
+  env :std
 
-  uses_from_macos "expat"
-  uses_from_macos "libiconv"
-  uses_from_macos "zlib"
+  depends_on "gmp" # 6.1.0 6.1.2
+  depends_on "isl" # 0.18 0.21
+  depends_on "libelf" # 0.8.13 0.8.13
+  depends_on "libmpc" # 1.0.3 1.1.0
+  depends_on "mpfr" # 3.1.4 4.0.2
+
+  uses_from_macos "expat" # 2.1.1 2.2.9
+  uses_from_macos "libiconv" # 1.14 1.16
+  uses_from_macos "zlib" # 1.2.8 1.2.11
 
   def install
     cd "src" do
@@ -21,7 +23,7 @@ class GccArmNoneEabi < Formula
     end
 
     inreplace "build-common.sh" do |s|
-      s.gsub! /^clean_env$/, ""
+      #s.gsub! /^clean_env$/, ""
       s.gsub! "INSTALLDIR_NATIVE=$ROOT/install-native", "INSTALLDIR_NATIVE=#{prefix}"
       s.gsub! "INSTALLDIR_NATIVE_DOC=$ROOT/install-native/share/doc/gcc-arm-none-eabi", "INSTALLDIR_NATIVE_DOC=#{doc}"
 
@@ -40,9 +42,15 @@ class GccArmNoneEabi < Formula
       s.gsub! "--htmldir=$INSTALLDIR_NATIVE_DOC/html", "--htmldir=#{doc}/html"
       s.gsub! "--pdfdir=$INSTALLDIR_NATIVE_DOC/pdf", "--pdfdir=#{doc}/pdf"
       s.gsub! "--with-sysroot=$INSTALLDIR_NATIVE/arm-none-eabi", "--with-sysroot=#{prefix}/arm-none-eabi"
+
+      # Task III-11, IV-8, V-0 and V-1 generates package which we don't need
+      s.gsub! /^echo Task \[III-11\].*?popd/m, ""
+      s.gsub! /^echo Task \[IV-8\].*?popd/m, ""
+      s.gsub! /^echo Task \[V-0\].*?popd/m, ""
+      s.gsub! /^echo Task \[V-1\].*?popd/m, ""
     end
 
-    system "./build-toolchain.sh", "--build_type=ppa", "--skip_steps=manual"
+    system "./build-toolchain.sh", "--skip_steps=manual"
   end
 
   test do
