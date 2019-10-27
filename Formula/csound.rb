d478405230a58a5bfe64e671ad5a5619cce045f1
Nathan Whetsell <nathan.whetsell@gmail.com>
2019-10-27 07:49:40 -0400

    csound: add docs, HRTF data, HEAD

--- Formula/csound.rb
+++ Formula/csound.rb
@@ -3,7 +3,8 @@ class Csound < Formula
   homepage "https://csound.com"
   url "https://github.com/csound/csound/archive/6.13.0.tar.gz"
   sha256 "183beeb3b720bfeab6cc8af12fbec0bf9fef2727684ac79289fd12d0dfee728b"
-  revision 3
+  revision 4
+  head "https://github.com/csound/csound.git", :branch => "develop"
 
   bottle do
     sha256 "bc1ee99a636b4bd41f5908437f28dd0d29c5b17a0bdb8738d0abcc46177775a8" => :mojave
@@ -37,6 +38,21 @@ class Csound < Formula
     sha256 "2716e916a9dd9445b2a4de1f2325da818b7f097ec7004d453c83b10205167100"
   end
 
+  resource "csound-manual-html" do
+    url "https://github.com/csound/csound/releases/download/6.13.0/Csound6.13.0_manual_html.zip"
+    sha256 "7b70190ba9ef7e885545b88012ce012ffc48043a17edb6ae1326a646dfc359b4"
+  end
+
+  resource "csound-manual-pdf-a4" do
+    url "https://github.com/csound/csound/releases/download/6.13.0/Csound6.13.0_manual_A4_pdf.zip"
+    sha256 "62f511ac0ad8f6327583310714f61e9b07558fc57014859099200e1d05e5a642"
+  end
+
+  resource "csound-manual-pdf-letter" do
+    url "https://github.com/csound/csound/releases/download/6.13.0/Csound6.13.0_manual_pdf.zip"
+    sha256 "09d62a688bdaef6a53e9d33feee1290c18099b4f927cdba54f3a79ab41bd91d7"
+  end
+
   resource "getfem" do
     url "https://download.savannah.gnu.org/releases/getfem/stable/getfem-5.3.tar.gz"
     sha256 "9d10a1379fca69b769c610c0ee93f97d3dcb236d25af9ae4cadd38adf2361749"
@@ -68,14 +84,26 @@ class Csound < Formula
 
     libexec.install "#{buildpath}/interfaces/ctcsound.py"
 
-    version = Language::Python.major_minor_version "python3"
-    (lib/"python#{version}/site-packages/homebrew-csound.pth").write <<~EOS
+    python_version = Language::Python.major_minor_version "python3"
+    (lib/"python#{python_version}/site-packages/homebrew-csound.pth").write <<~EOS
       import site; site.addsitedir('#{libexec}')
     EOS
+
+    (pkgshare/"hrtf-data").install Dir["samples/*.dat"]
+
+    resource("csound-manual-html").stage { (doc/"html").install Dir["*"] }
+    resource("csound-manual-pdf-a4").stage { (doc/"pdf").install "Csound6.13.0_manual_A4.pdf" }
+    resource("csound-manual-pdf-letter").stage { (doc/"pdf").install "Csound6.13.0_manual.pdf" }
   end
 
   def caveats; <<~EOS
-    To use the Python bindings, you may need to add to your .bash_profile:
+    The manual as HTML is in
+      #{HOMEBREW_PREFIX}/share/doc/csound/html
+
+    HRTF data files are in
+      #{HOMEBREW_PREFIX}/share/csound/hrtf-data
+
+    To use the Python bindings, you may need to add to #{shell_profile}:
       export DYLD_FRAMEWORK_PATH="$DYLD_FRAMEWORK_PATH:#{opt_prefix}/Frameworks"
   EOS
   end
@@ -83,16 +111,18 @@ class Csound < Formula
   test do
     (testpath/"test.orc").write <<~EOS
       0dbfs = 1
+      sr = 44100
       gi_peer link_create
       gi_programHandle faustcompile "process = _;", "--vectorize --loop-variant 1"
       FLrun
       gi_fluidEngineNumber fluidEngine
       gi_realVector la_i_vr_create 1
       pyinit
+      pyruni "print('hello, world')"
       instr 1
           a_, a_, a_ chuap 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-          pyruni "from __future__ import print_function; print('hello, world')"
           a_signal STKPlucked 440, 1
+          a_, a_ hrtfstat a_signal, 0, 0, sprintf("hrtf-%d-left.dat", sr), sprintf("hrtf-%d-right.dat", sr), 9, sr
           hdf5write "test.h5", a_signal
           out a_signal
       endin
@@ -105,11 +135,12 @@ class Csound < Formula
 
     ENV["OPCODE6DIR64"] = "#{HOMEBREW_PREFIX}/Frameworks/CsoundLib64.framework/Resources/Opcodes64"
     ENV["RAWWAVE_PATH"] = "#{HOMEBREW_PREFIX}/share/stk/rawwaves"
+    ENV["SADIR"] = "#{HOMEBREW_PREFIX}/share/csound/hrtf-data"
 
     require "open3"
     stdout, stderr, status = Open3.capture3("#{bin}/csound test.orc test.sco")
 
-    assert_equal true, status.success?
+    assert status.success?
     assert_equal "hello, world\n", stdout
     assert_match /^rtaudio:/, stderr
     assert_match /^rtmidi:/, stderr
