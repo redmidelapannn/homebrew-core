900108f919ae33eb12f74a83ef793f4c5d93db09
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-10-28 18:50:05 +0000

    vlang: add 0.1.20 bottle.

--- Formula/vlang.rb
+++ Formula/vlang.rb
@@ -4,6 +4,13 @@ class Vlang < Formula
   url "https://github.com/vlang/v/archive/0.1.20.tar.gz"
   sha256 "8102b48b2c82be6be14633e76e71e215aab5221198315436f97be53e1abe1f5d"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e4ee9f68494a6d784c45147073841beee04950a1db79ff31ad764eea1a2ff4df" => :catalina
+    sha256 "e44ea51b2e22ce6e25401d6c03d27074e4282a91ab293a9294cf67ce02e1f87e" => :mojave
+    sha256 "aba32ac9ec1ebb45106be00c2e0308d41f6785d9ffa09104f1cbc88ba4ee1e53" => :high_sierra
+  end
+
   resource "vc" do
     url "https://github.com/vlang/vc/archive/0.1.20.tar.gz"
     sha256 "5b4fc1f39c3aef5214a3366e0d514ee2879a2e52a918dc0181df833028a0eb72"
