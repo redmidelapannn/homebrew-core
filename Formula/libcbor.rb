10d4073de13221ec2f3700eb7676bd6c62b85ba8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-01 16:19:08 +0000

    libcbor: add 0.5.0 bottle.

--- Formula/libcbor.rb
+++ Formula/libcbor.rb
@@ -1,5 +1,12 @@
  class Libcbor < Formula
    desc "CBOR format implementation for C"
+  bottle do
+    cellar :any
+    sha256 "d1f249e7c60ea01884649528acf102fb5eb2b586f6654a5991f46d35be878337" => :catalina
+    sha256 "9bd1f63ac8b46eb4c6973f1ecd86e47aa21961548b4e67b3773f836860a929dd" => :mojave
+    sha256 "811869b1a7c244270e7b8baac34ad1b59aa18047668099fad7f36f76eb24082d" => :high_sierra
+  end
+
    homepage "http://libcbor.org/"
    url "https://github.com/PJK/libcbor/archive/v0.5.0.tar.gz"
    sha256 "9bbec94bb385bad3cd2f65482e5d343ddb97e9ffe261123ea0faa3bfea51d320"
