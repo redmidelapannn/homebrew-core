6b6c9d706ab383ddf204c8efd1237ba05eef53d5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-02 09:42:58 +0000

    dav1d: update 0.4.0 bottle.

--- Formula/dav1d.rb
+++ Formula/dav1d.rb
@@ -6,6 +6,7 @@ class Dav1d < Formula
 
   bottle do
     cellar :any
+    sha256 "22f7badae1ea32590f81b57d9a1d30c6d430f66a0050d7cd3b03aa5b556ccb47" => :catalina
     sha256 "27deef5c5fb949caede94841ecb6f68c21f84f86a5251b3f5ea6b677abfaedab" => :mojave
     sha256 "af6fd10a3a742d96e099994bf9731505e569d4354d79d964bbe62b4125b6e932" => :high_sierra
     sha256 "00bc367af9e2af989a7106eb9cf710511ed5d9b9a95d29b4edf897decad7c96f" => :sierra
