290e2ec3cf87b5cdb63e39cd93191c4c0e71a0b8
FX Coudert <fxcoudert@gmail.com>
2019-11-02 17:12:33 +0900

    lfe: fix for Catalina

--- Formula/lfe.rb
+++ Formula/lfe.rb
@@ -18,12 +18,15 @@ class Lfe < Formula
   def install
     system "make"
     system "make", "MANINSTDIR=#{man}", "install-man"
-    system "make", "emacs"
     libexec.install "bin", "ebin"
     bin.install_symlink (libexec/"bin").children
     doc.install Dir["doc/*.txt"]
     pkgshare.install "dev", "examples", "test"
-    elisp.install Dir["emacs/*.elc"]
+
+    if MacOS.version <= :mojave
+      system "make", "emacs"
+      elisp.install Dir["emacs/*.elc"]
+    end
   end
 
   test do
