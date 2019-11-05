8eb75a3dfdb3477e0321323cb40fb6ba4724d042
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-05 21:31:27 +0000

    mtlswift: add 0.9.0 bottle.

--- Formula/mtlswift.rb
+++ Formula/mtlswift.rb
@@ -5,6 +5,12 @@ class Mtlswift < Formula
   head "https://github.com/s1ddok/mtlswift.git"
   version "0.9.0"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "eb731917d87817b55f7ca4e94d61e596b6353730c15f126e741f6f16226f8203" => :catalina
+    sha256 "b4216c18888e536d21c30ea21abdf32b676210022e7c2139a4ddde3256919673" => :mojave
+  end
+
   depends_on :xcode => "11.1"
 
   def install
