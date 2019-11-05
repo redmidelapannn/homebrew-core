72802e4effc61d3b568f2c6a000fc31f03c8126b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-05 21:18:16 +0000

    lanraragi: add 0.6.6 bottle.

--- Formula/lanraragi.rb
+++ Formula/lanraragi.rb
@@ -7,6 +7,13 @@ class Lanraragi < Formula
   sha256 "bc4e8db0bc4570390f5e92327ecb33caabea98696b3708f270aa06775935716b"
   head "https://github.com/Difegue/LANraragi.git"
 
+  bottle do
+    cellar :any
+    sha256 "48c1414c984380f22843897e096d066cdababbbbf2fb6060a4a328c927bb1a47" => :catalina
+    sha256 "3470e9e930ba0f1bffd255c6f61f7db6f32f06b2e703b158a252b96b51434c5b" => :mojave
+    sha256 "dd528d9c0040c1ff2d95738cbb2840d3acd04d09f09e9bbb2a10fd38e39534de" => :high_sierra
+  end
+
   depends_on "pkg-config" => :build
   depends_on "cpanminus"
   depends_on "jpeg"
