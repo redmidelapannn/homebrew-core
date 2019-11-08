56a0e4e97cab4d6cfe112cf8f016ae1e2c215651
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-08 22:45:17 +0000

    cmdg: add 1.00 bottle.

--- Formula/cmdg.rb
+++ Formula/cmdg.rb
@@ -3,6 +3,13 @@ class Cmdg < Formula
   homepage "https://github.com/ThomasHabets/cmdg"
   url "https://github.com/ThomasHabets/cmdg/archive/cmdg-1.00.tar.gz"
   sha256 "4a723a812eae27fac1334e14cfc9b92955e6a3c1c43f1de87aa294d72bb7dc25"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9782dd735edd897f92e1cc07bf1c60d5bb0057b547723e40423346f09d6288b5" => :catalina
+    sha256 "2890e999c103c026f4e89d4088b44eaa3bf935366c3c17f58401d03104d5b53a" => :mojave
+    sha256 "f2e0ae928a8c65917992b2e4d7b3ee5c6dc865ba9a9e99cbb015027f79763d6c" => :high_sierra
+  end
+
   depends_on "go"
   def install
     ENV["GOPATH"] = buildpath
