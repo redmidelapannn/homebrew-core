d9cd3da0149be0e95f889bef9d342b41ec945f06
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-08 19:25:21 +0000

    libcds: update 2.3.3 bottle.

--- Formula/libcds.rb
+++ Formula/libcds.rb
@@ -6,10 +6,10 @@ class Libcds < Formula
 
   bottle do
     cellar :any
-    sha256 "d2ac7fe9c424707b1a2698fabed2a81645bb4f79f9fb943a279aee1cfc5d38d9" => :catalina
-    sha256 "ed2baad62fe26da2891255221ee90a8d69f84214431dce923dd7c55aafa39668" => :mojave
-    sha256 "9723b22f7a6e84ef208226241ed3b19dd02b4c9aed9c01284f55bd9b825e216e" => :high_sierra
-    sha256 "fdc30046c9f96c04d7c52d43e948d2e238fb93eee5514101a0dd0ca4de3f2b32" => :sierra
+    rebuild 1
+    sha256 "4673ada5c2552d2e3b78e6f6f0bd59a9b2deffd4704031423fecdace4de09cfc" => :catalina
+    sha256 "324421c48a916fcfed8e3efefad8d3af536be9490948c3a5536981cc813fa2ef" => :mojave
+    sha256 "cc9999aef57d9cc746d4929f096a2db0cb8e2d22b75c9e9699a0c51e2b1bd2ae" => :high_sierra
   end
 
   depends_on "cmake" => :build
