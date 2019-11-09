1d9598d87939ebdc5e35a55a424c18404a9b40e2
Caleb Xu <calebcenter@live.com>
2019-11-09 08:50:27 -0500

    numpy 1.17.3

--- Formula/numpy.rb
+++ Formula/numpy.rb
@@ -1,8 +1,8 @@
 class Numpy < Formula
   desc "Package for scientific computing with Python"
   homepage "https://www.numpy.org/"
-  url "https://files.pythonhosted.org/packages/ac/36/325b27ef698684c38b1fe2e546e2e7ef9cecd7037bcdb35c87efec4356af/numpy-1.17.2.zip"
-  sha256 "73615d3edc84dd7c4aeb212fa3748fb83217e00d201875a47327f55363cef2df"
+  url "https://files.pythonhosted.org/packages/b6/d6/be8f975f5322336f62371c9abeb936d592c98c047ad63035f1b38ae08efe/numpy-1.17.3.zip"
+  sha256 "a0678793096205a4d784bd99f32803ba8100f639cf3b932dc63b21621390ea7e"
   head "https://github.com/numpy/numpy.git"
 
   bottle do
