a92cf3265fa6dd1cc155881a5b97ae63c8ebd816
Alexander Bayandin <a.bayandin@gmail.com>
2019-11-12 10:53:27 +0000

    jupyterlab: revision bump for python

--- Formula/jupyterlab.rb
+++ Formula/jupyterlab.rb
@@ -3,6 +3,7 @@ class Jupyterlab < Formula
   homepage "https://jupyter.org/"
   url "https://files.pythonhosted.org/packages/57/a8/9aea7e00e8634f6e73bc2641033fee2d59e5d506784474e5e5d55eb15d55/jupyterlab-1.2.0.tar.gz"
   sha256 "ce945579a6b20628d275efd4debffef96fa66ef83a10a62d7087ca5610b61668"
+  revision 1
 
   bottle do
     cellar :any
