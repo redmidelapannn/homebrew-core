cab1d0d5f946e26d46eb83273eb18d6b639473af
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-13 20:48:11 +0000

    kubernetes-helm@3: add 3.0.0 bottle.

--- Formula/kubernetes-helm@3.rb
+++ Formula/kubernetes-helm@3.rb
@@ -5,6 +5,13 @@ class KubernetesHelmAT3 < Formula
       :tag      => "v3.0.0",
       :revision => "e29ce2a54e96cd02ccfce88bee4f58bb6e2a28b6"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4bd78cf9758df6b11b911a69979798b74d01f7a9a23dd1d28e85fc3e895ef548" => :catalina
+    sha256 "b82e6286768aeab30f7c2d00a1f7c2ddf58e74968328fcd925bea9035592f370" => :mojave
+    sha256 "cb8ec19e1c6ac73396eba0acb0859894760796dcfd4e77364ebdff36fea2f7c4" => :high_sierra
+  end
+
   keg_only :versioned_formula
 
   depends_on "glide" => :build
