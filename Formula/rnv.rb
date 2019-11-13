81ef532b0cf955ae11bafc7ec01fa6a5620f111d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-13 21:55:13 +0000

    rnv: update 1.7.11 bottle.

--- Formula/rnv.rb
+++ Formula/rnv.rb
@@ -6,13 +6,10 @@ class Rnv < Formula
 
   bottle do
     cellar :any
-    sha256 "9a780a7b9ed3b264a7d0471aba7aac503b60640af76156028ecf118a0c35665e" => :catalina
-    sha256 "06a2cb705d679da7de638434f45e28764dcd448863d31f6b39ab090dfde4c04f" => :mojave
-    sha256 "9bf4571824c6d8e837cfcad7ac5e16c6bfc120d4638f0428cdfb8f14203b8c41" => :high_sierra
-    sha256 "8dd3263bb656dcca22605b12faf4c6f54d65e5040e58a7a464c85b69ca19dc99" => :sierra
-    sha256 "1c1aa519b786f842b39720e33900e92a2f2f8deef403755e79e2d3b518897ff1" => :el_capitan
-    sha256 "6d46cb2e6476e22b8bb04d00f599884aa8e44ba7e199ad860e4f15795b04e83b" => :yosemite
-    sha256 "f9c4575d7384100b1cc97d9f421b5906ea5068f612c346ffa0238db6c8e855be" => :mavericks
+    rebuild 1
+    sha256 "c6b244c49e6fd700ac29fc754d7bfafc008f56524bb5e4d9b0b8a98ce8c2641e" => :catalina
+    sha256 "f45d599809c019afac6837062556006a8fc1581f3ee37b0e120393d60f76da9b" => :mojave
+    sha256 "2af95c556f19752faafd61308eba31b526cd1839df79de21f2f5f2857b27349f" => :high_sierra
   end
 
   depends_on "expat"
