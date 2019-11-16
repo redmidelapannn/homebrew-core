97e3192f68fe54ffb0be7210daa4708fec261fb9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-16 19:26:44 +0000

    flutter: add stable bottle.

--- Formula/flutter.rb
+++ Formula/flutter.rb
@@ -4,6 +4,12 @@ class Flutter < Formula
   url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v1.9.1+hotfix.6-stable.zip"
   sha256 "8d0b3e217e45fbde64e117c5932ec5bd18ced0e8e8fba80a0fec95e38854bb6a"
 
+  bottle do
+    sha256 "ff33c6370810b590a1730fd7c892b5500d80c35f7da25db6c75f64b622b146d7" => :catalina
+    sha256 "ff33c6370810b590a1730fd7c892b5500d80c35f7da25db6c75f64b622b146d7" => :mojave
+    sha256 "ff33c6370810b590a1730fd7c892b5500d80c35f7da25db6c75f64b622b146d7" => :high_sierra
+  end
+
   def install
     cp_r ".", prefix
   end
