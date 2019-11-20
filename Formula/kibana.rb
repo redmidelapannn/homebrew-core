b5a78ff37f92571bc51e4c90d0d762507df98986
Rui Chen <chenrui333@gmail.com>
2019-11-20 16:56:11 -0500

    kibana 6.8.5

--- Formula/kibana.rb
+++ Formula/kibana.rb
@@ -2,8 +2,8 @@ class Kibana < Formula
   desc "Analytics and search dashboard for Elasticsearch"
   homepage "https://www.elastic.co/products/kibana"
   url "https://github.com/elastic/kibana.git",
-      :tag      => "v6.8.3",
-      :revision => "c814843540abb9dedf0f89bddc61bee364527743"
+      :tag      => "v6.8.5",
+      :revision => "cce96d289311e075978e0443a45698aef42c9014"
   head "https://github.com/elastic/kibana.git"
 
   bottle do
