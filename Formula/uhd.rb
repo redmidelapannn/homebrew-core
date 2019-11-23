b6dcc33859edad1b6a7f1ae1623a286b08c6b3b5
Bo Anderson <mail@boanderson.me>
2019-11-23 16:36:59 +0000

    uhd 3.14.1.1

--- Formula/uhd.rb
+++ Formula/uhd.rb
@@ -1,8 +1,8 @@
 class Uhd < Formula
   desc "Hardware driver for all USRP devices"
   homepage "https://files.ettus.com/manual/"
-  url "https://github.com/EttusResearch/uhd/archive/v3.14.1.0.tar.gz"
-  sha256 "8fc1ad70d80f7f69a30c957fee218ef8767cfd5a0ee4f0830e506f2b22e5b923"
+  url "https://github.com/EttusResearch/uhd/archive/v3.14.1.1.tar.gz"
+  sha256 "8cbcb22d12374ceb2859689b1d68d9a5fa6bd5bd82407f66952863d5547d27d0"
   head "https://github.com/EttusResearch/uhd.git"
 
   bottle do
@@ -19,8 +19,8 @@ class Uhd < Formula
   depends_on "python"
 
   resource "Mako" do
-    url "https://files.pythonhosted.org/packages/eb/f3/67579bb486517c0d49547f9697e36582cd19dafb5df9e687ed8e22de57fa/Mako-1.0.7.tar.gz"
-    sha256 "4e02fde57bd4abb5ec400181e4c314f56ac3e49ba4fb8b0d50bba18cb27d25ae"
+    url "https://files.pythonhosted.org/packages/b0/3c/8dcd6883d009f7cae0f3157fb53e9afb05a0d3d33b3db1268ec2e6f4a56b/Mako-1.1.0.tar.gz"
+    sha256 "a36919599a9b7dc5d86a7a8988f23a9a3a3d083070023bab23d64f7f1d1e0a4b"
   end
 
   def install
@@ -31,8 +31,8 @@ class Uhd < Formula
       system "python3", *Language::Python.setup_install_args(libexec/"vendor")
     end
 
-    mkdir "host/build" do
-      system "cmake", "..", *std_cmake_args, "-DENABLE_PYTHON3=ON", "-DENABLE_STATIC_LIBS=ON"
+    mkdir "host-build" do
+      system "cmake", "../host", *std_cmake_args, "-DENABLE_PYTHON3=ON", "-DENABLE_STATIC_LIBS=ON"
       system "make"
       system "make", "test"
       system "make", "install"
