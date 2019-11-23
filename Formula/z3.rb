693ccb13200a13054e4393d47ab300113443d7c4
Alexander Bayandin <a.bayandin@gmail.com>
2019-11-23 09:33:29 +0000

    z3: revision bump for python

--- Formula/z3.rb
+++ Formula/z3.rb
@@ -3,6 +3,7 @@ class Z3 < Formula
   homepage "https://github.com/Z3Prover/z3"
   url "https://github.com/Z3Prover/z3/archive/z3-4.8.6.tar.gz"
   sha256 "37922fa5085170cad6504498d9758fb63c61d5cb5b68689c11a6c5e84f0311b3"
+  revision 1
   head "https://github.com/Z3Prover/z3.git"
 
   bottle do
@@ -38,5 +39,7 @@ class Z3 < Formula
     system ENV.cc, "-I#{include}", "-L#{lib}", "-lz3",
            pkgshare/"examples/c/test_capi.c", "-o", testpath/"test"
     system "./test"
+
+    system "python3", "-c", "import z3"
   end
 end
