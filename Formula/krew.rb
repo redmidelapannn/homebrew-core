8b444ba4e492d9c2cd2e24e016e0e140d6cb9fee
Elan Ruusamäe <glen@delfi.ee>
2019-11-25 15:22:20 +0200

    krew: build from source

--- Formula/krew.rb
+++ Formula/krew.rb
@@ -1,11 +1,20 @@
 class Krew < Formula
   desc "Package manager for kubectl plugins"
   homepage "https://krew.dev"
-  url "https://github.com/kubernetes-sigs/krew/releases/download/v0.3.2/krew.tar.gz"
-  sha256 "643d2936d00766e198a3748d2ea3f002177a3747f9fe476923c7e0002ab662f1"
+  url "https://github.com/kubernetes-sigs/krew/archive/v0.3.2.tar.gz"
+  sha256 "c1807bdf1f504061a75cce67874529533f511aa02a09e313684f2e507f1dd195"
+
+  depends_on "go" => :build
+  depends_on "kubernetes-cli"
 
   def install
-    bin.install "krew-darwin_amd64" => "kubectl-krew"
+    ENV["CGO_ENABLED"] = "0"
+    # build in local dir to avoid this error:
+    # go build: cannot write multiple packages to non-directory /usr/local/Cellar/krew/0.3.2/bin/krew
+    mkdir "build"
+    system "go", "build", "-o", "build", "-tags", "netgo", "./cmd/krew/..."
+    # install as kubectl-krew for kubectl to find as plugin
+    bin.install "build/krew" => "kubectl-krew"
   end
 
   test do
