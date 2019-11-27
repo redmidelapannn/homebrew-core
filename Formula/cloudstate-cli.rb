40285598de0ef2cebb9c4fdbb43b6a1295a35773
Adriano Santos <sleipnir@bsd.com.br>
2019-11-27 12:06:59 -0300

    Add CloudState CLI Formula

---
+++ Formula/cloudstate-cli.rb
@@ -0,0 +1,14 @@
+class CloudstateCli < Formula
+  desc "Cloudstate CLI"
+  homepage "https://cloudstate.io/"
+  url "https://github.com/sleipnir/cloudstate-cli/releases/download/0.3.54/cloudstate-0.3.54-osx.tar.gz"
+  sha256 "c46f8abc9383bd43405cf1ccb3f7956bd0098748e344b40e5caeaeeff8d9a9ab"
+
+  def install
+    bin.install "release/cloudstate"
+  end
+
+  test do
+    system "false"
+  end
+end
