876f892da6b3ce06da347d6b59fad412beed2b68
Rudi Grinberg <me@rgrinberg.com>
2019-11-27 16:10:33 +0700

    Make sure that ocaml is present for tests

--- Formula/dune.rb
+++ Formula/dune.rb
@@ -5,7 +5,7 @@ class Dune < Formula
   sha256 "9f993b8263775a2236fd4308e2fe2413d3ee925a52858e4d9e18e7f170c4b3f6"
   head "https://github.com/ocaml/dune.git"
 
-  depends_on "ocaml" => :build
+  depends_on "ocaml" => [:build, :test]
 
   def install
     system "ocaml", "configure.ml"
