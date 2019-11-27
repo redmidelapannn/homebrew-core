81ec5d2bc3b8f3ebbe52af6258832f6c6ed89294
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-27 16:37:12 +0000

    cloudstate-cli: add 0.3.54 bottle.

--- Formula/cloudstate-cli.rb
+++ Formula/cloudstate-cli.rb
@@ -4,6 +4,13 @@ class CloudstateCli < Formula
   url "https://github.com/sleipnir/cloudstate-cli/releases/download/0.3.54/cloudstate-0.3.54-osx.tar.gz"
   sha256 "c46f8abc9383bd43405cf1ccb3f7956bd0098748e344b40e5caeaeeff8d9a9ab"
 
+  bottle do
+    cellar :any
+    sha256 "d15bcbbe7707aabb89aa06f9f6c496705934df64f8bc280fe97316e5f872aae1" => :catalina
+    sha256 "d15bcbbe7707aabb89aa06f9f6c496705934df64f8bc280fe97316e5f872aae1" => :mojave
+    sha256 "d15bcbbe7707aabb89aa06f9f6c496705934df64f8bc280fe97316e5f872aae1" => :high_sierra
+  end
+
   def install
     bin.install "release/cloudstate"
   end
