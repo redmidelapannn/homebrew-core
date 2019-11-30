224b523c9fd4f1b8a7a6313f49223c30680b62bb
Alois Schlögl <alois.schloegl@gmail.com>
2019-11-30 21:28:03 +0100

    include biosig tools - in addition to libbiosig

--- Formula/biosig.rb
+++ Formula/biosig.rb
@@ -1,5 +1,5 @@
-class Libbiosig < Formula
-  desc "Biosig library"
+class Biosig < Formula
+  desc "Tools for biomedical signal processing and data conversion"
   homepage "https://biosig.sourceforge.io"
   url "https://downloads.sourceforge.net/project/biosig/BioSig%20for%20C_C%2B%2B/src/biosig4c%2B%2B-1.9.5.src.tar.gz"
   sha256 "20e72a5a07d1bf8baa649efe437b4d3ed99944f0e4dfc1fbe23bfbe4d9749ed5"
@@ -9,6 +9,7 @@ class Libbiosig < Formula
   depends_on "gnu-tar" => :build
   depends_on "pkg-config" => :build
   depends_on "dcmtk"
+  depends_on "libb64"
   depends_on "suite-sparse"
   depends_on "tinyxml" # if version == "1.9.5"
 
@@ -19,13 +20,12 @@ class Libbiosig < Formula
                           "--prefix=#{prefix}"
 
     system "make"
-    system "make", "install_headers"
-    system "make", "install_libbiosig"
+    system "make", "install_tools"
   end
 
   test do
-    # prepare for linuxbrew
-    # ext = { true => "dylib", false => "so" }
-    system "ls", "/usr/local/lib/libbiosig.dylib" # + ext[OS.mac?]
+    system "save2gdf", "-h"
+    system "physicalunits"
+    system "biosig_fhir"
   end
 end
