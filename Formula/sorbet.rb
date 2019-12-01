947175779ca88d7738ac5fb4d24a65f13de8ec52
Farid Zakaria <farid.m.zakaria@gmail.com>
2019-12-01 10:21:34 -0800

    Updated version & D_LIBCPP_DISABLE_AVAILABILITY

--- Formula/sorbet.rb
+++ Formula/sorbet.rb
@@ -1,22 +1,24 @@
 class Sorbet < Formula
   desc "Fast, powerful type checker designed for Ruby"
   homepage "https://sorbet.org"
-  url "https://github.com/sorbet/sorbet/archive/0.4.5059.20191122150919-e82c71a19.tar.gz"
-  version "0.4.5059"
-  sha256 "6f7921c4689ced005f2ee5cf82c6470a876f3b70d32161e6dacbb4bf2fa10c32"
+  url "https://github.com/sorbet/sorbet/archive/0.4.5085.20191130175112-e9dddf67d.tar.gz"
+  version "0.4.5085"
+  sha256 "79d73af69ac393db244c62b3847a496669a4dbd9c9b1c3fcf55d9d2be382aec5"
 
   depends_on "autoconf" => :build
+  depends_on "bazel" => :build
   depends_on "coreutils" => :build
   uses_from_macos "llvm" => :build
   depends_on "parallel" => :build
   depends_on :xcode => :build
 
   def install
-    # 1. use the ./bazel script which installs a very specific bazel version and it loads the *.rc files
+    # 1. use the bazel homebrew dependency not the bazel script file local
     # 2. jemalloc is not set because it does not build on all platforms
     #    specifically on Mojave since it builds with Bazel, it does not get the correct header paths for the SDK.
     #    the headers are searched only /usr/include which have been removed in Mojave to a different path
-    system "./bazel", "build", "--define=release=true", "--compilation_mode=opt", "--config=debugsymbols",
+    # 3. D_LIBCPP_DISABLE_AVAILABILITY is to make it build on older compilers https://github.com/sorbet/sorbet/issues/1281#issuecomment-511543604
+    system "#{Formula["bazel"].opt_bin}/bazel", "build", "--define=release=true", "--compilation_mode=opt", "--copt=-D_LIBCPP_DISABLE_AVAILABILITY",
            "--config=static-libs", "--config=versioned", "--verbose_failures", "//main:sorbet"
     bin.install "bazel-bin/main/sorbet" => "sorbet"
   end
