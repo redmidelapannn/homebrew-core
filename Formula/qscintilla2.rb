df1b5c31c1ccf84a08881a4990b592a5c6b32dfe
Alexander Bayandin <a.bayandin@gmail.com>
2019-12-02 19:26:01 +0000

    qscintilla2: revision bump for python@3.8

--- Formula/qscintilla2.rb
+++ Formula/qscintilla2.rb
@@ -3,7 +3,7 @@ class Qscintilla2 < Formula
   homepage "https://www.riverbankcomputing.com/software/qscintilla/intro"
   url "https://downloads.sourceforge.net/project/pyqt/QScintilla2/QScintilla-2.10.4/QScintilla_gpl-2.10.4.tar.gz"
   sha256 "0353e694a67081e2ecdd7c80e1a848cf79a36dbba78b2afa36009482149b022d"
-  revision 3
+  revision 4
 
   bottle do
     cellar :any
@@ -14,7 +14,7 @@ class Qscintilla2 < Formula
   end
 
   depends_on "pyqt"
-  depends_on "python"
+  depends_on "python@3.8"
   depends_on "qt"
   depends_on "sip"
 
