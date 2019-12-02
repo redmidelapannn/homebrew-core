10b298c047b03e771f315b67b9c8d1a8929ca607
chenxu <6235641+cxwx@users.noreply.github.com>
2019-12-02 20:43:33 +0800

    Create ccfits.rb
    mv ccfits from brewsci to core

---
+++ Formula/ccfits.rb
@@ -0,0 +1,20 @@
+class Ccfits < Formula
+  homepage "https://heasarc.gsfc.nasa.gov/fitsio/CCfits/"
+  url "https://heasarc.gsfc.nasa.gov/fitsio/CCfits/CCfits-2.5.tar.gz"
+  sha256 "938ecd25239e65f519b8d2b50702416edc723de5f0a5387cceea8c4004a44740"
+
+ 
+  option "without-check", "Disable build-time checking (not recommended)"
+
+  depends_on "cfitsio"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "check" if build.with? "check"
+    system "make", "install"
+  end
+end
