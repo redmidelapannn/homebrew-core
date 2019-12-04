3e9ccd1d57ba063ca4ed3810324aa64442b86676
Jarry Shaw <jarryshaw@icloud.com>
2019-12-04 20:20:37 +0800

    Move the require inside of the class

--- Formula/node-sass.rb
+++ Formula/node-sass.rb
@@ -1,12 +1,14 @@
-require "language/node"
-
 class NodeSass < Formula
+  require "language/node"
+
   desc "JavaScript implementation of a Sass compiler"
   homepage "https://github.com/sass/dart-sass"
   url "https://registry.npmjs.org/sass/-/sass-1.23.7.tgz"
   sha256 "6113e3c3034a8d7701c9ecc73e7c2f53e6e419a47d42a63e26ac4546a878328e"
 
   depends_on "node"
+
+  conflicts_with "dart-sass", :because => "both install a `sass` binary"
 
   def install
     system "npm", "install", *Language::Node.std_npm_install_args(libexec)
