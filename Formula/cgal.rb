5efe054ed350d1d3fb0c110fcfd8b5fb3c8195ad
Fritz Mueller <fritzm@fritzm.org>
2019-12-07 22:55:08 -0800

    cgal 5.0: update test code

--- Formula/cgal.rb
+++ Formula/cgal.rb
@@ -34,7 +34,6 @@ class Cgal < Formula
   test do
     # https://doc.cgal.org/latest/Algebraic_foundations/Algebraic_foundations_2interoperable_8cpp-example.html
     (testpath/"surprise.cpp").write <<~EOS
-      #include <CGAL/basic.h>
       #include <CGAL/Coercion_traits.h>
       #include <CGAL/IO/io.h>
       template <typename A, typename B>
