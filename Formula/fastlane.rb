aa1e4153a71c00d6901421c0ed6f056d434d0f3e
Josh Holtz <josh@rokkincat.com>
2019-12-11 11:17:34 -0600

    Update fastlane.rb

--- Formula/fastlane.rb
+++ Formula/fastlane.rb
@@ -1,27 +1,55 @@
 class Fastlane < Formula
   desc "Easiest way to build and release mobile apps"
   homepage "https://fastlane.tools"
-  url "https://github.com/fastlane/packaged-fastlane/archive/2.0.0.tar.gz"
-  version "2.137.0"
-  sha256 "5065f68499823f0137a848af7b975f4fccb4085a366f4fc2cfc27ba7e0188d41"
-  head "https://github.com/fastlane/packaged-fastlane.git"
+  url "https://github.com/fastlane/fastlane/archive/2.137.0.tar.gz"
+  sha256 "ff3813d75bbced1030b5d7e3e714e75f358788f74d951be796515e8ec4e7ec79"
+  head "https://github.com/fastlane/fastlane.git"
 
-  depends_on "ruby@2.5"
+  RUBY_DEP = "ruby@2.5".freeze
+
+  depends_on RUBY_DEP
 
   def install
-    system "./install", version.to_s, prefix
-  end
+    # Build and unpack gem into lib directory to reference from our Gemfile
+    system "gem", "build", "fastlane.gemspec"
+    gem_name = Dir["fastlane-*.gem"].last
+    version_name = File.basename(gem_name, ".gem")
+    system "gem", "unpack", gem_name, "--target", lib
 
-  def fastlane_dir_bin
-    File.join(prefix, "bin")
-  end
+    # Need to also unpack the gemspec to install from bundler
+    # Manually copying since 'gem unpack' doesn't recognize '--target'
+    system "gem", "unpack", gem_name, "--spec"
+    cp "#{version_name}.gemspec", "#{lib}/#{version_name}/fastlane.gemspec"
 
-  def fastlane_executable
-    File.join(fastlane_dir_bin, "fastlane")
+    # Gemfile referencing our unpacked gem
+    (prefix/libexec/"Gemfile").write <<~EOS
+      source "https://rubygems.org"
+      gem "fastlane", path: "#{lib}/#{version_name}"
+    EOS
+
+    ENV["GEM_HOME"] = libexec
+    ENV["BUNDLE_PATH"] = libexec
+    ENV["BUNDLE_GEMFILE"] = libexec/"Gemfile"
+    ENV.prepend_path "PATH", Formula[RUBY_DEP].opt_bin
+
+    # Installing bundler and fastlane dependencies
+    system "gem", "install", "bundler"
+    bundle = Dir["#{libexec}/**/bundle"].last
+    system bundle, "install", "--without", "test"
+
+    # fastlane executable referencing our gems installed in libexec
+    (bin/"fastlane").write <<~EOS
+      #!/bin/bash
+      export PATH="#{Formula[RUBY_DEP].opt_bin}:$PATH}"
+      GEM_HOME="#{libexec}" GEM_PATH="#{libexec}" BUNDLE_GEMFILE="#{libexec}/Gemfile" \\
+        exec "#{bundle}" exec "#{libexec}/bin/fastlane" "$@"
+    EOS
+    chmod("+x", File.join(bin, "fastlane"))
   end
 
   test do
-    assert_true File.exist?(fastlane_executable)
-    assert_true File.exist?(File.join(prefix, "bundle-env"))
+    output = shell_output("#{bin}/fastlane --version")
+    assert_true output.include?("fastlane #{version}")
+    assert_true output.include?("#{prefix}/lib/fastlane-#{versionb}/bin/fastlane")
   end
 end
