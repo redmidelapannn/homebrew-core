cc9f4cc21b39876396b18273a21f83f812beafb7
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-12 03:24:26 +0000

    pulsarctl: add 0.2.0 bottle.

--- Formula/pulsarctl.rb
+++ Formula/pulsarctl.rb
@@ -6,6 +6,13 @@ class Pulsarctl < Formula
       :revision => "2faa2b8033f51ad8ea3d246fd5effa47263262e2"
   sha256 "d0d6b55017cf349dd893248b0541c8026db9be5d5a7c5523d7bb91426c279fe3"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f710c405c496103e1d91756825ff1b6bd46b3adafe64dc8b8c14532047079a9c" => :catalina
+    sha256 "6fbce1297d326f7a2733bdb966465f2678296c5e3a281c5e1114eaf65e3eac12" => :mojave
+    sha256 "14c6927aafe048c1d6f17ac3b4d468b13363c98858bf0a9fcd7ff4c69df2bfeb" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
