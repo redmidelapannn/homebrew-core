2eb7dfdb469e3dc33c5096bfef3f42aacf70f620
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-13 18:42:50 +0000

    chicon: add 0.3.0 bottle.

--- Formula/chicon.rb
+++ Formula/chicon.rb
@@ -5,6 +5,12 @@ class Chicon < Formula
   sha256 "ccad1f028be8f5122a221c89267e7d188b032d23f3aff255ea7afafb26adf80f"
   head "https://github.com/okdana/chicon.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ce894c169547232036450aa0346437f4100bde480bb83e1678ba939f90f20b61" => :catalina
+    sha256 "43c3c1105bf4a283e558b835a9bbca608f449e3ce16c6d3ff9ca07a8fc7e9edb" => :mojave
+  end
+
   depends_on :xcode => ["10.2", :build]
 
   def install
