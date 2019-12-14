19c14dfa0634449062492fcd2d4193928a3a9329
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-14 16:09:20 +0000

    x86_64-elf-binutils: add 2.32 bottle.

--- Formula/x86_64-elf-binutils.rb
+++ Formula/x86_64-elf-binutils.rb
@@ -5,6 +5,12 @@ class X8664ElfBinutils < Formula
   mirror "https://ftpmirror.gnu.org/binutils/binutils-2.32.tar.gz"
   sha256 "9b0d97b3d30df184d302bced12f976aa1e5fbf4b0be696cdebc6cca30411a46e"
 
+  bottle do
+    sha256 "f7cda0b2705f796e2fccc8612c2a18a1dd85c0bd9f51985f9f77c24f4a12be24" => :catalina
+    sha256 "40467bb41a0d7651e11c73529c002e16f2c444b95b02d63e7969ef1474e42af1" => :mojave
+    sha256 "01843c43d684ef2e61d1f954ecd978e18187231b2e3edbf9caa6629f48c97b0b" => :high_sierra
+  end
+
   def install
     system "./configure", "--target=x86_64-elf",
                           "--enable-targets=all",
