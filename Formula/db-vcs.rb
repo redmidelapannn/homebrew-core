23399052be5026af02c10b4aacea28b7f4c0a985
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-23 02:50:22 +0000

    db-vcs: add 1.0 bottle.

--- Formula/db-vcs.rb
+++ Formula/db-vcs.rb
@@ -4,6 +4,13 @@ class DbVcs < Formula
   url "https://github.com/infostreams/db/archive/1.0.tar.gz"
   sha256 "a21f717ead07058242f28d90bd3d56f478f05039f0628e8f177c4383c36efefd"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "391c0ba55f4838b55a569b02e26e5bd02f6fbc148d9a3220e2f512721e78c5c9" => :catalina
+    sha256 "391c0ba55f4838b55a569b02e26e5bd02f6fbc148d9a3220e2f512721e78c5c9" => :mojave
+    sha256 "391c0ba55f4838b55a569b02e26e5bd02f6fbc148d9a3220e2f512721e78c5c9" => :high_sierra
+  end
+
   def install
     libexec.install "db"
     libexec.install "bin/"
