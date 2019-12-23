8050ab1999ac40308bd0e8dd5f5ccacd1360aee2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-23 09:23:51 +0000

    ices0: add 0.4.11 bottle.

--- Formula/ices0.rb
+++ Formula/ices0.rb
@@ -5,6 +5,12 @@ class Ices0 < Formula
   sha256 "9179496149e763a75fea37bf6ec12947cad4bdb868b401d34728b353b836047a"
   head "https://github.com/Moonbase59/ices0.git"
 
+  bottle do
+    sha256 "f019b543d137eee9a81cb9480019c528edd7753e66037ab025b0a760b5d498d8" => :catalina
+    sha256 "a463e7beb00b40e394c1f1562db07bb8db5ed2d2bfb7f634fdaeacb043310bc2" => :mojave
+    sha256 "897ab110fafc48363db181685725338e21203380b75c35d95a5de8c9b60a2ce6" => :high_sierra
+  end
+
   # building
   depends_on "autoconf" => :build
   depends_on "automake" => :build
