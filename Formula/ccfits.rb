e9f744e2fddde41e0363f431e9cbb98e1cd84f77
Chen Xu <chenxu@icrhome04.icrr.u-tokyo.ac.jp>
2019-12-23 22:12:08 +0900

    mv ccfits to ccfits.rb

--- Formula/ccfits.rb
+++ Formula/ccfits.rb
@@ -1,5 +1,5 @@
 class Ccfits < Formula
-  desc "c++ fits api"
+  desc "object oriented interface to the cfitsio library"
   homepage "https://heasarc.gsfc.nasa.gov/fitsio/CCfits/"
   url "https://heasarc.gsfc.nasa.gov/fitsio/CCfits/CCfits-2.5.tar.gz"
   sha256 "938ecd25239e65f519b8d2b50702416edc723de5f0a5387cceea8c4004a44740"
