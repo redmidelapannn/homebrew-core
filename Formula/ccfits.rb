a049d73174b5733fe77db8f9af724ec018f44bde
chenxu <6235641+cxwx@users.noreply.github.com>
2019-12-24 13:54:06 +0800

    something
    * C: 2: col 9: Description should start with a capital letter
    * C: 17: col 1: Trailing whitespace detected.

--- Formula/ccfits.rb
+++ Formula/ccfits.rb
@@ -1,5 +1,5 @@
 class Ccfits < Formula
-  desc "object oriented interface to the cfitsio library"
+  desc "Object oriented interface to the cfitsio library"
   homepage "https://heasarc.gsfc.nasa.gov/fitsio/CCfits/"
   url "https://heasarc.gsfc.nasa.gov/fitsio/CCfits/CCfits-2.5.tar.gz"
   sha256 "938ecd25239e65f519b8d2b50702416edc723de5f0a5387cceea8c4004a44740"
@@ -14,7 +14,7 @@ class Ccfits < Formula
     system "make"
     system "make", "install"
   end
-  
+
   test do
     (testpath/"test.cpp").write <<~EOS
       #include <CCfits/CCfits>
