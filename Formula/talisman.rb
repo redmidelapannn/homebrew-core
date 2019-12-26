7ffacbfb0411f8250f7078a0b56b32e1ed04a613
FX Coudert <fxcoudert@gmail.com>
2019-12-26 17:26:44 +0100

    Update talisman.rb

--- Formula/talisman.rb
+++ Formula/talisman.rb
@@ -16,22 +16,6 @@ class Talisman < Formula
 
   test do
     system "git", "init", "."
-    expected = <<~TOUT
-
-
-      d8888b. db    db d8b   db d8b   db d888888b d8b   db  d888b    .d8888.  .o88b.  .d8b.  d8b   db
-      88  `8D 88    88 888o  88 888o  88   `88'   888o  88 88' Y8b   88'  YP d8P  Y8 d8' `8b 888o  88
-      88oobY' 88    88 88V8o 88 88V8o 88    88    88V8o 88 88        `8bo.   8P      88ooo88 88V8o 88
-      88`8b   88    88 88 V8o88 88 V8o88    88    88 V8o88 88  ooo     `Y8b. 8b      88~~~88 88 V8o88
-      88 `88. 88b  d88 88  V888 88  V888   .88.   88  V888 88. ~8~   db   8D Y8b  d8 88   88 88  V888 db db
-      88   YD ~Y8888P' VP   V8P VP   V8P Y888888P VP   V8P  Y888P    `8888Y'  `Y88P' YP   YP VP   V8P VP VP
-
-
-
-      Please check 'talisman_reports/data' folder for the talisman scan report
-
-    TOUT
-
-    assert_equal expected, shell_output(bin/"talisman --scan")
+    assert_match "talisman scan report", shell_output(bin/"talisman --scan")
   end
 end
