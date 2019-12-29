6c24a6cbf8beeebc76bad26eb728fd5359013694
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-29 14:52:56 +0000

    libcdr: update 0.1.5_1 bottle.

--- Formula/libcdr.rb
+++ Formula/libcdr.rb
@@ -7,10 +7,9 @@ class Libcdr < Formula
 
   bottle do
     cellar :any
-    sha256 "1c27e4e351f32291d89656aa08d9a88202e8fe85858ca55264f1a2ea3fb90694" => :catalina
-    sha256 "d5362604839f1948a8b594c53e2db0c1bd419ab63c59e158813d56e916d7201f" => :mojave
-    sha256 "99fe39b40cf1977d09536fd15fa7557042c200e5848424b98b1b67fa8ed54198" => :high_sierra
-    sha256 "78dec5bba67665dc518c0eb579ed2c0d00065ee5c1ac6da234f05e0c9d734c83" => :sierra
+    sha256 "ced057ed5bfd03a97abcbb9da4410cae6932c1936a3a1897566a5f73f3f07270" => :catalina
+    sha256 "077d8f4ad42f4057fe0ae6d09865162bf7b06e425da79aefd2e6d30967b924cb" => :mojave
+    sha256 "f22448bf25b9f1cfbd3c8bf28f9b2681d68b220f4ef8cd2dffb2aceb7b6bf610" => :high_sierra
   end
 
   depends_on "cppunit" => :build
