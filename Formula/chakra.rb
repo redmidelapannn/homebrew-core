a508d20d542a1c02dd2972495d3c6f88a892f57f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-29 14:52:58 +0000

    chakra: update 1.11.13_1 bottle.

--- Formula/chakra.rb
+++ Formula/chakra.rb
@@ -7,9 +7,7 @@ class Chakra < Formula
 
   bottle do
     cellar :any
-    sha256 "a31f2ec0d7394a2faf7634ce6173df13a081f0927c126564c13e56cde8c6759f" => :mojave
-    sha256 "ef89ad378b0561ffe8ed919f94ce0d887fa0ef8afe8c76730430a2a6f7800dc6" => :high_sierra
-    sha256 "2473383f68da532b43dd94ca9d37471cdd1f2e69cf20dd58314ad3729f4f8844" => :sierra
+    sha256 "bf84f291da9daacf717f41221e3e5a08678dcd24e2bc6bb668919b324e710c77" => :high_sierra
   end
 
   depends_on "cmake" => :build
