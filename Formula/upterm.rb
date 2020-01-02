6abe54acfae769c5494f7113c4cf8213f57779c2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-02 22:55:57 +0000

    upterm: add 0.0.5 bottle.

--- Formula/upterm.rb
+++ Formula/upterm.rb
@@ -5,6 +5,13 @@ class Upterm < Formula
   sha256 "76b45f8124fdf6f04e118543f39f6532482be0f5087a3c8d36bfd688902560ee"
   head "https://github.com/jingweno/upterm.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "0639f6ef8b2283368343ad222968abc61f9ec892465e620bbf26dda89e148fe7" => :catalina
+    sha256 "21a396f1785084778bb2b802dbecf2cad38a6dd54eeb1e1a2cf8893221e31c1f" => :mojave
+    sha256 "50bb864a770044c3a50a88464f2b7ed9993003d4fa13d71f398e056485386f31" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
