6febc067db78b6b98e5d42097992e304ded728f2
jd-a <16205186+jd-a@users.noreply.github.com>
2020-01-03 23:28:01 +0100

    Update jdupes.rb

--- Formula/jdupes.rb
+++ Formula/jdupes.rb
@@ -10,8 +10,8 @@ class Jdupes < Formula
     sha256 "5914122e74633a5d22c16102e0ffbf43c6a5d5903ecc75d41b050339af913b96" => :mojave
     sha256 "f2132c6e2a0a1402a0dcc0205efc2ae80fcc1e70508f1910db3113de0d64ce1d" => :high_sierra
     sha256 "95ce9083f5ee6dabdaae7ac9165b51231aa8df5240452f3eaec9c172064082cc" => :sierra
- end
-  
+  end
+
   def install
     system "make", "install", "PREFIX=#{prefix}"
   end
