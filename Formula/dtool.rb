d4d946e120b313904df44ef0ca2377dd56716c3c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-06 17:35:51 +0000

    dtool: add 0.4.0 bottle.

--- Formula/dtool.rb
+++ Formula/dtool.rb
@@ -5,6 +5,11 @@ class Dtool < Formula
   version "0.4.0"
   sha256 "98ee2948fa959cf53700873a1e8fc5473a72fb3d9dec41725f42bd3a8803e0e2"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "aebee0845b60a2cb7e6215aa34410fb6ae221df5714091795319cf546747565d" => :high_sierra
+  end
+
   def install
     bin.install "dtool"
   end
