42de5b716de94ee88a27222788631cb1e5fb8eae
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-07 14:29:49 +0000

    avian: update 1.2.0 bottle.

--- Formula/avian.rb
+++ Formula/avian.rb
@@ -7,13 +7,10 @@ class Avian < Formula
 
   bottle do
     cellar :any
-    rebuild 1
-    sha256 "07a5c761ffc3bc57db4a5b0dbd952a47fcb2b62f1083eee3106be50031adfa5e" => :mojave
-    sha256 "ee881641717eed4a61ab64f832ab420401ed7a814fd32e546ae0765b5b27de6b" => :high_sierra
-    sha256 "ef092ec60093190857b558fd9a663ca0c6d0356bb9af7798be21cf466678f27e" => :sierra
-    sha256 "d2719509725f4c1fad3a53c32de18aff5d45685fb35ae352f1d51fc61e566f4a" => :el_capitan
-    sha256 "d002876c03742fc7ec4157fff598e7c11ed1e62f97ce1b217f8b089db87e43ed" => :yosemite
-    sha256 "20dd7125d138e05021b473d026190d8f4652e807afcfe057614e5c2e66ce0ed1" => :mavericks
+    rebuild 2
+    sha256 "c3d1bf1ccfbcfa2f8d79183ebc24d159ed2fedf6238edb5f32ed11eff90a1126" => :catalina
+    sha256 "87176512df4d932e4d2c3bee65c9016a4dcd29e2a75df2ce698b3c8a85fc65fe" => :mojave
+    sha256 "9ffd6d120920eb5f0c03b06e0cf511c81f6b270b5e38c29a2c12b0f029de9e10" => :high_sierra
   end
 
   depends_on :java => "1.8"
