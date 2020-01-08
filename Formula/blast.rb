b006c2f562d1d914d92395b6fdf4dfbc05f7ef98
Christiam Camacho <christiam@hotmail.com>
2020-01-08 12:52:43 -0500

    blast restore bottle

--- Formula/blast.rb
+++ Formula/blast.rb
@@ -5,6 +5,13 @@ class Blast < Formula
   version "2.10.0"
   sha256 "3acdd9cec01c4f43e56aeaf89049cb8f8013d60b9c1705eced10166967f1d926"
 
+  bottle do
+    sha256 "9fef86c970bdc8556a479920bffd3d33a57c0ce7bdcad4c44f2469e116f940aa" => :catalina
+    sha256 "e07f0dafa79bd72359cf467ba8cf8e51d76665c1571128dc7cc5d7857c5b92d8" => :mojave
+    sha256 "3de6646d96d9fdbf6b76fdf57a14612c8eebbfa1327f0d42e0402b516b6298ec" => :high_sierra
+    sha256 "57f2e2f9c65aa5364eb72a9bbdf6948a30af2975a53445a4b43203f56395da7c" => :sierra
+  end 
+
   depends_on "lmdb"
 
   conflicts_with "proj", :because => "both install a `libproj.a` library"
