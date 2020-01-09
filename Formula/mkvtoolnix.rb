7ade419b0fc3f44e145c09fc69efd7f25c6124a4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-09 07:21:47 +0000

    mkvtoolnix: update 42.0.0 bottle.

--- Formula/mkvtoolnix.rb
+++ Formula/mkvtoolnix.rb
@@ -6,9 +6,8 @@ class Mkvtoolnix < Formula
 
   bottle do
     cellar :any
-    sha256 "09e5dff1545f5d9180a460ffc008ce2c81b54bb09a35c01f251fe7a210d976f5" => :catalina
-    sha256 "264d111e6877d5afe49f410a9473d1fb792145bbc6892f8c429f594a43eb2c7c" => :mojave
-    sha256 "b6d36bf12a372847b73e7e4dd1f1ca80924f5cbd4629a3143ed3de9f264e1a70" => :high_sierra
+    sha256 "bd4e9b29b0899b33a5423699a46fb67a91156c632c8b5c75ebe81c1c60a47595" => :catalina
+    sha256 "4c33135585341927b70255f1566264c87854625b1e3d8d5c87a1f18f36e9d1ec" => :mojave
   end
 
   head do
