660c2d04005f4a477a6a7a0c0491b38947e9bff5
FX Coudert <fxcoudert@gmail.com>
2020-01-12 12:08:03 +0100

    pygobject3: revision for libffi

--- Formula/pygobject3.rb
+++ Formula/pygobject3.rb
@@ -3,6 +3,7 @@ class Pygobject3 < Formula
   homepage "https://wiki.gnome.org/Projects/PyGObject"
   url "https://download.gnome.org/sources/pygobject/3.34/pygobject-3.34.0.tar.xz"
   sha256 "87e2c9aa785f352ef111dcc5f63df9b85cf6e05e52ff04f803ffbebdacf5271a"
+  revision 1
 
   bottle do
     cellar :any
