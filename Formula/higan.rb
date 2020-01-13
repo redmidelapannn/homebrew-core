a94e797015284c8d924e8a5b1297de121b585341
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-13 03:13:14 +0000

    higan: add 107 bottle.

--- Formula/higan.rb
+++ Formula/higan.rb
@@ -5,6 +5,13 @@ class Higan < Formula
   sha256 "ac7aaabd88f3cde2915a435d11ba70f5570e1a2c17bbb572cf56fb9644dbc30f"
   head "https://github.com/byuu/higan.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5a3d16fabac3ab477f9bf9f48e916a4df40503032b1e2f39260073bf1a7b5ae5" => :catalina
+    sha256 "818d414ab3b9d89a62363fd63fde9f6bbcc84bd7a026c34a26923fb18b6641dd" => :mojave
+    sha256 "b481ca50d65cc48dc692806af769bbe39a13289f9377153bf6e7b7250707bc9b" => :high_sierra
+  end
+
   def install
     system "make", "-C", "higan", "local=false"
     system "make", "-C", "icarus"
