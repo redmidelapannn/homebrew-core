f049a22efaa39077af9fa21aec9da644d1c45ed0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-14 12:53:10 +0000

    wilson: add 0.3.1 bottle.

--- Formula/wilson.rb
+++ Formula/wilson.rb
@@ -5,6 +5,13 @@ class Wilson < Formula
   sha256 "5e428579afbc4005b3730d34f3166f8b27c738bade2e95c49ec465120c0c307a"
   version "0.3.1"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "563fa1028c3f4158bf78110438750608fc7718f49a0d7f7be330bf7f82b4dd86" => :catalina
+    sha256 "563fa1028c3f4158bf78110438750608fc7718f49a0d7f7be330bf7f82b4dd86" => :mojave
+    sha256 "563fa1028c3f4158bf78110438750608fc7718f49a0d7f7be330bf7f82b4dd86" => :high_sierra
+  end
+
   def install
     bin.install "wilson_darwin_amd64" => "wilson"
   end
