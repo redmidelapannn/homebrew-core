632f541ea206edd01a31024377032354ec2b6c05
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-19 04:28:28 +0000

    t: add 1.2.0 bottle.

--- Formula/t.rb
+++ Formula/t.rb
@@ -5,6 +5,13 @@ class T < Formula
   sha256 "6620f6754d86cd245413645e8fecfb837ea5eb1209948b6e4031b25d1534b5ce"
   head "https://github.com/sjl/t.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4d945c52d940d58bc165ae5ee55856be6bd978b2fbb65c0c91cec45c32db522b" => :catalina
+    sha256 "4d945c52d940d58bc165ae5ee55856be6bd978b2fbb65c0c91cec45c32db522b" => :mojave
+    sha256 "4d945c52d940d58bc165ae5ee55856be6bd978b2fbb65c0c91cec45c32db522b" => :high_sierra
+  end
+
   depends_on "python"
 
   def install
