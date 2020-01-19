b96bfc9f4fe95f909ad0ee14da54f4d3b10cbeb5
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-19 16:05:37 +0000

    libmill: update 1.18 bottle.

--- Formula/libmill.rb
+++ Formula/libmill.rb
@@ -7,11 +7,9 @@ class Libmill < Formula
 
   bottle do
     cellar :any
-    sha256 "26dd0efaf85765fd70f49d78ef0150731eed569711b1f246601f4c6f6cccced5" => :mojave
-    sha256 "5e0fff326f0d0d5a7c09f20bb1612ba4034bec1cc3befd99977bfb5b587ca70b" => :high_sierra
-    sha256 "6eb891740171f0471d420da7e92df829dd64e108cdf115588a639f6be3906e0a" => :sierra
-    sha256 "37103d981777f9c30dda9add67a2cafc72b405ee7d10ac3a32b1f267b0b5cda5" => :el_capitan
-    sha256 "4f8a2b4ddbcac88459c0f4b463e497a82e8d6a02b43419e29e2be8230c15123d" => :yosemite
+    rebuild 1
+    sha256 "93cb370861d980e70a396545439463db6393a697adc04a97add69ecb7f1e3eee" => :mojave
+    sha256 "4e81b9641391658bdf313d406c3ec6b4be735057d7db82ae4a34abcca1b5f127" => :high_sierra
   end
 
   depends_on "autoconf" => :build
