0073b246bfa57c315aae6351ecdbfcbfa65dbfed
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-20 09:36:15 +0000

    libsigrokdecode: add 0.5.3 bottle.

--- Formula/libsigrokdecode.rb
+++ Formula/libsigrokdecode.rb
@@ -4,6 +4,12 @@ class Libsigrokdecode < Formula
   url "https://sigrok.org/download/source/libsigrokdecode/libsigrokdecode-0.5.3.tar.gz"
   sha256 "c50814aa6743cd8c4e88c84a0cdd8889d883c3be122289be90c63d7d67883fc0"
 
+  bottle do
+    sha256 "8ea5d5bc1e5a734af9519faa01377a82e019af2446f14273fd301a93d33d4416" => :catalina
+    sha256 "87cc8e5de6292a94bb1553c597dff0cf1fbf83c37aa78eaca76f2c15aee1af1d" => :mojave
+    sha256 "bcc2526c1b16a9c80b66696dfbb60005837e674c14cf802ff199ed13997041b6" => :high_sierra
+  end
+
   # glib-2.0 >= 2.32.0
   depends_on "gettext" => [:build, :test]
   depends_on "make" => :build
