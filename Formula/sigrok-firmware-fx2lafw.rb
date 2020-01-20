952ba1234344f262b9c0c81f97bfd6820cf759c8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-20 09:36:15 +0000

    sigrok-firmware-fx2lafw: add 0.1.7 bottle.

--- Formula/sigrok-firmware-fx2lafw.rb
+++ Formula/sigrok-firmware-fx2lafw.rb
@@ -4,6 +4,13 @@ class SigrokFirmwareFx2lafw < Formula
   url "https://sigrok.org/download/source/sigrok-firmware-fx2lafw/sigrok-firmware-fx2lafw-0.1.7.tar.gz"
   sha256 "a3f440d6a852a46e2c5d199fc1c8e4dacd006bc04e0d5576298ee55d056ace3b"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b0de4c591b415c12d70832d8011535306818106d77dcfd1ac926116244cd2541" => :catalina
+    sha256 "b0de4c591b415c12d70832d8011535306818106d77dcfd1ac926116244cd2541" => :mojave
+    sha256 "b0de4c591b415c12d70832d8011535306818106d77dcfd1ac926116244cd2541" => :high_sierra
+  end
+
   depends_on "make" => :build
   depends_on "sdcc" => :build
 
