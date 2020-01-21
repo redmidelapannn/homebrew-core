51bbb6f494bb75ce83bc67810ec23ce4b4dab21b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-21 07:09:54 +0000

    dockly: add 3.14.4 bottle.

--- Formula/dockly.rb
+++ Formula/dockly.rb
@@ -6,6 +6,13 @@ class Dockly < Formula
   url "https://registry.npmjs.org/dockly/-/dockly-3.14.4.tgz"
   sha256 "6c24f868e063693c3274ce6f3219bbc852bde7b9532235ac1463fe3698b75c16"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4229ff8abc9e45330f82f919e4120740032a1a415a66d97e4fbca00aa207a6c9" => :catalina
+    sha256 "eedb4738d830cb3cb7b6f6dfeb553fe49c0710561a6d9e820d15695152871a6d" => :mojave
+    sha256 "ee85d6a2dd32810293a741bd625dae22fc4b49f13b30df265a50728219833935" => :high_sierra
+  end
+
   depends_on "node"
 
   def install
