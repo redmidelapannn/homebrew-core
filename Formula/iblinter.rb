1f57e85917fd9a175791757adce5d194acdc983c
Yuta Saito <kateinoigakukun@gmail.com>
2020-01-25 09:55:39 +0900

    Use tarboll instead of git revision for iblinter.rb

--- Formula/iblinter.rb
+++ Formula/iblinter.rb
@@ -1,9 +1,7 @@
 class Iblinter < Formula
   desc "Linter tool for Interface Builder"
   homepage "https://github.com/IBDecodable/IBLinter"
-  url "https://github.com/IBDecodable/IBLinter.git",
-      :tag      => "0.4.20",
-      :revision => "4b518dafbd64a329374f285c5f27bc6c08be7a38"
+  url "https://github.com/IBDecodable/IBLinter/archive/0.4.21.tar.gz"
   head "https://github.com/IBDecodable/IBLinter.git"
   depends_on :xcode => ["10.2", :build]
 
