a17a508e775e589cd3da443b746270430c7fcdaa
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-26 12:17:20 +0000

    rnp: add 0.13.1 bottle.

--- Formula/rnp.rb
+++ Formula/rnp.rb
@@ -4,6 +4,13 @@ class Rnp < Formula
   url "https://github.com/rnpgp/rnp.git", :tag => "v0.13.1"
   head "https://github.com/rnpgp/rnp.git"
 
+  bottle do
+    cellar :any
+    sha256 "8bb68513c2e5668617d87dafd11ab6dc0282ab9310b08cb94cab06442500dc62" => :catalina
+    sha256 "c76fd6326ba712abc9be79144842c27a54087503249281b2150ef8278edc56a1" => :mojave
+    sha256 "809727d23a59d725928f07388b2d9ec491c49a5aaf8654b449219773fb38ea8a" => :high_sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "botan"
   depends_on "json-c"
