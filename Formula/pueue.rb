0c53fd0a6735ed35a95604fccf823f5f66617faf
Daniele Riccucci <daniele.riccucci@gmail.com>
2020-02-01 15:27:47 +0100

    pueue 0.1.4 (new formula)

---
+++ Formula/pueue.rb
@@ -0,0 +1,66 @@
+class Pueue < Formula
+  desc "Command-line tool for managing long-running shell commands"
+  homepage "https://github.com/Nukesor/pueue"
+  url "https://github.com/Nukesor/pueue/archive/v0.1.4.tar.gz"
+  sha256 "8e156d1037a88448219d9c5bdbc844799e616604985c86a83541f0a23481f302"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+    rm bin/"completions_pueue"
+
+    prefix.install_metafiles
+
+    bash_completion.install "utils/completions/pueue.bash" => "pueue.bash"
+    fish_completion.install "utils/completions/pueue.fish" => "pueue.fish"
+  end
+
+  plist_options :manual => "pueued"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/pueued</string>
+            <string>--verbose</string>
+          </array>
+          <key>KeepAlive</key>
+          <false/>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>WorkingDirectory</key>
+          <string>#{var}</string>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/pueued.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/pueued.log</string>
+        </dict>
+      </plist>
+    EOS
+  end
+
+  test do
+    mkdir testpath/"Library/Preferences"
+
+    begin
+      pid = fork do
+        exec bin/"pueued"
+      end
+      sleep 5
+      cmd = "#{bin}/pueue status"
+      assert_match /Task list is empty.*/m, shell_output(cmd)
+    ensure
+      Process.kill("TERM", pid)
+    end
+
+    assert_match "Pueue daemon #{version}", shell_output("#{bin}/pueued --version")
+    assert_match "Pueue client #{version}", shell_output("#{bin}/pueue --version")
+  end
+end
