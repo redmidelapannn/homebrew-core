c81e16ff46cdd44b2d7f5b489b543b9fa363587c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-05 11:44:37 +0000

    webp2gif: add 0.1 bottle.

--- Formula/webp2gif.rb
+++ Formula/webp2gif.rb
@@ -4,6 +4,13 @@ class Webp2gif < Formula
   url "https://github.com/elsonwx/webp2gif/archive/v0.1.tar.gz"
   sha256 "3c79adbb5637b4b20d2779a725c8b04941a8943c0b95142da40343ac8f077297"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "974c98e13650bad0d6e559e6faa153e8ee3cb809ebd9b8caaa0ff9f42d7e1a64" => :catalina
+    sha256 "974c98e13650bad0d6e559e6faa153e8ee3cb809ebd9b8caaa0ff9f42d7e1a64" => :mojave
+    sha256 "974c98e13650bad0d6e559e6faa153e8ee3cb809ebd9b8caaa0ff9f42d7e1a64" => :high_sierra
+  end
+
   depends_on "imagemagick"
   depends_on "webp"
 
