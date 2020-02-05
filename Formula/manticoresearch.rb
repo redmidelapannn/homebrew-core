873b31b95fb37348f15d62bca3d6e3a193dfdd43
Adrian Nuta <adriannuta@gmail.com>
2020-02-05 10:45:00 +0200

    remove revision

--- Formula/manticoresearch.rb
+++ Formula/manticoresearch.rb
@@ -3,7 +3,6 @@ class Manticoresearch < Formula
   homepage "https://www.manticoresearch.com"
   url "https://github.com/manticoresoftware/manticoresearch/releases/download/3.3.0/manticore-3.3.0-200204-01fc8ad-release.tar.gz"
   sha256 "f62801f6eb50bd08cb8fe976f0a3a43c7600b979a1ced8d14b8261ca06eaf22c"
-  revision 1
   head "https://github.com/manticoresoftware/manticoresearch.git"
 
   bottle do
