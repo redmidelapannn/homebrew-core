be2a503b9ea3a16cd1ed87607f7f3c73501e9086
Iddan Aaronsohn <mail@aniddan.com>
2020-02-07 12:16:44 +0200

    Fix homepage link

--- Formula/cayley.rb
+++ Formula/cayley.rb
@@ -1,6 +1,6 @@
 class Cayley < Formula
   desc "An open-source graph database"
-  homepage "An open-source graph database"
+  homepage "https://cayley.io/"
   url "https://github.com/cayleygraph/cayley/releases/download/v0.7.7/cayley_0.7.7_darwin_amd64.tar.gz"
   sha256 "37ef9043cb20aeffa93d57f360fdee3e0c9d1b2fa8d404a00f3bff1b58937206"
 
