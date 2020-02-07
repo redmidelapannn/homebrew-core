d40d8bb33e2b0c13c1ab3e0dae099a4e659459e3
Alois Schloegl <alois.schloegl@ist.ac.at>
2020-02-07 14:20:02 +0100

    use gawk for testing - awk behaves strangely, and we use gawk anyway at built time

--- Formula/biosig.rb
+++ Formula/biosig.rb
@@ -4,11 +4,11 @@ class Biosig < Formula
   url "https://downloads.sourceforge.net/project/biosig/BioSig%20for%20C_C%2B%2B/src/biosig4c%2B%2B-1.9.5.src.tar.gz"
   sha256 "20e72a5a07d1bf8baa649efe437b4d3ed99944f0e4dfc1fbe23bfbe4d9749ed5"
 
-  depends_on "gawk" => :build
   depends_on "gnu-sed" => :build
   depends_on "gnu-tar" => :build
   depends_on "pkg-config" => :build
   depends_on "dcmtk"
+  depends_on "gawk"
   depends_on "libb64"
   depends_on "suite-sparse"
   depends_on "tinyxml"
@@ -30,8 +30,8 @@ class Biosig < Formula
     assert_match "biosig_fhir provides fhir binary template for biosignal data", shell_output("#{bin}/biosig_fhir 2>&1").strip
     file = Dir::Tmpname.create(["test", ".gdf"]) {}
     system "wget", "https://pub.ist.ac.at/~schloegl/download/TEST_44x86_e1.GDF", "-O", file
-    assert_match "1", shell_output("#{bin}/save2gdf -json "+file+" | awk -F\"[:, ]\" '/NumberOfChannels/ {N=$(NF-1)} /ChannelNumber/ {n++; } END {print n==N;}'").strip
-    assert_match "1", shell_output("#{bin}/biosig_fhir "+file+" | awk -F\"[:, ]\" '/NumberOfChannels/ {N=$(NF-1)} /ChannelNumber/ {n++; } END {print n==N;}'").strip
+    assert_match "1", shell_output("#{bin}/save2gdf -json "+file+" | gawk -F\"[:, ]\" '/NumberOfChannels/ {N=$(NF-1)} /ChannelNumber/ {n++; } END {print (n==N);}'").strip
+    assert_match "1", shell_output("#{bin}/biosig_fhir "+file+" | gawk -F\"[:, ]\" '/NumberOfChannels/ {N=$(NF-1)} /ChannelNumber/ {n++; } END {print (n==N);}'").strip
     rm(file)
   end
 end
