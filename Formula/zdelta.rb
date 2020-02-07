42362ee526c53a7c21143015766b4a51418a9320
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-07 20:30:23 +0000

    zdelta: update 2.1 bottle.

--- Formula/zdelta.rb
+++ Formula/zdelta.rb
@@ -7,13 +7,10 @@ class Zdelta < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c7d235578c62851ad7639ced736eb94ce833e9afb172fe65a37869bac1a80da4" => :catalina
-    sha256 "d98c1b4f7a6b63238bacd06d0a5640e7f0d07838bb4b3e15ac7dc9d699a5835a" => :mojave
-    sha256 "12c977074ebb388cb671b3a996cacda4c5c47fbf96100f73ffddcbf83976824d" => :high_sierra
-    sha256 "49b891fbf6b6f93796cb3080dcbefc1d873000e84d74a4bf2dd851b625e6a698" => :sierra
-    sha256 "3a186612374b9b2aca2e56f5dd68049c0b1ef952e4cb0b07263faf2ea62f136a" => :el_capitan
-    sha256 "2ade2838217be1b9f6bc55be6bf05fa5046ae09a42d17b714b9f2a73f934c993" => :yosemite
-    sha256 "86f93c2e260d321d3bf30b34c2313d2cec5bc6d23bfb5a86cf99ab6b5f64f157" => :mavericks
+    rebuild 1
+    sha256 "23c5cd4a759f1b2bd46f1ece1b18b9c6024e35efd5b2e1bf31518212726b25fe" => :catalina
+    sha256 "83d4896d4d54a1213f06c4acbf27d011903d046d9c0a9ccdeddd893b7a03ad81" => :mojave
+    sha256 "d5a4379f2bb33a00c1b5f1d906aeca30a2f57bb330760380069a6413ab0086b4" => :high_sierra
   end
 
   def install
