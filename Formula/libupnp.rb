c965092bf730b3076ef46f52abc5c4159a22cc7e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-07 02:27:57 +0000

    libupnp: update 1.10.1 bottle.

--- Formula/libupnp.rb
+++ Formula/libupnp.rb
@@ -6,11 +6,9 @@ class Libupnp < Formula
 
   bottle do
     cellar :any
-    sha256 "f1aef281c1909cd4c596cbed4275d12776aa3b6a4003aaa1e0038648218052b1" => :catalina
-    sha256 "28cc0a51186e7a4a52709620ec37e923002a8537e1e2db5c2096fed4987731a7" => :mojave
-    sha256 "cbca37b45cb652c73d4c5ae0ae087338bb4c606f5be4306c5d998c39c382bb4b" => :high_sierra
-    sha256 "b870845572dd6d11ed90fedfb367bbd53066f6b9c90e522b97ce88ae53ccddfe" => :sierra
-    sha256 "9b660881232e6ce94a375962c1df55f179f69335c7d11907b9a9c5dd81693360" => :el_capitan
+    sha256 "4975e881b4ef713414e8b227439f8ab37eda6b4db1da4abc6f0eb249d1cb865c" => :catalina
+    sha256 "3330274143c9d9a0104a4897ede4fdfb5dbaa177f0a10205051d00829cf2b79b" => :mojave
+    sha256 "ee4f2c782ee77e1c1d13e405e2110938c7393a52b1cb40211fa7bd46d745108c" => :high_sierra
   end
 
   def install
