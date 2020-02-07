a623fa344a1c1f8785d7d64f0abbdaa150313efb
Iddan Aaronsohn <mail@aniddan.com>
2020-02-07 19:24:43 +0200

    Fixed using packr2

--- Formula/cayley.rb
+++ Formula/cayley.rb
@@ -38,10 +38,10 @@ class Cayley < Formula
       ldflags = "-s -w -X #{version_flag}=#{version} -X #{git_revision_flag}=#{commit}"
 
       # Install packr2
-      system "go", "get", "-u", "github.com/gobuffalo/packr/v2/packr2"
+      system "go", "get", "github.com/gobuffalo/packr/v2/packr2"
 
       # Run packr to generate .go files that pack the static files into bytes that can be bundled into the Go binary.
-      system "packr2"
+      system "#{buildpath}/bin/packr2"
 
       # Build the binary
       system "go", "build", "-o", bin/"cayley", "-ldflags", ldflags, ".../cmd/cayley"
