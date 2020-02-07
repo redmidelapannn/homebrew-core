06d2d98b735b7a96d9b6c747e098bec2ed28d31d
Iddan Aaronsohn <mail@aniddan.com>
2020-02-07 12:26:20 +0200

    Revert test and add explicit version

--- Formula/cayley.rb
+++ Formula/cayley.rb
@@ -2,6 +2,7 @@ class Cayley < Formula
   desc "An open-source graph database"
   homepage "https://cayley.io/"
   url "https://github.com/cayleygraph/cayley/releases/download/v0.7.7/cayley_0.7.7_darwin_amd64.tar.gz"
+  version "0.7.7"
   sha256 "37ef9043cb20aeffa93d57f360fdee3e0c9d1b2fa8d404a00f3bff1b58937206"
 
   bottle :unneeded
@@ -11,6 +12,6 @@ class Cayley < Formula
   end
 
   test do
-    system "#{bin}/cayley", "version"
+    assert_match version.to_s, shell_output("#{bin}/cayley version")
   end
 end
