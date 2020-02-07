635b845165f4ab9e828b0e0e1e6b9162b918a83f
Alois Schloegl <alois.schloegl@ist.ac.at>
2020-02-07 11:37:10 +0100

    fix audit errors

--- Formula/biosig.rb
+++ Formula/biosig.rb
@@ -28,10 +28,10 @@ class Biosig < Formula
     assert_match "usage: save2gdf [OPTIONS] SOURCE DEST", shell_output("#{bin}/save2gdf -h").strip
     assert_match "mV\t4274\t0x10b2\t0.001\tV", shell_output("#{bin}/physicalunits mV").strip
     assert_match "biosig_fhir provides fhir binary template for biosignal data", shell_output("#{bin}/biosig_fhir 2>&1").strip
-    file = Dir::Tmpname.create(['test','.gdf']) {}
+    file = Dir::Tmpname.create(["test", ".gdf"]) {}
     system "wget", "https://pub.ist.ac.at/~schloegl/download/TEST_44x86_e1.GDF", "-O", file
     assert_match "1", shell_output("#{bin}/save2gdf -json "+file+" |gawk -F\"[:, ]\" '/NumberOfChannels/ {N=$(NF-1)} /ChannelNumber/ {n++; } END {print n==N;}'").strip
     assert_match "1", shell_output("#{bin}/biosig_fhir "+file+" |gawk -F\"[:, ]\" '/NumberOfChannels/ {N=$(NF-1)} /ChannelNumber/ {n++; } END {print n==N;}'").strip
-    system "rm", file
+    rm(file)
   end
 end
