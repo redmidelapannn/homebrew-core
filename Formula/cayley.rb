109281ef18bc34506acbae6ccb3e4a322db3c5d4
Iddan Aaronsohn <mail@aniddan.com>
2020-02-07 13:34:59 +0200

    Remove download ui (it will be added back in 0.8.0 oops)

--- Formula/cayley.rb
+++ Formula/cayley.rb
@@ -39,9 +39,6 @@ class Cayley < Formula
 
       # Download dependencies
       system "go", "mod", "download"
-      
-      # Download UI
-      system "go", "run", "./cmd/download_ui/download_ui.go"
 
       # Run packr to generate .go files that pack the static files into bytes that can be bundled into the Go binary.
       system "packr2"
