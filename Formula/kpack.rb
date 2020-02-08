b9e1094608c70d1707e5d60c4c277b9897dd066a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-08 15:49:10 +0000

    kpack: add 1.0.12 bottle.

--- Formula/kpack.rb
+++ Formula/kpack.rb
@@ -4,6 +4,13 @@ class Kpack < Formula
   url "https://github.com/KnightOS/kpack/archive/1.0.12.tar.gz"
   sha256 "f63e53a390ef9300caaa9042ec030154d4825ebaa360ffb96720df11a591af4f"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a8276457c1147c185436b1f1465432a33a55f51ec76abbb180d6482b06b35529" => :catalina
+    sha256 "289d820f0dc85c59ba2cfa61ed611b0b7f3cb4116c249d4c2350f585015b4f99" => :mojave
+    sha256 "8b08541701846ff97f5dd2d13d890e724c32a346d0526e3ff4eac19488684721" => :high_sierra
+  end
+
   depends_on "asciidoc" => :build
   depends_on "cmake" => :build
 
