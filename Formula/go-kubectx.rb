5a49df3cf4093e2e925703bf740a72d3d1f7c730
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-10 09:05:46 +0000

    go-kubectx: add 0.1.0 bottle.

--- Formula/go-kubectx.rb
+++ Formula/go-kubectx.rb
@@ -5,6 +5,13 @@ class GoKubectx < Formula
   sha256 "9bcb7797a2581fd240a12141a129a8e62997ddc32ba75b4973563cafb995db2a"
   head "https://github.com/aca/go-kubectx.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "0e940715f47d0972545be6f678e8bd52d11af227dc814a4fe79aa2bde7d66438" => :catalina
+    sha256 "8fd7389b6bc07459c43c812c58f6b5a5171a2b5733c4d6606aabcb98196aa851" => :mojave
+    sha256 "26965f32dcd0776dbfa8046ff9663baaf44d1250be430b1143928445dd6f0d3e" => :high_sierra
+  end
+
   depends_on "go" => :build
   depends_on "fzf"
 
