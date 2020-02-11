5ef11669ee947dd76af530af40d8680a2ce8223f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-11 06:10:40 +0000

    lldpd: update 1.0.5 bottle.

--- Formula/lldpd.rb
+++ Formula/lldpd.rb
@@ -5,9 +5,10 @@ class Lldpd < Formula
   sha256 "2dd3b212f4dbabfcbb2794c0010b245f9f8e74b387984e757be6243a74c6cb99"
 
   bottle do
-    sha256 "f37abc384f5435813515146bfa157d53d07611bce4b1b177ddc3d3410416b0b5" => :catalina
-    sha256 "578f07789b6d8f8d0ab98d64707ec14a1879005a06bfe88f9d0447d474e7f2cf" => :mojave
-    sha256 "4f04dc494f01c2ce011ea48c1e85efb3c985e98fe5b31ce768019d3746acd5ea" => :high_sierra
+    rebuild 1
+    sha256 "a1a73c4a965cf886d8e7be6eaaa7446baf4db242008e687aa431cf007c234cb9" => :catalina
+    sha256 "76fb327dd39f6d77697b4e9df5880bb017bd4aa5ee587604f7c58703e49f7e6f" => :mojave
+    sha256 "9a2b9134c7f9445b9d707aa2f8bcba03f7726a266aad5c68d059923b5bbd0c32" => :high_sierra
   end
 
   depends_on "pkg-config" => :build
