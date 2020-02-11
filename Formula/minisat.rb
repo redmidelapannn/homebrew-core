e50a314502dc96d9009edbfce9d25b40f177ceac
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-11 10:06:12 +0000

    minisat: add 2.2.1 bottle.

--- Formula/minisat.rb
+++ Formula/minisat.rb
@@ -8,6 +8,13 @@ class Minisat < Formula
     url "https://github.com/stp/minisat/archive/releases/2.2.1.tar.gz"
     sha256 "432985833596653fcd698ab439588471cc0f2437617d0df2bb191a0252ba423d"
   end
+  bottle do
+    cellar :any
+    sha256 "625baa3aa506d56fd3f071c491434000e7a1a02c48b97b31d4b7784218a26df6" => :catalina
+    sha256 "d3f04e4cd97af8ff4a166434cbba56d7c28c90fe88180048a3bdfb185ec0f49b" => :mojave
+    sha256 "87ba4850c49a187f27b74e27c3ebb5d9a6381b59e6e18b9739045ad3151061a8" => :high_sierra
+  end
+
 
   depends_on "cmake" => :build
 
