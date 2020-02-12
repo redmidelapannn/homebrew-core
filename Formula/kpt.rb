6225d20c7eb6ef36e68e71a21c9d3969ab8cfe02
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-12 19:47:15 +0000

    kpt: add 0.4.0 bottle.

--- Formula/kpt.rb
+++ Formula/kpt.rb
@@ -4,6 +4,13 @@ class Kpt < Formula
   url "https://github.com/GoogleContainerTools/kpt/archive/v0.4.0.tar.gz"
   sha256 "63133d79cebfda47a281bee31bf10e1ec6f40556d6dac32546a54e91ee58ce9a"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "1286f1c1bbcb99f6593745eda18e1c88cc041d6503e954028ab78abea711a2fa" => :catalina
+    sha256 "f0396c0825a77f2c6afb09e49a1d69a70f819174165a162197e82f3a54029061" => :mojave
+    sha256 "185068c91aa3e6c9ae3de40bed5f458b414ed953698adfaea1a572404e3d9474" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
