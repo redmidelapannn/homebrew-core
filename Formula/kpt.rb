97422600c3f6b8db20484fafeb4131772cb30714
Tim Hartmann <tfhartmann@gmail.com>
2020-02-12 14:16:39 -0500

    tests: added a test
    Simple test to compare version

--- Formula/kpt.rb
+++ Formula/kpt.rb
@@ -1,8 +1,5 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Kpt < Formula
-  desc "kpt is a toolkit to help you manage, manipulate, customize, and apply Kubernetes Resource configuration data files."
+  desc "Toolkit to manage,and apply Kubernetes Resource config data files"
   homepage "https://googlecontainertools.github.io/kpt"
   url "https://github.com/GoogleContainerTools/kpt/archive/v0.4.0.tar.gz"
   sha256 "63133d79cebfda47a281bee31bf10e1ec6f40556d6dac32546a54e91ee58ce9a"
@@ -10,21 +7,11 @@ class Kpt < Formula
   depends_on "go" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     ENV["GO111MODULE"] = "on"
     system "go", "build", "-ldflags", "-X main.version=#{version}", *std_go_args
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test kpt`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    assert_match version.to_s, shell_output("#{bin}/kpt version")
   end
 end
