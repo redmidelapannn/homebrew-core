c956e7720e0f553d2917d1713647e9a9cc6a2b7d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-12 02:30:21 +0000

    awscli-ssm-plugin: add 1.1.54.0 bottle.

--- Formula/awscli-ssm-plugin.rb
+++ Formula/awscli-ssm-plugin.rb
@@ -7,6 +7,13 @@ class AwscliSsmPlugin < Formula
   version "1.1.54.0"
   sha256 "d9b558193370b2ecc0ddba001b6ee974b14b60d4d247851706e26a9811f15349"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "e84cfa85a52ea76cb14b2b3330d00a6deae6198155b3feffbdec7cf9fcc465a0" => :catalina
+    sha256 "0d4fbc1b7d22c0f018f15fc944fc04a3dbed094d1fe592b9409d06cf01effe26" => :mojave
+    sha256 "d6ad3aa98c84f5367ffc7b55a542b26905b5d428eec4da496992e7594a7d1c6d" => :high_sierra
+  end
+
   depends_on "awscli"
 
   def install
