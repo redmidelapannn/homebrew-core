40770da7d30b12382f0c64bb42d5ccfbba6a132d
yonaskolb <yonaskolb@gmail.com>
2020-02-13 22:41:00 +1100

    mint 0.14.0

--- Formula/mint.rb
+++ Formula/mint.rb
@@ -1,8 +1,8 @@
 class Mint < Formula
   desc "Dependency manager that installs and runs Swift command-line tool packages"
   homepage "https://github.com/yonaskolb/Mint"
-  url "https://github.com/yonaskolb/Mint/archive/0.13.0.tar.gz"
-  sha256 "5a922ad89c7b9b3e1854507c342f36d8c8ba7a488a09114550fe06320208e5f9"
+  url "https://github.com/yonaskolb/Mint/archive/0.14.0.tar.gz"
+  sha256 "ebb734858c4acbdf1fc9d27c6dc374b3adc415387dfe76b19d63a7b5dbcae4dc"
 
   bottle do
     cellar :any_skip_relocation
