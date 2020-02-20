9944f96288443a667cadbe26c15c809996c9df7a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-20 18:37:31 +0000

    unit: add 1.15.0 bottle.

--- Formula/unit.rb
+++ Formula/unit.rb
@@ -5,6 +5,12 @@ class Unit < Formula
   sha256 "f8b231d02865b5695825a2a77ecbfd0cff33c0954c0d351d2d773d7e5f6237dd"
   head "https://hg.nginx.org/unit", :using => :hg
 
+  bottle do
+    sha256 "c59e860a15e09d81137cece6e5e751fb1b94a5ef517a760c1c51a1d2d1fb42c9" => :catalina
+    sha256 "dc305ef31e54cd9df6631e8491a5a4154e2af4e06015eaf5d97857aec706d339" => :mojave
+    sha256 "486ec455d0cd0a3c96340c0713feea93968e15d9bf583b83eefb9277ab5e5059" => :high_sierra
+  end
+
   depends_on "openssl@1.1"
 
   def install
