2d34d21a011ade7243b06b13dea15b73f1f3ce57
Caleb Xu <calebcenter@live.com>
2020-02-21 16:59:47 -0500

    bear 2.4.3

--- Formula/bear.rb
+++ Formula/bear.rb
@@ -1,8 +1,8 @@
 class Bear < Formula
   desc "Generate compilation database for clang tooling"
   homepage "https://github.com/rizsotto/Bear"
-  url "https://github.com/rizsotto/Bear/archive/2.4.2.tar.gz"
-  sha256 "e80c0d622a8192a1ec0c0efa139e5767c6c4b1defe1c75fc99cf680c6d1816c0"
+  url "https://github.com/rizsotto/Bear/archive/2.4.3.tar.gz"
+  sha256 "74057678642080d193a9f65a804612e1d5b87da5a1f82ee487bbc44eb34993f2"
   head "https://github.com/rizsotto/Bear.git"
 
   bottle do
@@ -13,11 +13,11 @@ class Bear < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "python"
+  depends_on "python@3.8"
 
   def install
     args = std_cmake_args + %W[
-      -DPYTHON_EXECUTABLE=#{Formula["python"].opt_bin}/python3
+      -DPYTHON_EXECUTABLE=#{Formula["python@3.8"].opt_bin}/python3.8
     ]
     system "cmake", ".", *args
     system "make", "install"
