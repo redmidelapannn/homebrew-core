8fca6550286691a94450d657beb6990c54f4737a
chenxu <6235641+cxwx@users.noreply.github.com>
2020-02-23 21:47:05 +0800

    change several
    delete revision
    uses_from_macos ncurses
    use python@3.8
    modify caveats
    modify test

--- Formula/gammalib.rb
+++ Formula/gammalib.rb
@@ -3,14 +3,13 @@ class Gammalib < Formula
   homepage "http://cta.irap.omp.eu/gammalib/about.html"
   url "http://cta.irap.omp.eu/ctools/releases/gammalib/gammalib-1.6.3.tar.gz"
   sha256 "b93bc03562ba1fcb21ab6f48943f9c28230c04d1b0ef986569c88b05d8e9dbbe"
-  revision 1
 
   depends_on "cfitsio"
   depends_on "doxygen"
-  depends_on "ncurses"
-  depends_on "python"
+  depends_on "python@3.8"
   depends_on "readline"
   depends_on "swig"
+  uses_from_macos "ncurses"
 
   def install
     system "./configure", "PYTHON=/usr/local/bin/python3", "--prefix=#{prefix}"
@@ -19,23 +18,22 @@ class Gammalib < Formula
   end
 
   def caveats; <<~EOS
-    Because gammalib depends on several installation-dependent
-    environment variables to function properly, you should
-    add the following commands to your shell initialization
-    script or call them directly
-    before using gammalib.
-
-    For zsh users:
+    Because gammalib expects a set of environment variables you should source:
       export GAMMALIB=/usr/local/
       source $GAMMALIB/bin/gammalib-init.sh
+    before using gammalib.
   EOS
   end
 
   test do
     (testpath/"test.py").write <<~EOS
       import gammalib
-      print(gammalib.__version__)
+      dir=gammalib.GSkyDir()
+      dir.radec_deg(83.63,22.01)
+      l=dir.l_deg()
+      b=dir.b_deg()
+      print(l,b)
     EOS
-    assert_match /1.6.3/, shell_output("python test.py")
+    assert_match /(184.55973405309402, -5.7891829467816827)/, shell_output("python test.py")
   end
 end
