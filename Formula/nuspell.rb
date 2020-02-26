00abb38e13c071da72ae428b2415edb6c597341b
PanderMusubi <pander@users.sourceforge.net>
2020-02-26 14:45:46 +0100

    improved test

--- Formula/nuspell.rb
+++ Formula/nuspell.rb
@@ -6,9 +6,21 @@ class Nuspell < Formula
 
   depends_on "catch2" => :build
   depends_on "cmake" => :build
+  depends_on "p7zip" => :build
   depends_on "boost"
+  depends_on :macos => [:catalina, :high_sierra, :dunno]
   uses_from_macos "ruby" => :build
   uses_from_macos "icu4c"
+
+  resource "test_dictionary" do
+    url "http.us.debian.org/debian/pool/main/s/scowl/hunspell-en-us_2018.04.16-1_all.deb"
+    sha256 "d1964cff134a5774664737c9d585701a86c2191079019707f1293a4c6d8f93f3"
+  end
+
+  resource "test_wordlist" do
+    url "http.us.debian.org/debian/pool/main/s/scowl/wamerican-small_2018.04.16-1_all.deb"
+    sha256 "b06dc81ec85e3ef5fe00ccc95ad9e96b38f3b77800049ea366cfd6376eda3b37"
+  end
 
   def install
     mkdir "build" do
@@ -16,9 +28,6 @@ class Nuspell < Formula
       system "gem", "install", "ronn"
       ENV.prepend_path "PATH", buildpath/"gem_home/bin"
       system "cmake", "..", "-DBUILD_SHARED_LIBS=ON", *std_cmake_args
-      system "cmake", "--build", "."
-      # internal unit tests of the code and external tests of the application
-      system "cmake", "--build", ".", "--target", "test"
       system "cmake", "--build", ".", "--target", "install"
     end
   end
@@ -27,12 +36,18 @@ class Nuspell < Formula
     Dictionary files (*.aff and *.dic) should be placed in
     ~/Library/Spelling/ or /Library/Spelling/.  Homebrew itself
     provides no dictionaries for Nuspell, but you can download
-    compatible dictionaries from other sources, such as
+    compatible Hunspell dictionaries from other sources, such as
     https://wiki.documentfoundation.org/Language_support_of_LibreOffice .
   EOS
   end
 
   test do
-    system "#{bin}/nuspell", "-D"
+    testpath.install resource("test_dictionary")
+    system "7z", "x", "-aoa", "hunspell-en-us_2018.04.16-1_all.deb"
+    system "tar", "xf", "data.tar"
+    testpath.install resource("test_wordlist")
+    system "7z", "x", "-aoa", "wamerican-small_2018.04.16-1_all.deb"
+    system "tar", "xf", "data.tar"
+    assert(pipe_output("#{bin}/nuspell -d usr/share/hunspell/en_US usr/share/dict/american-english-small | grep '^*'").size > 45000)
   end
 end
