0ebf65236efedbaace52aa8aaaf5eea3f42bc3e4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-28 21:22:11 +0000

    qp: add 1.0.1 bottle.

--- Formula/qp.rb
+++ Formula/qp.rb
@@ -4,6 +4,13 @@ class Qp < Formula
   url "https://github.com/paybase/qp/releases/download/1.0.1/qp-1.0.1-darwin.tar.gz"
   sha256 "a4a5e6caa86b99fc54bf8db8d36def3b64ffbea5b03de094ec8db423425d8401"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c82518f5524fe1bfda2c5af79b309e204d88aebfe1f23ce88b9f53014209a4fe" => :catalina
+    sha256 "c82518f5524fe1bfda2c5af79b309e204d88aebfe1f23ce88b9f53014209a4fe" => :mojave
+    sha256 "c82518f5524fe1bfda2c5af79b309e204d88aebfe1f23ce88b9f53014209a4fe" => :high_sierra
+  end
+
   def install
     mkdir bin.to_s
     cp "./qp", bin.to_s
