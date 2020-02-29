517fbd7764930412ea397eede5a4bdcd1f7fefb0
Michka Popoff <michkapopoff@gmail.com>
2020-02-29 20:35:13 +0100

    pyenv: depend on Python@3.8

--- Formula/pyenv.rb
+++ Formula/pyenv.rb
@@ -3,6 +3,7 @@ class Pyenv < Formula
   homepage "https://github.com/pyenv/pyenv"
   url "https://github.com/pyenv/pyenv/archive/v1.2.17.tar.gz"
   sha256 "ff10d1c0e51b2f6e5325bbd7bd4314ddf1e8b7fd00f22652b578ae03c858f74b"
+  revision 1
   version_scheme 1
   head "https://github.com/pyenv/pyenv.git"
 
@@ -16,6 +17,7 @@ class Pyenv < Formula
   depends_on "autoconf"
   depends_on "openssl@1.1"
   depends_on "pkg-config"
+  depends_on "python@3.8"
   depends_on "readline"
 
   uses_from_macos "bzip2"
@@ -26,6 +28,7 @@ class Pyenv < Formula
 
   def install
     inreplace "libexec/pyenv", "/usr/local", HOMEBREW_PREFIX
+    inreplace "libexec/pyenv-versions", "system pyenv-which python", "system pyenv-which python3"
 
     system "src/configure"
     system "make", "-C", "src"
@@ -41,6 +44,11 @@ class Pyenv < Formula
   end
 
   test do
+    # python@3.8 is keg only. Pyenv needs Python 3 in the path
+    # to work, so provide our version. Users can later on choose
+    # which python they want to use by modifying .pyenv/version
+    ENV.prepend_path "PATH", Formula["python@3.8"].opt_libexec/"bin"
+
     shell_output("eval \"$(#{bin}/pyenv init -)\" && pyenv versions")
   end
 end
