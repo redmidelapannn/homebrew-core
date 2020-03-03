326ca7f251554fa1617232c58e76e938a5fc7779
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-03 09:26:29 +0000

    eye-d3: update 0.9.2 bottle.

--- Formula/eye-d3.rb
+++ Formula/eye-d3.rb
@@ -6,9 +6,10 @@ class EyeD3 < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "c8cdfbcf1b5ec18ad698d9df2b8e918a8ce9fc29251d63998fec1b78e1913b63" => :catalina
-    sha256 "c8cdfbcf1b5ec18ad698d9df2b8e918a8ce9fc29251d63998fec1b78e1913b63" => :mojave
-    sha256 "c8cdfbcf1b5ec18ad698d9df2b8e918a8ce9fc29251d63998fec1b78e1913b63" => :high_sierra
+    rebuild 1
+    sha256 "6fe1df226b3e05861081b402f177993e6482dd4cc167dd20542917cf9971f5ea" => :catalina
+    sha256 "6fe1df226b3e05861081b402f177993e6482dd4cc167dd20542917cf9971f5ea" => :mojave
+    sha256 "6fe1df226b3e05861081b402f177993e6482dd4cc167dd20542917cf9971f5ea" => :high_sierra
   end
 
   depends_on "libmagic"
