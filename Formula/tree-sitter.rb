fa2b87da77bffadaf4e3c31b2a251c253c3e9908
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-03 05:32:31 +0000

    tree-sitter: add 0.16.5 bottle.

--- Formula/tree-sitter.rb
+++ Formula/tree-sitter.rb
@@ -5,6 +5,13 @@ class TreeSitter < Formula
   sha256 "3c6357d1a153c43f481d07f1a8743ed8b3134a2934689d564795a0d083ff7c7e"
   head "https://github.com/tree-sitter/tree-sitter.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "faec5b06011be155b2b47d3d355da8796d73bc6ede89924cfb2e3e3ce718fb80" => :catalina
+    sha256 "14918f7b889033fdd48430c9696297a54b351b240cb35c9e20bf55ca8a33e3f6" => :mojave
+    sha256 "05ff5a78482c62015aa6ca004f9a274a24d4f20deb7d89704e9157b47a0cb25d" => :high_sierra
+  end
+
   def install
     # this shell script invokes clang or CC, and builds a .a with ar
     system "script/build-lib"
