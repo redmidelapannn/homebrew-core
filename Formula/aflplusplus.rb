3868336ee5ad0aca16d7f63835c53438d059c8c6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-03 11:17:14 +0000

    aflplusplus: add 2.62 bottle.

--- Formula/aflplusplus.rb
+++ Formula/aflplusplus.rb
@@ -4,6 +4,12 @@ class Aflplusplus < Formula
   url "https://github.com/vanhauser-thc/AFLplusplus/archive/2.62c.tar.gz"
   sha256 "cde181ac733aa3a1212ffcb494bb9306a2086c7521fb006719b0e15cd8015c63"
 
+  bottle do
+    sha256 "61201b88a4bc6eba2c6260cf7cda79243fd618311a14dda1e8e6425371271969" => :catalina
+    sha256 "d712b50534452f1660d0366785190d110a1dd2ef110a9d69e5102a8620f9f951" => :mojave
+    sha256 "40503a37e653faf5df88af49dfd539461d6e4171c0f54efb7b563700d9ec6668" => :high_sierra
+  end
+
   depends_on "automake" => :build
 
   conflicts_with "afl-fuzz"
