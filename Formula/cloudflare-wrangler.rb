8f05e8349270e4da58c2a3c61a1732c7530a916a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-04 01:46:51 +0000

    cloudflare-wrangler: add 1.8.0 bottle.

--- Formula/cloudflare-wrangler.rb
+++ Formula/cloudflare-wrangler.rb
@@ -5,6 +5,13 @@ class CloudflareWrangler < Formula
   sha256 "ebb31c2549733991823f965025da493bbc2a1644d02668549700793a29df0229"
   head "https://github.com/cloudflare/wrangler.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d726cac238cce6ff0411290dce9a3c05d81af57e3605b77821fd0936374419b4" => :catalina
+    sha256 "e8a61715057bea3d4d06d362d265d5b68f3e1d31ffac35b82c5edd1a9cf6c817" => :mojave
+    sha256 "d5a5510b623fbe8bee730d9bd043578bb331d4c7330081e90cf71e5f7830cdaf" => :high_sierra
+  end
+
   depends_on "rust" => :build
 
   def install
