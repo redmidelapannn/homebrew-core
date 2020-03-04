c400e71107ba4a416714d387929b2d15db9e1eff
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-04 15:58:39 +0000

    yazpp: update 1.6.5 bottle.

--- Formula/yazpp.rb
+++ Formula/yazpp.rb
@@ -6,12 +6,10 @@ class Yazpp < Formula
 
   bottle do
     cellar :any
-    sha256 "fc6c551c54b78b477836368f8f4c24f39bc8324ced4aaed418ed6ebde071c130" => :catalina
-    sha256 "ad3ae23deb4f16249fbfc8794a30116911a211c76adbc024948cf9b8842a55b4" => :mojave
-    sha256 "870f730cc4ee76700749f4091d111cb0e9a529d43c1ba7cb40b36807e49d9b76" => :high_sierra
-    sha256 "794e2e265413005b3c26a0fa38e1ab8957bd1ec13cf4abb63730070181d9beb4" => :sierra
-    sha256 "292447a86953bb10361130542d2db9e0c0fc410e9be3b13b8c80891fbfaeec20" => :el_capitan
-    sha256 "6f769c30797af9cb98bf02491706f96b7085eed2d5d05c377e51ca5e0bf8541a" => :yosemite
+    rebuild 1
+    sha256 "c328368c34f97a1d3dcb2b49c2077c02e0baf471bd144a821c976d37617ecb5e" => :catalina
+    sha256 "a30c17ce291aa0c0e03c55916ae1cbd65e34260a34bb3eb5a1ef7376610c17e5" => :mojave
+    sha256 "281ccfaa8ab855a549775f192af0eef52c5614260d5538cec50e947a476b1425" => :high_sierra
   end
 
   depends_on "yaz"
