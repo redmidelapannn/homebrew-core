457787e3f4b346adbf3f39a7ab4707939cc82534
Denis Isidoro <denisidoro@users.noreply.github.com>
2020-03-05 19:27:08 -0300

    Update navi.rb

--- Formula/navi.rb
+++ Formula/navi.rb
@@ -22,7 +22,6 @@ class Navi < Formula
 
   test do
     assert_match "navi " + version, shell_output("#{bin}/navi --version")
-
-assert_match "foo", shell_output("NAVI_PATH=#{prefix}/tests/cheats #{bin}/navi best simple")
+    assert_match "foo", shell_output("NAVI_PATH=#{prefix}/tests/cheats #{bin}/navi best simple")
   end
 end
