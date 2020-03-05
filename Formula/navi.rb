44b5f5a03634df14d5ee222a91924da1d8dfa173
Denis Isidoro <denisidoro@users.noreply.github.com>
2020-03-05 10:51:06 -0300

    Build from source

--- Formula/navi.rb
+++ Formula/navi.rb
@@ -1,24 +1,31 @@
 class Navi < Formula
-  version '2.0.0'
-  desc "Interactive cheatsheet tool for the command-line."
+  version "2.0.0"
+  desc "Interactive cheatsheet tool for the command-line"
   homepage "https://github.com/denisidoro/navi"
+  url "https://github.com/denisidoro/navi/archive/v#{version}.tar.gz"
+  sha256 "2e65f6c8757dd5107c4ac7032e3cc1bfec785cfec84e33b2d1b365a31c8ee17b"
 
-  if OS.mac?
-      url "https://github.com/denisidoro/navi/releases/download/v#{version}/navi-x86_64-osx.tar.gz"
-      sha256 "fbaa842936a1d82e48f4417d1571f8e0f58d50dfa76d20b50283fbd9d62efc97"
-  elsif OS.linux?
-      url "https://github.com/denisidoro/navi/releases/download/v#{version}/navi-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "0d1b7b83ed12177e1f506f8e4bd9ac7f8e113bc3f977416ac8703d912fb13593"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "64b6dac7d4c689ab6aa5c88ef58369249af2d9a6ac44f56769685569a673dd56" => :catalina
+    sha256 "64b6dac7d4c689ab6aa5c88ef58369249af2d9a6ac44f56769685569a673dd56" => :mojave
+    sha256 "64b6dac7d4c689ab6aa5c88ef58369249af2d9a6ac44f56769685569a673dd56" => :high_sierra
   end
 
+  depends_on "rust" => :build
   depends_on "fzf"
 
   def install
-    libexec.install Dir["*"]
-    bin.write_exec_script (libexec/"navi")
+    rm "navi"
+    system "cargo", "build", "--release", "--locked"
+    mv "target/release/navi" "navi"
+    libexec.install "navi"
+    libexec.install Dir["cheats/*"]
+    libexec.install Dir["shell/*"]
+    bin.write_exec_script(libexec/"navi")
   end
-  
+
   test do
-    assert_equal "navi " + version, shell_output("#{bin}/navi --version")
+    assert_match "navi " + version, shell_output("#{bin}/navi --version")
   end  
 end
