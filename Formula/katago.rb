d6efbc009be0a9858ef549763add68424cd2d985
Martin Häcker <mhaecker@mac.com>
2020-03-06 11:06:00 +0100

    Add new networks as resources.

--- Formula/katago.rb
+++ Formula/katago.rb
@@ -15,15 +15,21 @@ class Katago < Formula
   depends_on "boost"
   depends_on "libzip"
 
-  resource "15b-network" do
-    url "https://github.com/lightvector/KataGo/releases/download/v1.3.2/g170e-b15c192-s1672170752-d466197061.txt.gz", :using => :nounzip
-    sha256 "09456899f1b9155217f4ca059cf8a68f79b7eba22cf6b7c00ffb7f17ce067967"
-  end
-
   resource "20b-network" do
-    url "https://github.com/lightvector/KataGo/releases/download/v1.3.2/g170-b20c256x2-s1913382912-d435450331.txt.gz", :using => :nounzip
-    sha256 "c309c0361ca1109084dd026235c9fa0254ffe22eecf096b6c5988a9d902a75ca"
+    url "https://github.com/lightvector/KataGo/releases/download/v1.3.3/g170e-b20c256x2-s2430231552-d525879064.bin.gz", :using => :nounzip
+    sha256 "770f65c5cfa6e7ebba1b972768406668afdfc2e65d61e45b8cb468677f5fea4d"
   end
+  
+  resource "30b-network" do
+    url "https://github.com/lightvector/KataGo/releases/download/v1.3.3/g170-b30c320x2-s1287828224-d525929064.bin.gz", :using => :nounzip
+    sha256 "3c2864fda18d8bc595b1fb65ec25cc998ac90cd203a6269c56691e195297c325"
+  end
+  
+  resource "40b-network" do
+    url "https://github.com/lightvector/KataGo/releases/download/v1.3.3/g170-b40c256x2-s1349368064-d524332537.bin.gz", :using => :nounzip
+    sha256 "74ea1a4e9c0a461b9fb35a297d11f10e3fcfee32e5b710720784120fe52cbad0"
+  end
+  
 
   def install
     cd "cpp" do
