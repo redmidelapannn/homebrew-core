0d1fab471416ba08c1b465bd9692602f13ce2130
Medya Gh <medya@google.com>
2020-03-06 16:52:59 -0800

    minikube 1.8.0

--- Formula/minikube.rb
+++ Formula/minikube.rb
@@ -2,8 +2,8 @@ class Minikube < Formula
   desc "Run a Kubernetes cluster locally"
   homepage "https://minikube.sigs.k8s.io/"
   url "https://github.com/kubernetes/minikube.git",
-      :tag      => "v1.7.3",
-      :revision => "436667c819c324e35d7e839f8116b968a2d0a3ff"
+      :tag      => "v1.8.0",
+      :revision => "a48abe2ac6951dcc3dec45fd546ba4fe0bf42494"
   head "https://github.com/kubernetes/minikube.git"
 
   bottle do
