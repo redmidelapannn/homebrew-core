1c6c118b7d49bf25aba0451c002cef53b02d2262
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-08 04:10:35 +0000

    rakudo: update 2020.01 bottle.

--- Formula/rakudo.rb
+++ Formula/rakudo.rb
@@ -5,9 +5,10 @@ class Rakudo < Formula
   sha256 "2e02fcfca2f79ec6bd2dc0f0840ae108ec76f030f3080aa47f23f80f92b6c3b0"
 
   bottle do
-    sha256 "3bfa404b4001f30e06d1794ef03664443399f9d8e07c030cdf7f8aa5b1e61741" => :catalina
-    sha256 "45becd098c8d4d1e7f2f800c07d94a996dd813e9af96c49db1e833f98780429e" => :mojave
-    sha256 "c426e9201384e35d43371712e64ed07c4a04ec458c6ee8b2850f7f419bd49fb2" => :high_sierra
+    rebuild 1
+    sha256 "aae57165a6ae9f3f3f230c44ee01d56f4b4a21a99872dd78c78b243f6697c88b" => :catalina
+    sha256 "8b2ec2efb686d8cb61c98b9407f9544f4510e434c57c73be8bba9cc1549892db" => :mojave
+    sha256 "b973a05e3aca014bfdb64e6f3731d335c000ac1600bceb463d1d69a3f7f90a8a" => :high_sierra
   end
 
   depends_on "nqp"
