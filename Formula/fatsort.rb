63e0058c4c4ac9b5a915c6bccc127f17414a9feb
Rui Chen <chenrui333@gmail.com>
2020-03-08 12:01:36 -0400

    fatsort 1.6.2

--- Formula/fatsort.rb
+++ Formula/fatsort.rb
@@ -1,9 +1,9 @@
 class Fatsort < Formula
   desc "Sorts FAT16 and FAT32 partitions"
   homepage "https://fatsort.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/fatsort/fatsort-1.5.0.456.tar.xz"
-  version "1.5.0"
-  sha256 "a835b47814fd30d5bad464b839e9fc404bc1a6f5a5b1f6ed760ce9744915de95"
+  url "https://downloads.sourceforge.net/project/fatsort/fatsort-1.6.2.605.tar.xz"
+  version "1.6.2"
+  sha256 "08cafa36f86ab89e7a3241d437df9af8a008549d7d13416256b7aaa006d5ffb7"
 
   bottle do
     cellar :any_skip_relocation
