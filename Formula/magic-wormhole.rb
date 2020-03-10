6531def43d271f15d41c8a4ec9841857ecffbe7a
Bo Anderson <mail@boanderson.me>
2020-03-10 22:02:57 +0000

    magic-wormhole: revision bump to fix virtualenv symlinks

--- Formula/magic-wormhole.rb
+++ Formula/magic-wormhole.rb
@@ -5,7 +5,7 @@ class MagicWormhole < Formula
   homepage "https://github.com/warner/magic-wormhole"
   url "https://files.pythonhosted.org/packages/77/15/9438290bab8146efc0213f7c3d9645d9bc5a2e885e4049477e7432e40336/magic-wormhole-0.11.2.tar.gz"
   sha256 "ae79667bdbb39fba7d315e36718db383651b45421813366cfaceb069e222d905"
-  revision 3
+  revision 4
 
   bottle do
     cellar :any
