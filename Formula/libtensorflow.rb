4152ce8f6f166b411dd13e92ae32e51086dc0478
Alexander Bayandin <a.bayandin@gmail.com>
2020-03-11 12:04:49 +0000

    libtensorflow 2.2.0

--- Formula/libtensorflow.rb
+++ Formula/libtensorflow.rb
@@ -3,9 +3,8 @@ class Libtensorflow < Formula
 
   desc "C interface for Google's OS library for Machine Intelligence"
   homepage "https://www.tensorflow.org/"
-  url "https://github.com/tensorflow/tensorflow/archive/v2.1.0.tar.gz"
-  sha256 "638e541a4981f52c69da4a311815f1e7989bf1d67a41d204511966e1daed14f7"
-  revision 1
+  url "https://github.com/tensorflow/tensorflow/archive/v2.2.0-rc2.tar.gz"
+  sha256 "3e0cffc98ad3767dd16a09b7f25a99961e21d65cfcad67ed3a9b1a01318c7e94"
 
   bottle do
     cellar :any
@@ -15,17 +14,17 @@ class Libtensorflow < Formula
   end
 
   depends_on "bazel" => :build
-  depends_on :java => ["1.8", :build]
+  depends_on "openjdk" => :build
   depends_on "python@3.8" => :build
 
   resource "numpy" do
-    url "https://files.pythonhosted.org/packages/40/de/0ea5092b8bfd2e3aa6fdbb2e499a9f9adf810992884d414defc1573dca3f/numpy-1.18.1.zip"
-    sha256 "b6ff59cee96b454516e47e7721098e6ceebef435e3e21ac2d6c3b8b02628eb77"
+    url "https://files.pythonhosted.org/packages/84/1e/ff467ac56bfeaea51d4a2e72d315c1fe440b20192fea7e460f0f248acac8/numpy-1.18.2.zip"
+    sha256 "e7894793e6e8540dbeac77c87b489e331947813511108ae097f1715c018b8f3d"
   end
 
   resource "six" do
-    url "https://files.pythonhosted.org/packages/94/3e/edcf6fef41d89187df7e38e868b2dd2182677922b600e880baad7749c865/six-1.13.0.tar.gz"
-    sha256 "30f610279e8b2578cab6db20741130331735c781b56053c59c4076da27f06b66"
+    url "https://files.pythonhosted.org/packages/21/9f/b251f7f8a76dec1d6651be194dfba8fb8d7781d10ab3987190de8391d08e/six-1.14.0.tar.gz"
+    sha256 "236bdbdce46e6e6a3d61a337c0f8b763ca1e8717c03b369e87a7ec7ce1319c0a"
   end
 
   resource "test-model" do
@@ -34,18 +33,15 @@ class Libtensorflow < Formula
   end
 
   def install
-    # Bazel fails if version from .bazelversion doesn't match bazel version, so just to use the latest one
+    # Bazel fails if version from .bazelversion doesn't match bazel version, so just delete it
     rm_f ".bazelversion"
-
-    cmd = Language::Java.java_home_cmd("1.8")
-    ENV["JAVA_HOME"] = Utils.popen_read(cmd).chomp
 
     venv = virtualenv_create("#{buildpath}/venv", "python3")
     (resources.map(&:name).to_set - ["test-model"]).each do |r|
       venv.pip_install resource(r)
     end
     ENV["PYTHON_BIN_PATH"] = "#{buildpath}/venv/bin/python"
-
+    ENV["JAVA_HOME"] = Formula["openjdk"].opt_prefix
     ENV["CC_OPT_FLAGS"] = "-march=native"
     ENV["TF_IGNORE_MAX_BAZEL_VERSION"] = "1"
     ENV["TF_NEED_JEMALLOC"] = "1"
