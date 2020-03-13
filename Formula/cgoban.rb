1b888e66470a69679bc0a3cd7d9440f1e4cb56ec
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-13 11:49:24 +0000

    cgoban: update 1.9.12 bottle.

--- Formula/cgoban.rb
+++ Formula/cgoban.rb
@@ -6,12 +6,10 @@ class Cgoban < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "95ba2672c251a7bb23614aee4711c84ceca8453679549847083fb5e025645f04" => :catalina
-    sha256 "9784461d9756059fa03d3239c3497ee3087fcaa67bcf235bdec2bab543560fae" => :mojave
-    sha256 "14efcd85d7d9f5a15fe0693eb8ebd4b1ee8b49fb7604681be91c14964af0cee3" => :high_sierra
-    sha256 "4f88f760ce464806c607e9a29da5f701cc6d395f27110cd866579fdf8737931a" => :sierra
-    sha256 "f6f9b32dddefb3474dca91d55c0aecdacec7e3b7ccbb2cb8c9b151b41e16f4d0" => :el_capitan
-    sha256 "65b5a814e1b4e3c115c1fb873d8a740ee4c0b39eff33331336d9bfac073e1a27" => :yosemite
+    rebuild 1
+    sha256 "6270ce26bcc430553edb53ea9591a31e112e197ff1303e8802370cc0f17151f4" => :catalina
+    sha256 "30288451ee59de5f94000b3a07946bdc21f64907278545b1f2ebfeb8b89b8636" => :mojave
+    sha256 "4b9863194da434f636025abf7392e6c5604eb35d28b19836b4493049d13041f8" => :high_sierra
   end
 
   depends_on :x11
