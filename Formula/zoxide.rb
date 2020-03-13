7c71eac0d07c1ef1b02267051f250f8b81e8d723
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-13 00:55:42 +0000

    zoxide: add 0.2.0 bottle.

--- Formula/zoxide.rb
+++ Formula/zoxide.rb
@@ -4,6 +4,13 @@ class Zoxide < Formula
   url "https://github.com/ajeetdsouza/zoxide/archive/v0.2.0.tar.gz"
   sha256 "628c394d7b5031cf986c5a8b24083dc5941a21d7491ae916684c3322ac748318"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "97572c4876291ad80679b57c69698dbbbcfd6759287ae1fc3eeb9fd481a37c02" => :catalina
+    sha256 "ab883169e2862a003dcb83292981cef2207e52d5086c25627d67844eaa94b2e8" => :mojave
+    sha256 "471419a9991f35feeba9a16d51b53ec13add49c1e0a14a95323f698973883d8b" => :high_sierra
+  end
+
   depends_on "rust" => :build
 
   def install
