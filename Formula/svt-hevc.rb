ca27cc1cd262692f6d42e253d6ef2ebf8ca2873b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-14 15:47:11 +0000

    svt-hevc: add 1.4.3 bottle.

--- Formula/svt-hevc.rb
+++ Formula/svt-hevc.rb
@@ -5,6 +5,13 @@ class SvtHevc < Formula
   sha256 "08bf2b1075609788194bf983d693ab38ed30214ec966afa67895209fd0bf2179"
   head "https://github.com/OpenVisualCloud/SVT-HEVC.git"
 
+  bottle do
+    cellar :any
+    sha256 "31a151f7caeea14aac9435b45e733a6ec8bfe5007dbe329e69497ee2ba64d3bc" => :catalina
+    sha256 "5522b35c21383ea554c2e955b78a6bdf31e70547c3d37b4441f8b057149bf02d" => :mojave
+    sha256 "52357f1c460c738950b5edbb421801a2f28380102352fddd0e752d2cda3346dd" => :high_sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "yasm" => :build
 
