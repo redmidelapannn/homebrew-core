cc324b3b1acce9fcb118da76265cc8874f9a524b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-15 05:46:08 +0000

    gforth: update 0.7.3_2 bottle.

--- Formula/gforth.rb
+++ Formula/gforth.rb
@@ -7,10 +7,9 @@ class Gforth < Formula
 
   bottle do
     cellar :any
-    rebuild 2
-    sha256 "9a3670647cbf4c87a62a271032ff04b70de04a9b2b3091be0c2a00cbfba860f2" => :catalina
-    sha256 "cba74a11ad3333f1462e3defcbc5903a1bc05ecca2d7686633da15affb12ebf5" => :mojave
-    sha256 "5643950f876b48bab6c92c8e9c016b8cf026907ee0c21f81a6a68db5b0a70e15" => :high_sierra
+    sha256 "e000e0ff55ee6b9ddf3c907d7e24855a89c68dc6cca03653b289ce52706fba0e" => :catalina
+    sha256 "b84e07778e39420b38cfa4f6010cb33f94f0daff3431ef4023c19d82bdba8017" => :mojave
+    sha256 "499a5d7e5897a5c7afba0b32b4bdac10ee2acc8d2d08581976bd79d9c99ca03a" => :high_sierra
   end
 
   depends_on "emacs" => :build
