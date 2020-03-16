5286ef0e1c5a000005d5bc9ccf71df1463da7258
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-16 21:03:01 +0000

    hmm: add 0.2 bottle.

--- Formula/hmm.rb
+++ Formula/hmm.rb
@@ -5,6 +5,13 @@ class Hmm < Formula
   sha256 "4e2d474d2eb70f313ef6a97a5f392310e0317be9f7d88d46ddf5fe9b9e6a1910"
   head "https://github.com/samwho/hmm.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ad5aaa87bd8721afc264650df2f6e5c9b81e6f55c7b925162d0bf107d3474c92" => :catalina
+    sha256 "b0142696b1619a1b0797baf8124792275f90872acf172f41cd3c8f7d82ef9f61" => :mojave
+    sha256 "35b7de025386953caac20ae899bfa53008cfcea621750b877ac932d3e4dec51f" => :high_sierra
+  end
+
   depends_on "rust" => :build
 
   def install
