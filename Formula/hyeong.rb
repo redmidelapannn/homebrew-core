b421727313160dc72538e390ff754cd53e60bf45
buttercrab <jaeyong0201@gmail.com>
2020-03-19 22:37:44 +0900

    hyeong 0.1.0 (new formula)

--- Formula/hyeong.rb
+++ Formula/hyeong.rb
@@ -1,7 +1,7 @@
 class Hyeong < Formula
   desc "Hyeo-ung Programming Language Compiler in Rust (beta release)"
   homepage "https://github.com/buttercrab/hyeo-ung-lang"
-  url "https://github.com/buttercrab/hyeo-ung-lang/releases/download/v#{version}/hyeong-#{version}-darwin-x86-64.tar.gz"
+  url "https://github.com/buttercrab/hyeo-ung-lang/releases/download/v0.1.0/hyeong-0.1.0-darwin-x86-64.tar.gz"
   version "0.1.0"
   sha256 "24990f87afe0813a0d67ba6525bc822b73f470c58692b5eedcdb5203dec2aa63"
 
