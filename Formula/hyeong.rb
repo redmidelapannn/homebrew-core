fd77563b265c26cc9cb85fdca289a2e9f244eced
buttercrab <jaeyong0201@gmail.com>
2020-03-19 21:40:59 +0900

    hyeong 0.1.0 (new formula)

--- Formula/hyeong.rb
+++ Formula/hyeong.rb
@@ -3,13 +3,8 @@ class Hyeong < Formula
   homepage "https://github.com/buttercrab/hyeo-ung-lang"
   version "0.1.0"
 
-  if OS.mac?
-    url "https://github.com/buttercrab/hyeo-ung-lang/releases/download/v#{version}/hyeong-#{version}-darwin-x86-64.tar.gz"
-    sha256 "24990f87afe0813a0d67ba6525bc822b73f470c58692b5eedcdb5203dec2aa63"
-  elsif OS.linux?
-    url "https://github.com/buttercrab/hyeo-ung-lang/releases/download/v#{version}/hyeong-#{version}-linux-x86-64.tar.gz"
-    sha256 "82ede138f347af3034a4c2b3efd84393ec60acd33af77f209cd59e4afb74fe98"
-  end
+  url "https://github.com/buttercrab/hyeo-ung-lang/releases/download/v#{version}/hyeong-#{version}-darwin-x86-64.tar.gz"
+  sha256 "24990f87afe0813a0d67ba6525bc822b73f470c58692b5eedcdb5203dec2aa63"
 
   def install
     bin.install "hyeong"
