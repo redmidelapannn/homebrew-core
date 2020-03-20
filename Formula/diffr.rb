b0c96d9a4bed3795db0fa1861f38484cb3952eb0
Ryan Delaney <ryan.delaney@gmail.com>
2020-03-20 15:57:04 -0700

    Use hash from the redirected download link

--- Formula/diffr.rb
+++ Formula/diffr.rb
@@ -2,7 +2,7 @@ class Diffr < Formula
   desc "LCS based diff highlighting tool to ease code review from your terminal"
   homepage "https://github.com/mookid/diffr"
   url "https://github.com/mookid/diffr/archive/v0.1.4.tar.gz"
-  sha256 "4e23c57b64f7085adbdfd09ea9a9156eab446c7dd199a46905de8d85304d2765"
+  sha256 "2613b57778df4466a20349ef10b9e022d0017b4aee9a47fb07e78779f444f8cb"
 
   bottle do
     cellar :any_skip_relocation
