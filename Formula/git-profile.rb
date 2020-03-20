9c580f3c8560773c8df354012917953e7199fa70
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-20 02:09:58 +0000

    git-profile: add 0.1.0 bottle.

--- Formula/git-profile.rb
+++ Formula/git-profile.rb
@@ -5,6 +5,13 @@ class GitProfile < Formula
   sha256 "999404c4872d5af9b3fbb651337617fdc5ab075015267a64ae044cb14ff12ea1"
   head "https://github.com/bobbo/git-profile.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "f28cc47bfd8469fb22532f8cc246b8f9a20dd404be29654f94787c6bc2ea7095" => :catalina
+    sha256 "811690d3247da780b0123a08a9e332964fdd64588cb5c1a6edf1648ce678bd38" => :mojave
+    sha256 "e2182db2efc12a564eaa116f21714db857eda8e02bbed297c40d7d90a43c55f2" => :high_sierra
+  end
+
   depends_on "rust" => :build
 
   def install
