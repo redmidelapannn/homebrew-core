3e96f2db2d132312c8e3e57e9e2b9041675ab83e
Fabian Brandt-Tumescheit <fabian.brandt-tumescheit@informatik.hu-berlin.de>
2020-03-24 09:42:13 +0100

    libnetworkit 6.1 (new formula)

---
+++ Formula/libnetworkit.rb
@@ -0,0 +1,37 @@
+class Libnetworkit < Formula
+  desc "NetworKit is an OS-toolkit for large-scale network analysis"
+  homepage "https://networkit.github.io"
+  url "https://github.com/networkit/networkit/archive/6.1.tar.gz"
+  sha256 "22c953ea1054c356663b31c77114c2f0c8fec17e0e707aeec23026241beab9b2"
+
+  depends_on "cmake" => :build
+  depends_on "libomp"
+  depends_on "tlx"
+
+  def install
+    mkdir "build" do
+      system "cmake", ".", *std_cmake_args,
+                           "-DNETWORKIT_EXT_TLX=#{Formula["tlx"].opt_prefix}",
+                           "-DOpenMP_CXX_FLAGS='-Xpreprocessor -fopenmp -I/usr/local/opt/libomp/include'",
+                           "-DOpenMP_CXX_LIB_NAMES='omp'",
+                           "-DOpenMP_omp_LIBRARY=/usr/local/opt/libomp/lib/libomp.dylib",
+                           ".."
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <networkit/graph/Graph.hpp>
+      int main()
+      {
+        // Try to create a graph with five nodes
+        NetworKit::Graph g(5);
+
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-L#{lib}", "-lnetworkit", "-o", "test", "-std=c++11"
+    system "./test"
+  end
+end
