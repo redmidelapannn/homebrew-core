9ac02a9409fab81a320b2a523665d23353907a8b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-27 13:58:15 +0000

    yosys: update 0.9 bottle.

--- Formula/yosys.rb
+++ Formula/yosys.rb
@@ -7,10 +7,10 @@ class Yosys < Formula
   head "https://github.com/YosysHQ/yosys.git"
 
   bottle do
-    sha256 "0e66fd2b20dd12deb72912e6730bcaa6ca45f99743d2f6a5cfa0b6d92398a30a" => :catalina
-    sha256 "f08231e871b392051da5e08d777c2ab06017718918825155ede2599512f63044" => :mojave
-    sha256 "7affc089d87cd4df5bc4d482cf445572b8fbb056eb7629ea8a7439b972ccb085" => :high_sierra
-    sha256 "910e842bc8d1337b978ea0f7b93f693a17ec12f93a1a3bac6a53306087d5c8b6" => :sierra
+    rebuild 1
+    sha256 "15bb20dcf753111cea60c55612bf5d945a117e351a8167ac51bbfa4cb66d8a4a" => :catalina
+    sha256 "f98d5216cab36c4e72a925ab264a6c356dd80f849c3bbdbe8d2531caba74b0ae" => :mojave
+    sha256 "dea6bb68a42b42b047b8336e829799b2f32f9fc302fac47d089c0df1d2539d23" => :high_sierra
   end
 
   depends_on "bison" => :build
