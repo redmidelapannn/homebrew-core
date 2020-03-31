6319a7e65dfb75368b6a9cf8f2b63ee25fa663f8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-31 02:41:04 +0000

    jobber: add 1.4.1 bottle.

--- Formula/jobber.rb
+++ Formula/jobber.rb
@@ -5,6 +5,13 @@ class Jobber < Formula
   sha256 "891b1f86f65690b36cfd7f0c3c78f9d81fdc976e85a8055ac4da0daf59adf066"
   head "https://github.com/dshearer/jobber.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "a4e088e0e43a454f1d44d838c91747154af12102f271123200c0cf63968b1f68" => :catalina
+    sha256 "facb15fbd0da25139ea8c8888b45314cba99d82ffe81719c304c989b96687ab6" => :mojave
+    sha256 "6121a3d39cf0db07d7fc01b7ea6fc4e52c18b60d73aee7336e32b44c7b7e82e9" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
