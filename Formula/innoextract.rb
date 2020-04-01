21f86d650e67386ec3b4f28dd4229ae6b6ca9d0d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-01 19:10:08 +0000

    innoextract: update 1.8 bottle.

--- Formula/innoextract.rb
+++ Formula/innoextract.rb
@@ -16,9 +16,10 @@ class Innoextract < Formula
 
   bottle do
     cellar :any
-    sha256 "d3a81822f7925b57904c0c669c4296b3ed0df8551c1cc3039c58341af674bf01" => :catalina
-    sha256 "c83524adccf3e591b2e72a7cc72668972f379a9aad9f8a555cc2ce3fa0a90143" => :mojave
-    sha256 "92323fd4044aae1db881b3e18ce58b8c51e77a8b745b4e9c120e500cf2cb3ed1" => :high_sierra
+    rebuild 1
+    sha256 "9ab79180f67a940da8f0b739e1f576070c0424dd4f4d451620ae9dc057b5e96b" => :catalina
+    sha256 "e1226575e1d7837502ed4e17ebaa411f266994adbaa57f82c375cccd829be862" => :mojave
+    sha256 "0eef612716ae051c48777e9e549b78439d82e3012253782174e9c8ef43a88770" => :high_sierra
   end
 
   depends_on "cmake" => :build
