afbcc71025439502c043f2ae7b1d5a2ce19b8e13
Aaron Liu <aaronliu0130@gmail.com>
2020-04-01 13:13:45 +0800

    Update terminalimageviewer.rb

--- Formula/terminalimageviewer.rb
+++ Formula/terminalimageviewer.rb
@@ -1,7 +1,7 @@
 class Terminalimageviewer < Formula
   desc "Display images in a terminal using block graphic characters"
   homepage "https://github.com/stefanhaustein/TerminalImageViewer"
-  url "https://github.com/stefanhaustein/TerminalImageViewer/archive/v1.0.0.zip"
+  url "https://github.com/stefanhaustein/TerminalImageViewer/archive/v1.0.0.tar.gz"
   sha256 "ab44f7d7b26152590a929fec6586e33a3b47737426467fc02c5d5149593ba094"
   head "https://github.com/stefanhaustein/TerminalImageViewer.git"
   depends_on "gcc"
