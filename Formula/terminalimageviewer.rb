980427b974d5a362e44153742eec34c72042366f
Aaron Liu <aaronliu0130@gmail.com>
2020-04-01 13:02:15 +0800

    Update terminalimageviewer.rb
    Audit and build error fixes

--- Formula/terminalimageviewer.rb
+++ Formula/terminalimageviewer.rb
@@ -1,14 +1,15 @@
 class Terminalimageviewer < Formula
-  desc "Small program to display images in a terminal using RGB ANSI codes and unicode block graphics characters"
+  desc "Display images in a terminal using block graphic characters"
   homepage "https://github.com/stefanhaustein/TerminalImageViewer"
   url "https://github.com/stefanhaustein/TerminalImageViewer/archive/v1.0.0.zip"
   sha256 "ab44f7d7b26152590a929fec6586e33a3b47737426467fc02c5d5149593ba094"
   head "https://github.com/stefanhaustein/TerminalImageViewer.git"
-  depends_on "imagemagick"
   depends_on "gcc"
+  depends_on "imagemagick"
   def install
     cd "src/main/cpp" do
-      system "make"
+      system "g++-9", "-std=c++17"," -Wall", "-fpermissive", "-fexceptions", "-O2", "-c", "tiv.cpp", "-o", "tiv.o"
+      system "g++-9", "tiv.o", "-o", "tiv", "-lstdc++fs", "-pthread", "-s"
       bin.install "tiv"
     end
   end
