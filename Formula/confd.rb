de3b112d7823bcf4a735119e221e7f65b76bcd62
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-01 07:24:23 +0000

    confd: add 0.16.0 bottle.

--- Formula/confd.rb
+++ Formula/confd.rb
@@ -5,6 +5,13 @@ class Confd < Formula
   sha256 "4a6c4d87fab77aa9827370541024a365aa6b4c8c25a3a9cab52f95ba6b9a97ea"
   head "https://github.com/kelseyhightower/confd.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c8aaa0f9d1b2e95c516af908920f3b3cf46094bac624bccd902b1a9b0a1856a1" => :catalina
+    sha256 "8e906ae8f026b6297eb5bd0caaefb5539b2ce5e5bed26a611b26b35961ca17ee" => :mojave
+    sha256 "1c4387106dbb0ace3786ecaf5bd4adffb11a6c99559a0b0b4aa4fcebe19ed08f" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
