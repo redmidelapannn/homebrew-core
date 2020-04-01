3c7418a5f8195e17a8b14cb86631eeecd575f8fe
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-01 17:57:34 +0000

    sd: update 0.7.2 bottle.

--- Formula/sd.rb
+++ Formula/sd.rb
@@ -6,10 +6,9 @@ class Sd < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "bdd3186fb20a398f02a3d2b9dee49e7f60f91e9566cddfc81be0ba4ce72fff2a" => :catalina
-    sha256 "29f6491b5f39d63482793ac52f703ed037c8f0759c9d18c5585ebdf3cab711bd" => :mojave
-    sha256 "0beab832bb7d2570a61d7c01033de8d47c3543e98d1b3fe20dead77ef5f28f7d" => :high_sierra
+    sha256 "4014ba7e8b90f76a31b3087009b98879456b89289b0aae0e8283af17f878b86b" => :catalina
+    sha256 "60b8217162989d199c66cf717dc52c88185296533c759a03457dd6c51f5e5c2b" => :mojave
+    sha256 "96bf4655919701583139e34f0e30f85a9d95e02d7afb644146108aa353a39145" => :high_sierra
   end
 
   depends_on "rust" => :build
