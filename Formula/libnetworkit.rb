24c18e6abcd8d3a8543883860a6373c0dfc131f0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-02 05:12:05 +0000

    libnetworkit: add 6.1 bottle.

--- Formula/libnetworkit.rb
+++ Formula/libnetworkit.rb
@@ -4,6 +4,12 @@ class Libnetworkit < Formula
   url "https://github.com/networkit/networkit/archive/6.1.tar.gz"
   sha256 "22c953ea1054c356663b31c77114c2f0c8fec17e0e707aeec23026241beab9b2"
 
+  bottle do
+    cellar :any
+    sha256 "2ef2eb869def3a16e9709a6d22acab4f39a5cd40dc8290a4926ce155e86156dd" => :mojave
+    sha256 "5fc2673403ff62551f05ee1817e2168fb3fbdfcbb531e8e359006b97b8c49bcf" => :high_sierra
+  end
+
   depends_on "cmake" => :build
   depends_on "libomp"
   depends_on "tlx"
