915f638b83fe2d54b44fafdb18e00656b1fcfebe
Misty De Meo <mistydemeo@gmail.com>
2020-04-02 23:46:54 -0700

    fontforge 20200314

--- Formula/fontforge.rb
+++ Formula/fontforge.rb
@@ -1,9 +1,8 @@
 class Fontforge < Formula
   desc "Command-line outline and bitmap font editor/converter"
   homepage "https://fontforge.github.io"
-  url "https://github.com/fontforge/fontforge/releases/download/20190801/fontforge-20190801.tar.gz"
-  sha256 "d92075ca783c97dc68433b1ed629b9054a4b4c74ac64c54ced7f691540f70852"
-  revision 1
+  url "https://github.com/fontforge/fontforge/releases/download/20200314/fontforge-20200314.tar.xz"
+  sha256 "cd190b237353dc3f48ddca7b0b3439da8ec4fcf27911d14cc1ccc76c1a47c861"
 
   bottle do
     cellar :any
@@ -12,6 +11,7 @@ class Fontforge < Formula
     sha256 "0e91f1858662fa08537287af6e1884ef94377a46a5eb24cec61f3f9f41c48e8a" => :high_sierra
   end
 
+  depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "cairo"
   depends_on "fontconfig"
@@ -31,24 +31,14 @@ class Fontforge < Formula
   uses_from_macos "libxml2"
 
   def install
-    ENV["PYTHON_CFLAGS"] = `python3-config --cflags`.chomp
-    ENV["PYTHON_LIBS"] = `python3-config --ldflags`.chomp
+    mkdir "build" do
+      system "cmake", "..", "-DENABLE_GUI=OFF",
+                            "-DENABLE_FONTFORGE_EXTRAS=ON",
+                            *std_cmake_args
+      system "make", "install"
 
-    system "./configure", "--prefix=#{prefix}",
-                          "--enable-python-scripting=3",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--without-x"
-    system "make", "install"
-
-    # The app here is not functional.
-    # If you want GUI/App support, check the caveats to see how to get it.
-    (pkgshare/"osx/FontForge.app").rmtree
-
-    # Build extra tools
-    cd "contrib/fonttools" do
-      system "make"
-      bin.install Dir["*"].select { |f| File.executable? f }
+      # The "extras" built above don't get installed by default.
+      bin.install Dir["bin/*"].select { |f| File.executable? f }
     end
   end
 
