cb469743a721a32623b92dd9749a4e63cafc6c1c
Dror Levin <spatz@psybear.com>
2020-04-04 18:47:32 +0300

    ipython: use python3.8 in install block as well

--- Formula/ipython.rb
+++ Formula/ipython.rb
@@ -119,20 +119,20 @@ class Ipython < Formula
   end
 
   def install
-    xy = Language::Python.major_minor_version "python3"
+    xy = Language::Python.major_minor_version "python3.8"
     ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python#{xy}/site-packages"
 
     # install other resources
     ipykernel = resource("ipykernel")
     (resources - [ipykernel]).each do |r|
       r.stage do
-        system "python3", *Language::Python.setup_install_args(libexec/"vendor")
+        system "python3.8", *Language::Python.setup_install_args(libexec/"vendor")
       end
     end
 
     # install and link IPython
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
-    system "python3", *Language::Python.setup_install_args(libexec)
+    system "python3.8", *Language::Python.setup_install_args(libexec)
     bin.install libexec/"bin/ipython"
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
 
@@ -141,7 +141,7 @@ class Ipython < Formula
 
     # install IPyKernel
     ipykernel.stage do
-      system "python3", *Language::Python.setup_install_args(libexec/"vendor")
+      system "python3.8", *Language::Python.setup_install_args(libexec/"vendor")
     end
 
     # install kernel
