616149a94d794e4622c80867a6a914ebace4eff4
fishtreesugar <fishtreesugar@gmail.com>
2020-04-04 14:43:58 +0800

    try to build with latest ghc, haskell/cabal#6327

--- Formula/cabal-install.rb
+++ Formula/cabal-install.rb
@@ -3,7 +3,7 @@ class CabalInstall < Formula
   homepage "https://www.haskell.org/cabal/"
   url "https://hackage.haskell.org/package/cabal-install-3.2.0.0/cabal-install-3.2.0.0.tar.gz"
   sha256 "a0555e895aaf17ca08453fde8b19af96725da8398e027aa43a49c1658a600cb0"
-  head "https://github.com/haskell/cabal.git", :branch => "2.4"
+  head "https://github.com/haskell/cabal.git", :branch => "3.2"
 
   bottle do
     cellar :any_skip_relocation
@@ -12,16 +12,10 @@ class CabalInstall < Formula
     sha256 "72616fee2252d33d00e79ecd1778f0f8abffd71e339482dda5927c10d2574746" => :high_sierra
   end
 
-  # Temporarily depend on older GHC for building cabal-install itself, due to
-  # https://github.com/Homebrew/homebrew-core/pull/46828
-  # https://github.com/haskell/cabal/issues/6327
-  depends_on "ghc@8.6" => :build
   depends_on "ghc"
   uses_from_macos "zlib"
 
   def install
-    ENV.prepend_path "PATH", Formula["ghc@8.6"].opt_bin
-
     cd "cabal-install" if build.head?
 
     system "sh", "bootstrap.sh", "--sandbox"
