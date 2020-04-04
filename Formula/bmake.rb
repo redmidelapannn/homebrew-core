399dc00eb281a6476722e904a4715ebdd492adf9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-04 05:33:00 +0000

    bmake: update 20200402 bottle.

--- Formula/bmake.rb
+++ Formula/bmake.rb
@@ -5,9 +5,9 @@ class Bmake < Formula
   sha256 "cf15f204ad8eea3396c2c7179b5ec0cffb06c9628b6f91050c6f2cbcaabb8928"
 
   bottle do
-    sha256 "61b77e5ea73e1cdaf0188d68b3ca2b06d49651bb210c3800d7227f07765788fb" => :catalina
-    sha256 "d478f714529d5893085fdc064863caaa429b3b21b125269b88932f39a3b60997" => :mojave
-    sha256 "318d703f8a240bd5fe27eb43714660f3d59b1bffdce8007e36cc5a5d77487cdc" => :high_sierra
+    sha256 "791bcb4dd437155d7771c72000062beabf78e216d44b4b28b8b6ad33a54fef54" => :catalina
+    sha256 "eb71acc9382ec79c173bbe193847531e29488c402fed5b7669d454dacc63cae7" => :mojave
+    sha256 "47b6c68f8c58858549362de3658879a8960baa8aabaf008b462546cd7b020640" => :high_sierra
   end
 
   def install
