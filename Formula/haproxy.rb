d2eb3fccee02941dc607a000f65d179cf09b58b8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-04 01:54:01 +0000

    haproxy: update 2.1.4 bottle.

--- Formula/haproxy.rb
+++ Formula/haproxy.rb
@@ -6,9 +6,9 @@ class Haproxy < Formula
 
   bottle do
     cellar :any
-    sha256 "1ab7f5cdb7122093cb499c7baa0d934881773149bbfd27aaffde23feb7abc8da" => :catalina
-    sha256 "d57304df184c4f131e3f51e985369b969c2a38c699067c151b55c597d62b1377" => :mojave
-    sha256 "3099a24f5d6ca48e791646140854b24ce1a0b1742c10c315e048841e602cdf88" => :high_sierra
+    sha256 "0c7dd82a7aeb37da3bc352b66ed6097523a7851be10ef06e896f10e7d883e0ce" => :catalina
+    sha256 "9c41f484ea0cb6ca6cedbe605697440cd55942c84154d583229e68c9a87f684f" => :mojave
+    sha256 "3111a991c259d579a76c9593b01631f47f8fc4ef3b9f5a39bc53b3dcbf448185" => :high_sierra
   end
 
   depends_on "openssl@1.1"
