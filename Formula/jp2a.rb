d853229820cbbd00cecbbcc58d760accb949d453
Illya Moskvin <imoskvin@artic.edu>
2020-04-04 21:59:14 -0500

    jp2a: comment on downgrade to 1.0.6

--- Formula/jp2a.rb
+++ Formula/jp2a.rb
@@ -1,9 +1,13 @@
 class Jp2a < Formula
   desc "Convert JPG images to ASCII"
   homepage "https://csl.name/jp2a/"
+  # Do not change source from SourceForge to GitHub until this issue is resolved:
+  # https://github.com/cslarsen/jp2a/issues/8
+  # Currently, GitHub only has jp2a v1.0.7, which is broken as described above.
+  # jp2a v1.0.6 is stable, but it is only available on SourceForge, not GitHub. 
   url "https://downloads.sourceforge.net/project/jp2a/jp2a/1.0.6/jp2a-1.0.6.tar.gz"
   sha256 "0930ac8a9545c8a8a65dd30ff80b1ae0d3b603f2ef83b04226da0475c7ccce1c"
-  revision 1
+  version_scheme 1
 
   bottle do
     cellar :any
