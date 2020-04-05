1d706f01c3f0759ba9ed64e28c1e2233ef566255
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-05 06:55:45 +0000

    emacs-dracula: add 1.7.0 bottle.

--- Formula/emacs-dracula.rb
+++ Formula/emacs-dracula.rb
@@ -7,6 +7,13 @@ class EmacsDracula < Formula
     url "https://github.com/dracula/emacs/archive/v1.7.0.tar.gz"
     sha256 "dbbcc968cf8187a8ada9f040137ba03dc0e51b285e96e128d26cea05cf470330"
   end
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "cd6c068e93321defb92aaac5874511cb826fc89fa8c9db9954b8f4d172c37f02" => :catalina
+    sha256 "cd6c068e93321defb92aaac5874511cb826fc89fa8c9db9954b8f4d172c37f02" => :mojave
+    sha256 "cd6c068e93321defb92aaac5874511cb826fc89fa8c9db9954b8f4d172c37f02" => :high_sierra
+  end
+
 
   depends_on "emacs"
 
