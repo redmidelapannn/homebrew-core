9c23b8f2f91472b19bfbe3791b364001f5894d25
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-05 02:27:22 +0000

    swiftformat: update 0.44.7 bottle.

--- Formula/swiftformat.rb
+++ Formula/swiftformat.rb
@@ -7,9 +7,9 @@ class Swiftformat < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "64097d967738decbd341baa3d17017028aaf65adcf4fce3cbf8ea166c9bff241" => :catalina
-    sha256 "96f820d08a8f0b9e1bd986abf792730ffa4365af822621d11780dae555d5fe76" => :mojave
-    sha256 "df268070fc2ce986e6a33fcef5ecccb6aa14dff2e48f9723568ee2f43fe16efc" => :high_sierra
+    sha256 "dfc99984ccc544587437de7f7a9c988efef27e1926048b5d32c6035d98ad5a94" => :catalina
+    sha256 "756cb8f8d8970bcd987a3b7283780ef6449df9dd03332ecf91f0a977f4c7656a" => :mojave
+    sha256 "ba97f18be475519a1cc5d8a5b84d604876d402621199ddbd609d2e4f301e5fb6" => :high_sierra
   end
 
   depends_on :xcode => ["10.1", :build]
