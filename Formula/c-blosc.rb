122468c73c3307ad198af20dae8b24b1db54ec65
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-05 22:59:02 +0000

    c-blosc: update 1.18.1 bottle.

--- Formula/c-blosc.rb
+++ Formula/c-blosc.rb
@@ -6,9 +6,9 @@ class CBlosc < Formula
 
   bottle do
     cellar :any
-    sha256 "4896df1cf1f325a0499f55801d5c9cace67ce4234e029532a1a5acb912bb8a12" => :catalina
-    sha256 "fd241bcebdb57fa54ffb06ab55f0f59cb213346e7d9f217a7aece1a4317cec6f" => :mojave
-    sha256 "8f961c2826d79cc0e80a5154896120d05ccd57621dbfb1257349979e9ce4ddf3" => :high_sierra
+    sha256 "c46daac1d7825a11898af51b4cd164d7950c9676b6a26ef698793c9850d0c2a7" => :catalina
+    sha256 "664212e137fdee165a38f085a09f4d037e72ab44e728083e2ee45995928ab838" => :mojave
+    sha256 "f2fe09103011af04367888bbc33da0cb3e086e7a23c9eb1347a2426d75414fd2" => :high_sierra
   end
 
   depends_on "cmake" => :build
