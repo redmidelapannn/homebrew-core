749850b0c6587ea93c9fb9860ed5fd849a65e384
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-06 08:59:41 +0000

    geph2: add 0.20.2 bottle.

--- Formula/geph2.rb
+++ Formula/geph2.rb
@@ -4,6 +4,13 @@ class Geph2 < Formula
   url "https://github.com/geph-official/geph2/archive/v0.20.2.tar.gz"
   sha256 "fe5bef5ed04ab6f2e254e7e142ce93d57f5f1c157651db2c29aaf3cf13abf647"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d290f4fc037ecd649dcec532c0f27b8bab89cc461c045c3ec577f7c3fd32b7ad" => :catalina
+    sha256 "d290f4fc037ecd649dcec532c0f27b8bab89cc461c045c3ec577f7c3fd32b7ad" => :mojave
+    sha256 "d290f4fc037ecd649dcec532c0f27b8bab89cc461c045c3ec577f7c3fd32b7ad" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
