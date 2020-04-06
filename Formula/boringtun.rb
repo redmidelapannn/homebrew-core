2b909c96899a57254efd8368d1a30ca3842cf51b
Rui Chen <chenrui333@gmail.com>
2020-04-06 09:07:32 -0400

    boringtun 0.3.0

--- Formula/boringtun.rb
+++ Formula/boringtun.rb
@@ -1,8 +1,8 @@
 class Boringtun < Formula
   desc "Userspace WireGuard implementation in Rust"
   homepage "https://github.com/cloudflare/boringtun"
-  url "https://github.com/cloudflare/boringtun/archive/v0.2.0.tar.gz"
-  sha256 "544c72fc482b636e7f6460bfee205adafc55de534067819e4e4914980f0d1350"
+  url "https://github.com/cloudflare/boringtun/archive/v0.3.0.tar.gz"
+  sha256 "1107b0170a33769db36876334261924edc71dfc1eb00f9b464c7d2ad6d5743d3"
   head "https://github.com/cloudflare/boringtun.git"
 
   bottle do
