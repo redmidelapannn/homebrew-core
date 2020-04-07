38f71e12433610343403e9eb232f966bf2a431ba
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-07 01:49:15 +0000

    bench: update 1.0.12 bottle.

--- Formula/bench.rb
+++ Formula/bench.rb
@@ -10,10 +10,10 @@ class Bench < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "cd770017ec21cd829d97013812172c21dc6ce8b00426029a692f5dd5d82dc0fe" => :mojave
-    sha256 "c6cfcfc380c022f98a32a061268dca710f109600aa6d52c5b5e541855889ea14" => :high_sierra
-    sha256 "54eb5c88dc8c8d8fa43a16fa5439b2592f0fac57bf1eeb895b181de6b2d56f94" => :sierra
-    sha256 "7081e2d83fb8dee4d72fa203e0d6751eb70e1d38d64c5366f1f04cdd72fec90b" => :el_capitan
+    rebuild 1
+    sha256 "100f160aeefe50858fca272869d369b52c03ff707bea39da1d896176dff82a97" => :catalina
+    sha256 "322908a72279e28fb8058fe36a86f4e83544159c6d5843dfeff6caaf0388e7a4" => :mojave
+    sha256 "88d5d6271227f8a0110913f1c652ed20ec8e5a9df2ff09194b7cb06858a0eed8" => :high_sierra
   end
 
   depends_on "cabal-install" => :build
