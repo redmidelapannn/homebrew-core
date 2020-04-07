e9e7016b75c212bce9d104bbf59f19054fb18f8f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-07 08:57:07 +0000

    awscli: update 2.0.7 bottle.

--- Formula/awscli.rb
+++ Formula/awscli.rb
@@ -8,9 +8,9 @@ class Awscli < Formula
   head "https://github.com/aws/aws-cli.git", :branch => "v2"
 
   bottle do
-    sha256 "6f118d818eb7d0b07cbe8bb9245c5ab33d2e2db09b1292c1fb5a0cc8a5329287" => :catalina
-    sha256 "33ea1167f7df3d920e4562abada36ed309289ebddcca718ef35867dd21bcf30a" => :mojave
-    sha256 "027b7946e7e61818eac9a81e80e7fd717908d3b1b90d04dc5306c2396b85f583" => :high_sierra
+    sha256 "391d05dafa5dddec0adbbfcdb31a0b35926885f0d77371070952592d5b6fabc9" => :catalina
+    sha256 "c54be383061c128a95951bc9e37c54c01ff699005a197ed0fdd512534dbcebc4" => :mojave
+    sha256 "48f880aa4f57d13c043744833793084d0877425220f52eb576f4bc49c9df9378" => :high_sierra
   end
 
   # Some AWS APIs require TLS1.2, which system Python doesn't have before High
