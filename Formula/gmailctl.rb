723455eb7a6e98f4a78861e4d74343dcfaeb4ad1
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-07 16:49:08 +0000

    gmailctl: add 0.6.0 bottle.

--- Formula/gmailctl.rb
+++ Formula/gmailctl.rb
@@ -4,6 +4,13 @@ class Gmailctl < Formula
   url "https://github.com/mbrt/gmailctl/archive/v0.6.0.tar.gz"
   sha256 "e188d5d18ac84ca86c94fb2b90d219ae3bea1e1ddee1966fae904c93bf27f233"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "0dafaab299749fb4e0756dfe02a68d8393bf9c39b041d3778b9245233b6a7a81" => :catalina
+    sha256 "d34d390cea833e696c31da94cdb04bcefd4832f48eabb1c50fee4b07fd05374b" => :mojave
+    sha256 "e1955f05c43db784360cf07abcbb066fc15809b3ed4134a734aa9593d4bb8501" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
