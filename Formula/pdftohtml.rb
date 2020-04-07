24aa2878879c804e0f52dacb70bc029dcfb05b26
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-07 23:02:23 +0000

    pdftohtml: update 0.40a bottle.

--- Formula/pdftohtml.rb
+++ Formula/pdftohtml.rb
@@ -6,10 +6,10 @@ class Pdftohtml < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 2
-    sha256 "d8a6e5bb1d84ee766898543d77307b4a9a6e6f826ebe9cc48ce6db8bb24c8923" => :catalina
-    sha256 "c49245634c48c7c24501cfb848a98e4b6a281ff0cf89235bb7a7ce09619e66ad" => :mojave
-    sha256 "200be428031e013f58b792b092b56e74743d6362d747b0c883bb95269d7a5e72" => :high_sierra
+    rebuild 3
+    sha256 "86fc0fc1ec7a2b860c778cf8a55240e6aeb3d337ed68e844ef7c7e3e4cb5ca79" => :catalina
+    sha256 "cd9998cd6678310dcbf827fc2257b5fd0de706e7fb2457e9a11e1bb0f6e83d95" => :mojave
+    sha256 "03c3bfbbc04d6e04339045ca8819637b37e645c1095934640f9851826d4590d2" => :high_sierra
   end
 
   conflicts_with "pdf2image", "poppler", "xpdf",
