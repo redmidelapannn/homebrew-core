b5f465a03584241e8f8a5ebd92ecab1cf43faa48
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-07 07:26:34 +0000

    bcoin: add 2.0.0 bottle.

--- Formula/bcoin.rb
+++ Formula/bcoin.rb
@@ -5,6 +5,12 @@ class Bcoin < Formula
   homepage "https://bcoin.io"
   url "https://github.com/bcoin-org/bcoin/archive/v2.0.0.tar.gz"
   sha256 "ff99b735d3b23624455d72eada0de7f7ba4207f4a4cc0cab963ca2036de254a3"
+  bottle do
+    sha256 "1ddf0d2b1dc1821478b4e34fb13b62eca62611eea7fbe8ce022dbb6c4a8cdd14" => :catalina
+    sha256 "99ebb4efe574968f1e6a15b650dff7f6a1d04bf04764d54f4b8715c20cfe39e9" => :mojave
+    sha256 "3b4367711e145b5749e6492d629f3fc5790c2d24c7905f00b7047772bccded8b" => :high_sierra
+  end
+
   depends_on "python@3.8" => :build
   depends_on "node"
 
