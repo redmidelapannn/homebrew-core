20aec47b4d6d40b1967fc7ba398ad0f1ab79308c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-07 23:02:24 +0000

    delta: update 2006.08.03 bottle.

--- Formula/delta.rb
+++ Formula/delta.rb
@@ -6,14 +6,10 @@ class Delta < Formula
 
   bottle do
     cellar :any_skip_relocation
-    rebuild 2
-    sha256 "244dfd6407c2b65ad33ca707b8642f51d5f63c8056ddd45baf5bc3734dc545ec" => :catalina
-    sha256 "a6116fb7212cb2271b5c73c1bb53f51aeb33bfcc734e77bd42396a968744a42c" => :mojave
-    sha256 "46734f3eb952455ecd9237ce455aebb3e66be791bbf190021d894dae39d55b66" => :high_sierra
-    sha256 "07e775a1054966ad2924512386643bc8cb4ef3ad7e12ce9a140015c82fba3072" => :sierra
-    sha256 "202409012500969cfd034c9d44c441a809445a3b367d514357346438aa850f14" => :el_capitan
-    sha256 "d3374cc3e84c93bb84615b1669503ea8b708ab65baf629ee0be9a728b12b10bc" => :yosemite
-    sha256 "04102ae55ffc2cc4351816b010544b854c21f1c5e2a462a6af0e57ec2f57b501" => :mavericks
+    rebuild 3
+    sha256 "b67231611bb8d67a9b5f27b64d737f8b7b9282a400a5b1f69984f42fe3c77ee0" => :catalina
+    sha256 "973478bf573ed7ccf7ccb53094a69a3b22b01fb6513f14bf1f17fda6306f10aa" => :mojave
+    sha256 "b4a8b2d79d6987eb766adb17847c7065e904557e7d2bb0af5cebc9a84dfb5291" => :high_sierra
   end
 
   conflicts_with "git-delta", :because => "both install a `delta` binary"
