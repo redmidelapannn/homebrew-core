11fe91f6c4cb6c19f8ef3c01e8ed4eaf0884600d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-07 03:41:33 +0000

    libdvdcss: update 1.4.2 bottle.

--- Formula/libdvdcss.rb
+++ Formula/libdvdcss.rb
@@ -6,11 +6,10 @@ class Libdvdcss < Formula
 
   bottle do
     cellar :any
-    sha256 "352a2c343c04e65ee38fe154c797a29cc9cca509212e2296e9cd54e3e824ce29" => :catalina
-    sha256 "645422cdd6facba8137146fd12df0538b27432a72bc79c5ca8c2667ab9fc70bc" => :mojave
-    sha256 "4029db91ed7536435bd29db6b67f55509be13e70b6170337edec72daad8992c4" => :high_sierra
-    sha256 "907d51957c4674ddeb27b458dcf5f1f4b382219bda893fc8908147acc1c2b1ea" => :sierra
-    sha256 "0aaed21ecd3c8d3b4a9997300a599de5a541689ab200a6ffce52167b2ce5b664" => :el_capitan
+    rebuild 1
+    sha256 "ea741f5452116ba10c6e2667c091a8f7fe7fb0ed9c17ed703e1ce968f4bbe427" => :catalina
+    sha256 "26abd7508ae5b4f49bbffeba67ea2dcd68fd1bb5404a2bc40cc2cd274c184c4b" => :mojave
+    sha256 "ca89a41e79312d87d730ed549b94d540ea1d04abbf1027197a6214c4e586b78d" => :high_sierra
   end
 
   head do
