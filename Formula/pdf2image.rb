b507e511defb38a49db54e028826872277c8bc3e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-07 23:02:22 +0000

    pdf2image: update 0.53 bottle.

--- Formula/pdf2image.rb
+++ Formula/pdf2image.rb
@@ -5,12 +5,10 @@ class Pdf2image < Formula
   sha256 "e8672c3bdba118c83033c655d90311db003557869c92903e5012cdb368a68982"
 
   bottle do
-    sha256 "7a62006adfc88fc38c5333d94836127d4f51228291dfddc726f10a1cac1b6383" => :catalina
-    sha256 "722d8eefc7f7f12555f4997ca13470fda8b508a2683bc9edc6bfa5b883707202" => :mojave
-    sha256 "59ba0fc004f64cafaeb8e9beb6c07964b8df8fb10bf653aeb83713e3610ac182" => :high_sierra
-    sha256 "46df7ae58a1bfc73cd1a1c1075de032724514d29995e965169d69f51409cad7e" => :sierra
-    sha256 "c12d781ab5136a717cb88cadb50b2dfcd1f67cf263b5b668b1e171f562bcb072" => :el_capitan
-    sha256 "a0bb792123e4754d5cf80cf248e8932dd1885616af2c4c9c7f00e35cda962725" => :yosemite
+    rebuild 1
+    sha256 "69f7776970d56a44935485f4caa67b3e07fe32328ec50db6ce378597df81a375" => :catalina
+    sha256 "d65f584062a5ef2036750204f83fea3d58514f39740de25a86df754e39dcf950" => :mojave
+    sha256 "452c77cdf3b6190fb3cba1251af14005b27375cf8650953adea4cd7c81e038e0" => :high_sierra
   end
 
   depends_on "freetype"
