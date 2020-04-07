6c6a7042b76ebb8feb9ff7f70db4b91e77c9eeaa
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-07 21:06:16 +0000

    ungit: update 1.5.6 bottle.

--- Formula/ungit.rb
+++ Formula/ungit.rb
@@ -8,9 +8,10 @@ class Ungit < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "df95d903830745c30847bb392c132d1ae7f7153dc93fa0907ae0476b23259fdb" => :catalina
-    sha256 "cdc3fb0e814a174f6638f89ea71883898256e08a10bd550fea0055046ec3fe19" => :mojave
-    sha256 "5159c2c7cd97a7de351b8121c93b57a5cdebd59287cab7ff24ad668256ca180f" => :high_sierra
+    rebuild 1
+    sha256 "0b043b56fd582c734b726d711e7c84efc570c787915c9ccab9971e5832721087" => :catalina
+    sha256 "37fe583ae197f5f58ea6856d4fed01c6d22eb5fdc5918ac9f3c12b8cfc0cec71" => :mojave
+    sha256 "22a948a97417c0b2c45e0e43acaad3bbd7fa586eb0720927254831737fc467d2" => :high_sierra
   end
 
   depends_on "node"
