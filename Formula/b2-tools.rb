1edd1ae289b923aace6335fc16a666a9d93fb256
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-08 12:45:41 +0000

    b2-tools: update 1.4.2_1 bottle.

--- Formula/b2-tools.rb
+++ Formula/b2-tools.rb
@@ -9,9 +9,9 @@ class B2Tools < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "e21e5a9daf848f8a36850cad7d18073c8b8af9db6bf2e6860549b6e7123e2b00" => :catalina
-    sha256 "6d034a4b3ef822dd53b09a17ff0e924d6ad130cc6b376287425cc0300d425bc6" => :mojave
-    sha256 "450cd0e09c64458f5af99414f523f56b93275c5efa6ed2c0a6a18420917d2a31" => :high_sierra
+    sha256 "359dc46390fbfc8b78ee10a1ca122b3bae6164030be4db88bfa5458c9bd25306" => :catalina
+    sha256 "c7c3ecfe13f1b9ea4091d791f0361f359953c2f1bc5f30774bed11e6e6ba3c31" => :mojave
+    sha256 "6c9171b570863d3dc3770d2c5ff22f5ec4925705cd5ff054f150773ba9362a3d" => :high_sierra
   end
 
   depends_on "python@3.8"
