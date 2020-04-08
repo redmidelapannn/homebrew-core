142db43018b5bc645ec1f64cc015832d4c9208a3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-08 19:39:41 +0000

    gnutls: update 3.6.13 bottle.

--- Formula/gnutls.rb
+++ Formula/gnutls.rb
@@ -6,9 +6,8 @@ class Gnutls < Formula
   sha256 "32041df447d9f4644570cf573c9f60358e865637d69b7e59d1159b7240b52f38"
 
   bottle do
-    sha256 "3368ca58ab6284e849a47ac03c5634b799d550f6348b9dd0794d68bc747c6ca6" => :catalina
-    sha256 "f05a5a764d462c533d5f0777d3bedb3c26847255ce246c6b4eb8b6b2dfd18e15" => :mojave
-    sha256 "323964700f54c32854c860baef7f310a8a8c6b886912c22cff6503cc42f5e224" => :high_sierra
+    sha256 "8403f9e815f40bcd44b56d000fbeaa58412038b8b0ec46ec94947f537fdb9267" => :mojave
+    sha256 "78cdbdae53178b34bf98f87e9d65669ea104a4e6c7222cd4c712bb1c192cbe11" => :high_sierra
   end
 
   depends_on "autoconf" => :build
