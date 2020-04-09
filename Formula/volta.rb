6ea94cd4d1f204ead3b45774524ff712a096381a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-09 18:33:10 +0000

    volta: add 0.7.2 bottle.

--- Formula/volta.rb
+++ Formula/volta.rb
@@ -5,6 +5,13 @@ class Volta < Formula
     :revision => "10a9bee148d2a833069d6cc27aea56af3092dc6e",
     :tag      => "v0.7.2"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8d84b665093d92ce260ebff1cc3c36a3fc8f0bea975e06278fae7b2d81d12043" => :catalina
+    sha256 "9b8299ba11bee6e301e02adf1c16d6f323fcf86cc8c191240a4993a16b508688" => :mojave
+    sha256 "b0f1e5d59debf9f5d3348933dcb77bdadc9413fc5c120eaab10ddc3bba7ac0b8" => :high_sierra
+  end
+
   depends_on "rust" => :build
 
   def install
