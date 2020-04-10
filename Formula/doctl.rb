f9dd429896dce677676c107d5309f81eb18ce053
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-10 08:45:52 +0000

    doctl: update 1.40.0_1 bottle.

--- Formula/doctl.rb
+++ Formula/doctl.rb
@@ -8,9 +8,9 @@ class Doctl < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0d2e9299897198232be035d7f9e44548f1f30a4fd6017a985171f62ca8726432" => :catalina
-    sha256 "1447c8b1ccf051028d66a0f2f40a5a741120a1a179daea3af2c71cf6aeb935e1" => :mojave
-    sha256 "5f16e005f514d492247a90d4eec34af46485d6ae4012e48324476967656cc97b" => :high_sierra
+    sha256 "c948ba9079ea6741ee6fd4d2126a301a961087a4de535f99410aa14d9afea966" => :catalina
+    sha256 "dd7fb701b1af78a4eeb98233310f9a3b8e46d6c527c46ea8ac688ffaee51a782" => :mojave
+    sha256 "94d6ce73703d6db22372073a7cd06584c8de9174da672c0f4d39d941c4206e22" => :high_sierra
   end
 
   depends_on "go" => :build
