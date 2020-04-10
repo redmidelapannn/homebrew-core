6a5a3c57933bb691b9e89335a7c6447789c3b98c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-10 03:56:14 +0000

    wolfmqtt: add 1.4 bottle.

--- Formula/wolfmqtt.rb
+++ Formula/wolfmqtt.rb
@@ -6,6 +6,13 @@ class Wolfmqtt < Formula
       :revision => "af3d2926773b2c97f5e3a86ea2562e339a91b747"
   head "https://github.com/wolfSSL/wolfMQTT.git"
 
+  bottle do
+    cellar :any
+    sha256 "91a0b891edee1d2919eaa5a88ce96cbbebc4a6efe177e201345bdd3633bf254a" => :catalina
+    sha256 "90f85f7b9017648e50be0f469641044906fca68f360106d08a37da67e8e23c83" => :mojave
+    sha256 "a7d6c33f571c515bb99573fd2a90e98b1a077d493a7632f63e8e6e45706e2f20" => :high_sierra
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
