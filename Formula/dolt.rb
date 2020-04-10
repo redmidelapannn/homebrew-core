86ebe5f9b693cd62098e3dc354eaf49842e36663
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-10 18:12:08 +0000

    dolt: add 0.15.2 bottle.

--- Formula/dolt.rb
+++ Formula/dolt.rb
@@ -4,6 +4,13 @@ class Dolt < Formula
   url "https://github.com/liquidata-inc/dolt/archive/v0.15.2.tar.gz"
   sha256 "4ad1a89bb76dec9bb3863c1dcbf200d68e02fa2c5b2f68f1e952e64a3fc9b16b"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "55a013cef8d31999c8952873384f4581bbdeef902d0b5a02fd5fc24afc04cce9" => :catalina
+    sha256 "f46f3a5a97954b0689aabf7bfed5c43915aaf11053af70b5420a5a59890ba63f" => :mojave
+    sha256 "f3ac4eb10b1822e7e5a738eb2f5a4105c4428c2d3cede90e059c029b5bf2702e" => :high_sierra
+  end
+
   depends_on "go" => :build
 
   def install
