ab3d5e405e15833399bd2dffb13ec9cab2026280
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-10 13:15:59 +0000

    docker-compose: update 1.25.5 bottle.

--- Formula/docker-compose.rb
+++ Formula/docker-compose.rb
@@ -9,9 +9,9 @@ class DockerCompose < Formula
 
   bottle do
     cellar :any
-    sha256 "463bd4a012d5bed971040db53b737bd4c89ae9ee05a14d4d738cf06e9bebd9f9" => :catalina
-    sha256 "e3572703ef4cf05000d0eac018b0a01d2d82d7a54941667f2581bfd26e12959f" => :mojave
-    sha256 "3c74f2f820c364a88e66667a372fa0c5bf06516e8f0e234b70990cc1cf99fd80" => :high_sierra
+    sha256 "8b8fda9528565e7ed389826de46237aec1950e5b0a7c44311e3801afe681da68" => :catalina
+    sha256 "7b98ac39666ff2649e180b2208353735eac99af16af0f67241735b32bcb22679" => :mojave
+    sha256 "e85536c60d7dd1325504d9799c89d693c7803690ea92e7063cbe9ea18cbd8bea" => :high_sierra
   end
 
   depends_on "libyaml"
