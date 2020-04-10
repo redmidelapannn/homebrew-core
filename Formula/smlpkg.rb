77088b57e6d741395d6bb867294983d86017fd82
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-10 04:49:57 +0000

    smlpkg: add 0.1.3 bottle.

--- Formula/smlpkg.rb
+++ Formula/smlpkg.rb
@@ -5,6 +5,13 @@ class Smlpkg < Formula
   sha256 "cfa7eeff951e04df428694fda38917ee2aaaf0532e2d1dbea7ab4c150f4fe2f0"
   head "https://github.com/diku-dk/smlpkg.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "764defa53ad80c91e06c6441283420a90971ccb73d095feb2688cc0b9a83f139" => :catalina
+    sha256 "99fb2219f67e27bbc3d59f115f49d63841eecf0b80142bcbd90c989eaac2280a" => :mojave
+    sha256 "7e66d64ab4b69870ecce31a0948a8b240de4097e29a64d394cb1ca23874d0a8d" => :high_sierra
+  end
+
   depends_on "mlkit"
 
   def install
