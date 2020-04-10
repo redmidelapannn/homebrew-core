e2e32dd378a22dbb5bce6363cdd96eb327767a37
404NetworkError <404NetworkError@users.noreply.github.com>
2020-04-10 07:13:30 -0400

    ocrmypdf: revision for qpdf & update pikepdf resource to 1.11.0

--- Formula/ocrmypdf.rb
+++ Formula/ocrmypdf.rb
@@ -5,6 +5,7 @@ class Ocrmypdf < Formula
   homepage "https://github.com/jbarlow83/OCRmyPDF"
   url "https://files.pythonhosted.org/packages/48/78/da99a62b459eac0069b11698027b7db964f6b00541ea7fb6d510599cec24/ocrmypdf-9.7.0.tar.gz"
   sha256 "78adc233a4de86c852777c64fd2126b2f8f188dc98551d6ccb6b02eb61bf6b69"
+  revision 1
 
   bottle do
     cellar :any
@@ -58,8 +59,8 @@ class Ocrmypdf < Formula
   end
 
   resource "pikepdf" do
-    url "https://files.pythonhosted.org/packages/7c/36/12fa475b50ad6740c78873b9a801c3c29e2212eb18a2847f39fcef3c1ed7/pikepdf-1.10.4.tar.gz"
-    sha256 "33cb95de843947222686d584852152e95a1a4e40f80f9fbd9c87e82f2993c15a"
+    url "https://files.pythonhosted.org/packages/96/6b/fa2619a014453b67c3152222a1fdaeda84d6ce30dd9bf799760fe6946456/pikepdf-1.11.0.tar.gz"
+    sha256 "bad914a8b1d9be0a7451feba29f25dbc47e14aed85fdb11bfa1b3405a3ca3a6d"
   end
 
   resource "Pillow" do
